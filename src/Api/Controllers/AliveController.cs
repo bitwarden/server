1da53f0ecc1d11602dde913d8744d16d927cc4b2
Kyle Spearrin <kyle.spearrin@gmail.com>
2016-02-20 23:29:28 -0500

    added alive controller and endpoint

---
+++ src/Api/Controllers/AliveController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+
+namespace Bit.Api.Controllers
+{
+    [Route("alive")]
+    public class AliveController : Controller
+    {
+        [HttpGet("")]
+        public DateTime Get()
+        {
+            return DateTime.UtcNow;
+        }
+    }
+}
