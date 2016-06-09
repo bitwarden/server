8664b6bb2e0d586e591a7d253a1abd537086b8a7
Kyle Spearrin <kyle.spearrin@gmail.com>
2016-06-09 18:53:53 -0400

    remove old doc db settings

--- src/Core/GlobalSettings.cs
+++ src/Core/GlobalSettings.cs
@@ -8,19 +8,9 @@ namespace Bit.Core
         public virtual string SiteName { get; set; }
         public virtual string BaseVaultUri { get; set; }
         public virtual string JwtSigningKey { get; set; }
-        public virtual DocumentDBSettings DocumentDB { get; set; } = new DocumentDBSettings();
         public virtual SqlServerSettings SqlServer { get; set; } = new SqlServerSettings();
         public virtual MailSettings Mail { get; set; } = new MailSettings();
         public virtual LoggrSettings Loggr { get; set; } = new LoggrSettings();
-
-        public class DocumentDBSettings
-        {
-            public string Uri { get; set; }
-            public string Key { get; set; }
-            public string DatabaseId { get; set; }
-            public string CollectionIdPrefix { get; set; }
-            public int NumberOfCollections { get; set; }
-        }
 
         public class SqlServerSettings
         {
