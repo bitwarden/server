1dabed975eed8a883f0d5c8859980b340cef3af9
Kyle Spearrin <kyle.spearrin@gmail.com>
2016-10-06 19:12:16 -0400

    added mail project for designing mail templates

---
+++ src/Mail/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bit.Mail
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services) { }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseFileServer();
+            app.UseBrowserLink();
+        }
+    }
+}
---
+++ src/Mail/gulpfile.js
@@ -0,0 +1,20 @@
+﻿/// <binding BeforeBuild='build, dist' Clean='clean' ProjectOpened='build. dist' />
+
+var gulp = require('gulp'),
+    rimraf = require('rimraf'),
+    premailer = require('gulp-premailer');
+
+var paths = {
+    dist: '../../mail_dist/',
+    wwwroot: './wwwroot/'
+};
+
+gulp.task('inline', ['clean'], function () {
+    return gulp.src(paths.wwwroot + 'templates/*.html')
+        .pipe(premailer())
+        .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('clean', function (cb) {
+    return rimraf(paths.dist, cb);
+});
