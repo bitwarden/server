b08c960cc0a46d03bf32c18eb0787edecd770e5a
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-01-20 22:29:01 -0500

    Noop services

---
+++ src/Core/Services/Implementations/NoopMailService.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Bit.Core.Domains;
+
+namespace Bit.Core.Services
+{
+    public class NoopMailService : IMailService
+    {
+        public Task SendChangeEmailAlreadyExistsEmailAsync(string fromEmail, string toEmail)
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task SendChangeEmailEmailAsync(string newEmailAddress, string token)
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task SendMasterPasswordHintEmailAsync(string email, string hint)
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task SendNoMasterPasswordHintEmailAsync(string email)
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task SendWelcomeEmailAsync(User user)
+        {
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ src/Core/Services/Implementations/NoopPushService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Bit.Core.Domains;
+
+namespace Bit.Core.Services
+{
+    public class NoopPushService : IPushService
+    {
+        public Task PushSyncCipherCreateAsync(Cipher cipher)
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task PushSyncCipherDeleteAsync(Cipher cipher)
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task PushSyncCiphersAsync(Guid userId)
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task PushSyncCipherUpdateAsync(Cipher cipher)
+        {
+            return Task.FromResult(0);
+        }
+    }
+}
