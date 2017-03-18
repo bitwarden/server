3932ed5d67c71a2b28ab705695972ef88e96ea99
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-03-18 00:01:11 -0400

    folder repo

---
+++ src/Core/Repositories/IFolderRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Bit.Core.Models.Table;
+
+namespace Bit.Core.Repositories
+{
+    public interface IFolderRepository : IRepository<Folder, Guid>
+    {
+    }
+}
---
+++ src/Core/Repositories/SqlServer/FolderRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Bit.Core.Models.Table;
+
+namespace Bit.Core.Repositories.SqlServer
+{
+    public class FolderRepository : Repository<Folder, Guid>, IFolderRepository
+    {
+        public FolderRepository(GlobalSettings globalSettings)
+            : this(globalSettings.SqlServer.ConnectionString)
+        { }
+
+        public FolderRepository(string connectionString)
+            : base(connectionString)
+        { }
+    }
+}
