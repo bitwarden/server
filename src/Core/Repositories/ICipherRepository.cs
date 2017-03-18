d7a1cd1ce3ae85a015727c54ccf0c0b3dcea3acd
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-03-18 00:02:06 -0400

    cipher repo interface to use details

--- src/Core/Repositories/ICipherRepository.cs
+++ src/Core/Repositories/ICipherRepository.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Bit.Core.Models.Table;
+using Core.Models.Data;
 
 namespace Bit.Core.Repositories
 {
     public interface ICipherRepository : IRepository<Cipher, Guid>
     {
         Task<Cipher> GetByIdAsync(Guid id, Guid userId);
-        Task<ICollection<Cipher>> GetManyByUserIdAsync(Guid userId);
-        Task<ICollection<Cipher>> GetManyByTypeAndUserIdAsync(Enums.CipherType type, Guid userId);
-        Task<Tuple<ICollection<Cipher>, ICollection<Guid>>> GetManySinceRevisionDateAndUserIdWithDeleteHistoryAsync(
+        Task<ICollection<CipherDetails>> GetManyByUserIdAsync(Guid userId);
+        Task<ICollection<CipherDetails>> GetManyByTypeAndUserIdAsync(Enums.CipherType type, Guid userId);
+        Task<Tuple<ICollection<CipherDetails>, ICollection<Guid>>> GetManySinceRevisionDateAndUserIdWithDeleteHistoryAsync(
             DateTime sinceRevisionDate, Guid userId);
         Task UpdateUserEmailPasswordAndCiphersAsync(User user, IEnumerable<Cipher> ciphers);
         Task CreateAsync(IEnumerable<Cipher> ciphers);
