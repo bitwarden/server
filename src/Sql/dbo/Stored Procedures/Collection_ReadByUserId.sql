564b8194c1cb76b1272e7946f9816cf94492c228
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-05-15 21:39:28 -0400

    only return collections for confirmed users

--- src/Sql/dbo/Stored Procedures/Collection_ReadByUserId.sql
+++ src/Sql/dbo/Stored Procedures/Collection_ReadByUserId.sql
@@ -10,6 +10,8 @@ BEGIN
         [dbo].[CollectionView] C
     INNER JOIN
         [dbo].[OrganizationUser] OU ON C.[OrganizationId] = OU.[OrganizationId]
+    INNER JOIN
+        [dbo].[Organization] O ON C.[OrganizationId] = O.[Id]
     LEFT JOIN
         [dbo].[CollectionUser] CU ON OU.[AccessAll] = 0 AND CU.[CollectionId] = C.[Id] AND CU.[OrganizationUserId] = [OU].[Id]
     LEFT JOIN
@@ -20,6 +22,8 @@ BEGIN
         [dbo].[CollectionGroup] CG ON G.[AccessAll] = 0 AND CG.[CollectionId] = C.[Id] AND CG.[GroupId] = GU.[GroupId]
     WHERE
         OU.[UserId] = @UserId
+        AND OU.[Status] = 2 -- 2 = Confirmed
+        AND O.[Enabled] = 1
         AND (
             OU.[AccessAll] = 1
             OR CU.[CollectionId] IS NOT NULL
