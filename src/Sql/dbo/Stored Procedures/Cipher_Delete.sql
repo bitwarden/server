d3499dce84525ddece3c25375d0551d6511d1be8
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-06-08 23:49:55 -0400

    centralize select logic to UserCipherDetails func
    - Also create cipher bulk move and delete sprocs

---
+++ src/Sql/dbo/Stored Procedures/Cipher_Delete.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [dbo].[Cipher_Delete]
+    @Ids AS [dbo].[GuidIdArray] READONLY,
+    @UserId AS UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    ;WITH [CTE] AS (
+        SELECT
+            [Id],
+            [Edit]
+        FROM
+            [dbo].[UserCipherDetails](@UserId)
+    )
+    DELETE
+    FROM
+        [CTE]
+    WHERE
+        [Edit] = 1
+        AND [Id] IN (@Ids)
+
+    EXEC [dbo].[User_BumpAccountRevisionDate] @UserId
+    -- TODO: What if some that were deleted were organization ciphers? Then bump by org ids.
+END
---
+++ src/Sql/dbo/Stored Procedures/Cipher_Move.sql
@@ -0,0 +1,27 @@
+﻿CREATE PROCEDURE [dbo].[Cipher_Move]
+    @Ids AS [dbo].[GuidIdArray] READONLY,
+    @FolderId AS UNIQUEIDENTIFIER,
+    @UserId AS UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    ;WITH [CTE] AS (
+        SELECT
+            [Id],
+            [Edit],
+            [FolderId]
+        FROM
+            [dbo].[UserCipherDetails](@UserId)
+    )
+    UPDATE
+        [CTE]
+    SET
+        [FolderId] = @FolderId
+    WHERE
+        [Edit] = 1
+        AND [Id] IN (@Ids)
+
+    EXEC [dbo].[User_BumpAccountRevisionDate] @UserId
+    -- TODO: What if some that were updated were organization ciphers? Then bump by org ids.
+END
