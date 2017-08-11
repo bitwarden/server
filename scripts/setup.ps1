02bb037e38bc7fc676ba617832f813650f8d8ade
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-08-11 15:05:50 -0400

    sick ascii art yo

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -2,11 +2,28 @@ param (
     [string]$outputDir = "c:/bitwarden"
 )
 
+$year = (Get-Date).year
+
+Write-Host @'
+ _     _ _                         _            
+| |__ (_) |___      ____ _ _ __ __| | ___ _ __  
+| '_ \| | __\ \ /\ / / _` | '__/ _` |/ _ \ '_ \ 
+| |_) | | |_ \ V  V / (_| | | | (_| |  __/ | | |
+|_.__/|_|\__| \_/\_/ \__,_|_|  \__,_|\___|_| |_|
+'@
+
+Write-Host "
+Open source password management solutions
+Copyright 2015-${year}, 8bit Solutions LLC
+https://bitwarden.com, https://github.com/bitwarden
+"
+
 if(!(Test-Path -Path $outputDir )){
     New-Item -ItemType directory -Path $outputDir
 }
 
 docker --version
+echo ""
 
 [string]$installId = $( Read-Host "(!) Enter your installation id (get it at https://bitwarden.com/host/)" )
 [string]$installKey = $( Read-Host "(!) Enter your installation key" )
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,29 @@
 #!/usr/bin/env bash
 set -e
 
-OUTPUT_DIR=/etc/bitwarden
-mkdir -p $OUTPUT_DIR
+YEAR=$(date +'%Y')
+
+cat << "EOF"
+ _     _ _                         _            
+| |__ (_) |___      ____ _ _ __ __| | ___ _ __  
+| '_ \| | __\ \ /\ / / _` | '__/ _` |/ _ \ '_ \ 
+| |_) | | |_ \ V  V / (_| | | | (_| |  __/ | | |
+|_.__/|_|\__| \_/\_/ \__,_|_|  \__,_|\___|_| |_|
+
+EOF
+
+cat << EOF
+Open source password management solutions
+Copyright 2015-$YEAR, 8bit Solutions LLC
+https://bitwarden.com, https://github.com/bitwarden
+
+EOF
 
 docker --version
+echo ""
+
+OUTPUT_DIR=/etc/bitwarden
+mkdir -p $OUTPUT_DIR
 
 echo "(!) Enter your installation id (get it at https://bitwarden.com/host/): "
 read INSTALL_ID
