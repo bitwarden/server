72848a0623e3747659203526a9e2295b3c460a0d
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-08-12 23:10:01 -0400

    added more gateway enums

--- src/Core/Enums/GatewayType.cs
+++ src/Core/Enums/GatewayType.cs
@@ -3,6 +3,9 @@
     public enum GatewayType : byte
     {
         Stripe = 0,
-        Braintree = 1
+        Braintree = 1,
+        AppStore = 2,
+        PlayStore = 3,
+        Coinbase = 4
     }
 }
