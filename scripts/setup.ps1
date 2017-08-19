fb80878bb0b874719e69c16d914fafff44045734
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-08-19 09:36:03 -0400

    update setup

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -25,12 +25,8 @@ if(!(Test-Path -Path $outputDir )){
 docker --version
 echo ""
 
-[string]$installId = $( Read-Host "(!) Enter your installation id (get it at https://bitwarden.com/host/)" )
-[string]$installKey = $( Read-Host "(!) Enter your installation key" )
 [string]$domain = $( Read-Host "(!) Enter the domain name for your bitwarden instance (ex. bitwarden.company.com)" )
 [string]$letsEncrypt = $( Read-Host "(!) Do you want to use Let's Encrypt to generate a free SSL certificate? (y/n)" )
-
-$databasePassword=-join ((48..57) + (97..122) | Get-Random -Count 32 | % {[char]$_})
 
 if($letsEncrypt -eq "y") {
     [string]$email = $( Read-Host "(!) Enter your email address (Let's Encrypt will send you certificate expiration reminders)" )
@@ -39,9 +35,11 @@ if($letsEncrypt -eq "y") {
     if(!(Test-Path -Path $letsEncryptPath )){
         New-Item -ItemType directory -Path $letsEncryptPath
     }
-    docker run -it --rm --name certbot -p 80:80 -v $outputDir/letsencrypt:/etc/letsencrypt/ certbot/certbot certonly --standalone --noninteractive --preferred-challenges http --email $email --agree-tos -d $domain
+    docker run -it --rm --name certbot -p 80:80 -v $outputDir/letsencrypt:/etc/letsencrypt/ certbot/certbot `
+        certonly --standalone --noninteractive --agree-tos --preferred-challenges http --email $email -d $domain
 }
 
-docker run -it --rm --name setup -v ${outputDir}:/bitwarden bitwarden/setup dotnet Setup.dll -domain ${domain} -letsencrypt ${letsEncrypt} -db_pass ${databasePassword} -install_id $installId -install_key $installKey
+docker run -it --rm --name setup -v ${outputDir}:/bitwarden bitwarden/setup `
+    dotnet Setup.dll -install 1 -domain ${domain} -letsencrypt ${letsEncrypt}
 
 echo "Setup complete"
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -25,25 +25,21 @@ echo ""
 OUTPUT_DIR=/etc/bitwarden
 mkdir -p $OUTPUT_DIR
 
-echo "(!) Enter your installation id (get it at https://bitwarden.com/host/): "
-read INSTALL_ID
-echo -e "\n(!) Enter your installation key: "
-read INSTALL_KEY
-echo -e "\n(!) Enter the domain name for your bitwarden instance (ex. bitwarden.company.com): "
+echo -e "(!) Enter the domain name for your bitwarden instance (ex. bitwarden.company.com): "
 read DOMAIN
 echo -e "\n(!) Do you want to use Let's Encrypt to generate a free SSL certificate? (y/n): "
 read LETS_ENCRYPT
-
-DATABASE_PASSWORD=$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 32)
 
 if [ $LETS_ENCRYPT == 'y' ]
 then
     echo -e "\n(!) Enter your email address (Let's Encrypt will send you certificate expiration reminders): "
     read EMAIL
     mkdir -p $OUTPUT_DIR/letsencrypt/live/$DOMAIN
-    docker run -it --rm --name certbot -p 80:80 -v $OUTPUT_DIR/letsencrypt:/etc/letsencrypt/ certbot/certbot certonly --standalone --noninteractive --preferred-challenges http --email $EMAIL --agree-tos -d $DOMAIN
+    docker run -it --rm --name certbot -p 80:80 -v $OUTPUT_DIR/letsencrypt:/etc/letsencrypt/ certbot/certbot \
+        certonly --standalone --noninteractive  --agree-tos --preferred-challenges http --email $EMAIL -d $DOMAIN
 fi
 
-docker run -it --rm --name setup -v $OUTPUT_DIR:/bitwarden bitwarden/setup dotnet Setup.dll -domain $DOMAIN -letsencrypt $LETS_ENCRYPT -db_pass $DATABASE_PASSWORD -install_id $INSTALL_ID -install_key $INSTALL_KEY
+docker run -it --rm --name setup -v $OUTPUT_DIR:/bitwarden bitwarden/setup \
+    dotnet Setup.dll -install 1 -domain $DOMAIN -letsencrypt $LETS_ENCRYPT
 
 echo -e "\nSetup complete"
