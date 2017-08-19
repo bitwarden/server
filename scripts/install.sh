df67bb796bf31c42fff7393ac6f2f18ee4e92688
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-08-19 15:52:10 -0400

    more script fixes

--- scripts/install.sh
+++ scripts/install.sh
@@ -30,8 +30,7 @@ read -p "(!) Do you want to use Let's Encrypt to generate a free SSL certificate
 
 if [ $LETS_ENCRYPT == 'y' ]
 then
-    echo -e "\n(!) Enter your email address (Let's Encrypt will send you certificate expiration reminders): "
-    read EMAIL
+    read -p "(!) Enter your email address (Let's Encrypt will send you certificate expiration reminders): " EMAIL
     mkdir -p $OUTPUT_DIR/letsencrypt/live/$DOMAIN
     docker run -it --rm --name certbot -p 80:80 -v $OUTPUT_DIR/letsencrypt:/etc/letsencrypt/ certbot/certbot \
         certonly --standalone --noninteractive  --agree-tos --preferred-challenges http --email $EMAIL -d $DOMAIN
@@ -40,4 +39,5 @@ fi
 docker run -it --rm --name setup -v $OUTPUT_DIR:/bitwarden bitwarden/setup \
     dotnet Setup.dll -install 1 -domain $DOMAIN -letsencrypt $LETS_ENCRYPT
 
-echo -e "\nSetup complete"
+echo ""
+echo "Setup complete"
