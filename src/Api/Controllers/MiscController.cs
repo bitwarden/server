bac8ef9013b2b32ad6cc03b94df6032380d53874
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-08-19 07:59:19 -0400

    version api and re-route other misc apis

---
+++ src/Api/Controllers/MiscController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Bit.Core.Models.Api;
+
+namespace Bit.Api.Controllers
+{
+    public class MiscController : Controller
+    {
+        [HttpGet("~/alive")]
+        public DateTime Get()
+        {
+            return DateTime.UtcNow;
+        }
+
+        [HttpGet("~/claims")]
+        public IActionResult Claims()
+        {
+            return new JsonResult(User?.Claims?.Select(c => new { c.Type, c.Value }));
+        }
+
+        [HttpGet("~/version")]
+        public VersionResponseModel Version()
+        {
+            return new VersionResponseModel();
+        }
+    }
+}
---
+++ src/Core/Models/Api/Response/VersionResponseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+namespace Bit.Core.Models.Api
+{
+    public class VersionResponseModel : ResponseModel
+    {
+        public VersionResponseModel()
+            : base("version")
+        {
+            Version = Assembly.GetEntryAssembly()
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                .InformationalVersion;
+            VersionInt = Convert.ToInt32(Version.Replace(".", string.Empty));
+        }
+
+        public string Version { get; set; }
+        public int VersionInt { get; set; }
+    }
+}
