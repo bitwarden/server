8bbaa40307ace1e3a6f110d1616e523552cb78c3
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-08-19 16:17:32 -0400

    accept output param

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,28 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-YEAR=$(date +'%Y')
-
-cat << "EOF"
- _     _ _                         _            
-| |__ (_) |___      ____ _ _ __ __| | ___ _ __  
-| '_ \| | __\ \ /\ / / _` | '__/ _` |/ _ \ '_ \ 
-| |_) | | |_ \ V  V / (_| | | | (_| |  __/ | | |
-|_.__/|_|\__| \_/\_/ \__,_|_|  \__,_|\___|_| |_|
-
-EOF
-
-cat << EOF
-Open source password management solutions
-Copyright 2015-$YEAR, 8bit Solutions LLC
-https://bitwarden.com, https://github.com/bitwarden
-
-EOF
-
 docker --version
 echo ""
 
 OUTPUT_DIR=~/bitwarden
+if [ $# -eq 1 ]
+then
+    OUTPUT_DIR=$1
+fi
 mkdir -p $OUTPUT_DIR
 
 read -p "(!) Enter the domain name for your bitwarden instance (ex. bitwarden.company.com): " DOMAIN
