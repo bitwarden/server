ba640736835822c7f3dcfeef3dfb6839c411d891
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-08-23 22:39:31 -0400

    Nginx

--- util/Nginx/build.sh
+++ util/Nginx/build.sh
@@ -3,7 +3,7 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo -e "\n## Building nginx"
+echo -e "\n## Building Nginx"
 
 echo -e "\nBuilding docker image"
 docker --version
