ccd6b784bef40e1681c46498bc1b905c0f78d729
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-08-29 23:05:52 -0400

    cleanup old backups

--- util/MsSql/backup-db.sh
+++ util/MsSql/backup-db.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# Delete backup files older than 30 days
+find /etc/bitwarden/mssql/backups/ -type f -name '*.BAK' -mindepth 1 -mtime +30 -delete
+
+# Do a new backup
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P ${SA_PASSWORD} -i /backup-db.sql
