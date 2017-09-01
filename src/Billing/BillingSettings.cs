bd567e985362d3e7e577560dc18249bbfd1f66e4
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-09-01 14:07:50 -0400

    remove unused jobskey

--- src/Billing/BillingSettings.cs
+++ src/Billing/BillingSettings.cs
@@ -5,6 +5,5 @@
         public virtual string StripeWebhookKey { get; set; }
         public virtual string StripeWebhookSecret { get; set; }
         public virtual string BraintreeWebhookKey { get; set; }
-        public virtual string JobsKey { get; set; }
     }
 }
