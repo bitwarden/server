753496b95dff67be3254b64f0114abec06533f97
Hinton <hinton.oscar@gmail.com>
2017-10-09 18:58:59 +0200

    Use In-memory cache instead of custom file cache.

--- src/Icons/Controllers/IconController.cs
+++ src/Icons/Controllers/IconController.cs
@@ -1,70 +1,52 @@
 ﻿using System;
-using System.IO;
 using System.Net.Http;
-using System.Runtime.Serialization.Formatters.Binary;
 using System.Threading.Tasks;
 using Icons.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Icons.Controllers
 {
     [Route("[controller]")]
     public class IconController : Controller
     {
-        private readonly IHostingEnvironment _hostingEnvironment;
+        private readonly IMemoryCache _cache;
 
-        public IconController(IHostingEnvironment hostingEnvironment)
+        public IconController(IMemoryCache memoryCache)
         {
-            _hostingEnvironment = hostingEnvironment;
+            this._cache = memoryCache;
         }
 
         [HttpGet]
-        public async Task<FileResult> Get([FromQuery] string domain)
+        public async Task<IActionResult> Get([FromQuery] string domain)
         {
             var uri = BuildUrl(domain);
-            var fileName = $"{_hostingEnvironment.ContentRootPath}/cache/{domain}.cache";
 
-            // Attempt to load the icon from the cache.
-            if (FileExists(fileName))
+            Icon icon = await _cache.GetOrCreateAsync(domain, async entry =>
             {
-                using (Stream stream = System.IO.File.Open(fileName, FileMode.Open))
+                entry.AbsoluteExpiration = DateTime.Now.AddDays(1);
+
+                var httpClient = new HttpClient();
+                var response = await httpClient.GetAsync(uri);
+
+                if (!response.IsSuccessStatusCode)
                 {
-                    var binaryFormatter = new BinaryFormatter();
-                    var icon = (Icon)binaryFormatter.Deserialize(stream);
-
-                    if (icon.HasNotExpired())
-                    {
-                        return new FileContentResult(icon.Image, icon.Format);
-                    }
+                    return null;
                 }
-            }
 
-            var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync(uri);
-
-            if (!response.IsSuccessStatusCode)
-            {
-                throw new Exception("Cannot load the image");
-            }
-
-            // Serialize the icon.
-            using (Stream stream = System.IO.File.Open(fileName, FileMode.Create))
-            {
-                var icon = new Icon(
+                return new Icon(
                     await response.Content.ReadAsByteArrayAsync(),
                     response.Content.Headers.ContentType.MediaType
                 );
+            });
 
-                var binaryFormatter = new BinaryFormatter();
-                binaryFormatter.Serialize(stream, icon);
-                return new FileContentResult(icon.Image, icon.Format);
+            if (icon == null)
+            {
+                return NotFound("Cannot load the icon.");
             }
-        }
 
-        private static bool FileExists(string fileName)
-        {
-            return System.IO.File.Exists(fileName);
+            return new FileContentResult(icon.Image, icon.Format);
         }
 
         private static string BuildUrl(string domain)
--- src/Icons/Startup.cs
+++ src/Icons/Startup.cs
@@ -23,6 +23,7 @@ namespace Icons
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMemoryCache();
             services.AddMvc();
         }
 
