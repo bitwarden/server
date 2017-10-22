294a80436e328e23742ad3a50884ef30e9bf89fc
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-10-21 23:16:06 -0400

    fix walgreens photo mapping

--- src/Icons/Services/DomainMappingService.cs
+++ src/Icons/Services/DomainMappingService.cs
@@ -8,7 +8,7 @@ namespace Bit.Icons.Services
         {
             ["login.yahoo.com"] = "yahoo.com",
             ["accounts.google.com"] = "google.com",
-            ["photo.wallgreens.com"] = "wallgreens.com",
+            ["photo.walgreens.com"] = "walgreens.com",
             ["passport.yandex.com"] = "yandex.com",
             // TODO: Add others here
         };
