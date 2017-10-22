184c5f7f968f0cb5c01e19858b6c02b32f64f630
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-10-21 22:06:21 -0400

    map yandex

--- src/Icons/Services/DomainMappingService.cs
+++ src/Icons/Services/DomainMappingService.cs
@@ -9,6 +9,7 @@ namespace Bit.Icons.Services
             ["login.yahoo.com"] = "yahoo.com",
             ["accounts.google.com"] = "google.com",
             ["photo.wallgreens.com"] = "wallgreens.com",
+            ["passport.yandex.com"] = "yandex.com",
             // TODO: Add others here
         };
 
