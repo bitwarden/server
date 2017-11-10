6110da4ba95197bd95b73741459f2732814faa5b
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-11-10 15:22:19 -0500

    marry existing users before adding

--- src/Core/Services/Implementations/OrganizationService.cs
+++ src/Core/Services/Implementations/OrganizationService.cs
@@ -1184,10 +1184,28 @@ namespace Bit.Core.Services
                 }
             }
 
-            // Add new users
             if(newUsers?.Any() ?? false)
             {
-                var existingUsersSet = new HashSet<string>(existingExternalUsers.Select(u => u.ExternalId));
+                // Marry existing users
+                var existingUsersEmailsDict = existingUsers
+                    .Where(u => string.IsNullOrWhiteSpace(u.ExternalId))
+                    .ToDictionary(u => u.Email);
+                var newUsersEmailsDict = newUsers.ToDictionary(u => u.Email);
+                var usersToAttach = existingUsersEmailsDict.Keys.Intersect(newUsersEmailsDict.Keys).ToList();
+                foreach(var user in usersToAttach)
+                {
+                    var orgUserDetails = existingUsersEmailsDict[user];
+                    var orgUser = await _organizationUserRepository.GetByIdAsync(orgUserDetails.Id);
+                    if(orgUser != null)
+                    {
+                        orgUser.ExternalId = newUsersEmailsDict[user].ExternalId;
+                        await _organizationUserRepository.UpsertAsync(orgUser);
+                        existingExternalUsersIdDict.Add(orgUser.ExternalId, orgUser.Id);
+                    }
+                }
+
+                // Add new users
+                var existingUsersSet = new HashSet<string>(existingExternalUsersIdDict.Keys);
                 var usersToAdd = newUsersSet.Except(existingUsersSet).ToList();
 
                 var seatsAvailable = int.MaxValue;
@@ -1218,23 +1236,6 @@ namespace Bit.Core.Services
                         {
                             continue;
                         }
-                    }
-                }
-
-                var existingUsersEmailsDict = existingUsers
-                    .Where(u => string.IsNullOrWhiteSpace(u.ExternalId))
-                    .ToDictionary(u => u.Email);
-                var newUsersEmailsDict = newUsers.ToDictionary(u => u.Email);
-                var usersToAttach = existingUsersEmailsDict.Keys.Intersect(newUsersEmailsDict.Keys).ToList();
-                foreach(var user in usersToAttach)
-                {
-                    var orgUserDetails = existingUsersEmailsDict[user];
-                    var orgUser = await _organizationUserRepository.GetByIdAsync(orgUserDetails.Id);
-                    if(orgUser != null)
-                    {
-                        orgUser.ExternalId = newUsersEmailsDict[user].ExternalId;
-                        await _organizationUserRepository.UpsertAsync(orgUser);
-                        existingExternalUsersIdDict.Add(orgUser.ExternalId, orgUser.Id);
                     }
                 }
             }
