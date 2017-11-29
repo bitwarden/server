c988171f090100e2e6bd55a3644d71565f115f2d
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-11-29 13:40:56 -0500

    welcome email should use web vault url hostname

--- src/Core/Models/Mail/BaseMailModel.cs
+++ src/Core/Models/Mail/BaseMailModel.cs
@@ -1,8 +1,22 @@
-﻿namespace Bit.Core.Models.Mail
+﻿using System;
+
+namespace Bit.Core.Models.Mail
 {
     public class BaseMailModel
     {
         public string SiteName { get; set; }
         public string WebVaultUrl { get; set; }
+        public string WebVaultUrlHostname
+        {
+            get
+            {
+                if(Uri.TryCreate(WebVaultUrl, UriKind.Absolute, out Uri uri))
+                {
+                    return uri.Host;
+                }
+
+                return WebVaultUrl;
+            }
+        }
     }
 }
