755c73deba0e46a67b766920b331e8e2010a5663
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-12-04 10:12:11 -0500

    stub out events project

---
+++ src/Events/Controllers/EventsController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Events.Controllers
+{
+    public class EventsController : Controller
+    {
+        [HttpPost]
+        [Route("~/")]
+        public void Post([FromBody]string value)
+        {
+        }
+    }
+}
---
+++ src/Events/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bit.Events
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Options
+            services.AddOptions();
+
+            // Mvc
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if(env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
