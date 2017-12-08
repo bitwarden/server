8626d7e769206b4fbc118dd2e7d89a15e66543c1
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-12-08 15:02:54 -0500

    events processor web job project

---
+++ src/EventsProcessor/Functions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace Bit.EventsProcessor
+{
+    public class Functions
+    {
+        public async static Task ProcessQueueMessageAsync(
+            [QueueTrigger("event")] string message, TextWriter logger, CancellationToken token)
+        {
+            await logger.WriteLineAsync(message);
+        }
+    }
+}
---
+++ src/EventsProcessor/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.WebJobs;
+
+namespace Bit.EventsProcessor
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            var config = new JobHostConfiguration();
+            if(config.IsDevelopment)
+            {
+                config.UseDevelopmentSettings();
+            }
+
+            var host = new JobHost(config);
+            host.RunAndBlock();
+        }
+    }
+}
