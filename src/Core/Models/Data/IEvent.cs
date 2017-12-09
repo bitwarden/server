89d488739d36ebb945a0fecd918948afec538244
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-12-08 23:12:19 -0500

    re-order event interface

--- src/Core/Models/Data/IEvent.cs
+++ src/Core/Models/Data/IEvent.cs
@@ -5,14 +5,14 @@ namespace Bit.Core.Models.Data
 {
     public interface IEvent
     {
-        Guid? ActingUserId { get; set; }
-        Guid? CipherId { get; set; }
-        Guid? CollectionId { get; set; }
         DateTime Date { get; set; }
-        Guid? GroupId { get; set; }
-        Guid? OrganizationId { get; set; }
-        Guid? OrganizationUserId { get; set; }
         EventType Type { get; set; }
         Guid? UserId { get; set; }
+        Guid? OrganizationId { get; set; }
+        Guid? CipherId { get; set; }
+        Guid? CollectionId { get; set; }
+        Guid? GroupId { get; set; }
+        Guid? OrganizationUserId { get; set; }
+        Guid? ActingUserId { get; set; }
     }
 }
