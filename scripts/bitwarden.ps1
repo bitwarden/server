13ad536d9c8c871928c6d383bc7368b8fa6a8fa6
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-12-28 22:12:39 -0500

    update docker version refs

--- scripts/bitwarden.ps1
+++ scripts/bitwarden.ps1
@@ -42,8 +42,8 @@ if($output -eq "") {
 
 $scriptsDir = "${output}\scripts"
 $githubBaseUrl = "https://raw.githubusercontent.com/bitwarden/core/master"
-$coreVersion = "1.15.1"
-$webVersion = "1.21.0"
+$coreVersion = "1.16.0"
+$webVersion = "1.22.0"
 
 # Functions
 
