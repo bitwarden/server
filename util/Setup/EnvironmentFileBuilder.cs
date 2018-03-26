aa3223b9743fb52191cac7c9ba56adee2ab27ed4
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-03-26 14:57:10 -0400

    internal identity is on port 5000 now

--- util/Setup/EnvironmentFileBuilder.cs
+++ util/Setup/EnvironmentFileBuilder.cs
@@ -125,7 +125,7 @@ globalSettings__selfHosted=true
 globalSettings__baseServiceUri__vault=http://localhost
 globalSettings__baseServiceUri__api=http://localhost/api
 globalSettings__baseServiceUri__identity=http://localhost/identity
-globalSettings__baseServiceUri__internalIdentity=http://identity
+globalSettings__baseServiceUri__internalIdentity=http://identity:5000
 globalSettings__pushRelayBaseUri=https://push.bitwarden.com
 globalSettings__installation__identityUri=https://identity.bitwarden.com
 ");
