a790c37fcc5a9cff72455244a4b381f0d280de82
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-04-02 21:35:08 -0400

    docker version bump

--- scripts/bitwarden.ps1
+++ scripts/bitwarden.ps1
@@ -42,8 +42,8 @@ if($output -eq "") {
 
 $scriptsDir = "${output}\scripts"
 $githubBaseUrl = "https://raw.githubusercontent.com/bitwarden/core/master"
-$coreVersion = "1.18.0"
-$webVersion = "1.25.0"
+$coreVersion = "1.18.1"
+$webVersion = "1.25.1"
 
 # Functions
 
