8471f558e34e934df3e036b91765168a4f2d2ef5
Mart124 <37041094+Mart124@users.noreply.github.com>
2018-05-29 14:17:43 +0200

    Improve mssql backups (#298)
    * Improve mssql backups
    
    * Launch DB backup at 23:59

--- util/MsSql/backup-db.sql
+++ util/MsSql/backup-db.sql
@@ -1,11 +1,8 @@
-DECLARE @TheDate NVARCHAR(20)
-SET @TheDate = REPLACE(CONVERT(VARCHAR, GETDATE(),101),'/','') + '_' +  REPLACE(CONVERT(VARCHAR, GETDATE(),108),':','')
-
 DECLARE @BackupFile varchar(100)
-SET @BackupFile = '/etc/bitwarden/mssql/backups/vault_FULL_'+ @TheDate + '.BAK'
+SET @BackupFile = '/etc/bitwarden/mssql/backups/vault_FULL_$(now).BAK'
 
 DECLARE @BackupName varchar(100)
-SET @BackupName = 'vault full backup for '+ @TheDate
+SET @BackupName = 'vault full backup for $(now)'
 
 DECLARE @BackupCommand NVARCHAR(1000)
 SET @BackupCommand = 'BACKUP DATABASE [vault] TO DISK = ''' + @BackupFile + ''' WITH INIT, NAME= ''' + @BackupName + ''', NOSKIP, NOFORMAT'
