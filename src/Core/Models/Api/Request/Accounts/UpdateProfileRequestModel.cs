5c17f910fe8afa73831aee30ff5147317658b812
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-06-23 11:55:45 -0400

    remove culture from request model

--- src/Core/Models/Api/Request/Accounts/UpdateProfileRequestModel.cs
+++ src/Core/Models/Api/Request/Accounts/UpdateProfileRequestModel.cs
@@ -9,16 +9,11 @@ namespace Bit.Core.Models.Api
         public string Name { get; set; }
         [StringLength(50)]
         public string MasterPasswordHint { get; set; }
-        [Required]
-        [RegularExpression("^[a-z]{2}-[A-Z]{2}$")]
-        public string Culture { get; set; }
 
         public User ToUser(User existingUser)
         {
             existingUser.Name = Name;
             existingUser.MasterPasswordHint = string.IsNullOrWhiteSpace(MasterPasswordHint) ? null : MasterPasswordHint;
-            existingUser.Culture = Culture;
-
             return existingUser;
         }
     }
