8d0d54026b51a331d9b2e9c5e3dddd27962bc646
Mart124 <37041094+Mart124@users.noreply.github.com>
2018-08-08 14:59:42 +0200

    Prune our own images only, ps1 script

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -88,7 +88,7 @@ function Docker-Compose-Pull {
 }
 
 function Docker-Prune {
-    docker image prune -f
+    docker image prune -f --filter="label=com.bitwarden.product=bitwarden"
 }
 
 function Update-Lets-Encrypt {
