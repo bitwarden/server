502836d5c5c3cfbb1f4fcb8aa65a96a90a9f366e
Mart124 <37041094+Mart124@users.noreply.github.com>
2018-08-08 14:49:55 +0200

    Only prune our own images

--- scripts/run.sh
+++ scripts/run.sh
@@ -130,7 +130,8 @@ function dockerComposePull() {
 }
 
 function dockerPrune() {
-    docker image prune -f
+    docker image prune -f --filter="label=com.bitwarden.product=bitwarden"
+    # Perhaps we could add "-a", to recover disk space after automatic update ?
 }
 
 function updateLetsEncrypt() {
