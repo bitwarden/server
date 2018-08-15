580e9e51e50917a4a46fd29ebabd25be2b3ae15e
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-08-14 22:09:41 -0400

    remove mail and function projects

--- util/Function/Models/AccessRuleResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Bit.Function.Models
-{
-    public class AccessRuleResponse
-    {
-        public bool Success { get; set; }
-        public AccessRuleResultResponse Result { get; set; }
-    }
-}
