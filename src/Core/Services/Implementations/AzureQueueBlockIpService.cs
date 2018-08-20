ad5b71643add159c6509592513f406b587d87ced
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-08-20 16:26:15 -0400

    ip ban is for 30 minutes

--- src/Core/Services/Implementations/AzureQueueBlockIpService.cs
+++ src/Core/Services/Implementations/AzureQueueBlockIpService.cs
@@ -29,7 +29,7 @@ namespace Bit.Core.Services
 
             if(!permanentBlock)
             {
-                await _unblockIpQueue.AddMessageAsync(message, null, new TimeSpan(2, 0, 0), null, null);
+                await _unblockIpQueue.AddMessageAsync(message, null, new TimeSpan(0, 30, 0), null, null);
             }
         }
 
