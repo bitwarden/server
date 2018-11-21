86a2d89d6aa388086e731eb1c0c2bce8157d9ab5
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-11-21 14:52:53 -0500

    bump docker version

--- scripts/bitwarden.ps1
+++ scripts/bitwarden.ps1
@@ -43,7 +43,7 @@ if ($output -eq "") {
 
 $scriptsDir = "${output}\scripts"
 $githubBaseUrl = "https://raw.githubusercontent.com/bitwarden/core/master"
-$coreVersion = "1.26.0"
+$coreVersion = "1.27.0"
 $webVersion = "2.5.0"
 
 # Functions
--- scripts/bitwarden.sh
+++ scripts/bitwarden.sh
@@ -37,7 +37,7 @@ fi
 
 SCRIPTS_DIR="$OUTPUT/scripts"
 GITHUB_BASE_URL="https://raw.githubusercontent.com/bitwarden/core/master"
-COREVERSION="1.26.0"
+COREVERSION="1.27.0"
 WEBVERSION="2.5.0"
 
 # Functions
