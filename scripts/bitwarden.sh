199132e547429469f75c61fc44bc50a8ee97f31f
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-11-26 16:28:56 -0500

    bump docker version for web vault

--- scripts/bitwarden.sh
+++ scripts/bitwarden.sh
@@ -38,7 +38,7 @@ fi
 SCRIPTS_DIR="$OUTPUT/scripts"
 GITHUB_BASE_URL="https://raw.githubusercontent.com/bitwarden/core/master"
 COREVERSION="1.27.0"
-WEBVERSION="2.6.0"
+WEBVERSION="2.6.1"
 
 # Functions
 
