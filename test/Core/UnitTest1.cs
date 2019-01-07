3d6162504c34dc13060b7119e365069ab54307b5
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-01-07 17:14:42 -0500

    tsub out core test project

---
+++ test/Core/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Bit.Core.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
