5cdfc83903998e283be84acdb5182fe159606278
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-02-11 23:27:32 -0500

    remove bt webhook key

--- src/Billing/BillingSettings.cs
+++ src/Billing/BillingSettings.cs
@@ -5,7 +5,6 @@
         public virtual string JobsKey { get; set; }
         public virtual string StripeWebhookKey { get; set; }
         public virtual string StripeWebhookSecret { get; set; }
-        public virtual string BraintreeWebhookKey { get; set; }
         public virtual PayPalSettings PayPal { get; set; } = new PayPalSettings();
 
         public class PayPalSettings
