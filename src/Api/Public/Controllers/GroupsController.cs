176078f04bc881119f6b446ae19e9aeb473085aa
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-02-26 22:06:39 -0500

    Stub out public api for org groups

---
+++ src/Api/Public/Controllers/GroupsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bit.Api.Public.Controllers
+{
+    [Route("public/groups")]
+    [Authorize("Organization")]
+    public class GroupsController : Controller
+    {
+        [HttpGet("{id}")]
+        public JsonResult Get(string id)
+        {
+            return new JsonResult("Hello " + id);
+        }
+    }
+}
