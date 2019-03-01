6e4df8cb0ba8cf5ac7c24d2bad68589e795d4261
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-02-28 20:51:47 -0500

    test group get api for swagger

--- src/Api/Public/Controllers/GroupsController.cs
+++ src/Api/Public/Controllers/GroupsController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Bit.Core.Models.Api.Public;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,10 +9,23 @@ namespace Bit.Api.Public.Controllers
     [Authorize("Organization")]
     public class GroupsController : Controller
     {
+        /// <summary>
+        /// Retrieves a specific product by unique id
+        /// </summary>
+        /// <remarks>Awesomeness!</remarks>
+        /// <response code="200">Group created</response>
+        /// <response code="400">Group has missing/invalid values</response>
+        /// <response code="500">Oops! Can't create your product right now</response>
         [HttpGet("{id}")]
-        public JsonResult Get(string id)
+        [ProducesResponseType(typeof(GroupResponseModel), 200)]
+        public IActionResult Get(Guid id)
         {
-            return new JsonResult("Hello " + id);
+            return new JsonResult(new GroupResponseModel(new Core.Models.Table.Group
+            {
+                Id = id,
+                Name = "test",
+                OrganizationId = Guid.NewGuid()
+            }));
         }
     }
 }
---
+++ src/Core/Models/Api/Public/Response/GroupResponseModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Bit.Core.Models.Table;
+
+namespace Bit.Core.Models.Api.Public
+{
+    public class GroupResponseModel : ResponseModel
+    {
+        public GroupResponseModel(Group group, string obj = "group")
+            : base(obj)
+        {
+            if(group == null)
+            {
+                throw new ArgumentNullException(nameof(group));
+            }
+
+            Id = group.Id;
+            OrganizationId = group.OrganizationId;
+            Name = group.Name;
+            AccessAll = group.AccessAll;
+            ExternalId = group.ExternalId;
+        }
+
+        public Guid Id { get; set; }
+        public Guid OrganizationId { get; set; }
+        public string Name { get; set; }
+        public bool AccessAll { get; set; }
+        public string ExternalId { get; set; }
+    }
+}
