a6b14131ef805dd52c2cf9082b8bdcfa329d5685
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-03-06 09:15:23 -0500

    public collection apis

---
+++ src/Core/Models/Api/Public/Request/CollectionCreateUpdateRequestModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Bit.Core.Models.Table;
+
+namespace Bit.Core.Models.Api.Public
+{
+    public class CollectionCreateUpdateRequestModel : GroupBaseModel
+    {
+        /// <summary>
+        /// The associated groups that this collection is assigned to.
+        /// </summary>
+        public IEnumerable<AssociationWithPermissionsRequestModel> Groups { get; set; }
+
+        public Collection ToCollection(Collection existingCollection)
+        {
+            // TODO
+            // existingCollection.ExternalId = ExternalId;
+            return existingCollection;
+        }
+    }
+}
