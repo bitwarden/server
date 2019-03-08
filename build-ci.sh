10d6ace700073efcb7ac62ed7476d08b5229e704
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-03-08 14:16:44 -0500

    build-ci script

---
+++ build-ci.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Repo tag name: ${APPVEYOR_REPO_TAG_NAME}"
+echo "Rebuild: ${APPVEYOR_RE_BUILD}"
+
+PROD_DEPLOY=false
+TAG_NAME=""
+if [ "${APPVEYOR_REPO_TAG_NAME}" != "" -a "${APPVEYOR_RE_BUILD}" == "true" ]
+then
+    PROD_DEPLOY=true
+    TAG_NAME=${APPVEYOR_REPO_TAG_NAME#"v"}
+fi
+
+chmod +x ./build.sh
+
+#dotnet build --configuration Debug
+
+./build.sh
+./build.sh tag dev
+
+if [ $PROD_DEPLOY ]
+then
+    ./build.sh tag beta
+    ./build.sh tag $TAG_NAME
+fi
+
+docker images
+./build.sh push dev
+
+if [ $PROD_DEPLOY ]
+then
+    ./build.sh push beta
+    ./build.sh push latest
+    ./build.sh push $TAG_NAME
+fi
