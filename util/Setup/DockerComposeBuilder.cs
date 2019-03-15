5867d87faa80e06a67d98a8a499b6c0d34e17ada
Youri Westerman <youri.westerman@nedac.eu>
2019-03-15 14:38:57 +0100

    Use docker-compose file version 3.4

--- util/Setup/DockerComposeBuilder.cs
+++ util/Setup/DockerComposeBuilder.cs
@@ -62,7 +62,7 @@ namespace Bit.Setup
                 }
             }
 
-            public string ComposeVersion { get; set; } = "3";
+            public string ComposeVersion { get; set; } = "3.4";
             public bool MssqlDataDockerVolume { get; set; }
             public string HttpPort { get; set; }
             public string HttpsPort { get; set; }
