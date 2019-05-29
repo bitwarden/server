35804e10cfab823884bc39b495bd4131ef99b851
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-05-28 23:55:35 -0400

    collection cipher query improvements

--- util/Migrator/DbScripts/2019-05-28_00_CollectionCipherImprovements.sql
+++ util/Migrator/DbScripts/2019-05-28_00_CollectionCipherImprovements.sql
@@ -1,4 +1,67 @@
-﻿CREATE PROCEDURE [dbo].[CollectionCipher_UpdateCollections]
+﻿IF OBJECT_ID('[dbo].[CollectionCipher_UpdateCollectionsAdmin]') IS NOT NULL
+BEGIN
+    DROP PROCEDURE [dbo].[CollectionCipher_UpdateCollectionsAdmin]
+END
+GO
+
+CREATE PROCEDURE [dbo].[CollectionCipher_UpdateCollectionsAdmin]
+    @CipherId UNIQUEIDENTIFIER,
+    @OrganizationId UNIQUEIDENTIFIER,
+    @CollectionIds AS [dbo].[GuidIdArray] READONLY
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    ;WITH [AvailableCollectionsCTE] AS(
+        SELECT
+            Id
+        FROM
+            [dbo].[Collection]
+        WHERE
+            OrganizationId = @OrganizationId
+    ),
+    [CollectionCiphersCTE] AS(
+        SELECT
+            [CollectionId],
+            [CipherId]
+        FROM
+            [dbo].[CollectionCipher]
+        WHERE
+            [CipherId] = @CipherId
+    )
+    MERGE
+        [CollectionCiphersCTE] AS [Target]
+    USING 
+        @CollectionIds AS [Source]
+    ON
+        [Target].[CollectionId] = [Source].[Id]
+        AND [Target].[CipherId] = @CipherId
+    WHEN NOT MATCHED BY TARGET
+    AND [Source].[Id] IN (SELECT [Id] FROM [AvailableCollectionsCTE]) THEN
+        INSERT VALUES
+        (
+            [Source].[Id],
+            @CipherId
+        )
+    WHEN NOT MATCHED BY SOURCE
+    AND [Target].[CipherId] = @CipherId THEN
+        DELETE
+    ;
+
+    IF @OrganizationId IS NOT NULL
+    BEGIN
+        EXEC [dbo].[User_BumpAccountRevisionDateByOrganizationId] @OrganizationId
+    END
+END
+GO
+
+IF OBJECT_ID('[dbo].[CollectionCipher_UpdateCollections]') IS NOT NULL
+BEGIN
+    DROP PROCEDURE [dbo].[CollectionCipher_UpdateCollections]
+END
+GO
+
+CREATE PROCEDURE [dbo].[CollectionCipher_UpdateCollections]
     @CipherId UNIQUEIDENTIFIER,
     @UserId UNIQUEIDENTIFIER,
     @CollectionIds AS [dbo].[GuidIdArray] READONLY
@@ -77,3 +140,4 @@ BEGIN
         EXEC [dbo].[User_BumpAccountRevisionDateByOrganizationId] @OrgId
     END
 END
+GO
