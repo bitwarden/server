d009aa57c02ab1178c151b775f378bded5811834
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-09-13 16:10:20 -0400

    noop meta data repo

---
+++ src/Core/Repositories/Noop/MetaDataRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Bit.Core.Repositories.Noop
+{
+    public class MetaDataRepository : IMetaDataRespository
+    {
+        public Task DeleteAsync(string id)
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task<IDictionary<string, string>> GetAsync(string id)
+        {
+            return Task.FromResult(null as IDictionary<string, string>);
+        }
+
+        public Task<string> GetAsync(string id, string prop)
+        {
+            return Task.FromResult(null as string);
+        }
+
+        public Task UpsertAsync(string id, IDictionary<string, string> dict)
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task UpsertAsync(string id, KeyValuePair<string, string> keyValuePair)
+        {
+            return Task.FromResult(0);
+        }
+    }
+}
