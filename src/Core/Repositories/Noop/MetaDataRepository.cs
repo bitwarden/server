dcbe30b80f4629e0e4ea70614aa6c97777e88c08
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-09-17 16:09:23 -0400

    noop

--- src/Core/Repositories/Noop/MetaDataRepository.cs
+++ src/Core/Repositories/Noop/MetaDataRepository.cs
@@ -5,27 +5,27 @@ namespace Bit.Core.Repositories.Noop
 {
     public class MetaDataRepository : IMetaDataRespository
     {
-        public Task DeleteAsync(string id)
+        public Task DeleteAsync(string objectName, string id)
         {
             return Task.FromResult(0);
         }
 
-        public Task<IDictionary<string, string>> GetAsync(string id)
+        public Task<IDictionary<string, string>> GetAsync(string objectName, string id)
         {
             return Task.FromResult(null as IDictionary<string, string>);
         }
 
-        public Task<string> GetAsync(string id, string prop)
+        public Task<string> GetAsync(string objectName, string id, string prop)
         {
             return Task.FromResult(null as string);
         }
 
-        public Task UpsertAsync(string id, IDictionary<string, string> dict)
+        public Task UpsertAsync(string objectName, string id, IDictionary<string, string> dict)
         {
             return Task.FromResult(0);
         }
 
-        public Task UpsertAsync(string id, KeyValuePair<string, string> keyValuePair)
+        public Task UpsertAsync(string objectName, string id, KeyValuePair<string, string> keyValuePair)
         {
             return Task.FromResult(0);
         }
