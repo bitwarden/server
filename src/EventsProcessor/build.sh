e7625eda863bf521335dd063da61472ffae0ba6f
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-10-04 13:46:46 -0400

    build script

---
+++ src/EventsProcessor/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo -e "\n## Building Event Processor"
+
+echo -e "\nBuilding app"
+echo ".NET Core version $(dotnet --version)"
+echo "Restore"
+dotnet restore $DIR/Icons.csproj
+echo "Clean"
+dotnet clean $DIR/Icons.csproj -c "Release" -o $DIR/obj/Docker/publish
+echo "Publish"
+dotnet publish $DIR/Icons.csproj -c "Release" -o $DIR/obj/Docker/publish
+
+if [ "$1" != "nodocker" ]
+then
+    echo -e "\nBuilding docker image"
+    docker --version
+    docker build -t bitwarden/icons $DIR/.
+fi
