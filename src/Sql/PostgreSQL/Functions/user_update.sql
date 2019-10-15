64fa40ef4964385b4bbd14ddfd5a12faed962f6b
benjamin luton <benjamin@luton.net.au>
2019-10-16 08:51:46 +1000

    remove tabs

--- src/Sql/PostgreSQL/Functions/user_update.sql
+++ src/Sql/PostgreSQL/Functions/user_update.sql
@@ -1,44 +1,44 @@
 drop function user_update(UUID,VARCHAR,VARCHAR,INT4,VARCHAR,VARCHAR,VARCHAR,VARCHAR,TEXT,VARCHAR,TEXT,TEXT,TIMESTAMPTZ,TEXT,TEXT,TEXT,INT4,TEXT,TEXT,INT8,INT2,INT2,VARCHAR,VARCHAR,VARCHAR,INT4,INT4,TIMESTAMPTZ,TIMESTAMPTZ);
 
-CREATE OR REPLACE FUNCTION user_update 
+CREATE OR REPLACE FUNCTION user_update
 (
-    _id									UUID,
-    _name 								VARCHAR,
-    _email 								VARCHAR,
-    _email_verified 					INT,
-    _master_password 					VARCHAR,
-    _master_password_hint		 		VARCHAR,
-    _culture 							VARCHAR,
-    _security_stamp 					VARCHAR,
-    _two_factor_providers 				TEXT,
-    _two_factor_recovery_code 			VARCHAR,
-    _equivalent_domains 				TEXT,
+    _id                                 UUID,
+    _name                               VARCHAR,
+    _email                              VARCHAR,
+    _email_verified                     INT,
+    _master_password                    VARCHAR,
+    _master_password_hint               VARCHAR,
+    _culture                            VARCHAR,
+    _security_stamp                     VARCHAR,
+    _two_factor_providers               TEXT,
+    _two_factor_recovery_code           VARCHAR,
+    _equivalent_domains                 TEXT,
     _excluded_global_equivalent_domains TEXT,
-    _account_revision_date	 			TIMESTAMPTZ,
-    _key 								TEXT,
-    _public_key 						TEXT,
-    _private_key 						TEXT ,
-    _premium 							INT,
-    _premium_expiration_date 			TEXT,
-    _renewal_reminder_date 				TEXT,
-    _storage 							BIGINT,
-    _max_storage_gb 					SMALLINT,
-    _gateway 							SMALLINT,
-    _gateway_customer_id 				VARCHAR,
-    _gateway_subscription_id 			VARCHAR,
-    _license_key 						VARCHAR,
-    _kdf 								INT,
-    _kdf_iterations 					INT,
-    _creation_date 						TIMESTAMPTZ,
-    _revision_date 						TIMESTAMPTZ
+    _account_revision_date              TIMESTAMPTZ,
+    _key                                TEXT,
+    _public_key                         TEXT,
+    _private_key                        TEXT ,
+    _premium                            INT,
+    _premium_expiration_date            TEXT,
+    _renewal_reminder_date              TEXT,
+    _storage                            BIGINT,
+    _max_storage_gb                     SMALLINT,
+    _gateway                            SMALLINT,
+    _gateway_customer_id                VARCHAR,
+    _gateway_subscription_id            VARCHAR,
+    _license_key                        VARCHAR,
+    _kdf                                INT,
+    _kdf_iterations                     INT,
+    _creation_date                      TIMESTAMPTZ,
+    _revision_date                      TIMESTAMPTZ
 )
 RETURNS VOID
 LANGUAGE plpgsql
 AS
 $$
 begin
-	
-	UPDATE
+
+  UPDATE
         "user"
     SET name = _name,
         email = _email,
@@ -73,4 +73,3 @@ begin
         ;
 end;
 $$
-
