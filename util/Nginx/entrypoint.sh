820f136084086d46dda9dc2defd5978726a7ed7f
Mart124 <37041094+Mart124@users.noreply.github.com>
2019-11-21 20:40:26 +0100

    Rotate nginx logs

--- util/Nginx/entrypoint.sh
+++ util/Nginx/entrypoint.sh
@@ -41,4 +41,21 @@ chown -R $USERNAME:$GROUPNAME /var/run/nginx
 chown -R $USERNAME:$GROUPNAME /var/cache/nginx
 chown -R $USERNAME:$GROUPNAME /var/log/nginx
 
+# Launch a loop to rotate nginx logs on a daily basis
+# User can disable this setting env NGINX_LOGROTATE=0
+
+while [[ "$NGINX_LOGROTATE" != "0" ]]
+do
+  sleep $((24 * 3600 - (`date +%H` * 3600 + `date +%M` * 60 + `date +%S`)))
+  ts=$(date +%Y%m%d_%H%M%S)
+  mv /var/log/nginx/access.log /var/log/nginx/access.$ts.log
+  mv /var/log/nginx/error.log /var/log/nginx/error.$ts.log
+  kill -USR1 `cat /var/run/nginx/nginx.pid`
+  sleep 1
+  gzip /var/log/nginx/access.$ts.log
+  gzip /var/log/nginx/error.$ts.log
+  find /var/log/nginx/ -name "*.gz" -mtime +32 -delete
+done &
+disown %1
+
 exec gosu $USERNAME:$GROUPNAME nginx -g 'daemon off;'
