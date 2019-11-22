92935161448932520476e797c2cfd94b584642b7
Mart124 <37041094+Mart124@users.noreply.github.com>
2019-11-22 18:40:48 +0100

    Update backup-db.sh

--- util/MsSql/backup-db.sh
+++ util/MsSql/backup-db.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-# Backup timestamp
-export now=${1:-$(date +%Y%m%d_%H%M%S)}
+while [ "$SQL_BACKUP" != "0" ]
+do
+  # Let's sleep until 23:59
+  sleep $((24 * 3600 - 60 - (`date +%H` * 3600 + `date +%M` * 60 + `date +%S`)))
 
-# Do a new backup
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P ${SA_PASSWORD} -i /backup-db.sql
+  # Backup timestamp
+  export now=${1:-$(date +%Y%m%d_%H%M%S)}
 
-# Delete backup files older than 30 days
-grep -B1 "BACKUP DATABASE successfully" /var/opt/mssql/log/errorlog | grep -q _$now.BAK &&
-find /etc/bitwarden/mssql/backups/ -mindepth 1 -type f -name '*.BAK' -mtime +32 -delete
+  # Do a new backup
+  /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P ${SA_PASSWORD} -i /backup-db.sql
+
+  # Delete backup files older than 30 days
+  grep -B1 "BACKUP DATABASE successfully" /var/opt/mssql/log/errorlog | grep -q _$now.BAK &&
+  find /etc/bitwarden/mssql/backups/ -mindepth 1 -type f -name '*.BAK' -mtime +32 -delete
+done
