5a9c169d3d209529cffcb6d99aef02bd68a1ea1d
benjamin luton <benjamin@luton.net.au>
2020-03-17 15:29:38 +1000

    upload

--- src/Sql/PostgreSQL/Functions/DMS/cipher_create.sql
+++ src/Sql/PostgreSQL/Functions/DMS/cipher_create.sql
@@ -1,23 +1,19 @@
-CREATE OR REPLACE PROCEDURE vault_dbo.cipher_create(par_id uuid, par_userid uuid, par_organizationid uuid, par_type numeric, par_data text, par_favorites text, par_folders text, par_attachments text, par_creationdate timestamp without time zone, par_revisiondate timestamp without time zone)
+CREATE OR REPLACE PROCEDURE cipher_create(par_id uuid, par_user_id uuid, par_organization_id uuid, par_type numeric, par_data text, par_favorites text, par_folders text, par_attachments text, par_creation_date timestamp without time zone, par_revision_date timestamp without time zone)
  LANGUAGE plpgsql
 AS $procedure$
 BEGIN
-    /*
-    [7810 - Severity CRITICAL - PostgreSQL doesn't support the SET NOCOUNT. If need try another way to send message back to the client application.]
-    SET NOCOUNT ON
-    */
-    INSERT INTO vault_dbo.cipher (id, userid, organizationid, type, data, favorites, folders, attachments, creationdate, revisiondate)
+    INSERT INTO cipher (id, user_id, organization_id, type, data, favorites, folders, attachments, creation_date, revision_date)
     VALUES (par_Id,
     CASE
-        WHEN par_OrganizationId IS NULL THEN par_UserId
+        WHEN par_Organization_Id IS NULL THEN par_UserId
         ELSE NULL
-    END, par_OrganizationId, par_Type, par_Data, par_Favorites, par_Folders, par_Attachments, par_CreationDate, par_RevisionDate);
+    END, par_Organization_Id, par_Type, par_Data, par_Favorites, par_Folders, par_Attachments, par_Creation_Date, par_Revision_Date);
 
-    IF par_OrganizationId IS NOT NULL THEN
-        CALL vault_dbo.user_bumpaccountrevisiondatebycipherid(par_Id, par_OrganizationId);
+    IF par_Organization_Id IS NOT NULL THEN
+        CALL user_bump_account_revision_date_by_cipher_id(par_Id, par_Organization_Id);
     ELSE
-        IF par_UserId IS NOT NULL THEN
-            CALL vault_dbo.user_bumpaccountrevisiondate(par_UserId);
+        IF par_User_Id IS NOT NULL THEN
+            CALL user_bump_account_revision_date(par_User_Id);
         END IF;
     END IF;
 END;
