a7d773680605274407fd87f9663ea727ed29fd0c
Kyle Spearrin <kspearrin@users.noreply.github.com>
2020-06-09 10:25:37 -0400

    Add i18nservice abstraction (#770)

---
+++ src/Core/Services/II18nService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Localization;
+
+namespace Bit.Core.Services
+{
+    public interface II18nService
+    {
+        LocalizedString GetLocalizedHtmlString(string key);
+        string Translate(string key, params object[] args);
+        string T(string key, params object[] args);
+    }
+}
