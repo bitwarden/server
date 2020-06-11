4fd8dae3cbe96cfa4a213fa1e0e80a4d6dfe8765
Kyle Spearrin <kyle.spearrin@gmail.com>
2020-06-11 14:17:26 -0400

    update gulp to v4 and remove old packages

--- src/Admin/gulpfile.js
+++ src/Admin/gulpfile.js
@@ -1,14 +1,10 @@
 ﻿/// <binding BeforeBuild='build' Clean='clean' ProjectOpened='build' />
 
-const gulp = require('gulp'),
-    rimraf = require('rimraf'),
-    merge = require('merge-stream'),
-    runSequence = require('run-sequence'),
-    googleWebFonts = require('gulp-google-webfonts'),
-    concat = require('gulp-concat'),
-    cssmin = require('gulp-cssmin'),
-    uglify = require('gulp-uglify'),
-    sass = require('gulp-sass');
+const gulp = require('gulp');
+const merge = require('merge-stream');
+const googleWebFonts = require('gulp-google-webfonts');
+const sass = require('gulp-sass');
+const del = require('del');
 
 const paths = {};
 paths.webroot = './wwwroot/';
@@ -25,21 +21,11 @@ paths.minJs = paths.jsDir + '**/*.min.js';
 paths.libJs = paths.libDir + '**/*.js';
 paths.libMinJs = paths.libDir + '**/*.min.js';
 
-gulp.task('clean:js', (cb) => {
-    rimraf(paths.minJs, cb);
-});
+function clean() {
+    return del([paths.minJs, paths.cssDir, paths.libDir]);
+}
 
-gulp.task('clean:css', (cb) => {
-    rimraf(paths.cssDir, cb);
-});
-
-gulp.task('clean:lib', (cb) => {
-    rimraf(paths.libDir, cb);
-});
-
-gulp.task('clean', ['clean:js', 'clean:css', 'clean:lib']);
-
-gulp.task('lib', ['clean:lib'], () => {
+function lib() {
     const libs = [
         {
             src: paths.npmDir + 'bootstrap/dist/js/*',
@@ -67,27 +53,30 @@ gulp.task('lib', ['clean:lib'], () => {
         return gulp.src(lib.src).pipe(gulp.dest(lib.dest));
     });
     return merge(tasks);
-});
+}
 
-gulp.task('sass', () => {
+function runSass() {
     return gulp.src(paths.sass)
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(gulp.dest(paths.cssDir));
-});
+}
 
-gulp.task('sass:watch', () => {
-    gulp.watch(paths.sass, ['sass']);
-});
+function sassWatch() {
+    gulp.watch(paths.sass, runSass);
+}
 
-gulp.task('webfonts', () => {
+function webfonts() {
     return gulp.src('./webfonts.list')
         .pipe(googleWebFonts({
             fontsDir: 'webfonts',
             cssFilename: 'webfonts.css'
         }))
         .pipe(gulp.dest(paths.cssDir));
-});
+}
 
-gulp.task('build', function (cb) {
-    return runSequence('clean', ['lib', 'sass', 'webfonts'], cb);
-});
+exports.build = gulp.series(clean, gulp.parallel([lib, runSass, webfonts]));
+exports['sass:watch'] = sassWatch;
+exports.sass = runSass;
+exports.lib = lib;
+exports.webfonts = webfonts;
+exports.clean = clean;
