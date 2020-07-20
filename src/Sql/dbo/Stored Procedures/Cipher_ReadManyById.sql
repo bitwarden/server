e4a76379574468bcc4a7b04a72e2c565401f527b
Addison Beck <addisonbeck@MacBook-Pro.local>
2020-07-19 22:12:33 -0500

    Set up API methods for bulk admin delete

---
+++ src/Sql/dbo/Stored Procedures/Cipher_ReadManyById.sql
@@ -0,0 +1,13 @@
+CREATE PROCEDURE [dbo].[Cipher_ReadManyById]
+    @Ids AS [dbo].[GuidIdArray] READONLY,
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    SELECT
+        *
+    FROM
+        [dbo].[CipherView]
+    WHERE
+        [Id] = IN (SELECT* FROM @Id)
+END
