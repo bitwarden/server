3af9451eb22293b02a6a011cd98ad2f58ecf9503
Addison Beck <addisonbeck@MacBook-Pro.local>
2020-07-21 12:04:24 -0500

    used existing methods for fetching org ciphers on bulk delete

--- src/Core/Repositories/ICipherRepository.cs
+++ src/Core/Repositories/ICipherRepository.cs
@@ -14,7 +14,6 @@ namespace Bit.Core.Repositories
         Task<bool> GetCanEditByIdAsync(Guid userId, Guid cipherId);
         Task<ICollection<CipherDetails>> GetManyByUserIdAsync(Guid userId, bool withOrganizations = true);
         Task<ICollection<Cipher>> GetManyByOrganizationIdAsync(Guid organizationId);
-        Task<IEnumerable<CipherDetails>> GetManyDetailsByOrganizationIdAsync(Guid organizationId);
         Task CreateAsync(Cipher cipher, IEnumerable<Guid> collectionIds);
         Task CreateAsync(CipherDetails cipher);
         Task CreateAsync(CipherDetails cipher, IEnumerable<Guid> collectionIds);
