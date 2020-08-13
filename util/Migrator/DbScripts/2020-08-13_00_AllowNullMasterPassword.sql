cd926ca8f6744fbeafb858f098ab61230955f798
Kyle Spearrin <kspearrin@users.noreply.github.com>
2020-08-13 17:30:10 -0400

    allow user registration for sso (#865)

---
+++ util/Migrator/DbScripts/2020-08-13_00_AllowNullMasterPassword.sql
@@ -0,0 +1,19 @@
+﻿ALTER TABLE
+    [dbo].[User]
+ALTER COLUMN
+    [MasterPassword] NVARCHAR (300) NULL
+GO
+
+IF EXISTS(SELECT * FROM sys.views WHERE [Name] = 'UserView')
+BEGIN
+    DROP VIEW [dbo].[UserView]
+END
+GO
+
+CREATE VIEW [dbo].[UserView]
+AS
+SELECT
+    *
+FROM
+    [dbo].[User]
+GO
