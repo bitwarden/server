22565398c49ec64ad76d87c4309d2aece6d06961
Addison Beck <addisonbeck1@gmail.com>
2020-08-19 17:47:52 -0400

    facilitate linking/unlinking existing users from an sso enabled org

--- src/Core/Repositories/SqlServer/SsoUserRepository.cs
+++ src/Core/Repositories/SqlServer/SsoUserRepository.cs
@@ -1,4 +1,8 @@
-﻿using Bit.Core.Models.Table;
+﻿using System.Threading.Tasks;
+using Bit.Core.Models.Table;
+using System.Data.SqlClient;
+using System.Data;
+using Dapper;
 
 namespace Bit.Core.Repositories.SqlServer
 {
@@ -11,5 +15,16 @@ namespace Bit.Core.Repositories.SqlServer
         public SsoUserRepository(string connectionString, string readOnlyConnectionString)
             : base(connectionString, readOnlyConnectionString)
         { }
+
+        public override async Task DeleteAsync(SsoUser ssoUser)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                var results = await connection.ExecuteAsync(
+                    $"[{Schema}].[SsoUser_Delete]",
+                    new { UserId = ssoUser.UserId, OrganizationId = ssoUser.OrganizationId },
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
     }
 }
---
+++ util/Migrator/DbScripts/2020-08-19_00_AddIdentifierToOrgView.sql
@@ -0,0 +1,39 @@
+IF EXISTS(SELECT * FROM sys.views WHERE [Name] = 'OrganizationUserOrganizationDetailsView')
+BEGIN
+    DROP VIEW [dbo].[OrganizationUserOrganizationDetailsView];
+END
+GO
+
+CREATE VIEW [dbo].[OrganizationUserOrganizationDetailsView]
+AS
+SELECT
+    OU.[UserId],
+    OU.[OrganizationId],
+    O.[Name],
+    O.[Enabled],
+    O.[UsePolicies],
+    O.[UseSso],
+    O.[UseGroups],
+    O.[UseDirectory],
+    O.[UseEvents],
+    O.[UseTotp],
+    O.[Use2fa],
+    O.[UseApi],
+    O.[SelfHost],
+    O.[UsersGetPremium],
+    O.[Seats],
+    O.[MaxCollections],
+    O.[MaxStorageGb],
+    O.[Identifier],
+    OU.[Key],
+    OU.[Status],
+    OU.[Type],
+    SU.[ExternalId] SsoExternalId
+FROM
+    [dbo].[OrganizationUser] OU
+INNER JOIN
+    [dbo].[Organization] O ON O.[Id] = OU.[OrganizationId]
+LEFT JOIN
+    [dbo].[SsoUser] SU ON SU.[UserId] = OU.[UserId] AND SU.[OrganizationId] = OU.[OrganizationId]
+
+GO
