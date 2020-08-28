38728143d8cb5d948e168d8c5ba56e688e26314c
Kyle Spearrin <kspearrin@users.noreply.github.com>
2020-08-28 13:32:15 -0400

    Added static client store (#899)

---
+++ src/Core/IdentityServer/StaticClientStore.cs
@@ -0,0 +1,24 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bit.Core.IdentityServer
+{
+    public class StaticClientStore
+    {
+        public StaticClientStore(GlobalSettings globalSettings)
+        {
+            ApiClients = new List<Client>
+            {
+                new ApiClient(globalSettings, "mobile", 90, 1),
+                new ApiClient(globalSettings, "web", 30, 1),
+                new ApiClient(globalSettings, "browser", 30, 1),
+                new ApiClient(globalSettings, "desktop", 30, 1),
+                new ApiClient(globalSettings, "cli", 30, 1),
+                new ApiClient(globalSettings, "connector", 30, 24)
+            }.ToDictionary(c => c.ClientId);
+        }
+
+        public IDictionary<string, Client> ApiClients { get; private set; }
+    }
+}
