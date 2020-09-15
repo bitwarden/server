1c6c599b8d8b7ebdd0f066c2e6a1fd7ddfca35e7
Kyle Spearrin <kspearrin@users.noreply.github.com>
2020-09-15 10:17:44 -0400

    Created sso config service with save (#936)

---
+++ src/Core/Services/ISsoConfigService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Bit.Core.Models.Table;
+
+namespace Bit.Core.Services
+{
+    public interface ISsoConfigService
+    {
+        Task SaveAsync(SsoConfig config);
+    }
+}
