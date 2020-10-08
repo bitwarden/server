14ff2266af62bce81d4e9e02d7a606d9999da93e
Chad Scharf <3904944+cscharf@users.noreply.github.com>
2020-10-08 10:35:12 -0400

    Revert Docker version

--- scripts/bitwarden.ps1
+++ scripts/bitwarden.ps1
@@ -24,7 +24,7 @@ if ($output -eq "") {
 
 $scriptsDir = "${output}\scripts"
 $githubBaseUrl = "https://raw.githubusercontent.com/bitwarden/server/master"
-$coreVersion = "1.37.2"
+$coreVersion = "1.37.1"
 $webVersion = "2.16.1"
 
 # Functions
