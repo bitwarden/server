ec4d126a1174a956609a9be5b3ef7eaae8efa6e5
Chad Scharf <3904944+cscharf@users.noreply.github.com>
2020-10-09 15:50:54 -0400

    Docker version bump - 1.37.2 (#965)
    * Docker version bump - 1.37.2
    
    * Web version bump for docker

--- scripts/bitwarden.ps1
+++ scripts/bitwarden.ps1
@@ -24,8 +24,8 @@ if ($output -eq "") {
 
 $scriptsDir = "${output}\scripts"
 $githubBaseUrl = "https://raw.githubusercontent.com/bitwarden/server/master"
-$coreVersion = "1.37.1"
-$webVersion = "2.16.1"
+$coreVersion = "1.37.2"
+$webVersion = "2.16.2"
 
 # Functions
 
