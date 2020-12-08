5aa24b28c49d3ef0bad7281fbd8aabcd2e496e3f
Chad Scharf <3904944+cscharf@users.noreply.github.com>
2020-12-08 10:19:34 -0500

    Self-hosting version-bump to 1.38.2

--- scripts/bitwarden.sh
+++ scripts/bitwarden.sh
@@ -37,7 +37,7 @@ fi
 
 SCRIPTS_DIR="$OUTPUT/scripts"
 GITHUB_BASE_URL="https://raw.githubusercontent.com/bitwarden/server/master"
-COREVERSION="1.38.1"
+COREVERSION="1.38.2"
 WEBVERSION="2.17.1"
 
 # Functions
