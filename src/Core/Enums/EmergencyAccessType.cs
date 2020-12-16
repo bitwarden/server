0f1af2333e9e2af5aecd1b290130f3925a7e280d
Oscar Hinton <hinton.oscar@gmail.com>
2020-12-16 20:36:47 +0100

    Add support for Emergency Access (#1000)
    * Add support for Emergency Access
    
    * Add migration script
    
    * Review comments
    
    * Ensure grantor has premium when inviting new grantees.
    
    * Resolve review comments
    
    * Remove two factor references

---
+++ src/Core/Enums/EmergencyAccessType.cs
@@ -0,0 +1,8 @@
+﻿namespace Bit.Core.Enums
+{
+    public enum EmergencyAccessType : byte
+    {
+        View = 0,
+        Takeover = 1,
+    }
+}
---
+++ src/Sql/dbo/Stored Procedures/EmergencyAccessDetails_ReadByGrantorId.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[EmergencyAccessDetails_ReadByGrantorId]
+    @GrantorId UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    SELECT
+        *
+    FROM
+        [dbo].[EmergencyAccessDetailsView]
+    WHERE
+        [GrantorId] = @GrantorId
+END
