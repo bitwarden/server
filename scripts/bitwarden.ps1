f310a797470b10ac6534b84506f7d8c8e7eb3d30
Chad Scharf <3904944+cscharf@users.noreply.github.com>
2021-01-26 10:34:47 -0500

    self-hosted version bump v1.39.1, web v2.18.1

--- scripts/bitwarden.ps1
+++ scripts/bitwarden.ps1
@@ -24,8 +24,8 @@ if ($output -eq "") {
 
 $scriptsDir = "${output}\scripts"
 $githubBaseUrl = "https://raw.githubusercontent.com/bitwarden/server/master"
-$coreVersion = "1.39.0"
-$webVersion = "2.18.0"
+$coreVersion = "1.39.1"
+$webVersion = "2.18.1"
 
 # Functions
 
--- scripts/bitwarden.sh
+++ scripts/bitwarden.sh
@@ -32,8 +32,8 @@ fi
 
 SCRIPTS_DIR="$OUTPUT/scripts"
 GITHUB_BASE_URL="https://raw.githubusercontent.com/bitwarden/server/master"
-COREVERSION="1.39.0"
-WEBVERSION="2.18.0"
+COREVERSION="1.39.1"
+WEBVERSION="2.18.1"
 
 echo "bitwarden.sh version $COREVERSION"
 docker --version
