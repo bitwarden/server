00853bc250062f7065c88adf3950f6026daabf6c
Addison Beck <abeck@bitwarden.com>
2021-01-30 17:56:37 -0500

    Import Invite Error (#1121)
    * throw error if user is already invited on import
    
    * added back the single InviteUser OrgService method

--- src/Core/Services/IOrganizationService.cs
+++ src/Core/Services/IOrganizationService.cs
@@ -30,6 +30,8 @@ namespace Bit.Core.Services
         Task UpdateAsync(Organization organization, bool updateBilling = false);
         Task UpdateTwoFactorProviderAsync(Organization organization, TwoFactorProviderType type);
         Task DisableTwoFactorProviderAsync(Organization organization, TwoFactorProviderType type);
+        Task<OrganizationUser> InviteUserAsync(Guid organizationId, Guid? invitingUserId, string email,
+            OrganizationUserType type, bool accessAll, string externalId, IEnumerable<SelectionReadOnly> collections);
         Task<List<OrganizationUser>> InviteUserAsync(Guid organizationId, Guid? invitingUserId, string externalId, OrganizationUserInvite orgUserInvite);
         Task ResendInviteAsync(Guid organizationId, Guid? invitingUserId, Guid organizationUserId);
         Task<OrganizationUser> AcceptUserAsync(Guid organizationUserId, User user, string token,
