e350daeeeefd0806f27077a5327332e2c0063e54
Matt Gibson <mgibson@bitwarden.com>
2021-02-24 13:03:16 -0600

    Use sas token for send downloads (#1157)
    * Remove Url from SendFileModel
    
    Url is now generated on the fly with limited lifetime.
    
    New model houses the download url generated
    
    * Create API endpoint for getting Send file download url
    
    * Generate limited-life Azure download urls
    
    * Lint fix

--- src/Core/Models/Api/SendFileModel.cs
+++ src/Core/Models/Api/SendFileModel.cs
@@ -11,14 +11,12 @@ namespace Bit.Core.Models.Api
         public SendFileModel(SendFileData data, GlobalSettings globalSettings)
         {
             Id = data.Id;
-            Url = $"{globalSettings.Send.BaseUrl}/{data.Id}";
             FileName = data.FileName;
             Size = data.SizeString;
             SizeName = CoreHelpers.ReadableBytesSize(data.Size);
         }
 
         public string Id { get; set; }
-        public string Url { get; set; }
         [EncryptedString]
         [EncryptedStringLength(1000)]
         public string FileName { get; set; }
