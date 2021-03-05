9b6059d5f880dab42a4015b1b9bcaf757f1433e8
Matt Gibson <mgibson@bitwarden.com>
2021-03-05 17:14:39 -0600

    Revert "Push syncs on Send Access"
    This reverts commit 6a3eb7af4f1d4233b6e8854c863b2e263d7dea54.

--- src/Core/Services/Implementations/SendService.cs
+++ src/Core/Services/Implementations/SendService.cs
@@ -212,7 +212,7 @@ namespace Bit.Core.Services
             }
 
             send.AccessCount++;
-            await SaveSendAsync(send);
+            await _sendRepository.ReplaceAsync(send);
             return (await _sendFileStorageService.GetSendFileDownloadUrlAsync(send, fileId), false, false);
         }
 
@@ -234,7 +234,7 @@ namespace Bit.Core.Services
                 send.AccessCount++;
             }
 
-            await SaveSendAsync(send);
+            await _sendRepository.ReplaceAsync(send);
             await RaiseReferenceEventAsync(send, ReferenceEventType.SendAccessed);
             return (send, false, false);
         }
