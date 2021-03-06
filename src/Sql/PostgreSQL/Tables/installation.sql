ce08d7aba43653685f292e2bad1feddd14ce7106
Daniel James Smith <djsmith@web.de>
2021-03-06 01:45:09 +0100

    Fixes #1101: Extend email column length to 256 characters (Postgres)

--- src/Sql/PostgreSQL/Tables/installation.sql
+++ src/Sql/PostgreSQL/Tables/installation.sql
@@ -3,7 +3,7 @@
 
 CREATE TABLE IF NOT EXISTS installation (
     id            UUID             NOT NULL,
-    email         VARCHAR (50)     NOT NULL,
+    email         VARCHAR (256)    NOT NULL,
     key           VARCHAR (150)    NOT NULL,
     enabled       BIT              NOT NULL,
     creation_date TIMESTAMPTZ     NOT NULL,
