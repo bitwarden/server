9b2bf0bbcf1d30993ac84a5af3f7ad718acda264
Matt Gibson <mgibson@bitwarden.com>
2021-03-19 14:09:36 -0500

    Lint fix

--- src/Core/Models/Api/Response/SendFileUploadDataResponseModel.cs
+++ src/Core/Models/Api/Response/SendFileUploadDataResponseModel.cs
@@ -4,10 +4,11 @@ namespace Bit.Core.Models.Api.Response
 {
     public class SendFileUploadDataResponseModel : ResponseModel
     {
+        public SendFileUploadDataResponseModel() : base("send-fileUpload") { }
+        
         public string Url { get; set; }
         public FileUploadType FileUploadType { get; set; }
         public SendResponseModel SendResponse { get; set; }
 
-        public SendFileUploadDataResponseModel() : base("send-fileUpload") { }
     }
 }
