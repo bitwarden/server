3b1837020ad372e67587bc8aff1db3a993df4db2
Thomas Rittson <trittson@bitwarden.com>
2021-03-25 10:35:38 +1000

    Add SendOptionsPolicy to Portal

--- src/Core/Models/Data/SendOptionsPolicyData.cs
+++ src/Core/Models/Data/SendOptionsPolicyData.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Bit.Core.Models.Data
 {
     public class SendOptionsPolicyData
     {
+        [Display(Name = "DisableHideEmail")]
         public bool DisableHideEmail { get; set; }
     }
 }
