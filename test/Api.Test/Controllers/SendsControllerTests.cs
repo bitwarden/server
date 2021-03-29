0f9822a853f200143d09068c660a29a7906548b3
Thomas Rittson <trittson@bitwarden.com>
2021-03-30 08:52:55 +1000

    Add unit test for hiding emails on sends

---
+++ test/Api.Test/Controllers/SendsControllerTests.cs
@@ -0,0 +1,79 @@
+using AutoFixture.Xunit2;
+using Bit.Api.Controllers;
+using Bit.Core.Enums;
+using Bit.Core.Models.Api;
+using Bit.Core.Models.Table;
+using Bit.Core.Repositories;
+using Bit.Core.Services;
+using Bit.Core.Settings;
+using Bit.Core.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using NSubstitute;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using Xunit;
+
+namespace Bit.Api.Test.Controllers
+{
+    public class SendsControllerTests : IDisposable
+    {
+
+        private readonly SendsController _sut;
+        private readonly GlobalSettings _globalSettings;
+        private readonly IUserService _userService;
+        private readonly ISendRepository _sendRepository;
+        private readonly ISendService _sendService;
+        private readonly ISendFileStorageService _sendFileStorageService;
+        private readonly ILogger<SendsController> _logger;
+
+        public SendsControllerTests()
+        {
+            _userService = Substitute.For<IUserService>();
+            _sendRepository = Substitute.For<ISendRepository>();
+            _sendService = Substitute.For<ISendService>();
+            _sendFileStorageService = Substitute.For<ISendFileStorageService>();
+            _globalSettings = new GlobalSettings();
+            _logger = Substitute.For<ILogger<SendsController>>();
+
+            _sut = new SendsController(
+                _sendRepository,
+                _userService,
+                _sendService,
+                _sendFileStorageService,
+                _logger,
+                _globalSettings
+            );
+        }
+
+        public void Dispose()
+        {
+            _sut?.Dispose();
+        }
+
+        [Theory, AutoData]
+        public async Task SendsController_WhenSendHidesEmail_CreatorIdentifierShouldBeNull(
+            Guid id, Send send, User user)
+        {
+            var accessId = CoreHelpers.Base64UrlEncode(id.ToByteArray());
+
+            send.Id = default;
+            send.Type = SendType.Text;
+            send.Data = JsonConvert.SerializeObject(new Dictionary<string, string>());
+            send.HideEmail = true;
+
+            _sendService.AccessAsync(id, null).Returns((send, false, false));
+            _userService.GetUserByIdAsync(Arg.Any<Guid>()).Returns(user);
+
+            var request = new SendAccessRequestModel();
+            var actionResult = await _sut.Access(accessId, request);
+            var response = (actionResult as ObjectResult)?.Value as SendAccessResponseModel;
+
+            Assert.NotNull(response);
+            Assert.Null(response.CreatorIdentifier);
+        }
+    }
+}
+
