ea7120905d1fdf73ef982303990040c76b300971
Kyle Spearrin <kyle.spearrin@gmail.com>
2021-04-01 15:47:52 -0400

    admin setting for controlling trash deleting dates

--- src/Admin/Jobs/DeleteCiphersJob.cs
+++ src/Admin/Jobs/DeleteCiphersJob.cs
@@ -4,6 +4,7 @@ using Bit.Core;
 using Bit.Core.Jobs;
 using Bit.Core.Repositories;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Quartz;
 
 namespace Bit.Admin.Jobs
@@ -11,19 +12,28 @@ namespace Bit.Admin.Jobs
     public class DeleteCiphersJob : BaseJob
     {
         private readonly ICipherRepository _cipherRepository;
+        private readonly AdminSettings _adminSettings;
 
         public DeleteCiphersJob(
             ICipherRepository cipherRepository,
+            IOptions<AdminSettings> adminSettings,
             ILogger<DeleteCiphersJob> logger)
             : base(logger)
         {
             _cipherRepository = cipherRepository;
+            _adminSettings = adminSettings?.Value;
         }
 
         protected async override Task ExecuteJobAsync(IJobExecutionContext context)
         {
             _logger.LogInformation(Constants.BypassFiltersEventId, "Execute job task: DeleteDeletedAsync");
-            await _cipherRepository.DeleteDeletedAsync(DateTime.UtcNow.AddDays(-30));
+            var deleteDate = DateTime.UtcNow.AddDays(-30);
+            var daysAgoSetting = (_adminSettings?.DeleteTrashDaysAgo).GetValueOrDefault();
+            if (daysAgoSetting > 0)
+            {
+                deleteDate = DateTime.UtcNow.AddDays(-1 * daysAgoSetting);
+            }
+            await _cipherRepository.DeleteDeletedAsync(deleteDate);
             _logger.LogInformation(Constants.BypassFiltersEventId, "Finished job task: DeleteDeletedAsync");
         }
     }
