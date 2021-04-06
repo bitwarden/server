48d230adc4a58f656ab941b74eb2394c107fe48b
Vince Grassia <593223+vgrassia@users.noreply.github.com>
2021-04-06 12:45:14 -0400

    Remove redundant variable assignment

--- util/Setup/CertBuilder.cs
+++ util/Setup/CertBuilder.cs
@@ -29,11 +29,6 @@ namespace Bit.Setup
             if (!_context.Config.Ssl)
             {
                 var skipSSL = _context.Parameters.ContainsKey("skip-ssl") && (_context.Parameters["skip-ssl"] == "true" || _context.Parameters["skip-ssl"] == "1");
-                if (_context.Parameters.ContainsKey("skip-ssl"))
-                {
-                    skipSSL = _context.Parameters["skip-ssl"] == "true" ||
-                                          _context.Parameters["skip-ssl"] == "1";
-                }
 
                 if (!skipSSL)
                 {
