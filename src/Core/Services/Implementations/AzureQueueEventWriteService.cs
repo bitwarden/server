2e42a9da79d93f74603d43021053d2f8ab4ec199
Matt Gibson <mgibson@bitwarden.com>
2021-04-13 13:38:17 -0500

    Split up azure messages according to max size

--- src/Core/Services/Implementations/AzureQueueEventWriteService.cs
+++ src/Core/Services/Implementations/AzureQueueEventWriteService.cs
@@ -4,12 +4,15 @@ using Azure.Storage.Queues;
 using Newtonsoft.Json;
 using Bit.Core.Models.Data;
 using Bit.Core.Settings;
+using System.Linq;
+using System.Text;
 
 namespace Bit.Core.Services
 {
     public class AzureQueueEventWriteService : IEventWriteService
     {
         private readonly QueueClient _queueClient;
+        private const int MAX_MESSAGE_BODY = 128000;
 
         private JsonSerializerSettings _jsonSettings = new JsonSerializerSettings
         {
@@ -30,8 +33,33 @@ namespace Bit.Core.Services
 
         public async Task CreateManyAsync(IList<IEvent> e)
         {
-            var json = JsonConvert.SerializeObject(e, _jsonSettings);
-            await _queueClient.SendMessageAsync(json);
+            foreach(var json in SerializeMany(e))
+            {
+                await _queueClient.SendMessageAsync(json);
+            }
+        }
+
+        private IEnumerable<string> SerializeMany(IList<IEvent> events)
+        {
+            var strings = new List<string>();
+            var jsonEvents = events.Select(e => JsonConvert.SerializeObject(e, _jsonSettings));
+            var stringBuilder = new StringBuilder("[");
+            foreach(var jsonEvent in jsonEvents)
+            {
+                if (stringBuilder.Length + jsonEvent.Length + 2 < MAX_MESSAGE_BODY)
+                {
+                    stringBuilder.Append($",{jsonEvent}");
+                }
+                else
+                {
+                    stringBuilder.Append("]");
+                    strings.Append(stringBuilder.ToString());
+                    stringBuilder = new StringBuilder("[");
+                }
+            }
+            stringBuilder.Append("]");
+            strings.Append(stringBuilder.ToString());
+            return strings;
         }
     }
 }
