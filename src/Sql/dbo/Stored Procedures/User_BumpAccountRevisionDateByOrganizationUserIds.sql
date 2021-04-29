db3f78b256e0ad9a1f16e5b412c4b5cec708febf
Matt Gibson <mgibson@bitwarden.com>
2021-04-29 10:33:36 -0500

    Simplify bump account revision sproc

--- src/Sql/dbo/Stored Procedures/User_BumpAccountRevisionDateByOrganizationUserIds.sql
+++ src/Sql/dbo/Stored Procedures/User_BumpAccountRevisionDateByOrganizationUserIds.sql
@@ -4,24 +4,15 @@ AS
 BEGIN
     SET NOCOUNT ON
 
-    SELECT
-        UserId
-    INTO
-        #UserIds
-    FROM
-        [dbo].[OrganizationUser] OU
-    INNER JOIN
-        @OrganizationUserIds OUIds on OUIds.Id = OU.Id
-    WHERE
-        OU.[Status] = 2 -- Confirmed
-
     UPDATE
         U
     SET
         U.[AccountRevisionDate] = GETUTCDATE()
     FROM
-        [dbo].[User] U
-    Inner JOIN
-        #UserIds ON U.[Id] = #UserIds.[UserId]
+        @OrganizationUserIds OUIDs
+    INNER JOIN
+        [dbo].[OrganizationUser] OU ON OUIDs.Id = OU.Id AND OU.[Status] = 2 -- Confirmed
+    INNER JOIN
+        [dbo].[User] U ON OU.UserId = U.Id
 END
 GO
