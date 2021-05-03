e248cfb2782a1226af96e269e9596f51c0a9c57b
Matt Gibson <mgibson@bitwarden.com>
2021-05-03 08:10:01 -0500

    Handle messageId internally.
    JsonProperty attribute allows newtonsoft to assign to the private setter

--- src/Core/Models/Data/AzureQueueMessage.cs
+++ src/Core/Models/Data/AzureQueueMessage.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Bit.Core.Models.Data
 {
     public class AzureQueueMessage<T> where T : class
     {
         public bool BlobBackedMessage => Message == null && Messages == null;
-        public Guid MessageId { get; set; }
+        [JsonProperty]
+        public Guid MessageId { get; private set; }
         public string BlobPath { get; set; } = null;
         public T Message { get; set; } = null;
         public IEnumerable<T> Messages { get; set; } = null;
