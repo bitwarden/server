2958548f67f3ccc5e2ebebaaa53175536407297c
Matt Gibson <mgibson@bitwarden.com>
2021-05-02 22:06:42 -0500

    Upload As AzureQueueMessage

--- src/Core/Services/Implementations/AzureQueueMailService.cs
+++ src/Core/Services/Implementations/AzureQueueMailService.cs
@@ -3,30 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Azure.Storage.Queues;
+using Bit.Core.Models.Data;
 using Bit.Core.Models.Mail;
 using Bit.Core.Settings;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
 using Newtonsoft.Json;
 
 namespace Bit.Core.Services
 {
-    public class AzureQueueMailService : AzureQueueService<IMailQueueMessage>, IMailEnqueuingService
+    public class AzureQueueMailService : IMailEnqueuingService
     {
-        protected override QueueClient QueueClient { get; }
-        protected override JsonSerializerSettings JsonSettings { get; } = new JsonSerializerSettings
+        public const string QueueMessageContainerName = "queue-messages";
+        private readonly QueueClient _queueClient;
+        private readonly CloudBlobClient _blobClient;
+        private readonly JsonSerializerSettings _jsonSettings = new JsonSerializerSettings
         {
             NullValueHandling = NullValueHandling.Ignore
         };
+        private  CloudBlobContainer _queueMessageContainer;
 
         public AzureQueueMailService(
             GlobalSettings globalSettings)
         {
-            QueueClient = new QueueClient(globalSettings.Mail.ConnectionString, "mail");
+            _queueClient = new QueueClient(globalSettings.Mail.ConnectionString, "mail");
+            var storageAccount = CloudStorageAccount.Parse(globalSettings.Mail.ConnectionString);
+            _blobClient = storageAccount.CreateCloudBlobClient();
         }
 
-        public Task EnqueueAsync(IMailQueueMessage message, Func<IMailQueueMessage, Task> fallback) =>
-            CreateAsync(message);
+        public async Task EnqueueAsync(IMailQueueMessage message, Func<IMailQueueMessage, Task> fallback)
+        {
+            await SendMessageAsync(new AzureQueueMessage<IMailQueueMessage>
+            {
+                Message = message
+            });
+        }
 
-        public Task EnqueueManyAsync(IEnumerable<IMailQueueMessage> messages, Func<IMailQueueMessage, Task> fallback) =>
-            CreateManyAsync(messages);
+        public async Task EnqueueManyAsync(IEnumerable<IMailQueueMessage> messages, Func<IMailQueueMessage, Task> fallback)
+        {
+            await SendMessageAsync(new AzureQueueMessage<IMailQueueMessage>
+            {
+                Messages = messages
+            });
+        }
+
+        private async Task SendMessageAsync(AzureQueueMessage<IMailQueueMessage> message)
+        {
+            var json = JsonConvert.SerializeObject(message, _jsonSettings);
+            if (json.Length > _queueClient.MessageMaxBytes)
+            {
+                await InitAsync();
+                var blob = _queueMessageContainer.GetBlockBlobReference($"{message.MessageId}");
+                await blob.UploadTextAsync(json);
+
+                json = JsonConvert.SerializeObject(message.ToBlobBackedMessage(), _jsonSettings);
+            }
+            await _queueClient.SendMessageAsync(json);
+        }
+
+        private async Task InitAsync()
+        {
+            if (_queueMessageContainer == null)
+            {
+                _queueMessageContainer = _blobClient.GetContainerReference(QueueMessageContainerName);
+                await _queueMessageContainer.CreateIfNotExistsAsync(BlobContainerPublicAccessType.Blob, null, null);
+            }
+        }
     }
 }
