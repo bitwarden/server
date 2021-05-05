d571efa49ddf94af4ec8111906f7e4e7a7caaa7f
Chad Scharf <3904944+cscharf@users.noreply.github.com>
2021-05-04 23:49:22 -0400

    Revert "Added fido2 registration to other services"
    This reverts part of commit 8f64f4a99f1de8052b3cd3a08c19e9bca5598848.

--- bitwarden_license/src/Sso/Startup.cs
+++ bitwarden_license/src/Sso/Startup.cs
@@ -43,15 +43,6 @@ namespace Bit.Sso
             // Context
             services.AddScoped<ICurrentContext, CurrentContext>();
 
-            // Fido2
-            services.AddFido2(options =>
-            {
-                options.ServerDomain = new Uri(globalSettings.BaseServiceUri.Vault).Host;
-                options.ServerName = "Bitwarden";
-                options.Origin = globalSettings.BaseServiceUri.Vault;
-                options.TimestampDriftTolerance = 300000;
-            });
-
             // Mvc
             services.AddControllersWithViews();
 
