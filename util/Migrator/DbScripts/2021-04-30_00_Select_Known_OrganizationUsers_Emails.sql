4a4fcb988e2ed0f173d6bf658d09542c6c3e48c3
Matt Gibson <mgibson@bitwarden.com>
2021-05-07 12:58:15 -0500

    consolidate migrations

--- util/Migrator/DbScripts/2021-04-30_00_Select_Known_OrganizationUsers_Emails.sql
+++ util/Migrator/DbScripts/2021-04-30_00_Select_Known_OrganizationUsers_Emails.sql
@@ -1,3 +1,14 @@
+-- Create EmailArray type
+IF NOT EXISTS (
+    SELECT *
+FROM sys.types
+WHERE [Name] = 'EmailArray'
+    AND is_user_defined = 1
+)
+CREATE TYPE [dbo].[EmailArray] AS TABLE (
+    [Email] NVARCHAR(256) NOT NULL);
+GO
+
 IF OBJECT_ID('[dbo].[OrganizationUser_SelectKnownEmails]') IS NOT NULL
 BEGIN
     DROP PROCEDURE [dbo].[OrganizationUser_SelectKnownEmails]
@@ -20,19 +31,19 @@ BEGIN
         INNER JOIN
         (
             SELECT
-                U.[Email] as 'UEmail',
-                OU.[Email] as 'OUEmail',
-                OU.OrganizationId
-            FROM
-                [dbo].[User] U
+            U.[Email] as 'UEmail',
+            OU.[Email] as 'OUEmail',
+            OU.OrganizationId
+        FROM
+            [dbo].[User] U
             RIGHT JOIN
-                [dbo].[OrganizationUser] OU ON OU.[UserId] = U.[Id]
-            WHERE
+            [dbo].[OrganizationUser] OU ON OU.[UserId] = U.[Id]
+        WHERE
                 OU.OrganizationId = @OrganizationId
         ) OUU ON OUU.[UEmail] = E.[Email] OR OUU.[OUEmail] = E.[Email]
     WHERE
         (@OnlyUsers = 0 AND (OUU.UEmail IS NOT NULL OR OUU.OUEmail IS NOT NULL)) OR
         (@OnlyUsers = 1 AND (OUU.UEmail IS NOT NULL))
-    
+
 END
 GO
