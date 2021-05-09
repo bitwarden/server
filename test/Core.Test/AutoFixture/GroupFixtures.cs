52170f8f3f1c303f2a93d0c5790dd4e3122027f0
hantatsang <hantatsang@gmail.com>
2021-05-09 22:58:07 +1000

    Rewrite with AutoFixture, improve tests

---
+++ test/Core.Test/AutoFixture/GroupFixtures.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using AutoFixture;
+using Bit.Core.Models.Table;
+using Bit.Core.Test.AutoFixture.Attributes;
+
+namespace Bit.Core.Test.AutoFixture
+{
+    internal class GroupOrganization : ICustomization
+    {
+        public bool UseGroups { get; set; }
+
+        public void Customize(IFixture fixture)
+        {
+            var organizationId = Guid.NewGuid();
+
+            fixture.Customize<Organization>(composer => composer
+                .With(o => o.Id, organizationId)
+                .With(o => o.UseGroups, UseGroups));
+
+            fixture.Customize<Group>(composer => composer.With(g => g.OrganizationId, organizationId));
+        }
+    }
+
+    internal class GroupOrganizationAutoDataAttribute : CustomAutoDataAttribute
+    {
+        public GroupOrganizationAutoDataAttribute() : base(
+            new SutProviderCustomization(), new GroupOrganization { UseGroups = true })
+        { }
+    }
+
+    internal class GroupOrganizationNotUseGroupsAutoDataAttribute : CustomAutoDataAttribute
+    {
+        public GroupOrganizationNotUseGroupsAutoDataAttribute() : base(
+            new SutProviderCustomization(), new GroupOrganization { UseGroups = false })
+        { }
+    }
+}
--- test/Core.Test/Services/GroupServiceTests.cs
+++ test/Core.Test/Services/GroupServiceTests.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Bit.Core.Exceptions;
-using Bit.Core.Models.Mail;
+using Bit.Core.Models.Data;
+using Bit.Core.Models.Table;
 using Bit.Core.Repositories;
 using Bit.Core.Services;
+using Bit.Core.Test.AutoFixture;
+using Bit.Core.Test.AutoFixture.Attributes;
 using NSubstitute;
 using Xunit;
 
@@ -11,138 +15,124 @@ namespace Bit.Core.Test.Services
 {
     public class GroupServiceTests
     {
-        private readonly GroupService _sut;
-
-        private readonly IEventService _eventService;
-        private readonly IOrganizationRepository _organizationRepository;
-        private readonly IOrganizationUserRepository _organizationUserRepository;
-        private readonly IGroupRepository _groupRepository;
-
-        public GroupServiceTests()
+        [Theory, GroupOrganizationAutoData]
+        public async Task SaveAsync_DefaultGroupId_CreatesGroupInRepository(Group group, Organization organization, SutProvider<GroupService> sutProvider)
         {
-            _eventService = Substitute.For<IEventService>();
-            _organizationRepository = Substitute.For<IOrganizationRepository>();
-            _organizationUserRepository = Substitute.For<IOrganizationUserRepository>();
-            _groupRepository = Substitute.For<IGroupRepository>();
+            group.Id = default(Guid);
+            sutProvider.GetDependency<IOrganizationRepository>().GetByIdAsync(organization.Id).Returns(organization);
+            organization.UseGroups = true;
+            var utcNow = DateTime.UtcNow;
 
-            _sut = new GroupService(
-                _eventService,
-                _organizationRepository,
-                _organizationUserRepository,
-                _groupRepository
-            );
+            await sutProvider.Sut.SaveAsync(group);
+
+            await sutProvider.GetDependency<IGroupRepository>().Received().CreateAsync(group);
+            await sutProvider.GetDependency<IEventService>().Received()
+                .LogGroupEventAsync(group, Enums.EventType.Group_Created);
+            Assert.True(group.CreationDate - utcNow < TimeSpan.FromSeconds(1));
+            Assert.True(group.RevisionDate - utcNow < TimeSpan.FromSeconds(1));
         }
 
-        [Fact]
-        public async Task SaveAsync_DefaultGroupId_CreatesGroupInRepository()
+        [Theory, GroupOrganizationAutoData]
+        public async Task SaveAsync_DefaultGroupIdAndCollections_CreatesGroupInRepository(Group group, Organization organization, List<SelectionReadOnly> collections, SutProvider<GroupService> sutProvider)
         {
-            var organization = new Models.Table.Organization
-            {
-                Id = Guid.NewGuid(),
-                UseGroups = true,
-            };
-            _organizationRepository
-                .GetByIdAsync(organization.Id)
-                .Returns(organization);
-            var group = new Models.Table.Group
-            {
-                OrganizationId = organization.Id,
-            };
+            group.Id = default(Guid);
+            sutProvider.GetDependency<IOrganizationRepository>().GetByIdAsync(organization.Id).Returns(organization);
+            organization.UseGroups = true;
+            var utcNow = DateTime.UtcNow;
 
-            await _sut.SaveAsync(group);
+            await sutProvider.Sut.SaveAsync(group, collections);
 
-            await _groupRepository.Received().CreateAsync(group);
+            await sutProvider.GetDependency<IGroupRepository>().Received().CreateAsync(group, collections);
+            await sutProvider.GetDependency<IEventService>().Received()
+                .LogGroupEventAsync(group, Enums.EventType.Group_Created);
+            Assert.True(group.CreationDate - utcNow < TimeSpan.FromSeconds(1));
+            Assert.True(group.RevisionDate - utcNow < TimeSpan.FromSeconds(1));
         }
 
-        [Fact]
-        public async Task SaveAsync_NonExistingOrganizationId_ThrowsBadRequest()
+        [Theory, GroupOrganizationAutoData]
+        public async Task SaveAsync_NonDefaultGroupId_ReplaceGroupInRepository(Group group, Organization organization, List<SelectionReadOnly> collections, SutProvider<GroupService> sutProvider)
         {
-            var group = new Models.Table.Group
-            {
-                Id = Guid.NewGuid(),
-            };
+            organization.UseGroups = true;
+            sutProvider.GetDependency<IOrganizationRepository>().GetByIdAsync(organization.Id).Returns(organization);
 
-            await Assert.ThrowsAsync<BadRequestException>(() => _sut.SaveAsync(group));
+            await sutProvider.Sut.SaveAsync(group, collections);
+
+            await sutProvider.GetDependency<IGroupRepository>().Received().ReplaceAsync(group, collections);
+            await sutProvider.GetDependency<IEventService>().Received()
+                .LogGroupEventAsync(group, Enums.EventType.Group_Updated);
+            Assert.True(group.RevisionDate - DateTime.UtcNow < TimeSpan.FromSeconds(1));
         }
 
-        [Fact]
-        public async Task SaveAsync_OrganizationDoesNotUseGroups_ThrowsBadRequest()
+        [Theory, CustomAutoData(typeof(SutProviderCustomization))]
+        public async Task SaveAsync_NonExistingOrganizationId_ThrowsBadRequest(Group group, Organization organization, SutProvider<GroupService> sutProvider)
         {
-            var organization = new Models.Table.Organization
-            {
-                Id = Guid.NewGuid(),
-            };
-            _organizationRepository
-                .GetByIdAsync(organization.Id)
-                .Returns(organization);
-            var group = new Models.Table.Group
-            {
-                OrganizationId = organization.Id,
-            };
-
-            await Assert.ThrowsAsync<BadRequestException>(() => _sut.SaveAsync(group));
+            var exception = await Assert.ThrowsAsync<BadRequestException>(
+                () => sutProvider.Sut.SaveAsync(group));
+            Assert.Contains("Organization not found", exception.Message);
+            await sutProvider.GetDependency<IGroupRepository>().DidNotReceiveWithAnyArgs().CreateAsync(default);
+            await sutProvider.GetDependency<IGroupRepository>().DidNotReceiveWithAnyArgs().ReplaceAsync(default);
+            await sutProvider.GetDependency<IEventService>().DidNotReceiveWithAnyArgs().LogGroupEventAsync(default, default, default);
         }
 
-        [Fact]
-        public async Task DeleteUserAsync_ValidData_DeletesUserInGroupRepository()
+        [Theory, GroupOrganizationNotUseGroupsAutoData]
+        public async Task SaveAsync_OrganizationDoesNotUseGroups_ThrowsBadRequest(Group group, Organization organization, SutProvider<GroupService> sutProvider)
         {
-            // organization
-            var organization = new Models.Table.Organization
-            {
-                Id = Guid.NewGuid(),
-                UseGroups = true,
-            };
-            _organizationRepository
-                .GetByIdAsync(organization.Id)
-                .Returns(organization);
-            // user
-            var organizationUser = new Models.Table.OrganizationUser
-            {
-                Id = Guid.NewGuid(),
-                OrganizationId = organization.Id,
-            };
-            _organizationUserRepository.GetByIdAsync(organizationUser.Id)
+            sutProvider.GetDependency<IOrganizationRepository>().GetByIdAsync(organization.Id).Returns(organization);
+
+            var exception = await Assert.ThrowsAsync<BadRequestException>(
+                () => sutProvider.Sut.SaveAsync(group));
+
+            Assert.Contains("This organization cannot use groups", exception.Message);
+            await sutProvider.GetDependency<IGroupRepository>().DidNotReceiveWithAnyArgs().CreateAsync(default);
+            await sutProvider.GetDependency<IGroupRepository>().DidNotReceiveWithAnyArgs().ReplaceAsync(default);
+            await sutProvider.GetDependency<IEventService>().DidNotReceiveWithAnyArgs().LogGroupEventAsync(default, default, default);
+        }
+
+        [Theory, CustomAutoData(typeof(SutProviderCustomization))]
+        public async Task DeleteAsync_ValidData_DeletesGroup(Group group, SutProvider<GroupService> sutProvider)
+        {
+            await sutProvider.Sut.DeleteAsync(group);
+
+            await sutProvider.GetDependency<IGroupRepository>().Received().DeleteAsync(group);
+            await sutProvider.GetDependency<IEventService>().Received()
+                .LogGroupEventAsync(group, Enums.EventType.Group_Deleted);
+        }
+
+        [Theory, CustomAutoData(typeof(SutProviderCustomization))]
+        public async Task DeleteUserAsync_ValidData_DeletesUserInGroupRepository(Group group, Organization organization, OrganizationUser organizationUser, SutProvider<GroupService> sutProvider)
+        {
+            group.OrganizationId = organization.Id;
+            organization.UseGroups = true;
+            sutProvider.GetDependency<IOrganizationRepository>().GetByIdAsync(organization.Id).Returns(organization);
+            organizationUser.OrganizationId = organization.Id;
+            sutProvider.GetDependency<IOrganizationUserRepository>().GetByIdAsync(organizationUser.Id)
                 .Returns(organizationUser);
-            // group
-            var group = new Models.Table.Group
-            {
-                OrganizationId = organization.Id,
-            };
 
-            await _sut.DeleteUserAsync(group, organizationUser.Id);
+            await sutProvider.Sut.DeleteUserAsync(group, organizationUser.Id);
 
-            await _groupRepository.Received().DeleteUserAsync(group.Id, organizationUser.Id);
+            await sutProvider.GetDependency<IGroupRepository>().Received().DeleteUserAsync(group.Id, organizationUser.Id);
+            await sutProvider.GetDependency<IEventService>().Received()
+                .LogOrganizationUserEventAsync(organizationUser, Enums.EventType.OrganizationUser_UpdatedGroups);
         }
 
-        [Fact]
-        public async Task DeleteUserAsync_InvalidUser_ThrowsNotFound()
+        [Theory, CustomAutoData(typeof(SutProviderCustomization))]
+        public async Task DeleteUserAsync_InvalidUser_ThrowsNotFound(Group group, Organization organization, OrganizationUser organizationUser, SutProvider<GroupService> sutProvider)
         {
-            // user
-            var nonOrganizationUser = new Models.Table.OrganizationUser
-            {
-                Id = Guid.NewGuid(),
-            };
-            _organizationUserRepository.GetByIdAsync(nonOrganizationUser.Id)
-                .Returns(nonOrganizationUser);
-            // organization
-            var organizationId = Guid.NewGuid();
-            var organization = new Models.Table.Organization
-            {
-                Id = organizationId,
-            };
-            _organizationRepository
-                .GetByIdAsync(organization.Id)
-                .Returns(organization);
-            // group
-            var group = new Models.Table.Group
-            {
-                OrganizationId = organization.Id,
-            };
+            group.OrganizationId = organization.Id;
+            organization.UseGroups = true;
+            sutProvider.GetDependency<IOrganizationRepository>().GetByIdAsync(organization.Id).Returns(organization);
+            // organizationUser.OrganizationId = organization.Id;
+            sutProvider.GetDependency<IOrganizationUserRepository>().GetByIdAsync(organizationUser.Id)
+                .Returns(organizationUser);
 
+            await sutProvider.GetDependency<IGroupRepository>().DidNotReceiveWithAnyArgs()
+                .DeleteUserAsync(default, default);
+            await sutProvider.GetDependency<IEventService>().DidNotReceiveWithAnyArgs()
+                .LogOrganizationUserEventAsync(default, default);
             // user not in organization
-            await Assert.ThrowsAsync<NotFoundException>(() => _sut.DeleteUserAsync(group, nonOrganizationUser.Id));
+            await Assert.ThrowsAsync<NotFoundException>(() => sutProvider.Sut.DeleteUserAsync(group, organizationUser.Id));
             // invalid user
-            await Assert.ThrowsAsync<NotFoundException>(() => _sut.DeleteUserAsync(group, Guid.NewGuid()));
+            await Assert.ThrowsAsync<NotFoundException>(() => sutProvider.Sut.DeleteUserAsync(group, Guid.NewGuid()));
         }
     }
 }
