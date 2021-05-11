faf6de4ccd4eb713bb760707d42ed2f76f3f7087
Matt Gibson <mgibson@bitwarden.com>
2021-05-11 09:57:05 -0500

    Fix formatting

--- src/Core/Models/Mail/IMailQueueMessage.cs
+++ src/Core/Models/Mail/IMailQueueMessage.cs
@@ -7,8 +7,8 @@ namespace Bit.Core.Models.Mail
         string Subject { get; set; }
         IEnumerable<string> ToEmails { get; set; }
         IEnumerable<string> BccEmails { get; set; }
-        string Category {get; set;}
+        string Category { get; set; }
         string TemplateName { get; set; }
         object Model { get; set; }
-}
+    }
 }
