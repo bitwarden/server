da8ca843dd097ea0a80c356916865a33ba44e30a
Kyle Spearrin <kyle.spearrin@gmail.com>
2021-05-13 12:54:00 -0400

    adds suppose for postal and multi service mail delivery

---
+++ src/Core/Services/Implementations/MultiServiceMailDeliveryService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Bit.Core.Settings;
+using Microsoft.AspNetCore.Hosting;
+using System.Net.Http;
+using Bit.Core.Models.Mail;
+using Bit.Core.Utilities;
+
+namespace Bit.Core.Services
+{
+    public class MultiServiceMailDeliveryService : IMailDeliveryService
+    {
+        private readonly IMailDeliveryService _sesService;
+        private readonly IMailDeliveryService _postalService;
+        private readonly int _postalPercentage;
+
+        private static Random _random = new Random();
+
+        public MultiServiceMailDeliveryService(
+            GlobalSettings globalSettings,
+            IWebHostEnvironment hostingEnvironment,
+            IHttpClientFactory httpClientFactory,
+            ILogger<AmazonSesMailDeliveryService> sesLogger,
+            ILogger<PostalMailDeliveryService> postalLogger)
+        {
+            _sesService = new AmazonSesMailDeliveryService(globalSettings, hostingEnvironment, sesLogger);
+
+            if (CoreHelpers.SettingHasValue(globalSettings.Mail?.PostalApiKey) &&
+                CoreHelpers.SettingHasValue(globalSettings.Mail?.PostalDomain))
+            {
+                _postalService = new PostalMailDeliveryService(globalSettings, postalLogger, hostingEnvironment,
+                    httpClientFactory);
+            }
+
+            // 2% by default
+            _postalPercentage = (globalSettings.Mail?.PostalPercentage).GetValueOrDefault(2);
+        }
+
+        public async Task SendEmailAsync(MailMessage message)
+        {
+            var roll = _random.Next(0, 99);
+            if (_postalService != null && roll < _postalPercentage)
+            {
+                await _postalService.SendEmailAsync(message);
+            }
+            else
+            {
+                await _sesService.SendEmailAsync(message);
+            }
+        }
+    }
+}
---
+++ src/Core/Services/Implementations/PostalMailDeliveryService.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Threading.Tasks;
+using Bit.Core.Settings;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Hosting;
+using System.Text;
+
+namespace Bit.Core.Services
+{
+    public class PostalMailDeliveryService : IMailDeliveryService
+    {
+        private readonly GlobalSettings _globalSettings;
+        private readonly ILogger<PostalMailDeliveryService> _logger;
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly string _baseTag;
+        private readonly string _from;
+        private readonly string _reply;
+
+        public PostalMailDeliveryService(
+            GlobalSettings globalSettings,
+            ILogger<PostalMailDeliveryService> logger,
+            IWebHostEnvironment hostingEnvironment,
+            IHttpClientFactory clientFactory)
+        {
+            _globalSettings = globalSettings;
+            _logger = logger;
+            _clientFactory = clientFactory;
+            _baseTag = $"Env: {hostingEnvironment.EnvironmentName}, " +
+                $"Server: {globalSettings.ProjectName?.Replace(' ', '_')}";
+            _from = $"\"{globalSettings.SiteName}\" <no-reply@{_globalSettings.Mail.PostalDomain}>";
+            _reply = $"\"{globalSettings.SiteName}\" <{globalSettings.Mail.ReplyToEmail}>";
+        }
+
+        public async Task SendEmailAsync(Models.Mail.MailMessage message)
+        {
+            var httpClient = _clientFactory.CreateClient("PostalMailDeliveryService");
+            httpClient.DefaultRequestHeaders.Add("X-Server-API-Key", _globalSettings.Mail.PostalApiKey);
+
+            var request = new PostalRequest
+            {
+                subject = message.Subject,
+                from = _from,
+                reply_to = _reply,
+                html_body = message.HtmlContent,
+                to = new List<string>(),
+                tag = _baseTag
+            };
+            foreach (var address in message.ToEmails)
+            {
+                request.to.Add(address);
+            }
+
+            if (message.BccEmails != null)
+            {
+                request.bcc = new List<string>();
+                foreach (var address in message.BccEmails)
+                {
+                    request.bcc.Add(address);
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(message.TextContent))
+            {
+                request.plain_body = message.TextContent;
+            }
+
+            if (!string.IsNullOrWhiteSpace(message.Category))
+            {
+                request.tag = string.Concat(request.tag, ", Cat: ", message.Category);
+            }
+
+            var reqJson = JsonConvert.SerializeObject(request);
+            var responseMessage = await httpClient.PostAsync(
+                $"https://{_globalSettings.Mail.PostalDomain}/api/v1/send/message",
+                new StringContent(reqJson, Encoding.UTF8, "application/json"));
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var json = await responseMessage.Content.ReadAsStringAsync();
+                var response = JsonConvert.DeserializeObject<PostalResponse>(json);
+                if (response.status != "success")
+                {
+                    _logger.LogError("Postal send status was not successful: {0}, {1}",
+                        response.status, response.message);
+                }
+            }
+            else
+            {
+                _logger.LogError("Postal send failed: {0}", responseMessage.StatusCode);
+            }
+        }
+
+        public class PostalRequest
+        {
+            public List<string> to { get; set; }
+            public List<string> cc { get; set; }
+            public List<string> bcc { get; set; }
+            public string tag { get; set; }
+            public string from { get; set; }
+            public string reply_to { get; set; }
+            public string plain_body { get; set; }
+            public string html_body { get; set; }
+            public string subject { get; set; }
+        }
+
+        public class PostalResponse
+        {
+            public string status { get; set; }
+            public string message { get; set; }
+        }
+    }
+}
