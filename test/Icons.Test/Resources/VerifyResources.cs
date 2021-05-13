bfea4ed55120b47cebf47cd9276cc115635babe9
Justin Baur <136baur@gmail.com>
2021-05-12 21:41:33 -0400

    Added tests to verify resource names

---
+++ test/Icons.Test/Resources/VerifyResources.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Xunit;
+
+namespace Bit.Icons.Test.Resources
+{
+    public class VerifyResources
+    {
+        [Theory]
+        [InlineData("Bit.Icons.Resources.public_suffix_list.dat")]
+        public void Resources_FoundAndReadable(string resourceName)
+        {
+            var assembly = typeof(Program).GetTypeInfo().Assembly;
+
+            using (var resource = assembly.GetManifestResourceStream(resourceName))
+            {
+                Assert.NotNull(resource);
+                Assert.True(resource.CanRead);
+            }
+        }
+    }
+}
