dc8e06260f7012caa14efcd38d21ccc5775c2b7f
addison <addisonbeck1@gmail.com>
2021-05-14 11:41:48 -0400

    initial pass at device repo

--- src/Core/Repositories/EntityFramework/DeviceRepository.cs
+++ src/Core/Repositories/EntityFramework/DeviceRepository.cs
@@ -6,7 +6,6 @@ using AutoMapper;
 using Bit.Core.Models.Table;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using DataModel = Bit.Core.Models.Data;
 using EfModel = Bit.Core.Models.EntityFramework;
 using TableModel = Bit.Core.Models.Table;
 
@@ -30,24 +29,45 @@ namespace Bit.Core.Repositories.EntityFramework
             }
         }
 
-        public Task<Device> GetByIdAsync(Guid id, Guid userId)
+        public async Task<Device> GetByIdAsync(Guid id, Guid userId)
         {
-            throw new NotImplementedException();
+            var device = await base.GetByIdAsync(id);
+            if (device == null || device.UserId != userId)
+            {
+                return null;
+            }
+
+            return device;
         }
 
-        public Task<Device> GetByIdentifierAsync(string identifier)
+        public async Task<Device> GetByIdentifierAsync(string identifier)
         {
-            throw new NotImplementedException();
+            using (var scope = ServiceScopeFactory.CreateScope())
+            {
+                var dbContext = GetDatabaseContext(scope);
+                var query = dbContext.Devices.Where(d => d.Identifier == identifier);
+                return await query.FirstOrDefaultAsync();
+            }
         }
 
-        public Task<Device> GetByIdentifierAsync(string identifier, Guid userId)
+        public async Task<Device> GetByIdentifierAsync(string identifier, Guid userId)
         {
-            throw new NotImplementedException();
+            using (var scope = ServiceScopeFactory.CreateScope())
+            {
+                var dbContext = GetDatabaseContext(scope);
+                var query = dbContext.Devices.Where(d => d.Identifier == identifier && d.UserId == userId);
+                return await query.FirstOrDefaultAsync();
+            }
         }
 
-        public Task<ICollection<Device>> GetManyByUserIdAsync(Guid userId)
+        public async Task<ICollection<Device>> GetManyByUserIdAsync(Guid userId)
         {
-            throw new NotImplementedException();
+            using (var scope = ServiceScopeFactory.CreateScope())
+            {
+                var dbContext = GetDatabaseContext(scope);
+                var query = dbContext.Devices.Where(d => d.UserId == userId);
+                return (ICollection<Device>)await query.ToListAsync();
+            }
         }
     }
 }
