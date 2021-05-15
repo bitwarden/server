aec752fd3a10f8bbdeb050f2ded91f750ba0a25a
hantatsang <hantatsang@gmail.com>
2021-05-15 19:23:13 +1000

    Add missing CollectionFixtures

---
+++ test/Core.Test/AutoFixture/CollectionFixtures.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AutoFixture;
+using Bit.Core.Test.AutoFixture.Attributes;
+using Bit.Core.Test.AutoFixture.OrganizationFixtures;
+
+namespace Bit.Core.Test.AutoFixture
+{
+    internal class CollectionDefaultIdAutoDataAttribute : CustomAutoDataAttribute
+    {
+        public CollectionDefaultIdAutoDataAttribute() : base(new SutProviderCustomization(), new Organization { CollectionId = default(Guid)})
+        { }
+    }
+
+    internal class CollectionNonDefaultIdAutoDataAttribute : CustomAutoDataAttribute
+    {
+        public CollectionNonDefaultIdAutoDataAttribute() : base(new SutProviderCustomization(), new Organization { CollectionId = Guid.NewGuid() })
+        { }
+    }
+}
