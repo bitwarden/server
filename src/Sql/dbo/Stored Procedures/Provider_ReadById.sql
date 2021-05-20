61307e11b06a8d7374b0f4570e2c29a02275e76c
Oscar Hinton <oscar@oscarhinton.com>
2021-05-20 14:39:26 +0200

    Provider: Initial db structure (#1309)
    * Initial db structure

---
+++ src/Sql/dbo/Stored Procedures/Provider_ReadById.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[Provider_ReadById]
+    @Id UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    SELECT
+        *
+    FROM
+        [dbo].[ProviderView]
+    WHERE
+        [Id] = @Id
+END
---
+++ src/Sql/dbo/Tables/ProviderUser.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[ProviderUser] (
+    [Id]           UNIQUEIDENTIFIER    NOT NULL,
+    [ProviderId]   UNIQUEIDENTIFIER    NOT NULL,
+    [UserId]       UNIQUEIDENTIFIER    NULL,
+    [Email]        NVARCHAR (256)      NULL,
+    [Key]          VARCHAR (MAX)       NULL,
+    [Status]       TINYINT             NOT NULL,
+    [Type]         TINYINT             NOT NULL,
+    [Permissions]  NVARCHAR (MAX)      NULL,
+    [CreationDate] DATETIME2 (7)       NOT NULL,
+    [RevisionDate] DATETIME2 (7)       NOT NULL,
+    CONSTRAINT [PK_ProviderUser] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_ProviderUser_Provider] FOREIGN KEY ([ProviderId]) REFERENCES [dbo].[Provider] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_ProviderUser_User] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([Id])
+);
