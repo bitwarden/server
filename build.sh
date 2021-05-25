55b34796af91e74476c823012a0c4d56174d068a
Vince Grassia <593223+vgrassia@users.noreply.github.com>
2021-05-25 11:33:46 -0400

    Update build script to remove unused PUSH and TAG features

--- build.sh
+++ build.sh
@@ -4,88 +4,44 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo ""
+echo "Building Bitwarden"
+echo "=================="
 
-if [ $# -gt 1 -a "$1" == "push" ]
-then
-    TAG=$2
+chmod u+x "$DIR/src/Api/build.sh"
+"$DIR/src/Api/build.sh"
 
-    echo "Pushing Bitwarden ($TAG)"
-    echo "========================"
+chmod u+x "$DIR/src/Identity/build.sh"
+"$DIR/src/Identity/build.sh"
 
-    docker push bitwarden/api:$TAG
-    docker push bitwarden/identity:$TAG
-    docker push bitwarden/server:$TAG
-    docker push bitwarden/attachments:$TAG
-    docker push bitwarden/icons:$TAG
-    docker push bitwarden/notifications:$TAG
-    docker push bitwarden/events:$TAG
-    docker push bitwarden/admin:$TAG
-    docker push bitwarden/nginx:$TAG
-    docker push bitwarden/k8s-proxy:$TAG
-    docker push bitwarden/sso:$TAG
-    docker push bitwarden/portal:$TAG
-    docker push bitwarden/mssql:$TAG
-    docker push bitwarden/setup:$TAG
-elif [ $# -gt 1 -a "$1" == "tag" ]
-then
-    TAG=$2
+chmod u+x "$DIR/util/Server/build.sh"
+"$DIR/util/Server/build.sh"
 
-    echo "Tagging Bitwarden as '$TAG'"
+chmod u+x "$DIR/util/Nginx/build.sh"
+"$DIR/util/Nginx/build.sh"
 
-    docker tag bitwarden/api bitwarden/api:$TAG
-    docker tag bitwarden/identity bitwarden/identity:$TAG
-    docker tag bitwarden/server bitwarden/server:$TAG
-    docker tag bitwarden/attachments bitwarden/attachments:$TAG
-    docker tag bitwarden/icons bitwarden/icons:$TAG
-    docker tag bitwarden/notifications bitwarden/notifications:$TAG
-    docker tag bitwarden/events bitwarden/events:$TAG
-    docker tag bitwarden/admin bitwarden/admin:$TAG
-    docker tag bitwarden/nginx bitwarden/nginx:$TAG
-    docker tag bitwarden/k8s-proxy bitwarden/k8s-proxy:$TAG
-    docker tag bitwarden/sso bitwarden/sso:$TAG
-    docker tag bitwarden/portal bitwarden/portal:$TAG
-    docker tag bitwarden/mssql bitwarden/mssql:$TAG
-    docker tag bitwarden/setup bitwarden/setup:$TAG
-else
-    echo "Building Bitwarden"
-    echo "=================="
+chmod u+x "$DIR/util/Attachments/build.sh"
+"$DIR/util/Attachments/build.sh"
 
-    chmod u+x "$DIR/src/Api/build.sh"
-    "$DIR/src/Api/build.sh"
+chmod u+x "$DIR/src/Icons/build.sh"
+"$DIR/src/Icons/build.sh"
 
-    chmod u+x "$DIR/src/Identity/build.sh"
-    "$DIR/src/Identity/build.sh"
+chmod u+x "$DIR/src/Notifications/build.sh"
+"$DIR/src/Notifications/build.sh"
 
-    chmod u+x "$DIR/util/Server/build.sh"
-    "$DIR/util/Server/build.sh"
+chmod u+x "$DIR/src/Events/build.sh"
+"$DIR/src/Events/build.sh"
 
-    chmod u+x "$DIR/util/Nginx/build.sh"
-    "$DIR/util/Nginx/build.sh"
+chmod u+x "$DIR/src/Admin/build.sh"
+"$DIR/src/Admin/build.sh"
 
-    chmod u+x "$DIR/util/Attachments/build.sh"
-    "$DIR/util/Attachments/build.sh"
+chmod u+x "$DIR/bitwarden_license/src/Sso/build.sh"
+"$DIR/bitwarden_license/src/Sso/build.sh"
 
-    chmod u+x "$DIR/src/Icons/build.sh"
-    "$DIR/src/Icons/build.sh"
+chmod u+x "$DIR/bitwarden_license/src/Portal/build.sh"
+"$DIR/bitwarden_license/src/Portal/build.sh"
 
-    chmod u+x "$DIR/src/Notifications/build.sh"
-    "$DIR/src/Notifications/build.sh"
+chmod u+x "$DIR/util/MsSql/build.sh"
+"$DIR/util/MsSql/build.sh"
 
-    chmod u+x "$DIR/src/Events/build.sh"
-    "$DIR/src/Events/build.sh"
-
-    chmod u+x "$DIR/src/Admin/build.sh"
-    "$DIR/src/Admin/build.sh"
-
-    chmod u+x "$DIR/bitwarden_license/src/Sso/build.sh"
-    "$DIR/bitwarden_license/src/Sso/build.sh"
-
-    chmod u+x "$DIR/bitwarden_license/src/Portal/build.sh"
-    "$DIR/bitwarden_license/src/Portal/build.sh"
-
-    chmod u+x "$DIR/util/MsSql/build.sh"
-    "$DIR/util/MsSql/build.sh"
-
-    chmod u+x "$DIR/util/Setup/build.sh"
-    "$DIR/util/Setup/build.sh"
-fi
+chmod u+x "$DIR/util/Setup/build.sh"
+"$DIR/util/Setup/build.sh"
