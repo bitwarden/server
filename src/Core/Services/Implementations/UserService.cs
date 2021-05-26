d7f3507d4479967c9ea50b3fe8794895cf0e2c3e
Vincent Salucci <26154748+vincentsalucci@users.noreply.github.com>
2021-05-26 15:51:54 -0500

    [Reset Password] Added new event type for admin password reset (#1350)

--- src/Core/Services/Implementations/UserService.cs
+++ src/Core/Services/Implementations/UserService.cs
@@ -690,7 +690,7 @@ namespace Bit.Core.Services
 
             await _userRepository.ReplaceAsync(user);
             // TODO Reset Password - Send email alerting user of changed password
-            await _eventService.LogUserEventAsync(user.Id, EventType.User_ChangedPassword);
+            await _eventService.LogOrganizationUserEventAsync(orgUser, EventType.OrganizationUser_AdminResetPassword);
             await _pushService.PushLogOutAsync(user.Id);
 
             return IdentityResult.Success;
