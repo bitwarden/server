3da80b9dcaa38a5a3b96a27e14d5dd194eefb781
Kyle Spearrin <kspearrin@users.noreply.github.com>
2021-06-16 12:47:41 -0400

    hcaptcha validation on password login (#1398)

--- src/Core/Utilities/ServiceCollectionExtensions.cs
+++ src/Core/Utilities/ServiceCollectionExtensions.cs
@@ -253,6 +253,16 @@ namespace Bit.Core.Utilities
             {
                 services.AddSingleton<IReferenceEventService, AzureQueueReferenceEventService>();
             }
+
+            if (!globalSettings.SelfHosted && CoreHelpers.SettingHasValue(globalSettings.Captcha?.HCaptchaSecretKey) &&
+                CoreHelpers.SettingHasValue(globalSettings.Captcha?.HCaptchaSiteKey))
+            {
+                services.AddSingleton<ICaptchaValidationService, HCaptchaValidationService>();
+            }
+            else
+            {
+                services.AddSingleton<ICaptchaValidationService, NoopCaptchaValidationService>();
+            }
         }
 
         public static void AddNoopServices(this IServiceCollection services)
