9f10b16dc42e9b109784c641203174900632fc96
Chad Scharf <3904944+cscharf@users.noreply.github.com>
2021-07-07 10:49:59 -0400

    Fix queue message encoding for Azure (UTF-16 in XML) (#1439)
    * Revert "Encode into b64 to avoid illegal xml encoding when sending to Azure (#1425)"
    
    This reverts commit 2c9a5bb4ab58ab68192b2fb94ff459b7326a99b1.
    
    * Azure queue to use base64 encoding universally
    
    * Ensure byte size calc is using encoded byte count
    
    * Remove message text extension from blockIP svc
    
    * Remove unused using on blockIp hosted service

--- src/Core/Utilities/CoreHelpers.cs
+++ src/Core/Utilities/CoreHelpers.cs
@@ -23,6 +23,10 @@ using Microsoft.Azure.Storage.Blob;
 using Bit.Core.Models.Table;
 using IdentityModel;
 using System.Text.Json;
+using Bit.Core.Enums.Provider;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using System.Threading;
 
 namespace Bit.Core.Utilities
 {
@@ -875,6 +879,23 @@ namespace Bit.Core.Utilities
             }
             list.Add(item);
             return list;
+        }
+
+        public static string DecodeMessageText(this QueueMessage message)
+        {
+            var text = message?.MessageText;
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return text;
+            }
+            try
+            {
+                return Base64DecodeString(text);
+            }
+            catch
+            {
+                return text;
+            }
         }
     }
 }
