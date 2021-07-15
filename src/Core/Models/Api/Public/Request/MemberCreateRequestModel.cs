b2c95be31a6d2e071125479b1f550627fea79202
Thomas Rittson <trittson@bitwarden.com>
2021-07-15 16:10:32 +1000

    Remove duplicate checks, use attributes instead

--- src/Core/Models/Api/Public/Request/MemberCreateRequestModel.cs
+++ src/Core/Models/Api/Public/Request/MemberCreateRequestModel.cs
@@ -6,34 +6,20 @@ using Bit.Core.Utilities;
 
 namespace Bit.Core.Models.Api.Public
 {
-    public class MemberCreateRequestModel : MemberUpdateRequestModel, IValidatableObject
+    public class MemberCreateRequestModel : MemberUpdateRequestModel
     {
         /// <summary>
         /// The member's email address.
         /// </summary>
         /// <example>jsmith@example.com</example>
         [Required]
+        [StringLength(256)]
         [StrictEmailAddress]
         public string Email { get; set; }
 
         public override OrganizationUser ToOrganizationUser(OrganizationUser existingUser)
         {
             throw new NotImplementedException();
-        }
-
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            var strictEmailAttribute = new StrictEmailAddressAttribute();
-            if (!strictEmailAttribute.IsValid(Email))
-            {
-                yield return new ValidationResult($"Email is not valid.",
-                    new string[] { nameof(Email) });
-            }
-            else if (Email.Length > 256)
-            {
-                yield return new ValidationResult($"Email is longer than 256 characters.",
-                    new string[] { nameof(Email) });
-            }
         }
     }
 }
