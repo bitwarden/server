7abb05391406a8ed29645db117afecf1c976946e
Thomas Rittson <31796059+eliykat@users.noreply.github.com>
2021-07-16 08:01:51 +1000

    Refactor email attributes (#1458)
    * Add StrictEmailAddress attribute
    
    * Remove duplicate checks, use attributes instead
    
    * Rename EmailAddressListAttribute

---
+++ src/Core/Utilities/StrictEmailAddressAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Bit.Core.Utilities
+{
+    public class StrictEmailAddressAttribute : ValidationAttribute
+    {
+        public StrictEmailAddressAttribute()
+            : base("The {0} field is not a valid e-mail address.")
+        {}
+        
+        public override bool IsValid(object value)
+        {
+            var emailAddress = value?.ToString();
+            if (emailAddress == null)
+            {
+                return false;
+            }
+
+            var illegalChars = @"[\s<>()]";
+            if (Regex.IsMatch(emailAddress, illegalChars))
+            {
+                return false;
+            }
+             
+            return new EmailAddressAttribute().IsValid(emailAddress);
+        }
+    }
+}
