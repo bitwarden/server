94bdade7f41185e9f98198b561d541b27e177dd2
Thomas Rittson <trittson@bitwarden.com>
2021-07-22 15:35:47 +1000

    Fix linting

--- test/Core.Test/Utilities/StrictEmailAddressAttributeTests.cs
+++ test/Core.Test/Utilities/StrictEmailAddressAttributeTests.cs
@@ -20,22 +20,22 @@ namespace Bit.Core.Test.Utilities
         }
         
         [Theory]
-        [InlineData(null)]                      // null
-        [InlineData("hello@world.com\t")]       // trailing tab char
-        [InlineData("\thello@world.com")]       // leading tab char
-        [InlineData("hel\tlo@world.com")]       // local-part tab char
-        [InlineData("hello@world.com\b")]       // trailing backspace char
-        [InlineData("\"   \"hello@world.com")]  // leading spaces in quotes
-        [InlineData("hello@world.com\"    \"")] // trailing spaces in quotes
-        [InlineData("hel\"   \"lo@world.com")]  // local-part spaces in quotes
-        [InlineData("hello there@world.com")]   // unescaped unquoted spaces
-        [InlineData("Hello World <hello@world.com>")]    // friendly from
-        [InlineData("<hello@world.com>")]       // wrapped angle brackets
-        [InlineData("hello(comment)there@world.com")]   // comment
-        [InlineData("hello@world.com.")]        // trailing period
-        [InlineData(".hello@world.com")]        // leading period
-        [InlineData("hello@world.com;")]        // trailing semicolon
-        [InlineData(";hello@world.com")]        // leading semicolon
+        [InlineData(null)]                          // null
+        [InlineData("hello@world.com\t")]           // trailing tab char
+        [InlineData("\thello@world.com")]           // leading tab char
+        [InlineData("hel\tlo@world.com")]           // local-part tab char
+        [InlineData("hello@world.com\b")]           // trailing backspace char
+        [InlineData("\"   \"hello@world.com")]      // leading spaces in quotes
+        [InlineData("hello@world.com\"    \"")]     // trailing spaces in quotes
+        [InlineData("hel\"   \"lo@world.com")]      // local-part spaces in quotes
+        [InlineData("hello there@world.com")]       // unescaped unquoted spaces
+        [InlineData("Hello <hello@world.com>")]     // friendly from
+        [InlineData("<hello@world.com>")]           // wrapped angle brackets
+        [InlineData("hello(com)there@world.com")]   // comment
+        [InlineData("hello@world.com.")]            // trailing period
+        [InlineData(".hello@world.com")]            // leading period
+        [InlineData("hello@world.com;")]            // trailing semicolon
+        [InlineData(";hello@world.com")]            // leading semicolon
         [InlineData("hello@world.com; hello@world.com")]    // semicolon separated list
         [InlineData("hello@world.com, hello@world.com")]    // comma separated list
 
