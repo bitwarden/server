37f235f1fb5b39db5a8e33a58324e2190d7c5596
Chad Scharf <3904944+cscharf@users.noreply.github.com>
2021-08-26 18:30:23 -0400

    Update to web version 2.22.2

--- scripts/bitwarden.sh
+++ scripts/bitwarden.sh
@@ -35,7 +35,7 @@ GITHUB_BASE_URL="https://raw.githubusercontent.com/bitwarden/server/master"
 
 # Please do not create pull requests modifying the version numbers.
 COREVERSION="1.42.2"
-WEBVERSION="2.22.1"
+WEBVERSION="2.22.2"
 
 echo "bitwarden.sh version $COREVERSION"
 docker --version
