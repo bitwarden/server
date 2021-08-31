f2520ed7bee94500a0411413d1f00bebe1c76342
Vincent Salucci <26154748+vincentsalucci@users.noreply.github.com>
2021-08-31 14:23:06 -0500

    [Reset Password] Enable force password reset (#1557)

--- src/Core/Services/Implementations/UserService.cs
+++ src/Core/Services/Implementations/UserService.cs
@@ -690,11 +690,7 @@ namespace Bit.Core.Services
 
             user.RevisionDate = user.AccountRevisionDate = DateTime.UtcNow;
             user.Key = key;
-            
-            /* 
-            The reset password flow is disabled for the August 2021 release.
-            user.ForcePasswordReset = true; 
-            */
+            user.ForcePasswordReset = true;
 
             await _userRepository.ReplaceAsync(user);
             await _mailService.SendAdminResetPasswordEmailAsync(user.Email, user.Name ?? user.Email, org.Name);
