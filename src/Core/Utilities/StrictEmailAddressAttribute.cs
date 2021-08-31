06ca7a5d991acbdb153bbc2ce9ee6a6fe27ca7bf
Thomas Rittson <31796059+eliykat@users.noreply.github.com>
2021-08-31 13:49:11 +1000

    Add support for international domain names (IDN) in email addresses (#1512)
    * Adjust email address checking to handle unicode
    
    * ASCII only in local part
    * allow unicode in second-level and top-level domain
    
    * Add PunyEncoding/Decoding methods and tests
    
    * Use PunyEncoding for outbound email recipients
    
    * Use MailKit for punycode, handle edge cases
    
    * Punyencode all email addresses in mailServices
    
    * Remove punyencoding from HandlebarsMailService
    
    * Add to punyencoding tests
    
    * Use more inclusive e-mail error
    
    * Fix comment wording
    
    * Apply StrictEmail checking to emergency access invite
    
    * Remove punyDecode helper

--- src/Core/Utilities/StrictEmailAddressAttribute.cs
+++ src/Core/Utilities/StrictEmailAddressAttribute.cs
@@ -7,7 +7,7 @@ namespace Bit.Core.Utilities
     public class StrictEmailAddressAttribute : ValidationAttribute
     {
         public StrictEmailAddressAttribute()
-            : base("The {0} field is not a valid e-mail address.")
+            : base("The {0} field is not a supported e-mail address format.")
         {}
         
         public override bool IsValid(object value)
@@ -31,7 +31,18 @@ namespace Bit.Core.Utilities
                 return false;
             }
 
-            if (!Regex.IsMatch(emailAddress, @"@.+\.[A-Za-z0-9]+$"))
+            /**
+            The regex below is intended to catch edge cases that are not handled by the general parsing check above.
+            This enforces the following rules:
+            * Requires ASCII only in the local-part (code points 0-127)
+            * Requires an @ symbol
+            * Allows any char in second-level domain name, including unicode and symbols
+            * Requires at least one period (.) separating SLD from TLD
+            * Must end in a letter (including unicode)
+            See the unit tests for examples of what is allowed.
+            **/
+            var emailFormat = @"[\x00-\x7F]+@.+\.\p{L}+$";
+            if (!Regex.IsMatch(emailAddress, emailFormat))
             {
                 return false;
             }
