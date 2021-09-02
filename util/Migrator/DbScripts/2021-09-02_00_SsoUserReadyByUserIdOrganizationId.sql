5c7622965ba0478cc6a236c835be1629eded0cc2
Thomas Rittson <trittson@bitwarden.com>
2021-09-02 16:56:02 +1000

    Check for existing db query before creating

--- util/Migrator/DbScripts/2021-09-02_00_SsoUserReadyByUserIdOrganizationId.sql
+++ util/Migrator/DbScripts/2021-09-02_00_SsoUserReadyByUserIdOrganizationId.sql
@@ -1,3 +1,9 @@
+IF OBJECT_ID('[dbo].[SsoUser_ReadByUserIdOrganizationId]') IS NOT NULL
+BEGIN
+    DROP PROCEDURE [dbo].[SsoUser_ReadByUserIdOrganizationId]
+END
+GO
+
 CREATE PROCEDURE [dbo].[SsoUser_ReadByUserIdOrganizationId]
     @UserId UNIQUEIDENTIFIER,
     @OrganizationId UNIQUEIDENTIFIER
