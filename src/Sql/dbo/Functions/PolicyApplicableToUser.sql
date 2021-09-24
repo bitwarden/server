a5a509ca475fd7bc1d3bc8640e2f559ce6c0508f
Thomas Rittson <trittson@bitwarden.com>
2021-09-24 16:02:10 +1000

    Refactor SQL to remove unnecessary operations

--- src/Sql/dbo/Functions/PolicyApplicableToUser.sql
+++ src/Sql/dbo/Functions/PolicyApplicableToUser.sql
@@ -13,9 +13,15 @@ FROM
 INNER JOIN
     [dbo].[OrganizationUserView] OU ON P.[OrganizationId] = OU.[OrganizationId]
 LEFT JOIN
-    [dbo].[ProviderUserProviderOrganizationDetailsView] PUPO 
-        ON OU.[OrganizationId] = PUPO.[OrganizationId]
-        AND OU.[UserId] = PUPO.[UserId]
+    (SELECT
+        PU.UserId,
+        PO.OrganizationId
+    FROM
+        [dbo].[ProviderUserView] PU
+    INNER JOIN
+        [ProviderOrganizationView] PO ON PO.[ProviderId] = PU.[ProviderId]) PUPO
+    ON PUPO.UserId = OU.UserId
+    AND PUPO.OrganizationId = P.OrganizationId
 WHERE
     OU.[UserId] = @UserId 
     AND P.[Type] = @PolicyType
@@ -26,4 +32,4 @@ WHERE
         OU.[Permissions] IS NULL
         OR COALESCE(JSON_VALUE(OU.[Permissions], '$.managePolicies'), 'false') = 'false'
     )
-    AND PUPO.[ProviderId] IS NULL   -- Not a provider
+    AND PUPO.[UserId] IS NULL   -- Not a provider
