b0264a5d71f195cc0ec78eab46921efd86afe67e
Matt Gibson <mgibson@bitwarden.com>
2021-10-05 07:57:19 -0400

    Improve formatting

--- util/Migrator/DbScripts/2021-09-21_00_SplitManageCollectionsJson.sql
+++ util/Migrator/DbScripts/2021-09-21_00_SplitManageCollectionsJson.sql
@@ -3,17 +3,21 @@ UPDATE [vault_dev].[dbo].[OrganizationUser]
 SET [Permissions] = 
     JSON_MODIFY(
         JSON_MODIFY(
-            [Permissions], '$.editAssignedCollections', CAST(ISNULL(
-                                                            ISNULL(
-                                                                JSON_VALUE([Permissions], '$.editAssignedCollections'),
-                                                                JSON_VALUE([Permissions], '$.manageAssignedCollections')),
-                                                            0) AS BIT)
+            [Permissions],
+            '$.editAssignedCollections',
+            CAST(ISNULL(
+                    ISNULL(
+                        JSON_VALUE([Permissions], '$.editAssignedCollections'),
+                        JSON_VALUE([Permissions], '$.manageAssignedCollections')
+                  ),
+                0) AS BIT)
         ),
-        '$.deleteAssignedCollections', CAST(ISNULL(
-                                        ISNULL(
-                                            JSON_VALUE([Permissions], '$.deleteAssignedCollections'),
-                                            JSON_VALUE([Permissions], '$.manageAssignedCollections')),
-                                        0) AS BIT)
+        '$.deleteAssignedCollections',
+        CAST(ISNULL(
+            ISNULL(
+                JSON_VALUE([Permissions], '$.deleteAssignedCollections'),
+                JSON_VALUE([Permissions], '$.manageAssignedCollections')),
+            0) AS BIT)
     )
 WHERE [Permissions] IS NOT NULL
     AND ISJSON([Permissions]) > 0
@@ -28,23 +32,27 @@ SET [Permissions] =
     JSON_MODIFY(
         JSON_MODIFY(
             JSON_MODIFY(
-                [Permissions], '$.createNewCollections', CAST(ISNULL(
-                                                            ISNULL(
-                                                                JSON_VALUE([Permissions], '$.createNewCollections'),
-                                                                JSON_VALUE([Permissions], '$.manageAllCollections')),
-                                                            0) AS BIT)
+                [Permissions],
+                '$.createNewCollections',
+                CAST(ISNULL(
+                    ISNULL(
+                        JSON_VALUE([Permissions], '$.createNewCollections'),
+                        JSON_VALUE([Permissions], '$.manageAllCollections')),
+                    0) AS BIT)
             ),
-            '$.editAnyCollection', CAST(ISNULL(
-                                    ISNULL(
-                                        JSON_VALUE([Permissions], '$.editAnyCollection'),
-                                        JSON_VALUE([Permissions], '$.manageAllCollections')),
-                                    0) AS BIT)
+            '$.editAnyCollection',
+            CAST(ISNULL(
+                ISNULL(
+                    JSON_VALUE([Permissions], '$.editAnyCollection'),
+                    JSON_VALUE([Permissions], '$.manageAllCollections')),
+                0) AS BIT)
         ),
-        '$.deleteAnyCollection', CAST(ISNULL(
-                                    ISNULL(
-                                        JSON_VALUE([Permissions], '$.deleteAnyCollection'),
-                                        JSON_VALUE([Permissions], '$.manageAllCollections')),
-                                    0) AS BIT)
+        '$.deleteAnyCollection',
+            CAST(ISNULL(
+                ISNULL(
+                    JSON_VALUE([Permissions], '$.deleteAnyCollection'),
+                    JSON_VALUE([Permissions], '$.manageAllCollections')),
+                0) AS BIT)
     )
 WHERE [Permissions] IS NOT NULL
     AND ISJSON([Permissions]) > 0 
--- util/MySqlMigrations/Scripts/2021-09-21_00_SplitManageCollectionsPermission.sql
+++ util/MySqlMigrations/Scripts/2021-09-21_00_SplitManageCollectionsPermission.sql
@@ -1,17 +1,20 @@
 -- Split Manage Assigned Collections into edit and delete
 UPDATE `bw-vault`.`OrganizationUser`
 SET `Permissions` =
-	JSON_INSERT(`Permissions`,
-		'$.editAssignedCollections', IFNULL(
-										IFNULL(
-											JSON_EXTRACT(`Permissions`,'$.editAssignedCollections'),
-											JSON_EXTRACT(`Permissions`, '$.manageAssignedCollections')),
-										false),
-		'$.deleteAssignedCollections', IFNULL(
-										IFNULL(
-											JSON_EXTRACT(`Permissions`, '$.deleteAssignedCollections'),
-											JSON_EXTRACT(`Permissions`, '$.manageAssignedCollections')),
-										false)
+	JSON_INSERT(
+		`Permissions`,
+		'$.editAssignedCollections',
+		IFNULL(
+			IFNULL(
+				JSON_EXTRACT(`Permissions`,'$.editAssignedCollections'),
+				JSON_EXTRACT(`Permissions`, '$.manageAssignedCollections')),
+			false),
+		'$.deleteAssignedCollections', 
+		IFNULL(
+			IFNULL(
+				JSON_EXTRACT(`Permissions`, '$.deleteAssignedCollections'),
+				JSON_EXTRACT(`Permissions`, '$.manageAssignedCollections')),
+			false)
 	)
 WHERE `Permissions` IS NOT NULL
     AND JSON_VALID(`Permissions`) > 0
@@ -23,22 +26,26 @@ WHERE `Permissions` IS NOT NULL
 -- Split Manage All Collections into create, edit, and delete
 UPDATE `bw-vault`.`OrganizationUser`
 SET `Permissions` =
-	JSON_INSERT(`Permissions`,
-		'$.createNewCollections', IFNULL(
-									IFNULL(
-										JSON_EXTRACT(`Permissions`, '$.createNewColletions'),
-                                        JSON_EXTRACT(`Permissions`, '$.manageAllCollections')),
-									false),
-		'$.editAnyCollection', IFNULL(
-								IFNULL(
-									JSON_EXTRACT(`Permissions`, '$.editAnyCollection'),
-                                    JSON_EXTRACT(`Permissions`, '$.manageAllCollections')),
-								false),
-		'$.deleteAnyCollection', IFNULL(
-									IFNULL(
-										JSON_EXTRACT(`Permissions`, '$.deleteAnyCollection'),
-                                        JSON_EXTRACT(`Permissions`, '$.manageAllCollections')),
-									false)
+	JSON_INSERT(
+		`Permissions`,
+		'$.createNewCollections',
+		IFNULL(
+			IFNULL(
+				JSON_EXTRACT(`Permissions`, '$.createNewColletions'),
+				JSON_EXTRACT(`Permissions`, '$.manageAllCollections')),
+			false),
+		'$.editAnyCollection',
+		IFNULL(
+			IFNULL(
+				JSON_EXTRACT(`Permissions`, '$.editAnyCollection'),
+				JSON_EXTRACT(`Permissions`, '$.manageAllCollections')),
+			false),
+		'$.deleteAnyCollection',
+		IFNULL(
+			IFNULL(
+				JSON_EXTRACT(`Permissions`, '$.deleteAnyCollection'),
+				JSON_EXTRACT(`Permissions`, '$.manageAllCollections')),
+			false)
 	)
 WHERE `Permissions` IS NOT NULL
 	AND JSON_VALID(`Permissions`) > 0
