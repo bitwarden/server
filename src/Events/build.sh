e13dcb9dfa9479cb4b53eafe4242f928bccf5000
Joseph Flinn <joseph.s.flinn@gmail.com>
2021-10-06 12:41:43 -0700

    revamping the build scripts

--- src/Events/build.sh
+++ src/Events/build.sh
@@ -13,7 +13,3 @@ echo "Clean"
 dotnet clean "$DIR/Events.csproj" -c "Release" -o "$DIR/obj/build-output/publish"
 echo "Publish"
 dotnet publish "$DIR/Events.csproj" -c "Release" -o "$DIR/obj/build-output/publish"
-
-echo -e "\nBuilding docker image"
-docker --version
-docker build -t bitwarden/events "$DIR/."
