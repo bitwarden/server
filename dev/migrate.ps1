008e5b2166c9901b953eabfd15738c240479b474
Hinton <hinton.oscar@gmail.com>
2021-10-06 13:46:00 +0200

    Fix migrate command on mac

--- dev/migrate.ps1
+++ dev/migrate.ps1
@@ -1,3 +1,3 @@
 # Creates the vault_dev database, and runs all the migrations.
 
-docker-compose -f .\docker-compose.yml --profile mssql exec mssql bash /mnt/helpers/migrate.sh @args
+docker-compose --profile mssql exec mssql bash /mnt/helpers/migrate.sh @args
