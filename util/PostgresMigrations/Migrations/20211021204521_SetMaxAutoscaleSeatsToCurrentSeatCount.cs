34b3326a969f37935bec45fa1851a205a6415277
Matt Gibson <mgibson@bitwarden.com>
2021-10-21 17:13:37 -0400

    Default autoscaling to off
    (cherry picked from commit b0763cfb84cbfcfd19365c69beaccaeb0bf3c2d9)

---
+++ util/PostgresMigrations/Migrations/20211021204521_SetMaxAutoscaleSeatsToCurrentSeatCount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Bit.Core.Utilities;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bit.PostgresMigrations.Migrations
+{
+    public partial class SetMaxAutoscaleSeatsToCurrentSeatCount : Migration
+    {
+        private const string _scriptLocation =
+            "PostgresMigration.Scripts.2021-10-21_00_SetMaxAutoscaleSeatCount.sql";
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(CoreHelpers.GetEmbeddedResourceContentsAsync(_scriptLocation));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            throw new Exception("Irreversible migration");
+        }
+    }
+}
