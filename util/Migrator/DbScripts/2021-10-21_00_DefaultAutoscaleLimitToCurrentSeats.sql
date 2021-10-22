86013937f346c17f9f5c684d1572ce93de1ff093
Matt Gibson <mgibson@bitwarden.com>
2021-10-22 07:43:55 -0500

    Update util/Migrator/DbScripts/2021-10-21_00_DefaultAutoscaleLimitToCurrentSeats.sql
    Co-authored-by: Chad Scharf <3904944+cscharf@users.noreply.github.com>

--- util/Migrator/DbScripts/2021-10-21_00_DefaultAutoscaleLimitToCurrentSeats.sql
+++ util/Migrator/DbScripts/2021-10-21_00_DefaultAutoscaleLimitToCurrentSeats.sql
@@ -1,5 +1,5 @@
 IF NOT EXISTS (SELECT TOP(1) 1 FROM [dbo].[Organization] WHERE [MaxAutoscaleSeats] IS NOT NULL) 
-    AND NOT EXISTS ( SELECT TOP(1) 1 FROM [dbo].[Organization] WHERE OwnersNotifiedOfAutoscaling IS NOT NULL)
+    AND NOT EXISTS ( SELECT TOP(1) 1 FROM [dbo].[Organization] WHERE [OwnersNotifiedOfAutoscaling] IS NOT NULL)
 BEGIN
 UPDATE [dbo].[Organization]
     SET MaxAutoscaleSeats = Seats
