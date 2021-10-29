fcbf0f094e5d8923b9b69ca4397edd8874269c1f
Matt Gibson <mgibson@bitwarden.com>
2021-10-29 18:43:45 -0400

    WIP: Organization sponsorship flow

--- test/Common/AutoFixture/Attributes/SutAutoDataAttribute.cs
+++ test/Common/AutoFixture/Attributes/SutAutoDataAttribute.cs
@@ -6,6 +6,11 @@ using AutoFixture.Xunit2;
 
 namespace Bit.Test.Common.AutoFixture.Attributes
 {
+    public class SutProviderCustomizeAttribute : BitCustomizeAttribute
+    {
+        public override ICustomization GetCustomization() => new SutProviderCustomization();
+    }
+
     public class SutAutoDataAttribute : CustomAutoDataAttribute
     {
         public SutAutoDataAttribute(params Type[] iCustomizationTypes) : base(
