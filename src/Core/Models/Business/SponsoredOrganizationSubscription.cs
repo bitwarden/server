a350bde2b0cf8ac16d94a04378a6c2c75d5eb3c7
Matt Gibson <mgibson@bitwarden.com>
2021-11-05 13:38:59 -0400

    Add sponsorship id to subscription metadata

--- src/Core/Models/Business/SponsoredOrganizationSubscription.cs
+++ src/Core/Models/Business/SponsoredOrganizationSubscription.cs
@@ -5,8 +5,10 @@ namespace Bit.Core.Models.Business
 {
     public class SponsoredOrganizationSubscription
     {
+        public const string OrganizationSponsorhipIdMetadataKey = "OrganizationSponsorshipId";
         private readonly string _customerId;
         private readonly Organization _org;
+        private readonly OrganizationSponsorship _sponsorship;
         private readonly StaticStore.Plan _plan;
         private readonly StaticStore.SponsoredPlan _sponsoredPlan;
         private readonly List<Stripe.TaxRate> _taxRates;
@@ -14,6 +16,7 @@ namespace Bit.Core.Models.Business
         public SponsoredOrganizationSubscription(Organization org, OrganizationSponsorship sponsorship, Stripe.Subscription existingSubscription)
         {
             _org = org;
+            _sponsorship = sponsorship;
             _customerId = org.GatewayCustomerId;
             _plan = Utilities.StaticStore.GetPlan(org.PlanType);
             _sponsoredPlan = Utilities.StaticStore.GetSponsoredPlan(sponsorship.PlanSponsorshipType.Value);
@@ -21,9 +24,14 @@ namespace Bit.Core.Models.Business
         }
 
         public SponsorOrganizationSubscriptionOptions GetSponsorSubscriptionOptions(int additionalSeats = 0,
-            int additionalStorageGb = 0, bool premiumAccessAddon = false) =>
-            new SponsorOrganizationSubscriptionOptions(_customerId, _org, _plan,
+            int additionalStorageGb = 0, bool premiumAccessAddon = false)
+        {
+            var subCreateOptions = new SponsorOrganizationSubscriptionOptions(_customerId, _org, _plan,
                 _sponsoredPlan, _taxRates, additionalSeats, additionalStorageGb, premiumAccessAddon);
+
+            subCreateOptions.Metadata.Add(OrganizationSponsorhipIdMetadataKey, _sponsorship.Id.ToString());
+            return subCreateOptions;
+        }
 
         public OrganizationUpgradeSubscriptionOptions RemoveOrganizationSubscriptionOptions(int additionalSeats = 0,
             int additionalStorageGb = 0, bool premiumAccessAddon = false) =>
