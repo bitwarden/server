e820aac719786107e9496c12c5cd87763e888c81
Matt Gibson <mgibson@bitwarden.com>
2021-11-09 10:40:31 -0500

    Use userId to find org users

--- test/Api.Test/Controllers/OrganizationSponsorshipsControllerTests.cs
+++ test/Api.Test/Controllers/OrganizationSponsorshipsControllerTests.cs
@@ -58,45 +58,21 @@ namespace Bit.Api.Test.Controllers
         [Theory]
         [BitMemberAutoData(nameof(NonConfirmedOrganizationUsersStatuses))]
         public async Task CreateSponsorship_BadSponsoringUserStatus_ThrowsBadRequest(
-            OrganizationUserStatusType statusType, Guid userId, Organization org, OrganizationUser orgUser,
+            OrganizationUserStatusType statusType, Organization org, OrganizationUser orgUser,
             OrganizationSponsorshipRequestModel model, SutProvider<OrganizationSponsorshipsController> sutProvider)
         {
             org.PlanType = PlanType.EnterpriseAnnually;
             orgUser.Status = statusType;
-            orgUser.UserId = userId;
-            model.OrganizationUserId = orgUser.Id;
 
             sutProvider.GetDependency<IOrganizationRepository>().GetByIdAsync(org.Id).Returns(org);
-            sutProvider.GetDependency<ICurrentContext>().UserId.Returns(userId);
-            sutProvider.GetDependency<IOrganizationUserRepository>().GetByIdAsync(orgUser.Id).Returns(orgUser);
+            sutProvider.GetDependency<ICurrentContext>().UserId.Returns(orgUser.UserId);
+            sutProvider.GetDependency<IOrganizationUserRepository>().GetByOrganizationAsync(org.Id, orgUser.UserId.Value)
+                .Returns(orgUser);
 
             var exception = await Assert.ThrowsAsync<BadRequestException>(() =>
                 sutProvider.Sut.CreateSponsorship(org.Id.ToString(), model));
 
-            Assert.Contains("Only confirm users can sponsor other organizations.", exception.Message);
-            await sutProvider.GetDependency<IOrganizationSponsorshipService>()
-                .DidNotReceiveWithAnyArgs()
-                .OfferSponsorshipAsync(default, default, default, default, default);
-        }
-
-        [Theory]
-        [BitAutoData("c56c7ab4-a174-412a-a822-abe53ea71d50")]
-        public async Task CreateSponsorship_CreateSponsorshipAsDifferentUser_ThrowsBadRequest(Guid userId,
-            Organization org, OrganizationUser orgUser, OrganizationSponsorshipRequestModel model,
-            SutProvider<OrganizationSponsorshipsController> sutProvider)
-        {
-            org.PlanType = PlanType.EnterpriseAnnually;
-            orgUser.Status = OrganizationUserStatusType.Confirmed;
-            model.OrganizationUserId = orgUser.Id;
-
-            sutProvider.GetDependency<IOrganizationRepository>().GetByIdAsync(org.Id).Returns(org);
-            sutProvider.GetDependency<ICurrentContext>().UserId.Returns(userId);
-            sutProvider.GetDependency<IOrganizationUserRepository>().GetByIdAsync(orgUser.Id).Returns(orgUser);
-
-            var exception = await Assert.ThrowsAsync<BadRequestException>(() =>
-                sutProvider.Sut.CreateSponsorship(org.Id.ToString(), model));
-
-            Assert.Contains("Can only create organization sponsorships for yourself.", exception.Message);
+            Assert.Contains("Only confirmed users can sponsor other organizations.", exception.Message);
             await sutProvider.GetDependency<IOrganizationSponsorshipService>()
                 .DidNotReceiveWithAnyArgs()
                 .OfferSponsorshipAsync(default, default, default, default, default);
@@ -110,11 +86,11 @@ namespace Bit.Api.Test.Controllers
         {
             org.PlanType = PlanType.EnterpriseAnnually;
             orgUser.Status = OrganizationUserStatusType.Confirmed;
-            model.OrganizationUserId = orgUser.Id;
 
             sutProvider.GetDependency<IOrganizationRepository>().GetByIdAsync(org.Id).Returns(org);
             sutProvider.GetDependency<ICurrentContext>().UserId.Returns(orgUser.UserId);
-            sutProvider.GetDependency<IOrganizationUserRepository>().GetByIdAsync(orgUser.Id).Returns(orgUser);
+            sutProvider.GetDependency<IOrganizationUserRepository>().GetByOrganizationAsync(org.Id, orgUser.UserId.Value)
+                .Returns(orgUser);
             sutProvider.GetDependency<IOrganizationSponsorshipRepository>()
                 .GetBySponsoringOrganizationUserIdAsync(orgUser.Id).Returns(sponsorship);
 
