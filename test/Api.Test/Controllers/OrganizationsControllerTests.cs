780dd57fac3decca2b7dcbfe9857594287ddc063
Thomas Rittson <trittson@bitwarden.com>
2021-11-15 17:49:06 +1000

    Don't use sutProvider for test

--- test/Api.Test/Controllers/OrganizationsControllerTests.cs
+++ test/Api.Test/Controllers/OrganizationsControllerTests.cs
@@ -1,22 +1,59 @@
 using AutoFixture.Xunit2;
-using System.Threading.Tasks;
-using Xunit;
-using Bit.Core.Test.AutoFixture;
 using Bit.Api.Controllers;
-using System;
+using Bit.Core.Context;
 using Bit.Core.Exceptions;
-using Bit.Core.Services;
-using NSubstitute;
 using Bit.Core.Models.Table;
 using Bit.Core.Repositories;
+using Bit.Core.Services;
+using Bit.Core.Settings;
+using NSubstitute;
+using System.Threading.Tasks;
+using System;
+using Xunit;
 
 namespace Bit.Api.Test.Controllers
 {
-    public class OrganizationsControllerTests
+    public class OrganizationsControllerTests: IDisposable
     {
+        private readonly GlobalSettings _globalSettings;
+        private readonly ICurrentContext _currentContext;
+        private readonly IOrganizationRepository _organizationRepository;
+        private readonly IOrganizationService _organizationService;
+        private readonly IOrganizationUserRepository _organizationUserRepository;
+        private readonly IPaymentService _paymentService;
+        private readonly IPolicyRepository _policyRepository;
+        private readonly ISsoConfigRepository _ssoConfigRepository;
+        private readonly ISsoConfigService _ssoConfigService;
+        private readonly IUserService _userService;
+
+        private readonly OrganizationsController _sut;
+
+        public OrganizationsControllerTests()
+        {
+            _currentContext = Substitute.For<ICurrentContext>();
+            _globalSettings = Substitute.For<GlobalSettings>();
+            _organizationRepository = Substitute.For<IOrganizationRepository>();
+            _organizationService = Substitute.For<IOrganizationService>();
+            _organizationUserRepository = Substitute.For<IOrganizationUserRepository>();
+            _paymentService = Substitute.For<IPaymentService>();
+            _policyRepository = Substitute.For<IPolicyRepository>();
+            _ssoConfigRepository = Substitute.For<ISsoConfigRepository>();
+            _ssoConfigService = Substitute.For<ISsoConfigService>();
+            _userService = Substitute.For<IUserService>();
+
+            _sut = new OrganizationsController(_organizationRepository, _organizationUserRepository,
+                _policyRepository, _organizationService, _userService, _paymentService, _currentContext,
+                _ssoConfigRepository, _ssoConfigService, _globalSettings);
+        }
+
+        public void Dispose()
+        {
+            _sut?.Dispose();
+        }
+
         [Theory, AutoData]
         public async Task OrganizationsController_WhenUserTriestoLeaveOrganizationUsingKeyConnector_Throws(
-            Guid orgId, SutProvider<OrganizationsController> sutProvider)
+            Guid orgId, OrganizationsController sut)
         {
             var ssoConfig = new SsoConfig
             {
@@ -26,17 +63,15 @@ namespace Bit.Api.Test.Controllers
                 OrganizationId = orgId,
             };
 
-            sutProvider.GetDependency<ISsoConfigRepository>()
-                .GetByOrganizationIdAsync(orgId).Returns(ssoConfig);
+            _ssoConfigRepository.GetByOrganizationIdAsync(orgId).Returns(ssoConfig);
 
             var exception = await Assert.ThrowsAsync<BadRequestException>(
-                () => sutProvider.Sut.Leave(orgId.ToString()));
+                () => sut.Leave(orgId.ToString()));
 
             Assert.Contains("You cannot leave an Organization that is using Key Connector.",
                 exception.Message);
 
-            await sutProvider.GetDependency<IOrganizationService>().DidNotReceiveWithAnyArgs()
-                .DeleteUserAsync(default, default);
+            await _organizationService.DidNotReceiveWithAnyArgs().DeleteUserAsync(default, default);
         }
     }
 }
