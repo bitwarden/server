3fb844779a3f28b093f27529aa232b90c394f3ef
Matt Gibson <mgibson@bitwarden.com>
2021-12-20 08:38:36 -0500

    Test AccessPolicyResult boolean logic

--- test/Core.Test/AccessPolicies/AccessPolicyResultTests.cs
+++ test/Core.Test/AccessPolicies/AccessPolicyResultTests.cs
@@ -1,18 +1,60 @@
-﻿using Bit.Core.AccessPolicies;
+﻿using System.Threading.Tasks;
+using Bit.Core.AccessPolicies;
 using Bit.Test.Common.AutoFixture.Attributes;
+using Stripe;
 using Xunit;
 
 namespace Bit.Core.Test.AccessPolicies
 {
     public class AccessPolicyResultTests
     {
-        [Theory]
-        [BitAutoData]
-        public void AccessPolicyResult_Concatenates_Errors(AccessPolicyResult first, AccessPolicyResult second)
+        [Fact]
+        public void Concatenates_Errors()
         {
-            var result = first.And(second);
+            var result = AccessPolicyResult.Fail("first").And(AccessPolicyResult.Fail("second"));
 
-            Assert.Equal($"{first.BlockReason}\n{second.BlockReason}", result.BlockReason);
+            Assert.Equal($"first\nsecond", result.BlockReason);
+        }
+
+        [Fact]
+        public void LazyAnd_SingleError()
+        {
+            var result = AccessPolicyResult.Fail("first").LazyAnd(AccessPolicyResult.Fail("second"));
+            Assert.Equal(AccessPolicyResult.Fail("first"), result);
+        }
+
+        [Fact]
+        public void AndFails()
+        {
+            var result = AccessPolicyResult.Success.And(AccessPolicyResult.Fail("second"));
+            Assert.Equal(AccessPolicyResult.Fail("second"), result);
+        }
+
+        public void FuncAndOverload_IsLazy()
+        {
+            var secondPolicyRan = false;
+            var result = AccessPolicyResult.Fail("first").And(() =>
+            {
+                secondPolicyRan = true;
+                return AccessPolicyResult.Success;
+            });
+
+            Assert.Equal(AccessPolicyResult.Fail("first"), result);
+            Assert.False(secondPolicyRan);
+        }
+
+        [Fact]
+        public async Task AndAsync_IsLazy()
+        {
+            var secondPolicyRan = false;
+            var result = await AccessPolicyResult.Fail("first").AndAsync(() =>
+            {
+                secondPolicyRan = true;
+                return Task.FromResult(AccessPolicyResult.Success);
+            });
+
+            Assert.Equal(AccessPolicyResult.Fail("first"), result);
+            Assert.False(secondPolicyRan);
         }
     }
 }
