22cbe27c89a87ca52e5dcad1425005a99263dee2
Matt Gibson <mgibson@bitwarden.com>
2021-12-23 14:25:21 -0500

    Use epoch milliseconds for expiration in tokens

--- src/Core/Tokenizer/ExpiringToken.cs
+++ src/Core/Tokenizer/ExpiringToken.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Text.Json.Serialization;
+using Bit.Core.Utilities;
 
 namespace Bit.Core.Tokenizer
 {
     public abstract class ExpiringToken : ITokenable
     {
+        [JsonConverter(typeof(EpochDateTimeJsonConverter))]
         public DateTime ExpirationDate { get; set; }
         public bool Valid => ExpirationDate < DateTime.UtcNow && TokenIsValid();
         protected abstract bool TokenIsValid();
