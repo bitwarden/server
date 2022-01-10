109d52c0a393d68d054b2acdc0be8f5ace4585a4
Justin Baur <admin@justinbaur.com>
2022-01-10 16:12:15 -0500

    Run formatting

--- test/Core.Test/Identity/BaseTokenProviderTests.cs
+++ test/Core.Test/Identity/BaseTokenProviderTests.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -41,7 +41,7 @@ namespace Bit.Core.Test.Identity
             sutProvider.GetDependency<IServiceProvider>()
                 .GetService(typeof(IUserService))
                 .Returns(userService);
-            
+
             SetupUserService(userService, user);
 
             return userService;
