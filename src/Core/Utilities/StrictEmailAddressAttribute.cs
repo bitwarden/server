67b0f7e921003b0783d3ab5854d8d187c98460c7
Justin Baur <admin@justinbaur.com>
2022-01-14 07:32:55 -0500

    Use compiled Regex and cached items

--- src/Core/Utilities/StrictEmailAddressAttribute.cs
+++ src/Core/Utilities/StrictEmailAddressAttribute.cs
@@ -6,11 +6,27 @@ namespace Bit.Core.Utilities
 {
     public class StrictEmailAddressAttribute : ValidationAttribute
     {
+        /**
+        The regex below is intended to catch edge cases that are not handled by the general parsing check above.
+        This enforces the following rules:
+        * Requires ASCII only in the local-part (code points 0-127)
+        * Requires an @ symbol
+        * Allows any char in second-level domain name, including unicode and symbols
+        * Requires at least one period (.) separating SLD from TLD
+        * Must end in a letter (including unicode)
+        See the unit tests for examples of what is allowed.
+        **/
+        private static readonly Regex _emailFormatRegex = new Regex(@"[\x00-\x7F]+@.+\.\p{L}+$", RegexOptions.Compiled);
+        private static readonly EmailAddressAttribute _emailAddressAttribute = new EmailAddressAttribute();
+
         public StrictEmailAddressAttribute()
             : base("The {0} field is not a supported e-mail address format.")
         { }
 
         public override bool IsValid(object value)
+            => IsValidCore(value);
+
+        public static bool IsValidCore(object value)
         {
             var emailAddress = value?.ToString();
             if (emailAddress == null)
@@ -31,23 +47,13 @@ namespace Bit.Core.Utilities
                 return false;
             }
 
-            /**
-            The regex below is intended to catch edge cases that are not handled by the general parsing check above.
-            This enforces the following rules:
-            * Requires ASCII only in the local-part (code points 0-127)
-            * Requires an @ symbol
-            * Allows any char in second-level domain name, including unicode and symbols
-            * Requires at least one period (.) separating SLD from TLD
-            * Must end in a letter (including unicode)
-            See the unit tests for examples of what is allowed.
-            **/
-            var emailFormat = @"[\x00-\x7F]+@.+\.\p{L}+$";
-            if (!Regex.IsMatch(emailAddress, emailFormat))
+            
+            if (!_emailFormatRegex.IsMatch(emailAddress))
             {
                 return false;
             }
 
-            return new EmailAddressAttribute().IsValid(emailAddress);
+            return _emailAddressAttribute.IsValid(emailAddress);
         }
     }
 }
