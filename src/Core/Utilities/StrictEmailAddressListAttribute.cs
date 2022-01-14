02346f1c428a7652e029ab6f7654e9d3a11c8093
Justin Baur <admin@justinbaur.com>
2022-01-14 07:33:24 -0500

    Use static method

--- src/Core/Utilities/StrictEmailAddressListAttribute.cs
+++ src/Core/Utilities/StrictEmailAddressListAttribute.cs
@@ -8,7 +8,6 @@ namespace Bit.Core.Utilities
     {
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            var strictEmailAttribute = new StrictEmailAddressAttribute();
             var emails = value as IList<string>;
 
             if (!emails?.Any() ?? true)
@@ -24,7 +23,7 @@ namespace Bit.Core.Utilities
             for (var i = 0; i < emails.Count(); i++)
             {
                 var email = emails.ElementAt(i);
-                if (!strictEmailAttribute.IsValid(email))
+                if (!StrictEmailAddressAttribute.IsValidCore(email))
                 {
                     return new ValidationResult($"Email #{i + 1} is not valid.");
                 }
