c6818a330e6d23a307665df7203207c9b7e6514f
Vince Grassia <593223+vgrassia@users.noreply.github.com>
2022-02-16 16:25:22 -0500

    Updating Nginx container and adding Docker Compose

--- util/Nginx/entrypoint.sh
+++ util/Nginx/entrypoint.sh
@@ -1,47 +1,47 @@
 #!/bin/bash
 
-# Setup
+## Create user and group
+#
+#groupadd -o -g $LGID $GROUPNAME >/dev/null 2>&1 ||
+#groupmod -o -g $LGID $GROUPNAME >/dev/null 2>&1
+#useradd -o -u $LUID -g $GROUPNAME -s /bin/false $USERNAME >/dev/null 2>&1 ||
+#usermod -o -u $LUID -g $GROUPNAME -s /bin/false $USERNAME >/dev/null 2>&1
+#mkhomedir_helper $USERNAME
+#
+## The rest...
+#
+#chown -R $USERNAME:$GROUPNAME /etc/bitwarden
+#cp /etc/bitwarden/nginx/*.conf /etc/nginx/conf.d/
+#mkdir -p /etc/letsencrypt
+#chown -R $USERNAME:$GROUPNAME /etc/letsencrypt
+#mkdir -p /etc/ssl
+#chown -R $USERNAME:$GROUPNAME /etc/ssl
+#mkdir -p /var/run/nginx
+#touch /var/run/nginx/nginx.pid
+#chown -R $USERNAME:$GROUPNAME /var/run/nginx
+#chown -R $USERNAME:$GROUPNAME /var/cache/nginx
+#chown -R $USERNAME:$GROUPNAME /var/log/nginx
 
-GROUPNAME="bitwarden"
-USERNAME="bitwarden"
 
-LUID=${LOCAL_UID:-0}
-LGID=${LOCAL_GID:-0}
-
-# Step down from host root to well-known nobody/nogroup user
-
-if [ $LUID -eq 0 ]
-then
-    LUID=65534
+# Generate SSL certificates
+if [ "$BW_GENERATE_CERT" = "true" ]; then
+  if [ -z "$(ls -A /bitwarden/ssl/self)" ]; then
+    openssl req \
+    -x509 \
+    -newkey rsa:4096 \
+    -sha256 \
+    -nodes \
+    -days 36500 \
+    -keyout ${BW_KEY_PATH:-/bitwarden/ssl/self/private.key} \
+    -out ${BW_CERT_PATH:-/bitwarden/ssl/self/certificate.crt} \
+    -reqexts SAN \
+    -extensions SAN \
+    -config <(cat /usr/lib/ssl/openssl.cnf <(printf '[SAN]\nsubjectAltName=DNS:${BW_DOMAIN:-localhost}\nbasicConstraints=CA:true')) \
+    -subj \"/C=US/ST=California/L=Santa Barbara/O=Bitwarden Inc./OU=Bitwarden/CN=$BW_DOMAIN\"
+  fi
 fi
-if [ $LGID -eq 0 ]
-then
-    LGID=65534
-fi
-
-# Create user and group
-
-groupadd -o -g $LGID $GROUPNAME >/dev/null 2>&1 ||
-groupmod -o -g $LGID $GROUPNAME >/dev/null 2>&1
-useradd -o -u $LUID -g $GROUPNAME -s /bin/false $USERNAME >/dev/null 2>&1 ||
-usermod -o -u $LUID -g $GROUPNAME -s /bin/false $USERNAME >/dev/null 2>&1
-mkhomedir_helper $USERNAME
-
-# The rest...
-
-chown -R $USERNAME:$GROUPNAME /etc/bitwarden
-cp /etc/bitwarden/nginx/*.conf /etc/nginx/conf.d/
-mkdir -p /etc/letsencrypt
-chown -R $USERNAME:$GROUPNAME /etc/letsencrypt
-mkdir -p /etc/ssl
-chown -R $USERNAME:$GROUPNAME /etc/ssl
-mkdir -p /var/run/nginx
-touch /var/run/nginx/nginx.pid
-chown -R $USERNAME:$GROUPNAME /var/run/nginx
-chown -R $USERNAME:$GROUPNAME /var/cache/nginx
-chown -R $USERNAME:$GROUPNAME /var/log/nginx
 
 # Launch a loop to rotate nginx logs on a daily basis
-gosu $USERNAME:$GROUPNAME /bin/sh -c "/logrotate.sh loop >/dev/null 2>&1 &"
+gosu nginx:nginx /bin/sh -c "/logrotate.sh loop >/dev/null 2>&1 &"
 
-exec gosu $USERNAME:$GROUPNAME nginx -g 'daemon off;'
+exec gosu nginx:nginx nginx -g 'daemon off;'
