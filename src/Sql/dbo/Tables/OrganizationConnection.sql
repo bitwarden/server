602eff68d09ed209e25d61e5165b68e57222f089
Justin Baur <admin@justinbaur.com>
2022-03-02 15:15:24 -0500

    Fix table

--- src/Sql/dbo/Tables/OrganizationConnection.sql
+++ src/Sql/dbo/Tables/OrganizationConnection.sql
@@ -6,9 +6,8 @@ CREATE TABLE [dbo].[OrganizationConnection] (
     [Config]            NVARCHAR (MAX) NULL,
     CONSTRAINT [PK_OrganizationConnection] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_OrganizationConnection_OrganizationId] FOREIGN KEY ([OrganizationId]) REFERENCES [dbo].[Organization] ([Id])
-)
+);
 GO
 
 CREATE NONCLUSTERED INDEX [IX_OrganizationConnection_OrganizationId]
     ON [dbo].[OrganizationConnection]([OrganizationId] ASC);
-END
