2455cd3c59a821f7946fc56d0b8bec9b59802020
Matt Gibson <mgibson@bitwarden.com>
2022-03-21 10:06:15 -0400

    Allow setting bitwarden cloud api url
    Useful for testing api integration between installations and cloud

--- src/Core/Settings/GlobalSettings.cs
+++ src/Core/Settings/GlobalSettings.cs
@@ -424,6 +424,7 @@ namespace Bit.Core.Settings
         public class InstallationSettings
         {
             private string _identityUri;
+            private string _apiUri;
 
             public Guid Id { get; set; }
             public string Key { get; set; }
@@ -431,6 +432,10 @@ namespace Bit.Core.Settings
             {
                 get => string.IsNullOrWhiteSpace(_identityUri) ? "https://identity.bitwarden.com" : _identityUri;
                 set => _identityUri = value;
+            }
+            public string ApiUri
+            {
+                get => string.IsNullOrWhiteSpace(_apiUri) ? "https://api.biwarden.com" : _apiUri;
             }
         }
 
