175654ac7dea6a8511ea070ea9918739e4129731
Jacob Fink <jfink@bitwarden.com>
2022-03-29 09:49:17 -0400

    make userId and sponsorshipType non nullable

--- src/Core/Models/Api/Response/OrganizationSponsorships/OrganizationSponsorshipResponseModel.cs
+++ src/Core/Models/Api/Response/OrganizationSponsorships/OrganizationSponsorshipResponseModel.cs
@@ -6,11 +6,11 @@ namespace Bit.Core.Models.Api.Response.OrganizationSponsorships
 {
     public class OrganizationSponsorshipResponseModel
     {
-        public Guid? SponsoringOrganizationUserId { get; set; }
+        public Guid SponsoringOrganizationUserId { get; set; }
         public Guid? SponsoredOrganizationId { get; set; }
         public string FriendlyName { get; set; }
         public string OfferedToEmail { get; set; }
-        public PlanSponsorshipType? PlanSponsorshipType { get; set; }
+        public PlanSponsorshipType PlanSponsorshipType { get; set; }
         public DateTime? LastSyncDate { get; set; }
         public DateTime? ValidUntil { get; set; }
         public bool ToDelete { get; set; }
--- src/Core/Models/Data/OrganizationSponsorshipData.cs
+++ src/Core/Models/Data/OrganizationSponsorshipData.cs
@@ -5,11 +5,11 @@ namespace Bit.Core.Models.Data
 {
     public class OrganizationSponsorshipData
     {
-        public Guid? SponsoringOrganizationUserId { get; set; }
+        public Guid SponsoringOrganizationUserId { get; set; }
         public Guid? SponsoredOrganizationId { get; set; }
         public string FriendlyName { get; set; }
         public string OfferedToEmail { get; set; }
-        public PlanSponsorshipType? PlanSponsorshipType { get; set; }
+        public PlanSponsorshipType PlanSponsorshipType { get; set; }
         public DateTime? LastSyncDate { get; set; }
         public DateTime? ValidUntil { get; set; }
         public bool ToDelete { get; set; }
