8b1cfadcf0efb5de006057090cd715998b80f2c9
Jacob Fink <jfink@bitwarden.com>
2022-04-04 14:29:04 -0400

    remove extra method

--- src/Core/Repositories/IOrganizationSponsorshipRepository.cs
+++ src/Core/Repositories/IOrganizationSponsorshipRepository.cs
@@ -11,7 +11,6 @@ namespace Bit.Core.Repositories
         Task ReplaceManyAsync(IEnumerable<OrganizationSponsorship> organizationSponsorships);
         Task UpsertManyAsync(IEnumerable<OrganizationSponsorship> organizationSponsorships);
         Task DeleteManyAsync(IEnumerable<Guid> organizationSponsorshipIds);
-        Task DeleteExpired();
         Task<ICollection<OrganizationSponsorship>> GetManyBySponsoringOrganizationAsync(Guid sponsoringOrganizationId);
         Task<OrganizationSponsorship> GetBySponsoringOrganizationUserIdAsync(Guid sponsoringOrganizationUserId);
         Task<OrganizationSponsorship> GetBySponsoredOrganizationIdAsync(Guid sponsoredOrganizationId);
