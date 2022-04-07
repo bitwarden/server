793d57a81d21fadadbcca917b5edd28b230f4b8d
Vince Grassia <593223+vgrassia@users.noreply.github.com>
2022-04-07 16:33:52 -0400

    Update docker compose

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
 # Generate SSL certificates
-if [ "$BW_GENERATE_CERT" = "true" ]; then
-  if [ -z "$(ls -A /etc/bitwarden/ssl)" ]; then
-    openssl req \
-    -x509 \
-    -newkey rsa:4096 \
-    -sha256 \
-    -nodes \
-    -days 36500 \
-    -keyout /etc/bitwarden/ssl/${BW_SSL_KEY:-private.key} \
-    -out /etc/bitwarden/ssl/${BW_SSL_CERT:-certificate.crt} \
-    -reqexts SAN \
-    -extensions SAN \
-    -config <(cat /usr/lib/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:${BW_DOMAIN:-localhost}\nbasicConstraints=CA:true")) \
-    -subj "/C=US/ST=California/L=Santa Barbara/O=Bitwarden Inc./OU=Bitwarden/CN=${BW_DOMAIN:-localhost}"
-  fi
+if [ -z "$(ls -A /etc/bitwarden/ssl)" ]; then
+  openssl req \
+  -x509 \
+  -newkey rsa:4096 \
+  -sha256 \
+  -nodes \
+  -days 36500 \
+  -keyout /etc/bitwarden/ssl/${BW_SSL_KEY:-private.key} \
+  -out /etc/bitwarden/ssl/${BW_SSL_CERT:-certificate.crt} \
+  -reqexts SAN \
+  -extensions SAN \
+  -config <(cat /usr/lib/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:${BW_DOMAIN:-localhost}\nbasicConstraints=CA:true")) \
+  -subj "/C=US/ST=California/L=Santa Barbara/O=Bitwarden Inc./OU=Bitwarden/CN=${BW_DOMAIN:-localhost}"
 fi
 
 /usr/local/bin/confd -onetime -backend env
