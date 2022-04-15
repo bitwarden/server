54cee29dda607b0aa4bbb2b28c27ba0b2c058c1d
Matt Gibson <mgibson@bitwarden.com>
2022-04-15 19:40:53 -0400

    Use param xdocs on methods

--- test/Api.Test/AutoFixture/Attributes/ControllerCustomizeAttribute.cs
+++ test/Api.Test/AutoFixture/Attributes/ControllerCustomizeAttribute.cs
@@ -7,11 +7,14 @@ namespace Bit.Api.Test.AutoFixture.Attributes
     /// <summary>
     /// Disables setting of Auto Properties on the Controller to avoid ASP.net initialization errors from a mock environment. Still sets constructor dependencies.
     /// </summary>
-    /// <param name="controllerType">The Type of the controller to allow autofixture to create</param>
-
     public class ControllerCustomizeAttribute : BitCustomizeAttribute
     {
         private readonly Type _controllerType;
+
+        /// <summary>
+        /// Initialize an instance of the ControllerCustomizeAttribute class
+        /// </summary>
+        /// <param name="controllerType">The Type of the controller to allow autofixture to create</param>
         public ControllerCustomizeAttribute(Type controllerType)
         {
             _controllerType = controllerType;
