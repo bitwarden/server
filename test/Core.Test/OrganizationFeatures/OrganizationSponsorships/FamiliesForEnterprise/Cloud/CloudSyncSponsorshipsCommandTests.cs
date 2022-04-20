bd87f994497ed978210ebe84b351aa060f61e267
Matt Gibson <mgibson@bitwarden.com>
2022-04-20 15:21:25 -0400

    Fix failing test

--- test/Core.Test/OrganizationFeatures/OrganizationSponsorships/FamiliesForEnterprise/Cloud/CloudSyncSponsorshipsCommandTests.cs
+++ test/Core.Test/OrganizationFeatures/OrganizationSponsorships/FamiliesForEnterprise/Cloud/CloudSyncSponsorshipsCommandTests.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Bit.Core.Context;
 using Bit.Core.Entities;
 using Bit.Core.Enums;
 using Bit.Core.Exceptions;
@@ -81,10 +80,9 @@ namespace Bit.Core.Test.OrganizationFeatures.OrganizationSponsorships.FamiliesFo
 
         [Theory]
         [BitAutoData]
-        public async Task SyncOrganization_Success_RecordsEvent(Organization organization, Guid installationId,
+        public async Task SyncOrganization_Success_RecordsEvent(Organization organization,
             SutProvider<CloudSyncSponsorshipsCommand> sutProvider)
         {
-            sutProvider.GetDependency<ICurrentContext>().InstallationId.Returns(installationId);
             await sutProvider.Sut.SyncOrganization(organization, Array.Empty<OrganizationSponsorshipData>());
 
             await sutProvider.GetDependency<IEventService>().Received(1).LogOrganizationEventAsync(organization, EventType.Organization_SponsorshipsSynced, Arg.Any<DateTime?>());
