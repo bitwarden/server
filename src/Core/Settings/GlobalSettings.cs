bfe5fe34ecbc2acf4ece666cdb10566c9f8bac0d
Matt Gibson <mgibson@bitwarden.com>
2022-04-25 15:08:44 -0400

    Allow setting of ApiUri

--- src/Core/Settings/GlobalSettings.cs
+++ src/Core/Settings/GlobalSettings.cs
@@ -437,6 +437,7 @@ namespace Bit.Core.Settings
             public string ApiUri
             {
                 get => string.IsNullOrWhiteSpace(_apiUri) ? "https://api.biwarden.com" : _apiUri;
+                set => _apiUri = value;
             }
         }
 
