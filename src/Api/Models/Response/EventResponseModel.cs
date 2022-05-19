7b96c1850f356ae219c8cd0d788203b7572d73b1
Matt Gibson <mgibson@bitwarden.com>
2022-05-19 11:50:16 -0400

    Revert "Add InstallationId to event responses (#2007)"
    This reverts commit b4c219926df5c10dfcbc61d1c2205b2c1a196365.

--- src/Api/Models/Response/EventResponseModel.cs
+++ src/Api/Models/Response/EventResponseModel.cs
@@ -30,7 +30,6 @@ namespace Bit.Api.Models.Response
             Date = ev.Date;
             DeviceType = ev.DeviceType;
             IpAddress = ev.IpAddress;
-            InstallationId = ev.InstallationId;
         }
 
         public EventType Type { get; set; }
@@ -45,7 +44,6 @@ namespace Bit.Api.Models.Response
         public Guid? ProviderUserId { get; set; }
         public Guid? ProviderOrganizationId { get; set; }
         public Guid? ActingUserId { get; set; }
-        public Guid? InstallationId { get; set; }
         public DateTime Date { get; set; }
         public DeviceType? DeviceType { get; set; }
         public string IpAddress { get; set; }
