4d1ec5c4c10b5768d27024af123ab28885037633
Kyle Spearrin <kyle.spearrin@gmail.com>
2022-06-23 14:30:41 -0400

    group list filtering

--- bitwarden_license/src/Scim/Controllers/v2/GroupsController.cs
+++ bitwarden_license/src/Scim/Controllers/v2/GroupsController.cs
@@ -56,21 +56,60 @@ namespace Bit.Scim.Controllers.v2
         [HttpGet("")]
         public async Task<IActionResult> Get(
             Guid organizationId,
+            [FromQuery] string filter,
             [FromQuery] int? count,
             [FromQuery] int? startIndex)
         {
+            string nameFilter = null;
+            string externalIdFilter = null;
+            if (!string.IsNullOrWhiteSpace(filter))
+            {
+                if (filter.StartsWith("displayName eq "))
+                {
+                    nameFilter = filter.Substring(15).Trim('"');
+                }
+                else if (filter.StartsWith("externalId eq "))
+                {
+                    externalIdFilter = filter.Substring(14).Trim('"');
+                }
+            }
+
+            var groupList = new List<ScimGroupResponseModel> { };
             var groups = await _groupRepository.GetManyByOrganizationIdAsync(organizationId);
-            var groupList = groups.OrderBy(g => g.Name)
-                .Skip(startIndex.Value - 1)
-                .Take(count.Value)
-                .Select(g => new ScimGroupResponseModel(g))
-                .ToList();
+            var totalResults = 0;
+            if (!string.IsNullOrWhiteSpace(nameFilter))
+            {
+                var group = groups.FirstOrDefault(g => g.Name == nameFilter);
+                if (group != null)
+                {
+                    groupList.Add(new ScimGroupResponseModel(group));
+                }
+                totalResults = groupList.Count;
+            }
+            else if (!string.IsNullOrWhiteSpace(externalIdFilter))
+            {
+                var group = groups.FirstOrDefault(ou => ou.ExternalId == externalIdFilter);
+                if (group != null)
+                {
+                    groupList.Add(new ScimGroupResponseModel(group));
+                }
+                totalResults = groupList.Count;
+            }
+            else if (string.IsNullOrWhiteSpace(filter) && startIndex.HasValue && count.HasValue)
+            {
+                groupList = groups.OrderBy(g => g.Name)
+                    .Skip(startIndex.Value - 1)
+                    .Take(count.Value)
+                    .Select(g => new ScimGroupResponseModel(g))
+                    .ToList();
+                totalResults = groups.Count;
+            }
 
             var result = new ScimListResponseModel<ScimGroupResponseModel>
             {
                 Resources = groupList,
                 ItemsPerPage = count.GetValueOrDefault(groupList.Count),
-                TotalResults = groups.Count,
+                TotalResults = totalResults,
                 StartIndex = startIndex.GetValueOrDefault(1),
             };
             return new ObjectResult(result);
