d94aaadcd75339c3e07df024182421e551f37341
CarleyDiaz-Bitwarden <103955722+CarleyDiaz-Bitwarden@users.noreply.github.com>
2022-07-11 16:49:57 -0400

    Initial DB Changes migration for SM-2, SM-52 and SM-53

--- util/Migrator/DbScripts/2022-07-11_00_Initial_Organization.sql
+++ util/Migrator/DbScripts/2022-07-11_00_Initial_Organization.sql
@@ -1,4 +1,4 @@
-﻿IF OBJECT_ID('[dbo].[OrganizationPasswordManager]') IS NOT NULL
+IF OBJECT_ID('[dbo].[OrganizationPasswordManager]') IS NOT NULL
 
 BEGIN
     DROP TABLE [dbo].[OrganizationPasswordManager]
