108812129d9952dddbda44ef3a93667f7cd7a2cf
Rui Tome <rtome@bitwarden.com>
2022-07-22 10:22:38 +0100

    Updated controllers to use new methods in ICollectionService and ICipherService

--- src/Api/Controllers/OrganizationExportController.cs
+++ src/Api/Controllers/OrganizationExportController.cs
@@ -1,8 +1,5 @@
 ﻿using Bit.Api.Models.Response;
-using Bit.Core.Context;
 using Bit.Core.Entities;
-using Bit.Core.Exceptions;
-using Bit.Core.Repositories;
 using Bit.Core.Services;
 using Bit.Core.Settings;
 using Core.Models.Data;
@@ -15,28 +12,19 @@ namespace Bit.Api.Controllers
     [Authorize("Application")]
     public class OrganizationExportController : Controller
     {
-        private readonly ICipherRepository _cipherRepository;
-        private readonly ICollectionCipherRepository _collectionCipherRepository;
-        private readonly ICollectionRepository _collectionRepository;
-        private readonly ICurrentContext _currentContext;
-        private readonly IProviderService _providerService;
         private readonly IUserService _userService;
+        private readonly ICollectionService _collectionService;
+        private readonly ICipherService _cipherService;
         private readonly GlobalSettings _globalSettings;
 
         public OrganizationExportController(
-            ICipherRepository cipherRepository,
-            ICollectionCipherRepository collectionCipherRepository,
-            ICollectionRepository collectionRepository,
-            ICurrentContext currentContext,
-            IProviderService providerService,
+            ICipherService cipherService,
+            ICollectionService collectionService,
             IUserService userService,
             GlobalSettings globalSettings)
         {
-            _cipherRepository = cipherRepository;
-            _collectionCipherRepository = collectionCipherRepository;
-            _collectionRepository = collectionRepository;
-            _currentContext = currentContext;
-            _providerService = providerService;
+            _cipherService = cipherService;
+            _collectionService = collectionService;
             _userService = userService;
             _globalSettings = globalSettings;
         }
@@ -44,35 +32,15 @@ namespace Bit.Api.Controllers
         [HttpGet("export")]
         public async Task<OrganizationExportResponseModel> Export(Guid organizationId)
         {
-            if (!await _currentContext.ViewAllCollections(organizationId) && !await _currentContext.ManageUsers(organizationId))
-            {
-                throw new NotFoundException();
-            }
-
             var userId = _userService.GetProperUserId(User).Value;
 
-            IEnumerable<Collection> orgCollections;
-            IEnumerable<CipherOrganizationDetails> orgCiphers;
-
-            if (await _currentContext.OrganizationAdmin(organizationId))
-            {
-                // Admins, Owners and Providers can access all items even if not assigned to them
-                orgCollections = await _collectionRepository.GetManyByOrganizationIdAsync(organizationId);
-                orgCiphers = await _cipherRepository.GetManyOrganizationDetailsByOrganizationIdAsync(organizationId);
-            }
-            else
-            {
-                var collections = await _collectionRepository.GetManyByUserIdAsync(_currentContext.UserId.Value);
-                orgCollections = collections.Where(c => c.OrganizationId == organizationId);
-
-                var ciphers = await _cipherRepository.GetManyByUserIdAsync(userId, true);
-                orgCiphers = ciphers.Where(c => c.OrganizationId == organizationId);
-            }
+            IEnumerable<Collection> orgCollections = await _collectionService.GetOrganizationCollections(organizationId);
+            (IEnumerable<CipherOrganizationDetails> orgCiphers, Dictionary<Guid, IGrouping<Guid, CollectionCipher>> collectionCiphersGroupDict) = await _cipherService.GetOrganizationCiphers(userId, organizationId);
 
             var result = new OrganizationExportResponseModel
             {
                 Collections = GetOrganizationCollectionsResponse(orgCollections),
-                Ciphers = await GetOrganizationCiphersResponse(organizationId, orgCiphers)
+                Ciphers = await GetOrganizationCiphersResponse(orgCiphers, collectionCiphersGroupDict)
             };
 
             return result;
@@ -84,23 +52,11 @@ namespace Bit.Api.Controllers
             return new ListResponseModel<CollectionResponseModel>(collections);
         }
 
-        private async Task<ListResponseModel<CipherMiniDetailsResponseModel>> GetOrganizationCiphersResponse(Guid organizationId, IEnumerable<CipherOrganizationDetails> orgCiphers)
+        private async Task<ListResponseModel<CipherMiniDetailsResponseModel>> GetOrganizationCiphersResponse(IEnumerable<CipherOrganizationDetails> orgCiphers,
+            Dictionary<Guid, IGrouping<Guid, CollectionCipher>> collectionCiphersGroupDict)
         {
-            var orgCipherIds = orgCiphers.Select(c => c.Id);
-
-            var collectionCiphers = await _collectionCipherRepository.GetManyByOrganizationIdAsync(organizationId);
-            var collectionCiphersGroupDict = collectionCiphers
-                .Where(c => orgCipherIds.Contains(c.CipherId))
-                .GroupBy(c => c.CipherId).ToDictionary(s => s.Key);
-
             var responses = orgCiphers.Select(c => new CipherMiniDetailsResponseModel(c, _globalSettings,
                 collectionCiphersGroupDict, c.OrganizationUseTotp));
-
-            var providerId = await _currentContext.ProviderIdForOrg(organizationId);
-            if (providerId.HasValue)
-            {
-                await _providerService.LogProviderAccessToOrganizationAsync(organizationId);
-            }
 
             return new ListResponseModel<CipherMiniDetailsResponseModel>(responses);
         }
