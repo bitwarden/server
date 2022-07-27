147ec7aab16af77e44c7b6de122af336d67f6581
Rui Tome <rtome@bitwarden.com>
2022-07-27 09:24:10 +0100

    EC-247 Removed unnecessary alias from the SQL query

--- util/Migrator/DbScripts/2022-07-20_00_ProviderOrganizationListDetails.sql
+++ util/Migrator/DbScripts/2022-07-20_00_ProviderOrganizationListDetails.sql
@@ -11,7 +11,7 @@ SELECT
     PO.[CreationDate],
     PO.[RevisionDate],
     (SELECT COUNT(1) FROM [dbo].[OrganizationUser] OU WHERE OU.OrganizationId = PO.OrganizationId AND OU.Status = 2) UserCount,
-    O.Seats Seats,
+    O.[Seats],
     O.[Plan]
 FROM
     [dbo].[ProviderOrganization] PO
