64a1c588bd1c90a9c7b75fb92297c8e3c5801c97
Thomas Avery <43214426+Thomas-Avery@users.noreply.github.com>
2022-08-05 09:47:12 -0500

    [SM-67] Get all secrets by org ID (#2163)
    Add a controller to fetch secrets associated with an organization ID.
    
    To note, the [SecretsManager] attribute makes this controller only available for local development.

---
+++ src/Api/Controllers/SecretsController.cs
@@ -0,0 +1,27 @@
+﻿using Bit.Api.Models.Response;
+using Bit.Api.SecretManagerFeatures.Models.Response;
+using Bit.Api.Utilities;
+using Bit.Core.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bit.Api.Controllers
+{
+    [SecretsManager]
+    public class SecretsController : Controller
+    {
+        private readonly ISecretRepository _secretRepository;
+
+        public SecretsController(ISecretRepository secretRepository)
+        {
+            _secretRepository = secretRepository;
+        }
+
+        [HttpGet("organizations/{orgId}/secrets")]
+        public async Task<ListResponseModel<SecretResponseModel>> GetAsync([FromRoute] Guid orgId)
+        {
+            var results = await _secretRepository.GetManyByOrganizationIdAsync(orgId);
+            var responses = results.Select(secret => new SecretResponseModel(secret));
+            return new ListResponseModel<SecretResponseModel>(responses);
+        }
+    }
+}
