90dd42a0e6e63af1109c7d214f8dd9ec81863724
gbubemismith <gsmithwalter@gmail.com>
2022-08-09 23:10:23 -0600

    added get connection string

--- src/Api/Utilities/HealthCheckServices.cs
+++ src/Api/Utilities/HealthCheckServices.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics.CodeAnalysis;
+using Bit.Core.Enums;
 using Bit.Core.Settings;
 
 namespace Bit.Api.Utilities;
@@ -9,8 +10,28 @@ internal static class HealthCheckServices
     public static IServiceCollection ConfigureHealthCheckServices(this IServiceCollection services,
         GlobalSettings globalSettings, IHostEnvironment environment)
     {
+        var connectionString = GetConnectionString(globalSettings);
+
         services.AddHealthChecks();
+                
 
         return services;
+    }
+
+    private static string GetConnectionString(GlobalSettings globalSettings)
+    {
+        var selectedDatabaseProvider = globalSettings.DatabaseProvider.ToLowerInvariant();
+
+        if (string.IsNullOrEmpty(selectedDatabaseProvider))
+        {
+            throw new ArgumentNullException();
+        }
+
+        return selectedDatabaseProvider switch
+        {
+            "postgres" or "postgresql" => globalSettings.PostgreSql.ConnectionString,
+            "mysql" or "mariadb" => globalSettings.MySql.ConnectionString,
+            _ => globalSettings.SqlServer.ConnectionString
+        };
     }
 }
