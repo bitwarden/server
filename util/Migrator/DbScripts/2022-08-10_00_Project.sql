5833e6f2b295c0006f1666f66d9fd9689170d1ab
CarleyDiaz-Bitwarden <103955722+CarleyDiaz-Bitwarden@users.noreply.github.com>
2022-08-16 13:56:49 -0400

    Adding soft delete proc to migration

--- util/Migrator/DbScripts/2022-08-10_00_Project.sql
+++ util/Migrator/DbScripts/2022-08-10_00_Project.sql
@@ -132,3 +132,40 @@ BEGIN
     WHERE 
         [Id] = @Id
 END
+GO
+
+CREATE PROCEDURE [dbo].[Project_SoftDelete]
+    @Ids AS [dbo].[GuidIdArray] READONLY,
+    @OrganizationId AS UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    CREATE TABLE #Temp
+    ( 
+        [Id] UNIQUEIDENTIFIER NOT NULL
+    )
+
+    INSERT INTO #Temp
+    SELECT
+        [Id]
+    FROM
+        [dbo].[Project]  
+    WHERE
+        OrganizationId = @OrganizationId
+        AND [DeletedDate] IS NULL
+        AND [Id] IN (SELECT * FROM @Ids)
+
+    -- Delete Project
+    DECLARE @UtcNow DATETIME2(7) = GETUTCDATE();
+    UPDATE
+        [dbo].[Project]
+    SET
+        [DeletedDate] = @UtcNow,
+        [RevisionDate] = @UtcNow
+    WHERE
+        [Id] IN (SELECT [Id] FROM #Temp)
+
+    DROP TABLE #Temp
+END
+GO
