c2c4fbe6d1076c8caed2e17d8e39ba334536160f
Hinton <hinton@users.noreply.github.com>
2022-08-22 11:04:33 +0200

    Move EF integration tests to separate project

--- test/Core.Test/Helpers/Factories.cs
+++ test/Core.Test/Helpers/Factories.cs
@@ -1,36 +1,14 @@
 ﻿using Bit.Core.Settings;
-using Bit.Infrastructure.EntityFramework.Repositories;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 namespace Bit.Core.Test.Helpers.Factories
 {
-    public static class DatabaseOptionsFactory
-    {
-        public static List<DbContextOptions<DatabaseContext>> Options { get; } = new();
-
-        static DatabaseOptionsFactory()
-        {
-            var globalSettings = GlobalSettingsFactory.GlobalSettings;
-            if (!string.IsNullOrWhiteSpace(GlobalSettingsFactory.GlobalSettings.PostgreSql?.ConnectionString))
-            {
-                AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-                Options.Add(new DbContextOptionsBuilder<DatabaseContext>().UseNpgsql(globalSettings.PostgreSql.ConnectionString).Options);
-            }
-            if (!string.IsNullOrWhiteSpace(GlobalSettingsFactory.GlobalSettings.MySql?.ConnectionString))
-            {
-                var mySqlConnectionString = globalSettings.MySql.ConnectionString;
-                Options.Add(new DbContextOptionsBuilder<DatabaseContext>().UseMySql(mySqlConnectionString, ServerVersion.AutoDetect(mySqlConnectionString)).Options);
-            }
-        }
-    }
-
     public static class GlobalSettingsFactory
     {
-        public static GlobalSettings GlobalSettings { get; } = new GlobalSettings();
+        public static GlobalSettings GlobalSettings { get; } = new();
         static GlobalSettingsFactory()
         {
-            var configBuilder = new ConfigurationBuilder().AddUserSecrets<Bit.Api.Startup>();
+            var configBuilder = new ConfigurationBuilder().AddUserSecrets("bitwarden-Api");
             var Configuration = configBuilder.Build();
             ConfigurationBinder.Bind(Configuration.GetSection("GlobalSettings"), GlobalSettings);
         }
