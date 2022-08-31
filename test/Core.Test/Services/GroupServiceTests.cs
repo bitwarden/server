e0f9d99b4934f0778462e01101d26c99d12f2584
Rui Tomé <108268980+r-tome@users.noreply.github.com>
2022-08-31 16:03:13 +0100

    [EC-495] Updated GroupService.SaveAsync to check if collections are supplied and prevent deleting Group-to-Collection associations. Added unit test. (#2234)

--- test/Core.Test/Services/GroupServiceTests.cs
+++ test/Core.Test/Services/GroupServiceTests.cs
@@ -65,6 +65,20 @@ public class GroupServiceTests
     }
 
     [Theory, BitAutoData]
+    public async Task SaveAsync_NonDefaultGroupId_ReplaceGroupInRepository_NoCollections(Group group, Organization organization, SutProvider<GroupService> sutProvider)
+    {
+        organization.UseGroups = true;
+        sutProvider.GetDependency<IOrganizationRepository>().GetByIdAsync(organization.Id).Returns(organization);
+
+        await sutProvider.Sut.SaveAsync(group, null);
+
+        await sutProvider.GetDependency<IGroupRepository>().Received().ReplaceAsync(group);
+        await sutProvider.GetDependency<IEventService>().Received()
+            .LogGroupEventAsync(group, EventType.Group_Updated);
+        Assert.True(group.RevisionDate - DateTime.UtcNow < TimeSpan.FromSeconds(1));
+    }
+
+    [Theory, BitAutoData]
     public async Task SaveAsync_NonExistingOrganizationId_ThrowsBadRequest(Group group, SutProvider<GroupService> sutProvider)
     {
         var exception = await Assert.ThrowsAsync<BadRequestException>(
