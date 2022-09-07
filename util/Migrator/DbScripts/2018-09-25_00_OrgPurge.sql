8927df35a3f080e3277d996c1cea525b8c61e87a
Hinton <hinton@users.noreply.github.com>
2022-09-07 13:00:12 +0200

    Remove comment from migration

--- util/Migrator/DbScripts/2018-09-25_00_OrgPurge.sql
+++ util/Migrator/DbScripts/2018-09-25_00_OrgPurge.sql
@@ -61,7 +61,6 @@ BEGIN
 END
 GO
 
--- ref: https://blogs.msdn.microsoft.com/azuresqldbsupport/2016/07/03/how-to-maintain-azure-sql-indexes-and-statistics/
 CREATE Procedure [dbo].[AzureSQLMaintenance]
     (
         @operation nvarchar(10) = null,
