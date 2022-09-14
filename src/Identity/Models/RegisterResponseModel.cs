f3581c3832281e849c0a075beaf6aefb994b6c52
addison <addisonbeck1@gmail.com>
2022-09-14 13:43:21 -0400

    Send a captcha bypass token back from the register endpoint

---
+++ src/Identity/Models/RegisterResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using Bit.Core.Models.Api;
+
+namespace Bit.Identity.Models;
+
+public class RegisterResponseModel : ResponseModel
+{
+    public RegisterResponseModel(string captchaBypassToken)
+        : base("register")
+    {
+        CaptchaBypassToken = captchaBypassToken;
+    }
+
+    public string CaptchaBypassToken { get; set; }
+}
