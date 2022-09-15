564ac80edabbe9f9ff4d683db9046b28c29c0f1b
Addison Beck <addisonbeck1@gmail.com>
2022-09-15 10:02:37 -0400

    [SG-656] Send a captcha bypass token back from the register endpoint (#2278)
    * Send a captcha bypass token back from the register endpoint
    
    * [review] Use existing user
    
    * [review] Introduce ICaptcheProtectedResponseModel

--- test/Identity.Test/Controllers/AccountsControllerTests.cs
+++ test/Identity.Test/Controllers/AccountsControllerTests.cs
@@ -15,21 +15,23 @@ namespace Bit.Identity.Test.Controllers
 {
     public class AccountsControllerTests : IDisposable
     {
-
         private readonly AccountsController _sut;
         private readonly ILogger<AccountsController> _logger;
         private readonly IUserRepository _userRepository;
         private readonly IUserService _userService;
+        private readonly ICaptchaValidationService _captchaValidationService;
 
         public AccountsControllerTests()
         {
             _logger = Substitute.For<ILogger<AccountsController>>();
             _userRepository = Substitute.For<IUserRepository>();
             _userService = Substitute.For<IUserService>();
+            _captchaValidationService = Substitute.For<ICaptchaValidationService>();
             _sut = new AccountsController(
                 _logger,
                 _userRepository,
-                _userService
+                _userService,
+                _captchaValidationService
             );
         }
 
