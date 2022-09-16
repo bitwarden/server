b61241692abd44ff1027c39eb6b4642bd317c39b
Shane Melton <smelton@bitwarden.com>
2022-09-15 17:45:36 -0700

    [EC-16] Update migration script

--- util/Migrator/DbScripts/2022-09-15_00_ReadGroupsWithCollectionsByOrgId.sql
+++ util/Migrator/DbScripts/2022-09-15_00_ReadGroupsWithCollectionsByOrgId.sql
@@ -37,3 +37,62 @@ BEGIN
 
     EXEC [dbo].[CollectionGroup_ReadByOrganizationId] @OrganizationId
 END
+GO
+
+IF OBJECT_ID('[dbo].[Group_ReadByIds]') IS NOT NULL
+BEGIN
+    DROP PROCEDURE [dbo].[Group_ReadByIds];
+END
+GO
+
+CREATE PROCEDURE [dbo].[Group_ReadByIds]
+    @Ids AS [dbo].[GuidIdArray] READONLY
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    IF (SELECT COUNT(1) FROM @Ids) < 1
+        BEGIN
+            RETURN(-1)
+        END
+
+    SELECT
+        *
+    FROM
+        [dbo].[Group]
+    WHERE
+        [Id] IN (SELECT [Id] FROM @Ids)
+END
+GO
+
+IF OBJECT_ID('[dbo].[Group_DeleteByIdsOrganizationId]') IS NOT NULL
+BEGIN
+    DROP PROCEDURE [dbo].[Group_DeleteByIdsOrganizationId];
+END
+GO
+
+CREATE PROCEDURE [dbo].[Group_DeleteByIdsOrganizationId]
+    @Ids AS [dbo].[GuidIdArray] READONLY,
+    @OrganizationId AS UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON
+        
+    DECLARE @BatchSize INT = 100
+        
+    WHILE @BatchSize > 0
+    BEGIN
+        BEGIN TRANSACTION Group_DeleteMany_Groups
+            DELETE TOP(@BatchSize)
+            FROM
+                [dbo].[Group]
+            WHERE
+                [Id] IN (SELECT [Id] FROM @Ids)
+                AND [OrganizationId] = @OrganizationId
+                
+            SET @BatchSize = @@ROWCOUNT
+        COMMIT TRANSACTION Group_DeleteMany_Groups
+    END
+    
+    EXEC [dbo].[User_BumpAccountRevisionDateByOrganizationId] @OrganizationId
+END
