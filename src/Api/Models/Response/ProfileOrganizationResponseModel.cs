122a73c756cd3608cfa33f9dea9f23c791fd1005
Hinton <hinton@users.noreply.github.com>
2022-09-19 17:25:25 +0200

    Fix failing accounts test

--- src/Api/Models/Response/ProfileOrganizationResponseModel.cs
+++ src/Api/Models/Response/ProfileOrganizationResponseModel.cs
@@ -9,8 +9,8 @@ namespace Bit.Api.Models.Response
     public class ProfileOrganizationResponseModel : ResponseModel
     {
         public ProfileOrganizationResponseModel(string str) : base(str) { }
-
-        public ProfileOrganizationResponseModel(OrganizationUserOrganizationDetails organization) : this("profileOrganization")
+        public ProfileOrganizationResponseModel() : this("profileOrganization") { }
+        public ProfileOrganizationResponseModel(OrganizationUserOrganizationDetails organization) : this()
         {
             Id = organization.OrganizationId.ToString();
             Name = organization.Name;
