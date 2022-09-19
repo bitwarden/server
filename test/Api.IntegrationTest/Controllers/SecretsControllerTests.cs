a28c9a4c16ace19f5b2426e9f5c34879198a9fa8
Hinton <hinton@users.noreply.github.com>
2022-09-19 14:42:32 +0200

    Cleanup organization

--- test/Api.IntegrationTest/Controllers/SecretsControllerTests.cs
+++ test/Api.IntegrationTest/Controllers/SecretsControllerTests.cs
@@ -2,7 +2,7 @@
 using System.Text;
 using System.Text.Json;
 using Bit.Api.IntegrationTest.Factories;
-using Bit.Api.Models.Request.Organizations;
+using Bit.Api.IntegrationTest.Helpers;
 using Bit.Core.Entities;
 using Bit.Core.Repositories;
 using Xunit;
@@ -11,11 +11,13 @@ namespace Bit.Api.IntegrationTest.Controllers;
 
 public class SecretsControllerTest : IClassFixture<ApiApplicationFactory>
 {
-    private readonly string _mockEncryptedString = "2.3Uk+WNBIoU5xzmVFNcoWzz==|1MsPIYuRfdOHfu/0uY6H2Q==|/98sp4wb6pHP1VTZ9JcNCYgQjEUMFPlqJgCwRk1YXKg=";
+    private readonly string _mockEncryptedString =
+        "2.3Uk+WNBIoU5xzmVFNcoWzz==|1MsPIYuRfdOHfu/0uY6H2Q==|/98sp4wb6pHP1VTZ9JcNCYgQjEUMFPlqJgCwRk1YXKg=";
+
     private readonly int _secretsToDelete = 3;
     private readonly HttpClient _client;
     private readonly ApiApplicationFactory _factory;
-    private ISecretRepository _secretRepository;
+    private readonly ISecretRepository _secretRepository;
 
     public SecretsControllerTest(ApiApplicationFactory factory)
     {
@@ -28,30 +30,23 @@ public class SecretsControllerTest : IClassFixture<ApiApplicationFactory>
     public async Task DeleteSecrets()
     {
         var tokens = await _factory.LoginWithNewAccount();
+        var (organization, _) = await OrganizationTestHelpers.SignUpAsync(_factory);
 
-        var orgId = await CreateOrganization(tokens.Token);
-        var createdSecretIds = new List<Guid>();
-
+        var secretIds = new List<Guid>();
         for (var i = 0; i < _secretsToDelete; i++)
         {
             var secret = await _secretRepository.CreateAsync(new Secret
             {
-                OrganizationId = orgId,
+                OrganizationId = organization.Id,
                 Key = _mockEncryptedString,
                 Value = _mockEncryptedString,
                 Note = _mockEncryptedString
             });
-            createdSecretIds.Add(secret.Id);
+            secretIds.Add(secret.Id);
         }
 
-        using var message = new HttpRequestMessage(HttpMethod.Post, "/secrets/delete")
-        {
-            Content = new StringContent(JsonSerializer.Serialize(createdSecretIds),
-            Encoding.UTF8,
-            "application/json"),
-        };
-        message.Headers.Authorization = new AuthenticationHeaderValue("Bearer", tokens.Token);
-        var response = await _client.SendAsync(message);
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokens.Token);
+        var response = await _client.PostAsync("/secrets/delete", JsonContent.Create(secretIds));
         response.EnsureSuccessStatusCode();
 
         var content = await response.Content.ReadAsStringAsync();
@@ -61,35 +56,12 @@ public class SecretsControllerTest : IClassFixture<ApiApplicationFactory>
         var index = 0;
         foreach (var element in jsonResult.RootElement.GetProperty("data").EnumerateArray())
         {
-            Assert.Equal(createdSecretIds[index].ToString(), element.GetProperty("id").ToString());
+            Assert.Equal(secretIds[index].ToString(), element.GetProperty("id").ToString());
             Assert.Empty(element.GetProperty("error").ToString());
             index++;
         }
 
-        var secrets = await _secretRepository.GetManyByIds(createdSecretIds);
+        var secrets = await _secretRepository.GetManyByIds(secretIds);
         Assert.Empty(secrets);
-    }
-
-    private async Task<Guid> CreateOrganization(string token)
-    {
-        var request = new OrganizationCreateRequestModel()
-        {
-            Name = "Integration Test Org",
-            BillingEmail = "integration-test@bitwarden.com",
-            PlanType = Core.Enums.PlanType.Free,
-            Key = "test-key"
-        };
-        using var message = new HttpRequestMessage(HttpMethod.Post, $"/organizations")
-        {
-            Content = new StringContent(
-            JsonSerializer.Serialize(request),
-            Encoding.UTF8,
-            "application/json")
-        };
-        message.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
-        var response = await _client.SendAsync(message);
-        response.EnsureSuccessStatusCode();
-        var result = await response.Content.ReadAsStringAsync();
-        return new Guid(JsonDocument.Parse(result).RootElement.GetProperty("id").ToString());
     }
 }
