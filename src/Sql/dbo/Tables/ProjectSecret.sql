3272ef90edc9423614805f8bbe83f4e9e15ee59e
Hinton <hinton@users.noreply.github.com>
2022-09-19 15:26:25 +0200

    Indent sql

--- src/Sql/dbo/Tables/ProjectSecret.sql
+++ src/Sql/dbo/Tables/ProjectSecret.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [ProjectSecret] (
     [ProjectsId] UNIQUEIDENTIFIER NOT NULL,
-    [SecretsId] UNIQUEIDENTIFIER NOT NULL,
+    [SecretsId]  UNIQUEIDENTIFIER NOT NULL,
     CONSTRAINT [PK_ProjectSecret] PRIMARY KEY ([ProjectsId], [SecretsId]),
     CONSTRAINT [FK_ProjectSecret_Project_ProjectsId] FOREIGN KEY ([ProjectsId]) REFERENCES [Project] ([Id]) ON DELETE CASCADE,
     CONSTRAINT [FK_ProjectSecret_Secret_SecretsId] FOREIGN KEY ([SecretsId]) REFERENCES [Secret] ([Id]) ON DELETE CASCADE
