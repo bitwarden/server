6034c5c7b50de79a643c6e65915ab3f8b9d33ad6
CarleyDiaz-Bitwarden <103955722+CarleyDiaz-Bitwarden@users.noreply.github.com>
2022-09-21 15:13:29 -0400

    fixing merge conflicts

--- bitwarden_license/src/Commercial.Core/SecretManagerFeatures/SecretManagerCollectionExtensions.cs
+++ bitwarden_license/src/Commercial.Core/SecretManagerFeatures/SecretManagerCollectionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Bit.Commercial.Core.SecretManagerFeatures.Projects;
 using Bit.Commercial.Core.SecretManagerFeatures.Secrets;
+using Bit.Core.SecretManagerFeatures.Projects.Interfaces;
 using Bit.Core.SecretManagerFeatures.Secrets.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,6 +13,8 @@ namespace Bit.Commercial.Core.SecretManagerFeatures
             services.AddScoped<ICreateSecretCommand, CreateSecretCommand>();
             services.AddScoped<IUpdateSecretCommand, UpdateSecretCommand>();
             services.AddScoped<IDeleteSecretCommand, DeleteSecretCommand>();
+            services.AddScoped<ICreateProjectCommand, CreateProjectCommand>();
+            services.AddScoped<IUpdateProjectCommand, UpdateProjectCommand>();
         }
     }
 }
