ade14dc3ebd1b3ca24eca7dd33be62123aebefd9
Thomas Avery <tavery@bitwarden.com>
2022-09-27 16:36:13 -0500

    SM-207 list service accounts by org

---
+++ src/Api/Controllers/ServiceAccountsController.cs
@@ -0,0 +1,27 @@
+﻿using Bit.Api.Models.Response;
+using Bit.Api.SecretManagerFeatures.Models.Response;
+using Bit.Api.Utilities;
+using Bit.Core.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bit.Api.Controllers
+{
+    [SecretsManager]
+    public class ServiceAccountsController : Controller
+    {
+        private readonly IServiceAccountRepository _serviceAccountRepository;
+
+        public ServiceAccountsController(IServiceAccountRepository serviceAccountRepository)
+        {
+            _serviceAccountRepository = serviceAccountRepository;
+        }
+
+        [HttpGet("organizations/{organizationId}/service-accounts")]
+        public async Task<ListResponseModel<ServiceAccountResponseModel>> GetServiceAccountsByOrganizationAsync([FromRoute] Guid organizationId)
+        {
+            var serviceAccounts = await _serviceAccountRepository.GetManyByOrganizationIdAsync(organizationId);
+            var responses = serviceAccounts.Select(serviceAccount => new ServiceAccountResponseModel(serviceAccount));
+            return new ListResponseModel<ServiceAccountResponseModel>(responses);
+        }
+    }
+}
