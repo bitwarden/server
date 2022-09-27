f465a4fbc46b5b543fa869d8fcf4c408ab740786
Hinton <hinton@users.noreply.github.com>
2022-09-27 23:25:58 +0200

    Update migration

--- util/Migrator/DbScripts/2022-09-27_00_ApiKey.sql
+++ util/Migrator/DbScripts/2022-09-27_00_ApiKey.sql
@@ -1,20 +1,3 @@
-IF OBJECT_ID('[dbo].[ServiceAccount]') IS NULL
-BEGIN
-
-    CREATE TABLE [dbo].[ServiceAccount]
-    (
-        [Id]             UNIQUEIDENTIFIER NOT NULL,
-        [OrganizationId] UNIQUEIDENTIFIER NOT NULL,
-        [Name]           NVARCHAR(MAX) NULL,
-        [CreationDate]   DATETIME2(7) NOT NULL,
-        [RevisionDate]   DATETIME2(7) NOT NULL,
-        [DeletedDate]    DATETIME2(7) NULL,
-        CONSTRAINT [PK_ServiceAccount] PRIMARY KEY CLUSTERED ([Id] ASC),
-        CONSTRAINT [FK_ServiceAccount_OrganizationId] FOREIGN KEY ([OrganizationId]) REFERENCES [dbo].[Organization]([Id]) ON DELETE CASCADE
-    );
-END
-GO
-
 IF OBJECT_ID('[dbo].[ApiKey]') IS NULL
 BEGIN
     CREATE TABLE [dbo].[ApiKey] (
