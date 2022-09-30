a117de39d041bb4e66bf30cc5b31f96b812dbbfb
Rui Tome <rtome@bitwarden.com>
2022-09-30 11:39:59 +0100

    [EC-508] Created ScimServiceCollectionExtensions

---
+++ bitwarden_license/src/Scim/Utilities/ScimServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Bit.Scim.Commands.Users;
+using Bit.Scim.Commands.Users.Interfaces;
+
+namespace Bit.Scim.Utilities;
+
+public static class ScimServiceCollectionExtensions
+{
+    public static void AddScimCommands(this IServiceCollection services)
+    {
+        services.AddScoped<IPutUserCommand, PutUserCommand>();
+    }
+}
