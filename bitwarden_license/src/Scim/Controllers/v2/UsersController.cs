24b5684b9823b7e6afc8875c503766cc8ffe8273
Rui Tome <rtome@bitwarden.com>
2022-10-04 10:47:43 +0100

    [EC-508] Removed unneeded dependencies from UsersController

--- bitwarden_license/src/Scim/Controllers/v2/UsersController.cs
+++ bitwarden_license/src/Scim/Controllers/v2/UsersController.cs
@@ -10,7 +10,6 @@ using Bit.Scim.Queries.Users.Interfaces;
 using Bit.Scim.Utilities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
 
 namespace Bit.Scim.Controllers.v2;
 
@@ -20,32 +19,26 @@ namespace Bit.Scim.Controllers.v2;
 public class UsersController : Controller
 {
     private readonly IUserService _userService;
-    private readonly IUserRepository _userRepository;
     private readonly IOrganizationUserRepository _organizationUserRepository;
     private readonly IOrganizationService _organizationService;
     private readonly IScimContext _scimContext;
-    private readonly ScimSettings _scimSettings;
     private readonly IGetUserQuery _getUserQuery;
     private readonly IPutUserCommand _putUserCommand;
     private readonly ILogger<UsersController> _logger;
 
     public UsersController(
         IUserService userService,
-        IUserRepository userRepository,
         IOrganizationUserRepository organizationUserRepository,
         IOrganizationService organizationService,
         IScimContext scimContext,
-        IOptions<ScimSettings> scimSettings,
         IGetUserQuery getUserQuery,
         IPutUserCommand putUserCommand,
         ILogger<UsersController> logger)
     {
         _userService = userService;
-        _userRepository = userRepository;
         _organizationUserRepository = organizationUserRepository;
         _organizationService = organizationService;
         _scimContext = scimContext;
-        _scimSettings = scimSettings?.Value;
         _getUserQuery = getUserQuery;
         _putUserCommand = putUserCommand;
         _logger = logger;
--- bitwarden_license/src/Scim/Utilities/ScimServiceCollectionExtensions.cs
+++ bitwarden_license/src/Scim/Utilities/ScimServiceCollectionExtensions.cs
@@ -1,7 +1,7 @@
-﻿using Bit.Scim.Queries.Users;
-using Bit.Scim.Queries.Users.Interfaces;
 ﻿using Bit.Scim.Commands.Users;
 using Bit.Scim.Commands.Users.Interfaces;
+using Bit.Scim.Queries.Users;
+using Bit.Scim.Queries.Users.Interfaces;
 
 namespace Bit.Scim.Utilities;
 
@@ -10,7 +10,7 @@ public static class ScimServiceCollectionExtensions
     public static void AddScimUserQueries(this IServiceCollection services)
     {
         services.AddScoped<IGetUserQuery, GetUserQuery>();
-	}
+    }
 
     public static void AddScimUserCommands(this IServiceCollection services)
     {
