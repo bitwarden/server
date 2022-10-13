0f1444d77437626bac0240eda621e025e5abe268
Hinton <hinton@users.noreply.github.com>
2022-10-13 12:44:40 +0200

    Potential refactor for response

--- src/Api/SecretManagerFeatures/Models/Response/SecretIdentifierResponseModel.cs
+++ src/Api/SecretManagerFeatures/Models/Response/SecretIdentifierResponseModel.cs
@@ -3,34 +3,53 @@ using Bit.Core.Models.Api;
 
 namespace Bit.Api.SecretManagerFeatures.Models.Response;
 
-public class SecretIdentifierResponseModel : ResponseModel
+public class SecretWithProjectsListResponseModel : ResponseModel
 {
-    public SecretIdentifierResponseModel(Secret secret, string obj = "secret")
-        : base(obj)
+    public SecretWithProjectsListResponseModel(IEnumerable<Secret> secrets, string obj = "SecretsWithProjectsList") : base(obj)
     {
-        if (secret == null)
-        {
-            throw new ArgumentNullException(nameof(secret));
-        }
-
-        Id = secret.Id.ToString();
-        OrganizationId = secret.OrganizationId.ToString();
-        Key = secret.Key;
-        CreationDate = secret.CreationDate;
-        RevisionDate = secret.RevisionDate;
-        Projects = secret.Projects.ToDictionary(p => p.Id, p => p.Name);
+        Secrets = secrets.Select(s => new InnerSecret(s));
+        Projects = secrets.SelectMany(s => s.Projects).DistinctBy(p => p.Id).Select(p => new InnerProject(p));
     }
 
-    public string Id { get; set; }
+    public IEnumerable<InnerSecret> Secrets { get; set; }
+    public IEnumerable<InnerProject> Projects { get; set; }
 
-    public string OrganizationId { get; set; }
+    public class InnerProject
+    {
+        public InnerProject(Project project)
+        {
+            Id = project.Id;
+            Name = project.Name;
+        }
 
-    public string Key { get; set; }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
 
-    public DateTime CreationDate { get; set; }
+    public class InnerSecret
+    {
+        public InnerSecret(Secret secret)
+        {
+            Id = secret.Id.ToString();
+            OrganizationId = secret.OrganizationId.ToString();
+            Key = secret.Key;
+            CreationDate = secret.CreationDate;
+            RevisionDate = secret.RevisionDate;
+            Projects = secret.Projects.Select(p => p.Id);
+        }
 
-    public DateTime RevisionDate { get; set; }
+        public string Id { get; set; }
 
-    public Dictionary<System.Guid, string> Projects { get; set; }
+        public string OrganizationId { get; set; }
+
+        public string Key { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime RevisionDate { get; set; }
+
+        public IEnumerable<Guid> Projects { get; set; }
+    }
 }
+
 
