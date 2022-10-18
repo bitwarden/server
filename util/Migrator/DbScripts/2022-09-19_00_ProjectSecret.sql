8fcd1f0cf45ea3e2009f89027abb330ead73d70a
cd-bitwarden <106776772+cd-bitwarden@users.noreply.github.com>
2022-10-18 15:38:05 -0400

    Update util/Migrator/DbScripts/2022-09-19_00_ProjectSecret.sql
    Co-authored-by: Oscar Hinton <Hinton@users.noreply.github.com>

--- util/Migrator/DbScripts/2022-09-19_00_ProjectSecret.sql
+++ util/Migrator/DbScripts/2022-09-19_00_ProjectSecret.sql
@@ -112,7 +112,7 @@ IF OBJECT_ID('[dbo].[ProjectSecret]') IS NULL
 BEGIN
 CREATE TABLE [ProjectSecret] (
     [ProjectsId] UNIQUEIDENTIFIER NOT NULL,
-    [SecretsId] UNIQUEIDENTIFIER NOT NULL,
+    [SecretsId]  UNIQUEIDENTIFIER NOT NULL,
     CONSTRAINT [PK_ProjectSecret] PRIMARY KEY ([ProjectsId], [SecretsId]),
     CONSTRAINT [FK_ProjectSecret_Project_ProjectsId] FOREIGN KEY ([ProjectsId]) REFERENCES [Project] ([Id]) ON DELETE CASCADE,
     CONSTRAINT [FK_ProjectSecret_Secret_SecretsId] FOREIGN KEY ([SecretsId]) REFERENCES [Secret] ([Id]) ON DELETE CASCADE
