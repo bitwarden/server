e33376079be1591b3f899de9af3bb90194fc35bb
CarleyDiaz-Bitwarden <cdiaz@bitwarden.com>
2022-10-18 11:27:01 -0400

    migrations

--- src/Sql/dbo/Tables/ProjectSecret.sql
+++ src/Sql/dbo/Tables/ProjectSecret.sql
@@ -5,7 +5,6 @@
     CONSTRAINT [FK_ProjectSecret_Project_ProjectsId] FOREIGN KEY ([ProjectsId]) REFERENCES [Project] ([Id]) ON DELETE CASCADE,
     CONSTRAINT [FK_ProjectSecret_Secret_SecretsId] FOREIGN KEY ([SecretsId]) REFERENCES [Secret] ([Id]) ON DELETE CASCADE
 );
-GO
 
-CREATE INDEX [IX_ProjectSecret_SecretsId] ON [ProjectSecret] ([SecretsId]);
 GO
+CREATE INDEX [IX_ProjectSecret_SecretsId] ON [ProjectSecret] ([SecretsId]);
