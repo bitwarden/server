4a26c555995da12a4dc92a61a409aac48003e717
Gbubemi Smith <gsmith@bitwarden.com>
2022-10-18 14:50:48 -0400

    [SG-698] Refactored 2fa send email and identity to cater for passwordless (#2346)
    * Allow for auth request validation for sending two factor emails
    
    * Refactored 2fa send email and identity to cater for passwordless
    
    * Refactored 2fa send email and identity to cater for passwordless
    
    Signed-off-by: gbubemismith <gsmithwalter@gmail.com>
    
    * Inform that we track issues outside of Github (#2331)
    
    * Inform that we track issues outside of Github
    
    * Use checkboxes for info acknowledgement
    
    Signed-off-by: gbubemismith <gsmithwalter@gmail.com>
    
    * Refactored 2fa send email and identity to cater for passwordless
    
    * ran dotnet format
    
    Signed-off-by: gbubemismith <gsmithwalter@gmail.com>
    Co-authored-by: addison <addisonbeck1@gmail.com>

---
+++ src/Core/LoginFeatures/PasswordlessLogin/Interfaces/IVerifyAuthRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Bit.Core.LoginFeatures.PasswordlessLogin.Interfaces;
+
+public interface IVerifyAuthRequestCommand
+{
+    Task<bool> VerifyAuthRequestAsync(Guid authRequestId, string accessCode);
+}
