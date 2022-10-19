7722550b0ae4363869b7a20524fbf3da90b932b8
CarleyDiaz-Bitwarden <cdiaz@bitwarden.com>
2022-10-19 14:43:40 -0400

    adding back projects

--- src/Core/Entities/Secret.cs
+++ src/Core/Entities/Secret.cs
@@ -21,6 +21,8 @@ public class Secret : ITableObject<Guid>
 
     public DateTime? DeletedDate { get; set; }
 
+    public ICollection<Project>? Projects { get; set; }
+
     public void SetNewId()
     {
         if (Id == default(Guid))
