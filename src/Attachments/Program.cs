60002b744f7cfffcf0e965c86aa546fad0656831
Vince Grassia <593223+vgrassia@users.noreply.github.com>
2022-10-20 11:21:20 -0400

    Add Attachments project

---
+++ src/Attachments/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Bit.Attachments;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .AddCommandLine(args)
+            .Build();
+
+        var builder = new WebHostBuilder()
+            .UseConfiguration(config)
+            .UseKestrel()
+            .UseStartup<Startup>()
+            .ConfigureLogging((hostingContext, logging) =>
+            {
+                logging.AddConsole().AddDebug();
+            })
+            .ConfigureKestrel((context, options) => { });
+
+        var contentRoot = config.GetValue<string>("contentRoot");
+        if (!string.IsNullOrWhiteSpace(contentRoot))
+        {
+            builder.UseContentRoot(contentRoot);
+        }
+        else
+        {
+            builder.UseContentRoot(Directory.GetCurrentDirectory());
+        }
+
+        var webRoot = config.GetValue<string>("webRoot");
+        if (string.IsNullOrWhiteSpace(webRoot))
+        {
+            builder.UseWebRoot(webRoot);
+        }
+
+        var host = builder.Build();
+        host.Run();
+    }
+}
