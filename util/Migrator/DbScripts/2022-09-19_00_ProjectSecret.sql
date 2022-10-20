e2649e664d32b6f033762403549aa98bdd300985
cd-bitwarden <106776772+cd-bitwarden@users.noreply.github.com>
2022-10-20 11:26:30 -0400

    Update util/Migrator/DbScripts/2022-09-19_00_ProjectSecret.sql
    Co-authored-by: Oscar Hinton <Hinton@users.noreply.github.com>

--- util/Migrator/DbScripts/2022-09-19_00_ProjectSecret.sql
+++ util/Migrator/DbScripts/2022-09-19_00_ProjectSecret.sql
@@ -110,15 +110,15 @@ ALTER TABLE [Secret] ADD CONSTRAINT [FK_Secret_OrganizationId] FOREIGN KEY ([Org
 
 IF OBJECT_ID('[dbo].[ProjectSecret]') IS NULL
 BEGIN
-CREATE TABLE [ProjectSecret] (
-    [ProjectsId] UNIQUEIDENTIFIER NOT NULL,
-    [SecretsId]  UNIQUEIDENTIFIER NOT NULL,
-    CONSTRAINT [PK_ProjectSecret] PRIMARY KEY ([ProjectsId], [SecretsId]),
-    CONSTRAINT [FK_ProjectSecret_Project_ProjectsId] FOREIGN KEY ([ProjectsId]) REFERENCES [Project] ([Id]) ON DELETE CASCADE,
-    CONSTRAINT [FK_ProjectSecret_Secret_SecretsId] FOREIGN KEY ([SecretsId]) REFERENCES [Secret] ([Id]) ON DELETE CASCADE
-);
+  CREATE TABLE [ProjectSecret] (
+      [ProjectsId] UNIQUEIDENTIFIER NOT NULL,
+      [SecretsId]  UNIQUEIDENTIFIER NOT NULL,
+       CONSTRAINT [PK_ProjectSecret] PRIMARY KEY ([ProjectsId], [SecretsId]),
+      CONSTRAINT [FK_ProjectSecret_Project_ProjectsId] FOREIGN KEY ([ProjectsId]) REFERENCES [Project] ([Id]) ON DELETE CASCADE,
+      CONSTRAINT [FK_ProjectSecret_Secret_SecretsId] FOREIGN KEY ([SecretsId]) REFERENCES [Secret] ([Id]) ON DELETE CASCADE
+  );
 
-CREATE INDEX [IX_ProjectSecret_SecretsId] ON [ProjectSecret] ([SecretsId]);
+  CREATE INDEX [IX_ProjectSecret_SecretsId] ON [ProjectSecret] ([SecretsId]);
 
 END
 
