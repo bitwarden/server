2d68bd281ea875b69f831ae327395d0b0b5d351b
addison <addisonbeck1@gmail.com>
2022-10-20 13:28:38 -0400

    Add ClientSecrets to ApiClient class

--- src/Core/IdentityServer/ApiClient.cs
+++ src/Core/IdentityServer/ApiClient.cs
@@ -11,7 +11,8 @@ namespace Bit.Core.IdentityServer
             string id,
             int refreshTokenSlidingDays,
             int accessTokenLifetimeHours,
-            string[] scopes = null)
+            string[] scopes = null,
+            ICollection<Secret> clientSecrets = null)
         {
             ClientId = id;
             AllowedGrantTypes = new[] { GrantType.ResourceOwnerPassword, GrantType.AuthorizationCode };
@@ -62,6 +63,11 @@ namespace Bit.Core.IdentityServer
                 }
                 RedirectUris = cliUris;
                 PostLogoutRedirectUris = cliUris;
+                
+                if (clientSecrets != null) 
+                {
+                  ClientSecrets = clientSecrets;
+                }
             }
             else if (id == "mobile")
             {
