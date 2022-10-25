351f62866b1e9094dc235e1ddf279371e3e6025c
Gbubemi Smith <gsmith@bitwarden.com>
2022-10-25 17:14:48 -0400

    [SG-763] Store the fact that a Passwordless request was denied in the AuthRequest table (#2363)
    * Added migrations for sqlserver and mysql
    
    * Added migrations for postgres
    
    * renamed mysql migration script to make naming uniform
    
    * introduced approved field to the update auth request controller;This change would keep track of denied passwordless requests
    
    * Recreated the authRequestView, introduced the approved field to the create procedure and updated the response model
    
    * Formatted code
    
    * fixed incorrect syntax in the AuthRequest_Create.sql SP

---
+++ util/MySqlMigrations/Migrations/20221024210500_PasswordlessAuthRequestAddApprovedColumn.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.MySqlMigrations.Migrations;
+
+public partial class PasswordlessAuthRequestAddApprovedColumn : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<bool>(
+            name: "Approved",
+            table: "AuthRequest",
+            type: "tinyint(1)",
+            nullable: true);
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "Approved",
+            table: "AuthRequest");
+    }
+}
