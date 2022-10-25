69353d51e16e6149bf16c7ceeec1b03c28e73792
Hinton <hinton@users.noreply.github.com>
2022-10-25 15:44:03 +0200

    Fix migration

--- util/Migrator/DbScripts/2022-10-25_00_AccessPolicy.sql
+++ util/Migrator/DbScripts/2022-10-25_00_AccessPolicy.sql
@@ -1,10 +1,11 @@
 ﻿-- Remove ON DELETE for service accounts
 IF EXISTS (SELECT name FROM sys.foreign_keys WHERE name = 'FK_ServiceAccount_OrganizationId')
 BEGIN
-    ALTER TABLE [ServiceAccount] DROP CONSTRAINT FK_ServiceAccount_OrganizationId;
+    ALTER TABLE [ServiceAccount] DROP CONSTRAINT [FK_ServiceAccount_OrganizationId];
 END
 
 ALTER TABLE [ServiceAccount] ADD CONSTRAINT [FK_ServiceAccount_OrganizationId] FOREIGN KEY ([OrganizationId]) REFERENCES [dbo].[Organization] ([Id]);
+GO
 
 IF OBJECT_ID('[dbo].[AccessPolicy]') IS NULL
 BEGIN
