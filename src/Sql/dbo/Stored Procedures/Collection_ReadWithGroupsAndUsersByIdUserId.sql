a6d4bd9d4d842e21ec7dfeee59d2d664e20eeb4f
Andreas Coroiu <andreas.coroiu@gmail.com>
2022-10-25 10:38:34 +0200

    [EC-73] feat: add new stored procedures

---
+++ src/Sql/dbo/Stored Procedures/Collection_ReadWithGroupsAndUsersByIdUserId.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[Collection_ReadWithGroupsAndUsersByIdUserId]
+    @Id UNIQUEIDENTIFIER,
+    @UserId UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    EXEC [dbo].[Collection_ReadByIdUserId] @Id, @UserId
+
+    EXEC [dbo].[CollectionGroup_ReadByCollectionId] @Id
+
+    EXEC [dbo].[CollectionUser_ReadByCollectionId] @Id
+END
