4b3d3a20a7a5ec98af06da2b7ec6badeac6b1c94
Hinton <hinton@users.noreply.github.com>
2022-11-03 12:53:32 +0100

    Implement EF

--- src/Infrastructure.EntityFramework/Repositories/ApiKeyRepository.cs
+++ src/Infrastructure.EntityFramework/Repositories/ApiKeyRepository.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Bit.Core.Repositories;
 using Bit.Infrastructure.EntityFramework.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Bit.Infrastructure.EntityFramework.Repositories;
@@ -12,5 +13,12 @@ public class ApiKeyRepository : Repository<Core.Entities.ApiKey, ApiKey, Guid>, 
     {
     }
 
-    public Task<ICollection<Core.Entities.ApiKey>> GetManyByServiceAccountIdAsync(Guid id) => throw new NotImplementedException();
+    public async Task<ICollection<Core.Entities.ApiKey>> GetManyByServiceAccountIdAsync(Guid id)
+    {
+        using var scope = ServiceScopeFactory.CreateScope();
+        var dbContext = GetDatabaseContext(scope);
+        var apiKeys = await GetDbSet(dbContext).Where(e => e.ServiceAccountId == id).ToListAsync();
+
+        return Mapper.Map<List<Core.Entities.ApiKey>>(apiKeys);
+    }
 }
