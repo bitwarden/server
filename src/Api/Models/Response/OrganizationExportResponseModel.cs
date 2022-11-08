4f95b98ea88ae3c6ef39559ab9c987c741dd8f2c
Rui Tome <rtome@bitwarden.com>
2022-11-08 09:33:04 +0000

    Revert "[EC-584] Removed ListResponseModel from OrganizationExportResponseModel (#2316)"
    This reverts commit 656eadd17d8d197bfe6309ea8b45f8a5ae24167f.

--- src/Api/Models/Response/OrganizationExportResponseModel.cs
+++ src/Api/Models/Response/OrganizationExportResponseModel.cs
@@ -1,21 +1,11 @@
-﻿using Bit.Core.Models.Api;
+﻿namespace Bit.Api.Models.Response;
 
-namespace Bit.Api.Models.Response;
-
-public class OrganizationExportResponseModel : ResponseModel
+public class OrganizationExportResponseModel
 {
-    public OrganizationExportResponseModel() : base("organizationExport")
+    public OrganizationExportResponseModel()
     {
     }
 
-    public IEnumerable<CollectionResponseModel> Collections { get; set; }
-
-    public IEnumerable<CipherMiniDetailsResponseModel> Ciphers { get; set; }
-}
-
-[Obsolete("This version is for backwards compatibility for client version 2022.9.0")]
-public class OrganizationExportListResponseModel
-{
     public ListResponseModel<CollectionResponseModel> Collections { get; set; }
 
     public ListResponseModel<CipherMiniDetailsResponseModel> Ciphers { get; set; }
