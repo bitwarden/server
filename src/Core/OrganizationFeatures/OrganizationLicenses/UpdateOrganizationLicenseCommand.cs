b8e1453f5b708ea8031e373a6634b29889a74763
Thomas Rittson <trittson@bitwarden.com>
2022-11-10 14:38:33 +1000

    WIP: pass in objects, simplify conditional logic

--- src/Core/OrganizationFeatures/OrganizationLicenses/UpdateOrganizationLicenseCommand.cs
+++ src/Core/OrganizationFeatures/OrganizationLicenses/UpdateOrganizationLicenseCommand.cs
@@ -4,50 +4,31 @@ using Bit.Core.Enums;
 using Bit.Core.Exceptions;
 using Bit.Core.Models.Business;
 using Bit.Core.Models.OrganizationConnectionConfigs;
-using Bit.Core.Utilities;
 using Bit.Core.OrganizationFeatures.OrganizationLicenses.Interfaces;
-using Bit.Core.Repositories;
+using Bit.Core.Models.Data.Organizations.OrganizationUsers;
 using Bit.Core.Services;
 using Bit.Core.Settings;
+using Bit.Core.Utilities;
 
 namespace Bit.Core.OrganizationFeatures.OrganizationLicenses;
 
-public class UpdateOrganizationLicenseCommand: IUpdateOrganizationLicenseCommand
+public class UpdateOrganizationLicenseCommand : IUpdateOrganizationLicenseCommand
 {
-    private readonly IOrganizationRepository _organizationRepository;
-    private readonly ICollectionRepository _collectionRepository;
-    private readonly IGroupRepository _groupRepository;
     private readonly ILicensingService _licensingService;
-    private readonly IPolicyRepository _policyRepository;
-    private readonly ISsoConfigRepository _ssoConfigRepository;
     private readonly IGlobalSettings _globalSettings;
-    private readonly IOrganizationConnectionRepository _organizationConnectionRepository;
-    private readonly IOrganizationUserRepository _organizationUserRepository;
     private readonly IOrganizationService _organizationService;
 
     public UpdateOrganizationLicenseCommand(
-        IOrganizationRepository organizationRepository,
-        ICollectionRepository collectionRepository,
-        IGroupRepository groupRepository,
         ILicensingService licensingService,
-        ISsoConfigRepository ssoConfigRepository,
         IGlobalSettings globalSettings,
-        IOrganizationConnectionRepository organizationConnectionRepository,
-        IOrganizationUserRepository organizationUserRepository,
         IOrganizationService organizationService)
     {
-        _organizationRepository = organizationRepository;
-        _collectionRepository = collectionRepository;
-        _groupRepository = groupRepository;
         _licensingService = licensingService;
-        _ssoConfigRepository = ssoConfigRepository;
         _globalSettings = globalSettings;
-        _organizationConnectionRepository = organizationConnectionRepository;
-        _organizationUserRepository = organizationUserRepository;
         _organizationService = organizationService;
     }
-    
-    public async Task UpdateLicenseAsync(Organization organization, OrganizationLicense license)
+
+    public async Task UpdateLicenseAsync(Organization organization, OrganizationLicense license, SsoConfig ssoConfig)
     {
         license.CanUse(_globalSettings, _licensingService);
         await ValidateLicenseForOrganizationAsync(organization, license);
@@ -55,98 +36,71 @@ public class UpdateOrganizationLicenseCommand: IUpdateOrganizationLicenseCommand
         await UpdateOrganizationAsync(organization, license);
     }
 
-    private async Task ValidateLicenseForOrganizationAsync(Organization organization, OrganizationLicense license)
+    private async Task ValidateLicenseForOrganizationAsync(Organization organization, OrganizationLicense license, 
+        SsoConfig ssoConfig, IEnumerable<Policy> policies, IEnumerable<Collection> collections, IEnumerable<Group> groups,
+        IEnumerable<OrganizationConnection> scimConnections, IEnumerable<OrganizationUserUserDetails> orgUsers,
+        IEnumerable<Organization> enabledOrganizations)
     {
-        var enabledOrganizations = await _organizationRepository.GetManyByEnabledAsync();
+        // TODO: Consider getting by licenseKey in database query, rather than passing in all orgs
         if (enabledOrganizations.Any(o => o.LicenseKey.Equals(license.LicenseKey) && o.Id != organization.Id))
         {
             throw new BadRequestException("License is already in use by another organization.");
         }
 
-        if (license.Seats.HasValue &&
-            (!organization.Seats.HasValue || organization.Seats.Value > license.Seats.Value))
+        var occupiedSeats = orgUsers.Count(ou => ou.OccupiesOrganizationSeat);
+        if (license.Seats.HasValue && occupiedSeats > license.Seats.Value)
         {
-            var orgUsers = await _organizationUserRepository.GetManyDetailsByOrganizationAsync(organization.Id);
-            var occupiedSeats  = orgUsers.Count(ou => ou.OccupiesOrganizationSeat);
-            if (occupiedSeats > license.Seats.Value)
-            {
-                throw new BadRequestException($"Your organization currently has {occupiedSeats} seats filled. " +
-                    $"Your new license only has ({license.Seats.Value}) seats. Remove some users.");
-            }
+            throw new BadRequestException($"Your organization currently has {occupiedSeats} seats filled. " +
+                $"Your new license only has ({license.Seats.Value}) seats. Remove some users.");
         }
 
-        if (license.MaxCollections.HasValue && (!organization.MaxCollections.HasValue ||
-            organization.MaxCollections.Value > license.MaxCollections.Value))
+        var collectionCount = collections.Count();
+        if (license.MaxCollections.HasValue && collectionCount > license.MaxCollections.Value)
         {
-            var collectionCount = await _collectionRepository.GetCountByOrganizationIdAsync(organization.Id);
-            if (collectionCount > license.MaxCollections.Value)
-            {
-                throw new BadRequestException($"Your organization currently has {collectionCount} collections. " +
-                    $"Your new license allows for a maximum of ({license.MaxCollections.Value}) collections. " +
-                    "Remove some collections.");
-            }
+            throw new BadRequestException($"Your organization currently has {collectionCount} collections. " +
+                $"Your new license allows for a maximum of ({license.MaxCollections.Value}) collections. " +
+                "Remove some collections.");
         }
 
-        if (!license.UseGroups && organization.UseGroups)
+        if (!license.UseGroups && organization.UseGroups && groups.Any())
         {
-            var groups = await _groupRepository.GetManyByOrganizationIdAsync(organization.Id);
-            if (groups.Any())
-            {
-                throw new BadRequestException($"Your organization currently has {groups.Count} groups. " +
-                    $"Your new license does not allow for the use of groups. Remove all groups.");
-            }
+            throw new BadRequestException($"Your organization currently has {groups.Count()} groups. " +
+                $"Your new license does not allow for the use of groups. Remove all groups.");
         }
 
-        if (!license.UsePolicies && organization.UsePolicies)
+        var enabledPolicyCount = policies.Count(p => p.Enabled);
+        if (!license.UsePolicies && organization.UsePolicies && enabledPolicyCount > 0)
         {
-            var policies = await _policyRepository.GetManyByOrganizationIdAsync(organization.Id);
-            if (policies.Any(p => p.Enabled))
-            {
-                throw new BadRequestException($"Your organization currently has {policies.Count} enabled " +
-                    $"policies. Your new license does not allow for the use of policies. Disable all policies.");
-            }
+            throw new BadRequestException($"Your organization currently has {enabledPolicyCount} enabled " +
+                $"policies. Your new license does not allow for the use of policies. Disable all policies.");
         }
 
-        if (!license.UseSso && organization.UseSso)
+        if (!license.UseSso && organization.UseSso && ssoConfig is { Enabled: true })
         {
-            var ssoConfig = await _ssoConfigRepository.GetByOrganizationIdAsync(organization.Id);
-            if (ssoConfig is { Enabled: true})
-            {
-                throw new BadRequestException($"Your organization currently has a SSO configuration. " +
-                    $"Your new license does not allow for the use of SSO. Disable your SSO configuration.");
-            }
+            throw new BadRequestException($"Your organization currently has a SSO configuration. " +
+                $"Your new license does not allow for the use of SSO. Disable your SSO configuration.");
         }
 
-        if (!license.UseKeyConnector && organization.UseKeyConnector)
+        if (!license.UseKeyConnector && organization.UseKeyConnector && 
+            ssoConfig != null && ssoConfig.GetData().KeyConnectorEnabled)
         {
-            var ssoConfig = await _ssoConfigRepository.GetByOrganizationIdAsync(organization.Id);
-            if (ssoConfig != null && ssoConfig.GetData().KeyConnectorEnabled)
-            {
-                throw new BadRequestException($"Your organization currently has Key Connector enabled. " +
-                    $"Your new license does not allow for the use of Key Connector. Disable your Key Connector.");
-            }
+            throw new BadRequestException($"Your organization currently has Key Connector enabled. " +
+                $"Your new license does not allow for the use of Key Connector. Disable your Key Connector.");
         }
 
-        if (!license.UseScim && organization.UseScim)
+        if (!license.UseScim && organization.UseScim && 
+            scimConnections != null && 
+            scimConnections.Any(c => c.GetConfig<ScimConfig>() is {Enabled: true}))
         {
-            var scimConnections = await _organizationConnectionRepository.GetByOrganizationIdTypeAsync(organization.Id,
-                OrganizationConnectionType.Scim);
-            if (scimConnections != null && scimConnections.Any(c => c.GetConfig<ScimConfig>()?.Enabled == true))
-            {
-                throw new BadRequestException("Your new plan does not allow the SCIM feature. " +
-                    "Disable your SCIM configuration.");
-            }
+            throw new BadRequestException("Your new plan does not allow the SCIM feature. " +
+                "Disable your SCIM configuration.");
         }
 
-        if (!license.UseResetPassword && organization.UseResetPassword)
+        if (!license.UseResetPassword && organization.UseResetPassword &&
+            policies.Any(p => p.Type == PolicyType.ResetPassword && p.Enabled))
         {
-            var resetPasswordPolicy =
-                await _policyRepository.GetByOrganizationIdTypeAsync(organization.Id, PolicyType.ResetPassword);
-            if (resetPasswordPolicy is { Enabled: true })
-            {
-                throw new BadRequestException("Your new license does not allow the Password Reset feature. "
-                    + "Disable your Password Reset policy.");
-            }
+            throw new BadRequestException("Your new license does not allow the Password Reset feature. "
+                + "Disable your Password Reset policy.");
         }
     }
 
@@ -184,7 +138,7 @@ public class UpdateOrganizationLicenseCommand: IUpdateOrganizationLicenseCommand
         organization.ExpirationDate = license.Expires;
         organization.LicenseKey = license.LicenseKey;
         organization.RevisionDate = DateTime.UtcNow;
-        
+
         await _organizationService.ReplaceAndUpdateCacheAsync(organization);
     }
 }
