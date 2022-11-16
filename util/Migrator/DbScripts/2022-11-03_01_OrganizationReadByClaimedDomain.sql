005a41d84b2c2c06b78fa9929056ffbfdb7011f3
gbubemismith <gsmithwalter@gmail.com>
2022-11-16 00:42:09 -0500

    Added stored procedure to read claimed domains

---
+++ util/Migrator/DbScripts/2022-11-03_01_OrganizationReadByClaimedDomain.sql
@@ -0,0 +1,17 @@
+-- SP to get claimed domain by domain name
+CREATE PROCEDURE [dbo].[OrganizationDomain_ReadByClaimedDomain]
+    @DomainName NVARCHAR(255)
+AS
+BEGIN
+    SET NOCOUNT ON
+
+SELECT
+    *
+FROM
+    [dbo].[OrganizationDomain]
+WHERE
+    [DomainName] = @DomainName
+  AND
+    [VerifiedDate] IS NOT NULL
+END
+GO
