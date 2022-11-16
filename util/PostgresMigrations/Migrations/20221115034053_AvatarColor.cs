355b2dd6fba90915571fecba76c2338c07f5460f
Brandon Maharaj <bmaharaj@bitwarden.com>
2022-11-16 13:07:06 -0500

    Update 20221115034053_AvatarColor.cs

--- util/PostgresMigrations/Migrations/20221115034053_AvatarColor.cs
+++ util/PostgresMigrations/Migrations/20221115034053_AvatarColor.cs
@@ -1,28 +1,26 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Bit.PostgresMigrations.Migrations
+namespace Bit.PostgresMigrations.Migrations;
+
+public partial class AvatarColor : Migration
 {
-    public partial class AvatarColor : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
 
-            migrationBuilder.AddColumn<string>(
-                name: "AvatarColor",
-                table: "User",
-                type: "character varying(7)",
-                maxLength: 7,
-                nullable: true);
-        }
+        migrationBuilder.AddColumn<string>(
+            name: "AvatarColor",
+            table: "User",
+            type: "character varying(7)",
+            maxLength: 7,
+            nullable: true);
+    }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "AvatarColor",
-                table: "User");
-        }
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "AvatarColor",
+            table: "User");
     }
 }
