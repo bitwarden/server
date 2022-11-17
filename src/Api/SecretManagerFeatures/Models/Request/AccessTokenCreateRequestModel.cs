a3c0a1ba6f1a2af63c61f9f02bef8cea0cf31ed4
Thomas Avery <43214426+Thomas-Avery@users.noreply.github.com>
2022-11-17 11:06:20 -0600

    [SM-259] Add create access token endpoint for service accounts (#2411)
    * Add create access token for service accounts

---
+++ src/Api/SecretManagerFeatures/Models/Request/AccessTokenCreateRequestModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Bit.Core.Entities;
+using Bit.Core.Utilities;
+
+namespace Bit.Api.SecretManagerFeatures.Models.Request;
+
+public class AccessTokenCreateRequestModel
+{
+    [Required]
+    [EncryptedString]
+    [EncryptedStringLength(200)]
+    public string Name { get; set; }
+    [Required]
+    [EncryptedString]
+    [EncryptedStringLength(4000)]
+    public string EncryptedPayload { get; set; }
+    [Required]
+    [EncryptedString]
+    public string Key { get; set; }
+    [Required]
+    public DateTime ExpireAt { get; set; }
+
+    public ApiKey ToApiKey(Guid serviceAccountId)
+    {
+        return new ApiKey()
+        {
+            ServiceAccountId = serviceAccountId,
+            Name = Name,
+            Key = Key,
+            ExpireAt = ExpireAt,
+            Scope = "api.secrets",
+            EncryptedPayload = EncryptedPayload,
+
+        };
+    }
+}
