6f2c649d9d4070088a5d7072c7b19654cbec6741
Gbubemi Smith <gsmith@bitwarden.com>
2022-11-17 10:16:19 -0500

    created DNS service verification using DNSClient (#2401)

---
+++ src/Core/Services/Implementations/DnsResolverService.cs
@@ -0,0 +1,21 @@
+﻿using Bit.Core.Exceptions;
+using DnsClient;
+
+namespace Bit.Core.Services;
+
+public class DnsResolverService : IDnsResolverService
+{
+    public async Task<bool> ResolveAsync(string domain, string txtRecord, CancellationToken cancellationToken = default)
+    {
+        var lookup = new LookupClient();
+        var result = await lookup.QueryAsync(new DnsQuestion(domain, QueryType.TXT), cancellationToken);
+        if (!result.HasError)
+        {
+            return result.Answers.TxtRecords()
+                .Select(t => t?.EscapedText?.FirstOrDefault())
+                .Any(t => t == txtRecord);
+        }
+
+        throw new TxtRecordNotFoundException();
+    }
+}
