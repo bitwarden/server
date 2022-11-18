68f198765a285140dd3cee049ccad81a9372acc4
Kyle Spearrin <kyle.spearrin@gmail.com>
2022-11-18 10:32:15 -0500

    fix joins on some ef queries

--- src/Infrastructure.EntityFramework/Repositories/Queries/UserBumpAccountRevisionDateByCipherIdQuery.cs
+++ src/Infrastructure.EntityFramework/Repositories/Queries/UserBumpAccountRevisionDateByCipherIdQuery.cs
@@ -16,29 +16,33 @@ public class UserBumpAccountRevisionDateByCipherIdQuery : IQuery<User>
     public IQueryable<User> Run(DatabaseContext dbContext)
     {
         var query = from u in dbContext.Users
+
                     join ou in dbContext.OrganizationUsers
                         on u.Id equals ou.UserId
+
                     join collectionCipher in dbContext.CollectionCiphers
                         on _cipher.Id equals collectionCipher.CipherId into cc_g
                     from cc in cc_g.DefaultIfEmpty()
+
                     join collectionUser in dbContext.CollectionUsers
-                        on cc.CollectionId equals collectionUser.CollectionId into cu_g
+                        on new { ou.AccessAll, OrganizationUserId = ou.Id, cc.CollectionId } equals
+                           new { AccessAll = false, collectionUser.OrganizationUserId, collectionUser.CollectionId } into cu_g
                     from cu in cu_g.DefaultIfEmpty()
-                    where ou.AccessAll &&
-                            cu.OrganizationUserId == ou.Id
+
                     join groupUser in dbContext.GroupUsers
-                        on ou.Id equals groupUser.OrganizationUserId into gu_g
+                        on new { CollectionId = (Guid?)cu.CollectionId, ou.AccessAll, OrganizationUserId = ou.Id } equals
+                           new { CollectionId = (Guid?)null, AccessAll = false, groupUser.OrganizationUserId }  into gu_g
                     from gu in gu_g.DefaultIfEmpty()
-                    where cu.CollectionId == null &&
-                            !ou.AccessAll
+
                     join grp in dbContext.Groups
                         on gu.GroupId equals grp.Id into g_g
                     from g in g_g.DefaultIfEmpty()
+
                     join collectionGroup in dbContext.CollectionGroups
-                        on cc.CollectionId equals collectionGroup.CollectionId into cg_g
+                        on new { g.AccessAll, gu.GroupId, cc.CollectionId } equals
+                           new { AccessAll = false, collectionGroup.GroupId, collectionGroup.CollectionId } into cg_g
                     from cg in cg_g.DefaultIfEmpty()
-                    where !g.AccessAll &&
-                            cg.GroupId == gu.GroupId
+
                     where ou.OrganizationId == _cipher.OrganizationId &&
                             ou.Status == OrganizationUserStatusType.Confirmed &&
                             (cu.CollectionId != null ||
