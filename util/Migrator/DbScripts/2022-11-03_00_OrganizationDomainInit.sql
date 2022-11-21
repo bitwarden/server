1a63f03fa632e8345f8103ed33970463c107daa8
gbubemismith <gsmithwalter@gmail.com>
2022-11-21 13:07:39 -0500

    Updated the OrganizationDomain update procedure

--- util/Migrator/DbScripts/2022-11-03_00_OrganizationDomainInit.sql
+++ util/Migrator/DbScripts/2022-11-03_00_OrganizationDomainInit.sql
@@ -74,6 +74,10 @@ GO
 --Update
 CREATE OR ALTER PROCEDURE [dbo].[OrganizationDomain_Update]
     @Id UNIQUEIDENTIFIER OUTPUT,
+    @OrganizationId UNIQUEIDENTIFIER,
+    @Txt VARCHAR(MAX),
+    @DomainName NVARCHAR(255),
+    @CreationDate   DATETIME2(7),
     @VerifiedDate   DATETIME2(7),
     @NextRunDate    DATETIME2(7),
     @NextRunCount   TINYINT
@@ -84,6 +88,10 @@ BEGIN
 UPDATE
     [dbo].[OrganizationDomain]
 SET
+    [OrganizationId] = @OrganizationId,
+    [Txt] = @Txt,
+    [DomainName] = @DomainName,
+    [CreationDate] = @CreationDate,
     [VerifiedDate] = @VerifiedDate,
     [NextRunDate] = @NextRunDate,
     [NextRunCount] = @NextRunCount
