969d5cd870659ab08464c619da5371c8784a59ae
gbubemismith <gsmithwalter@gmail.com>
2022-11-25 14:30:02 -0500

    Added verification domain service implementation

---
+++ src/Core/Services/Implementations/VerificationDomainService.cs
@@ -0,0 +1,14 @@
+namespace Bit.Core.Services;
+
+public class VerificationDomainService : IVerificationDomainService
+{
+    public VerificationDomainService()
+    {
+        
+    }
+    
+    public async Task VerifyOrganizationsDomain()
+    {
+        
+    }
+}
