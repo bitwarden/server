d5315d664e8348087ebdfeac5d7cabe1a2e6a7a2
Todd Martin <tmartin@bitwarden.com>
2022-11-27 10:58:43 -0500

    Revert "Added SignalR logging filter."
    This reverts commit cecd4d4a868da74f19e8c3fad39b98b95a96c041.

--- src/Notifications/Program.cs
+++ src/Notifications/Program.cs
@@ -14,14 +14,35 @@ public class Program
             {
                 webBuilder.UseStartup<Startup>();
                 webBuilder.ConfigureLogging((hostingContext, logging) =>
-                { 
-                    logging.AddFilter("Microsoft.AspNetCore.SignalR", LogLevel.Debug);
-                    logging.AddFilter("Microsoft.AspNetCore.Http.Connections", LogLevel.Debug);
                     logging.AddSerilog(hostingContext, (e, globalSettings) =>
                     {
+                        var context = e.Properties["SourceContext"].ToString();
+                        if (context.Contains("IdentityServer4.Validation.TokenValidator") ||
+                            context.Contains("IdentityServer4.Validation.TokenRequestValidator"))
+                        {
+                            return e.Level >= globalSettings.MinLogLevel.NotificationsSettings.IdentityToken;
+                        }
+
+                        if (e.Level == LogEventLevel.Error &&
+                            e.MessageTemplate.Text == "Failed connection handshake.")
+                        {
+                            return false;
+                        }
+
+                        if (e.Level == LogEventLevel.Error &&
+                            e.MessageTemplate.Text.StartsWith("Failed writing message."))
+                        {
+                            return false;
+                        }
+
+                        if (e.Level == LogEventLevel.Warning &&
+                            e.MessageTemplate.Text.StartsWith("Heartbeat took longer"))
+                        {
+                            return false;
+                        }
+
                         return e.Level >= globalSettings.MinLogLevel.NotificationsSettings.Default;
-                    });
-                });
+                    }));
             })
             .Build()
             .Run();
