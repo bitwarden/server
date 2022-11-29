d0b92679c7eda90956b4be06d2ee421d0f6618cf
Todd Martin <tmartin@bitwarden.com>
2022-11-29 15:40:08 -0500

    Added debug-level logging for Notification Hub

--- src/Notifications/Controllers/SendController.cs
+++ src/Notifications/Controllers/SendController.cs
@@ -11,9 +11,12 @@ public class SendController : Controller
 {
     private readonly IHubContext<NotificationsHub> _hubContext;
 
-    public SendController(IHubContext<NotificationsHub> hubContext)
+    private readonly ILogger _logger;
+
+    public SendController(IHubContext<NotificationsHub> hubContext, ILogger logger)
     {
         _hubContext = hubContext;
+        _logger = logger;
     }
 
     [HttpPost("~/send")]
@@ -25,7 +28,7 @@ public class SendController : Controller
             var notificationJson = await reader.ReadToEndAsync();
             if (!string.IsNullOrWhiteSpace(notificationJson))
             {
-                await HubHelpers.SendNotificationToHubAsync(notificationJson, _hubContext, null);
+                await HubHelpers.SendNotificationToHubAsync(notificationJson, _hubContext, null, _logger);
             }
         }
     }
