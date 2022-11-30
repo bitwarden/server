13f13c67029f3eaf95a5a146923fef4d0336965a
Thomas Avery <tavery@bitwarden.com>
2022-11-30 17:51:26 -0600

    Run dotnet format

--- util/PostgresMigrations/Migrations/20221130232731_NeverExpireApiKey.cs
+++ util/PostgresMigrations/Migrations/20221130232731_NeverExpireApiKey.cs
@@ -1,34 +1,32 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Bit.PostgresMigrations.Migrations
-{
-    public partial class NeverExpireApiKey : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "ExpireAt",
-                table: "ApiKey",
-                type: "timestamp without time zone",
-                nullable: true,
-                oldClrType: typeof(DateTime),
-                oldType: "timestamp without time zone");
-        }
+namespace Bit.PostgresMigrations.Migrations;
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "ExpireAt",
-                table: "ApiKey",
-                type: "timestamp without time zone",
-                nullable: false,
-                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
-                oldClrType: typeof(DateTime),
-                oldType: "timestamp without time zone",
-                oldNullable: true);
-        }
+public partial class NeverExpireApiKey : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<DateTime>(
+            name: "ExpireAt",
+            table: "ApiKey",
+            type: "timestamp without time zone",
+            nullable: true,
+            oldClrType: typeof(DateTime),
+            oldType: "timestamp without time zone");
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<DateTime>(
+            name: "ExpireAt",
+            table: "ApiKey",
+            type: "timestamp without time zone",
+            nullable: false,
+            defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+            oldClrType: typeof(DateTime),
+            oldType: "timestamp without time zone",
+            oldNullable: true);
     }
 }
