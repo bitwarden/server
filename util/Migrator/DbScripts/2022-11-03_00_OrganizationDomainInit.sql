bd79de6c4778abc88b993570ef0c25c531525d12
gbubemismith <gsmithwalter@gmail.com>
2022-12-03 23:42:45 -0500

    Added stored procedure to get organization sso details by email address

--- util/Migrator/DbScripts/2022-11-03_00_OrganizationDomainInit.sql
+++ util/Migrator/DbScripts/2022-11-03_00_OrganizationDomainInit.sql
@@ -163,3 +163,34 @@ FROM
 WHERE
     [OrganizationId] = @OrganizationId
 END
+GO
+
+-- SP to get Organization SSO Provider details by Email
+CREATE OR ALTER PROCEDURE [dbo].[OrganizationDomainSsoDetails_ReadByEmail]
+    @Email NVARCHAR(256)
+AS
+BEGIN
+    SET NOCOUNT ON
+        
+    DECLARE @Domain NVARCHAR(256)
+
+SELECT @Domain = SUBSTRING(@Email, CHARINDEX( '@', @Email) + 1, LEN(@Email))
+
+SELECT
+    O.Id AS OrganizationId,
+    O.[Name] AS OrganizationName,
+    O.UseSso AS SsoAvailable,
+    P.Enabled AS SsoRequired,
+    O.Identifier AS OrganizationIdentifier,
+    OD.VerifiedDate
+FROM
+    [dbo].[OrganizationView] O
+    INNER JOIN [dbo].[OrganizationDomainView] OD
+ON O.Id = OD.OrganizationId
+    INNER JOIN [dbo].[PolicyView] P
+    ON O.Id = P.OrganizationId
+WHERE OD.DomainName = @Domain
+  AND O.Enabled = 1
+  AND P.[Type] = 4 -- SSO Type
+END
+GO
