f81914ef99418b3430cfff09ebabadfcec7b004b
Jacob Fink <jfink@bitwarden.com>
2022-12-05 10:50:11 -0500

    [EC-14] fix whitespace

--- src/Core/OrganizationFeatures/OrganizationCollections/DeleteCollectionCommand.cs
+++ src/Core/OrganizationFeatures/OrganizationCollections/DeleteCollectionCommand.cs
@@ -9,6 +9,7 @@ public class DeleteCollectionCommand : IDeleteCollectionCommand
 {
     private readonly ICollectionRepository _collectionRepository;
     private readonly IEventService _eventService;
+
     public DeleteCollectionCommand(
         ICollectionRepository collectionRepository,
         IEventService eventService)
@@ -16,23 +17,22 @@ public class DeleteCollectionCommand : IDeleteCollectionCommand
         _collectionRepository = collectionRepository;
         _eventService = eventService;
     }
+
     public async Task DeleteAsync(Collection collection)
     {
         await _collectionRepository.DeleteAsync(collection);
         await _eventService.LogCollectionEventAsync(collection, Enums.EventType.Collection_Deleted);
     }
+
     public async Task DeleteManyAsync(IEnumerable<Guid> collectionIds)
     {
-
         var ids = collectionIds as Guid[] ?? collectionIds.ToArray();
         var collectionsToDelete = await _collectionRepository.GetManyByManyIdsAsync(ids);
         await this.DeleteManyAsync(collectionsToDelete);
-
     }
 
     public async Task DeleteManyAsync(IEnumerable<Collection> collections)
     {
-
         await _collectionRepository.DeleteManyAsync(collections.Select(c => c.Id));
 
         var deleteDate = DateTime.UtcNow;
@@ -40,6 +40,5 @@ public class DeleteCollectionCommand : IDeleteCollectionCommand
         {
             await _eventService.LogCollectionEventAsync(collection, Enums.EventType.Collection_Deleted, deleteDate);
         }
-
     }
 }
