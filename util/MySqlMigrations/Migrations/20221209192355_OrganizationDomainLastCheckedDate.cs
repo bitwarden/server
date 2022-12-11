47a17613a7eb4045c5a3ce1d6c7074a72df554a5
gbubemismith <gsmithwalter@gmail.com>
2022-12-11 01:14:44 -0500

    Ef migrattion to add new LastCheckedDate column

--- util/MySqlMigrations/Migrations/20221209192355_OrganizationDomainLastCheckedDate.cs
+++ util/MySqlMigrations/Migrations/20221209192355_OrganizationDomainLastCheckedDate.cs
@@ -1,26 +1,24 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Bit.MySqlMigrations.Migrations
-{
-    public partial class OrganizationDomainLastCheckedDate : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<DateTime>(
-                name: "LastCheckedDate",
-                table: "OrganizationDomain",
-                type: "datetime(6)",
-                nullable: true);
-        }
+namespace Bit.MySqlMigrations.Migrations;
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "LastCheckedDate",
-                table: "OrganizationDomain");
-        }
+public partial class OrganizationDomainLastCheckedDate : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<DateTime>(
+            name: "LastCheckedDate",
+            table: "OrganizationDomain",
+            type: "datetime(6)",
+            nullable: true);
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "LastCheckedDate",
+            table: "OrganizationDomain");
     }
 }
--- util/PostgresMigrations/Migrations/20221209194623_OrganizationDomainLastCheckedDate.cs
+++ util/PostgresMigrations/Migrations/20221209194623_OrganizationDomainLastCheckedDate.cs
@@ -1,26 +1,24 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Bit.PostgresMigrations.Migrations
-{
-    public partial class OrganizationDomainLastCheckedDate : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<DateTime>(
-                name: "LastCheckedDate",
-                table: "OrganizationDomain",
-                type: "timestamp without time zone",
-                nullable: true);
-        }
+namespace Bit.PostgresMigrations.Migrations;
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "LastCheckedDate",
-                table: "OrganizationDomain");
-        }
+public partial class OrganizationDomainLastCheckedDate : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<DateTime>(
+            name: "LastCheckedDate",
+            table: "OrganizationDomain",
+            type: "timestamp without time zone",
+            nullable: true);
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "LastCheckedDate",
+            table: "OrganizationDomain");
     }
 }
