3814b601b94e67e1967988d53a82d8c00050e184
Rui Tome <rtome@bitwarden.com>
2022-12-26 17:04:17 +0000

    [EC-427] Provider table Type and BillingPhone MySql migrations

---
+++ util/MySqlMigrations/Migrations/20221226164641_ProviderAddProviderTypeBillingPhone.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.MySqlMigrations.Migrations
+{
+    public partial class ProviderAddProviderTypeBillingPhone : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BillingPhone",
+                table: "Provider",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<byte>(
+                name: "Type",
+                table: "Provider",
+                type: "tinyint unsigned",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BillingPhone",
+                table: "Provider");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Provider");
+        }
+    }
+}
