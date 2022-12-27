aa799bf324d48b11e4d9ee582a68deec303a361d
gbubemismith <gsmithwalter@gmail.com>
2022-12-27 01:01:32 -0700

    Fixed lint

--- test/Core.Test/OrganizationFeatures/OrganizationDomains/VerifyOrganizationDomainCommandTests.cs
+++ test/Core.Test/OrganizationFeatures/OrganizationDomains/VerifyOrganizationDomainCommandTests.cs
@@ -2,7 +2,6 @@
 using Bit.Core.Enums;
 using Bit.Core.Exceptions;
 using Bit.Core.OrganizationFeatures.OrganizationDomains;
-using Bit.Core.OrganizationFeatures.OrganizationDomains.Interfaces;
 using Bit.Core.Repositories;
 using Bit.Core.Services;
 using Bit.Test.Common.AutoFixture;
@@ -29,14 +28,17 @@ public class VerifyOrganizationDomainCommandTests
 
         await Assert.ThrowsAsync<NotFoundException>(requestAction);
     }
-    
+
     [Theory, BitAutoData]
     public async Task VerifyOrganizationDomain_ShouldThrowConflict_WhenDomainHasBeenClaimed(Guid id,
         SutProvider<VerifyOrganizationDomainCommand> sutProvider)
     {
         var expected = new OrganizationDomain
         {
-            Id = id, OrganizationId = Guid.NewGuid(), DomainName = "Test Domain", Txt = "btw+test18383838383"
+            Id = id,
+            OrganizationId = Guid.NewGuid(),
+            DomainName = "Test Domain",
+            Txt = "btw+test18383838383"
         };
         expected.SetVerifiedDate();
         sutProvider.GetDependency<IOrganizationDomainRepository>()
@@ -48,35 +50,41 @@ public class VerifyOrganizationDomainCommandTests
         var exception = await Assert.ThrowsAsync<ConflictException>(requestAction);
         Assert.Contains("Domain has already been verified.", exception.Message);
     }
-    
+
     [Theory, BitAutoData]
     public async Task VerifyOrganizationDomain_ShouldThrowConflict_WhenDomainHasBeenClaimedByAnotherOrganization(Guid id,
         SutProvider<VerifyOrganizationDomainCommand> sutProvider)
     {
         var expected = new OrganizationDomain
         {
-            Id = id, OrganizationId = Guid.NewGuid(), DomainName = "Test Domain", Txt = "btw+test18383838383"
+            Id = id,
+            OrganizationId = Guid.NewGuid(),
+            DomainName = "Test Domain",
+            Txt = "btw+test18383838383"
         };
         sutProvider.GetDependency<IOrganizationDomainRepository>()
             .GetByIdAsync(id)
             .Returns(expected);
         sutProvider.GetDependency<IOrganizationDomainRepository>()
             .GetClaimedDomainsByDomainNameAsync(expected.DomainName)
-            .Returns(new List<OrganizationDomain> {expected});
+            .Returns(new List<OrganizationDomain> { expected });
 
         var requestAction = async () => await sutProvider.Sut.VerifyOrganizationDomain(id);
 
         var exception = await Assert.ThrowsAsync<ConflictException>(requestAction);
         Assert.Contains("The domain is not available to be claimed.", exception.Message);
     }
-    
+
     [Theory, BitAutoData]
     public async Task VerifyOrganizationDomain_ShouldVerifyDomainUpdateAndLogEvent_WhenTxtRecordExists(Guid id,
         SutProvider<VerifyOrganizationDomainCommand> sutProvider)
     {
         var expected = new OrganizationDomain
         {
-            Id = id, OrganizationId = Guid.NewGuid(), DomainName = "Test Domain", Txt = "btw+test18383838383"
+            Id = id,
+            OrganizationId = Guid.NewGuid(),
+            DomainName = "Test Domain",
+            Txt = "btw+test18383838383"
         };
         sutProvider.GetDependency<IOrganizationDomainRepository>()
             .GetByIdAsync(id)
@@ -96,14 +104,17 @@ public class VerifyOrganizationDomainCommandTests
         await sutProvider.GetDependency<IEventService>().Received(1)
             .LogOrganizationDomainEventAsync(Arg.Any<OrganizationDomain>(), EventType.OrganizationDomain_Verified);
     }
-    
+
     [Theory, BitAutoData]
     public async Task VerifyOrganizationDomain_ShouldNotSetVerifiedDate_WhenTxtRecordDoesNotExist(Guid id,
         SutProvider<VerifyOrganizationDomainCommand> sutProvider)
     {
         var expected = new OrganizationDomain
         {
-            Id = id, OrganizationId = Guid.NewGuid(), DomainName = "Test Domain", Txt = "btw+test18383838383"
+            Id = id,
+            OrganizationId = Guid.NewGuid(),
+            DomainName = "Test Domain",
+            Txt = "btw+test18383838383"
         };
         sutProvider.GetDependency<IOrganizationDomainRepository>()
             .GetByIdAsync(id)
@@ -119,6 +130,6 @@ public class VerifyOrganizationDomainCommandTests
 
         Assert.Null(result.VerifiedDate);
         await sutProvider.GetDependency<IEventService>().Received(1)
-            .LogOrganizationDomainEventAsync(Arg.Any<OrganizationDomain>(),  EventType.OrganizationDomain_NotVerified);
+            .LogOrganizationDomainEventAsync(Arg.Any<OrganizationDomain>(), EventType.OrganizationDomain_NotVerified);
     }
 }
