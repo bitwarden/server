b5bb8e228eb207e207da1674d94d665f1986440b
Rui Tome <rtome@bitwarden.com>
2022-12-27 10:40:34 +0000

    [EC-427] Add mysql migration script

--- util/MySqlMigrations/Migrations/20221226164641_ProviderAddProviderTypeBillingPhone.cs
+++ util/MySqlMigrations/Migrations/20221226164641_ProviderAddProviderTypeBillingPhone.cs
@@ -2,36 +2,35 @@
 
 #nullable disable
 
-namespace Bit.MySqlMigrations.Migrations
+namespace Bit.MySqlMigrations.Migrations;
+
+public partial class ProviderAddProviderTypeBillingPhone : Migration
 {
-    public partial class ProviderAddProviderTypeBillingPhone : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<string>(
-                name: "BillingPhone",
-                table: "Provider",
-                type: "longtext",
-                nullable: true)
-                .Annotation("MySql:CharSet", "utf8mb4");
+        migrationBuilder.AddColumn<string>(
+            name: "BillingPhone",
+            table: "Provider",
+            type: "longtext",
+            nullable: true)
+            .Annotation("MySql:CharSet", "utf8mb4");
 
-            migrationBuilder.AddColumn<byte>(
-                name: "Type",
-                table: "Provider",
-                type: "tinyint unsigned",
-                nullable: false,
-                defaultValue: (byte)0);
-        }
+        migrationBuilder.AddColumn<byte>(
+            name: "Type",
+            table: "Provider",
+            type: "tinyint unsigned",
+            nullable: false,
+            defaultValue: (byte)0);
+    }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "BillingPhone",
-                table: "Provider");
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "BillingPhone",
+            table: "Provider");
 
-            migrationBuilder.DropColumn(
-                name: "Type",
-                table: "Provider");
-        }
+        migrationBuilder.DropColumn(
+            name: "Type",
+            table: "Provider");
     }
 }
