64dd55e94f1d48fe9c3d31233fc1ddc036ca8d68
Rui Tome <rtome@bitwarden.com>
2022-12-29 16:03:24 +0000

    [EC-787] Add new stored procedure OrganizationUser_ReadByUserIdWithPolicyDetails

---
+++ src/Sql/dbo/Stored Procedures/OrganizationUser_ReadByUserIdWithPolicyDetails.sql
@@ -0,0 +1,39 @@
+﻿CREATE PROCEDURE [dbo].[OrganizationUser_ReadByUserIdWithPolicyDetails]
+    @UserId UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON
+SELECT
+    P.[OrganizationId],
+    P.[Type] as PolicyType,
+    P.[Enabled] as PolicyEnabled,
+    OU.[Type] as OrganizationUserType,
+    OU.[Status] as OrganizationUserStatus,
+    (CASE WHEN OU.[Permissions] IS NULL THEN 0 ELSE (CASE WHEN COALESCE(JSON_VALUE(OU.[Permissions], '$.managePolicies'), 'false') = 'false' THEN 0 ELSE 1 END) END) as CanManagePolicies,
+    (CASE WHEN PUPO.[UserId] IS NULL THEN 0 ELSE 1 END) as IsProvider
+FROM
+    [dbo].[PolicyView] P
+INNER JOIN
+    [dbo].[OrganizationUserView] OU ON P.[OrganizationId] = OU.[OrganizationId]
+LEFT JOIN
+    (SELECT
+        PU.UserId,
+        PO.OrganizationId
+    FROM
+        [dbo].[ProviderUserView] PU
+    INNER JOIN
+        [ProviderOrganizationView] PO ON PO.[ProviderId] = PU.[ProviderId]) PUPO
+    ON PUPO.UserId = OU.UserId
+    AND PUPO.OrganizationId = P.OrganizationId
+WHERE
+    (
+        (
+            OU.[Status] != 0     -- OrgUsers who have accepted their invite and are linked to a UserId
+            AND OU.[UserId] = @UserId
+        )
+       OR (
+            OU.[Status] = 0     -- 'Invited' OrgUsers are not linked to a UserId yet, so we have to look up their email
+            AND OU.[Email] IN (SELECT U.Email FROM [dbo].[UserView] U WHERE U.Id = @UserId)
+        )
+    )
+END
