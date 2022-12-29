50f791e993205b1a408b72eab9a49582934b6c61
Rui Tome <rtome@bitwarden.com>
2022-12-29 16:04:56 +0000

    [EC-787] Add OrganizationUserPolicyDetails to represent policies applicable to a specific user

---
+++ src/Core/Models/Data/Organizations/OrganizationUsers/OrganizationUserPolicyDetails.cs
@@ -0,0 +1,20 @@
+﻿using Bit.Core.Enums;
+
+namespace Bit.Core.Models.Data.Organizations.OrganizationUsers;
+
+public class OrganizationUserPolicyDetails
+{
+    public Guid OrganizationId { get; set; }
+
+    public PolicyType PolicyType { get; set; }
+
+    public bool PolicyEnabled { get; set; }
+
+    public OrganizationUserType OrganizationUserType { get; set; }
+
+    public OrganizationUserStatusType OrganizationUserStatus { get; set; }
+
+    public bool CanManagePolicies { get; set; }
+
+    public bool IsProvider { get; set; }
+}
