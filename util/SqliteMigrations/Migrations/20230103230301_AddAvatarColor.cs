6f1a9deb5cd17ae7750ad5cac787c0cdf5b00973
Todd Martin <tmartin@bitwarden.com>
2023-01-03 18:35:26 -0500

    Linting

--- util/SqliteMigrations/Migrations/20230103230301_AddAvatarColor.cs
+++ util/SqliteMigrations/Migrations/20230103230301_AddAvatarColor.cs
@@ -2,25 +2,24 @@
 
 #nullable disable
 
-namespace Bit.SqliteMigrations.Migrations
-{
-    public partial class AddAvatarColor : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<string>(
-                name: "AvatarColor",
-                table: "User",
-                type: "TEXT",
-                maxLength: 7,
-                nullable: true);
-        }
+namespace Bit.SqliteMigrations.Migrations;
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "AvatarColor",
-                table: "User");
-        }
+public partial class AddAvatarColor : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<string>(
+            name: "AvatarColor",
+            table: "User",
+            type: "TEXT",
+            maxLength: 7,
+            nullable: true);
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "AvatarColor",
+            table: "User");
     }
 }
