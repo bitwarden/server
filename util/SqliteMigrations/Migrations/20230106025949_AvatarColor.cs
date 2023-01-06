0466aa0fc8a5139ef847c064800b94b1ecca649f
Kyle Spearrin <kyle.spearrin@gmail.com>
2023-01-05 22:07:34 -0500

    lint fix

--- util/SqliteMigrations/Migrations/20230106025949_AvatarColor.cs
+++ util/SqliteMigrations/Migrations/20230106025949_AvatarColor.cs
@@ -2,25 +2,24 @@
 
 #nullable disable
 
-namespace Bit.SqliteMigrations.Migrations
-{
-    public partial class AvatarColor : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<string>(
-                name: "AvatarColor",
-                table: "User",
-                type: "TEXT",
-                maxLength: 7,
-                nullable: true);
-        }
+namespace Bit.SqliteMigrations.Migrations;
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "AvatarColor",
-                table: "User");
-        }
+public partial class AvatarColor : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<string>(
+            name: "AvatarColor",
+            table: "User",
+            type: "TEXT",
+            maxLength: 7,
+            nullable: true);
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "AvatarColor",
+            table: "User");
     }
 }
