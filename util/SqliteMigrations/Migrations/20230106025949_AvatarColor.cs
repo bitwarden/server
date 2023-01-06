47ee8361c9a93640d4694e5edbf6a505180dc0ab
Kyle Spearrin <kyle.spearrin@gmail.com>
2023-01-05 22:02:29 -0500

    add sqlite migrations for avatar color

---
+++ util/SqliteMigrations/Migrations/20230106025949_AvatarColor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.SqliteMigrations.Migrations
+{
+    public partial class AvatarColor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AvatarColor",
+                table: "User",
+                type: "TEXT",
+                maxLength: 7,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvatarColor",
+                table: "User");
+        }
+    }
+}
