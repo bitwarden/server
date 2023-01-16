ed991e319c2a80d1a432a0c2436d2780255726ef
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2023-01-16 08:44:51 -0500

    Fix EF IntegrationTest

--- test/Core.Test/AutoFixture/GlobalSettingsFixtures.cs
+++ test/Core.Test/AutoFixture/GlobalSettingsFixtures.cs
@@ -1,8 +1,12 @@
 ﻿using System.Reflection;
+using System.Text;
 using AutoFixture;
 using AutoFixture.Kernel;
 using AutoFixture.Xunit2;
+using Bit.Core;
 using Bit.Core.Test.Helpers.Factories;
+using Microsoft.AspNetCore.DataProtection;
+using Moq;
 
 namespace Bit.Test.Common.AutoFixture;
 
@@ -15,13 +19,34 @@ public class GlobalSettingsBuilder : ISpecimenBuilder
             throw new ArgumentNullException(nameof(context));
         }
 
-        var pi = request as ParameterInfo;
         var fixture = new Fixture();
 
-        if (pi == null || pi.ParameterType != typeof(Bit.Core.Settings.GlobalSettings))
+        if (request is not ParameterInfo pi)
+        {
             return new NoSpecimen();
+        }
 
-        return GlobalSettingsFactory.GlobalSettings;
+        if (pi.ParameterType == typeof(Bit.Core.Settings.GlobalSettings))
+        {
+            return GlobalSettingsFactory.GlobalSettings;
+        }
+
+        if (pi.ParameterType == typeof(IDataProtectionProvider))
+        {
+            var dataProtector = new Mock<IDataProtector>();
+                dataProtector
+                    .Setup(d => d.Unprotect(It.IsAny<byte[]>()))
+                    .Returns<byte[]>(data => Encoding.UTF8.GetBytes("P|" + Encoding.UTF8.GetString(data))); // I THINK?
+
+            var dataProtectionProvider = new Mock<IDataProtectionProvider>();
+            dataProtectionProvider
+                .Setup(x => x.CreateProtector(Constants.DatabaseFieldProtectorPurpose))
+                .Returns(dataProtector.Object);
+
+            return dataProtectionProvider.Object;
+        }
+
+        return new NoSpecimen();
     }
 }
 
--- test/Infrastructure.EFIntegration.Test/Helpers/DatabaseOptionsFactory.cs
+++ test/Infrastructure.EFIntegration.Test/Helpers/DatabaseOptionsFactory.cs
@@ -1,6 +1,11 @@
-﻿using Bit.Core.Test.Helpers.Factories;
+﻿using System.Text;
+using Bit.Core;
+using Bit.Core.Test.Helpers.Factories;
 using Bit.Infrastructure.EntityFramework.Repositories;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
 
 namespace Bit.Infrastructure.EFIntegration.Test.Helpers;
 
@@ -10,16 +15,39 @@ public static class DatabaseOptionsFactory
 
     static DatabaseOptionsFactory()
     {
+        var services = new ServiceCollection()
+            .AddSingleton(sp =>
+            {
+                var dataProtector = new Mock<IDataProtector>();
+                dataProtector
+                    .Setup(d => d.Unprotect(It.IsAny<byte[]>()))
+                    .Returns<byte[]>(data => Encoding.UTF8.GetBytes("P|" + Encoding.UTF8.GetString(data))); // I THINK?
+
+                var dataProtectionProvider = new Mock<IDataProtectionProvider>();
+                dataProtectionProvider
+                    .Setup(x => x.CreateProtector(Constants.DatabaseFieldProtectorPurpose))
+                    .Returns(dataProtector.Object);
+
+                return dataProtectionProvider.Object;
+            })
+            .BuildServiceProvider();
+
         var globalSettings = GlobalSettingsFactory.GlobalSettings;
         if (!string.IsNullOrWhiteSpace(GlobalSettingsFactory.GlobalSettings.PostgreSql?.ConnectionString))
         {
             AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-            Options.Add(new DbContextOptionsBuilder<DatabaseContext>().UseNpgsql(globalSettings.PostgreSql.ConnectionString).Options);
+            Options.Add(new DbContextOptionsBuilder<DatabaseContext>()
+                .UseNpgsql(globalSettings.PostgreSql.ConnectionString)
+                .UseApplicationServiceProvider(services)
+                .Options);
         }
         if (!string.IsNullOrWhiteSpace(GlobalSettingsFactory.GlobalSettings.MySql?.ConnectionString))
         {
             var mySqlConnectionString = globalSettings.MySql.ConnectionString;
-            Options.Add(new DbContextOptionsBuilder<DatabaseContext>().UseMySql(mySqlConnectionString, ServerVersion.AutoDetect(mySqlConnectionString)).Options);
+            Options.Add(new DbContextOptionsBuilder<DatabaseContext>()
+                .UseMySql(mySqlConnectionString, ServerVersion.AutoDetect(mySqlConnectionString))
+                .UseApplicationServiceProvider(services)
+                .Options);
         }
     }
 }
