4546d90e46b84549a7cd8a20da4681b249952e0b
Colton Hurst <colton@coltonhurst.com>
2023-01-17 15:17:18 -0500

    SM-365: Add Export endpoint

---
+++ src/Api/Controllers/SecretsManagerPortingController.cs
@@ -0,0 +1,44 @@
+﻿using Bit.Api.SecretManagerFeatures.Models.Response;
+using Bit.Api.Utilities;
+using Bit.Core.Exceptions;
+using Bit.Core.Repositories;
+using Bit.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bit.Api.Controllers;
+
+[SecretsManager]
+public class SecretsManagerPortingController : Controller
+{
+    private readonly ISecretRepository _secretRepository;
+    private readonly IProjectRepository _projectRepository;
+    private readonly IUserService _userService;
+
+    public SecretsManagerPortingController(ISecretRepository secretRepository, IProjectRepository projectRepository, IUserService userService)
+    {
+        _secretRepository = secretRepository;
+        _projectRepository = projectRepository;
+        _userService = userService;
+    }
+
+    [HttpGet("sm/{organizationId}/export")]
+    public async Task<SMExportResponseModel> Export([FromRoute] Guid organizationId, [FromRoute] string format = "json")
+    {
+        var userId = _userService.GetProperUserId(User).Value;
+        var projects = await _projectRepository.GetManyByOrganizationIdAsync(organizationId, userId);
+        var secrets = await _secretRepository.GetManyByOrganizationIdAsync(organizationId);
+
+        if (secrets == null && projects == null)
+        {
+            throw new NotFoundException();
+        }
+
+        return new SMExportResponseModel(projects, secrets);
+    }
+
+    [HttpPost("sm/{organizationId}/import")]
+    public async Task<SMExportResponseModel> Import()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Api/SecretManagerFeatures/Models/Response/SMExportResponseModel.cs
@@ -0,0 +1,48 @@
+﻿using Bit.Core.Entities;
+using Bit.Core.Models.Api;
+
+namespace Bit.Api.SecretManagerFeatures.Models.Response;
+
+public class SMExportResponseModel : ResponseModel
+{
+    public SMExportResponseModel(IEnumerable<Project> projects, IEnumerable<Secret> secrets, string obj = "SecretsManagerExport") : base(obj)
+    {
+        Secrets = secrets.Select(s => new InnerSecret(s));
+        Projects = projects.Select(p => new InnerProject(p));
+    }
+
+    public IEnumerable<InnerProject> Projects { get; set; }
+    public IEnumerable<InnerSecret> Secrets { get; set; }
+
+    public class InnerProject
+    {
+        public InnerProject(Project project)
+        {
+            Id = project.Id;
+            Name = project.Name;
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class InnerSecret
+    {
+        public InnerSecret(Secret secret)
+        {
+            Id = secret.Id.ToString();
+            Key = secret.Key;
+            Value = secret.Value;
+            Note = secret.Note;
+            ProjectIds = secret.Projects?.Select(p => p.Id);
+        }
+
+        public string Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public string Note { get; set; }
+        public IEnumerable<Guid> ProjectIds { get; set; }
+    }
+}
+
+
