4ddeb50c2ba69334b9385209fc344d1080d135c9
Rui Tome <rtome@bitwarden.com>
2023-01-17 16:58:12 +0000

    [EC-428] Added Type, BusinessName and BillingEmail to CreateProviderModel

--- src/Admin/Models/CreateProviderModel.cs
+++ src/Admin/Models/CreateProviderModel.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Bit.Core.Entities.Provider;
+using Bit.Core.Enums.Provider;
 
 namespace Bit.Admin.Models;
 
@@ -6,7 +8,26 @@ public class CreateProviderModel
 {
     public CreateProviderModel() { }
 
+    [Display(Name = "Provider Type")]
+    public ProviderType Type { get; set; }
+
     [Display(Name = "Owner Email")]
     [Required]
     public string OwnerEmail { get; set; }
+
+    [Display(Name = "Business Name")]
+    public string BusinessName { get; set; }
+
+    [Display(Name = "Primary Billing Email")]
+    public string BillingEmail { get; set; }
+
+    public virtual Provider ToProvider()
+    {
+        return new Provider()
+        {
+            Type = Type,
+            BusinessName = BusinessName,
+            BillingEmail = BillingEmail?.ToLowerInvariant().Trim()
+        };
+    }
 }
