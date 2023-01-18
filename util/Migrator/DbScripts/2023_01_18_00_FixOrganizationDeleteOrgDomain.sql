476fc6df68c3de80b5d34c91b7e0898dcc4fd1cc
gbubemismith <gsmithwalter@gmail.com>
2023-01-18 13:27:23 +0100

    Added stored procedure to fix FX contstraint issue when deleting an organization

---
+++ util/Migrator/DbScripts/2023_01_18_00_FixOrganizationDeleteOrgDomain.sql
@@ -0,0 +1,88 @@
+-- Create OrganizationDomain delete by OrganizationId
+CREATE OR ALTER PROCEDURE [dbo].[OrganizationDomain_OrganizationDeleted]
+    @OrganizationId UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON
+
+DELETE
+FROM
+    [dbo].[OrganizationDomain]
+WHERE
+    [OrganizationId] = @OrganizationId
+END
+GO
+
+-- Update Organization Delete By Id to include OrganizationDomain_OrganizationDeleted
+CREATE OR ALTER PROCEDURE [dbo].[Organization_DeleteById]
+    @Id UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    EXEC [dbo].[User_BumpAccountRevisionDateByOrganizationId] @Id
+
+    DECLARE @BatchSize INT = 100
+    WHILE @BatchSize > 0
+BEGIN
+BEGIN TRANSACTION Organization_DeleteById_Ciphers
+
+        DELETE TOP(@BatchSize)
+        FROM
+            [dbo].[Cipher]
+        WHERE
+            [UserId] IS NULL
+            AND [OrganizationId] = @Id
+
+        SET @BatchSize = @@ROWCOUNT
+
+        COMMIT TRANSACTION Organization_DeleteById_Ciphers
+END
+
+BEGIN TRANSACTION Organization_DeleteById
+
+DELETE
+FROM
+    [dbo].[SsoUser]
+WHERE
+    [OrganizationId] = @Id
+
+DELETE
+FROM
+    [dbo].[SsoConfig]
+WHERE
+    [OrganizationId] = @Id
+
+DELETE CU
+    FROM 
+        [dbo].[CollectionUser] CU
+    INNER JOIN 
+        [dbo].[OrganizationUser] OU ON [CU].[OrganizationUserId] = [OU].[Id]
+    WHERE 
+        [OU].[OrganizationId] = @Id
+
+DELETE
+FROM
+    [dbo].[OrganizationUser]
+WHERE
+    [OrganizationId] = @Id
+
+DELETE
+FROM
+    [dbo].[ProviderOrganization]
+WHERE
+    [OrganizationId] = @Id
+
+    EXEC [dbo].[OrganizationApiKey_OrganizationDeleted] @Id
+    EXEC [dbo].[OrganizationConnection_OrganizationDeleted] @Id
+    EXEC [dbo].[OrganizationSponsorship_OrganizationDeleted] @Id
+
+DELETE
+FROM
+    [dbo].[Organization]
+WHERE
+    [Id] = @Id
+
+    COMMIT TRANSACTION Organization_DeleteById
+END
+GO
