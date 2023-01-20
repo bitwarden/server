2f74c0f68b96ee2d850930f0aacdd3186fb0fe12
Rui Tome <rtome@bitwarden.com>
2023-01-20 16:04:54 +0000

    [EC-1014] Added EF migrations

---
+++ util/PostgresMigrations/Migrations/20230120160253_OrganizationStatus.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.PostgresMigrations.Migrations
+{
+    public partial class OrganizationStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "Status",
+                table: "Organization",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Organization");
+        }
+    }
+}
