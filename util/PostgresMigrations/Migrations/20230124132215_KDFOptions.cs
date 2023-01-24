a6a37a8ea1c3665f84b7b92a5dd12668d878cc37
Kyle Spearrin <kyle.spearrin@gmail.com>
2023-01-24 08:23:13 -0500

    add ef migrations

---
+++ util/PostgresMigrations/Migrations/20230124132215_KDFOptions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.PostgresMigrations.Migrations
+{
+    public partial class KDFOptions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "KdfMemory",
+                table: "User",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "KdfParallelism",
+                table: "User",
+                type: "integer",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KdfMemory",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "KdfParallelism",
+                table: "User");
+        }
+    }
+}
