c16890c74108478991ce0c747551a79c3339508b
Hinton <hinton@users.noreply.github.com>
2023-01-24 16:29:53 +0100

    Fix unoirts

--- bitwarden_license/src/Commercial.Core/SecretManager/SecretManagerCollectionExtensions.cs
+++ bitwarden_license/src/Commercial.Core/SecretManager/SecretManagerCollectionExtensions.cs
@@ -1,9 +1,4 @@
-﻿using Bit.Commercial.Core.SecretManager.AccessPolicies;
-using Bit.Commercial.Core.SecretManager.AccessTokens;
-using Bit.Commercial.Core.SecretManager.Projects;
-using Bit.Commercial.Core.SecretManager.Secrets;
-using Bit.Commercial.Core.SecretManager.ServiceAccounts;
-using Bit.Core.SecretsManager.Commands.AccessPolicies.Interfaces;
+﻿using Bit.Core.SecretsManager.Commands.AccessPolicies.Interfaces;
 using Bit.Core.SecretsManager.Commands.AccessTokens.Interfaces;
 using Bit.Core.SecretsManager.Commands.Projects.Interfaces;
 using Bit.Core.SecretsManager.Commands.Secrets.Interfaces;
