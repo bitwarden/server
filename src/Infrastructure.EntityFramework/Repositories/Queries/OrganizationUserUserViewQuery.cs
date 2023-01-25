225d4e1c3f24684169fb3707520862e4b876e85a
Brandon Maharaj <bmaharaj@bitwarden.com>
2023-01-24 21:53:29 -0500

    work: baseline for org stuff

--- src/Infrastructure.EntityFramework/Repositories/Queries/OrganizationUserUserViewQuery.cs
+++ src/Infrastructure.EntityFramework/Repositories/Queries/OrganizationUserUserViewQuery.cs
@@ -19,6 +19,7 @@ public class OrganizationUserUserDetailsViewQuery : IQuery<OrganizationUserUserD
             OrganizationId = x.ou.OrganizationId,
             Name = x.u.Name,
             Email = x.u.Email ?? x.ou.Email,
+            AvatarColor = x.u.AvatarColor,
             TwoFactorProviders = x.u.TwoFactorProviders,
             Premium = x.u.Premium,
             Status = x.ou.Status,
---
+++ util/Migrator/DbScripts/2023-01-23_00_AddAvatarToOrganizationUsers.sql
@@ -0,0 +1,41 @@
+CREATE OR ALTER VIEW [dbo].[OrganizationUserUserDetailsView]
+AS
+SELECT
+    OU.[Id],
+    OU.[UserId],
+    OU.[OrganizationId],
+    U.[Name],
+    ISNULL(U.[Email], OU.[Email]) Email,
+    U.[AvatarColor],
+    U.[TwoFactorProviders],
+    U.[Premium],
+    OU.[Status],
+    OU.[Type],
+    OU.[AccessAll],
+    OU.[ExternalId],
+    SU.[ExternalId] SsoExternalId,
+    OU.[Permissions],
+    OU.[ResetPasswordKey],
+    U.[UsesKeyConnector]
+FROM
+    [dbo].[OrganizationUser] OU
+LEFT JOIN
+    [dbo].[User] U ON U.[Id] = OU.[UserId]
+LEFT JOIN
+    [dbo].[SsoUser] SU ON SU.[UserId] = OU.[UserId] AND SU.[OrganizationId] = OU.[OrganizationId]
+GO
+
+CREATE OR ALTER PROCEDURE [dbo].[OrganizationUserUserDetails_ReadByOrganizationId]
+    @OrganizationId UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    SELECT
+        *
+    FROM
+        [dbo].[OrganizationUserUserDetailsView]
+    WHERE
+        [OrganizationId] = @OrganizationId
+END
+GO
