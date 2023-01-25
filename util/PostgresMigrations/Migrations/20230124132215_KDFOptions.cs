377b5302ad16816daf62f50bb1455ae815e6a60e
Kyle Spearrin <kyle.spearrin@gmail.com>
2023-01-25 10:28:43 -0500

    fix lint issues and sql csproj

--- util/PostgresMigrations/Migrations/20230124132215_KDFOptions.cs
+++ util/PostgresMigrations/Migrations/20230124132215_KDFOptions.cs
@@ -2,34 +2,33 @@
 
 #nullable disable
 
-namespace Bit.PostgresMigrations.Migrations
+namespace Bit.PostgresMigrations.Migrations;
+
+public partial class KDFOptions : Migration
 {
-    public partial class KDFOptions : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<int>(
-                name: "KdfMemory",
-                table: "User",
-                type: "integer",
-                nullable: true);
+        migrationBuilder.AddColumn<int>(
+            name: "KdfMemory",
+            table: "User",
+            type: "integer",
+            nullable: true);
 
-            migrationBuilder.AddColumn<int>(
-                name: "KdfParallelism",
-                table: "User",
-                type: "integer",
-                nullable: true);
-        }
+        migrationBuilder.AddColumn<int>(
+            name: "KdfParallelism",
+            table: "User",
+            type: "integer",
+            nullable: true);
+    }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "KdfMemory",
-                table: "User");
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "KdfMemory",
+            table: "User");
 
-            migrationBuilder.DropColumn(
-                name: "KdfParallelism",
-                table: "User");
-        }
+        migrationBuilder.DropColumn(
+            name: "KdfParallelism",
+            table: "User");
     }
 }
