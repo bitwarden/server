b6df05e2594d14fc45475e84e1a1213d50bda0a2
Hinton <hinton@users.noreply.github.com>
2023-01-27 10:18:06 +0100

    Fix error

--- test/IntegrationTestCommon/Factories/WebApplicationFactoryBase.cs
+++ test/IntegrationTestCommon/Factories/WebApplicationFactoryBase.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace Bit.IntegrationTestCommon.Factories;
 
