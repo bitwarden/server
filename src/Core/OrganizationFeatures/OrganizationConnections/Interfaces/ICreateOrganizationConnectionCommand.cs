82908b1fb74c29a757319fa72b01b1a02a9b3a5d
Thomas Rittson <31796059+eliykat@users.noreply.github.com>
2023-01-31 07:42:10 +1000

    [EC-826] Merge license sync feature branch to master (#2587)
    * [EC-634] Extract GenerateLicenseAsync to a query (#2373)
    
    * [EC-637] Add license sync to server (#2453)
    
    * [EC-1036] Show correct license sync date (#2626)
    
    * Update method name per new pattern

--- src/Core/OrganizationFeatures/OrganizationConnections/Interfaces/ICreateOrganizationConnectionCommand.cs
+++ src/Core/OrganizationFeatures/OrganizationConnections/Interfaces/ICreateOrganizationConnectionCommand.cs
@@ -1,9 +1,10 @@
 ﻿using Bit.Core.Entities;
 using Bit.Core.Models.Data.Organizations.OrganizationConnections;
+using Bit.Core.Models.OrganizationConnectionConfigs;
 
 namespace Bit.Core.OrganizationFeatures.OrganizationConnections.Interfaces;
 
 public interface ICreateOrganizationConnectionCommand
 {
-    Task<OrganizationConnection> CreateAsync<T>(OrganizationConnectionData<T> connectionData) where T : new();
+    Task<OrganizationConnection> CreateAsync<T>(OrganizationConnectionData<T> connectionData) where T : IConnectionConfig;
 }
