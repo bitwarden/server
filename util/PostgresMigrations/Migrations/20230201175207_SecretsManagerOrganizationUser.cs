c1d2fbcd93549b4e6ef721c96dc2d28dd4c0caf0
Thomas Avery <tavery@bitwarden.com>
2023-02-01 11:57:00 -0600

    run dotnet format

--- util/PostgresMigrations/Migrations/20230201175207_SecretsManagerOrganizationUser.cs
+++ util/PostgresMigrations/Migrations/20230201175207_SecretsManagerOrganizationUser.cs
@@ -2,25 +2,24 @@
 
 #nullable disable
 
-namespace Bit.PostgresMigrations.Migrations
-{
-    public partial class SecretsManagerOrganizationUser : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<bool>(
-                name: "AccessSecretsManager",
-                table: "OrganizationUser",
-                type: "boolean",
-                nullable: false,
-                defaultValue: false);
-        }
+namespace Bit.PostgresMigrations.Migrations;
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "AccessSecretsManager",
-                table: "OrganizationUser");
-        }
+public partial class SecretsManagerOrganizationUser : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<bool>(
+            name: "AccessSecretsManager",
+            table: "OrganizationUser",
+            type: "boolean",
+            nullable: false,
+            defaultValue: false);
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "AccessSecretsManager",
+            table: "OrganizationUser");
     }
 }
--- util/SqliteMigrations/Migrations/20230201175211_SecretsManagerOrganizationUser.cs
+++ util/SqliteMigrations/Migrations/20230201175211_SecretsManagerOrganizationUser.cs
@@ -2,25 +2,24 @@
 
 #nullable disable
 
-namespace Bit.SqliteMigrations.Migrations
-{
-    public partial class SecretsManagerOrganizationUser : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<bool>(
-                name: "AccessSecretsManager",
-                table: "OrganizationUser",
-                type: "INTEGER",
-                nullable: false,
-                defaultValue: false);
-        }
+namespace Bit.SqliteMigrations.Migrations;
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "AccessSecretsManager",
-                table: "OrganizationUser");
-        }
+public partial class SecretsManagerOrganizationUser : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<bool>(
+            name: "AccessSecretsManager",
+            table: "OrganizationUser",
+            type: "INTEGER",
+            nullable: false,
+            defaultValue: false);
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "AccessSecretsManager",
+            table: "OrganizationUser");
     }
 }
