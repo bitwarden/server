ea85e216ce47d2c6d7e3fd940610498265a9bba9
Thomas Avery <tavery@bitwarden.com>
2023-02-01 11:53:40 -0600

    Add EF migrations for SecretsManager flag

---
+++ util/SqliteMigrations/Migrations/20230201175211_SecretsManagerOrganizationUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.SqliteMigrations.Migrations
+{
+    public partial class SecretsManagerOrganizationUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AccessSecretsManager",
+                table: "OrganizationUser",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccessSecretsManager",
+                table: "OrganizationUser");
+        }
+    }
+}
