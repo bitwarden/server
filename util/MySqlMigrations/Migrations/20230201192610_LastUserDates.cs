f38a8509cdfdae9afff977f2b1272486556d7e01
Kyle Spearrin <kyle.spearrin@gmail.com>
2023-02-01 16:38:16 -0500

    fix lint problems

--- util/MySqlMigrations/Migrations/20230201192610_LastUserDates.cs
+++ util/MySqlMigrations/Migrations/20230201192610_LastUserDates.cs
@@ -1,56 +1,52 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Bit.MySqlMigrations.Migrations
+public partial class LastUserDates : Migration
 {
-    public partial class LastUserDates : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<DateTime>(
-                name: "LastEmailChangeDate",
-                table: "User",
-                type: "datetime(6)",
-                nullable: true);
+        migrationBuilder.AddColumn<DateTime>(
+            name: "LastEmailChangeDate",
+            table: "User",
+            type: "datetime(6)",
+            nullable: true);
 
-            migrationBuilder.AddColumn<DateTime>(
-                name: "LastKdfChangeDate",
-                table: "User",
-                type: "datetime(6)",
-                nullable: true);
+        migrationBuilder.AddColumn<DateTime>(
+            name: "LastKdfChangeDate",
+            table: "User",
+            type: "datetime(6)",
+            nullable: true);
 
-            migrationBuilder.AddColumn<DateTime>(
-                name: "LastKeyRotationDate",
-                table: "User",
-                type: "datetime(6)",
-                nullable: true);
+        migrationBuilder.AddColumn<DateTime>(
+            name: "LastKeyRotationDate",
+            table: "User",
+            type: "datetime(6)",
+            nullable: true);
 
-            migrationBuilder.AddColumn<DateTime>(
-                name: "LastPasswordChangeDate",
-                table: "User",
-                type: "datetime(6)",
-                nullable: true);
-        }
+        migrationBuilder.AddColumn<DateTime>(
+            name: "LastPasswordChangeDate",
+            table: "User",
+            type: "datetime(6)",
+            nullable: true);
+    }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "LastEmailChangeDate",
-                table: "User");
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "LastEmailChangeDate",
+            table: "User");
 
-            migrationBuilder.DropColumn(
-                name: "LastKdfChangeDate",
-                table: "User");
+        migrationBuilder.DropColumn(
+            name: "LastKdfChangeDate",
+            table: "User");
 
-            migrationBuilder.DropColumn(
-                name: "LastKeyRotationDate",
-                table: "User");
+        migrationBuilder.DropColumn(
+            name: "LastKeyRotationDate",
+            table: "User");
 
-            migrationBuilder.DropColumn(
-                name: "LastPasswordChangeDate",
-                table: "User");
-        }
+        migrationBuilder.DropColumn(
+            name: "LastPasswordChangeDate",
+            table: "User");
     }
 }
