cf669286eda971e060a52b1852ba910df7e34a87
Thomas Avery <43214426+Thomas-Avery@users.noreply.github.com>
2023-02-06 11:26:06 -0600

    [SM-429] Add permission checks to access policy endpoints (#2628)
    * Add permission checks to access policy endpoints
    
    * Fix unit tests
    
    * Add service account grant permission checks
    
    * Add service account grant tests
    
    * Add new endpoint unit tests
    
    * Cleanup unit tests add integration tests
    
    * User permission enum in create tests
    
    * Swap to NotFoundException for access checks
    
    * Add filter for potential grantees
    
    * Add in AccessSecretsManager check and test it
    
    * Add code review updates
    
    * Code review updates
    
    * Refactor potential grantees endpoint
    
    * Code review updates

--- bitwarden_license/src/Commercial.Core/SecretsManager/Commands/AccessPolicies/CreateAccessPoliciesCommand.cs
+++ bitwarden_license/src/Commercial.Core/SecretsManager/Commands/AccessPolicies/CreateAccessPoliciesCommand.cs
@@ -1,4 +1,6 @@
-﻿using Bit.Core.Exceptions;
+﻿using Bit.Core.Context;
+using Bit.Core.Enums;
+using Bit.Core.Exceptions;
 using Bit.Core.SecretsManager.Commands.AccessPolicies.Interfaces;
 using Bit.Core.SecretsManager.Entities;
 using Bit.Core.SecretsManager.Repositories;
@@ -8,22 +10,52 @@ namespace Bit.Commercial.Core.SecretsManager.Commands.AccessPolicies;
 public class CreateAccessPoliciesCommand : ICreateAccessPoliciesCommand
 {
     private readonly IAccessPolicyRepository _accessPolicyRepository;
+    private readonly ICurrentContext _currentContext;
+    private readonly IProjectRepository _projectRepository;
 
-    public CreateAccessPoliciesCommand(IAccessPolicyRepository accessPolicyRepository)
+    public CreateAccessPoliciesCommand(
+        IAccessPolicyRepository accessPolicyRepository,
+        ICurrentContext currentContext,
+        IProjectRepository projectRepository)
     {
+        _projectRepository = projectRepository;
         _accessPolicyRepository = accessPolicyRepository;
+        _currentContext = currentContext;
     }
 
-    public async Task<List<BaseAccessPolicy>> CreateAsync(List<BaseAccessPolicy> accessPolicies)
+    public async Task<IEnumerable<BaseAccessPolicy>> CreateForProjectAsync(Guid projectId,
+        List<BaseAccessPolicy> accessPolicies, Guid userId)
     {
+        var project = await _projectRepository.GetByIdAsync(projectId);
+        if (project == null || !_currentContext.AccessSecretsManager(project.OrganizationId))
+        {
+            throw new NotFoundException();
+        }
+
+        var orgAdmin = await _currentContext.OrganizationAdmin(project.OrganizationId);
+        var accessClient = AccessClientHelper.ToAccessClient(_currentContext.ClientType, orgAdmin);
+
+        var hasAccess = accessClient switch
+        {
+            AccessClientType.NoAccessCheck => true,
+            AccessClientType.User => await _projectRepository.UserHasWriteAccessToProject(project.Id, userId),
+            _ => false,
+        };
+
+        if (!hasAccess)
+        {
+            throw new NotFoundException();
+        }
+
         var distinctAccessPolicies = accessPolicies.DistinctBy(baseAccessPolicy =>
         {
             return baseAccessPolicy switch
             {
                 UserProjectAccessPolicy ap => new Tuple<Guid?, Guid?>(ap.OrganizationUserId, ap.GrantedProjectId),
                 GroupProjectAccessPolicy ap => new Tuple<Guid?, Guid?>(ap.GroupId, ap.GrantedProjectId),
-                ServiceAccountProjectAccessPolicy ap => new Tuple<Guid?, Guid?>(ap.ServiceAccountId, ap.GrantedProjectId),
-                _ => throw new ArgumentException("Unsupported access policy type provided.", nameof(baseAccessPolicy))
+                ServiceAccountProjectAccessPolicy ap => new Tuple<Guid?, Guid?>(ap.ServiceAccountId,
+                    ap.GrantedProjectId),
+                _ => throw new ArgumentException("Unsupported access policy type provided.", nameof(baseAccessPolicy)),
             };
         }).ToList();
 
@@ -39,7 +71,7 @@ public class CreateAccessPoliciesCommand : ICreateAccessPoliciesCommand
                 throw new BadRequestException("Resource already exists");
             }
         }
-
-        return await _accessPolicyRepository.CreateManyAsync(accessPolicies);
+        await _accessPolicyRepository.CreateManyAsync(accessPolicies);
+        return await _accessPolicyRepository.GetManyByGrantedProjectIdAsync(projectId);
     }
 }
