1ee14d93e63f4843f35ca573383ad1de577ce8ba
Thomas Avery <43214426+Thomas-Avery@users.noreply.github.com>
2023-02-07 14:30:22 -0600

    [SM-473] Access Policies - Service Accounts (#2658)
    * Add service account access policy endpoints
    
    * Add unit & integration tests for new endpoints
    
    * Fix formatting on response models
    
    * Cleanup unit tests

--- src/Core/SecretsManager/Commands/AccessPolicies/Interfaces/ICreateAccessPoliciesCommand.cs
+++ src/Core/SecretsManager/Commands/AccessPolicies/Interfaces/ICreateAccessPoliciesCommand.cs
@@ -5,4 +5,5 @@ namespace Bit.Core.SecretsManager.Commands.AccessPolicies.Interfaces;
 public interface ICreateAccessPoliciesCommand
 {
     Task<IEnumerable<BaseAccessPolicy>> CreateForProjectAsync(Guid projectId, List<BaseAccessPolicy> accessPolicies, Guid userId);
+    Task<IEnumerable<BaseAccessPolicy>> CreateForServiceAccountAsync(Guid serviceAccountId, List<BaseAccessPolicy> accessPolicies, Guid userId);
 }
