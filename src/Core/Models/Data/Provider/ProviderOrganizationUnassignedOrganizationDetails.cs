34210276386bedd430387e6a359ee3c25608e00b
Rui Tome <rtome@bitwarden.com>
2023-02-08 16:05:58 +0000

    [EC-432] Added IProviderOrganizationRepository.SearchAsync

---
+++ src/Core/Models/Data/Provider/ProviderOrganizationUnassignedOrganizationDetails.cs
@@ -0,0 +1,14 @@
+﻿using Bit.Core.Enums;
+
+namespace Bit.Core.Models.Data;
+
+public class ProviderOrganizationUnassignedOrganizationDetails
+{
+    public Guid OrganizationId { get; set; }
+
+    public string Name { get; set; }
+
+    public PlanType PlanType { get; set; }
+
+    public string OwnerEmail { get; set; }
+}
--- src/Infrastructure.EntityFramework/Repositories/ProviderOrganizationRepository.cs
+++ src/Infrastructure.EntityFramework/Repositories/ProviderOrganizationRepository.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Bit.Core.Entities.Provider;
+using Bit.Core.Enums;
 using Bit.Core.Models.Data;
 using Bit.Core.Repositories;
 using Bit.Infrastructure.EntityFramework.Repositories.Queries;
@@ -14,6 +15,33 @@ public class ProviderOrganizationRepository :
     public ProviderOrganizationRepository(IServiceScopeFactory serviceScopeFactory, IMapper mapper)
         : base(serviceScopeFactory, mapper, context => context.ProviderOrganizations)
     { }
+
+    public async Task<ICollection<ProviderOrganizationUnassignedOrganizationDetails>> SearchAsync(string name, string ownerEmail, int skip, int take)
+    {
+        using (var scope = ServiceScopeFactory.CreateScope())
+        {
+            var dbContext = GetDatabaseContext(scope);
+            var query = (from o in dbContext.Organizations
+                         join ou in dbContext.OrganizationUsers
+                             on o.Id equals ou.OrganizationId
+                         join u in dbContext.Users
+                             on ou.UserId equals u.Id
+                         where !dbContext.ProviderOrganizations.Any(po => po.OrganizationId == o.Id) &&
+                               ou.Type == OrganizationUserType.Owner &&
+                               (string.IsNullOrWhiteSpace(name) || EF.Functions.Like(o.Name, $"%{name}%")) &&
+                               (string.IsNullOrWhiteSpace(ownerEmail) || u.Email == ownerEmail)
+                         orderby o.CreationDate descending
+                         select new { o, u }).Skip(skip).Take(take).Select(r => new ProviderOrganizationUnassignedOrganizationDetails
+                         {
+                             OrganizationId = r.o.Id,
+                             Name = r.o.Name,
+                             OwnerEmail = r.u.Email,
+                             PlanType = r.o.PlanType
+                         });
+
+            return await query.ToArrayAsync();
+        }
+    }
 
     public async Task<ICollection<ProviderOrganizationOrganizationDetails>> GetManyDetailsByProviderAsync(Guid providerId)
     {
