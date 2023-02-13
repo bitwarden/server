e1b35f65da7e8ec6f7dc269bf94a8e046b90f2bc
Rui Tome <rtome@bitwarden.com>
2023-02-13 15:46:46 +0000

    [EC-432] Renamed migration script and added missing sproc

--- util/Migrator/DbScripts/2023-02-07_00_ProviderAddExistingOrganizations.sql
+++ util/Migrator/DbScripts/2023-02-07_00_ProviderAddExistingOrganizations.sql
@@ -30,3 +30,45 @@ ORDER BY O.[CreationDate] DESC
 OFFSET @Skip ROWS
 FETCH NEXT @Take ROWS ONLY
 END
+GO
+
+CREATE PROCEDURE [dbo].[ProviderOrganization_CreateWithManyOrganizations]
+    @Id UNIQUEIDENTIFIER,
+    @ProviderId UNIQUEIDENTIFIER,
+    @OrganizationId UNIQUEIDENTIFIER,
+    @Key VARCHAR(MAX),
+    @Settings NVARCHAR(MAX),
+    @CreationDate DATETIME2(7),
+    @RevisionDate DATETIME2(7),
+    @OrganizationIds AS [dbo].[GuidIdArray] READONLY
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    -- Insert
+    INSERT INTO
+        [dbo].[ProviderOrganization]
+    SELECT
+        NEWID(),
+        @ProviderId,
+        [Source].[Id],
+        @Key,
+        @Settings,
+        @CreationDate,
+        @RevisionDate
+    FROM
+        @OrganizationIds AS [Source]
+    INNER JOIN
+        [dbo].[Organization] O ON [Source].[Id] = O.[Id]
+WHERE
+    NOT EXISTS (
+    SELECT
+    1
+    FROM
+    [dbo].[ProviderOrganization]
+    WHERE
+    [ProviderId] = @ProviderId
+  AND [OrganizationId] = [Source].[Id]
+    )
+END
+GO
