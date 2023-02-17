515564dd2667bbaebe2ee6abb4f78eb13e2f39ff
Shane Melton <smelton@bitwarden.com>
2023-02-16 17:00:40 -0800

    [EC-1070] Add MasterPasswordPolicyData model

---
+++ src/Core/Models/Data/Organizations/Policies/MasterPasswordPolicyData.cs
@@ -0,0 +1,16 @@
+﻿namespace Bit.Core.Models.Data.Organizations.Policies;
+
+public class MasterPasswordPolicyData : IPolicyDataModel
+{
+    public int MinComplexity { get; set; }
+
+    public int MinLength { get; set; }
+
+    public bool RequireUpper { get; set; }
+
+    public bool RequireNumbers { get; set; }
+
+    public bool RequireSpecial { get; set; }
+
+    public bool EnforceOnLogin { get; set; }
+}
