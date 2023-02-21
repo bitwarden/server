b8c7d50c16280e234886900a556145ea092df1d3
gbubemismith <gsmithwalter@gmail.com>
2023-02-21 12:06:15 -0500

    Added check to detremine if certain docker services are running and if not starts them

--- dev/ef_migrate.ps1
+++ dev/ef_migrate.ps1
@@ -4,6 +4,34 @@ param (
   $Name
 )
 
+# Set docker compose path
+$dockerPath = "docker-compose.yml"
+
+# DB service provider names
+$mysqlService = "mysql"
+$postgresService = "postgres"
+
+# Get container ids
+$mysqlContainerId = docker-compose -f $dockerPath ps -q $mysqlService
+$postgresContainerId = docker-compose -f $dockerPath ps -q $postgresService
+
+# Get status 
+$mysqlStatus = docker container inspect --format "{{.State.Status}}" $mysqlContainerId
+$postgresStatus = docker container inspect --format "{{.State.Status}}" $postgresContainerId
+
+
+if ($mysqlStatus -ne "running") {
+    Write-Output "--- Attempting to start $mysqlService service ---"
+    # Start the container
+    docker container start $mysqlContainerId
+}
+
+if ($postgresStatus -ne "running") {
+    Write-Output "--- Attempting to start $postgresService service ---"
+    # Start the container
+    docker container start $postgresContainerId
+}
+
 dotnet tool restore
 
 $providers = @{
