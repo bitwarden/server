d171f7c936f17ddc84664a889cb6e86424fa3c1e
Rui Tome <rtome@bitwarden.com>
2023-02-21 15:42:51 +0000

    [EC-432] Added DisplayName attributes to ProviderOrganizationViewModel and used them in the view

--- src/Admin/Models/ProviderOrganizationViewModel.cs
+++ src/Admin/Models/ProviderOrganizationViewModel.cs
@@ -1,8 +1,14 @@
-﻿namespace Bit.Admin.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bit.Admin.Models;
 
 public class ProviderOrganizationViewModel : PagedModel<ProviderOrganizationItemViewModel>
 {
     public Guid ProviderId { get; set; }
+
+    [Display(Name = "Organization Name")]
     public string OrganizationName { get; set; }
+
+    [Display(Name = "Owner Email")]
     public string OrganizationOwnerEmail { get; set; }
 }
