eb71329bb906d842e6a15ac65c2ed9f10c7e8d74
Michal Checinski <mchecinski@bitwarden.com>
2023-02-24 14:41:12 +0100

    Add migrator cli

---
+++ util/MigratorCLI/Program.cs
@@ -0,0 +1,67 @@
+﻿using Bit.Migrator;
+using CommandDotNet;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+internal class Program
+{
+    private static int Main(string[] args)
+    {
+        return new AppRunner<Program>().Run(args);
+    }
+
+    [DefaultCommand]
+    public void Execute(
+        [Option('c', Description = "Connection string to the database on which migrations will be executed.")]
+        string databaseConnectionString,
+        [Option('v')]
+        bool verbose) => MigrateDatabase(databaseConnectionString, verbose);
+
+    private static void MigrateDatabase(string databaseConnectionString, bool verbose = false, int attempt = 1)
+    {
+        var logger = CreateLogger();
+
+        try
+        {
+            Console.WriteLine("Migrating database.");
+            var migrator = new DbMigrator(databaseConnectionString, logger);
+            var success = migrator.MigrateMsSqlDatabase(verbose);
+            if (success)
+            {
+                Console.WriteLine("Migration successful.");
+            }
+            else
+            {
+                Console.WriteLine("Migration failed.");
+            }
+        }
+        catch (SqlException e)
+        {
+            if (e.Message.Contains("Server is in script upgrade mode") && attempt < 10)
+            {
+                var nextAttempt = attempt + 1;
+                Console.WriteLine("Database is in script upgrade mode. " +
+                    "Trying again (attempt #{0})...", nextAttempt);
+                Thread.Sleep(20000);
+                MigrateDatabase(databaseConnectionString, verbose, nextAttempt);
+                return;
+            }
+            throw;
+        }
+    }
+
+    private static ILogger<DbMigrator> CreateLogger()
+    {
+        var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder
+                .AddFilter("Microsoft", LogLevel.Warning)
+                .AddFilter("System", LogLevel.Warning)
+                .AddFilter("DbMigrator.DbMigrator", LogLevel.Debug)
+                .AddConsole();
+        });
+        var logger = loggerFactory.CreateLogger<DbMigrator>();
+        return logger;
+    }
+}
