afc5868cc6b1fb272fb24006092105656c2957c6
André Bispo <abispo@bitwarden.com>
2023-02-27 09:32:54 +0000

    [PM-1095][PM-1104] Add new template for title and contact us. Add new template for user organization invite

---
+++ src/Core/Models/Mail/BaseTitleContactUsMailModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Bit.Core.Models.Mail
+{
+    public class BaseTitleContactUsMailModel : BaseMailModel
+    {
+        public string TitleFirst { get; set; }
+        public string TitleSecondBold { get; set; }
+        public string TitleThird { get; set; }
+    }
+}
+
