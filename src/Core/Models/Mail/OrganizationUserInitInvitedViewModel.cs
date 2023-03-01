f4dd70f2d8f1a30562a6fd00f6a17c07c6486012
Rui Tome <rtome@bitwarden.com>
2023-03-01 14:13:22 +0000

    [AC-431] Modified OrganizationUserInitInvitedViewModel to inherit properties from OrganizationUserInvitedViewModel

--- src/Core/Models/Mail/OrganizationUserInitInvitedViewModel.cs
+++ src/Core/Models/Mail/OrganizationUserInitInvitedViewModel.cs
@@ -1,15 +1,8 @@
 ﻿namespace Bit.Core.Models.Mail;
 
-public class OrganizationUserInitInvitedViewModel : BaseMailModel
+public class OrganizationUserInitInvitedViewModel : OrganizationUserInvitedViewModel
 {
-    public string OrganizationName { get; set; }
-    public string OrganizationId { get; set; }
-    public string OrganizationUserId { get; set; }
-    public string Email { get; set; }
-    public string OrganizationNameUrlEncoded { get; set; }
-    public string Token { get; set; }
-    public string ExpirationDate { get; set; }
-    public string Url => string.Format("{0}/accept-init-organization?organizationId={1}&" +
+    public new string Url => string.Format("{0}/accept-init-organization?organizationId={1}&" +
         "organizationUserId={2}&email={3}&organizationName={4}&token={5}",
         WebVaultUrl,
         OrganizationId,
