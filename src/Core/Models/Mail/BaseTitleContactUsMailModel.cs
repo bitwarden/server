61767e5dcf7c00de5f014c34090fbd6a36ca75a5
André Bispo <abispo@bitwarden.com>
2023-03-01 11:12:34 +0000

    Code format

--- src/Core/Models/Mail/BaseTitleContactUsMailModel.cs
+++ src/Core/Models/Mail/BaseTitleContactUsMailModel.cs
@@ -1,11 +1,9 @@
-﻿using System;
-namespace Bit.Core.Models.Mail
+﻿namespace Bit.Core.Models.Mail;
+
+public class BaseTitleContactUsMailModel : BaseMailModel
 {
-    public class BaseTitleContactUsMailModel : BaseMailModel
-    {
-        public string TitleFirst { get; set; }
-        public string TitleSecondBold { get; set; }
-        public string TitleThird { get; set; }
-    }
+    public string TitleFirst { get; set; }
+    public string TitleSecondBold { get; set; }
+    public string TitleThird { get; set; }
 }
 
