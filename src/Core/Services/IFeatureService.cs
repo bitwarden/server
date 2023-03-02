83d0a7ca47282b29920f2737b803c68a571992c6
Matt Bishop <mbishop@bitwarden.com>
2023-03-02 14:29:46 -0500

    Document online method

--- src/Core/Services/IFeatureService.cs
+++ src/Core/Services/IFeatureService.cs
@@ -2,5 +2,9 @@
 
 public interface IFeatureService
 {
+    /// <summary>
+    /// Checks whether online access to feature status is available.
+    /// </summary>
+    /// <returns>True if the service is online, otherwise false.</returns>
     bool IsOnline();
 }
