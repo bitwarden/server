520f14e39e1cf9478ec02d8380591e37012f304c
Matt Bishop <mbishop@bitwarden.com>
2023-03-02 16:19:24 -0500

    Feature accessors with context

--- src/Core/Services/IFeatureService.cs
+++ src/Core/Services/IFeatureService.cs
@@ -1,4 +1,6 @@
-﻿namespace Bit.Core.Services;
+﻿using Bit.Core.Context;
+
+namespace Bit.Core.Services;
 
 public interface IFeatureService
 {
@@ -7,4 +9,31 @@ public interface IFeatureService
     /// </summary>
     /// <returns>True if the service is online, otherwise false.</returns>
     bool IsOnline();
+
+    /// <summary>
+    /// Checks whether a given feature is enabled.
+    /// </summary>
+    /// <param name="key">The key of the feature to check.</param>
+    /// <param name="currentContext">A context providing information that can be used to evaluate whether a feature should be on or off.</param>
+    /// <param name="defaultValue">The default value for the feature.</param>
+    /// <returns>True if the feature is enabled, otherwise false.</returns>
+    bool IsEnabled(string key, ICurrentContext currentContext, bool defaultValue = false);
+
+    /// <summary>
+    /// Gets the integer variation of a feature.
+    /// </summary>
+    /// <param name="key">The key of the feature to check.</param>
+    /// <param name="currentContext">A context providing information that can be used to evaluate the feature value.</param>
+    /// <param name="defaultValue">The default value for the feature.</param>
+    /// <returns>The feature variation value.</returns>
+    int GetIntVariation(string key, ICurrentContext currentContext, int defaultValue = 0);
+
+    /// <summary>
+    /// Gets the string variation of a feature.
+    /// </summary>
+    /// <param name="key">The key of the feature to check.</param>
+    /// <param name="currentContext">A context providing information that can be used to evaluate the feature value.</param>
+    /// <param name="defaultValue">The default value for the feature.</param>
+    /// <returns>The feature variation value.</returns>
+    string GetStringVariation(string key, ICurrentContext currentContext, string defaultValue = null);
 }
