63cba51c08f9108ae195bc367cd3cac90d6f54be
Shane Melton <smelton@bitwarden.com>
2023-03-06 11:17:11 -0800

    [AC-1070] Introduce MasterPasswordPolicyResponseModel

---
+++ src/Core/Models/Api/Response/MasterPasswordPolicyResponseModel.cs
@@ -0,0 +1,33 @@
+﻿using Bit.Core.Models.Data.Organizations.Policies;
+
+namespace Bit.Core.Models.Api.Response;
+
+public class MasterPasswordPolicyResponseModel : ResponseModel
+{
+    public MasterPasswordPolicyResponseModel(MasterPasswordPolicyData data) : base("masterPasswordPolicy")
+    {
+        if (data == null)
+        {
+            return;
+        }
+
+        MinComplexity = data.MinComplexity ?? 0;
+        MinLength = data.MinLength ?? 0;
+        RequireUpper = data.RequireUpper ?? false;
+        RequireNumbers = data.RequireNumbers ?? false;
+        RequireSpecial = data.RequireSpecial ?? false;
+        EnforceOnLogin = data.EnforceOnLogin ?? false;
+    }
+
+    public int MinComplexity { get; set; }
+
+    public int MinLength { get; set; }
+
+    public bool RequireUpper { get; set; }
+
+    public bool RequireNumbers { get; set; }
+
+    public bool RequireSpecial { get; set; }
+
+    public bool EnforceOnLogin { get; set; }
+}
--- src/Core/Models/Data/Organizations/Policies/MasterPasswordPolicyData.cs
+++ src/Core/Models/Data/Organizations/Policies/MasterPasswordPolicyData.cs
@@ -2,15 +2,37 @@
 
 public class MasterPasswordPolicyData : IPolicyDataModel
 {
-    public int MinComplexity { get; set; }
+    public int? MinComplexity { get; set; } = 0;
+    public int? MinLength { get; set; } = 0;
+    public bool? RequireUpper { get; set; } = false;
+    public bool? RequireNumbers { get; set; } = false;
+    public bool? RequireSpecial { get; set; } = false;
+    public bool? EnforceOnLogin { get; set; } = false;
 
-    public int MinLength { get; set; }
+    /// <summary>
+    /// Combine the other policy data with this instance, taking the most secure options
+    /// </summary>
+    /// <param name="other">The other policy instance to combine with this</param>
+    public void CombineWith(MasterPasswordPolicyData other)
+    {
+        if (other == null)
+        {
+            return;
+        }
 
-    public bool RequireUpper { get; set; }
+        if (other.MinComplexity.HasValue && other.MinComplexity > MinComplexity)
+        {
+            MinComplexity = other.MinComplexity;
+        }
 
-    public bool RequireNumbers { get; set; }
+        if (other.MinLength.HasValue && other.MinLength > MinLength)
+        {
+            MinLength = other.MinLength;
+        }
 
-    public bool RequireSpecial { get; set; }
-
-    public bool EnforceOnLogin { get; set; }
+        RequireUpper = (other.RequireUpper ?? false) || (RequireUpper ?? false);
+        RequireNumbers = (other.RequireNumbers ?? false) || (RequireNumbers ?? false);
+        RequireSpecial = (other.RequireSpecial ?? false) || (RequireNumbers ?? false);
+        EnforceOnLogin = (other.EnforceOnLogin ?? false) || (RequireNumbers ?? false);
+    }
 }
