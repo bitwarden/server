a0df350ea30a4c06afd6fe1f955ee4e7ef284e9e
Oscar Hinton <Hinton@users.noreply.github.com>
2023-03-06 20:25:27 +0100

    [SM-568] Delete service accounts (#2748)

---
+++ src/Core/SecretsManager/Commands/ServiceAccounts/Interfaces/IDeleteServiceAccountsCommand.cs
@@ -0,0 +1,9 @@
+﻿using Bit.Core.SecretsManager.Entities;
+
+namespace Bit.Core.SecretsManager.Commands.ServiceAccounts.Interfaces;
+
+public interface IDeleteServiceAccountsCommand
+{
+    Task<List<Tuple<ServiceAccount, string>>> DeleteServiceAccounts(List<Guid> ids, Guid userId);
+}
+
