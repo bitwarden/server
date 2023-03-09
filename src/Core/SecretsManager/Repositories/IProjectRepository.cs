eae4b61acfea5614f68dd53411bd050208ef0d72
Hinton <hinton@users.noreply.github.com>
2023-03-09 17:47:07 +0100

    Change how project permission is resolved

--- src/Core/SecretsManager/Repositories/IProjectRepository.cs
+++ src/Core/SecretsManager/Repositories/IProjectRepository.cs
@@ -1,6 +1,5 @@
 ﻿using Bit.Core.Enums;
 using Bit.Core.SecretsManager.Entities;
-using Bit.Core.SecretsManager.Models.Data;
 
 namespace Bit.Core.SecretsManager.Repositories;
 
@@ -9,7 +8,6 @@ public interface IProjectRepository
     Task<IEnumerable<Project>> GetManyByOrganizationIdAsync(Guid organizationId, Guid userId, AccessClientType accessType);
     Task<IEnumerable<Project>> GetManyByOrganizationIdWriteAccessAsync(Guid organizationId, Guid userId, AccessClientType accessType);
     Task<IEnumerable<Project>> GetManyByIds(IEnumerable<Guid> ids);
-    Task<ProjectPermissionDetails> GetPermissionDetailsByIdAsync(Guid id, Guid userId);
     Task<Project> GetByIdAsync(Guid id);
     Task<Project> CreateAsync(Project project);
     Task ReplaceAsync(Project project);
@@ -19,4 +17,5 @@ public interface IProjectRepository
     Task<bool> UserHasWriteAccessToProject(Guid id, Guid userId);
     Task<bool> ServiceAccountHasWriteAccessToProject(Guid id, Guid userId);
     Task<bool> ServiceAccountHasReadAccessToProject(Guid id, Guid userId);
+    Task<(bool Read, bool Write)> AccessToProjectAsync(Guid id, Guid userId, AccessClientType accessType);
 }
