65a111a5d3cba06cde70c9db81dd1b51553150dc
André Bispo <abispo@bitwarden.com>
2023-03-23 11:37:43 +0000

    [PM-1026] Add constant key to retrieve feature flag from darkly sdk.

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -28,6 +28,7 @@ public static class AuthenticationSchemes
 public static class FeatureFlagKeys
 {
     public const string SecretsManager = "secrets-manager";
+    public const string DisplayEuFlag = "display-eu-environment";
 
     public static List<string> GetAllKeys()
     {
