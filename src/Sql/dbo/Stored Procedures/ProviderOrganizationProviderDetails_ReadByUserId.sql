3026c2685dfc61f53682c375ae0864e38d348ddc
Rui Tome <rtome@bitwarden.com>
2023-03-28 17:14:07 +0100

    [AC-434] Added ProviderOrganizationProviderDetails_ReadByUserId

---
+++ src/Sql/dbo/Stored Procedures/ProviderOrganizationProviderDetails_ReadByUserId.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[ProviderOrganizationProviderDetails_ReadByUserId]
+    @UserId UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON
+
+SELECT
+    PO.Id,
+    PO.OrganizationId,
+    PO.ProviderId,
+    P.Name as ProviderName,
+    P.[Type] as ProviderType
+FROM
+    [dbo].[ProviderOrganizationView] PO
+    INNER JOIN
+        [dbo].[Organization] O ON PO.OrganizationId = O.Id
+    INNER JOIN
+        [dbo].[OrganizationUser] OU ON PO.OrganizationId = OU.OrganizationId
+    INNER JOIN
+        [dbo].[Provider] P ON PO.ProviderId = P.Id
+WHERE
+    OU.UserId = @UserId
+END
--- util/Migrator/DbScripts/2023-03-08_OrganizationProviderType.sql
+++ util/Migrator/DbScripts/2023-03-08_OrganizationProviderType.sql
@@ -57,3 +57,28 @@ LEFT JOIN
 LEFT JOIN
     [dbo].[OrganizationSponsorship] OS ON OS.[SponsoringOrganizationUserID] = OU.[Id]
 GO
+
+CREATE PROCEDURE [dbo].[ProviderOrganizationProviderDetails_ReadByUserId]
+    @UserId UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON
+
+SELECT
+    PO.Id,
+    PO.OrganizationId,
+    PO.ProviderId,
+    P.Name as ProviderName,
+    P.[Type] as ProviderType
+FROM
+    [dbo].[ProviderOrganizationView] PO
+INNER JOIN
+    [dbo].[Organization] O ON PO.OrganizationId = O.Id
+INNER JOIN
+    [dbo].[OrganizationUser] OU ON PO.OrganizationId = OU.OrganizationId
+INNER JOIN
+    [dbo].[Provider] P ON PO.ProviderId = P.Id
+WHERE
+    OU.UserId = @UserId
+END
+GO
