88df26d670832e584dc96450ed819756130029ff
Rui Tome <rtome@bitwarden.com>
2023-04-03 15:50:21 +0100

    [AC-1214] Saving Group ExternalId

--- src/Api/Models/Request/GroupRequestModel.cs
+++ src/Api/Models/Request/GroupRequestModel.cs
@@ -10,6 +10,8 @@ public class GroupRequestModel
     public string Name { get; set; }
     [Required]
     public bool? AccessAll { get; set; }
+    [StringLength(300)]
+    public string ExternalId { get; set; }
     public IEnumerable<SelectionReadOnlyRequestModel> Collections { get; set; }
     public IEnumerable<Guid> Users { get; set; }
 
@@ -25,6 +27,7 @@ public class GroupRequestModel
     {
         existingGroup.Name = Name;
         existingGroup.AccessAll = AccessAll.Value;
+        existingGroup.ExternalId = ExternalId;
         return existingGroup;
     }
 }
--- test/Api.Test/Controllers/GroupsControllerTests.cs
+++ test/Api.Test/Controllers/GroupsControllerTests.cs
@@ -29,7 +29,7 @@ public class GroupsControllerTests
         await sutProvider.GetDependency<ICreateGroupCommand>().Received(1).CreateGroupAsync(
             Arg.Is<Group>(g =>
                 g.OrganizationId == organization.Id && g.Name == groupRequestModel.Name &&
-                g.AccessAll == groupRequestModel.AccessAll),
+                g.AccessAll == groupRequestModel.AccessAll && g.ExternalId == groupRequestModel.ExternalId),
             organization,
             Arg.Any<IEnumerable<CollectionAccessSelection>>(),
             Arg.Any<IEnumerable<Guid>>());
@@ -38,6 +38,7 @@ public class GroupsControllerTests
         Assert.Equal(groupRequestModel.Name, response.Name);
         Assert.Equal(organization.Id.ToString(), response.OrganizationId);
         Assert.Equal(groupRequestModel.AccessAll, response.AccessAll);
+        Assert.Equal(groupRequestModel.ExternalId, response.ExternalId);
     }
 
     [Theory]
@@ -56,7 +57,7 @@ public class GroupsControllerTests
         await sutProvider.GetDependency<IUpdateGroupCommand>().Received(1).UpdateGroupAsync(
             Arg.Is<Group>(g =>
                 g.OrganizationId == organization.Id && g.Name == groupRequestModel.Name &&
-                g.AccessAll == groupRequestModel.AccessAll),
+                g.AccessAll == groupRequestModel.AccessAll && g.ExternalId == groupRequestModel.ExternalId),
             Arg.Is<Organization>(o => o.Id == organization.Id),
             Arg.Any<IEnumerable<CollectionAccessSelection>>(),
             Arg.Any<IEnumerable<Guid>>());
@@ -65,5 +66,6 @@ public class GroupsControllerTests
         Assert.Equal(groupRequestModel.Name, response.Name);
         Assert.Equal(organization.Id.ToString(), response.OrganizationId);
         Assert.Equal(groupRequestModel.AccessAll, response.AccessAll);
+        Assert.Equal(groupRequestModel.ExternalId, response.ExternalId);
     }
 }
