9c365060dbca0ff719e412ccc1e580d73d761619
Daniel James Smith <djsmith@web.de>
2023-04-16 17:08:34 +0200

    Move HibpController (reports) to tools

--- src/Api/Tools/Controllers/HibpController.cs
+++ src/Api/Tools/Controllers/HibpController.cs
@@ -8,7 +8,7 @@ using Bit.Core.Utilities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Bit.Api.Controllers;
+namespace Bit.Api.Tools.Controllers;
 
 [Route("hibp")]
 [Authorize("Application")]
