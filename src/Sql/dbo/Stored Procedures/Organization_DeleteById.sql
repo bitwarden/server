7095a43f1ce400d14769a5063c646efc14136d57
Colton Hurst <colton@coltonhurst.com>
2023-04-20 18:05:13 -0400

    SM-718: Delete appropriate Access Policies on Org deletion

--- src/Sql/dbo/Stored Procedures/Organization_DeleteById.sql
+++ src/Sql/dbo/Stored Procedures/Organization_DeleteById.sql
@@ -45,6 +45,14 @@ BEGIN
     WHERE 
         [OU].[OrganizationId] = @Id
 
+    DELETE AP
+    FROM
+        [dbo].[AccessPolicy] AP
+    INNER JOIN
+        [dbo].[OrganizationUser] OU ON [AP].[OrganizationUserId] = [OU].[Id]
+    WHERE
+        [OU].[OrganizationId] = @Id
+
     DELETE
     FROM 
         [dbo].[OrganizationUser]
@@ -79,6 +87,14 @@ BEGIN
         [dbo].[ApiKey] AK
     INNER JOIN
         [dbo].[ServiceAccount] SA ON [AK].[ServiceAccountId] = [SA].[Id]
+    WHERE
+        [SA].[OrganizationId] = @Id
+
+    DELETE AP
+    FROM
+        [dbo].[AccessPolicy] AP
+    INNER JOIN
+        [dbo].[ServiceAccount] SA ON [AP].[GrantedServiceAccountId] = [SA].[Id]
     WHERE
         [SA].[OrganizationId] = @Id
 
