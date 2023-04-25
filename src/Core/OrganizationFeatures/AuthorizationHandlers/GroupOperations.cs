e1468c24e32013eea138dd198270cc05b18db459
Thomas Rittson <trittson@bitwarden.com>
2023-04-25 14:27:06 +1000

    Remove unnecessary comment

--- src/Core/OrganizationFeatures/AuthorizationHandlers/GroupOperations.cs
+++ src/Core/OrganizationFeatures/AuthorizationHandlers/GroupOperations.cs
@@ -6,7 +6,6 @@ public class GroupOperationRequirement : OperationAuthorizationRequirement { }
 
 public static class GroupOperations
 {
-    // Operations on the Group object itself
     public static readonly GroupOperationRequirement Create = new() { Name = nameof(Create) };
     public static readonly GroupOperationRequirement Read = new() { Name = nameof(Read) };
     public static readonly GroupOperationRequirement Update = new() { Name = nameof(Update) };
