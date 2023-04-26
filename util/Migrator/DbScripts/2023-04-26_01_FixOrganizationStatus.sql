dd76d3f9ccf3b5706f8b44f228bb852fbb8a1534
Thomas Rittson <trittson@bitwarden.com>
2023-04-27 09:51:53 +1000

    Rewrite migration script to use positive requirements for Created state

--- util/Migrator/DbScripts/2023-04-26_01_FixOrganizationStatus.sql
+++ util/Migrator/DbScripts/2023-04-26_01_FixOrganizationStatus.sql
@@ -1,23 +1,18 @@
--- Update the status of organizations from Pending to Created if they don't meet the criteria for Pending
-UPDATE [dbo].[Organization]
-SET [Status] = 1 -- Created
-WHERE [Status] = 0 -- Pending
-    AND [Id] NOT IN (
-    -- Get all OrganizationIds where the Organization is
-    -- * associated with a Reseller, and
-    -- * doesn't have any confirmed users
-    -- (this is the criteria for being in the Pending state)
-        SELECT
-            po.[OrganizationId]
+-- Update the [Status] of organizations from Pending (0) to Created (1) if they have at least 1 confirmed user
+-- This is to fix an issue where the default value of 0 (Pending) was accidentally written back to the database
+-- for existing organizations
+UPDATE
+    [dbo].[Organization]
+SET
+    [Status] = 1 -- Created
+WHERE 
+    [Status] = 0 -- Pending
+    AND [Id] IN (
+        SELECT DISTINCT 
+            ou.[OrganizationId]
         FROM 
-            [dbo].[ProviderOrganization] po
-        INNER JOIN 
-            [dbo].[Provider] p ON po.[ProviderId] = p.[Id]
+            [dbo].OrganizationUser ou
         WHERE 
-            p.[Type] = 1 -- Reseller
-            AND po.[OrganizationId] NOT IN (
-                SELECT ou.[OrganizationId]
-                FROM [dbo].OrganizationUser ou
-                WHERE ou.[Status] > 0
-            )
+            ou.[Status] = 2   -- confirmed
     )
+GO
