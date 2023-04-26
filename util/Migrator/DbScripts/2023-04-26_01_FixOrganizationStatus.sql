91eb7c729c7558b21bb3727b95588c3bed0a2b99
Thomas Rittson <trittson@bitwarden.com>
2023-04-27 09:21:17 +1000

    Add comments

--- util/Migrator/DbScripts/2023-04-26_01_FixOrganizationStatus.sql
+++ util/Migrator/DbScripts/2023-04-26_01_FixOrganizationStatus.sql
@@ -2,15 +2,22 @@
 UPDATE [dbo].[Organization]
 SET [Status] = 1 -- Created
 WHERE [Status] = 0 -- Pending
-  AND [Id] NOT IN (
-    SELECT po.[OrganizationId]
-    FROM [dbo].[ProviderOrganization] po
-    INNER JOIN [dbo].[Provider] p
-        ON po.[ProviderId] = p.[Id]
-    WHERE p.[Type] = 1 -- Reseller
-        AND po.[OrganizationId] NOT IN (
-            SELECT ou.[OrganizationId]
-            FROM [dbo].OrganizationUser ou
-            WHERE ou.[Status] > 0
-        )
-  )
+    AND [Id] NOT IN (
+    -- Get all OrganizationIds where the Organization is
+    -- * associated with a Reseller, and
+    -- * doesn't have any confirmed users
+    -- (this is the criteria for being in the Pending state)
+        SELECT
+            po.[OrganizationId]
+        FROM 
+            [dbo].[ProviderOrganization] po
+        INNER JOIN 
+            [dbo].[Provider] p ON po.[ProviderId] = p.[Id]
+        WHERE 
+            p.[Type] = 1 -- Reseller
+            AND po.[OrganizationId] NOT IN (
+                SELECT ou.[OrganizationId]
+                FROM [dbo].OrganizationUser ou
+                WHERE ou.[Status] > 0
+            )
+    )
