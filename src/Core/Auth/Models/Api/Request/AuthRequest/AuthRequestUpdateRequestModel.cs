9588110f64c9740e29adc23f0dfc031da4a2f721
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2023-04-28 16:57:49 -0400

    Refactor AuthRequest Logic into Service

---
+++ src/Core/Auth/Models/Api/Request/AuthRequest/AuthRequestUpdateRequestModel.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Bit.Core.Auth.Models.Api.Request.AuthRequest;
+
+public class AuthRequestUpdateRequestModel
+{
+    public string Key { get; set; }
+    public string MasterPasswordHash { get; set; }
+    [Required]
+    public string DeviceIdentifier { get; set; }
+    [Required]
+    public bool RequestApproved { get; set; }
+}
