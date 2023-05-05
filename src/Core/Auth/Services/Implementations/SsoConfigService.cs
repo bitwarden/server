cf3fcf9e95c4e95ec47fcf78f10df0629ceeb4e7
Shane Melton <smelton@bitwarden.com>
2023-05-05 10:43:46 -0700

    [AC-1145] Formatting

--- src/Core/Auth/Services/Implementations/SsoConfigService.cs
+++ src/Core/Auth/Services/Implementations/SsoConfigService.cs
@@ -1,7 +1,6 @@
 ﻿using Bit.Core.Auth.Entities;
 using Bit.Core.Auth.Enums;
 using Bit.Core.Auth.Repositories;
-using Bit.Core.Context;
 using Bit.Core.Entities;
 using Bit.Core.Enums;
 using Bit.Core.Exceptions;
