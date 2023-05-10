620a7e0a8d6ab35a383501d18f8961e52f7364c4
Shane Melton <smelton@bitwarden.com>
2023-05-10 12:52:08 -0700

    [AC-1145] Add trusted devices option to SSO Config Data (#2909)
    * [AC-1145] Add TDE feature flag
    
    * [AC-1145] Update .gitignore to ignore flags.json in the Api project
    
    * [AC-1145] Introduce MemberDecryptionType property on SsoConfigurationData
    
    * [AC-1145] Add MemberDecryptionType to the SsoConfigurationDataRequest model
    
    * [AC-1145] Automatically enable password reset policy on TDE selection
    
    * [AC-1145] Remove references to obsolete KeyConnectorEnabled field
    
    * [AC-1145] Formatting
    
    * [AC-1145] Update XML doc reference to MemberDecryptionType

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -30,6 +30,7 @@ public static class FeatureFlagKeys
     public const string SecretsManager = "secrets-manager";
     public const string DisplayEuEnvironment = "display-eu-environment";
     public const string DisplayLowKdfIterationWarning = "display-kdf-iteration-warning";
+    public const string TrustedDeviceEncryption = "trusted-device-encryption";
 
     public static List<string> GetAllKeys()
     {
--- src/Core/Services/Implementations/PolicyService.cs
+++ src/Core/Services/Implementations/PolicyService.cs
@@ -1,4 +1,5 @@
-﻿using Bit.Core.Auth.Repositories;
+﻿using Bit.Core.Auth.Enums;
+using Bit.Core.Auth.Repositories;
 using Bit.Core.Entities;
 using Bit.Core.Enums;
 using Bit.Core.Exceptions;
@@ -185,7 +186,7 @@ public class PolicyService : IPolicyService
     {
 
         var ssoConfig = await _ssoConfigRepository.GetByOrganizationIdAsync(org.Id);
-        if (ssoConfig?.GetData()?.KeyConnectorEnabled == true)
+        if (ssoConfig?.GetData()?.MemberDecryptionType == MemberDecryptionType.KeyConnector)
         {
             throw new BadRequestException("Key Connector is enabled.");
         }
