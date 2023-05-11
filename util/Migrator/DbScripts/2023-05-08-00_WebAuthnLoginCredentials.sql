85db96c75e4228857375fd7c11e22896a665c3a0
Andreas Coroiu <andreas.coroiu@gmail.com>
2023-05-11 14:13:45 +0200

    [PM-2014] fix: add missing sql scripts
    Missed staging them when commiting

--- util/Migrator/DbScripts/2023-05-08-00_WebAuthnLoginCredentials.sql
+++ util/Migrator/DbScripts/2023-05-08-00_WebAuthnLoginCredentials.sql
@@ -150,3 +150,21 @@ BEGIN
     WHERE
         [Id] = @Id
 END
+
+GO
+CREATE PROCEDURE [dbo].[WebAuthnCredential_ReadByIdUserId]
+    @Id UNIQUEIDENTIFIER,
+    @UserId UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    SELECT
+        *
+    FROM
+        [dbo].[WebAuthnCredentialView]
+    WHERE
+        [Id] = @Id
+    AND
+        [UserId] = @UserId
+END
