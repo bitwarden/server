99b0953acdbc36d52daedc58f335085b084f33c0
Matt Bishop <mbishop@bitwarden.com>
2023-05-11 10:27:58 -0400

    Remove Secrets Manager LaunchDarkly feature flag (#2925)

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -27,7 +27,6 @@ public static class AuthenticationSchemes
 
 public static class FeatureFlagKeys
 {
-    public const string SecretsManager = "secrets-manager";
     public const string DisplayEuEnvironment = "display-eu-environment";
     public const string DisplayLowKdfIterationWarning = "display-kdf-iteration-warning";
     public const string TrustedDeviceEncryption = "trusted-device-encryption";
