75edd13ee8fed3b77f86a5755738c51eaaa6394c
Andreas Coroiu <andreas.coroiu@gmail.com>
2023-05-15 13:53:13 +0200

    [PM-2014] feat: add support for feature flag

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -30,6 +30,7 @@ public static class FeatureFlagKeys
     public const string SecretsManager = "secrets-manager";
     public const string DisplayEuEnvironment = "display-eu-environment";
     public const string DisplayLowKdfIterationWarning = "display-kdf-iteration-warning";
+    public const string PasswordlessLogin = "passwordless-login";
 
     public static List<string> GetAllKeys()
     {
