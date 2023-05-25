33dacc0efb3154e961f1c2345f6713ad3ba2ada6
Shane Melton <smelton@bitwarden.com>
2023-05-25 14:52:45 -0700

    [AC-1192] Create new OrganizationAuthRequestsController.cs

---
+++ src/Api/Controllers/OrganizationAuthRequestsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bit.Api.Controllers;
+
+[Route("organizations/{orgId}/auth-requests")]
+[Authorize("Application")]
+public class OrganizationAuthRequestsController : Controller
+{
+}
