eda8ce97899263b359466e0a25d443d197e0946b
Shane Melton <smelton@bitwarden.com>
2023-05-30 15:20:03 -0700

    [AC-1192] Introduce OrganizationAdminAuthRequest model

---
+++ src/Core/Auth/Models/Data/OrganizationAdminAuthRequest.cs
@@ -0,0 +1,20 @@
+﻿using Bit.Core.Auth.Entities;
+using Bit.Core.Auth.Enums;
+
+namespace Bit.Core.Auth.Models.Data;
+
+/// <summary>
+/// Represents an <see cref="AuthRequestType.AdminApproval"/> AuthRequest.
+/// Includes additional user and organization information.
+/// </summary>
+public class OrganizationAdminAuthRequest : AuthRequest
+{
+    /// <summary>
+    /// Email address of the requesting user
+    /// </summary>
+    public string Email { get; set; }
+
+    public Guid OrganizationUserId { get; set; }
+
+    public Guid OrganizationId { get; set; }
+}
