7e9a68f4cd0069d52cbac3990f4e4eed8ee08138
cyprain-okeke <cokeke@bitwarden.com>
2023-05-30 15:27:43 +0100

    Change the namespaces

--- util/PostgresMigrations/Migrations/20230530114251_AddSecretsManagerBillingFieldToOrganization.cs
+++ util/PostgresMigrations/Migrations/20230530114251_AddSecretsManagerBillingFieldToOrganization.cs
@@ -2,65 +2,65 @@
 
 #nullable disable
 
-namespace Bit.PostgresMigrations.Migrations
+namespace Bit.PostgresMigrations.Migrations;
+
+public partial class AddSecretsManagerBillingFieldToOrganization : Migration
 {
-    public partial class AddSecretsManagerBillingFieldToOrganization : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<int>(
-                name: "MaxAutoscaleSmSeats",
-                table: "Organization",
-                type: "integer",
-                nullable: true);
+        migrationBuilder.AddColumn<int>(
+            name: "MaxAutoscaleSmSeats",
+            table: "Organization",
+            type: "integer",
+            nullable: true);
 
-            migrationBuilder.AddColumn<int>(
-                name: "MaxAutoscaleSmServiceAccounts",
-                table: "Organization",
-                type: "integer",
-                nullable: true);
+        migrationBuilder.AddColumn<int>(
+            name: "MaxAutoscaleSmServiceAccounts",
+            table: "Organization",
+            type: "integer",
+            nullable: true);
 
-            migrationBuilder.AddColumn<int>(
-                name: "SmSeats",
-                table: "Organization",
-                type: "integer",
-                nullable: true);
+        migrationBuilder.AddColumn<int>(
+            name: "SmSeats",
+            table: "Organization",
+            type: "integer",
+            nullable: true);
 
-            migrationBuilder.AddColumn<int>(
-                name: "SmServiceAccounts",
-                table: "Organization",
-                type: "integer",
-                nullable: true);
+        migrationBuilder.AddColumn<int>(
+            name: "SmServiceAccounts",
+            table: "Organization",
+            type: "integer",
+            nullable: true);
 
-            migrationBuilder.AddColumn<bool>(
-                name: "UsePasswordManager",
-                table: "Organization",
-                type: "boolean",
-                nullable: false,
-                defaultValue: false);
-        }
+        migrationBuilder.AddColumn<bool>(
+            name: "UsePasswordManager",
+            table: "Organization",
+            type: "boolean",
+            nullable: false,
+            defaultValue: false);
+    }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "MaxAutoscaleSmSeats",
-                table: "Organization");
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "MaxAutoscaleSmSeats",
+            table: "Organization");
 
-            migrationBuilder.DropColumn(
-                name: "MaxAutoscaleSmServiceAccounts",
-                table: "Organization");
+        migrationBuilder.DropColumn(
+            name: "MaxAutoscaleSmServiceAccounts",
+            table: "Organization");
 
-            migrationBuilder.DropColumn(
-                name: "SmSeats",
-                table: "Organization");
+        migrationBuilder.DropColumn(
+            name: "SmSeats",
+            table: "Organization");
 
-            migrationBuilder.DropColumn(
-                name: "SmServiceAccounts",
-                table: "Organization");
+        migrationBuilder.DropColumn(
+            name: "SmServiceAccounts",
+            table: "Organization");
 
-            migrationBuilder.DropColumn(
-                name: "UsePasswordManager",
-                table: "Organization");
-        }
+        migrationBuilder.DropColumn(
+            name: "UsePasswordManager",
+            table: "Organization");
     }
 }
+
