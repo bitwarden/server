ab3410772bdec4604edc22f19212858480f4c9a1
Todd Martin <tmartin@bitwarden.com>
2023-06-01 10:50:35 -0400

    Added additional permission.

--- src/Admin/Utilities/RolePermissionMapping.cs
+++ src/Admin/Utilities/RolePermissionMapping.cs
@@ -94,6 +94,7 @@ public static class RolePermissionMapping
                 Permission.Tools_GenerateLicenseFile,
                 Permission.Tools_ManageTaxRates,
                 Permission.Tools_ManageStripeSubscriptions,
+                Permission.Tools_CreateEditTransaction,
                 Permission.Logs_View
             }
         },
