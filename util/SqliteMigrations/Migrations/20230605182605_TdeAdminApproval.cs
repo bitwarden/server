0ad206a7cd92a3d756afa73d87658e550b193e92
Shane Melton <smelton@bitwarden.com>
2023-06-05 13:44:16 -0700

    [AC-1192] Format migration scripts

--- util/SqliteMigrations/Migrations/20230605182605_TdeAdminApproval.cs
+++ util/SqliteMigrations/Migrations/20230605182605_TdeAdminApproval.cs
@@ -1,46 +1,44 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Bit.SqliteMigrations.Migrations
+namespace Bit.SqliteMigrations.Migrations;
+
+public partial class TdeAdminApproval : Migration
 {
-    public partial class TdeAdminApproval : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<Guid>(
-                name: "OrganizationId",
-                table: "AuthRequest",
-                type: "TEXT",
-                nullable: true);
+        migrationBuilder.AddColumn<Guid>(
+            name: "OrganizationId",
+            table: "AuthRequest",
+            type: "TEXT",
+            nullable: true);
 
-            migrationBuilder.CreateIndex(
-                name: "IX_AuthRequest_OrganizationId",
-                table: "AuthRequest",
-                column: "OrganizationId");
+        migrationBuilder.CreateIndex(
+            name: "IX_AuthRequest_OrganizationId",
+            table: "AuthRequest",
+            column: "OrganizationId");
 
-            migrationBuilder.AddForeignKey(
-                name: "FK_AuthRequest_Organization_OrganizationId",
-                table: "AuthRequest",
-                column: "OrganizationId",
-                principalTable: "Organization",
-                principalColumn: "Id");
-        }
+        migrationBuilder.AddForeignKey(
+            name: "FK_AuthRequest_Organization_OrganizationId",
+            table: "AuthRequest",
+            column: "OrganizationId",
+            principalTable: "Organization",
+            principalColumn: "Id");
+    }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropForeignKey(
-                name: "FK_AuthRequest_Organization_OrganizationId",
-                table: "AuthRequest");
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropForeignKey(
+            name: "FK_AuthRequest_Organization_OrganizationId",
+            table: "AuthRequest");
 
-            migrationBuilder.DropIndex(
-                name: "IX_AuthRequest_OrganizationId",
-                table: "AuthRequest");
+        migrationBuilder.DropIndex(
+            name: "IX_AuthRequest_OrganizationId",
+            table: "AuthRequest");
 
-            migrationBuilder.DropColumn(
-                name: "OrganizationId",
-                table: "AuthRequest");
-        }
+        migrationBuilder.DropColumn(
+            name: "OrganizationId",
+            table: "AuthRequest");
     }
 }
