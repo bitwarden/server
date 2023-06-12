5303a3736a4aea5eb0e46b664be3b6d70d70d249
cyprain-okeke <cokeke@bitwarden.com>
2023-06-12 18:22:22 +0100

    Add unit test for signup

--- test/Core.Test/OrganizationFeatures/OrganizationSignUp/OrganizationSignUpCommandTests.cs
+++ test/Core.Test/OrganizationFeatures/OrganizationSignUp/OrganizationSignUpCommandTests.cs
@@ -1,5 +1,7 @@
-﻿using Bit.Core.Context;
+﻿using AutoFixture;
+using Bit.Core.Context;
 using Bit.Core.Entities;
+using Bit.Core.Enums;
 using Bit.Core.Models.Business;
 using Bit.Core.Models.StaticStore;
 using Bit.Core.OrganizationFeatures.OrganizationSignUp;
@@ -7,6 +9,7 @@ using Bit.Core.OrganizationFeatures.OrganizationSignUp.Interfaces;
 using Bit.Core.Repositories;
 using Bit.Core.Services;
 using Bit.Core.Tools.Services;
+using Bit.Test.Common.AutoFixture.Attributes;
 using NSubstitute;
 using Xunit;
 
@@ -14,37 +17,132 @@ namespace Bit.Core.Test.OrganizationFeatures.OrganizationSignUp;
 
 public class OrganizationSignUpCommandTests
 {
-    [Fact]
-    public async Task SignUpAsync_Should_ReturnOrganizationAndUser()
+    [Theory]
+    [BitAutoData]
+    public async Task SignUpAsync_WhenValidSignupAndFeatureFlagOff_ReturnsOrganizationAndOrganizationUser(
+        OrganizationSignup signup,
+        bool provider)
     {
         // Arrange
-        var organizationService = Substitute.For<IOrganizationService>();
+        var fixture = new Fixture();
         var paymentService = Substitute.For<IPaymentService>();
-        var referenceEventService = Substitute.For<IReferenceEventService>();
+        var currentContext = Substitute.For<ICurrentContext>();
+        var organizationService = Substitute.For<IOrganizationService>();
         var featureService = Substitute.For<IFeatureService>();
         var policyService = Substitute.For<IPolicyService>();
-        var currentContext = Substitute.For<ICurrentContext>();
+        var referenceEventService = Substitute.For<IReferenceEventService>();
         var organizationUserRepository = Substitute.For<IOrganizationUserRepository>();
         var organizationSignUpValidationStrategy = Substitute.For<IOrganizationSignUpValidationStrategy>();
-    
-        var sut = new OrganizationSignUpCommand(paymentService, currentContext, organizationService, featureService
-            , policyService, referenceEventService, organizationUserRepository, organizationSignUpValidationStrategy);
-    
-        var signup = new OrganizationSignup { /* provide necessary properties */ };
-        var provider = false;
-        var passwordManagerPlan = new Plan { /* provide necessary properties */ };
-        var secretsManagerPlan = new Plan { /* provide necessary properties */ };
-        var plans = new List<Plan> { passwordManagerPlan, secretsManagerPlan };
-        var organization = new Organization { /* provide necessary properties */ };
-        var organizationUser = new OrganizationUser { /* provide necessary properties */ };
-    
-        organizationService.SignUpAsync(Arg.Any<Organization>(), Arg.Any<Guid>(), Arg.Any<string>(), Arg.Any<string>(), Arg.Any<bool>())
-            .Returns(Task.FromResult(Tuple.Create(organization, organizationUser)));
-    
+
+        var sut = new OrganizationSignUpCommand(
+            paymentService,
+            currentContext,
+            organizationService,
+            featureService,
+            policyService,
+            referenceEventService,
+            organizationUserRepository,
+            organizationSignUpValidationStrategy
+        );
+        
+        var plans = fixture.Create<List<Plan>>();
+        
+        var passwordManagerPlan = fixture.Create<Organization>();
+        var organization = fixture.Create<Organization>();
+        var organizationUser = fixture.Create<OrganizationUser>();
+
+        featureService.IsEnabled("sm-ga-billing", Arg.Any<CurrentContext>()).Returns(true);
+        
+        policyService.AnyPoliciesApplicableToUserAsync(signup.Owner.Id, PolicyType.SingleOrg).Returns(false);
+        organizationUserRepository.GetCountByFreeOrganizationAdminUserAsync(signup.Owner.Id).Returns(0);
+        organizationService.SignUpAsync(Arg.Any<Organization>(), Arg.Any<Guid>(), Arg.Any<string>(), Arg.Any<string>(), true)
+            .Returns(new Tuple<Organization, OrganizationUser>(organization, organizationUser));
+
+        fixture.Inject(plans);
+        fixture.Inject(passwordManagerPlan);
+        fixture.Inject(organization);
+        fixture.Inject(organizationUser);
+        fixture.Inject(featureService);
+        fixture.Inject(policyService);
+        fixture.Inject(organizationService);
+        fixture.Inject(paymentService);
+        fixture.Inject(referenceEventService);
+        fixture.Inject(organizationUserRepository);
+        fixture.Inject(organizationSignUpValidationStrategy);
+
         // Act
+        signup.AdditionalStorageGb = 0;
+        signup.AdditionalSeats = 0;
         var result = await sut.SignUpAsync(signup, provider);
-    
+
         // Assert
+        Assert.NotNull(result);
+        Assert.Equal(organization, result.Item1);
+        Assert.Equal(organizationUser, result.Item2);
+    }
+    
+    [Theory]
+    [BitAutoData]
+    public async Task SignUpAsync_WhenValidSignupAndFeatureFlagOn_ReturnsOrganizationAndOrganizationUser(
+        OrganizationSignup signup,
+        bool provider)
+    {
+        // Arrange
+        var fixture = new Fixture();
+        var paymentService = Substitute.For<IPaymentService>();
+        var currentContext = Substitute.For<ICurrentContext>();
+        var organizationService = Substitute.For<IOrganizationService>();
+        var featureService = Substitute.For<IFeatureService>();
+        var policyService = Substitute.For<IPolicyService>();
+        var referenceEventService = Substitute.For<IReferenceEventService>();
+        var organizationUserRepository = Substitute.For<IOrganizationUserRepository>();
+        var organizationSignUpValidationStrategy = Substitute.For<IOrganizationSignUpValidationStrategy>();
+
+        var sut = new OrganizationSignUpCommand(
+            paymentService,
+            currentContext,
+            organizationService,
+            featureService,
+            policyService,
+            referenceEventService,
+            organizationUserRepository,
+            organizationSignUpValidationStrategy
+        );
+        
+        var plans = fixture.Create<List<Plan>>();
+        
+        var passwordManagerPlan = fixture.Create<Organization>();
+        var organization = fixture.Create<Organization>();
+        var organizationUser = fixture.Create<OrganizationUser>();
+
+        featureService.IsEnabled("SecretManagerGaBilling", Arg.Any<CurrentContext>()).Returns(true);
+        policyService.AnyPoliciesApplicableToUserAsync(signup.Owner.Id, PolicyType.SingleOrg).Returns(false);
+        organizationUserRepository.GetCountByFreeOrganizationAdminUserAsync(signup.Owner.Id).Returns(0);
+        organizationService.SignUpAsync(Arg.Any<Organization>(), Arg.Any<Guid>(), Arg.Any<string>(), Arg.Any<string>(), true)
+            .Returns(new Tuple<Organization, OrganizationUser>(organization, organizationUser));
+
+        fixture.Inject(plans);
+        fixture.Inject(passwordManagerPlan);
+        fixture.Inject(organization);
+        fixture.Inject(organizationUser);
+        fixture.Inject(featureService);
+        fixture.Inject(policyService);
+        fixture.Inject(organizationService);
+        fixture.Inject(paymentService);
+        fixture.Inject(referenceEventService);
+        fixture.Inject(organizationUserRepository);
+        fixture.Inject(organizationSignUpValidationStrategy);
+
+        // Act
+        signup.AdditionalStorageGb = 0;
+        signup.AdditionalSeats = 0;
+        signup.UseSecretsManager = true;
+        signup.AdditionalServiceAccount = 0;
+        signup.AdditionalSmSeats = 0;
+        var result = await sut.SignUpAsync(signup, provider);
+
+        // Assert
+        Assert.NotNull(result);
         Assert.Equal(organization, result.Item1);
         Assert.Equal(organizationUser, result.Item2);
     }
---
+++ test/Core.Test/OrganizationFeatures/OrganizationSignUp/PasswordManagerSignUpValidationStrategyTests.cs
@@ -0,0 +1,79 @@
+﻿using AutoFixture.Xunit2;
+using Bit.Core.Enums;
+using Bit.Core.Models.Business;
+using Bit.Core.OrganizationFeatures.OrganizationSignUp;
+using Bit.Test.Common.AutoFixture.Attributes;
+using Bit.Core.Exceptions;
+using Xunit;
+using Bit.Core.Models.StaticStore;
+
+namespace Bit.Core.Test.OrganizationFeatures.OrganizationSignUp;
+
+[SutProviderCustomize]
+public class PasswordManagerSignUpValidationStrategyTests
+{
+    [Theory]
+    [BitAutoData]
+    public void Validate_WhenPlanDoesNotAllowAdditionalStorageAndUpgradeRequestsAdditionalStorage_ThrowsBadRequestException(OrganizationUpgrade upgrade,
+        PasswordManagerSignUpValidationStrategy strategy)
+    {
+        var plan = new Plan { HasAdditionalStorageOption = false,BitwardenProduct = BitwardenProductType.PasswordManager};
+        upgrade.AdditionalStorageGb = 10;
+
+        Assert.Throws<BadRequestException>(() => strategy.Validate(plan, upgrade));
+    }
+    
+    [Theory]
+    [BitAutoData]
+    public void Validate_WhenUpgradeRequestsNegativeAdditionalStorage_ThrowsBadRequestException(
+        [Frozen] OrganizationUpgrade upgrade,
+        PasswordManagerSignUpValidationStrategy strategy)
+    {
+        var plan = new Plan { HasAdditionalStorageOption = true,BitwardenProduct = BitwardenProductType.PasswordManager};
+        upgrade.AdditionalStorageGb = -5;
+
+        Assert.Throws<BadRequestException>(() => strategy.Validate(plan, upgrade));
+    }
+    
+    [Theory]
+    [BitAutoData]
+    public void Validate_WhenNoSeatsAfterUpgrade_ThrowsBadRequestException(
+        [Frozen] Plan plan,
+        [Frozen] OrganizationUpgrade upgrade,
+        PasswordManagerSignUpValidationStrategy strategy)
+    {
+        plan.BaseSeats = 5;
+        plan.BitwardenProduct = BitwardenProductType.PasswordManager;
+        upgrade.AdditionalSeats = -5;
+
+        Assert.Throws<BadRequestException>(() => strategy.Validate(plan, upgrade));
+    }
+    
+    [Theory]
+    [BitAutoData]
+    public void Validate_WhenUpgradeRequestsNegativeAdditionalSeats_ThrowsBadRequestException(
+        [Frozen] Plan plan,
+        [Frozen] OrganizationUpgrade upgrade,
+        PasswordManagerSignUpValidationStrategy strategy)
+    {
+        plan.BitwardenProduct = BitwardenProductType.PasswordManager;
+        upgrade.AdditionalSeats = -3;
+
+        Assert.Throws<BadRequestException>(() => strategy.Validate(plan, upgrade));
+    }
+
+    [Theory]
+    [BitAutoData]
+    public void Validate_WhenPlanDoesNotAllowAdditionalSeatsAndUpgradeRequestsAdditionalSeats_ThrowsBadRequestException(
+        [Frozen] Plan plan,
+        [Frozen] OrganizationUpgrade upgrade,
+        PasswordManagerSignUpValidationStrategy strategy)
+    {
+        plan.HasAdditionalSeatsOption = false;
+        plan.BitwardenProduct = BitwardenProductType.PasswordManager;
+        upgrade.AdditionalSeats = 2;
+
+        Assert.Throws<BadRequestException>(() => strategy.Validate(plan, upgrade));
+    }
+
+}
