817c3d9b5f90cecc69e7e40d890174e670beb9bd
Matt Gibson <mgibson@bitwarden.com>
2023-06-16 08:29:11 -0400

    `dotnet format` :robot:

--- src/Icons/Util/UriBuilderExtension.cs
+++ src/Icons/Util/UriBuilderExtension.cs
@@ -1,4 +1,4 @@
-#nullable enable
+﻿#nullable enable
 
 namespace Bit.Icons.Extensions;
 
--- test/Icons.Test/Services/ServiceTestBase.cs
+++ test/Icons.Test/Services/ServiceTestBase.cs
@@ -1,4 +1,4 @@
-using System.Net;
+﻿using System.Net;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
