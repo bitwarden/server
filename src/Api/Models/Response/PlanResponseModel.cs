570cf37dce87563e2a161b163634b4d0ace77abb
cyprain-okeke <cokeke@bitwarden.com>
2023-06-22 00:12:09 +0100

    Add the missing property

--- src/Api/Models/Response/PlanResponseModel.cs
+++ src/Api/Models/Response/PlanResponseModel.cs
@@ -55,7 +55,8 @@ public class PlanResponseModel : ResponseModel
 
         AdditionalPricePerServiceAccount = plan.AdditionalPricePerServiceAccount;
         BaseServiceAccount = plan.BaseServiceAccount;
-        MaxServiceAccount = plan.MaxServiceAccount;
+        MaxServiceAccounts = plan.MaxServiceAccounts;
+        MaxAdditionalServiceAccounts = plan.MaxAdditionalServiceAccount;
         HasAdditionalServiceAccountOption = plan.HasAdditionalServiceAccountOption;
         MaxProjects = plan.MaxProjects;
         BitwardenProduct = plan.BitwardenProduct;
@@ -108,7 +109,8 @@ public class PlanResponseModel : ResponseModel
 
     public decimal? AdditionalPricePerServiceAccount { get; set; }
     public short? BaseServiceAccount { get; set; }
-    public short? MaxServiceAccount { get; set; }
+    public short? MaxServiceAccounts { get; set; }
+    public short? MaxAdditionalServiceAccounts { get; set; }
     public bool HasAdditionalServiceAccountOption { get; set; }
     public short? MaxProjects { get; set; }
     public BitwardenProductType BitwardenProduct { get; set; }
