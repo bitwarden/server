03725c1cfa7d92ed69c92eaae77b1a70dadd0c84
Rui Tome <rtome@bitwarden.com>
2023-06-29 15:34:06 +0100

    [AC-1462] Created IAvailableServiceAccountsQuery along its implementation and with unit tests

---
+++ bitwarden_license/src/Commercial.Core/SecretsManager/Queries/ServiceAccounts/AvailableServiceAccountsQuery.cs
@@ -0,0 +1,35 @@
+﻿using Bit.Core.Exceptions;
+using Bit.Core.Repositories;
+using Bit.Core.SecretsManager.Queries.ServiceAccounts.Interfaces;
+using Bit.Core.SecretsManager.Repositories;
+
+namespace Bit.Commercial.Core.SecretsManager.Queries.ServiceAccounts;
+
+public class AvailableServiceAccountsQuery : IAvailableServiceAccountsQuery
+{
+    private readonly IOrganizationRepository _organizationRepository;
+    private readonly IServiceAccountRepository _serviceAccountRepository;
+
+    public AvailableServiceAccountsQuery(
+        IOrganizationRepository organizationRepository,
+        IServiceAccountRepository serviceAccountRepository)
+    {
+        _organizationRepository = organizationRepository;
+        _serviceAccountRepository = serviceAccountRepository;
+    }
+
+    public async Task<int> GetAvailableServiceAccountsAsync(Guid organizationId)
+    {
+        var organization = await _organizationRepository.GetByIdAsync(organizationId);
+        if (organization == null)
+        {
+            throw new NotFoundException();
+        }
+
+        var serviceAccountCount = await _serviceAccountRepository.GetServiceAccountCountByOrganizationIdAsync(organizationId);
+
+        var availableServiceAccounts = Math.Max(0, organization.SmServiceAccounts.HasValue ? organization.SmServiceAccounts.Value - serviceAccountCount : 0);
+
+        return availableServiceAccounts;
+    }
+}
