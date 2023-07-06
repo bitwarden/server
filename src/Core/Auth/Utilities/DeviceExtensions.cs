1326ec93ed0f5d3250eb8d01be080f82d835fa03
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2023-07-06 13:48:45 -0400

    Add Remarks Comment

--- src/Core/Auth/Utilities/DeviceExtensions.cs
+++ src/Core/Auth/Utilities/DeviceExtensions.cs
@@ -9,6 +9,11 @@ public static class DeviceExtensions
     /// <summary>
     /// Gets a boolean representing if the device has enough information on it to determine whether or not it is trusted.
     /// </summary>
+    /// <remarks>
+    /// It is possible for a device to be un-trusted client side and not notify the server side. This should not be
+    /// the source of truth for whether a device is fully trusted and should just be considered that, to the server,
+    /// a device has the necessary information to be "trusted".
+    /// </remarks>
     public static bool IsTrusted(this Device device)
     {
         return !string.IsNullOrEmpty(device.EncryptedUserKey) &&
