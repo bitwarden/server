ede2b7b3828a6b8e1d5836c42730e21ed3b3fc57
tangowithfoxtrot <5676771+tangowithfoxtrot@users.noreply.github.com>
2023-07-08 04:30:02 -0700

    use better search string

--- .devcontainer/community_dev/configure_env.cmd
+++ .devcontainer/community_dev/configure_env.cmd
@@ -11,13 +11,13 @@ function Test-Env {
 }
 
 function Copy-MSSQLVar {
-  if (Select-String -Path "./dev/.env" -Pattern "MSSQL_SA_PASSWORD" -Quiet) {
+  if (Select-String -Path "./dev/.env" -Pattern "^MSSQL_SA_PASSWORD" -Quiet) {
     Write-Host "MSSQL_SA_PASSWORD already exists in ./dev/.env"
   }
   else {
     Write-Host "Copying MSSQL_PASSWORD to MSSQL_SA_PASSWORD"
     $DB_PASSWORD = Get-Content "./dev/.env" | Where-Object { $_ -match "^MSSQL_PASSWORD=" }
-    $DB_PASSWORD -replace "MSSQL_PASSWORD", "MSSQL_SA_PASSWORD" | Out-File -Append ./dev/.env
+    $DB_PASSWORD -replace "^MSSQL_PASSWORD", "MSSQL_SA_PASSWORD" | Out-File -Append ./dev/.env
   }
 }
 
