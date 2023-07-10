88338a94def20c7544f68c0c839f56689d09b34e
Colton Hurst <colton@coltonhurst.com>
2023-07-10 07:47:44 -0400

    SM-802: Add SecretsManagerBetaColumn migration

---
+++ util/PostgresMigrations/Migrations/20230710114625_SecretsManagerBetaColumn.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.PostgresMigrations.Migrations;
+
+public partial class SecretsManagerBetaColumn : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<bool>(
+            name: "SecretsManagerBeta",
+            table: "Organization",
+            type: "boolean",
+            nullable: false,
+            defaultValue: false);
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "SecretsManagerBeta",
+            table: "Organization");
+    }
+}
