81d3bd82f844ecc1d7476c16403afef6f225656d
Rui Tome <rtome@bitwarden.com>
2023-07-10 11:33:17 +0100

    [AC-1462] Deleted IAutoscaleServiceAccountsCommand as the logic will be moved to UpdateSecretsManagerSubscriptionCommand

--- src/Core/SecretsManager/Commands/ServiceAccounts/Interfaces/IAutoscaleServiceAccountsCommand.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Bit.Core.SecretsManager.Commands.ServiceAccounts.Interfaces;
-
-public interface IAutoscaleServiceAccountsCommand
-{
-    Task AutoscaleServiceAccountsAsync(Guid organizationId, int serviceAccountsToAdd);
-}
