1d9aeb37aaa6abeb0cfa5b3b37acc77896ef7c5c
Thomas Avery <43214426+Thomas-Avery@users.noreply.github.com>
2023-07-13 11:46:01 -0500

    [SM-707] Refactor authorization for Access Policy Commands  (#2905)
    * Extract authorization from access policy commands
    
    * Use auto mapper to ignore unwanted properties
    
    ---------

--- src/Api/SecretsManager/Models/Request/GrantedAccessPolicyRequest.cs
+++ src/Api/SecretsManager/Models/Request/GrantedAccessPolicyRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using Bit.Core.SecretsManager.Entities;
+using Bit.Infrastructure.EntityFramework.SecretsManager.Models;
+using ServiceAccountProjectAccessPolicy = Bit.Core.SecretsManager.Entities.ServiceAccountProjectAccessPolicy;
 
 namespace Bit.Api.SecretsManager.Models.Request;
 
@@ -14,10 +15,11 @@ public class GrantedAccessPolicyRequest
     [Required]
     public bool Write { get; set; }
 
-    public ServiceAccountProjectAccessPolicy ToServiceAccountProjectAccessPolicy(Guid serviceAccountId) =>
+    public ServiceAccountProjectAccessPolicy ToServiceAccountProjectAccessPolicy(Guid serviceAccountId, Guid organizationId) =>
         new()
         {
             ServiceAccountId = serviceAccountId,
+            ServiceAccount = new ServiceAccount() { Id = serviceAccountId, OrganizationId = organizationId },
             GrantedProjectId = GrantedId,
             Read = Read,
             Write = Write,
