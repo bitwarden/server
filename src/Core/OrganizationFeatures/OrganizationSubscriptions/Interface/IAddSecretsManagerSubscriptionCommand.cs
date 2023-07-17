2310d396c3883bcc5b2ecd31167c26682ad2670f
cyprain-okeke <cokeke@bitwarden.com>
2023-07-17 19:02:42 +0100

    Add xml doc comment

--- src/Core/OrganizationFeatures/OrganizationSubscriptions/Interface/IAddSecretsManagerSubscriptionCommand.cs
+++ src/Core/OrganizationFeatures/OrganizationSubscriptions/Interface/IAddSecretsManagerSubscriptionCommand.cs
@@ -2,6 +2,9 @@
 
 namespace Bit.Core.OrganizationFeatures.OrganizationSubscriptions.Interface;
 
+/// <summary>
+/// This is only for adding SM to an existing organization
+/// </summary>
 public interface IAddSecretsManagerSubscriptionCommand
 {
     Task<Organization> SignUpAsync(Organization organization, int additionalSeats, int additionalServiceAccounts);
