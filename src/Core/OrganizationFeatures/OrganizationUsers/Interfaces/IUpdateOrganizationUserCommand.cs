b40a4d502155bfbbeef4558027a33cd4c49cd1ec
Rui Tome <rtome@bitwarden.com>
2023-07-25 14:57:54 +0100

    [PM-3176] Created UpdateOrganizationUserCommand

---
+++ src/Core/OrganizationFeatures/OrganizationUsers/Interfaces/IUpdateOrganizationUserCommand.cs
@@ -0,0 +1,9 @@
+﻿using Bit.Core.Entities;
+using Bit.Core.Models.Data;
+
+namespace Bit.Core.OrganizationFeatures.OrganizationUsers.Interfaces;
+
+public interface IUpdateOrganizationUserCommand
+{
+    Task UpdateUserAsync(OrganizationUser user, Guid? savingUserId, IEnumerable<CollectionAccessSelection> collections, IEnumerable<Guid> groups);
+}
