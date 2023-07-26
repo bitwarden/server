2d6628523aa086970a30a7bb7d4eb0eadc4c5035
Colton Hurst <colton@coltonhurst.com>
2023-07-26 15:18:13 -0400

    SM-802: Add SecretsManagerBeta migration

--- util/SqliteMigrations/Migrations/20230726191546_SecretsManagerBetaColumn.Designer.cs
+++ util/SqliteMigrations/Migrations/20230726191546_SecretsManagerBetaColumn.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Bit.Infrastructure.EntityFramework.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bit.SqliteMigrations.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20230726191546_SecretsManagerBetaColumn")]
+    partial class SecretsManagerBetaColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.5");
