a4ea5c4dca2e2fef2f678c21188531c54da0d3ea
Rui Tome <rtome@bitwarden.com>
2023-08-03 21:14:38 +0100

    [AC-1360] Added EF migration scripts

---
+++ util/PostgresMigrations/Migrations/20230803195417_PopulateResellerNames.cs
@@ -0,0 +1,24 @@
+﻿using Bit.Core.Utilities;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.PostgresMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class PopulateResellerNames : Migration
+{
+    private const string _scriptLocation = "PostgresMigrations.HelperScripts.2023-08-03_00_PopulateResellerNames.psql";
+
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(CoreHelpers.GetEmbeddedResourceContentsAsync(_scriptLocation));
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        throw new Exception("Irreversible migration");
+    }
+}
