54f769b41f3e7f5e6b51074f5c57610924cf7be2
Rui Tome <rtome@bitwarden.com>
2023-08-04 09:38:39 +0100

    Revert "[AC-1360] Added EF scripts for MySQL, Postgres and Sqlite"
    This reverts commit 4b47d257556b5059167cbc58f6c624158783aae4.

--- util/SqliteMigrations/Scripts/2023-08-03_00_PopulateResellerNames.sql
+++
@@ -1,5 +0,0 @@
--- Reseller Providers were being created with a NULL value in the "Name" column.
--- This script will populate them with the value from "BusinessName" which was already required.
-UPDATE "Provider"
-SET "Name" = "BusinessName"
-WHERE "Name" IS NULL;
