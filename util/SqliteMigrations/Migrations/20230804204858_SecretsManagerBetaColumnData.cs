f93d594c05754bf6f732d076e16ba4163fa06e50
Colton Hurst <colton@coltonhurst.com>
2023-08-04 16:52:26 -0400

    SM-873: Remove SecretsManagerBetaColumn data migration and add new migration

---
+++ util/SqliteMigrations/Migrations/20230804204858_SecretsManagerBetaColumnData.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.SqliteMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class SecretsManagerBetaColumnData : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+
+    }
+}
