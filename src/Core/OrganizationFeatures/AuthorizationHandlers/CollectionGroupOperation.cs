ab90d9f9e43b1afff66808716d5e194da233314b
Shane Melton <smelton@bitwarden.com>
2023-08-15 10:46:29 -0700

    [AC-1174] Add CollectionGroupOperation.cs

---
+++ src/Core/OrganizationFeatures/AuthorizationHandlers/CollectionGroupOperation.cs
@@ -0,0 +1,17 @@
+﻿using Bit.Core.Entities;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+namespace Bit.Core.OrganizationFeatures.AuthorizationHandlers;
+
+public class CollectionGroupOperationRequirement : OperationAuthorizationRequirement
+{
+    public Collection Collection { get; set; }
+}
+
+public static class CollectionGroupOperation
+{
+    public static readonly CollectionGroupOperationRequirement Create = new() { Name = nameof(Create) };
+    public static readonly CollectionGroupOperationRequirement Delete = new() { Name = nameof(Delete) };
+
+    public static CollectionGroupOperationRequirement CreateForNewCollection(Collection collection) => new() { Name = nameof(CreateForNewCollection), Collection = collection };
+}
