0889d33fc71965f61b8b8a44ed7f920e8c0ae583
Shane Melton <smelton@bitwarden.com>
2023-08-15 11:58:14 -0700

    [AC-1174] Cleanup CollectionFixture customization
    Implement and use re-usable extension method to support seeded Guids

---
+++ test/Core.Test/AutoFixture/AutoFixtureExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq.Expressions;
+using AutoFixture.Dsl;
+
+namespace Bit.Core.Test.AutoFixture;
+
+public static class AutoFixtureExtensions
+{
+    /// <summary>
+    /// Registers that a writable Guid property should be assigned a random value that is derived from the given seed.
+    /// </summary>
+    /// <remarks>
+    /// This can be used to generate random Guids that are deterministic based on the seed and thus can be re-used for
+    /// different entities that share the same identifiers. e.g. Collections, CollectionUsers, and CollectionGroups can
+    /// all have the same Guids generate for their "collection id" properties.
+    /// </remarks>
+    /// <param name="composer"></param>
+    /// <param name="propertyPicker">The Guid property to register</param>
+    /// <param name="seed">The random seed to use for random Guid generation</param>
+    public static IPostprocessComposer<T> WithGuidFromSeed<T>(this IPostprocessComposer<T> composer, Expression<Func<T, Guid>> propertyPicker, int seed)
+    {
+        var rnd = new Random(seed);
+        return composer.With(propertyPicker, () =>
+        {
+            // While not as random/unique as Guid.NewGuid(), this is works well enough for testing purposes.
+            var bytes = new byte[16];
+            rnd.NextBytes(bytes);
+            return new Guid(bytes);
+        });
+    }
+}
--- test/Core.Test/AutoFixture/CollectionFixture.cs
+++ test/Core.Test/AutoFixture/CollectionFixture.cs
@@ -7,59 +7,39 @@ namespace Bit.Core.Test.AutoFixture;
 
 public class CollectionCustomization : ICustomization
 {
+    private const int _collectionIdSeed = 1;
+    private const int _userIdSeed = 2;
+    private const int _groupIdSeed = 3;
+
     public void Customize(IFixture fixture)
     {
         var orgId = Guid.NewGuid();
-        var initialSeed = new Random().Next();
-
-        // Use the same seed for each of these so that the collection Guids are the same for each type
-        // This way when lists of various collection entities are created, they will have the same Guids
-        var collectionIdRnd = new Random(initialSeed);
-        var collectionDetailsIdRnd = new Random(initialSeed);
-        var collectionUserCollectionIdRnd = new Random(initialSeed);
-        var collectionGroupCollectionIdRnd = new Random(initialSeed);
-
-        // Use the same seed for each of these so that the user Guids are the same for each type
-        // Increment the initial seed by 1 so that the user Guids are different from the collection Guids
-        var userIdRnd = new Random(initialSeed + 1);
-        var collectionUserUserIdRnd = new Random(initialSeed + 1);
-
-        var groupIdRnd = new Random(initialSeed + 2);
-        var collectionGroupGroupIdRnd = new Random(initialSeed + 2);
 
         fixture.Customize<OrganizationUser>(composer => composer
             .With(o => o.OrganizationId, orgId)
-            .With(o => o.Id, () => SeededGuid(userIdRnd)));
+            .WithGuidFromSeed(o => o.Id, _userIdSeed));
 
         fixture.Customize<Collection>(composer => composer
-            .With(cu => cu.OrganizationId, orgId)
-            .With(cu => cu.Id, () => SeededGuid(collectionIdRnd)));
+            .With(c => c.OrganizationId, orgId)
+            .WithGuidFromSeed(c => c.Id, _collectionIdSeed));
 
         fixture.Customize<CollectionDetails>(composer => composer
             .With(cd => cd.OrganizationId, orgId)
-            .With(cd => cd.Id, () => SeededGuid(collectionDetailsIdRnd)));
+            .WithGuidFromSeed(cd => cd.Id, _collectionIdSeed));
 
         fixture.Customize<CollectionUser>(c => c
-            .With(cu => cu.OrganizationUserId, () => SeededGuid(collectionUserUserIdRnd))
-            .With(cu => cu.CollectionId, () => SeededGuid(collectionUserCollectionIdRnd)));
+            .WithGuidFromSeed(cu => cu.OrganizationUserId, _userIdSeed)
+            .WithGuidFromSeed(cu => cu.CollectionId, _collectionIdSeed));
 
         fixture.Customize<Group>(composer => composer
             .With(o => o.OrganizationId, orgId)
-            .With(o => o.Id, () => SeededGuid(groupIdRnd)));
+            .WithGuidFromSeed(o => o.Id, _groupIdSeed));
 
         fixture.Customize<CollectionGroup>(c => c
-            .With(cu => cu.GroupId, () => SeededGuid(collectionGroupGroupIdRnd))
-            .With(cu => cu.CollectionId, () => SeededGuid(collectionGroupCollectionIdRnd)));
-    }
-
-    private static Guid SeededGuid(Random rnd)
-    {
-        var bytes = new byte[16];
-        rnd.NextBytes(bytes);
-        return new Guid(bytes);
+            .WithGuidFromSeed(cu => cu.GroupId, _groupIdSeed)
+            .WithGuidFromSeed(cu => cu.CollectionId, _collectionIdSeed));
     }
 }
-
 
 public class CollectionCustomizationAttribute : BitCustomizeAttribute
 {
