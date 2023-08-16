990060cc19863240f69ffb9bdb9987bc3fbe096b
Matt Bishop <mbishop@bitwarden.com>
2023-08-16 15:13:39 -0400

    Bootstrap logger

--- src/Extensions.Hosting/LoggerExtensions.cs
+++ src/Extensions.Hosting/LoggerExtensions.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Events;
 using Serilog.Formatting.Compact;
 
 namespace Bit.Extensions.Hosting;
 
 public static class LoggerExtensions
 {
+    public static object GetBootstrapLogger()
+    {
+        return new LoggerConfiguration()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateBootstrapLogger();
+    }
+
     public static void UseBitwardenLogging(
         this IHostBuilder hostBuilder)
     {
