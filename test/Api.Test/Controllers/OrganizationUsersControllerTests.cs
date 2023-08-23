ea73e3779348067f5a8f23543574b3d3e8a54951
Rui Tome <rtome@bitwarden.com>
2023-08-23 17:09:57 +0100

    [PM-3176] Added unit tests for OrganizationUsersController.Put

--- test/Api.Test/Controllers/OrganizationUsersControllerTests.cs
+++ test/Api.Test/Controllers/OrganizationUsersControllerTests.cs
@@ -1,7 +1,12 @@
-﻿using Bit.Api.Controllers;
+﻿using System.Security.Claims;
+using Bit.Api.Controllers;
 using Bit.Api.Models.Request.Organizations;
+using Bit.Core.Context;
 using Bit.Core.Entities;
+using Bit.Core.Exceptions;
+using Bit.Core.Models.Data;
 using Bit.Core.Models.Data.Organizations.Policies;
+using Bit.Core.OrganizationFeatures.OrganizationUsers.Interfaces;
 using Bit.Core.Repositories;
 using Bit.Core.Services;
 using Bit.Core.Utilities;
@@ -61,5 +66,55 @@ public class OrganizationUsersControllerTests
             .AcceptUserAsync(orgUserId, user, model.Token, sutProvider.GetDependency<IUserService>());
         await sutProvider.GetDependency<IOrganizationService>().Received(1)
             .UpdateUserResetPasswordEnrollmentAsync(orgId, user.Id, model.ResetPasswordKey, user.Id);
+    }
+
+    [Theory]
+    [BitAutoData]
+    public async Task Put_WithoutManageUsersPermission_Throws(Guid orgId, Guid orgUserId, OrganizationUserUpdateRequestModel model, SutProvider<OrganizationUsersController> sutProvider)
+    {
+        sutProvider.GetDependency<ICurrentContext>().ManageUsers(orgId).Returns(false);
+
+        await Assert.ThrowsAsync<NotFoundException>(() => sutProvider.Sut.Put(orgId, orgUserId, model));
+    }
+
+    [Theory]
+    [BitAutoData]
+    public async Task Put_WithNullOrgUser_Throws(Guid orgId, Guid orgUserId, OrganizationUserUpdateRequestModel model, SutProvider<OrganizationUsersController> sutProvider)
+    {
+        sutProvider.GetDependency<ICurrentContext>().ManageUsers(orgId).Returns(true);
+
+        await Assert.ThrowsAsync<NotFoundException>(() => sutProvider.Sut.Put(orgId, orgUserId, model));
+    }
+
+    [Theory]
+    [BitAutoData]
+    public async Task Put_WithNonMatchingOrgId_Throws(Guid orgId, OrganizationUser orgUser, OrganizationUserUpdateRequestModel model, SutProvider<OrganizationUsersController> sutProvider)
+    {
+        sutProvider.GetDependency<ICurrentContext>().ManageUsers(orgId).Returns(true);
+        sutProvider.GetDependency<IOrganizationUserRepository>().GetByIdAsync(orgUser.Id).Returns(orgUser);
+
+        await Assert.ThrowsAsync<NotFoundException>(() => sutProvider.Sut.Put(orgId, orgUser.Id, model));
+    }
+
+    [Theory]
+    [BitAutoData]
+    public async Task Put_Success(Guid orgId, OrganizationUser orgUser, OrganizationUserUpdateRequestModel model, Guid? savingUserId, SutProvider<OrganizationUsersController> sutProvider)
+    {
+        orgUser.OrganizationId = orgId;
+
+        sutProvider.GetDependency<ICurrentContext>().ManageUsers(orgId).Returns(true);
+        sutProvider.GetDependency<IOrganizationUserRepository>().GetByIdAsync(orgUser.Id).Returns(orgUser);
+        sutProvider.GetDependency<IUserService>().GetProperUserId(Arg.Any<ClaimsPrincipal>()).Returns(savingUserId);
+
+        await sutProvider.Sut.Put(orgId, orgUser.Id, model);
+
+        await sutProvider.GetDependency<ICurrentContext>().Received(1).ManageUsers(orgId);
+        await sutProvider.GetDependency<IOrganizationUserRepository>().Received(1).GetByIdAsync(orgUser.Id);
+        sutProvider.GetDependency<IUserService>().Received(1).GetProperUserId(Arg.Any<ClaimsPrincipal>());
+        await sutProvider.GetDependency<IUpdateOrganizationUserCommand>().Received(1).UpdateUserAsync(
+            Arg.Is<OrganizationUser>(ou => ou.Id == orgUser.Id && ou.OrganizationId == orgId),
+            savingUserId,
+            Arg.Any<IEnumerable<CollectionAccessSelection>>(),
+            model.Groups);
     }
 }
