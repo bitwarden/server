edc8139c6d80006dffbb9cd6aa1cbcb9feb37be7
Michal Checinski <mchecinski@bitwarden.com>
2023-08-24 13:01:17 +0200

    Enable logging in db migrator

--- util/Setup/Program.cs
+++ util/Setup/Program.cs
@@ -190,7 +190,7 @@ public class Program
         var vaultConnectionString = Helpers.GetValueFromEnvFile("global",
             "globalSettings__sqlServer__connectionString");
         var migrator = new DbMigrator(vaultConnectionString, null);
-        migrator.MigrateMsSqlDatabaseWithRetries(false);
+        migrator.MigrateMsSqlDatabaseWithRetries(true);
     }
 
     private static bool ValidateInstallation()
