8e18b3387c3308776af2dedc0ff63ecc4335fa6a
Shane Melton <smelton@bitwarden.com>
2023-08-28 13:42:29 -0700

    [AC-1174] Remove unnecessary BulkCollectionAccessRequestModel helpers

--- src/Api/Models/Request/BulkCollectionAccessRequestModel.cs
+++ src/Api/Models/Request/BulkCollectionAccessRequestModel.cs
@@ -1,55 +1,13 @@
 ﻿
 
 using Bit.Core.Entities;
+using Bit.Core.Exceptions;
 
 namespace Bit.Api.Models.Request;
 
 public class BulkCollectionAccessRequestModel
 {
     public IEnumerable<Guid> CollectionIds { get; set; }
-
     public IEnumerable<SelectionReadOnlyRequestModel> Groups { get; set; }
     public IEnumerable<SelectionReadOnlyRequestModel> Users { get; set; }
-
-    /// <summary>
-    /// Build a list of <see cref="CollectionUser"/> entities from combinations of every
-    /// <see cref="CollectionIds"/> and <see cref="Users"/>.
-    /// </summary>
-    public IEnumerable<CollectionUser> ToAllCollectionUsers()
-    {
-        if (Users == null)
-        {
-            return new List<CollectionUser>();
-        }
-
-        return CollectionIds.SelectMany(collectionId => Users.Select(u => new CollectionUser
-        {
-            CollectionId = collectionId,
-            OrganizationUserId = u.Id,
-            Manage = u.Manage,
-            HidePasswords = u.HidePasswords,
-            ReadOnly = u.ReadOnly
-        }));
-    }
-
-    /// <summary>
-    /// Build a list of <see cref="CollectionGroup"/> entities from combinations of every
-    /// <see cref="CollectionIds"/> and <see cref="Groups"/>.
-    /// </summary>
-    public IEnumerable<CollectionGroup> ToAllCollectionGroups()
-    {
-        if (Groups == null)
-        {
-            return new List<CollectionGroup>();
-        }
-
-        return CollectionIds.SelectMany(collectionId => Groups.Select(u => new CollectionGroup
-        {
-            CollectionId = collectionId,
-            GroupId = u.Id,
-            Manage = u.Manage,
-            HidePasswords = u.HidePasswords,
-            ReadOnly = u.ReadOnly
-        }));
-    }
 }
