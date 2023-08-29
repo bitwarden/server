73d18a1b020ac5fdc800f4186d7ead9b021946aa
Carlos Gonçalves <cgoncalves@bitwarden.com>
2023-08-29 18:51:53 +0100

    Bumped up minimum version

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -20,7 +20,7 @@ public static class Constants
     /// </summary>
     public const int OrganizationSelfHostSubscriptionGracePeriodDays = 60;
 
-    public const string CipherKeyEncryptionMinimumVersion = "2023.7.0";
+    public const string CipherKeyEncryptionMinimumVersion = "2023.8.2";
 }
 
 public static class TokenPurposes
