384c2d3d8c505feb350b64144001e61c9ac90a39
Todd Martin <tmartin@bitwarden.com>
2023-09-14 16:54:55 -0400

    Added in check for org users that have Premium through their org.

--- util/Migrator/DbScripts/2023-09-14_00_Remove_WebAuthn_For_Free_Users.sql
+++ util/Migrator/DbScripts/2023-09-14_00_Remove_WebAuthn_For_Free_Users.sql
@@ -10,26 +10,27 @@ declare @TwoFactorByUser TABLE
 (
     Id UNIQUEIDENTIFIER,
     Email NVARCHAR(256),
-    TwoFactorType INT,
-    TypeEnabled BIT
+    TwoFactorType INT
 )
 
 INSERT INTO @TwoFactorByUser
 SELECT  u.Id, 
         u.Email,
-        tfp1.[key] as TwoFactorType, 
-        CASE [Enabled] WHEN 'true' THEN 1 ELSE 0 END as TypeEnabled
+        tfp1.[key] as TwoFactorType
 FROM [User] u
+LEFT OUTER JOIN [OrganizationUser] ou on ou.UserId = u.Id
+LEFT OUTER JOIN [Organization] o on o.Id = ou.OrganizationId
 CROSS APPLY OPENJSON(u.TwoFactorProviders) tfp1
 CROSS APPLY OPENJSON(tfp1.[value]) WITH (
    [Enabled] nvarchar(10) '$.Enabled'
 ) tfp2
-WHERE [Enabled] = 'true'
-AND Premium = 0
+WHERE [Enabled] = 'true' -- We only want enabled 2FA methods
+AND Premium = 0 -- User isn't Premium
+AND (o IS NULL OR (o IS NOT NULL AND o.Enabled = 1 AND o.UsersGetPremium = 0)) -- User doesn't have Premium from their org
 
 select *
 from @TwoFactorByUser t1
-where t1.TwoFactorType = 7 and t1.TypeEnabled = 1
+where t1.TwoFactorType = 7
 AND NOT EXISTS (SELECT * FROM @TwoFactorByUser t2 WHERE t2.Id = t1.Id and t2.TwoFactorType <> 7)
 
 UPDATE [User]
