a81fd7886ae884f80238c82491dab428cf27b398
Todd Martin <tmartin@bitwarden.com>
2023-09-26 17:58:58 -0400

    Updated minimum client version in preparation for release.

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -20,7 +20,7 @@ public static class Constants
     /// </summary>
     public const int OrganizationSelfHostSubscriptionGracePeriodDays = 60;
 
-    public const string CipherKeyEncryptionMinimumVersion = "2023.7.0";
+    public const string CipherKeyEncryptionMinimumVersion = "2023.9.1";
 }
 
 public static class TokenPurposes
