5c9e9f532d8f4c9967ff0b860fce570d4c7e9578
Conner Turnbull <cturnbull@bitwarden.com>
2023-09-29 15:01:19 -0400

    Split plan script into two scripts for teams and enterprise

--- util/Migrator/DbScripts/2023-09-29_00_2019TeamsPlanFeatureUpgrade.sql
+++ util/Migrator/DbScripts/2023-09-29_00_2019TeamsPlanFeatureUpgrade.sql
@@ -1,15 +1,17 @@
 BEGIN TRY
     BEGIN TRANSACTION;
 
-    UPDATE [dbo].[Organization]
-    SET [Use2fa] = 1,
+    UPDATE
+        [dbo].[Organization]
+    SET
+        [Use2fa] = 1,
         [UseApi] = 1,
         [UseDirectory] = 1,
         [UseEvents] = 1,
         [UseGroups] = 1,
         [UsersGetPremium] = 1
-    WHERE [PlanType] >= 2 -- Teams (Monthly) 2019
-      AND [PlanType] <= 5; -- Enterprise (Annually) 2019
+    WHERE
+        [PlanType] IN (2, 3); -- Teams 2019
 
     COMMIT TRANSACTION;
 END TRY
