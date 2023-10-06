bc476b7779d24715752d42d122497ac6c7f7fb65
Jared Snider <jsnider@bitwarden.com>
2023-10-06 13:10:51 -0400

    PM-3275 - SetInitialMasterPasswordCommand.cs - Move summary from implementation to interface to better respect standards and the fact that the interface is the more seen piece of code.

--- src/Core/Auth/UserFeatures/UserMasterPassword/SetInitialMasterPasswordCommand.cs
+++ src/Core/Auth/UserFeatures/UserMasterPassword/SetInitialMasterPasswordCommand.cs
@@ -10,15 +10,6 @@ using Microsoft.Extensions.Logging;
 
 namespace Bit.Core.Auth.UserFeatures.UserMasterPassword;
 
-/// <summary>
-/// <para>Manages the setting of the initial master password for a <see cref="User"/> in an organization.</para>
-/// <para>This class is primarily invoked in two scenarios:</para>
-/// <para>1) In organizations configured with Single Sign-On (SSO) and master password decryption:
-/// just in time (JIT) provisioned users logging in via SSO are required to set a master password.</para>
-/// <para>2) In organizations configured with SSO and trusted devices decryption:
-/// Users who are upgraded to have admin account recovery permissions must set a master password
-/// to ensure their ability to reset other users' accounts.</para>
-/// </summary>
 public class SetInitialMasterPasswordCommand : ISetInitialMasterPasswordCommand
 {
     private readonly ILogger<SetInitialMasterPasswordCommand> _logger;
