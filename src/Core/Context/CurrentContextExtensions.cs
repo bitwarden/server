f3bda4288a3f828dd4f52b60122d6bb951c279fd
Jared Snider <jsnider@bitwarden.com>
2023-10-13 10:38:44 -0400

    PM-3275 - CurrentContextExtensions.cs - Refactor AnyOrgUserHasManageResetPasswordPermission per PR feedback to remove unnecessary var.

--- src/Core/Context/CurrentContextExtensions.cs
+++ src/Core/Context/CurrentContextExtensions.cs
@@ -7,16 +7,14 @@ public static class CurrentContextExtensions
     public static async Task<bool> AnyOrgUserHasManageResetPasswordPermission(this ICurrentContext currentContext,
         ICollection<OrganizationUserOrganizationDetails> organizationUserDetails)
     {
-        var hasManageResetPasswordPermission = false;
         // if user has a single org with manage reset password permission, then they have it
         foreach (var orgUserDetail in organizationUserDetails)
         {
             if (await currentContext.ManageResetPassword(orgUserDetail.OrganizationId))
             {
-                hasManageResetPasswordPermission = true;
-                break;
+                return true;
             }
         }
-        return hasManageResetPasswordPermission;
+        return false;
     }
 }
