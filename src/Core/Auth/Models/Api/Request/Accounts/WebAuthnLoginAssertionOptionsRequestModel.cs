1ffd97e935b57f3781deb65e9743bed67745f176
Andreas Coroiu <andreas.coroiu@gmail.com>
2023-10-18 10:28:23 +0200

    [PM-2032] feat: add request and response models

---
+++ src/Core/Auth/Models/Api/Request/Accounts/WebAuthnLoginAssertionOptionsRequestModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Bit.Core.Auth.Models.Api.Request.Accounts;
+
+public class WebAuthnLoginAssertionOptionsRequestModel
+{
+    // Requried to add support for non-discoverable logins
+    // [EmailAddress]
+    // [StringLength(256)]
+    // public string Email { get; set; }
+}
+
