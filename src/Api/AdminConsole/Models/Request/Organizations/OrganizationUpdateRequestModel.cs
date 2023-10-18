37e9d70bee15ecfe7471bd23b2b9598439a4a83d
Thomas Rittson <31796059+eliykat@users.noreply.github.com>
2023-10-19 01:27:56 +1000

    AC Team code ownership moves - Api project (#3351)

---
+++ src/Api/AdminConsole/Models/Request/Organizations/OrganizationUpdateRequestModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Bit.Core.Entities;
+using Bit.Core.Models.Data;
+using Bit.Core.Settings;
+
+namespace Bit.Api.AdminConsole.Models.Request.Organizations;
+
+public class OrganizationUpdateRequestModel
+{
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set; }
+    [StringLength(50)]
+    public string BusinessName { get; set; }
+    [EmailAddress]
+    [Required]
+    [StringLength(256)]
+    public string BillingEmail { get; set; }
+    public Permissions Permissions { get; set; }
+    public OrganizationKeysRequestModel Keys { get; set; }
+
+    public virtual Organization ToOrganization(Organization existingOrganization, GlobalSettings globalSettings)
+    {
+        if (!globalSettings.SelfHosted)
+        {
+            // These items come from the license file
+            existingOrganization.Name = Name;
+            existingOrganization.BusinessName = BusinessName;
+            existingOrganization.BillingEmail = BillingEmail?.ToLowerInvariant()?.Trim();
+        }
+        Keys?.ToOrganization(existingOrganization);
+        return existingOrganization;
+    }
+}
