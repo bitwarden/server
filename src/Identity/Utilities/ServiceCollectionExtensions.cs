3dcba006a7c9ff5e1ee0cbece8ccbda1db00594e
Todd Martin <tmartin@bitwarden.com>
2023-10-19 17:58:12 -0400

    Remove ExtensionGrantValidator

--- src/Identity/Utilities/ServiceCollectionExtensions.cs
+++ src/Identity/Utilities/ServiceCollectionExtensions.cs
@@ -44,8 +44,7 @@ public static class ServiceCollectionExtensions
             .AddResourceOwnerValidator<ResourceOwnerPasswordValidator>()
             .AddPersistedGrantStore<PersistedGrantStore>()
             .AddClientStore<ClientStore>()
-            .AddIdentityServerCertificate(env, globalSettings)
-            .AddExtensionGrantValidator<ExtensionGrantValidator>();
+            .AddIdentityServerCertificate(env, globalSettings);
 
         services.AddTransient<ICorsPolicyService, CustomCorsPolicyService>();
         return identityServerBuilder;
