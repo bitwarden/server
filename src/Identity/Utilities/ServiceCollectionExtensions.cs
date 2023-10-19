3c1c8e9bc56065799f1af949f5223715502fa70f
Andreas Coroiu <andreas.coroiu@gmail.com>
2023-10-19 16:59:08 +0200

    [PM-2032] chore: rename to `WebAuthnGrantValidator`

--- src/Identity/Utilities/ServiceCollectionExtensions.cs
+++ src/Identity/Utilities/ServiceCollectionExtensions.cs
@@ -44,7 +44,8 @@ public static class ServiceCollectionExtensions
             .AddResourceOwnerValidator<ResourceOwnerPasswordValidator>()
             .AddPersistedGrantStore<PersistedGrantStore>()
             .AddClientStore<ClientStore>()
-            .AddIdentityServerCertificate(env, globalSettings);
+            .AddIdentityServerCertificate(env, globalSettings)
+            .AddExtensionGrantValidator<WebAuthnGrantValidator>();
 
         services.AddTransient<ICorsPolicyService, CustomCorsPolicyService>();
         return identityServerBuilder;
