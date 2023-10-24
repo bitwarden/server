088978a8a0f926cbffc3299230884205ede5854c
Thomas Rittson <trittson@bitwarden.com>
2023-10-24 14:31:27 +1000

    Create identical v2 sprocs that use UserCipherDetails

---
+++ src/Sql/Vault/dbo/Stored Procedures/Cipher/CipherDetails_ReadByIdUserId_V2.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[CipherDetails_ReadByIdUserId_V2]
+    @Id UNIQUEIDENTIFIER,
+    @UserId UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    SELECT TOP 1
+        *
+    FROM
+        [dbo].[UserCipherDetails](@UserId)
+    WHERE
+        [Id] = @Id
+    ORDER BY
+        [Edit] DESC
+END
---
+++ src/Sql/Vault/dbo/Stored Procedures/Cipher/CipherDetails_ReadByUserId_V2.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[CipherDetails_ReadByUserId_V2]
+    @UserId UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    SELECT
+        *
+    FROM
+        [dbo].[UserCipherDetails](@UserId)
+END
