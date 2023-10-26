d47ebd6a5e4e7411b9afb3716a4c94c48148a25b
Thomas Rittson <trittson@bitwarden.com>
2023-10-26 15:36:30 +1000

    Revert "Delete excess sprocs for now"
    This reverts commit 86251c8aabd46b55c60bb81dae72da1ad9fc29d7.

---
+++ src/Sql/dbo/Functions/UserCollectionDetails_V2.sql
@@ -0,0 +1,30 @@
+﻿CREATE FUNCTION [dbo].[UserCollectionDetails_V2](@UserId UNIQUEIDENTIFIER)
+RETURNS TABLE
+AS RETURN
+SELECT
+    C.*,
+    COALESCE(CU.[ReadOnly], CG.[ReadOnly], 0) AS [ReadOnly],
+    COALESCE(CU.[HidePasswords], CG.[HidePasswords], 0) AS [HidePasswords],
+    COALESCE(CU.[Manage], CG.[Manage], 0) AS [Manage]
+FROM
+    [dbo].[CollectionView] C
+INNER JOIN
+    [dbo].[OrganizationUser] OU ON C.[OrganizationId] = OU.[OrganizationId]
+INNER JOIN
+    [dbo].[Organization] O ON O.[Id] = C.[OrganizationId]
+LEFT JOIN
+    [dbo].[CollectionUser] CU ON CU.[CollectionId] = C.[Id] AND CU.[OrganizationUserId] = [OU].[Id]
+LEFT JOIN
+    [dbo].[GroupUser] GU ON CU.[CollectionId] IS NULL AND GU.[OrganizationUserId] = OU.[Id]
+LEFT JOIN
+    [dbo].[Group] G ON G.[Id] = GU.[GroupId]
+LEFT JOIN
+    [dbo].[CollectionGroup] CG ON CG.[CollectionId] = C.[Id] AND CG.[GroupId] = GU.[GroupId]
+WHERE
+    OU.[UserId] = @UserId
+    AND OU.[Status] = 2 -- 2 = Confirmed
+    AND O.[Enabled] = 1
+    AND (
+        CU.[CollectionId] IS NOT NULL
+        OR CG.[CollectionId] IS NOT NULL
+    )
---
+++ src/Sql/dbo/Stored Procedures/User_BumpAccountRevisionDateByCipherId_V2.sql
@@ -0,0 +1,33 @@
+﻿CREATE PROCEDURE [dbo].[User_BumpAccountRevisionDateByCipherId_V2]
+    @CipherId UNIQUEIDENTIFIER,
+    @OrganizationId UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    UPDATE
+        U
+    SET
+        U.[AccountRevisionDate] = GETUTCDATE()
+    FROM
+        [dbo].[User] U
+    INNER JOIN
+        [dbo].[OrganizationUser] OU ON OU.[UserId] = U.[Id]
+    LEFT JOIN
+        [dbo].[CollectionCipher] CC ON CC.[CipherId] = @CipherId
+    LEFT JOIN
+        [dbo].[CollectionUser] CU ON CU.[OrganizationUserId] = OU.[Id] AND CU.[CollectionId] = CC.[CollectionId]
+    LEFT JOIN
+        [dbo].[GroupUser] GU ON CU.[CollectionId] IS NULL AND GU.[OrganizationUserId] = OU.[Id]
+    LEFT JOIN
+        [dbo].[Group] G ON G.[Id] = GU.[GroupId]
+    LEFT JOIN
+        [dbo].[CollectionGroup] CG ON CG.[GroupId] = GU.[GroupId] AND CG.[CollectionId] = CC.[CollectionId]
+    WHERE
+        OU.[OrganizationId] = @OrganizationId
+        AND OU.[Status] = 2 -- 2 = Confirmed
+        AND (
+            CU.[CollectionId] IS NOT NULL
+            OR CG.[CollectionId] IS NOT NULL
+        )
+END
