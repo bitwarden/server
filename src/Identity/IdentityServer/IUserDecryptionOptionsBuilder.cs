487354c8127c11ee273d58cf99181d29eb75b208
Andreas Coroiu <andreas.coroiu@gmail.com>
2023-10-27 15:10:48 +0200

    [PM-2023] feat: extract interface

---
+++ src/Identity/IdentityServer/IUserDecryptionOptionsBuilder.cs
@@ -0,0 +1,12 @@
+﻿using Bit.Core.Auth.Entities;
+using Bit.Core.Auth.Models.Api.Response;
+using Bit.Core.Entities;
+
+namespace Bit.Identity.IdentityServer;
+public interface IUserDecryptionOptionsBuilder
+{
+    Task<UserDecryptionOptions> BuildAsync();
+    IUserDecryptionOptionsBuilder ForUser(User user);
+    IUserDecryptionOptionsBuilder WithDevice(Device device);
+    IUserDecryptionOptionsBuilder WithSso(SsoConfig ssoConfig);
+}
