89fa57221fe130c61f63de22b3d62047ba428cac
Jake Fink <jfink@bitwarden.com>
2023-11-08 12:51:47 -0500

    add cipher and folder validators

---
+++ src/Api/Vault/Validators/CipherRotationValidator.cs
@@ -0,0 +1,42 @@
+﻿using Bit.Api.Auth.Validators;
+using Bit.Api.Vault.Models.Request;
+using Bit.Core.Entities;
+using Bit.Core.Exceptions;
+using Bit.Core.Vault.Entities;
+using Bit.Core.Vault.Repositories;
+
+namespace Bit.Api.Vault.Validators;
+
+public class CipherRotationValidator : IRotationValidator<IEnumerable<CipherWithIdRequestModel>, IEnumerable<Cipher>>
+{
+    private readonly ICipherRepository _cipherRepository;
+
+    public CipherRotationValidator(ICipherRepository cipherRepository)
+    {
+        _cipherRepository = cipherRepository;
+    }
+
+    public async Task<IEnumerable<Cipher>> ValidateAsync(User user, IEnumerable<CipherWithIdRequestModel> ciphers)
+    {
+        if (!ciphers.Any())
+        {
+            return null;
+        }
+
+        var existingCiphers = await _cipherRepository.GetManyByUserIdAsync(user.Id);
+        var result = new List<Cipher>();
+
+        foreach (var existing in existingCiphers)
+        {
+            var cipher = ciphers.FirstOrDefault(c => c.Id == existing.Id);
+            if (cipher == null)
+            {
+                throw new BadRequestException("All existing ciphers must be included in the rotation.");
+            }
+
+            result.Add(cipher.ToCipher(existing));
+        }
+
+        return result;
+    }
+}
---
+++ src/Api/Vault/Validators/FolderRotationValidator.cs
@@ -0,0 +1,42 @@
+﻿using Bit.Api.Auth.Validators;
+using Bit.Api.Vault.Models.Request;
+using Bit.Core.Entities;
+using Bit.Core.Exceptions;
+using Bit.Core.Vault.Entities;
+using Bit.Core.Vault.Repositories;
+
+namespace Bit.Api.Vault.Validators;
+
+public class FolderRotationValidator : IRotationValidator<IEnumerable<FolderWithIdRequestModel>, IEnumerable<Folder>>
+{
+    private readonly IFolderRepository _folderRepository;
+
+    public FolderRotationValidator(IFolderRepository folderRepository)
+    {
+        _folderRepository = folderRepository;
+    }
+
+    public async Task<IEnumerable<Folder>> ValidateAsync(User user, IEnumerable<FolderWithIdRequestModel> folders)
+    {
+        if (!folders.Any())
+        {
+            return null;
+        }
+
+        var existingFolders = await _folderRepository.GetManyByUserIdAsync(user.Id);
+        var result = new List<Folder>();
+
+        foreach (var existing in existingFolders)
+        {
+            var folder = folders.FirstOrDefault(c => c.Id == existing.Id);
+            if (folder == null)
+            {
+                throw new BadRequestException("All existing folders must be included in the rotation.");
+            }
+
+            result.Add(folder.ToFolder(existing));
+        }
+
+        return result;
+    }
+}
