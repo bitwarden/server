1527af70170e300015645af675fe6a25cfaf689f
Rui Tome <rtome@bitwarden.com>
2023-11-10 16:18:47 +0000

    [AC-1682] Added script to update [dbo].[CollectionUser] with [Manage] = 1 for all users with Manager role or 'EditAssignedCollections' permission

---
+++ util/Migrator/DbScripts/2023-11-10_00_ManagersEditAssignedCollectionUsers.sql
@@ -0,0 +1,12 @@
+-- Update [dbo].[CollectionUser] with [Manage] = 1 for all users with Manager role or 'EditAssignedCollections' permission
+UPDATE cu
+SET cu.[ReadOnly] = 0,
+    cu.[HidePasswords] = 0,
+    cu.[Manage] = 1
+FROM [dbo].[CollectionUser] cu
+JOIN [dbo].[Collection] c
+    ON cu.[CollectionId] = c.[Id]
+JOIN [dbo].[OrganizationUser] ou
+    ON cu.[OrganizationUserId] = ou.[Id]
+WHERE (ou.[Type] = 3 OR (ou.[Permissions] IS NOT NULL AND
+    ISJSON(ou.[Permissions]) > 0 AND JSON_VALUE(ou.[Permissions], '$.editAssignedCollections') = 'true'))
