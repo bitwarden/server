b75e08b03816d6b4612bac5f7f21e73e223db780
Conner Turnbull <cturnbull@bitwarden.com>
2023-11-14 10:22:53 -0500

    Removed index

--- util/Migrator/DbScripts/2023-11-14_00_2019FamilyPlanPremium.sql
+++ util/Migrator/DbScripts/2023-11-14_00_2019FamilyPlanPremium.sql
@@ -1,12 +1,3 @@
-IF NOT EXISTS(
-    SELECT *
-    FROM sys.indexes
-    WHERE name='IX_Organization_PlanType' AND object_id = OBJECT_ID('[dbo].[Organization]')
-)
-CREATE NONCLUSTERED INDEX [IX_Organization_PlanType]
-ON [dbo].[Organization] ([PlanType])
-WITH (ONLINE = ON);
-
 UPDATE
     [dbo].[Organization]
 SET
