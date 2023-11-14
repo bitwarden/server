6807237983756565ac380002c704962f5553aa89
Conner Turnbull <cturnbull@bitwarden.com>
2023-11-14 09:42:40 -0500

    Removed transaction and added a plan type index to organization

--- util/Migrator/DbScripts/2023-11-14_00_2019FamilyPlanPremium.sql
+++ util/Migrator/DbScripts/2023-11-14_00_2019FamilyPlanPremium.sql
@@ -1,16 +1,15 @@
-BEGIN TRY
-    BEGIN TRANSACTION;
+IF NOT EXISTS(
+    SELECT *
+    FROM sys.indexes
+    WHERE name='IX_Organization_PlanType' AND object_id = OBJECT_ID('[dbo].[Organization]')
+)
+CREATE NONCLUSTERED INDEX [IX_Organization_PlanType]
+ON [dbo].[Organization] ([PlanType])
+WITH (ONLINE = ON);
 
-    UPDATE
-        [dbo].[Organization]
-    SET
-        [UsersGetPremium] = 0
-    WHERE
-        [PlanType] = 1 -- Families 2019 Annual
-
-    COMMIT TRANSACTION;
-END TRY
-BEGIN CATCH
-    ROLLBACK TRANSACTION;
-    THROW;
-END CATCH
+UPDATE
+    [dbo].[Organization]
+SET
+    [UsersGetPremium] = 0
+WHERE
+    [PlanType] = 1 -- Families 2019 Annual
