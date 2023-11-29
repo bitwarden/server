e85ff6936fc766d1c4ca1845637cd0b6cce9ba79
Jared Snider <jsnider@bitwarden.com>
2023-11-29 14:52:56 -0500

    dotnet format

--- src/Core/Services/IMailService.cs
+++ src/Core/Services/IMailService.cs
@@ -1,7 +1,6 @@
-using Bit.Core.AdminConsole.Entities;
+﻿using Bit.Core.AdminConsole.Entities;
 using Bit.Core.AdminConsole.Entities.Provider;
 using Bit.Core.Auth.Entities;
-using Bit.Core.Auth.Models.Business;
 using Bit.Core.Entities;
 using Bit.Core.Models.Mail;
 
