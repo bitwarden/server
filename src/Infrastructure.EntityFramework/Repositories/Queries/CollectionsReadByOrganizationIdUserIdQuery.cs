3cfbe3beb0aba23fa4d692ec2e05c7fe77eac346
Thomas Rittson <trittson@bitwarden.com>
2023-12-01 15:45:31 +1000

    dotnet format

--- src/Infrastructure.EntityFramework/Repositories/Queries/CollectionsReadByOrganizationIdUserIdQuery.cs
+++ src/Infrastructure.EntityFramework/Repositories/Queries/CollectionsReadByOrganizationIdUserIdQuery.cs
@@ -17,27 +17,27 @@ public class CollectionsReadByOrganizationIdUserIdQuery : IQuery<Collection>
     public virtual IQueryable<Collection> Run(DatabaseContext dbContext)
     {
         var query = from c in dbContext.Collections
-            join o in dbContext.Organizations on c.OrganizationId equals o.Id
-            join ou in dbContext.OrganizationUsers
-                on new { OrganizationId = o.Id, UserId = (Guid?)_userId } equals
-                new { ou.OrganizationId, ou.UserId }
-            join cu in dbContext.CollectionUsers
-                on new { CollectionId = c.Id, OrganizationUserId = ou.Id } equals
-                new { cu.CollectionId, cu.OrganizationUserId } into cu_g
-            from cu in cu_g.DefaultIfEmpty()
-            join gu in dbContext.GroupUsers
-                on new { CollectionId = (Guid?)cu.CollectionId, OrganizationUserId = ou.Id } equals
-                new { CollectionId = (Guid?)null, gu.OrganizationUserId } into gu_g
-            from gu in gu_g.DefaultIfEmpty()
-            join g in dbContext.Groups on gu.GroupId equals g.Id into g_g
-            from g in g_g.DefaultIfEmpty()
-            join cg in dbContext.CollectionGroups
-                on new { CollectionId = c.Id, gu.GroupId } equals
-                new { cg.CollectionId, cg.GroupId } into cg_g
-            from cg in cg_g.DefaultIfEmpty()
-            where o.Id == _organizationId && o.Enabled && ou.Status == OrganizationUserStatusType.Confirmed
-                  && (!cu.ReadOnly || !cg.ReadOnly)
-            select c;
+                    join o in dbContext.Organizations on c.OrganizationId equals o.Id
+                    join ou in dbContext.OrganizationUsers
+                        on new { OrganizationId = o.Id, UserId = (Guid?)_userId } equals
+                        new { ou.OrganizationId, ou.UserId }
+                    join cu in dbContext.CollectionUsers
+                        on new { CollectionId = c.Id, OrganizationUserId = ou.Id } equals
+                        new { cu.CollectionId, cu.OrganizationUserId } into cu_g
+                    from cu in cu_g.DefaultIfEmpty()
+                    join gu in dbContext.GroupUsers
+                        on new { CollectionId = (Guid?)cu.CollectionId, OrganizationUserId = ou.Id } equals
+                        new { CollectionId = (Guid?)null, gu.OrganizationUserId } into gu_g
+                    from gu in gu_g.DefaultIfEmpty()
+                    join g in dbContext.Groups on gu.GroupId equals g.Id into g_g
+                    from g in g_g.DefaultIfEmpty()
+                    join cg in dbContext.CollectionGroups
+                        on new { CollectionId = c.Id, gu.GroupId } equals
+                        new { cg.CollectionId, cg.GroupId } into cg_g
+                    from cg in cg_g.DefaultIfEmpty()
+                    where o.Id == _organizationId && o.Enabled && ou.Status == OrganizationUserStatusType.Confirmed
+                          && (!cu.ReadOnly || !cg.ReadOnly)
+                    select c;
 
         return query;
     }
