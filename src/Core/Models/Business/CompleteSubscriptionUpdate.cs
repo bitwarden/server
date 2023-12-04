602a475682ebd7c39761d24c969166f5aedfba09
Alex Morask <amorask@bitwarden.com>
2023-12-04 10:56:37 -0500

    Fixing missing usings after master merge

--- src/Core/Models/Business/CompleteSubscriptionUpdate.cs
+++ src/Core/Models/Business/CompleteSubscriptionUpdate.cs
@@ -1,4 +1,4 @@
-﻿using Bit.Core.Entities;
+﻿using Bit.Core.AdminConsole.Entities;
 using Bit.Core.Enums;
 using Bit.Core.Exceptions;
 using Stripe;
--- test/Core.Test/Models/Business/CompleteSubscriptionUpdateTests.cs
+++ test/Core.Test/Models/Business/CompleteSubscriptionUpdateTests.cs
@@ -1,4 +1,4 @@
-﻿using Bit.Core.Entities;
+﻿using Bit.Core.AdminConsole.Entities;
 using Bit.Core.Enums;
 using Bit.Core.Models.Business;
 using Bit.Core.Test.AutoFixture.OrganizationFixtures;
