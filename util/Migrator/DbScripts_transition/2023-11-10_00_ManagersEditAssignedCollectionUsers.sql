4af3ada4f84346c05d32b2f7d5492397851c3d74
Rui Tome <rtome@bitwarden.com>
2023-12-06 15:56:35 +0000

    [AC-1682] Removed unnecessary Collection table join on ManagersEditAssignedCollectionUsers sql script

--- util/Migrator/DbScripts_transition/2023-11-10_00_ManagersEditAssignedCollectionUsers.sql
+++ util/Migrator/DbScripts_transition/2023-11-10_00_ManagersEditAssignedCollectionUsers.sql
@@ -4,9 +4,7 @@ SET cu.[ReadOnly] = 0,
     cu.[HidePasswords] = 0,
     cu.[Manage] = 1
 FROM [dbo].[CollectionUser] cu
-JOIN [dbo].[Collection] c
-    ON cu.[CollectionId] = c.[Id]
-JOIN [dbo].[OrganizationUser] ou
+INNER JOIN [dbo].[OrganizationUser] ou
     ON cu.[OrganizationUserId] = ou.[Id]
 WHERE (ou.[Type] = 3 OR (ou.[Permissions] IS NOT NULL AND
     ISJSON(ou.[Permissions]) > 0 AND JSON_VALUE(ou.[Permissions], '$.editAssignedCollections') = 'true'))
