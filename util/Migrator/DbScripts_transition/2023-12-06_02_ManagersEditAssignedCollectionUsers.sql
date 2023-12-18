7d718b677b8888b60fe714b6a0583c9523825a2f
Rui Tome <rtome@bitwarden.com>
2023-12-18 14:54:57 +0000

    [AC-1682] Add new rows to CollectionUser for Managers and users with EditAssignedCollections permission assigned to groups with collection access

--- util/Migrator/DbScripts_transition/2023-12-06_02_ManagersEditAssignedCollectionUsers.sql
+++ util/Migrator/DbScripts_transition/2023-12-06_02_ManagersEditAssignedCollectionUsers.sql
@@ -8,3 +8,18 @@ INNER JOIN [dbo].[OrganizationUser] ou
     ON cu.[OrganizationUserId] = ou.[Id]
 WHERE (ou.[Type] = 3 OR (ou.[Permissions] IS NOT NULL AND
     ISJSON(ou.[Permissions]) > 0 AND JSON_VALUE(ou.[Permissions], '$.editAssignedCollections') = 'true'))
+
+-- Insert rows to [dbo].[CollectionUser] for Managers and users with 'EditAssignedCollections' permission assigned to groups with collection access
+INSERT INTO [dbo].[CollectionUser] ([CollectionId], [OrganizationUserId], [ReadOnly], [HidePasswords], [Manage])
+SELECT cg.[CollectionId], ou.[Id], 0, 0, 1
+FROM [dbo].[CollectionGroup] cg
+INNER JOIN [dbo].[GroupUser] gu
+    ON cg.GroupId = gu.GroupId
+INNER JOIN [dbo].[OrganizationUser] ou
+    ON gu.OrganizationUserId = ou.[Id]
+WHERE (ou.[Type] = 3 OR
+    (ou.[Permissions] IS NOT NULL AND ISJSON(ou.[Permissions]) > 0 AND JSON_VALUE(ou.[Permissions], '$.editAssignedCollections') = 'true'))
+  AND NOT EXISTS (
+    SELECT 1 FROM [dbo].[CollectionUser] cu
+    WHERE cu.[CollectionId] = cg.[CollectionId] AND cu.[OrganizationUserId] = ou.[Id]
+  )
