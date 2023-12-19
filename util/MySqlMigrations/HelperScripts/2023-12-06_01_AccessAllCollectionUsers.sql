c5f1be4d79a9548f45c2bf88ffae0fd1ca969168
Rui Tome <rtome@bitwarden.com>
2023-12-19 19:49:57 +0000

    [AC-1682] MySql migrations

--- util/MySqlMigrations/HelperScripts/2023-12-06_01_AccessAllCollectionUsers.sql
+++ util/MySqlMigrations/HelperScripts/2023-12-06_01_AccessAllCollectionUsers.sql
@@ -1,26 +1,21 @@
 -- Update existing rows in CollectionUsers
 UPDATE CollectionUsers AS target
-INNER JOIN (
-    SELECT C.Id AS CollectionId, T.OrganizationUserId
-    FROM Collection C
-    INNER JOIN OrganizationUser T ON C.OrganizationId = T.OrganizationId
-    WHERE T.AccessAll = 1
-) AS source
-    ON target.CollectionId = source.CollectionId AND target.OrganizationUserId = source.OrganizationUserId
+INNER JOIN Collection AS C ON target.CollectionId = C.Id
+INNER JOIN OrganizationUser AS OU ON C.OrganizationId = OU.OrganizationId
 SET
     target.ReadOnly = 0,
     target.HidePasswords = 0,
-    target.Manage = 0;
+    target.Manage = 0
+WHERE OU.AccessAll = 1;
 
 -- Insert new rows into CollectionUsers
 INSERT INTO CollectionUsers (CollectionId, OrganizationUserId, ReadOnly, HidePasswords, Manage)
-SELECT source.CollectionId, source.OrganizationUserId, 0, 0, 0
-FROM (
-         SELECT C.Id AS CollectionId, T.OrganizationUserId
-         FROM Collection C
-                  INNER JOIN OrganizationUser T ON C.OrganizationId = T.OrganizationId
-         WHERE T.AccessAll = 1
-     ) AS source
-LEFT JOIN CollectionUsers AS target
-    ON target.CollectionId = source.CollectionId AND target.OrganizationUserId = source.OrganizationUserId
-WHERE target.CollectionId IS NULL;
+SELECT C.Id AS CollectionId, OU.Id AS OrganizationUserId, 0, 0, 0
+FROM Collection AS C
+INNER JOIN OrganizationUser AS OU ON C.OrganizationId = OU.OrganizationId
+WHERE OU.AccessAll = 1
+  AND NOT EXISTS (
+    SELECT 1
+    FROM CollectionUsers AS CU
+    WHERE CU.CollectionId = C.Id AND CU.OrganizationUserId = OU.Id
+);
--- util/MySqlMigrations/Migrations/20231219154701_FCAccessAllCollectionGroups.cs
+++ util/MySqlMigrations/Migrations/20231219154701_FCAccessAllCollectionGroups.cs
@@ -5,17 +5,13 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Bit.MySqlMigrations.Migrations;
 
-public partial class FlexibleCollections : Migration
+public partial class FCAccessAllCollectionGroups : Migration
 {
     private const string _accessAllCollectionGroupsScript = "MySqlMigrations.HelperScripts.2023-12-06_00_AccessAllCollectionGroups.sql";
-    private const string _accessAllCollectionUsersScript = "MySqlMigrations.HelperScripts.2023-12-06_01_AccessAllCollectionUsers.sql";
-    private const string _managersEditAssignedCollectionUsersScript = "MySqlMigrations.HelperScripts.2023-12-06_02_ManagersEditAssignedCollectionUsers.sql";
 
     protected override void Up(MigrationBuilder migrationBuilder)
     {
         migrationBuilder.Sql(CoreHelpers.GetEmbeddedResourceContentsAsync(_accessAllCollectionGroupsScript));
-        migrationBuilder.Sql(CoreHelpers.GetEmbeddedResourceContentsAsync(_accessAllCollectionUsersScript));
-        migrationBuilder.Sql(CoreHelpers.GetEmbeddedResourceContentsAsync(_managersEditAssignedCollectionUsersScript));
     }
 
     protected override void Down(MigrationBuilder migrationBuilder)
