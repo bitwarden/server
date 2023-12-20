73a13150f7e32a0e073d55d9bebe032509c81615
Rui Tome <rtome@bitwarden.com>
2023-12-20 12:25:55 +0000

    [AC-1682] Sqlite migrations + dotnet format

---
+++ util/SqliteMigrations/Migrations/20231219154710_FCAccessAllCollectionGroups.cs
@@ -0,0 +1,21 @@
+﻿using Bit.Core.Utilities;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.SqliteMigrations.Migrations;
+
+public partial class FCAccessAllCollectionGroups : Migration
+{
+    private const string _accessAllCollectionGroupsScript = "SqliteMigrations.HelperScripts.2023-12-06_00_AccessAllCollectionGroups.sql";
+
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(CoreHelpers.GetEmbeddedResourceContentsAsync(_accessAllCollectionGroupsScript));
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        throw new Exception("Irreversible migration");
+    }
+}
