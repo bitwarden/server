c1629d940e4a7fe1c9dc02f1bcd20f37bdffdca6
Rui Tome <rtome@bitwarden.com>
2023-12-20 11:57:36 +0000

    [AC-1682] Postgres migrations

---
+++ util/PostgresMigrations/Migrations/20231219154705_FCAccessAllCollectionGroups.cs
@@ -0,0 +1,21 @@
+﻿using Bit.Core.Utilities;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.PostgresMigrations.Migrations;
+
+public partial class FCAccessAllCollectionGroups : Migration
+{
+    private const string _accessAllCollectionGroupsScript = "PostgresMigrations.HelperScripts.2023-12-06_00_AccessAllCollectionGroups.psql";
+
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(CoreHelpers.GetEmbeddedResourceContentsAsync(_accessAllCollectionGroupsScript));
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        throw new Exception("Irreversible migration");
+    }
+}
