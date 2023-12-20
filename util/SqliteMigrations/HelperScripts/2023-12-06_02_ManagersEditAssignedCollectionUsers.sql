c2a43f5e7e17c91da735213697cd178cce375f2b
Rui Tome <rtome@bitwarden.com>
2023-12-20 12:25:55 +0000

    [AC-1682] Sqlite migrations + dotnet format

--- util/SqliteMigrations/HelperScripts/2023-12-06_02_ManagersEditAssignedCollectionUsers.sql
+++ util/SqliteMigrations/HelperScripts/2023-12-06_02_ManagersEditAssignedCollectionUsers.sql
@@ -1,12 +1,28 @@
--- Update "CollectionUser" with "Manage" = 1 for all users with Manager role or 'EditAssignedCollections' permission
-UPDATE "CollectionUsers"
-SET "ReadOnly" = 0,
+-- Update `CollectionUser` with `Manage` = 1 for all users with Manager role or 'EditAssignedCollections' permission
+UPDATE "CollectionUsers" AS cu
+SET
+    "ReadOnly" = 0,
     "HidePasswords" = 0,
     "Manage" = 1
 WHERE "OrganizationUserId" IN (
-    SELECT cu."OrganizationUserId"
-    FROM "CollectionUsers" cu
-             INNER JOIN "OrganizationUser" ou ON cu."OrganizationUserId" = ou."Id"
-    WHERE ou."Type" = 3 OR (ou."Permissions" IS NOT NULL AND
-                            JSON_VALID(ou."Permissions") AND json_extract(ou."Permissions", '$.editAssignedCollections') = 'true')
+    SELECT ou."Id"
+    FROM "OrganizationUser" AS ou
+    WHERE ou."Type" = 3 OR (
+        ou."Permissions" IS NOT NULL AND
+        JSON_VALID(ou."Permissions") > 0 AND
+        JSON_EXTRACT(ou."Permissions", '$.editAssignedCollections') = 'true'
+        )
+);
+
+-- Insert rows to CollectionUser for Managers and users with 'EditAssignedCollections' permission assigned to groups with collection access
+INSERT INTO "CollectionUsers" ("CollectionId", "OrganizationUserId", "ReadOnly", "HidePasswords", "Manage")
+SELECT cg."CollectionId", ou."Id", 0, 0, 1
+FROM "CollectionGroups" AS cg
+         INNER JOIN "GroupUser" AS gu ON cg."GroupId" = gu."GroupId"
+         INNER JOIN "OrganizationUser" AS ou ON gu."OrganizationUserId" = ou."Id"
+WHERE (ou."Type" = 3 OR
+       (ou."Permissions" IS NOT NULL AND JSON_VALID(ou."Permissions") > 0 AND JSON_EXTRACT(ou."Permissions", '$.editAssignedCollections')) = 'true')
+  AND NOT EXISTS (
+    SELECT 1 FROM "CollectionUsers" AS cu
+    WHERE cu."CollectionId" = cg."CollectionId" AND cu."OrganizationUserId" = ou."Id"
 );
