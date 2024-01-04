0d24d16ae7f51ea62f8c4ac48ee95886f9d75eb7
Kyle Spearrin <kyle.spearrin@gmail.com>
2024-01-04 10:30:57 -0500

    cosmos grant repo

---
+++ src/Core/Auth/Repositories/Cosmos/GrantRepository.cs
@@ -0,0 +1,44 @@
+﻿using Azure.Identity;
+using Bit.Core.Auth.Models.Data;
+using Bit.Core.Settings;
+using Microsoft.Azure.Cosmos;
+
+namespace Bit.Core.Auth.Repositories.Cosmos;
+
+public class GrantRepository : IGrantRepository
+{
+    private readonly CosmosClient _client;
+    private readonly Database _database;
+    private readonly Container _container;
+
+    public GrantRepository(GlobalSettings globalSettings)
+        : this(globalSettings.IdentityServer.CosmosConnectionString)
+    { }
+
+    public GrantRepository(string cosmosConnectionString)
+    {
+        _client = new CosmosClient(cosmosConnectionString, new DefaultAzureCredential());
+        _database = _client.GetDatabase("identity");
+        _container = _database.GetContainer("grant");
+    }
+
+    public async Task<IGrant> GetByKeyAsync(string key)
+    {
+        var response = await _container.ReadItemAsync<IGrant>(key, new PartitionKey(key));
+        return response.Resource;
+    }
+
+    public Task<ICollection<IGrant>> GetManyAsync(string subjectId, string sessionId, string clientId, string type) => throw new NotImplementedException();
+
+    public async Task SaveAsync(IGrant obj)
+    {
+        await _container.UpsertItemAsync(obj, new PartitionKey(obj.Key));
+    }
+
+    public async Task DeleteByKeyAsync(string key)
+    {
+        await _container.DeleteItemAsync<IGrant>(key, new PartitionKey(key));
+    }
+
+    public Task DeleteManyAsync(string subjectId, string sessionId, string clientId, string type) => throw new NotImplementedException();
+}
