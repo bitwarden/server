a4e0d87c5bc4fc4e3cae932a1dd231b6ece48723
Shane Melton <smelton@bitwarden.com>
2024-01-08 17:04:29 -0800

    [AC-1124] Update CiphersController and response models - Add new endpoint for assigned ciphers - Update existing endpoint to only return all ciphers when feature flag is enabled the user has access

--- src/Core/Vault/Models/Data/CipherDetails.cs
+++ src/Core/Vault/Models/Data/CipherDetails.cs
@@ -39,5 +39,27 @@ public class CipherDetailsWithCollections : CipherDetails
             : Array.Empty<Guid>();
     }
 
+    public CipherDetailsWithCollections(CipherOrganizationDetails cipher, Dictionary<Guid, IGrouping<Guid, CollectionCipher>> collectionCiphersGroupDict)
+    {
+        Id = cipher.Id;
+        UserId = cipher.UserId;
+        OrganizationId = cipher.OrganizationId;
+        Type = cipher.Type;
+        Data = cipher.Data;
+        Favorites = cipher.Favorites;
+        Folders = cipher.Folders;
+        Attachments = cipher.Attachments;
+        CreationDate = cipher.CreationDate;
+        RevisionDate = cipher.RevisionDate;
+        DeletedDate = cipher.DeletedDate;
+        Reprompt = cipher.Reprompt;
+        Key = cipher.Key;
+        OrganizationUseTotp = cipher.OrganizationUseTotp;
+
+        CollectionIds = collectionCiphersGroupDict != null && collectionCiphersGroupDict.TryGetValue(Id, out var value)
+            ? value.Select(cc => cc.CollectionId)
+            : Array.Empty<Guid>();
+    }
+
     public IEnumerable<Guid> CollectionIds { get; set; }
 }
