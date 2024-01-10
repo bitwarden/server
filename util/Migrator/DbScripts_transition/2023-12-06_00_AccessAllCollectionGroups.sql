86fc3903000db6364d0c1211c2128d5d820b8747
Rui Tome <rtome@bitwarden.com>
2024-01-10 11:34:31 +0000

    [AC-1682] Update FC data migration scripts to clear AccessAll flags and set all Managers to Users

--- util/Migrator/DbScripts_transition/2023-12-06_00_AccessAllCollectionGroups.sql
+++ util/Migrator/DbScripts_transition/2023-12-06_00_AccessAllCollectionGroups.sql
@@ -1,10 +1,10 @@
--- Create a temporary table to store the groups with AccessAll = 1
+-- Step 1: Create a temporary table to store the Groups with AccessAll = 1
 SELECT [Id] AS [GroupId], [OrganizationId]
 INTO #TempGroup
 FROM [dbo].[Group]
 WHERE [AccessAll] = 1;
 
--- Update existing rows in [dbo].[CollectionGroup]
+-- Step 2: Update existing rows in [dbo].[CollectionGroup]
 UPDATE CG
 SET
     CG.[ReadOnly] = 0,
@@ -15,7 +15,7 @@ INNER JOIN [dbo].[Collection] C ON CG.[CollectionId] = C.[Id]
 INNER JOIN #TempGroup TG ON CG.[GroupId] = TG.[GroupId]
 WHERE C.[OrganizationId] = TG.[OrganizationId];
 
--- Insert new rows into [dbo].[CollectionGroup]
+-- Step 3: Insert new rows into [dbo].[CollectionGroup]
 INSERT INTO [dbo].[CollectionGroup] ([CollectionId], [GroupId], [ReadOnly], [HidePasswords], [Manage])
 SELECT C.[Id], TG.[GroupId], 0, 0, 0
 FROM [dbo].[Collection] C
@@ -23,5 +23,11 @@ FROM [dbo].[Collection] C
     LEFT JOIN [dbo].[CollectionGroup] CG ON CG.[CollectionId] = C.[Id] AND CG.[GroupId] = TG.[GroupId]
 WHERE CG.[CollectionId] IS NULL;
 
--- Drop the temporary table
+-- Step 4: Update [dbo].[Group] to clear AccessAll flag
+UPDATE G
+SET [AccessAll] = 0
+FROM [dbo].[Group] G
+INNER JOIN #TempGroup TG ON G.[Id] = TG.[GroupId]
+
+-- Step 5: Drop the temporary table
 DROP TABLE #TempGroup;
