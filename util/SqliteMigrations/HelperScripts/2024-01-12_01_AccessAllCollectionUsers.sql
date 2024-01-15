e657a40214851f28997f9810b607582ee317407e
Rui Tome <rtome@bitwarden.com>
2024-01-15 16:33:08 +0000

    [AC-1682] sqlite data migrations

--- util/SqliteMigrations/HelperScripts/2024-01-12_01_AccessAllCollectionUsers.sql
+++ util/SqliteMigrations/HelperScripts/2024-01-12_01_AccessAllCollectionUsers.sql
@@ -1,4 +1,10 @@
--- Update existing rows in CollectionUsers
+-- Step 1: Create a temporary table
+CREATE TEMPORARY TABLE IF NOT EXISTS TempOrgUser AS
+SELECT "Id" AS "OrganizationUserId", "OrganizationId"
+FROM "OrganizationUser"
+WHERE "AccessAll" = 1;
+
+-- Step 2: Update existing rows in CollectionUsers
 UPDATE "CollectionUsers"
 SET
     "ReadOnly" = 0,
@@ -7,18 +13,34 @@ SET
 WHERE "CollectionId" IN (
     SELECT "C"."Id"
     FROM "Collection" "C"
-             INNER JOIN "OrganizationUser" "OU" ON "C"."OrganizationId" = "OU"."OrganizationId"
-    WHERE "OU"."AccessAll" = 1
+    INNER JOIN TempOrgUser "OU" ON "C"."OrganizationId" = "OU"."OrganizationId"
 );
 
--- Insert new rows into CollectionUsers
+-- Step 3: Insert new rows into CollectionUsers
 INSERT INTO "CollectionUsers" ("CollectionId", "OrganizationUserId", "ReadOnly", "HidePasswords", "Manage")
-SELECT "C"."Id" AS "CollectionId", "OU"."Id" AS "OrganizationUserId", 0, 0, 0
-FROM "Collection" "C"
-         INNER JOIN "OrganizationUser" "OU" ON "C"."OrganizationId" = "OU"."OrganizationId"
-WHERE "OU"."AccessAll" = 1
-  AND NOT EXISTS (
+SELECT C."Id" AS "CollectionId", OU."OrganizationUserId", 0, 0, 0
+FROM "Collection" C
+JOIN TempOrgUser OU ON C."OrganizationId" = OU."OrganizationId"
+WHERE NOT EXISTS (
     SELECT 1
     FROM "CollectionUsers" "CU"
-    WHERE "CU"."CollectionId" = "C"."Id" AND "CU"."OrganizationUserId" = "OU"."Id"
+    WHERE "CU"."CollectionId" = "C"."Id" AND "CU"."OrganizationUserId" = "OU"."OrganizationUserId"
 );
+
+-- Step 4: Update OrganizationUser to clear AccessAll flag
+UPDATE "OrganizationUser"
+SET "AccessAll" = 0
+WHERE "Id" IN (SELECT "OrganizationUserId" FROM TempOrgUser);
+
+-- Step 5: Update "User" AccountRevisionDate for each unique OrganizationUserId
+UPDATE "User"
+SET "AccountRevisionDate" = CURRENT_TIMESTAMP
+WHERE "Id" IN (
+    SELECT OU."UserId"
+    FROM "OrganizationUser" OU
+    JOIN TempOrgUser TOU ON OU."Id" = TOU."OrganizationUserId"
+    WHERE OU."Status" = 2
+);
+
+-- Step 6: Drop the temporary table
+DROP TABLE IF EXISTS TempOrgUser;
