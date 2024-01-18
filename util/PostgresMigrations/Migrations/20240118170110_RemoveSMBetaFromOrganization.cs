5f8897767854e007f66de3a48ba47bcc7e9dd16f
Colton Hurst <colton@coltonhurst.com>
2024-01-18 12:31:41 -0500

    SM-1012: Dotnet format

--- util/PostgresMigrations/Migrations/20240118170110_RemoveSMBetaFromOrganization.cs
+++ util/PostgresMigrations/Migrations/20240118170110_RemoveSMBetaFromOrganization.cs
@@ -2,28 +2,27 @@
 
 #nullable disable
 
-namespace Bit.PostgresMigrations.Migrations
+namespace Bit.PostgresMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class RemoveSMBetaFromOrganization : Migration
 {
     /// <inheritdoc />
-    public partial class RemoveSMBetaFromOrganization : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "SecretsManagerBeta",
-                table: "Organization");
-        }
+        migrationBuilder.DropColumn(
+            name: "SecretsManagerBeta",
+            table: "Organization");
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<bool>(
-                name: "SecretsManagerBeta",
-                table: "Organization",
-                type: "boolean",
-                nullable: false,
-                defaultValue: false);
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<bool>(
+            name: "SecretsManagerBeta",
+            table: "Organization",
+            type: "boolean",
+            nullable: false,
+            defaultValue: false);
     }
 }
