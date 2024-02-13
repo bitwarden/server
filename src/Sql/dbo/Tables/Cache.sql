726ab3fa5bd08a627a4dde308a8f4473e083e162
Kyle Spearrin <kyle.spearrin@gmail.com>
2024-02-13 08:01:31 -0500

    sqlserver cache table

---
+++ src/Sql/dbo/Tables/Cache.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Cache] (
+    [Id] [nvarchar](449) NOT NULL,
+    [Value] [varbinary](max) NOT NULL,
+    [ExpiresAtTime] [datetimeoffset](7) NOT NULL,
+    [SlidingExpirationInSeconds] [bigint] NULL,
+    [AbsoluteExpiration] [datetimeoffset](7) NULL,
+    CONSTRAINT [PK_Cache] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+GO
+
+CREATE NONCLUSTERED INDEX [IX_Cache_ExpiresAtTime]
+    ON [dbo].[Cache]([ExpiresAtTime] ASC);
+GO
+
