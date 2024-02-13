f236d4fa811eb1a3b428206c97a407e45b8aeb45
Kyle Spearrin <kyle.spearrin@gmail.com>
2024-02-13 09:03:48 -0500

    ef cache

---
+++ src/Infrastructure.EntityFramework/EntityFrameworkCache.cs
@@ -0,0 +1,71 @@
+﻿using Bit.Infrastructure.EntityFramework.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Internal;
+
+namespace Bit.Infrastructure.EntityFramework;
+
+public class EntityFrameworkCache : IDistributedCache
+{
+    private readonly DatabaseContext _databaseContext;
+    private readonly ISystemClock _systemClock;
+    private readonly TimeSpan _expiredItemsDeletionInterval = TimeSpan.FromMinutes(30);
+    private DateTimeOffset _lastExpirationScan;
+    private readonly Action _deleteExpiredCachedItemsDelegate;
+    private readonly TimeSpan _defaultSlidingExpiration = TimeSpan.FromMinutes(20);
+    private readonly object _mutex = new();
+
+    public EntityFrameworkCache(DatabaseContext databaseContext)
+    {
+        _databaseContext = databaseContext;
+        _systemClock = new SystemClock();
+        _deleteExpiredCachedItemsDelegate = DeleteExpiredCacheItems;
+    }
+
+    public byte[] Get(string key)
+    {
+        ArgumentNullException.ThrowIfNull(key);
+        var cache = _databaseContext.Cache
+            .Where(c => c.Id == key && c.ExpiresAtTime >= _systemClock.UtcNow)
+            .SingleOrDefault();
+        ScanForExpiredItemsIfRequired();
+        return cache?.Value;
+    }
+
+    public async Task<byte[]> GetAsync(string key, CancellationToken token = default)
+    {
+        ArgumentNullException.ThrowIfNull(key);
+        token.ThrowIfCancellationRequested();
+        var cache = await _databaseContext.Cache
+            .Where(c => c.Id == key && c.ExpiresAtTime >= _systemClock.UtcNow)
+            .SingleOrDefaultAsync();
+        ScanForExpiredItemsIfRequired();
+        return cache?.Value;
+    }
+    public void Refresh(string key) => throw new NotImplementedException();
+    public Task RefreshAsync(string key, CancellationToken token = default) => throw new NotImplementedException();
+    public void Remove(string key) => throw new NotImplementedException();
+    public Task RemoveAsync(string key, CancellationToken token = default) => throw new NotImplementedException();
+    public void Set(string key, byte[] value, DistributedCacheEntryOptions options) => throw new NotImplementedException();
+    public Task SetAsync(string key, byte[] value, DistributedCacheEntryOptions options, CancellationToken token = default) => throw new NotImplementedException();
+
+    private void ScanForExpiredItemsIfRequired()
+    {
+        lock (_mutex)
+        {
+            var utcNow = _systemClock.UtcNow;
+            if ((utcNow - _lastExpirationScan) > _expiredItemsDeletionInterval)
+            {
+                _lastExpirationScan = utcNow;
+                Task.Run(_deleteExpiredCachedItemsDelegate);
+            }
+        }
+    }
+
+    private void DeleteExpiredCacheItems()
+    {
+        _databaseContext.Cache
+            .Where(c => c.ExpiresAtTime < _systemClock.UtcNow)
+            .ExecuteDelete();
+    }
+}
