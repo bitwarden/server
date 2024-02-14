b653cea99b0c05b0491c8ea1335158093edb11eb
Kyle Spearrin <kyle.spearrin@gmail.com>
2024-02-14 08:29:03 -0500

    format

--- util/SqliteMigrations/Migrations/20240214022835_DistributedCache.cs
+++ util/SqliteMigrations/Migrations/20240214022835_DistributedCache.cs
@@ -1,42 +1,40 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Bit.SqliteMigrations.Migrations
+namespace Bit.SqliteMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class DistributedCache : Migration
 {
     /// <inheritdoc />
-    public partial class DistributedCache : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "Cache",
-                columns: table => new
-                {
-                    Id = table.Column<string>(type: "TEXT", maxLength: 449, nullable: false),
-                    Value = table.Column<byte[]>(type: "BLOB", nullable: true),
-                    ExpiresAtTime = table.Column<DateTime>(type: "TEXT", nullable: false),
-                    SlidingExpirationInSeconds = table.Column<long>(type: "INTEGER", nullable: true),
-                    AbsoluteExpiration = table.Column<DateTime>(type: "TEXT", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Cache", x => x.Id);
-                });
+        migrationBuilder.CreateTable(
+            name: "Cache",
+            columns: table => new
+            {
+                Id = table.Column<string>(type: "TEXT", maxLength: 449, nullable: false),
+                Value = table.Column<byte[]>(type: "BLOB", nullable: true),
+                ExpiresAtTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                SlidingExpirationInSeconds = table.Column<long>(type: "INTEGER", nullable: true),
+                AbsoluteExpiration = table.Column<DateTime>(type: "TEXT", nullable: true)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Cache", x => x.Id);
+            });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Cache_ExpiresAtTime",
-                table: "Cache",
-                column: "ExpiresAtTime");
-        }
+        migrationBuilder.CreateIndex(
+            name: "IX_Cache_ExpiresAtTime",
+            table: "Cache",
+            column: "ExpiresAtTime");
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "Cache");
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "Cache");
     }
 }
