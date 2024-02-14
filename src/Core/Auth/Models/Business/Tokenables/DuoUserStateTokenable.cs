f8e8a83d736bbd053ec1692e346a7f651d777d42
Jake Fink <jfink@bitwarden.com>
2024-02-14 16:35:54 -0500

    add duo state to 2fa

---
+++ src/Core/Auth/Models/Business/Tokenables/DuoUserStateTokenable.cs
@@ -0,0 +1,37 @@
+﻿using Bit.Core.Entities;
+using Bit.Core.Tokens;
+using Newtonsoft.Json;
+
+namespace Bit.Core.Auth.Models.Business.Tokenables;
+
+public class DuoUserStateTokenable : Tokenable
+{
+    public const string ClearTextPrefix = "BwDuoUserId";
+    public const string DataProtectorPurpose = "DuoUserIdTokenDataProtector";
+    public const string TokenIdentifier = "DuoUserIdToken";
+    public string Identifier { get; set; } = TokenIdentifier;
+    public Guid Id { get; set; }
+
+    public override bool Valid => Identifier == TokenIdentifier &&
+                                  Id != default;
+
+    [JsonConstructor]
+    public DuoUserStateTokenable()
+    {
+    }
+
+    public DuoUserStateTokenable(User user)
+    {
+        Id = user?.Id ?? default;
+    }
+
+    public bool TokenIsValid(User user)
+    {
+        if (Id == default || user == null)
+        {
+            return false;
+        }
+
+        return Id == user.Id;
+    }
+}
