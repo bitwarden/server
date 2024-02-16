4648ee0a421cfd04fd5b99cbd35348690a55be66
Alex Morask <amorask@bitwarden.com>
2024-02-16 10:14:37 -0500

    Revert "[AC-1980] Upgrade Stripe.net (#3596)"
    This reverts commit 2ad4bb8a79c5ec67aa83ff0b885a0278eddc83f4.

--- src/Billing/Models/StripeWebhookVersionContainer.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace Bit.Billing.Models;
-
-public class StripeWebhookVersionContainer
-{
-    [JsonPropertyName("api_version")]
-    public string ApiVersion { get; set; }
-}
