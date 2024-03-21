c5e6c3bc93dd7c970c8b531e15b115cdff0ac259
Rui Tome <rtome@bitwarden.com>
2024-03-21 13:28:35 +0000

    [AC-1682] Revert factories changes

--- util/PostgresMigrations/Factories.cs
+++ util/PostgresMigrations/Factories.cs
@@ -35,7 +35,7 @@ public class DatabaseContextFactory : IDesignTimeDbContextFactory<DatabaseContex
         var globalSettings = new GlobalSettingsFactory(args)
             .GlobalSettings;
         var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
-        var connectionString = globalSettings.PostgreSql?.ConnectionString ?? Environment.GetEnvironmentVariable("CONN_STR");
+        var connectionString = globalSettings.PostgreSql?.ConnectionString;
         if (string.IsNullOrWhiteSpace(connectionString))
         {
             throw new Exception("No Postgres connection string found.");
