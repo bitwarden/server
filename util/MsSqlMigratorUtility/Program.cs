15d66bd074e735162446c20fe1ec5adbd48ef0b0
Michal Checinski <mchecinski@bitwarden.com>
2024-03-22 16:18:10 +0100

    Fix after merge

--- util/MsSqlMigratorUtility/Program.cs
+++ util/MsSqlMigratorUtility/Program.cs
@@ -18,7 +18,7 @@ internal class Program
         string folderName = MigratorConstants.DefaultMigrationsFolderName,
         [Option("dry-run", Description = "Print the scripts that will be applied without actually executing them")]
         bool dryRun = false
-        ) => MigrateDatabase(databaseConnectionString, verbose, repeatable, folderName, dryRun);
+        ) => MigrateDatabase(databaseConnectionString, repeatable, folderName, dryRun);
 
     private static bool MigrateDatabase(string databaseConnectionString,
         bool repeatable = false, string folderName = "", bool dryRun = false)
