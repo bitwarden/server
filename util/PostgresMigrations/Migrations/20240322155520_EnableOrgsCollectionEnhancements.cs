61f611ed9d64999ae2bd0dbaff91e2c7b598462c
Rui Tome <rtome@bitwarden.com>
2024-03-25 13:59:28 +0000

    [AC-1682] Added Postgres migration and script
    (cherry picked from commit 9bde1604da8432a6066fc6b48e88738fdc171869)

---
+++ util/PostgresMigrations/Migrations/20240322155520_EnableOrgsCollectionEnhancements.cs
@@ -0,0 +1,22 @@
+﻿using Bit.Core.Utilities;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.PostgresMigrations.Migrations
+{
+    public partial class EnableOrgsCollectionEnhancements : Migration
+    {
+        private const string _enableOrgsCollectionEnhancementsScript = "PostgresMigrations.HelperScripts.2024-03-22_00_EnableOrgsCollectionEnhancements.psql";
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(CoreHelpers.GetEmbeddedResourceContentsAsync(_enableOrgsCollectionEnhancementsScript));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            throw new Exception("Irreversible migration");
+        }
+    }
+}
