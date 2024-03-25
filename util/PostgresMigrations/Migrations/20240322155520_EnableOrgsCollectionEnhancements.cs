95e76f823d90c1a18935d495a664fa0d4dcf3e89
Rui Tome <rtome@bitwarden.com>
2024-03-25 15:28:46 +0000

    [AC-1682] dotnet format
    (cherry picked from commit 00eea0621c7c1092ea51c936fe2e7389a46109b9)

--- util/PostgresMigrations/Migrations/20240322155520_EnableOrgsCollectionEnhancements.cs
+++ util/PostgresMigrations/Migrations/20240322155520_EnableOrgsCollectionEnhancements.cs
@@ -3,20 +3,19 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Bit.PostgresMigrations.Migrations
+namespace Bit.PostgresMigrations.Migrations;
+
+public partial class EnableOrgsCollectionEnhancements : Migration
 {
-    public partial class EnableOrgsCollectionEnhancements : Migration
+    private const string _enableOrgsCollectionEnhancementsScript = "PostgresMigrations.HelperScripts.2024-03-22_00_EnableOrgsCollectionEnhancements.psql";
+
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        private const string _enableOrgsCollectionEnhancementsScript = "PostgresMigrations.HelperScripts.2024-03-22_00_EnableOrgsCollectionEnhancements.psql";
+        migrationBuilder.Sql(CoreHelpers.GetEmbeddedResourceContentsAsync(_enableOrgsCollectionEnhancementsScript));
+    }
 
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.Sql(CoreHelpers.GetEmbeddedResourceContentsAsync(_enableOrgsCollectionEnhancementsScript));
-        }
-
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            throw new Exception("Irreversible migration");
-        }
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        throw new Exception("Irreversible migration");
     }
 }
