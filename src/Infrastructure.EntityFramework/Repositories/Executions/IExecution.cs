6edf1e017a6b4806d7e10b4acc07b1bda7ef55ba
Rui Tome <rtome@bitwarden.com>
2024-03-28 16:17:33 +0000

    [AC-1682] Moved EF data migration scripts to Infrastructure.EntityFramework project and added an interface (IExecution) to run them against the database

---
+++ src/Infrastructure.EntityFramework/Repositories/Executions/IExecution.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bit.Infrastructure.EntityFramework.Repositories.Executions;
+
+public interface IExecution
+{
+    /// <summary>
+    /// Executes a command against the database.
+    /// </summary>
+    /// <returns>Number of rows affected.</returns>
+    int Run(DatabaseContext dbContext);
+
+    /// <summary>
+    /// Executes a command against the database using the migration builder in the context of a migration.
+    /// </summary>
+    void Run(MigrationBuilder migrationBuilder);
+}
