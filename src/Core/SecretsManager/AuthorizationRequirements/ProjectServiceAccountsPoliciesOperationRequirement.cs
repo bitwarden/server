0b52e64b7c3ddb7c4f86ee567370e7f9be3f7c11
Thomas Avery <tavery@bitwarden.com>
2024-04-15 17:07:01 -0500

    Add new authz handler

---
+++ src/Core/SecretsManager/AuthorizationRequirements/ProjectServiceAccountsPoliciesOperationRequirement.cs
@@ -0,0 +1,14 @@
+﻿#nullable enable
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+namespace Bit.Core.SecretsManager.AuthorizationRequirements;
+
+public class ProjectServiceAccountsPoliciesOperationRequirement : OperationAuthorizationRequirement
+{
+
+}
+
+public static class ProjectServiceAccountsPoliciesOperations
+{
+    public static readonly ProjectServiceAccountsPoliciesOperationRequirement Updates = new() { Name = nameof(Updates) };
+}
