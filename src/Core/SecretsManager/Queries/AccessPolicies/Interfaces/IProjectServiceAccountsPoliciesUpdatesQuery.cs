fc7a1baba9decfd5e1dc8d60530aaff33d25c8f1
Thomas Avery <tavery@bitwarden.com>
2024-04-15 17:07:24 -0500

    Add new query

---
+++ src/Core/SecretsManager/Queries/AccessPolicies/Interfaces/IProjectServiceAccountsPoliciesUpdatesQuery.cs
@@ -0,0 +1,9 @@
+﻿#nullable enable
+using Bit.Core.SecretsManager.Models.Data;
+
+namespace Bit.Core.SecretsManager.Queries.AccessPolicies.Interfaces;
+
+public interface IProjectServiceAccountsPoliciesUpdatesQuery
+{
+    Task<ProjectServiceAccountsPoliciesUpdates> GetAsync(ProjectServiceAccountsAccessPolicies grantedPolicies);
+}
