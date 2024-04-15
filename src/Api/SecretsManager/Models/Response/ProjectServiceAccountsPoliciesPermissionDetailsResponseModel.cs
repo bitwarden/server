77d4b4f25a1eaff914dcf4d0658457e02528b0e1
Thomas Avery <tavery@bitwarden.com>
2024-04-15 17:05:29 -0500

    Add new models

---
+++ src/Api/SecretsManager/Models/Response/ProjectServiceAccountsPoliciesPermissionDetailsResponseModel.cs
@@ -0,0 +1,30 @@
+﻿#nullable enable
+using Bit.Core.Models.Api;
+using Bit.Core.SecretsManager.Models.Data;
+
+namespace Bit.Api.SecretsManager.Models.Response;
+
+public class ProjectServiceAccountsPoliciesPermissionDetailsResponseModel : ResponseModel
+{
+    private const string _objectName = "ProjectServiceAccountsPoliciesPermissionDetails";
+
+    public ProjectServiceAccountsPoliciesPermissionDetailsResponseModel(
+        ProjectServiceAccountsPoliciesPermissionDetails? policyPermissionDetails)
+        : base(_objectName)
+    {
+        if (policyPermissionDetails == null)
+        {
+            return;
+        }
+
+        ServiceAccountPolicies = policyPermissionDetails.ServiceAccountPoliciesDetails
+            .Select(x => new ServiceAccountProjectAccessPolicyPermissionDetailsResponseModel(x)).ToList();
+    }
+
+    public ProjectServiceAccountsPoliciesPermissionDetailsResponseModel() : base(_objectName)
+    {
+    }
+
+    public List<ServiceAccountProjectAccessPolicyPermissionDetailsResponseModel> ServiceAccountPolicies { get; set; } =
+        [];
+}
---
+++ src/Core/SecretsManager/Models/Data/ProjectServiceAccountsPoliciesPermissionDetails.cs
@@ -0,0 +1,9 @@
+﻿#nullable enable
+namespace Bit.Core.SecretsManager.Models.Data;
+
+public class ProjectServiceAccountsPoliciesPermissionDetails
+{
+    public Guid ProjectId { get; set; }
+    public Guid OrganizationId { get; set; }
+    public required IEnumerable<ServiceAccountProjectAccessPolicyPermissionDetails> ServiceAccountPoliciesDetails { get; set; }
+}
