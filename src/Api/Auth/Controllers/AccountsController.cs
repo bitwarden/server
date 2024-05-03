922aa7879f40c8b475bd2974a95860d44864d0c2
Jake Fink <jfink@bitwarden.com>
2024-05-03 17:05:20 -0400

    return exception if trying to overwrite keypair

--- src/Api/Auth/Controllers/AccountsController.cs
+++ src/Api/Auth/Controllers/AccountsController.cs
@@ -618,6 +618,11 @@ public class AccountsController : Controller
             throw new UnauthorizedAccessException();
         }
 
+        if (!string.IsNullOrWhiteSpace(user.PrivateKey) || !string.IsNullOrWhiteSpace(user.PublicKey))
+        {
+            throw new BadRequestException("User has existing keypair");
+        }
+
         await _userService.SaveUserAsync(model.ToUser(user));
         return new KeysResponseModel(user);
     }
