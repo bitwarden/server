ea4e1a186ff653b2763c31e5329d515a59f24f68
Cy Okeke <cokeke@bitwarden.com>
2024-05-06 09:27:15 +0100

    Fix the failing test cases
    Signed-off-by: Cy Okeke <cokeke@bitwarden.com>

--- test/Core.Test/Models/Business/SeatSubscriptionUpdateTests.cs
+++ test/Core.Test/Models/Business/SeatSubscriptionUpdateTests.cs
@@ -1,4 +1,5 @@
-﻿using Bit.Core.Enums;
+﻿using Bit.Core.AdminConsole.Entities;
+using Bit.Core.Enums;
 using Bit.Core.Models.Business;
 using Bit.Core.Utilities;
 using Bit.Test.Common.AutoFixture.Attributes;
@@ -24,9 +25,10 @@ public class SeatSubscriptionUpdateTests
     [BitAutoData(PlanType.TeamsAnnually)]
     [BitAutoData(PlanType.TeamsStarter)]
 
-    public void UpgradeItemsOptions_ReturnsCorrectOptions(PlanType planType)
+    public void UpgradeItemsOptions_ReturnsCorrectOptions(PlanType planType, Organization organization)
     {
         var plan = StaticStore.GetPlan(planType);
+        organization.PlanType = planType;
         var subscription = new Subscription
         {
             Items = new StripeList<SubscriptionItem>
@@ -42,7 +44,7 @@ public class SeatSubscriptionUpdateTests
                 }
             }
         };
-        var update = new StorageSubscriptionUpdate(plan.PasswordManager.StripeSeatPlanId, 100);
+        var update = new SeatSubscriptionUpdate(organization, plan, 100);
 
         var options = update.UpgradeItemsOptions(subscription);
 
@@ -50,17 +52,6 @@ public class SeatSubscriptionUpdateTests
         Assert.Equal(plan.PasswordManager.StripeSeatPlanId, options[0].Plan);
         Assert.Equal(100, options[0].Quantity);
         Assert.Null(options[0].Deleted);
-    }
-
-    [Theory]
-    [BitAutoData(PlanType.TeamsAnnually)]
-    public void RevertItemsOptions_ThrowsExceptionIfPrevStorageIsNull(PlanType planType)
-    {
-        var plan = StaticStore.GetPlan(planType);
-        var subscription = new Subscription();
-        var update = new StorageSubscriptionUpdate(plan.PasswordManager.StripeSeatPlanId, 100);
-
-        Assert.Throws<Exception>(() => update.RevertItemsOptions(subscription));
     }
 
     [Theory]
@@ -76,9 +67,10 @@ public class SeatSubscriptionUpdateTests
     [BitAutoData(PlanType.TeamsAnnually2019)]
     [BitAutoData(PlanType.TeamsAnnually2020)]
     [BitAutoData(PlanType.TeamsAnnually)]
-    public void RevertItemsOptions_ReturnsCorrectOptions(PlanType planType)
+    public void RevertItemsOptions_ReturnsCorrectOptions(PlanType planType, Organization organization)
     {
         var plan = StaticStore.GetPlan(planType);
+        organization.PlanType = planType;
         var subscription = new Subscription
         {
             Items = new StripeList<SubscriptionItem>
@@ -94,14 +86,14 @@ public class SeatSubscriptionUpdateTests
                 }
             }
         };
-        var update = new StorageSubscriptionUpdate(plan.PasswordManager.StripeSeatPlanId, 100);
+        var update = new SeatSubscriptionUpdate(organization, plan, 100);
         update.UpgradeItemsOptions(subscription);
 
         var options = update.RevertItemsOptions(subscription);
 
         Assert.Single(options);
         Assert.Equal(plan.PasswordManager.StripeSeatPlanId, options[0].Plan);
-        Assert.Equal(100, options[0].Quantity);
+        Assert.Equal(organization.Seats, options[0].Quantity);
         Assert.Null(options[0].Deleted);
     }
 }
