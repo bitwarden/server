ccbb2f548f2b233cd8bdb1251582b65467222a07
Addison Beck <hello@addisonbeck.com>
2024-05-06 19:59:53 -0500

    Declare a new command interface method
    Calling API functions of the `UpdateOrganizationAuthRequestCommand` need
    a function that can accept many auth request response objects and
    process them as approved or denied. To achieve this a new function has
    been added to `IUpdateOrganizationAuthRequestCommand` called
    `UpdateManyAsync()` that accepts an
    `IEnumberable<OrganizationAuthRequest>` and returns a `Task`.
    Implementations of this interface method will be used to bulk process
    auth requests as approved or denied.

--- src/Core/AdminConsole/OrganizationAuth/Interfaces/IUpdateOrganizationAuthRequestCommand.cs
+++ src/Core/AdminConsole/OrganizationAuth/Interfaces/IUpdateOrganizationAuthRequestCommand.cs
@@ -1,6 +1,9 @@
-﻿namespace Bit.Core.AdminConsole.OrganizationAuth.Interfaces;
+﻿using Bit.Core.AdminConsole.OrganizationAuth.Models;
+
+namespace Bit.Core.AdminConsole.OrganizationAuth.Interfaces;
 
 public interface IUpdateOrganizationAuthRequestCommand
 {
     Task UpdateAsync(Guid requestId, Guid userId, bool requestApproved, string encryptedUserKey);
+    Task UpdateManyAsync(Guid organizationId, IEnumerable<OrganizationAuthRequestUpdateCommandModel> authRequestUpdates);
 }
