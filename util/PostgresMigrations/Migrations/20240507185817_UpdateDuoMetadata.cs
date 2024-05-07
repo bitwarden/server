62beaa8dbd95c3564c1eaac3752bd36d2ed3c5c3
Ike Kottlowski <ikottlowski@bitwarden.com>
2024-05-07 16:34:15 -0700

    db provider migrations

---
+++ util/PostgresMigrations/Migrations/20240507185817_UpdateDuoMetadata.cs
@@ -0,0 +1,23 @@
+﻿using Bit.Core.Utilities;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.PostgresMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class UpdateDuoMetadata : Migration
+{
+    private const string _updateDuoMetadata = "PostgresMigrations.HelperScripts.2024-05-03_00_UpdateDuoMetadata.psql";
+    private const string _revertUpdateDuoMetadata = "PostgresMigrations.HelperScripts.2024-05-06_00_RevertUpdateDuoMetadata.psql";
+
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(CoreHelpers.GetEmbeddedResourceContentsAsync(_updateDuoMetadata));
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(CoreHelpers.GetEmbeddedResourceContentsAsync(_revertUpdateDuoMetadata));
+    }
+}
---
+++ util/SqliteMigrations/Migrations/20240507185822_UpdateDuoMetadata.cs
@@ -0,0 +1,23 @@
+﻿using Bit.Core.Utilities;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.SqliteMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class UpdateDuoMetadata : Migration
+{
+    private const string _updateDuoMetadata = "SqliteMigrations.HelperScripts.2024-05-03_00_UpdateDuoMetadata.sql";
+    private const string _revertUpdateDuoMetadata = "SqliteMigrations.HelperScripts.2024-05-06_00_RevertUpdateDuoMetadata.sql";
+
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(CoreHelpers.GetEmbeddedResourceContentsAsync(_updateDuoMetadata));
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(CoreHelpers.GetEmbeddedResourceContentsAsync(_revertUpdateDuoMetadata));
+    }
+}
