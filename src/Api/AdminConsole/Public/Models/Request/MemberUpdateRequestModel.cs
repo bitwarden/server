bafdb8ece8452791caf73a820190bdd7ae733f3d
Thomas Rittson <trittson@bitwarden.com>
2024-05-07 14:01:02 +1000

    Add validation

--- src/Api/AdminConsole/Public/Models/Request/MemberUpdateRequestModel.cs
+++ src/Api/AdminConsole/Public/Models/Request/MemberUpdateRequestModel.cs
@@ -1,9 +1,10 @@
-﻿using Bit.Core.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using Bit.Core.Entities;
 using Bit.Core.Enums;
 
 namespace Bit.Api.AdminConsole.Public.Models.Request;
 
-public class MemberUpdateRequestModel : MemberBaseModel
+public class MemberUpdateRequestModel : MemberBaseModel, IValidatableObject
 {
     /// <summary>
     /// The associated collections that this member can access.
@@ -28,5 +29,13 @@ public class MemberUpdateRequestModel : MemberBaseModel
         }
 
         return existingUser;
+    }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (Type is not OrganizationUserType.Custom && Permissions is not null)
+        {
+            yield return new ValidationResult("Only users with the Custom role may use custom permissions.");
+        }
     }
 }
