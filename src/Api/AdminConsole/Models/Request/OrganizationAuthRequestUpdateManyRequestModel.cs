fb2c762c55b5ae6b54fa42a4cf890a57a962b39a
Addison Beck <hello@addisonbeck.com>
2024-05-10 16:45:20 -0500

    Write a request model

---
+++ src/Api/AdminConsole/Models/Request/OrganizationAuthRequestUpdateManyRequestModel.cs
@@ -0,0 +1,13 @@
+﻿using Bit.Core.Utilities;
+
+namespace Bit.Api.AdminConsole.Models.Request;
+
+public class OrganizationAuthRequestUpdateManyRequestModel
+{
+    public Guid Id { get; set; }
+
+    [EncryptedString]
+    public string Key { get; set; }
+
+    public bool Approved { get; set; }
+}
