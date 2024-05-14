e4303bdaa7a40524f414ee09e7232a9fd807f6b2
Merissa Weinstein <merissa.k.weinstein@gmail.com>
2024-05-14 13:25:18 -0500

    remove onboarding feature flag

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -102,7 +102,6 @@ public static class AuthenticationSchemes
 
 public static class FeatureFlagKeys
 {
-    public const string VaultOnboarding = "vault-onboarding";
     public const string BrowserFilelessImport = "browser-fileless-import";
     public const string ReturnErrorOnExistingKeypair = "return-error-on-existing-keypair";
 
