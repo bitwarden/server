8ebf3244ec1d36dcefc713b2d44e659585a940a4
Bernd Schoolmann <mail@quexten.com>
2024-05-17 14:25:31 +0200

    Add unit test for key rotation guardrail

--- test/Core.Test/Auth/UserFeatures/UserKey/RotateUserKeyCommandTests.cs
+++ test/Core.Test/Auth/UserFeatures/UserKey/RotateUserKeyCommandTests.cs
@@ -1,7 +1,11 @@
 ﻿using Bit.Core.Auth.Models.Data;
 using Bit.Core.Auth.UserFeatures.UserKey.Implementations;
 using Bit.Core.Entities;
+using Bit.Core.Exceptions;
 using Bit.Core.Services;
+using Bit.Core.Vault.Entities;
+using Bit.Core.Vault.Models.Data;
+using Bit.Core.Vault.Repositories;
 using Bit.Test.Common.AutoFixture;
 using Bit.Test.Common.AutoFixture.Attributes;
 using Microsoft.AspNetCore.Identity;
@@ -48,5 +52,24 @@ public class RotateUserKeyCommandTests
 
         await sutProvider.GetDependency<IPushNotificationService>().ReceivedWithAnyArgs()
             .PushLogOutAsync(default, default);
+    }
+
+    [Theory, BitAutoData]
+    public async Task RotateUserKeyAsync_PreventDesyncedVaultRotation(
+        SutProvider<RotateUserKeyCommand> sutProvider,
+        User user,
+        RotateUserKeyData model,
+        List<CipherDetails> ciphers
+    )
+    {
+        model.Ciphers = new List<Cipher>();
+        sutProvider.GetDependency<IUserService>().CheckPasswordAsync(user, model.MasterPasswordHash)
+            .Returns(true);
+
+        sutProvider.GetDependency<ICipherRepository>().GetManyByUserIdAsync(user.Id, false, false)
+            .Returns(ciphers);
+
+        var exception = await Assert.ThrowsAsync<BadRequestException>(() => sutProvider.Sut.RotateUserKeyAsync(user, model));
+        Assert.Contains("No ciphers", exception.Message);
     }
 }
