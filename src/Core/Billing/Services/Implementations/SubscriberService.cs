8a010bd107c10429c0ddb0242f4667a88db3ef19
Cy Okeke <cokeke@bitwarden.com>
2024-05-20 20:17:37 +0100

    Move the gettax and paymentmethod from stripepayment class
    Signed-off-by: Cy Okeke <cokeke@bitwarden.com>

--- src/Core/Billing/Services/Implementations/SubscriberService.cs
+++ src/Core/Billing/Services/Implementations/SubscriberService.cs
@@ -1,5 +1,6 @@
 ﻿using Bit.Core.Billing.Models;
 using Bit.Core.Entities;
+using Bit.Core.Models.Business;
 using Bit.Core.Services;
 using Braintree;
 using Microsoft.Extensions.Logging;
@@ -329,5 +330,115 @@ public class SubscriberService(
                 await stripeAdapter.PaymentMethodDetachAsync(paymentMethod.Id);
             }
         }
+    }
+
+    public async Task<TaxInfo> GetTaxInformationAsync(ISubscriber subscriber)
+    {
+        ArgumentNullException.ThrowIfNull(subscriber);
+
+        if (string.IsNullOrWhiteSpace(subscriber.GatewayCustomerId))
+        {
+            logger.LogError("Cannot retrieve GatewayCustomerId for subscriber ({SubscriberID}) with no {FieldName}", subscriber.Id, nameof(subscriber.GatewaySubscriptionId));
+
+            return null;
+        }
+
+        var customer = await GetCustomerOrThrow(subscriber, new CustomerGetOptions { Expand = ["tax_ids"] });
+
+        if (customer is null)
+        {
+            logger.LogError("Could not find Stripe customer ({CustomerID}) for subscriber ({SubscriberID})",
+                subscriber.GatewayCustomerId, subscriber.Id);
+
+            return null;
+        }
+
+        var address = customer.Address;
+
+        // Line1 is required, so if missing we're using the subscriber name
+        // see: https://stripe.com/docs/api/customers/create#create_customer-address-line1
+        if (address is not null && string.IsNullOrWhiteSpace(address.Line1))
+        {
+            address.Line1 = null;
+        }
+
+        return MapToTaxInfo(customer);
+    }
+
+    public async Task<BillingInfo.BillingSource> GetPaymentMethodAsync(ISubscriber subscriber)
+    {
+        ArgumentNullException.ThrowIfNull(subscriber);
+        var customer = await GetCustomerOrThrow(subscriber, GetCustomerPaymentOptions());
+        if (customer == null)
+        {
+            logger.LogError("Could not find Stripe customer ({CustomerID}) for subscriber ({SubscriberID})",
+                subscriber.GatewayCustomerId, subscriber.Id);
+            return null;
+        }
+
+        if (customer.Metadata?.ContainsKey("btCustomerId") ?? false)
+        {
+            try
+            {
+                var braintreeCustomer = await braintreeGateway.Customer.FindAsync(
+                    customer.Metadata["btCustomerId"]);
+                if (braintreeCustomer?.DefaultPaymentMethod != null)
+                {
+                    return new BillingInfo.BillingSource(
+                        braintreeCustomer.DefaultPaymentMethod);
+                }
+            }
+            catch (Braintree.Exceptions.NotFoundException ex)
+            {
+                logger.LogError("An error occurred while trying to retrieve braintree customer ({SubscriberID}): {Error}", subscriber.Id, ex.Message);
+            }
+        }
+
+        if (customer.InvoiceSettings?.DefaultPaymentMethod?.Type == "card")
+        {
+            return new BillingInfo.BillingSource(
+                customer.InvoiceSettings.DefaultPaymentMethod);
+        }
+
+        if (customer.DefaultSource != null &&
+            (customer.DefaultSource is Card || customer.DefaultSource is BankAccount))
+        {
+            return new BillingInfo.BillingSource(customer.DefaultSource);
+        }
+
+        var paymentMethod = GetLatestCardPaymentMethod(customer.Id);
+        return paymentMethod != null ? new BillingInfo.BillingSource(paymentMethod) : null;
+    }
+
+    private static CustomerGetOptions GetCustomerPaymentOptions()
+    {
+        var customerOptions = new CustomerGetOptions();
+        customerOptions.AddExpand("default_source");
+        customerOptions.AddExpand("invoice_settings.default_payment_method");
+        return customerOptions;
+    }
+
+    private Stripe.PaymentMethod GetLatestCardPaymentMethod(string customerId)
+    {
+        var cardPaymentMethods = stripeAdapter.PaymentMethodListAutoPaging(
+            new PaymentMethodListOptions { Customer = customerId, Type = "card" });
+        return cardPaymentMethods.MaxBy(m => m.Created);
+    }
+
+    private TaxInfo MapToTaxInfo(Customer customer)
+    {
+        var address = customer.Address;
+        var taxId = customer.TaxIds?.FirstOrDefault();
+
+        return new TaxInfo
+        {
+            TaxIdNumber = taxId?.Value,
+            BillingAddressLine1 = address?.Line1,
+            BillingAddressLine2 = address?.Line2,
+            BillingAddressCity = address?.City,
+            BillingAddressState = address?.State,
+            BillingAddressPostalCode = address?.PostalCode,
+            BillingAddressCountry = address?.Country,
+        };
     }
 }
