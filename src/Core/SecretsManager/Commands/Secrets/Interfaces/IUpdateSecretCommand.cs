e68d41c17bf3176d4ba67f5a33fff97449b8a9fc
Thomas Avery <tavery@bitwarden.com>
2024-05-24 16:58:27 -0500

    Update interface

--- src/Core/SecretsManager/Commands/Secrets/Interfaces/IUpdateSecretCommand.cs
+++ src/Core/SecretsManager/Commands/Secrets/Interfaces/IUpdateSecretCommand.cs
@@ -1,9 +1,10 @@
-﻿using Bit.Core.SecretsManager.Entities;
+﻿#nullable enable
+using Bit.Core.SecretsManager.Entities;
 using Bit.Core.SecretsManager.Models.Data.AccessPolicyUpdates;
 
 namespace Bit.Core.SecretsManager.Commands.Secrets.Interfaces;
 
 public interface IUpdateSecretCommand
 {
-    Task<Secret> UpdateAsync(Secret secret, SecretAccessPoliciesUpdates accessPolicyUpdates);
+    Task<Secret> UpdateAsync(Secret secret, SecretAccessPoliciesUpdates? accessPolicyUpdates);
 }
