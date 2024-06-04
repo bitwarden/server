7e02d8c6548b18f45d05433f255125e1b5d7e68e
Bernd Schoolmann <mail@quexten.com>
2024-06-04 12:38:40 +0200

    Update tde offboarding request model

--- src/Api/Auth/Models/Request/Accounts/UpdateTdeOffboardingPasswordRequestModel.cs
+++ src/Api/Auth/Models/Request/Accounts/UpdateTdeOffboardingPasswordRequestModel.cs
@@ -1,10 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using Bit.Api.Models.Request.Organizations;
 
 namespace Bit.Api.Auth.Models.Request.Accounts;
 
-public class UpdateTdeOffboardingPasswordRequestModel : OrganizationUserResetPasswordRequestModel
+public class UpdateTdeOffboardingPasswordRequestModel
 {
+    [Required]
+    [StringLength(300)]
+    public string NewMasterPasswordHash { get; set; }
+    [Required]
+    public string Key { get; set; }
     [StringLength(50)]
     public string MasterPasswordHint { get; set; }
 }
