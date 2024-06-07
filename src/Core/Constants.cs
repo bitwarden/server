6652746090e5d1b6cbee6d54b95fb4f2ff0eb70f
Cesar Gonzalez <cgonzalez@bitwarden.com>
2024-06-07 07:23:32 -0500

    [PM-8027] Adding feature flag to allow us to fallback to the basic approach to field qualification for the inline menu

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -130,6 +130,7 @@ public static class FeatureFlagKeys
     public const string BulkDeviceApproval = "bulk-device-approval";
     public const string MemberAccessReport = "ac-2059-member-access-report";
     public const string BlockLegacyUsers = "block-legacy-users";
+    public const string UseBasicInlineMenuFieldQualification = 'use-basic-inline-menu-field-qualification'
 
     public static List<string> GetAllKeys()
     {
