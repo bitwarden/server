b5241f1a9731ea74f22c2f4fe1072421f857429e
Conner Turnbull <133619638+cturnbull-bitwarden@users.noreply.github.com>
2024-06-14 17:05:19 -0400

    Added missing enum import (#4192)

--- src/Billing/Services/Implementations/ProviderEventService.cs
+++ src/Billing/Services/Implementations/ProviderEventService.cs
@@ -1,6 +1,7 @@
 ﻿using Bit.Billing.Constants;
 using Bit.Core.AdminConsole.Repositories;
 using Bit.Core.Billing.Entities;
+using Bit.Core.Billing.Enums;
 using Bit.Core.Billing.Repositories;
 using Bit.Core.Enums;
 using Bit.Core.Utilities;
