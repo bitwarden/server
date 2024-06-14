0228eb4c1c4e58624d09c19bdef7c2fd963cd09b
Jared Snider <jsnider@bitwarden.com>
2024-06-14 15:01:55 -0400

    PM-7322 - Start on RegisterFinishRequestModel.cs

--- src/Core/Auth/Models/Api/Request/Accounts/RegisterFinishRequestModel.cs
+++ src/Core/Auth/Models/Api/Request/Accounts/RegisterFinishRequestModel.cs
@@ -1,6 +1,8 @@
-﻿namespace Bit.Core.Auth.Models.Api.Request.Accounts;
+﻿#nullable enable
+namespace Bit.Core.Auth.Models.Api.Request.Accounts;
 
 public class RegisterFinishRequestModel
 {
-
+    public string emailVerificationToken { get; set; }
+    public string? orgInviteToken { get; set; }
 }
