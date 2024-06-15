82147b0175796f3485b1d60684eda910d988d981
Jared Snider <jsnider@bitwarden.com>
2024-06-15 15:49:40 -0400

    PM-5092 - Add fromEmail flag + some docs

--- src/Core/Auth/Models/Mail/RegisterVerifyEmail.cs
+++ src/Core/Auth/Models/Mail/RegisterVerifyEmail.cs
@@ -4,7 +4,11 @@ namespace Bit.Core.Auth.Models.Mail;
 
 public class RegisterVerifyEmail : BaseMailModel
 {
-    public string Url => string.Format("{0}/finish-signup?token={1}&email={2}",
+    // We must include email in the URL even though it is already in the token so that the
+    // client can use it to create the master key when they set their password.
+    // We also have to include the fromEmail flag so that the client knows the user
+    // is coming to the finish signup page from an email link and not directly from another route in the app.
+    public string Url => string.Format("{0}/finish-signup?token={1}&email={2}&fromEmail=true",
         WebVaultUrl,
         Token,
         Email);
