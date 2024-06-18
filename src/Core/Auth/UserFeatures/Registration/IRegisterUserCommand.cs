56002d04af4b797c1a4ad04d75b6f6fa690cc83b
Jared Snider <jsnider@bitwarden.com>
2024-06-18 18:07:24 -0400

    PM-7322 Create IRegisterUserCommand

---
+++ src/Core/Auth/UserFeatures/Registration/IRegisterUserCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace Bit.Core.Auth.UserFeatures.Registration;
+
+public interface IRegisterUserCommand
+{
+
+    // TODO: create method RegisterUserViaEmailVerificationTokenAsync
+
+    // TODO: create method RegisterUserViaOrganizationInviteAsync
+
+
+}
