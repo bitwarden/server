3471353de8624b2ae5d97af4ab240f4318a6aaec
Jared Snider <jsnider@bitwarden.com>
2024-06-19 17:56:45 -0400

    PM07322 - Test RegisterUserCommand.RegisterUser(...) happy paths and one bad request path.

--- test/Core.Test/Auth/UserFeatures/Registration/RegisterUserCommandTests.cs
+++ test/Core.Test/Auth/UserFeatures/Registration/RegisterUserCommandTests.cs
@@ -1,9 +1,176 @@
-﻿using Bit.Test.Common.AutoFixture.Attributes;
+﻿using Bit.Core.AdminConsole.Entities;
+using Bit.Core.AdminConsole.Enums;
+using Bit.Core.AdminConsole.Repositories;
+using Bit.Core.Auth.Enums;
+using Bit.Core.Auth.Models;
+using Bit.Core.Auth.Models.Business.Tokenables;
+using Bit.Core.Auth.UserFeatures.Registration.Implementations;
+using Bit.Core.Entities;
+using Bit.Core.Exceptions;
+using Bit.Core.Repositories;
+using Bit.Core.Services;
+using Bit.Core.Settings;
+using Bit.Core.Tokens;
+using Bit.Core.Tools.Enums;
+using Bit.Core.Tools.Models.Business;
+using Bit.Core.Tools.Services;
+using Bit.Core.Utilities;
+using Bit.Test.Common.AutoFixture;
+using Bit.Test.Common.AutoFixture.Attributes;
+using Microsoft.AspNetCore.Identity;
+using NSubstitute;
+using Xunit;
 
 namespace Bit.Core.Test.Auth.UserFeatures.Registration;
 
 [SutProviderCustomize]
 public class RegisterUserCommandTests
 {
+    // Simple happy path test
+    [Theory]
+    [BitAutoData]
+    public async Task RegisterUser_NoOrgInviteOrOrgUserIdOrReferenceData_Succeeds(
+        SutProvider<RegisterUserCommand> sutProvider, User user, string masterPasswordHash)
+    {
+        // Arrange
+        user.ReferenceData = null;
+
+        sutProvider.GetDependency<IUserService>()
+            .CreateUserAsync(user, masterPasswordHash)
+            .Returns(IdentityResult.Success);
+
+        // Act
+        var result = await sutProvider.Sut.RegisterUser(user, masterPasswordHash, null, null);
+
+        // Assert
+        Assert.True(result.Succeeded);
+
+        await sutProvider.GetDependency<IUserService>()
+            .Received(1)
+            .CreateUserAsync(user, masterPasswordHash);
+
+        await sutProvider.GetDependency<IReferenceEventService>()
+            .Received(1)
+            .RaiseEventAsync(Arg.Is<ReferenceEvent>(refEvent => refEvent.Type == ReferenceEventType.Signup));
+    }
+
+    // Complex happy path test
+    [Theory]
+    [BitAutoData]
+    public async Task RegisterUser_ComplexHappyPath_Succeeds(
+        SutProvider<RegisterUserCommand> sutProvider, User user, string masterPasswordHash, OrganizationUser orgUser, string orgInviteToken, Guid orgUserId, Policy twoFactorPolicy)
+    {
+        // Arrange
+        sutProvider.GetDependency<IGlobalSettings>()
+            .DisableUserRegistration.Returns(false);
+
+        sutProvider.GetDependency<IGlobalSettings>()
+            .DisableUserRegistration.Returns(true);
+
+        orgUser.Email = user.Email;
+        orgUser.Id = orgUserId;
+
+        var orgInviteTokenable = new OrgUserInviteTokenable(orgUser);
+
+        sutProvider.GetDependency<IDataProtectorTokenFactory<OrgUserInviteTokenable>>()
+            .TryUnprotect(orgInviteToken, out Arg.Any<OrgUserInviteTokenable>())
+            .Returns(callInfo =>
+            {
+                callInfo[1] = orgInviteTokenable;
+                return true;
+            });
+
+        sutProvider.GetDependency<IOrganizationUserRepository>()
+            .GetByIdAsync(orgUserId)
+            .Returns(orgUser);
+
+        twoFactorPolicy.Enabled = true;
+        sutProvider.GetDependency<IPolicyRepository>()
+            .GetByOrganizationIdTypeAsync(orgUser.OrganizationId, PolicyType.TwoFactorAuthentication)
+            .Returns(twoFactorPolicy);
+
+        sutProvider.GetDependency<IUserService>()
+            .CreateUserAsync(user, masterPasswordHash)
+            .Returns(IdentityResult.Success);
+
+        var initiationPath = "trialInitiation";
+        user.ReferenceData = $"{{\"initiationPath\":\"{initiationPath}\"}}";
+
+        // Act
+        var result = await sutProvider.Sut.RegisterUser(user, masterPasswordHash, orgInviteToken, orgUserId);
+
+        // Assert
+        await sutProvider.GetDependency<IOrganizationUserRepository>()
+            .Received(1)
+            .GetByIdAsync(orgUserId);
+
+        await sutProvider.GetDependency<IPolicyRepository>()
+            .Received(1)
+            .GetByOrganizationIdTypeAsync(orgUser.OrganizationId, PolicyType.TwoFactorAuthentication);
+
+        sutProvider.GetDependency<IUserService>()
+            .Received(1)
+            .SetTwoFactorProvider(user, TwoFactorProviderType.Email);
+
+        // example serialized data: {"1":{"Enabled":true,"MetaData":{"Email":"0dbf746c-deaf-4318-811e-d98ea7155075"}}}
+        var twoFactorProviders = new Dictionary<TwoFactorProviderType, TwoFactorProvider>
+        {
+            [TwoFactorProviderType.Email] = new TwoFactorProvider
+            {
+                MetaData = new Dictionary<string, object> { ["Email"] = user.Email.ToLowerInvariant() },
+                Enabled = true
+            }
+        };
+
+        var serializedTwoFactorProviders =
+            JsonHelpers.LegacySerialize(twoFactorProviders, JsonHelpers.LegacyEnumKeyResolver);
+
+        Assert.Equal(user.TwoFactorProviders, serializedTwoFactorProviders);
+
+        await sutProvider.GetDependency<IUserService>()
+            .Received(1)
+            .CreateUserAsync(user, masterPasswordHash);
+
+        await sutProvider.GetDependency<IMailService>()
+            .Received(1)
+            .SendWelcomeEmailAsync(user);
+
+        await sutProvider.GetDependency<IReferenceEventService>()
+               .Received(1)
+               .RaiseEventAsync(Arg.Is<ReferenceEvent>(refEvent => refEvent.Type == ReferenceEventType.Signup && refEvent.SignupInitiationPath == initiationPath));
+
+        await sutProvider.GetDependency<IReferenceEventService>()
+            .Received(1)
+            .RaiseEventAsync(Arg.Is<ReferenceEvent>(refEvent => refEvent.Type == ReferenceEventType.Signup));
+
+        Assert.True(result.Succeeded);
+
+    }
+
+    [Theory]
+    [BitAutoData]
+    public async Task RegisterUser_InvalidOrgInviteWithDisabledOpenRegistrationTrue_ThrowsBadRequestException(
+        SutProvider<RegisterUserCommand> sutProvider, User user, string masterPasswordHash, OrganizationUser orgUser, string orgInviteToken)
+    {
+        // Arrange
+        sutProvider.GetDependency<IGlobalSettings>()
+            .DisableUserRegistration.Returns(true);
+
+        orgUser.Email = null; // make org user not match user and thus make tokenable invalid
+        var orgInviteTokenable = new OrgUserInviteTokenable(orgUser);
+
+        sutProvider.GetDependency<IDataProtectorTokenFactory<OrgUserInviteTokenable>>()
+             .TryUnprotect(orgInviteToken, out Arg.Any<OrgUserInviteTokenable>())
+             .Returns(callInfo =>
+             {
+                 callInfo[1] = orgInviteTokenable;
+                 return true;
+             });
+
+        // Act & Assert
+        var exception = await Assert.ThrowsAsync<BadRequestException>(() => sutProvider.Sut.RegisterUser(user, masterPasswordHash, orgInviteToken, null));
+        Assert.Equal("Open registration has been disabled by the system administrator.", exception.Message);
+    }
+
 
 }
