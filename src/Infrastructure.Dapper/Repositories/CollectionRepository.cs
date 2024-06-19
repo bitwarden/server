676cf807a97eefe771b92c5a7d6c7f479acb8cfe
Thomas Rittson <trittson@bitwarden.com>
2024-06-19 14:49:17 +1000

    Change repository to using new sprocs

--- src/Infrastructure.Dapper/Repositories/CollectionRepository.cs
+++ src/Infrastructure.Dapper/Repositories/CollectionRepository.cs
@@ -305,7 +305,7 @@ public class CollectionRepository : Repository<Collection, Guid>, ICollectionRep
         using (var connection = new SqlConnection(ConnectionString))
         {
             var results = await connection.ExecuteAsync(
-                $"[{Schema}].[Collection_CreateWithGroupsAndUsers_V2]",
+                $"[{Schema}].[Collection_CreateWithGroupsAndUsers]",
                 objWithGroupsAndUsers,
                 commandType: CommandType.StoredProcedure);
         }
@@ -321,7 +321,7 @@ public class CollectionRepository : Repository<Collection, Guid>, ICollectionRep
         using (var connection = new SqlConnection(ConnectionString))
         {
             var results = await connection.ExecuteAsync(
-                $"[{Schema}].[Collection_UpdateWithGroupsAndUsers_V2]",
+                $"[{Schema}].[Collection_UpdateWithGroupsAndUsers]",
                 objWithGroupsAndUsers,
                 commandType: CommandType.StoredProcedure);
         }
@@ -378,7 +378,7 @@ public class CollectionRepository : Repository<Collection, Guid>, ICollectionRep
         using (var connection = new SqlConnection(ConnectionString))
         {
             var results = await connection.ExecuteAsync(
-                $"[{Schema}].[CollectionUser_UpdateUsers_V2]",
+                $"[{Schema}].[CollectionUser_UpdateUsers]",
                 new { CollectionId = id, Users = users.ToArrayTVP() },
                 commandType: CommandType.StoredProcedure);
         }
