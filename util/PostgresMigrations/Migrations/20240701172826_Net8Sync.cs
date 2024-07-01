b30563d28bb9f1c975e79e7791a7f4d578045dfc
Matt Bishop <mbishop@bitwarden.com>
2024-07-01 13:44:18 -0400

    Format

--- util/PostgresMigrations/Migrations/20240701172826_Net8Sync.cs
+++ util/PostgresMigrations/Migrations/20240701172826_Net8Sync.cs
@@ -2,45 +2,44 @@
 
 #nullable disable
 
-namespace Bit.PostgresMigrations.Migrations
+namespace Bit.PostgresMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class Net8Sync : Migration
 {
     /// <inheritdoc />
-    public partial class Net8Sync : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropIndex(
-                name: "IX_ProviderInvoiceItem_Id_InvoiceId",
-                table: "ProviderInvoiceItem");
+        migrationBuilder.DropIndex(
+            name: "IX_ProviderInvoiceItem_Id_InvoiceId",
+            table: "ProviderInvoiceItem");
 
-            migrationBuilder.AlterColumn<string>(
-                name: "Discriminator",
-                table: "AccessPolicy",
-                type: "character varying(34)",
-                maxLength: 34,
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "text");
-        }
+        migrationBuilder.AlterColumn<string>(
+            name: "Discriminator",
+            table: "AccessPolicy",
+            type: "character varying(34)",
+            maxLength: 34,
+            nullable: false,
+            oldClrType: typeof(string),
+            oldType: "text");
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<string>(
-                name: "Discriminator",
-                table: "AccessPolicy",
-                type: "text",
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "character varying(34)",
-                oldMaxLength: 34);
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<string>(
+            name: "Discriminator",
+            table: "AccessPolicy",
+            type: "text",
+            nullable: false,
+            oldClrType: typeof(string),
+            oldType: "character varying(34)",
+            oldMaxLength: 34);
 
-            migrationBuilder.CreateIndex(
-                name: "IX_ProviderInvoiceItem_Id_InvoiceId",
-                table: "ProviderInvoiceItem",
-                columns: new[] { "Id", "InvoiceId" },
-                unique: true);
-        }
+        migrationBuilder.CreateIndex(
+            name: "IX_ProviderInvoiceItem_Id_InvoiceId",
+            table: "ProviderInvoiceItem",
+            columns: new[] { "Id", "InvoiceId" },
+            unique: true);
     }
 }
--- util/SqliteMigrations/Migrations/20240701172820_Net8Sync.cs
+++ util/SqliteMigrations/Migrations/20240701172820_Net8Sync.cs
@@ -2,27 +2,26 @@
 
 #nullable disable
 
-namespace Bit.SqliteMigrations.Migrations
+namespace Bit.SqliteMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class Net8Sync : Migration
 {
     /// <inheritdoc />
-    public partial class Net8Sync : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropIndex(
-                name: "IX_ProviderInvoiceItem_Id_InvoiceId",
-                table: "ProviderInvoiceItem");
-        }
+        migrationBuilder.DropIndex(
+            name: "IX_ProviderInvoiceItem_Id_InvoiceId",
+            table: "ProviderInvoiceItem");
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateIndex(
-                name: "IX_ProviderInvoiceItem_Id_InvoiceId",
-                table: "ProviderInvoiceItem",
-                columns: new[] { "Id", "InvoiceId" },
-                unique: true);
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateIndex(
+            name: "IX_ProviderInvoiceItem_Id_InvoiceId",
+            table: "ProviderInvoiceItem",
+            columns: new[] { "Id", "InvoiceId" },
+            unique: true);
     }
 }
