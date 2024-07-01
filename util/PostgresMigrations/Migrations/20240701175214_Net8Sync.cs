98c893a6e3ef3ef9652b4e8b56fb7ea1f9f12417
Matt Bishop <mbishop@bitwarden.com>
2024-07-01 14:01:17 -0400

    Forgot to format again

--- util/PostgresMigrations/Migrations/20240701175214_Net8Sync.cs
+++ util/PostgresMigrations/Migrations/20240701175214_Net8Sync.cs
@@ -2,125 +2,124 @@
 
 #nullable disable
 
-namespace Bit.PostgresMigrations.Migrations
+namespace Bit.PostgresMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class Net8Sync : Migration
 {
     /// <inheritdoc />
-    public partial class Net8Sync : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropIndex(
-                name: "IX_ProviderInvoiceItem_Id_InvoiceId",
-                table: "ProviderInvoiceItem");
+        migrationBuilder.DropIndex(
+            name: "IX_ProviderInvoiceItem_Id_InvoiceId",
+            table: "ProviderInvoiceItem");
 
-            migrationBuilder.AlterColumn<string>(
-                name: "PlanName",
-                table: "ProviderInvoiceItem",
-                type: "character varying(50)",
-                maxLength: 50,
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "text",
-                oldNullable: true);
+        migrationBuilder.AlterColumn<string>(
+            name: "PlanName",
+            table: "ProviderInvoiceItem",
+            type: "character varying(50)",
+            maxLength: 50,
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "text",
+            oldNullable: true);
 
-            migrationBuilder.AlterColumn<string>(
-                name: "InvoiceNumber",
-                table: "ProviderInvoiceItem",
-                type: "character varying(50)",
-                maxLength: 50,
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "text",
-                oldNullable: true);
+        migrationBuilder.AlterColumn<string>(
+            name: "InvoiceNumber",
+            table: "ProviderInvoiceItem",
+            type: "character varying(50)",
+            maxLength: 50,
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "text",
+            oldNullable: true);
 
-            migrationBuilder.AlterColumn<string>(
-                name: "InvoiceId",
-                table: "ProviderInvoiceItem",
-                type: "character varying(50)",
-                maxLength: 50,
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "text",
-                oldNullable: true);
+        migrationBuilder.AlterColumn<string>(
+            name: "InvoiceId",
+            table: "ProviderInvoiceItem",
+            type: "character varying(50)",
+            maxLength: 50,
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "text",
+            oldNullable: true);
 
-            migrationBuilder.AlterColumn<string>(
-                name: "ClientName",
-                table: "ProviderInvoiceItem",
-                type: "character varying(50)",
-                maxLength: 50,
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "text",
-                oldNullable: true);
+        migrationBuilder.AlterColumn<string>(
+            name: "ClientName",
+            table: "ProviderInvoiceItem",
+            type: "character varying(50)",
+            maxLength: 50,
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "text",
+            oldNullable: true);
 
-            migrationBuilder.AlterColumn<string>(
-                name: "Discriminator",
-                table: "AccessPolicy",
-                type: "character varying(34)",
-                maxLength: 34,
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "text");
-        }
+        migrationBuilder.AlterColumn<string>(
+            name: "Discriminator",
+            table: "AccessPolicy",
+            type: "character varying(34)",
+            maxLength: 34,
+            nullable: false,
+            oldClrType: typeof(string),
+            oldType: "text");
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<string>(
-                name: "PlanName",
-                table: "ProviderInvoiceItem",
-                type: "text",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "character varying(50)",
-                oldMaxLength: 50,
-                oldNullable: true);
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<string>(
+            name: "PlanName",
+            table: "ProviderInvoiceItem",
+            type: "text",
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "character varying(50)",
+            oldMaxLength: 50,
+            oldNullable: true);
 
-            migrationBuilder.AlterColumn<string>(
-                name: "InvoiceNumber",
-                table: "ProviderInvoiceItem",
-                type: "text",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "character varying(50)",
-                oldMaxLength: 50,
-                oldNullable: true);
+        migrationBuilder.AlterColumn<string>(
+            name: "InvoiceNumber",
+            table: "ProviderInvoiceItem",
+            type: "text",
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "character varying(50)",
+            oldMaxLength: 50,
+            oldNullable: true);
 
-            migrationBuilder.AlterColumn<string>(
-                name: "InvoiceId",
-                table: "ProviderInvoiceItem",
-                type: "text",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "character varying(50)",
-                oldMaxLength: 50,
-                oldNullable: true);
+        migrationBuilder.AlterColumn<string>(
+            name: "InvoiceId",
+            table: "ProviderInvoiceItem",
+            type: "text",
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "character varying(50)",
+            oldMaxLength: 50,
+            oldNullable: true);
 
-            migrationBuilder.AlterColumn<string>(
-                name: "ClientName",
-                table: "ProviderInvoiceItem",
-                type: "text",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "character varying(50)",
-                oldMaxLength: 50,
-                oldNullable: true);
+        migrationBuilder.AlterColumn<string>(
+            name: "ClientName",
+            table: "ProviderInvoiceItem",
+            type: "text",
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "character varying(50)",
+            oldMaxLength: 50,
+            oldNullable: true);
 
-            migrationBuilder.AlterColumn<string>(
-                name: "Discriminator",
-                table: "AccessPolicy",
-                type: "text",
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "character varying(34)",
-                oldMaxLength: 34);
+        migrationBuilder.AlterColumn<string>(
+            name: "Discriminator",
+            table: "AccessPolicy",
+            type: "text",
+            nullable: false,
+            oldClrType: typeof(string),
+            oldType: "character varying(34)",
+            oldMaxLength: 34);
 
-            migrationBuilder.CreateIndex(
-                name: "IX_ProviderInvoiceItem_Id_InvoiceId",
-                table: "ProviderInvoiceItem",
-                columns: new[] { "Id", "InvoiceId" },
-                unique: true);
-        }
+        migrationBuilder.CreateIndex(
+            name: "IX_ProviderInvoiceItem_Id_InvoiceId",
+            table: "ProviderInvoiceItem",
+            columns: new[] { "Id", "InvoiceId" },
+            unique: true);
     }
 }
