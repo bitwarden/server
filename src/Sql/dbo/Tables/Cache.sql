2af2bb04c1b21b644e23c124c153290088415937
Matt Bishop <mbishop@bitwarden.com>
2024-07-02 10:40:59 -0400

    Formatting

--- src/Sql/dbo/Tables/Cache.sql
+++ src/Sql/dbo/Tables/Cache.sql
@@ -1,9 +1,10 @@
-﻿CREATE TABLE [dbo].[Cache] (
-    [Id] [nvarchar](449) NOT NULL,
-    [Value] [varbinary](max) NOT NULL,
-    [ExpiresAtTime] [datetimeoffset](7) NOT NULL,
-    [SlidingExpirationInSeconds] [bigint] NULL,
-    [AbsoluteExpiration] [datetimeoffset](7) NULL,
+﻿CREATE TABLE [dbo].[Cache]
+(
+    [Id] NVARCHAR (449) NOT NULL,
+    [Value] VARBINARY (MAX) NOT NULL,
+    [ExpiresAtTime] DATETIMEOFFSET (7) NOT NULL,
+    [SlidingExpirationInSeconds] BIGINT NULL,
+    [AbsoluteExpiration] DATETIMEOFFSET (7) NULL,
     CONSTRAINT [PK_Cache] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 GO
@@ -11,4 +12,3 @@ GO
 CREATE NONCLUSTERED INDEX [IX_Cache_ExpiresAtTime]
     ON [dbo].[Cache]([ExpiresAtTime] ASC);
 GO
-
