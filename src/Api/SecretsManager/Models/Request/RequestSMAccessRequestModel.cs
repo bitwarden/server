55e01f03a5350cedc9495ec2eb694d579c6af2a8
cd-bitwarden <106776772+cd-bitwarden@users.noreply.github.com>
2024-07-02 10:27:40 -0400

    Prettier error message if user sends over an whitespace string

--- src/Api/SecretsManager/Models/Request/RequestSMAccessRequestModel.cs
+++ src/Api/SecretsManager/Models/Request/RequestSMAccessRequestModel.cs
@@ -6,6 +6,6 @@ public class RequestSMAccessRequestModel
 {
     [Required]
     public Guid OrganizationId { get; set; }
-    [Required]
+    [Required(ErrorMessage = "Add a note is a required field")]
     public string EmailContent { get; set; }
 }
