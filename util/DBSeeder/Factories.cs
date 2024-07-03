e9b4d09020fb2a5a827c71a02a4f44fe79d582bf
Robert Y <rkac@bitwarden.com>
2024-07-03 07:58:02 -0600

    fix include directive for factories.cs

--- util/DBSeeder/Factories.cs
+++ util/DBSeeder/Factories.cs
@@ -1,30 +1,28 @@
-﻿using Bit.Core.Settings;
-using Bit.Infrastructure.EntityFramework.Repositories;
+﻿using Bit.Infrastructure.EntityFramework.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Bit.DBSeeder;
 
 
-    public class DatabaseContextFactory : IDesignTimeDbContextFactory<DatabaseContext>
+public class DatabaseContextFactory : IDesignTimeDbContextFactory<DatabaseContext>
+{
+    public DatabaseContext CreateDbContext(string[] args)
     {
-        public DatabaseContext CreateDbContext(string[] args)
+        if (args.Length == 0 || string.IsNullOrWhiteSpace(args[0]))
         {
-            if (args.Length == 0 || string.IsNullOrWhiteSpace(args[0]))
-            {
-                throw new ArgumentException("Connection string must be provided as the first argument.");
-            }
-
-            var connectionString = args[0];
-
-            var services = new ServiceCollection();
-            services.AddDataProtection();
-            services.AddDbContext<DatabaseContext>(options =>
-                options.UseSqlServer(connectionString));
-            var serviceProvider = services.BuildServiceProvider();
-
-            return serviceProvider.GetRequiredService<DatabaseContext>();
+            throw new ArgumentException("Connection string must be provided as the first argument.");
         }
+
+        var connectionString = args[0];
+
+        var services = new ServiceCollection();
+        services.AddDataProtection();
+        services.AddDbContext<DatabaseContext>(options =>
+            options.UseSqlServer(connectionString));
+        var serviceProvider = services.BuildServiceProvider();
+
+        return serviceProvider.GetRequiredService<DatabaseContext>();
     }
+}
