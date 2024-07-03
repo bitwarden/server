d2cecdde560246a6dac3ebafac8a6fe043189911
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2024-07-03 19:57:37 +0000

    Update Tests

--- test/Identity.IntegrationTest/Endpoints/IdentityServerTests.cs
+++ test/Identity.IntegrationTest/Endpoints/IdentityServerTests.cs
@@ -607,7 +607,16 @@ public class IdentityServerTests : IClassFixture<IdentityApplicationFactory>
         var organizationUserRepository = _factory.Services.GetService<IOrganizationUserRepository>();
         var policyRepository = _factory.Services.GetService<IPolicyRepository>();
 
-        var organization = new Organization { Id = organizationId, Enabled = true, UseSso = ssoPolicyEnabled, UsePolicies = true };
+        var organization = new Organization
+        { 
+            Id = organizationId,
+            Name = $"Org Name | {organizationId}",
+            Enabled = true,
+            UseSso = ssoPolicyEnabled,
+            Plan = "Enterprise",
+            UsePolicies = true,
+            BillingEmail = $"billing-email+{organizationId}@example.com",
+        };
         await organizationRepository.CreateAsync(organization);
 
         var user = await userRepository.GetByEmailAsync(username);
