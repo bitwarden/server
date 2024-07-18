75bc63cbf4b182c6bf85199c1fa81e60a72309f7
Kyle Spearrin <kspearrin@users.noreply.github.com>
2024-07-18 09:35:51 -0400

    Update OrganizationIdpRequestModel.cs

--- src/Api/Auth/Models/Request/OrganizationIdpRequestModel.cs
+++ src/Api/Auth/Models/Request/OrganizationIdpRequestModel.cs
@@ -5,6 +5,6 @@ namespace Bit.Api.Auth.Models.Request.Organizations;
 public class OrganizationIdpRequestModel
 {
     [Required]
-    [StringLength(50)]
+    [StringLength(100)]
     public string IdpHost { get; set; }
 }
