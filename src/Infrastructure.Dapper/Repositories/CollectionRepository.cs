b36255be0731e74fa51523a743836ef290189a71
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2024-07-22 14:56:43 -0400

    Format

--- src/Infrastructure.Dapper/Repositories/CollectionRepository.cs
+++ src/Infrastructure.Dapper/Repositories/CollectionRepository.cs
@@ -1,5 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Data;
+﻿using System.Data;
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
 using Bit.Core.Entities;
 using Bit.Core.Models.Data;
