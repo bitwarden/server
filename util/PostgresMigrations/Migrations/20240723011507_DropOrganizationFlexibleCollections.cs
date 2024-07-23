f8cae0b3b52bf463c7737222f3b5999ed496983f
Thomas Rittson <trittson@bitwarden.com>
2024-07-23 11:57:52 +1000

    dotnet format

--- util/PostgresMigrations/Migrations/20240723011507_DropOrganizationFlexibleCollections.cs
+++ util/PostgresMigrations/Migrations/20240723011507_DropOrganizationFlexibleCollections.cs
@@ -2,28 +2,27 @@
 
 #nullable disable
 
-namespace Bit.PostgresMigrations.Migrations
+namespace Bit.PostgresMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class DropOrganizationFlexibleCollections : Migration
 {
     /// <inheritdoc />
-    public partial class DropOrganizationFlexibleCollections : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "FlexibleCollections",
-                table: "Organization");
-        }
+        migrationBuilder.DropColumn(
+            name: "FlexibleCollections",
+            table: "Organization");
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<bool>(
-                name: "FlexibleCollections",
-                table: "Organization",
-                type: "boolean",
-                nullable: false,
-                defaultValue: false);
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<bool>(
+            name: "FlexibleCollections",
+            table: "Organization",
+            type: "boolean",
+            nullable: false,
+            defaultValue: false);
     }
 }
