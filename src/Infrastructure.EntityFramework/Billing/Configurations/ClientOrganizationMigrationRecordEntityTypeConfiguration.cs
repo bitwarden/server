6c6aa13e02c9caf41eb7e0743163158f5470b9a3
Alex Morask <amorask@bitwarden.com>
2024-07-24 09:03:15 -0400

    Add ClientOrganizationMigrationRecord EF repository

---
+++ src/Infrastructure.EntityFramework/Billing/Configurations/ClientOrganizationMigrationRecordEntityTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Bit.Core.Billing.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bit.Infrastructure.EntityFramework.Billing.Configurations;
+
+public class ClientOrganizationMigrationRecordEntityTypeConfiguration : IEntityTypeConfiguration<ClientOrganizationMigrationRecord>
+{
+    public void Configure(EntityTypeBuilder<ClientOrganizationMigrationRecord> builder)
+    {
+        builder
+            .Property(c => c.Id)
+            .ValueGeneratedNever();
+
+        builder.ToTable(nameof(ClientOrganizationMigrationRecord));
+    }
+}
