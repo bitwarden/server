43231856e8cd01ea693101978ac9d782d4c0d7ec
Thomas Rittson <trittson@bitwarden.com>
2024-07-24 12:00:24 +1000

    Remove type

--- src/Sql/dbo/Stored Procedures/OrganizationUser_CreateMany.sql
+++ src/Sql/dbo/Stored Procedures/OrganizationUser_CreateMany.sql
@@ -1,5 +1,5 @@
 CREATE PROCEDURE [dbo].[OrganizationUser_CreateMany]
-    @OrganizationUsersInput [dbo].[OrganizationUserType] READONLY
+    @jsonData NVARCHAR(MAX)
 AS
 BEGIN
     SET NOCOUNT ON
@@ -22,20 +22,35 @@ BEGIN
         [AccessSecretsManager]
         )
     SELECT
-        OU.[Id],
-        OU.[OrganizationId],
-        OU.[UserId],
-        OU.[Email],
-        OU.[Key],
-        OU.[Status],
-        OU.[Type],
+        OUI.[Id],
+        OUI.[OrganizationId],
+        OUI.[UserId],
+        OUI.[Email],
+        OUI.[Key],
+        OUI.[Status],
+        OUI.[Type],
         0,  -- AccessAll will be removed shortly
-        OU.[ExternalId],
-        OU.[CreationDate],
-        OU.[RevisionDate],
-        OU.[Permissions],
-        OU.[ResetPasswordKey],
-        OU.[AccessSecretsManager]
+        OUI.[ExternalId],
+        OUI.[CreationDate],
+        OUI.[RevisionDate],
+        OUI.[Permissions],
+        OUI.[ResetPasswordKey],
+        OUI.[AccessSecretsManager]
     FROM
-        @OrganizationUsersInput OU
+        OPENJSON(@jsonData)
+        WITH (
+            [Id] UNIQUEIDENTIFIER '$.Id',
+            [OrganizationId] UNIQUEIDENTIFIER '$.OrganizationId',
+            [UserId] UNIQUEIDENTIFIER '$.UserId',
+            [Email] NVARCHAR(256) '$.Email',
+            [Key] VARCHAR(MAX) '$.Key',
+            [Status] SMALLINT '$.Status',
+            [Type] TINYINT '$.Type',
+            [ExternalId] NVARCHAR(300) '$.ExternalId',
+            [CreationDate] DATETIME2(7) '$.CreationDate',
+            [RevisionDate] DATETIME2(7) '$.RevisionDate',
+            [Permissions] NVARCHAR (MAX) '$.Permissions',
+            [ResetPasswordKey] VARCHAR (MAX) '$.ResetPasswordKey',
+            [AccessSecretsManager] BIT '$.AccessSecretsManager'
+        ) OUI
 END
