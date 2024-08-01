00937252b04d386e786d2e7fcdaf91b62886ed9f
Bernd Schoolmann <mail@quexten.com>
2024-08-01 17:32:59 +0200

    Add ssh key item type

---
+++ src/Api/Vault/Models/CipherSSHKeyModel.cs
@@ -0,0 +1,26 @@
+﻿using Bit.Core.Utilities;
+using Bit.Core.Vault.Models.Data;
+
+namespace Bit.Api.Vault.Models;
+
+public class CipherSSHKeyModel
+{
+    public CipherSSHKeyModel() { }
+
+    public CipherSSHKeyModel(CipherSSHKeyData data)
+    {
+        PrivateKey = data.PrivateKey;
+        PublicKey = data.PublicKey;
+        KeyAlgorithm = data.KeyAlgorithm;
+    }
+
+    [EncryptedString]
+    [EncryptedStringLength(5000)]
+    public string PrivateKey { get; set; }
+    [EncryptedString]
+    [EncryptedStringLength(1000)]
+    public string PublicKey { get; set; }
+    [EncryptedString]
+    [EncryptedStringLength(1000)]
+    public string KeyAlgorithm { get; set; }
+}
