594e5f32405e018d2f03b2f690e0ba71f2b7f810
Rui Tome <rtome@bitwarden.com>
2024-08-13 16:30:03 +0100

    Fix case on the sproc OrganizationUser_ReadManagedIdsByOrganizationId parameter

--- util/Migrator/DbScripts/2024-08-13_00_OrgUserReadManagedIdsByOrgId.sql
+++ util/Migrator/DbScripts/2024-08-13_00_OrgUserReadManagedIdsByOrgId.sql
@@ -1,5 +1,5 @@
 CREATE OR ALTER PROCEDURE [dbo].[OrganizationUser_ReadManagedIdsByOrganizationId]
-    @organizationId UNIQUEIDENTIFIER
+    @OrganizationId UNIQUEIDENTIFIER
 AS
 BEGIN
     SET NOCOUNT ON;
@@ -7,11 +7,11 @@ BEGIN
     SELECT OU.[Id]
     FROM [dbo].[OrganizationUser] OU
     INNER JOIN [dbo].[User] U ON OU.[UserId] = U.[Id]
-    WHERE OU.[OrganizationId] = @organizationId
+    WHERE OU.[OrganizationId] = @OrganizationId
     AND EXISTS (
         SELECT 1
         FROM [dbo].[OrganizationDomain] OD
-        WHERE OD.[OrganizationId] = @organizationId
+        WHERE OD.[OrganizationId] = @OrganizationId
             AND OD.[VerifiedDate] IS NOT NULL
             AND OD.[DomainName] = SUBSTRING(U.[Email], CHARINDEX('@', U.[Email]) + 1, LEN(U.[Email]))
     );
