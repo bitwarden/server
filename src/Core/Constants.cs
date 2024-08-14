db4ff79c91684644efdbc0404026dff4cbc6d533
Kyle Spearrin <kspearrin@users.noreply.github.com>
2024-08-14 10:44:22 -0400

    [AC-2928] Create automatic app login policy (#4562)
    * Create automatic app login policy
    
    * IDP Auto Submit Feature Flag (#4564)
    
    ---------
    
    Co-authored-by: Cesar Gonzalez <cgonzalez@bitwarden.com>

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -131,6 +131,7 @@ public static class FeatureFlagKeys
     public const string DeviceTrustLogging = "pm-8285-device-trust-logging";
     public const string AuthenticatorTwoFactorToken = "authenticator-2fa-token";
     public const string EnableUpgradePasswordManagerSub = "AC-2708-upgrade-password-manager-sub";
+    public const string IdpAutoSubmitLogin = "idp-auto-submit-login";
     public const string UnauthenticatedExtensionUIRefresh = "unauth-ui-refresh";
     public const string GenerateIdentityFillScriptRefactor = "generate-identity-fill-script-refactor";
     public const string DelayFido2PageScriptInitWithinMv2 = "delay-fido2-page-script-init-within-mv2";
