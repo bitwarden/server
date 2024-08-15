f34e6434916da21e72a7f9c498186595531b8458
Rui Tome <rtome@bitwarden.com>
2024-08-15 12:10:10 +0100

    dotnet format

--- src/Infrastructure.EntityFramework/AdminConsole/Repositories/Queries/OrganizationUserReadManagedIdsByOrganizationIdQuery.cs
+++ src/Infrastructure.EntityFramework/AdminConsole/Repositories/Queries/OrganizationUserReadManagedIdsByOrganizationIdQuery.cs
@@ -12,13 +12,13 @@ public class OrganizationUserReadManagedIdsByOrganizationIdQuery : IQuery<Guid>
     public IQueryable<Guid> Run(DatabaseContext dbContext)
     {
         var query = from ou in dbContext.OrganizationUsers
-            join u in dbContext.Users on ou.UserId equals u.Id
-            where ou.OrganizationId == _organizationId &&
-                  dbContext.OrganizationDomains
-                      .Any(od => od.OrganizationId == _organizationId &&
-                                 od.VerifiedDate != null &&
-                                 od.DomainName == u.Email.Substring(u.Email.IndexOf('@') + 1))
-            select ou.Id;
+                    join u in dbContext.Users on ou.UserId equals u.Id
+                    where ou.OrganizationId == _organizationId &&
+                          dbContext.OrganizationDomains
+                              .Any(od => od.OrganizationId == _organizationId &&
+                                         od.VerifiedDate != null &&
+                                         od.DomainName == u.Email.Substring(u.Email.IndexOf('@') + 1))
+                    select ou.Id;
 
         return query;
     }
