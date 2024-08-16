67cd929c9e4ce86124748de1b995c25711ddb79d
Rui Tome <rtome@bitwarden.com>
2024-08-16 14:26:19 +0100

    Add summary to IOrganizationDomainService.HasVerifiedDomainsAsync

--- src/Core/Services/IOrganizationDomainService.cs
+++ src/Core/Services/IOrganizationDomainService.cs
@@ -4,5 +4,8 @@ public interface IOrganizationDomainService
 {
     Task ValidateOrganizationsDomainAsync();
     Task OrganizationDomainMaintenanceAsync();
+    /// <summary>
+    /// Indicates if the organization has any verified domains.
+    /// </summary>
     Task<bool> HasVerifiedDomainsAsync(Guid orgId);
 }
