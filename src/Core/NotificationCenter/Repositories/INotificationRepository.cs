b5231750d61674e7496c4dc80eb09bfbdd0d5886
Thomas Avery <tavery@bitwarden.com>
2024-08-21 17:54:15 -0500

    Add repository interfaces

---
+++ src/Core/NotificationCenter/Repositories/INotificationRepository.cs
@@ -0,0 +1,12 @@
+﻿#nullable enable
+using Bit.Core.NotificationCenter.Entities;
+using Bit.Core.NotificationCenter.Models.Filter;
+using Bit.Core.Repositories;
+
+namespace Bit.Core.NotificationCenter.Repositories;
+
+public interface INotificationRepository : IRepository<Notification, Guid>
+{
+    Task<IEnumerable<Notification>> GetByUserIdAsync(Guid userId, NotificationFilter notificationFilter);
+    Task<IEnumerable<Notification>> GetByUserIdAndStatusAsync(Guid userId, NotificationFilter notificationFilter, NotificationStatusFilter statusFilter);
+}
