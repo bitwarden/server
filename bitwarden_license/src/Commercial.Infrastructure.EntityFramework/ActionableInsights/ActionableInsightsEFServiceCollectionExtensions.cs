aa6a8b18874179accc8600b6d25171440bce712d
Maciej Zieniuk <mzieniuk@bitwarden.com>
2024-08-21 22:52:43 +0100

    PM-11240: Report EF repository

---
+++ bitwarden_license/src/Commercial.Infrastructure.EntityFramework/ActionableInsights/ActionableInsightsEFServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Bit.Commercial.Infrastructure.EntityFramework.ActionableInsights.Repositories;
+using Bit.Core.ActionableInsights.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bit.Commercial.Infrastructure.EntityFramework.ActionableInsights;
+
+public static class ActionableInsightsEfServiceCollectionExtensions
+{
+    public static void AddActionableInsightsEfRepositories(this IServiceCollection services)
+    {
+        services.AddSingleton<IReportRepository, ReportRepository>();
+    }
+}
---
+++ bitwarden_license/src/Commercial.Infrastructure.EntityFramework/ActionableInsights/Repositories/ReportRepository.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Bit.Core.ActionableInsights.Entities;
+using Bit.Core.ActionableInsights.Repositories;
+using Bit.Infrastructure.EntityFramework.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bit.Commercial.Infrastructure.EntityFramework.ActionableInsights.Repositories;
+
+public class ReportRepository(IServiceScopeFactory serviceScopeFactory, IMapper mapper)
+    : Repository<Report, Report, Guid>(serviceScopeFactory, mapper, db => db.Report),
+        IReportRepository
+{
+    public async Task<Report?> GetByOrganizationId(Guid organizationId)
+    {
+        await using var scope = ServiceScopeFactory.CreateAsyncScope();
+        var dbContext = GetDatabaseContext(scope);
+        var report = await dbContext.Report
+            .Where(c => c.OrganizationId == organizationId)
+            .FirstOrDefaultAsync();
+        return Mapper.Map<Report>(report);
+    }
+}
