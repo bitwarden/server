f47de6436154d1bf5288ccbc43490bce6c30e294
Thomas Avery <tavery@bitwarden.com>
2024-08-21 17:53:59 -0500

    Setup EF

---
+++ src/Infrastructure.EntityFramework/NotificationCenter/Configurations/NotificationEntityTypeConfiguration.cs
@@ -0,0 +1,34 @@
+﻿#nullable enable
+using Bit.Infrastructure.EntityFramework.NotificationCenter.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bit.Infrastructure.EntityFramework.NotificationCenter.Configurations;
+
+public class NotificationEntityTypeConfiguration : IEntityTypeConfiguration<Notification>
+{
+    public void Configure(EntityTypeBuilder<Notification> builder)
+    {
+        builder
+            .Property(n => n.Id)
+            .ValueGeneratedNever();
+
+        builder
+            .HasKey(n => n.Id)
+            .IsClustered();
+
+        builder
+            .HasIndex(n => n.RevisionDate)
+            .IsClustered(false);
+
+        builder
+            .HasIndex(n => n.OrganizationId)
+            .IsClustered(false);
+
+        builder
+            .HasIndex(n => n.UserId)
+            .IsClustered(false);
+
+        builder.ToTable(nameof(Notification));
+    }
+}
