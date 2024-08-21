3fec2d7359601b3b70c1d62923af7e3a3c305412
Maciej Zieniuk <mzieniuk@bitwarden.com>
2024-08-21 22:53:41 +0100

    PM-11240: Commercial Dapper project, Report Dapper repository

---
+++ bitwarden_license/src/Commercial.Infrastructure.Dapper/ActionableInsights/Repositories/ReportRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+using Bit.Core.ActionableInsights.Entities;
+using Bit.Core.ActionableInsights.Repositories;
+using Bit.Core.Settings;
+using Bit.Infrastructure.Dapper.Repositories;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace Bit.Commercial.Infrastructure.Dapper.ActionableInsights.Repositories;
+
+public class ReportRepository(string connectionString, string readOnlyConnectionString)
+    : Repository<Report, Guid>(connectionString, readOnlyConnectionString), IReportRepository
+{
+    public ReportRepository(GlobalSettings globalSettings)
+        : this(globalSettings.SqlServer.ConnectionString, globalSettings.SqlServer.ReadOnlyConnectionString)
+    {
+    }
+
+
+    public async Task<Report?> GetByOrganizationId(Guid organizationId)
+    {
+        await using var connection = new SqlConnection(ConnectionString);
+        var results = await connection.QueryAsync<Report>(
+            $"[{Schema}].[Report_ReadByOrganizationId]",
+            new { OrganizationId = organizationId },
+            commandType: CommandType.StoredProcedure);
+
+        return results.SingleOrDefault();
+    }
+}
---
+++ bitwarden_license/src/Commercial.Infrastructure.Dapper/CommercialDapperServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Bit.Commercial.Infrastructure.Dapper.ActionableInsights.Repositories;
+using Bit.Core.ActionableInsights.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bit.Commercial.Infrastructure.Dapper;
+
+public static class CommercialDapperServiceCollectionExtensions
+{
+    public static void AddCommercialDapperRepositories(this IServiceCollection services)
+    {
+        services.AddSingleton<IReportRepository, ReportRepository>();
+    }
+}
