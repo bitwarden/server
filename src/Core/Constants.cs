8cfb3a08302d802aedd357d7f38a55534a4fd042
Maciej Zieniuk <mzieniuk@bitwarden.com>
2024-08-21 22:48:09 +0100

    PM-11240: Actionable Insights feature flag

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -139,6 +139,7 @@ public static class FeatureFlagKeys
     public const string NativeCarouselFlow = "native-carousel-flow";
     public const string NativeCreateAccountFlow = "native-create-account-flow";
     public const string AccountDeprovisioning = "pm-10308-account-deprovisioning";
+    public const string ActionableInsights = "bw-57-actionable-insights";
 
     public static List<string> GetAllKeys()
     {
