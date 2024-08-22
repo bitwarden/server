899d8f60bea1c3b7926f370950d13dd29e0f3b03
Thomas Avery <tavery@bitwarden.com>
2024-08-22 15:07:20 -0500

    EF updates

--- src/Infrastructure.EntityFramework/NotificationCenter/Repositories/NotificationRepository.cs
+++ src/Infrastructure.EntityFramework/NotificationCenter/Repositories/NotificationRepository.cs
@@ -22,33 +22,7 @@ public class NotificationRepository : Repository<Core.NotificationCenter.Entitie
         await using var scope = ServiceScopeFactory.CreateAsyncScope();
         var dbContext = GetDatabaseContext(scope);
 
-        IQueryable<Notification> notificationQuery;
-
-        if (notificationFilter.OrganizationIds != null && notificationFilter.OrganizationIds.Any())
-        {
-            notificationQuery = dbContext.Notifications
-                .Where(n =>
-                    (
-                        n.ClientType == notificationFilter.ClientType &&
-                        n.UserId == userId
-                    ) ||
-                    n.Global == true ||
-                    (
-                        n.OrganizationId != null &&
-                        n.UserId == null &&
-                        notificationFilter.OrganizationIds.Contains(n.OrganizationId.Value)
-                    )
-                );
-        }
-        else
-        {
-            notificationQuery = dbContext.Notifications
-                .Where(n =>
-                (
-                    n.ClientType == notificationFilter.ClientType &&
-                    n.UserId == userId
-                ) || n.Global == true);
-        }
+        var notificationQuery = BuildNotificationQuery(dbContext, userId, notificationFilter);
 
         var notifications = await notificationQuery.OrderByDescending(c => c.CreationDate).ToListAsync();
         return Mapper.Map<List<Core.NotificationCenter.Entities.Notification>>(notifications);
@@ -61,11 +35,28 @@ public class NotificationRepository : Repository<Core.NotificationCenter.Entitie
         await using var scope = ServiceScopeFactory.CreateAsyncScope();
         var dbContext = GetDatabaseContext(scope);
 
-        IQueryable<Notification> notificationQuery;
+        var notificationQuery = BuildNotificationQuery(dbContext, userId, notificationFilter);
 
+        notificationQuery = from n in notificationQuery
+                            join ns in dbContext.NotificationStatuses on n.Id equals ns.NotificationId into grouping
+                            from ns in grouping.DefaultIfEmpty()
+                            where
+                                (ns.UserId == userId || ns.UserId == null) &&
+                                (statusFilter.Read == true ? ns.ReadDate != null : ns.ReadDate == null) &&
+                                (statusFilter.Deleted == true ? ns.DeletedDate != null : ns.DeletedDate == null)
+                            select n;
+
+        var notifications = await notificationQuery.OrderByDescending(c => c.CreationDate).ToListAsync();
+
+        return Mapper.Map<List<Core.NotificationCenter.Entities.Notification>>(notifications);
+    }
+
+    private static IQueryable<Notification> BuildNotificationQuery(DatabaseContext dbContext, Guid userId,
+        NotificationFilter notificationFilter)
+    {
         if (notificationFilter.OrganizationIds != null && notificationFilter.OrganizationIds.Any())
         {
-            notificationQuery = dbContext.Notifications
+            return dbContext.Notifications
                 .Where(n =>
                     (
                         n.ClientType == notificationFilter.ClientType &&
@@ -79,26 +70,12 @@ public class NotificationRepository : Repository<Core.NotificationCenter.Entitie
                     )
                 );
         }
-        else
-        {
-            notificationQuery = dbContext.Notifications
-                .Where(n =>
-                (
-                    n.ClientType == notificationFilter.ClientType &&
-                    n.UserId == userId
-                ) || n.Global == true);
-        }
 
-        notificationQuery = from n in notificationQuery
-                            join ns in dbContext.NotificationStatuses on n.Id equals ns.NotificationId into grouping
-                            from ns in grouping.DefaultIfEmpty()
-                            where
-                                  (ns.UserId == userId || ns.UserId == null) &&
-                                  (statusFilter.Read == true ? ns.ReadDate != null : ns.ReadDate == null) &&
-                                  (statusFilter.Deleted == true ? ns.DeletedDate != null : ns.DeletedDate == null)
-                            select n;
-
-        var notifications = await notificationQuery.OrderByDescending(c => c.CreationDate).ToListAsync();
-        return Mapper.Map<List<Core.NotificationCenter.Entities.Notification>>(notifications);
+        return dbContext.Notifications
+            .Where(n =>
+            (
+                n.ClientType == notificationFilter.ClientType &&
+                n.UserId == userId
+            ) || n.Global == true);
     }
 }
