520c0bf5f875520dca1691edbc90a8451e20a39a
Jared Snider <jsnider@bitwarden.com>
2024-08-22 17:16:30 -0400

    PM-11252 - RegisterVerifyEmail - use url fragment structure to obfuscate query params from logging and prevent open redirects.

--- src/Core/Auth/Models/Mail/RegisterVerifyEmail.cs
+++ src/Core/Auth/Models/Mail/RegisterVerifyEmail.cs
@@ -10,7 +10,9 @@ public class RegisterVerifyEmail : BaseMailModel
     // is coming to the finish signup page from an email link and not directly from another route in the app.
     // Note 2: we cannot use a web vault url which contains a # as that is a reserved wild character on Android
     // so we must land on a redirect connector which will redirect to the finish signup page.
-    public string Url => string.Format("{0}/signup-redirect-connector.html?token={1}&email={2}&fromEmail=true",
+    // Note 3: The use of a fragment to indicate the redirect url is to prevent the query string from being logged by
+    // proxies and servers. It also helps reduce open redirect vulnerabilities.
+    public string Url => string.Format("{0}/redirect-connector.html#finish-signup?token={1}&email={2}&fromEmail=true",
         WebVaultUrl,
         Token,
         Email);
