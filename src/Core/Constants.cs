2351cd5c2ba293ee7a903e15c26c4ca0e18758cb
Cy Okeke <cokeke@bitwarden.com>
2024-08-28 09:46:58 +0100

    Add the feature flag
    Signed-off-by: Cy Okeke <cokeke@bitwarden.com>

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -139,6 +139,7 @@ public static class FeatureFlagKeys
     public const string NativeCreateAccountFlow = "native-create-account-flow";
     public const string AccountDeprovisioning = "pm-10308-account-deprovisioning";
     public const string NotificationBarAddLoginImprovements = "notification-bar-add-login-improvements";
+    public const string TrialPayment = "pm-8163-trial-payment";
 
     public static List<string> GetAllKeys()
     {
