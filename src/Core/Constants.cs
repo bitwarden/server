6dd8b1803c0a3ad0c894c470f3c1bd7ca0e0cf2c
Todd Martin <tmartin@bitwarden.com>
2024-08-30 16:25:48 -0400

    Linting

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -141,7 +141,7 @@ public static class FeatureFlagKeys
     public const string NotificationBarAddLoginImprovements = "notification-bar-add-login-improvements";
     public const string AC2476_DeprecateStripeSourcesAPI = "AC-2476-deprecate-stripe-sources-api";
     public const string PersistPopupView = "persist-popup-view";
-    
+
     public static List<string> GetAllKeys()
     {
         return typeof(FeatureFlagKeys).GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
