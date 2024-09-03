122f20bb3c70d009d4b3b2a48d9e5763b4738edb
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2024-09-03 16:41:48 -0400

    Add Proxy Project

---
+++ src/Proxy/Program.cs
@@ -0,0 +1,10 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var app = builder.Build();
+
+app.MapReverseProxy();
+
+app.Run();
