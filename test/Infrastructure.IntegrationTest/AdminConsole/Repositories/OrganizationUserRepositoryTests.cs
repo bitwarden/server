f3ddf2237f5915029d7aa8cc514af60d9fbcdcb8
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2024-09-10 11:05:39 -0400

    Prefer Long-Hand Options

--- test/Infrastructure.IntegrationTest/AdminConsole/Repositories/OrganizationUserRepositoryTests.cs
+++ test/Infrastructure.IntegrationTest/AdminConsole/Repositories/OrganizationUserRepositoryTests.cs
@@ -13,6 +13,7 @@ public class OrganizationUserRepositoryTests
         IOrganizationRepository organizationRepository,
         IOrganizationUserRepository organizationUserRepository)
     {
+        throw new Exception("Test Failure");
         var user = await userRepository.CreateAsync(new User
         {
             Name = "Test User",
