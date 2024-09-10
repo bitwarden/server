fe31e1e1481626e555d950398e3780510b8def9c
Thomas Rittson <trittson@bitwarden.com>
2024-09-10 14:10:34 +1000

    Move operations into separate class

--- src/Core/AdminConsole/OrganizationFeatures/OrganizationUsers/Authorization/OrganizationUserUserMiniDetailsAuthorizationHandler.cs
+++ src/Core/AdminConsole/OrganizationFeatures/OrganizationUsers/Authorization/OrganizationUserUserMiniDetailsAuthorizationHandler.cs
@@ -6,13 +6,6 @@ using Microsoft.AspNetCore.Authorization.Infrastructure;
 
 namespace Bit.Core.AdminConsole.OrganizationFeatures.OrganizationUsers.Authorization;
 
-public class OrganizationUserUserMiniDetailsOperationRequirement : OperationAuthorizationRequirement;
-
-public static class OrganizationUserUserMiniDetailsOperations
-{
-    public static readonly OrganizationUserUserMiniDetailsOperationRequirement ReadAll = new() { Name = nameof(ReadAll) };
-}
-
 public class OrganizationUserUserMiniDetailsAuthorizationHandler :
     AuthorizationHandler<OrganizationUserUserMiniDetailsOperationRequirement, OrganizationIdResource>
 {
