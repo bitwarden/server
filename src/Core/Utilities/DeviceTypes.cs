e77bdce70a08e45b5ab65ca8bdaea4e983091402
Maciej Zieniuk <mzieniuk@bitwarden.com>
2024-09-12 10:02:58 +0100

    PM-11123: Device Type mapping

--- src/Core/Utilities/DeviceTypes.cs
+++ src/Core/Utilities/DeviceTypes.cs
@@ -4,21 +4,44 @@ namespace Bit.Core.Utilities;
 
 public static class DeviceTypes
 {
-    public static IReadOnlyCollection<DeviceType> MobileTypes { get; } = new[]
-    {
+    public static IReadOnlyCollection<DeviceType> MobileTypes { get; } =
+    [
         DeviceType.Android,
         DeviceType.iOS,
-        DeviceType.AndroidAmazon,
-    };
+        DeviceType.AndroidAmazon
+    ];
 
-    public static IReadOnlyCollection<DeviceType> DesktopTypes { get; } = new[]
-    {
+    public static IReadOnlyCollection<DeviceType> DesktopTypes { get; } =
+    [
         DeviceType.LinuxDesktop,
         DeviceType.MacOsDesktop,
         DeviceType.WindowsDesktop,
         DeviceType.UWP,
         DeviceType.WindowsCLI,
         DeviceType.MacOsCLI,
-        DeviceType.LinuxCLI,
-    };
+        DeviceType.LinuxCLI
+    ];
+
+
+    public static IReadOnlyCollection<DeviceType> BrowserExtensionTypes { get; } =
+    [
+        DeviceType.ChromeExtension,
+        DeviceType.FirefoxExtension,
+        DeviceType.OperaExtension,
+        DeviceType.EdgeExtension,
+        DeviceType.VivaldiExtension,
+        DeviceType.SafariExtension
+    ];
+
+    public static IReadOnlyCollection<DeviceType> BrowserTypes { get; } =
+    [
+        DeviceType.ChromeBrowser,
+        DeviceType.FirefoxBrowser,
+        DeviceType.OperaBrowser,
+        DeviceType.EdgeBrowser,
+        DeviceType.IEBrowser,
+        DeviceType.SafariBrowser,
+        DeviceType.VivaldiBrowser,
+        DeviceType.UnknownBrowser
+    ];
 }
