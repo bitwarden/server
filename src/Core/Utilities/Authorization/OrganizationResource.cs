6b2aec91184de6e889a11b973648ee246b52c0c4
Thomas Rittson <trittson@bitwarden.com>
2024-09-16 11:28:53 +1000

    Add various authorization helpers

---
+++ src/Core/Utilities/Authorization/OrganizationResource.cs
@@ -0,0 +1,24 @@
+﻿#nullable enable
+
+namespace Bit.Core.Utilities.Authorization;
+
+/// <summary>
+/// A typed wrapper for an organization Guid. This is used for authorization checks because
+/// AuthorizationService needs more than just a Guid, but we also don't want to fetch the
+/// Organization object from the database each time when it's usually not needed.
+/// It implicitly converts to a regular Guid.
+/// </summary>
+public class OrganizationResource
+{
+    public OrganizationResource(Guid id)
+    {
+        Id = id;
+    }
+
+    private Guid Id { get; }
+
+    public static implicit operator Guid(OrganizationResource organizationResource) =>
+        organizationResource.Id;
+
+    public override string ToString() => Id.ToString();
+}
---
+++ test/Core.Test/Utilities/OrganizationResourceTests.cs
@@ -0,0 +1,23 @@
+﻿using Bit.Core.Utilities.Authorization;
+using Bit.Test.Common.AutoFixture.Attributes;
+using Xunit;
+
+namespace Bit.Core.Test.Utilities;
+
+public class OrganizationResourceTests
+{
+    [Theory, BitAutoData]
+    public void OrganizationResource_ConvertsToGuid(Guid id)
+    {
+        var organizationResource = new OrganizationResource(id);
+        Guid result = organizationResource;
+        Assert.Equal(0, result.CompareTo(id));
+    }
+
+    [Theory, BitAutoData]
+    public void OrganizationResource_ConvertsToString(Guid id)
+    {
+        var organizationResource = new OrganizationResource(id);
+        Assert.Equal(organizationResource.ToString(), id.ToString());
+    }
+}
