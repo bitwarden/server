93b7661a5035485cac93951e31ce13e9ac416028
Alex Morask <amorask@bitwarden.com>
2024-09-20 12:13:58 -0400

    Admin: Scaffold ProcessStripeEvents section

---
+++ src/Admin/Billing/Controllers/ProcessStripeEventsController.cs
@@ -0,0 +1,25 @@
+﻿using Bit.Admin.Billing.Models.ProcessStripeEvents;
+using Bit.Core.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bit.Admin.Billing.Controllers;
+
+[Authorize]
+[Route("process-stripe-events")]
+[SelfHosted(NotSelfHostedOnly = true)]
+public class ProcessStripeEventsController : Controller
+{
+    [HttpGet]
+    public ActionResult Index()
+    {
+        return View(new EventsFormModel());
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult ProcessAsync([FromForm] EventsFormModel model)
+    {
+        throw new NotImplementedException();
+    }
+}
