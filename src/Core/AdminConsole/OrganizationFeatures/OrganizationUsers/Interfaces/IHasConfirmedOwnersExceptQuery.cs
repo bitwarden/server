c1eda8093466bcb8fd87883e244efdf7b7a07c6e
Rui Tome <rtome@bitwarden.com>
2024-09-23 14:11:13 +0100

    Add HasConfirmedOwnersExceptQuery class, interface and unit tests

---
+++ src/Core/AdminConsole/OrganizationFeatures/OrganizationUsers/Interfaces/IHasConfirmedOwnersExceptQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace Bit.Core.AdminConsole.OrganizationFeatures.OrganizationUsers.Interfaces;
+
+public interface IHasConfirmedOwnersExceptQuery
+{
+    Task<bool> HasConfirmedOwnersExceptAsync(Guid organizationId, IEnumerable<Guid> organizationUsersId, bool includeProvider = true);
+}
