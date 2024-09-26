cd43535e66d5727c527feb79366ceeb604392d1c
Alex Morask <144709477+amorask-bitwarden@users.noreply.github.com>
2024-09-26 09:18:21 -0400

    [PM-12420] Stripe events recovery (#4793)
    * Billing: Add event recovery endpoints
    
    * Core: Add InternalBilling to BaseServiceUriSettings
    
    * Admin: Scaffold billing section
    
    * Admin: Scaffold ProcessStripeEvents section
    
    * Admin: Implement event processing
    
    * Run dotnet format

--- src/Billing/Services/Implementations/StripeFacade.cs
+++ src/Billing/Services/Implementations/StripeFacade.cs
@@ -6,6 +6,7 @@ public class StripeFacade : IStripeFacade
 {
     private readonly ChargeService _chargeService = new();
     private readonly CustomerService _customerService = new();
+    private readonly EventService _eventService = new();
     private readonly InvoiceService _invoiceService = new();
     private readonly PaymentMethodService _paymentMethodService = new();
     private readonly SubscriptionService _subscriptionService = new();
@@ -18,6 +19,13 @@ public class StripeFacade : IStripeFacade
         RequestOptions requestOptions = null,
         CancellationToken cancellationToken = default) =>
         await _chargeService.GetAsync(chargeId, chargeGetOptions, requestOptions, cancellationToken);
+
+    public async Task<Event> GetEvent(
+        string eventId,
+        EventGetOptions eventGetOptions = null,
+        RequestOptions requestOptions = null,
+        CancellationToken cancellationToken = default) =>
+        await _eventService.GetAsync(eventId, eventGetOptions, requestOptions, cancellationToken);
 
     public async Task<Customer> GetCustomer(
         string customerId,
