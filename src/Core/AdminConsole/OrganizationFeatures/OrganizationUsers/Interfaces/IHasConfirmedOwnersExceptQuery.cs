56ff00a6b238dd7b7ff9d4d42a5acc493fc2277a
Rui Tome <rtome@bitwarden.com>
2024-10-02 14:35:24 +0100

    Add summary to IHasConfirmedOwnersExceptQuery

--- src/Core/AdminConsole/OrganizationFeatures/OrganizationUsers/Interfaces/IHasConfirmedOwnersExceptQuery.cs
+++ src/Core/AdminConsole/OrganizationFeatures/OrganizationUsers/Interfaces/IHasConfirmedOwnersExceptQuery.cs
@@ -2,5 +2,11 @@
 
 public interface IHasConfirmedOwnersExceptQuery
 {
+    /// <summary>
+    /// Checks if an organization has any confirmed owners except for the ones in the <paramref name="organizationUsersId"/> list.
+    /// </summary>
+    /// <param name="organizationId">The organization ID.</param>
+    /// <param name="organizationUsersId">The organization user IDs to exclude.</param>
+    /// <param name="includeProvider">Whether to include the provider users in the count.</param>
     Task<bool> HasConfirmedOwnersExceptAsync(Guid organizationId, IEnumerable<Guid> organizationUsersId, bool includeProvider = true);
 }
