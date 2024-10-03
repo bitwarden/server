6a51e3b1a90815028ef4c453814862ce8af7c98c
Addison Beck <addison@bitwarden.com>
2024-10-03 13:43:54 -0400

    Split `LimitCollectionCreationDeletion` into two database columns (#4709)
    * Add new columns to `dbo.Organization` & its references
    
    * Feed existing data into new `dbo.Organization` column
    
    * Update Entity Framework database definitions
    
    * Move new EF columns out of the core entity definition
    
    * Generate Entity Framework migrations
    
    * Feed existing data into new `Organization` Entity Framework columns
    
    * Add a where clause to SQL migration

--- util/MySqlMigrations/Migrations/20240925202356_SyncOrganizationLimitCollectionCreationDeletionColumn.Designer.cs
+++ util/MySqlMigrations/Migrations/20240925202356_SyncOrganizationLimitCollectionCreationDeletionColumn.Designer.cs
@@ -4,6 +4,7 @@ using Bit.Infrastructure.EntityFramework.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bit.MySqlMigrations.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240925202356_SyncOrganizationLimitCollectionCreationDeletionColumn")]
+    partial class SyncOrganizationLimitCollectionCreationDeletionColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
