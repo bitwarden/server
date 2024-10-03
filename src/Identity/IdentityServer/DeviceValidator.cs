cef5a14745edc3f50561663faa08a696bf9cddcd
Ike Kottlowski <ikottlowski@bitwarden.com>
2024-10-02 17:37:18 -0700

    Unit Testing

--- src/Identity/IdentityServer/DeviceValidator.cs
+++ src/Identity/IdentityServer/DeviceValidator.cs
@@ -1,4 +1,4 @@
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Reflection;
 using Bit.Core.Context;
 using Bit.Core.Entities;
@@ -29,17 +29,25 @@ public class DeviceValidator(
     private readonly IMailService _mailService = mailService;
     private readonly ICurrentContext _currentContext = currentContext;
 
+    /// <summary>
+    /// Save a device to the database. If the device is already known, it will be returned.
+    /// </summary>
+    /// <param name="user">The user is assumed NOT null, still going to check though</param>
+    /// <param name="request">Duende Validated Request that contains the data to create the device object</param>
+    /// <returns>Returns null if user or device is malformed; The existing device if already in DB; a new device login</returns>
     public async Task<Device> SaveDeviceAsync(User user, ValidatedTokenRequest request)
     {
         var device = GetDeviceFromRequest(request);
-        if (device != null)
+        if (device != null && user != null)
         {
-            var existingDevice = await GetKnownDeviceAsync(user, request);
+            var existingDevice = await GetKnownDeviceAsync(user, device);
             if (existingDevice == null)
             {
                 device.UserId = user.Id;
                 await _deviceService.SaveAsync(device);
 
+                // This check just seems like a way to avoid sending an email on the first login
+                // maybe vestigial?
                 var now = DateTime.UtcNow;
                 if (now - user.CreationDate > TimeSpan.FromMinutes(10))
                 {
@@ -59,27 +67,33 @@ public class DeviceValidator(
     }
 
     public async Task<bool> KnownDeviceAsync(User user, ValidatedTokenRequest request) =>
-        (await GetKnownDeviceAsync(user, request)) != default;
+        (await GetKnownDeviceAsync(user, GetDeviceFromRequest(request))) != default;
 
-    protected async Task<Device> GetKnownDeviceAsync(User user, ValidatedTokenRequest request)
+    /*
+        This is called from a place in the code that has already verified the User is not null.
+        The user Check is safe but will almost always be true.
+    */
+    protected async Task<Device> GetKnownDeviceAsync(User user, Device device)
     {
         if (user == null)
         {
             return default;
         }
 
-        return await _deviceRepository.GetByIdentifierAsync(GetDeviceFromRequest(request).Identifier, user.Id);
+        return await _deviceRepository.GetByIdentifierAsync(device.Identifier, user.Id);
     }
 
     private Device GetDeviceFromRequest(ValidatedRequest request)
     {
         var deviceIdentifier = request.Raw["DeviceIdentifier"]?.ToString();
-        var deviceType = request.Raw["DeviceType"]?.ToString();
+        var requestDeviceType = request.Raw["DeviceType"]?.ToString();
         var deviceName = request.Raw["DeviceName"]?.ToString();
         var devicePushToken = request.Raw["DevicePushToken"]?.ToString();
 
-        if (string.IsNullOrWhiteSpace(deviceIdentifier) || string.IsNullOrWhiteSpace(deviceType) ||
-            string.IsNullOrWhiteSpace(deviceName) || !Enum.TryParse(deviceType, out DeviceType type))
+        if (string.IsNullOrWhiteSpace(deviceIdentifier) || 
+            string.IsNullOrWhiteSpace(requestDeviceType) ||
+            string.IsNullOrWhiteSpace(deviceName) || 
+            !Enum.TryParse(requestDeviceType, out DeviceType parsedDeviceType))
         {
             return null;
         }
@@ -88,7 +102,7 @@ public class DeviceValidator(
         {
             Identifier = deviceIdentifier,
             Name = deviceName,
-            Type = type,
+            Type = parsedDeviceType,
             PushToken = string.IsNullOrWhiteSpace(devicePushToken) ? null : devicePushToken
         };
     }
---
+++ test/Identity.Test/IdentityServer/DeviceValidatorTests.cs
@@ -0,0 +1,157 @@
+﻿using Bit.Core.Context;
+using Bit.Core.Entities;
+using Bit.Core.Enums;
+using Bit.Core.Repositories;
+using Bit.Core.Services;
+using Bit.Core.Settings;
+using Bit.Identity.IdentityServer;
+using Bit.Test.Common.AutoFixture.Attributes;
+using Duende.IdentityServer.Validation;
+using NSubstitute;
+using Xunit;
+using AuthFixtures = Bit.Identity.Test.AutoFixture;
+
+namespace Bit.Identity.Test.IdentityServer;
+
+public class DeviceValidatorTests
+{
+    private readonly IDeviceService _deviceService;
+    private readonly IDeviceRepository _deviceRepository;
+    private readonly GlobalSettings _globalSettings;
+    private readonly IMailService _mailService;
+    private readonly ICurrentContext _currentContext;
+    private readonly DeviceValidator _sut;
+
+    public DeviceValidatorTests()
+    {
+        _deviceService = Substitute.For<IDeviceService>();
+        _deviceRepository = Substitute.For<IDeviceRepository>();
+        _globalSettings = new GlobalSettings();
+        _mailService = Substitute.For<IMailService>();
+        _currentContext = Substitute.For<ICurrentContext>();
+        _sut = new DeviceValidator(
+            _deviceService,
+            _deviceRepository,
+            _globalSettings,
+            _mailService,
+            _currentContext);
+    }
+
+    [Theory]
+    [BitAutoData]
+    public async void SaveDeviceAsync_DeviceNull_ShouldReturnNull(
+        [AuthFixtures.ValidatedTokenRequest] ValidatedTokenRequest request,
+        User user)
+    {
+        // Arrange
+        request.Raw["DeviceIdentifier"] = null;
+
+        // Act
+        var device = await _sut.SaveDeviceAsync(user, request);
+
+        // Assert
+        Assert.Null(device);
+        await _mailService.DidNotReceive().SendNewDeviceLoggedInEmail(
+            Arg.Any<string>(), Arg.Any<string>(), Arg.Any<DateTime>(), Arg.Any<string>());
+    }
+
+    [Theory]
+    [BitAutoData]
+    public async void SaveDeviceAsync_Success(
+        [AuthFixtures.ValidatedTokenRequest] ValidatedTokenRequest request,
+        User user)
+    {
+        // Arrange
+        request = AddValidDeviceToREquest(request);
+
+        user.CreationDate = DateTime.UtcNow - TimeSpan.FromMinutes(11);
+        _globalSettings.DisableEmailNewDevice = false;
+
+        // Act
+        var device = await _sut.SaveDeviceAsync(user, request);
+
+        // Assert
+        Assert.NotNull(device);
+        Assert.Equal(user.Id, device.UserId);
+        Assert.Equal("DeviceIdentifier", device.Identifier);
+        Assert.Equal(DeviceType.Android, device.Type);
+        await _mailService.Received(1).SendNewDeviceLoggedInEmail(
+            user.Email, "Android", Arg.Any<DateTime>(), Arg.Any<string>());
+    }
+
+    [Theory]
+    [BitAutoData]
+    public async void SaveDeviceAsync_UserIsNull_ShouldReturnNull(
+        [AuthFixtures.ValidatedTokenRequest] ValidatedTokenRequest request)
+    {
+        // Arrange
+        request = AddValidDeviceToREquest(request);
+
+        // Act
+        var device = await _sut.SaveDeviceAsync(null, request);
+
+        // Assert
+        Assert.Null(device);
+        await _mailService.DidNotReceive().SendNewDeviceLoggedInEmail(
+            Arg.Any<string>(), Arg.Any<string>(), Arg.Any<DateTime>(), Arg.Any<string>());
+    }
+
+    [Theory]
+    [BitAutoData]
+    public async void SaveDeviceAsync_DeviceIsKnown_ShouldReturnDevice(
+        [AuthFixtures.ValidatedTokenRequest] ValidatedTokenRequest request,
+        User user,
+        Device device)
+    {
+        // Arrange
+        request = AddValidDeviceToREquest(request);
+
+        device.UserId = user.Id;
+        device.Identifier = "DeviceIdentifier";
+        device.Type = DeviceType.Android;
+        device.Name = "DeviceName";
+        device.PushToken = "DevicePushToken";
+        _deviceRepository.GetByIdentifierAsync(device.Identifier, user.Id).Returns(device);
+
+        // Act
+        var resultDevice = await _sut.SaveDeviceAsync(user, request);
+
+        // Assert
+        Assert.Equal(device, resultDevice);
+        await _mailService.DidNotReceive().SendNewDeviceLoggedInEmail(
+            Arg.Any<string>(), Arg.Any<string>(), Arg.Any<DateTime>(), Arg.Any<string>());
+    }
+
+    [Theory]
+    [BitAutoData]
+    public async void SaveDeviceAsync_DeviceUnknown_NewUser_ShouldSaveDevice_NoEmail(
+        [AuthFixtures.ValidatedTokenRequest] ValidatedTokenRequest request,
+        User user)
+    {
+        // Arrange
+        request = AddValidDeviceToREquest(request);
+        user.CreationDate = DateTime.UtcNow;
+        _deviceRepository.GetByIdentifierAsync(Arg.Any<string>(), Arg.Any<Guid>()).Returns(null as Device);
+
+        // Act
+        var device = await _sut.SaveDeviceAsync(user, request);
+
+        // Assert
+        Assert.NotNull(device);
+        Assert.Equal(user.Id, device.UserId);
+        Assert.Equal("DeviceIdentifier", device.Identifier);
+        Assert.Equal(DeviceType.Android, device.Type);
+        await _deviceService.Received(1).SaveAsync(device);
+        await _mailService.DidNotReceive().SendNewDeviceLoggedInEmail(
+            Arg.Any<string>(), Arg.Any<string>(), Arg.Any<DateTime>(), Arg.Any<string>());
+    }
+
+    private ValidatedTokenRequest AddValidDeviceToREquest(ValidatedTokenRequest request)
+    {
+        request.Raw["DeviceIdentifier"] = "DeviceIdentifier";
+        request.Raw["DeviceType"] = "Android";
+        request.Raw["DeviceName"] = "DeviceName";
+        request.Raw["DevicePushToken"] = "DevicePushToken";
+        return request;
+    }
+}
