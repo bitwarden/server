d95eab70f4daf7c670da798521044611f3f023ab
jrmccannon <jmccannon@bitwarden.com>
2024-10-04 09:57:50 -0500

    Fixing stored proc to only use SSO config instead of the policy.

--- src/Sql/dbo/Stored Procedures/VerfiedOrganaizationDomainSsoDetails_ReadByEmail.sql
+++ src/Sql/dbo/Stored Procedures/VerfiedOrganaizationDomainSsoDetails_ReadByEmail.sql
@@ -15,10 +15,9 @@ BEGIN
         OD.DomainName
     FROM [dbo].[OrganizationView] O
              INNER JOIN [dbo].[OrganizationDomainView] OD ON O.Id = OD.OrganizationId
-             LEFT JOIN [dbo].[PolicyView] P ON O.Id = P.OrganizationId
              LEFT JOIN [dbo].[Ssoconfig] S ON O.Id = S.OrganizationId
     WHERE OD.DomainName = @Domain
       AND O.Enabled = 1
       AND OD.VerifiedDate IS NOT NULL
-      AND P.Id IS NOT NULL AND P.[Type] = 4 -- SSO Type
+      AND S.Enabled = 1
 END
