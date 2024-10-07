e288ca97a3ac6ad293799d4a63c82a76d30e69ce
Jared McCannon <jmccannon@bitwarden.com>
2024-10-07 14:39:57 -0500

    [PM-12358] New Verified Organization Domain SSO Detail endpoint (#4838)
    * Added /domain/sso/verified to organization controller
    
    * Restricting sproc to only return verified domains if the org has sso. Adding name. corrected route. removed not found exception. Adding the sproc definition to the SQL project

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -144,6 +144,7 @@ public static class FeatureFlagKeys
     public const string TrialPayment = "PM-8163-trial-payment";
     public const string Pm3478RefactorOrganizationUserApi = "pm-3478-refactor-organizationuser-api";
     public const string RemoveServerVersionHeader = "remove-server-version-header";
+    public const string VerifiedSsoDomainEndpoint = "pm-12337-refactor-sso-details-endpoint";
 
     public static List<string> GetAllKeys()
     {
