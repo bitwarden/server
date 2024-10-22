00a9829101cb6d538a685431c0d27c835070bf4c
Maciej Zieniuk <mzieniuk@bitwarden.com>
2024-10-22 10:29:11 +0100

    PM-10600: Sending to specific client types for relay push notifications

--- src/Core/Models/Api/Request/PushSendRequestModel.cs
+++ src/Core/Models/Api/Request/PushSendRequestModel.cs
@@ -1,22 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿#nullable enable
+using System.ComponentModel.DataAnnotations;
 using Bit.Core.Enums;
 
 namespace Bit.Core.Models.Api;
 
 public class PushSendRequestModel : IValidatableObject
 {
-    public string UserId { get; set; }
-    public string OrganizationId { get; set; }
-    public string DeviceId { get; set; }
-    public string Identifier { get; set; }
-    [Required]
-    public PushType? Type { get; set; }
-    [Required]
-    public object Payload { get; set; }
+    public string? UserId { get; set; }
+    public string? OrganizationId { get; set; }
+    public string? DeviceId { get; set; }
+    public string? Identifier { get; set; }
+    [Required] public PushType Type { get; set; }
+    [Required] public object Payload { get; set; } = null!;
+    public bool Global { get; set; }
+    public ClientType? ClientType { get; set; }
 
     public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
     {
-        if (string.IsNullOrWhiteSpace(UserId) && string.IsNullOrWhiteSpace(OrganizationId))
+        if (string.IsNullOrWhiteSpace(UserId) && string.IsNullOrWhiteSpace(OrganizationId) && !Global)
         {
             yield return new ValidationResult($"{nameof(UserId)} or {nameof(OrganizationId)} is required.");
         }
