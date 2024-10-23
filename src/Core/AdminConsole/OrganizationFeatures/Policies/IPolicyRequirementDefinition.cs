98fc74244dc10484f09480b1a42a4e65d804b087
Thomas Rittson <trittson@bitwarden.com>
2024-10-23 15:42:12 +1000

    Initial WIP

---
+++ src/Core/AdminConsole/OrganizationFeatures/Policies/IPolicyRequirementDefinition.cs
@@ -0,0 +1,13 @@
+﻿using Bit.Core.AdminConsole.Entities;
+using Bit.Core.AdminConsole.Enums;
+
+namespace Bit.Core.AdminConsole.OrganizationFeatures.Policies;
+
+public interface IPolicyRequirement;
+
+public interface IPolicyRequirementDefinition<T> where T : IPolicyRequirement
+{
+    PolicyType Type { get; }
+    T Reduce(IEnumerable<Policy> policies);
+    bool FilterPredicate(Policy policy);
+}
---
+++ src/Core/AdminConsole/OrganizationFeatures/Policies/Implementations/PolicyRequirementsQuery.cs
@@ -0,0 +1,29 @@
+﻿using Bit.Core.AdminConsole.Repositories;
+using Org.BouncyCastle.Bcpg;
+
+namespace Bit.Core.AdminConsole.OrganizationFeatures.Policies.Implementations;
+
+public class PolicyRequirementsQuery
+{
+    private readonly IPolicyRepository _policyRepository;
+    private readonly IEnumerable<IPolicyRequirementDefinition<IPolicyRequirement>> _policyRequirementDefinitions;
+
+    public PolicyRequirementsQuery()
+    {
+        // TODO: deps
+    }
+
+
+    public async Task<T> GetAsync<T>(Guid userId) where T : IPolicyRequirement
+    {
+        var definition = _policyRequirementDefinitions.Single(def => def is IPolicyRequirementDefinition<T>);
+        var policies = await _policyRepository.GetManyByUserIdAsync(userId);
+
+        var enforceablePolicies = policies
+            .Where(p => p.Type == definition.Type)
+            .Where(p => definition.FilterPredicate(p));
+
+        // Why is cast necessary? maybe because it could be a *different* IPolicyRequirement?
+        return (T)definition.Reduce(enforceablePolicies);
+    }
+}
