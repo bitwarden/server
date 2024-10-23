dececee4b935f31706aeb534c48e37e96077ab8c
Rui Tome <rtome@bitwarden.com>
2024-10-23 10:28:19 +0100

    Edit summary message for IRemoveOrganizationUserCommand.UserLeaveAsync

--- src/Core/AdminConsole/OrganizationFeatures/OrganizationUsers/Interfaces/IRemoveOrganizationUserCommand.cs
+++ src/Core/AdminConsole/OrganizationFeatures/OrganizationUsers/Interfaces/IRemoveOrganizationUserCommand.cs
@@ -12,7 +12,7 @@ public interface IRemoveOrganizationUserCommand
     Task<List<Tuple<OrganizationUser, string>>> RemoveUsersAsync(Guid organizationId,
         IEnumerable<Guid> organizationUserIds, Guid? deletingUserId);
     /// <summary>
-    /// User leaves organization.
+    /// Removes a user from an organization when they have left voluntarily. This should only be called by the same user who is being removed.
     /// </summary>
     /// <param name="organizationId">Organization to leave.</param>
     /// <param name="userId">User to leave.</param>
