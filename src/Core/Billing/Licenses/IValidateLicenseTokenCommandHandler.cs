9cac042eefa06cedef30f1b0bde5e8fb8c666bba
Conner Turnbull <cturnbull@bitwarden.com>
2024-10-23 14:10:22 -0400

    wip license

---
+++ src/Core/Billing/Licenses/IValidateLicenseTokenCommandHandler.cs
@@ -0,0 +1,8 @@
+﻿using Bit.Core.Models.Common;
+
+namespace Bit.Core.Billing.Licenses;
+
+public interface IValidateLicenseTokenCommandHandler
+{
+    Result Handle(ValidateLicenseTokenCommand command);
+}
---
+++ src/Core/Billing/Licenses/Implementations/ValidateLicenseTokenCommand.cs
@@ -0,0 +1,21 @@
+﻿#nullable enable
+using Bit.Core.Entities;
+using Bit.Core.Models.Common;
+
+// ReSharper disable once CheckNamespace
+namespace Bit.Core.Billing.Licenses;
+
+public class ValidateLicenseTokenCommand
+{
+    public required ILicense License { get; init; }
+
+    public User? User { get; init; }
+}
+
+public class ValidateLicenseTokenCommandHandler : IValidateLicenseTokenCommandHandler
+{
+    public Result Handle(ValidateLicenseTokenCommand command)
+    {
+        throw new NotImplementedException();
+    }
+}
