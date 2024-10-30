65bc9bb01fb6363a48f349f2e8af39908bd78e49
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-10-30 09:11:22 +0100

    wip

--- src/Admin/AdminConsole/Components/Admins.cs
+++ src/Admin/AdminConsole/Components/Admins.cs
@@ -1,4 +1,5 @@
-﻿using Bit.Admin.AdminConsole.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using Bit.Admin.AdminConsole.Models;
 using Bit.Admin.Enums;
 using Bit.Admin.Services;
 using Microsoft.AspNetCore.Components;
@@ -11,4 +12,29 @@ public partial class Admins(
     private readonly bool _canResendEmailInvite = accessControlService.UserHasPermission(Permission.Provider_ResendEmailInvite);
 
     [Parameter] public ProviderViewModel Model { get; set; }
+
+    [SupplyParameterFromForm(FormName = "ManageAdminsForm")]
+    public ManageAdminsFormModel FormModel { get; set; } = new();
+
+    [Parameter]
+    public Guid ProviderId { get; set; }
+
+    [Parameter]
+    public Guid OwnerId { get; set; }
+
+    [SupplyParameterFromQuery]
+    public Guid? InviteResentTo { get; set; }
+
+    private async Task OnValidSubmitAsync()
+    {
+        await ProviderService.ResendProviderSetupInviteEmailAsync(ProviderId, OwnerId);
+        var uri = NavigationManager.GetUriWithQueryParameters($"/admin/providers/{ProviderId}/admins", new Dictionary<string, object?> { { "inviteResentTo", OwnerId.ToString() } });
+        NavigationManager.NavigateTo(uri);
+    }
+
+    public class ManageAdminsFormModel
+    {
+        [Required]
+        public Guid UserId { get; set; }
+    }
 }
