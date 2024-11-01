eaa40e82393448cb5cacf0239980a247abec3c53
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-11-01 09:35:37 +0100

    cleanup

--- src/Admin/AdminConsole/Components/ViewInformation.razor.cs
+++
@@ -1,9 +0,0 @@
-﻿using Bit.Admin.AdminConsole.Models;
-using Microsoft.AspNetCore.Components;
-
-namespace Bit.Admin.AdminConsole.Components;
-
-public partial class ViewInformation : ComponentBase
-{
-    [Parameter] public ProviderViewModel Model { get; set; }
-}
