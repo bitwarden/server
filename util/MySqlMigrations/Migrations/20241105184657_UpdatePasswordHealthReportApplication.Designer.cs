10c86f3d77aa10ffa1fe73094bd19597b1070e1d
voommen-livefront <voommen@livefront.com>
2024-11-05 12:48:22 -0600

    PM-13236 Update database model changes for Mysql

--- util/MySqlMigrations/Migrations/20241105184657_UpdatePasswordHealthReportApplication.Designer.cs
+++ util/MySqlMigrations/Migrations/20241105184657_UpdatePasswordHealthReportApplication.Designer.cs
@@ -4,6 +4,7 @@ using Bit.Infrastructure.EntityFramework.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bit.MySqlMigrations.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20241105184657_UpdatePasswordHealthReportApplication")]
+    partial class UpdatePasswordHealthReportApplication
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ util/MySqlMigrations/Migrations/20241105184657_UpdatePasswordHealthReportApplication.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.MySqlMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class UpdatePasswordHealthReportApplication : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        // migrationBuilder.DropForeignKey(
+        //     name: "FK_PasswordHealthReportApplications_Organization_OrganizationId",
+        //     table: "PasswordHealthReportApplications");
+
+        // migrationBuilder.DropPrimaryKey(
+        //     name: "PK_PasswordHealthReportApplications",
+        //     table: "PasswordHealthReportApplications");
+
+        // migrationBuilder.RenameTable(
+        //     name: "PasswordHealthReportApplications",
+        //     newName: "PasswordHealthReportApplication");
+
+        // migrationBuilder.RenameIndex(
+        //     name: "IX_PasswordHealthReportApplications_OrganizationId",
+        //     table: "PasswordHealthReportApplication",
+        //     newName: "IX_PasswordHealthReportApplication_OrganizationId");
+
+        // migrationBuilder.AddPrimaryKey(
+        //     name: "PK_PasswordHealthReportApplication",
+        //     table: "PasswordHealthReportApplication",
+        //     column: "Id");
+
+        // migrationBuilder.CreateIndex(
+        //     name: "IX_PasswordHealthReportApplication_Id",
+        //     table: "PasswordHealthReportApplication",
+        //     column: "Id");
+
+        // migrationBuilder.AddForeignKey(
+        //     name: "FK_PasswordHealthReportApplication_Organization_OrganizationId",
+        //     table: "PasswordHealthReportApplication",
+        //     column: "OrganizationId",
+        //     principalTable: "Organization",
+        //     principalColumn: "Id",
+        //     onDelete: ReferentialAction.Cascade);
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        // migrationBuilder.DropForeignKey(
+        //     name: "FK_PasswordHealthReportApplication_Organization_OrganizationId",
+        //     table: "PasswordHealthReportApplication");
+
+        // migrationBuilder.DropPrimaryKey(
+        //     name: "PK_PasswordHealthReportApplication",
+        //     table: "PasswordHealthReportApplication");
+
+        // migrationBuilder.DropIndex(
+        //     name: "IX_PasswordHealthReportApplication_Id",
+        //     table: "PasswordHealthReportApplication");
+
+        // migrationBuilder.RenameTable(
+        //     name: "PasswordHealthReportApplication",
+        //     newName: "PasswordHealthReportApplications");
+
+        // migrationBuilder.RenameIndex(
+        //     name: "IX_PasswordHealthReportApplication_OrganizationId",
+        //     table: "PasswordHealthReportApplications",
+        //     newName: "IX_PasswordHealthReportApplications_OrganizationId");
+
+        // migrationBuilder.AddPrimaryKey(
+        //     name: "PK_PasswordHealthReportApplications",
+        //     table: "PasswordHealthReportApplications",
+        //     column: "Id");
+
+        // migrationBuilder.AddForeignKey(
+        //     name: "FK_PasswordHealthReportApplications_Organization_OrganizationId",
+        //     table: "PasswordHealthReportApplications",
+        //     column: "OrganizationId",
+        //     principalTable: "Organization",
+        //     principalColumn: "Id",
+        //     onDelete: ReferentialAction.Cascade);
+    }
+}
