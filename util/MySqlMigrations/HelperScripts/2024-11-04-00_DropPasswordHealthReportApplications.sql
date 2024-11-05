0402c366a15d486fabe0935214214eb00d0497f9
voommen-livefront <voommen@livefront.com>
2024-11-05 16:00:23 -0600

    PM-13236 removed redundant script files

--- util/MySqlMigrations/HelperScripts/2024-11-04-00_DropPasswordHealthReportApplications.sql
+++
@@ -1,7 +0,0 @@
-START TRANSACTION;
-
--- Drop the PasswordHealthReportApplications (plural) table
--- the correct table is PasswordHealthReportApplication (singular)
-DROP TABLE IF EXISTS PasswordHealthReportApplications;
-
-COMMIT;
