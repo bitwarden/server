428104b9395faf74d7a907a6ed6d34be5f19c3f6
Bernd Schoolmann <mail@quexten.com>
2024-11-05 20:07:45 +0100

    Add ssh-agent flag

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -125,6 +125,7 @@ public static class FeatureFlagKeys
     public const string ProviderClientVaultPrivacyBanner = "ac-2833-provider-client-vault-privacy-banner";
     public const string DeviceTrustLogging = "pm-8285-device-trust-logging";
     public const string SSHKeyItemVaultItem = "ssh-key-vault-item";
+    public const string SSHAgent = "ssh-agent";
     public const string AuthenticatorTwoFactorToken = "authenticator-2fa-token";
     public const string EnableUpgradePasswordManagerSub = "AC-2708-upgrade-password-manager-sub";
     public const string IdpAutoSubmitLogin = "idp-auto-submit-login";
