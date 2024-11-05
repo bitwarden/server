addb438788be13431a5657c2c3683691641452fb
voommen-livefront <voommen@livefront.com>
2024-11-05 13:09:14 -0600

    PM-13236 updated the models to remove commented code

--- util/PostgresMigrations/Migrations/20241105181159_UpdatePasswordHealthReportApplication.cs
+++ util/PostgresMigrations/Migrations/20241105181159_UpdatePasswordHealthReportApplication.cs
@@ -10,77 +10,13 @@ public partial class UpdatePasswordHealthReportApplication : Migration
     /// <inheritdoc />
     protected override void Up(MigrationBuilder migrationBuilder)
     {
-        // migrationBuilder.DropForeignKey(
-        //     name: "FK_PasswordHealthReportApplications_Organization_OrganizationId",
-        //     table: "PasswordHealthReportApplications");
-
-        // migrationBuilder.DropPrimaryKey(
-        //     name: "PK_PasswordHealthReportApplications",
-        //     table: "PasswordHealthReportApplications");
-
-        // migrationBuilder.RenameTable(
-        //     name: "PasswordHealthReportApplications",
-        //     newName: "PasswordHealthReportApplication");
-
-        // migrationBuilder.RenameIndex(
-        //     name: "IX_PasswordHealthReportApplications_OrganizationId",
-        //     table: "PasswordHealthReportApplication",
-        //     newName: "IX_PasswordHealthReportApplication_OrganizationId");
-
-        // migrationBuilder.AddPrimaryKey(
-        //     name: "PK_PasswordHealthReportApplication",
-        //     table: "PasswordHealthReportApplication",
-        //     column: "Id");
-
-        // migrationBuilder.CreateIndex(
-        //     name: "IX_PasswordHealthReportApplication_Id",
-        //     table: "PasswordHealthReportApplication",
-        //     column: "Id");
-
-        // migrationBuilder.AddForeignKey(
-        //     name: "FK_PasswordHealthReportApplication_Organization_OrganizationId",
-        //     table: "PasswordHealthReportApplication",
-        //     column: "OrganizationId",
-        //     principalTable: "Organization",
-        //     principalColumn: "Id",
-        //     onDelete: ReferentialAction.Cascade);
+        // this file is not required, but the designer file is required 
+        // in order to keep the database models in sync with the database
+        // without this - the unit tests will fail when run on your local machine
     }
 
     /// <inheritdoc />
     protected override void Down(MigrationBuilder migrationBuilder)
     {
-        // migrationBuilder.DropForeignKey(
-        //     name: "FK_PasswordHealthReportApplication_Organization_OrganizationId",
-        //     table: "PasswordHealthReportApplication");
-
-        // migrationBuilder.DropPrimaryKey(
-        //     name: "PK_PasswordHealthReportApplication",
-        //     table: "PasswordHealthReportApplication");
-
-        // migrationBuilder.DropIndex(
-        //     name: "IX_PasswordHealthReportApplication_Id",
-        //     table: "PasswordHealthReportApplication");
-
-        // migrationBuilder.RenameTable(
-        //     name: "PasswordHealthReportApplication",
-        //     newName: "PasswordHealthReportApplications");
-
-        // migrationBuilder.RenameIndex(
-        //     name: "IX_PasswordHealthReportApplication_OrganizationId",
-        //     table: "PasswordHealthReportApplications",
-        //     newName: "IX_PasswordHealthReportApplications_OrganizationId");
-
-        // migrationBuilder.AddPrimaryKey(
-        //     name: "PK_PasswordHealthReportApplications",
-        //     table: "PasswordHealthReportApplications",
-        //     column: "Id");
-
-        // migrationBuilder.AddForeignKey(
-        //     name: "FK_PasswordHealthReportApplications_Organization_OrganizationId",
-        //     table: "PasswordHealthReportApplications",
-        //     column: "OrganizationId",
-        //     principalTable: "Organization",
-        //     principalColumn: "Id",
-        //     onDelete: ReferentialAction.Cascade);
     }
 }
