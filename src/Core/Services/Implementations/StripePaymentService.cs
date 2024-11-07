4f2d1670e627d944031b86ebcb8d04583d21e46b
Conner Turnbull <cturnbull@bitwarden.com>
2024-11-07 12:00:12 -0500

    Updated customer metadata to only store one old btCustomerId

--- src/Core/Services/Implementations/StripePaymentService.cs
+++ src/Core/Services/Implementations/StripePaymentService.cs
@@ -1360,8 +1360,7 @@ public class StripePaymentService : IPaymentService
         {
             if (braintreeCustomer?.Id != stripeCustomerMetadata["btCustomerId"])
             {
-                var nowSec = Utilities.CoreHelpers.ToEpocSeconds(DateTime.UtcNow);
-                stripeCustomerMetadata.Add($"btCustomerId_{nowSec}", stripeCustomerMetadata["btCustomerId"]);
+                stripeCustomerMetadata.Add("btCustomerId_old", stripeCustomerMetadata["btCustomerId"]);
             }
             stripeCustomerMetadata["btCustomerId"] = braintreeCustomer?.Id;
         }
