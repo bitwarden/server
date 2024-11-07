090238bd8d308e3755002a0cb7e020bcae7ba79a
voommen-livefront <voommen@livefront.com>
2024-11-07 10:19:28 -0600

    PM-13237 Add entity command and unit tests

---
+++ src/Core/Tools/ReportFeatures/ReportingServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Bit.Core.Tools.ReportFeatures.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bit.Core.Tools.ReportFeatures;
+
+public static class ReportingServiceCollectionExtensions
+{
+    public static void AddReportingServices(this IServiceCollection services)
+    {
+        services.AddScoped<IAddPasswordHealthReportApplicationCommand, AddPasswordHealthReportApplicationCommand>();
+    }
+}
