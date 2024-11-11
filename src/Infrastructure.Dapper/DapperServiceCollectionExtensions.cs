01630f9bf20ea07d632ea902df1d8a6989802bd4
Shane Melton <smelton@bitwarden.com>
2024-11-11 14:48:38 -0800

    [PM-14373] Add Dapper SecurityTask repository

--- src/Infrastructure.Dapper/DapperServiceCollectionExtensions.cs
+++ src/Infrastructure.Dapper/DapperServiceCollectionExtensions.cs
@@ -59,6 +59,7 @@ public static class DapperServiceCollectionExtensions
         services
             .AddSingleton<IClientOrganizationMigrationRecordRepository, ClientOrganizationMigrationRecordRepository>();
         services.AddSingleton<IPasswordHealthReportApplicationRepository, PasswordHealthReportApplicationRepository>();
+        services.AddSingleton<ISecurityTaskRepository, SecurityTaskRepository>();
 
         if (selfHosted)
         {
