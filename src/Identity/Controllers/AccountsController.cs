b8b6d3b0e82bd02877615da1f4231180d2f75ff1
Jake Fink <jfink@bitwarden.com>
2024-11-12 15:23:47 -0500

    add another comment

--- src/Identity/Controllers/AccountsController.cs
+++ src/Identity/Controllers/AccountsController.cs
@@ -89,6 +89,8 @@ public class AccountsController : Controller
         var token = await _sendVerificationEmailForRegistrationCommand.Run(model.Email, model.Name,
             model.ReceiveMarketingEmails);
         // a really cool comment
+        // another really cool comment
+
 
         var refEvent = new ReferenceEvent
         {
