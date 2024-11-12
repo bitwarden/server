2d74b11cc48694e75a90ee50c7e8367542bf3068
Cy Okeke <cokeke@bitwarden.com>
2024-11-12 14:19:49 +0100

    Removed unused OrganizationSponsorshipResponse

--- src/Api/Billing/Models/Responses/OrganizationSponsorshipResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using Bit.Core.AdminConsole.Entities;
-
-namespace Bit.Api.Billing.Models.Responses;
-
-public record OrganizationSponsorshipResponse(bool IsPolicyEnabled)
-{
-    public static OrganizationSponsorshipResponse From(Policy policy)
-        => new(policy.Enabled);
-}
