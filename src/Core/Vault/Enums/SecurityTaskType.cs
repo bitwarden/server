639d09eea6c0264359db75cd456e4eceaa1cfef2
Shane Melton <smelton@bitwarden.com>
2024-11-13 10:10:35 -0800

    Typo
    Co-authored-by: Matt Bishop <mbishop@bitwarden.com>

--- src/Core/Vault/Enums/SecurityTaskType.cs
+++ src/Core/Vault/Enums/SecurityTaskType.cs
@@ -7,6 +7,6 @@ public enum SecurityTaskType : byte
     /// <summary>
     /// Task to update a cipher's password that was found to be at-risk by an administrator
     /// </summary>
-    [Display(Name = "Update at risk credential")]
+    [Display(Name = "Update at-risk credential")]
     UpdateAtRiskCredential = 0
 }
