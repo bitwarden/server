eee7494c916feea5a881140888c0cec1cff4861b
Shane Melton <smelton@bitwarden.com>
2024-11-14 14:54:20 -0800

    [PM-14373] Introduce SecurityTask database table and repository  (#5025)
    * [PM-14373] Introduce SecurityTask entity and related enums
    
    * [PM-14373] Add Dapper SecurityTask repository
    
    * [PM-14373] Introduce MSSQL table, view, and stored procedures
    
    * [PM-14373] Add EF SecurityTask repository and type configurations
    
    * [PM-14373] Add EF Migration
    
    * [PM-14373] Add integration tests
    
    * [PM-14373] Formatting
    
    * Typo
    
    Co-authored-by: Matt Bishop <mbishop@bitwarden.com>
    
    * Typo
    
    Co-authored-by: Matt Bishop <mbishop@bitwarden.com>
    
    * [PM-14373] Remove DeleteById sproc
    
    * [PM-14373] SQL formatting
    
    ---------
    
    Co-authored-by: Matt Bishop <mbishop@bitwarden.com>

---
+++ src/Infrastructure.EntityFramework/Vault/Configurations/SecurityTaskEntityTypeConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Bit.Infrastructure.EntityFramework.Vault.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bit.Infrastructure.EntityFramework.Vault.Configurations;
+
+public class SecurityTaskEntityTypeConfiguration : IEntityTypeConfiguration<SecurityTask>
+{
+    public void Configure(EntityTypeBuilder<SecurityTask> builder)
+    {
+        builder
+            .Property(s => s.Id)
+            .ValueGeneratedNever();
+
+        builder
+            .HasKey(s => s.Id)
+            .IsClustered();
+
+        builder
+            .HasIndex(s => s.OrganizationId)
+            .IsClustered(false);
+
+        builder
+            .HasIndex(s => s.CipherId)
+            .IsClustered(false);
+
+        builder
+            .ToTable(nameof(SecurityTask));
+    }
+}
