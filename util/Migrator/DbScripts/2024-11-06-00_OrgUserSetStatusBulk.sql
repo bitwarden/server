99f98f25ee7738606d3ed2bfe74dd9932f56c5ed
jrmccannon <jmccannon@bitwarden.com>
2024-11-15 09:49:02 -0600

    Updated OrgIds to be a json string

--- util/Migrator/DbScripts/2024-11-06-00_OrgUserSetStatusBulk.sql
+++ util/Migrator/DbScripts/2024-11-06-00_OrgUserSetStatusBulk.sql
@@ -1,15 +1,28 @@
-CREATE OR ALTER PROCEDURE [dbo].[OrganizationUser_SetStatusForUsersById]
-    @OrganizationUserIds AS [dbo].[GuidIdArray] READONLY,
+CREATE PROCEDURE [dbo].[OrganizationUser_SetStatusForUsersById]
+    @OrganizationUserIds AS NVARCHAR(MAX),
     @Status SMALLINT
 AS
 BEGIN
     SET NOCOUNT ON
+
+    -- Declare a table variable to hold the parsed JSON data
+    DECLARE @ParsedIds TABLE (Id UNIQUEIDENTIFIER);
+
+    -- Parse the JSON input into the table variable
+    INSERT INTO @ParsedIds (Id)
+    SELECT value
+    FROM OPENJSON(@OrganizationUserIds);
+
+    -- Check if the input table is empty
+    IF (SELECT COUNT(1) FROM @ParsedIds) < 1
+        BEGIN
+            RETURN(-1);
+        END
+
     UPDATE
         [dbo].[OrganizationUser]
-    SET
-        [Status] = @Status
-    WHERE
-        [Id] IN (SELECT Id from @OrganizationUserIds)
-END
+    SET [Status] = @Status
+    WHERE [Id] IN (SELECT Id from @ParsedIds)
 
-EXEC [dbo].[User_BumpAccountRevisionDateByOrganizationUserIds] @OrganizationUserIds
+    EXEC [dbo].[User_BumpAccountRevisionDateByOrganizationUserIds] @OrganizationUserIds
+END
