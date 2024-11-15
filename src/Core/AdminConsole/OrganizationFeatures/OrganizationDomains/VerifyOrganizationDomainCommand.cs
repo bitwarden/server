eb79696dfbc811f8f0ea855bdb3f506fe20b6bfa
jrmccannon <jmccannon@bitwarden.com>
2024-11-15 14:13:15 -0600

    bringing changes from nc user changes.

--- src/Core/AdminConsole/OrganizationFeatures/OrganizationDomains/VerifyOrganizationDomainCommand.cs
+++ src/Core/AdminConsole/OrganizationFeatures/OrganizationDomains/VerifyOrganizationDomainCommand.cs
@@ -28,8 +28,6 @@ public class VerifyOrganizationDomainCommand(
     ILogger<VerifyOrganizationDomainCommand> logger)
     : IVerifyOrganizationDomainCommand
 {
-
-
     public async Task<OrganizationDomain> UserVerifyOrganizationDomainAsync(OrganizationDomain organizationDomain)
     {
         if (currentContext.UserId is null)
@@ -53,7 +51,7 @@ public class VerifyOrganizationDomainCommand(
         return domainVerificationResult;
     }
 
-    public async Task<OrganizationDomain> SystemVerifyOrganizationDomainAsync(OrganizationDomain organizationDomain) // need request object to hold who is doing the action
+    public async Task<OrganizationDomain> SystemVerifyOrganizationDomainAsync(OrganizationDomain organizationDomain)
     {
         var actingUser = new SystemUser(EventSystemUser.DomainVerification);
 
@@ -128,10 +126,8 @@ public class VerifyOrganizationDomainCommand(
     {
         if (featureService.IsEnabled(FeatureFlagKeys.AccountDeprovisioning))
         {
-            await policyService.SaveAsync(
-                new Policy { OrganizationId = organizationId, Type = PolicyType.SingleOrg, Enabled = true },
-                savingUserId: actingUser is StandardUser standardUser ? standardUser.UserId : null,
-                eventSystemUser: actingUser is SystemUser systemUser ? systemUser.SystemUserType : null);
+            await EnableSingleOrganizationPolicyAsync(organizationId, actingUser);
+            await SendVerifiedDomainUserEmailAsync(organizationId);
         }
     }
 
