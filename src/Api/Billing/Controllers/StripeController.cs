8e79be49e807b6fe7a0ca5192a39372b2887ff3b
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-11-19 13:52:16 +0100

    r

--- src/Api/Billing/Controllers/StripeController.cs
+++ src/Api/Billing/Controllers/StripeController.cs
@@ -1,6 +1,4 @@
-﻿using Bit.Api.Billing.Models.Responses;
-using Bit.Core.Billing.Services;
-using Bit.Core.Services;
+﻿using Bit.Core.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
@@ -10,8 +8,7 @@ namespace Bit.Api.Billing.Controllers;
 
 [Authorize("Application")]
 public class StripeController(
-    IStripeAdapter stripeAdapter,
-    ITaxService taxService) : Controller
+    IStripeAdapter stripeAdapter) : Controller
 {
     [HttpPost]
     [Route("~/setup-intent/bank-account")]
@@ -48,14 +45,5 @@ public class StripeController(
         var setupIntent = await stripeAdapter.SetupIntentCreate(options);
 
         return TypedResults.Ok(setupIntent.ClientSecret);
-    }
-
-    [HttpGet]
-    [AllowAnonymous]
-    [Route("~/tax/id-types")]
-    public IResult GetTaxIdTypes()
-    {
-        var response = TaxIdTypesResponse.From(taxService.GetTaxIdTypes());
-        return TypedResults.Ok(response);
     }
 }
