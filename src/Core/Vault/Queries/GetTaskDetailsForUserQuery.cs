b17ecbdee8933857915959aa26e9d68193c7d083
gbubemismith <gsmithwalter@gmail.com>
2024-11-20 15:59:33 -0500

    Added CQRS pattern

---
+++ src/Core/Vault/Queries/GetTaskDetailsForUserQuery.cs
@@ -0,0 +1,9 @@
+﻿using Bit.Core.Vault.Entities;
+using Bit.Core.Vault.Enums;
+
+namespace Bit.Core.Vault.Queries;
+
+public class GetTaskDetailsForUserQuery : IGetTaskDetailsForUserQuery
+{
+    public async Task<IEnumerable<SecurityTask>> GetTaskDetailsForUserAsync(Guid userId, IEnumerable<SecurityTaskStatus> status = null) => throw new NotImplementedException();
+}
---
+++ src/Core/Vault/Queries/IGetTaskDetailsForUserQuery.cs
@@ -0,0 +1,9 @@
+﻿using Bit.Core.Vault.Entities;
+using Bit.Core.Vault.Enums;
+
+namespace Bit.Core.Vault.Queries;
+
+public interface IGetTaskDetailsForUserQuery
+{
+    Task<IEnumerable<SecurityTask>> GetTaskDetailsForUserAsync(Guid userId, IEnumerable<SecurityTaskStatus> status = null);
+}
