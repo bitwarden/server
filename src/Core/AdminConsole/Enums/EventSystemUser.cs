1b75e35c319f588d9e8a5fe7fd9ccd6591813cf1
Jared McCannon <jmccannon@bitwarden.com>
2024-11-26 16:37:12 -0600

    [PM-10319] - Revoke Non Complaint Users for 2FA and Single Org Policy Enablement (#5037)
    - Revoking users when enabling single org and 2fa policies.
    - Updated emails sent when users are revoked via 2FA or Single Organization policy enablement
    
    Co-authored-by: Matt Bishop <mbishop@bitwarden.com>
    Co-authored-by: Rui Tomé <108268980+r-tome@users.noreply.github.com>

--- src/Core/AdminConsole/Enums/EventSystemUser.cs
+++ src/Core/AdminConsole/Enums/EventSystemUser.cs
@@ -2,6 +2,7 @@
 
 public enum EventSystemUser : byte
 {
+    Unknown = 0,
     SCIM = 1,
     DomainVerification = 2,
     PublicApi = 3,
--- src/Core/Services/IMailService.cs
+++ src/Core/Services/IMailService.cs
@@ -35,6 +35,8 @@ public interface IMailService
     Task SendOrganizationAcceptedEmailAsync(Organization organization, string userIdentifier, IEnumerable<string> adminEmails, bool hasAccessSecretsManager = false);
     Task SendOrganizationConfirmedEmailAsync(string organizationName, string email, bool hasAccessSecretsManager = false);
     Task SendOrganizationUserRemovedForPolicyTwoStepEmailAsync(string organizationName, string email);
+    Task SendOrganizationUserRevokedForTwoFactoryPolicyEmailAsync(string organizationName, string email);
+    Task SendOrganizationUserRevokedForPolicySingleOrgEmailAsync(string organizationName, string email);
     Task SendPasswordlessSignInAsync(string returnUrl, string token, string email);
     Task SendInvoiceUpcoming(
         string email,
