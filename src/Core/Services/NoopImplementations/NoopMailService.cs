f471fffe42a2a5c73448c720f36255354091a1ea
Jared McCannon <jmccannon@bitwarden.com>
2024-12-05 08:59:35 -0600

    [PM-10317] Email Users For Org Claiming Domain (#5094)
    * Revoking users when enabling single org and 2fa policies. Fixing tests.
    
    * Added migration.
    
    * Wrote tests and fixed bugs found.
    
    * Patch build process
    
    * Fixing tests.
    
    * Added unit test around disabling the feature flag.
    
    * Updated error message to be public and added test for validating the request.
    
    * formatting
    
    * Added some tests for single org policy validator.
    
    * Fix issues from merge.
    
    * Added sending emails to revoked non-compliant users.
    
    * Fixing name. Adding two factor policy email.
    
    * Send email when user has been revoked.
    
    * Correcting migration name.
    
    * Fixing templates and logic issue in Revoke command.
    
    * Moving interface into its own file.
    
    * Correcting namespaces for email templates.
    
    * correcting logic that would not allow normal users to revoke non owners.
    
    * Actually correcting the test and logic.
    
    * dotnet format. Added exec to bottom of bulk sproc
    
    * Update src/Core/AdminConsole/OrganizationFeatures/OrganizationUsers/RevokeNonCompliantOrganizationUserCommand.cs
    
    Co-authored-by: Rui Tomé <108268980+r-tome@users.noreply.github.com>
    
    * Updated OrgIds to be a json string
    
    * Fixing errors.
    
    * Updating test
    
    * Moving command result.
    
    * Formatting and request rename
    
    * Realized this would throw a null error from the system domain verification. Adding unknown type to event system user. Adding optional parameter to SaveAsync in policy service in order to pass in event system user.
    
    * Code review changes
    
    * Removing todos
    
    * Corrected test name.
    
    * Syncing filename to record name.
    
    * Fixing up the tests.
    
    * Added happy path test
    
    * Naming corrections. And corrected EF query.
    
    * added check against event service
    
    * Code review changes.
    
    * Fixing tests.
    
    * splitting up tests
    
    * Added templates and email side effect for claiming a domain.
    
    * bringing changes from nc user changes.
    
    * Switched to enqueue mail message.
    
    * Filled in DomainClaimedByOrganization.html.hbs
    
    * Added text document for domain claiming
    
    * Fixing migration script.
    
    * Remove old sproc
    
    * Limiting sending of the email down to users who are a part of the domain being claimed.
    
    * Added test for change
    
    * Renames and fixed up email.
    
    * Fixing up CSS
    
    ---------
    
    Co-authored-by: Matt Bishop <mbishop@bitwarden.com>
    Co-authored-by: Rui Tomé <108268980+r-tome@users.noreply.github.com>
    Co-authored-by: Rui Tome <rtome@bitwarden.com>

--- src/Core/Services/NoopImplementations/NoopMailService.cs
+++ src/Core/Services/NoopImplementations/NoopMailService.cs
@@ -3,6 +3,7 @@ using Bit.Core.AdminConsole.Entities.Provider;
 using Bit.Core.Auth.Entities;
 using Bit.Core.Billing.Enums;
 using Bit.Core.Entities;
+using Bit.Core.Models.Data.Organizations;
 using Bit.Core.Models.Mail;
 
 namespace Bit.Core.Services;
@@ -309,5 +310,6 @@ public class NoopMailService : IMailService
     {
         return Task.FromResult(0);
     }
+    public Task SendClaimedDomainUserEmailAsync(ManagedUserDomainClaimedEmails emailList) => Task.CompletedTask;
 }
 
