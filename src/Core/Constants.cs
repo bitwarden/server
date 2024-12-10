f916fcfc13ee2d2c05fdb28fbc9fc86b7dcfa476
Todd Martin <tmartin@bitwarden.com>
2024-12-10 14:16:52 -0500

    Disable cipher key encryption on self-hosted instances

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -174,7 +174,7 @@ public static class FeatureFlagKeys
         return new Dictionary<string, string>()
         {
             { DuoRedirect, "true" },
-            { CipherKeyEncryption, "true" },
+            { CipherKeyEncryption, "false" },
         };
     }
 }
