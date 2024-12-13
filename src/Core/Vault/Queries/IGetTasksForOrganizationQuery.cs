bf5ceff0b3d0d8f78f4766053f606e9369566a8c
Shane Melton <smelton@bitwarden.com>
2024-12-12 16:09:24 -0800

    [PM-14380] Introduce IGetTasksForOrganizationQuery

---
+++ src/Core/Vault/Queries/IGetTasksForOrganizationQuery.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+using Bit.Core.Vault.Entities;
+using Bit.Core.Vault.Enums;
+
+namespace Bit.Core.Vault.Queries;
+
+public interface IGetTasksForOrganizationQuery
+{
+    /// <summary>
+    /// Retrieves all security tasks for an organization.
+    /// </summary>
+    /// <param name="organizationId">The Id of the organization</param>
+    /// <param name="status">Optional filter for task status. If not provided, returns tasks of all statuses</param>
+    /// <returns>A collection of security tasks</returns>
+    Task<Collection<SecurityTask>> GetTasksAsync(Guid organizationId, SecurityTaskStatus? status = null);
+}
