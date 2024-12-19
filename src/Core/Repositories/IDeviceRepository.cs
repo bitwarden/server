c5d8b4cf5deeeaf7d948cc392a3fa915f4f3c794
Patrick Pimentel <ppimentel@bitwarden.com>
2024-12-19 13:46:19 -0500

    feat(device): Devices with Auth Request
    Added comment to describe thought process for response model being passed through by the repository.

--- src/Core/Repositories/IDeviceRepository.cs
+++ src/Core/Repositories/IDeviceRepository.cs
@@ -11,6 +11,10 @@ public interface IDeviceRepository : IRepository<Device, Guid>
     Task<Device?> GetByIdentifierAsync(string identifier);
     Task<Device?> GetByIdentifierAsync(string identifier, Guid userId);
     Task<ICollection<Device>> GetManyByUserIdAsync(Guid userId);
+    // The response model is passed in here because we lack a good data layer or DTO
+    // solution for creating non entity objects that come straight from a join
+    // of two tables. If we see this happening again maybe we consider a new approach
+    // to how joined data can be transformed coming out of the database and passed up.
     Task<ICollection<DeviceAuthRequestResponseModel>> GetManyByUserIdWithDeviceAuth(Guid userId, int expirationMinutes);
     Task ClearPushTokenAsync(Guid id);
 }
