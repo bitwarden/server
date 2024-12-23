7738ce454ec9af9d677373117f46e559d86a2c6b
gbubemismith <gsmithwalter@gmail.com>
2024-12-23 17:03:29 -0500

    added security authorization handler work temp

--- src/Core/Vault/Commands/Interfaces/ICreateManyTasksCommand.cs
+++ src/Core/Vault/Commands/Interfaces/ICreateManyTasksCommand.cs
@@ -4,5 +4,11 @@ namespace Bit.Core.Vault.Commands.Interfaces;
 
 public interface ICreateManyTasksCommand
 {
+    /// <summary>
+    /// Creates multiple security tasks for an organization.
+    /// </summary>
+    /// <param name="organizationId">The </param>
+    /// <param name="tasks"></param>
+    /// <returns></returns>
     Task CreateAsync(Guid organizationId, IEnumerable<SecurityTaskCreateRequest> tasks);
 }
