ba848ce082e0fa095f9cd261ad30d15b79b5c52a
gbubemismith <gsmithwalter@gmail.com>
2024-12-23 16:03:34 -0500

    created create many tasks command interface and class

---
+++ src/Core/Vault/Commands/CreateManyTasksCommand.cs
@@ -0,0 +1,12 @@
+﻿using Bit.Core.Vault.Commands.Interfaces;
+using Bit.Core.Vault.Models.Api;
+
+namespace Bit.Core.Vault.Commands;
+
+public class CreateManyTasksCommand : ICreateManyTasksCommand
+{
+    public async Task CreateAsync(Guid organizationId, IEnumerable<SecurityTaskCreateRequest> tasks)
+    {
+
+    }
+}
---
+++ src/Core/Vault/Commands/Interfaces/ICreateManyTasksCommand.cs
@@ -0,0 +1,8 @@
+﻿using Bit.Core.Vault.Models.Api;
+
+namespace Bit.Core.Vault.Commands.Interfaces;
+
+public interface ICreateManyTasksCommand
+{
+    Task CreateAsync(Guid organizationId, IEnumerable<SecurityTaskCreateRequest> tasks);
+}
