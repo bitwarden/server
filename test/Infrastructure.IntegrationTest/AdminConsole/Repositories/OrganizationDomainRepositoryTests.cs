26ef8f856d891017ad9a41274fe89753750cadd4
Jimmy Vo <huynhmaivo82@gmail.com>
2024-12-23 11:18:19 -0500

    Add tests for CreationDate.

--- test/Infrastructure.IntegrationTest/AdminConsole/Repositories/OrganizationDomainRepositoryTests.cs
+++ test/Infrastructure.IntegrationTest/AdminConsole/Repositories/OrganizationDomainRepositoryTests.cs
@@ -68,6 +68,49 @@ public class OrganizationDomainRepositoryTests
         Assert.NotNull(expectedDomain2);
     }
 
+    [DatabaseTheory, DatabaseData]
+    public async Task GetExpiredOrganizationDomainsAsync_ShouldReturnNotReturnDomainsPast72Hours(
+     IUserRepository userRepository,
+     IOrganizationRepository organizationRepository,
+     IOrganizationDomainRepository organizationDomainRepository)
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+
+        var user = await userRepository.CreateAsync(new User
+        {
+            Name = "Test User",
+            Email = $"test+{id}@example.com",
+            ApiKey = "TEST",
+            SecurityStamp = "stamp",
+        });
+
+        var organization = await organizationRepository.CreateAsync(new Organization
+        {
+            Name = $"Test Org {id}",
+            BillingEmail = user.Email,
+            Plan = "Test",
+            PrivateKey = "privatekey",
+
+        });
+
+        var pastDateForValidation = DateTime.UtcNow.AddDays(-6).Date;
+        var organizationDomain = new OrganizationDomain
+        {
+            OrganizationId = organization.Id,
+            DomainName = $"domain{id}@example.com",
+            Txt = "btw+12345",
+            CreationDate = pastDateForValidation
+        };
+        await organizationDomainRepository.CreateAsync(organizationDomain);
+
+        // Act
+        var domains = await organizationDomainRepository.GetExpiredOrganizationDomainsAsync();
+
+        var expectedDomain2 = domains.FirstOrDefault(domain => domain.DomainName == organizationDomain.DomainName);
+        Assert.Null(expectedDomain2);
+    }
+
 
     [DatabaseTheory, DatabaseData]
     public async Task GetExpiredOrganizationDomainsAsync_ShouldNotReturnVerifiedDomains(
