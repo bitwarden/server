653698832cbab4fc1d0f8baf9a766e263aa4f702
Rui Tome <rtome@bitwarden.com>
2024-12-23 15:51:49 +0000

    Rename error message default resource

--- src/Core/Resources/ErrorMessages.Designer.cs
+++ src/Core/Resources/ErrorMessages.Designer.cs
@@ -14,21 +14,21 @@ namespace Bit.Core.Resources {
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class ErrorMessages_en {
+    internal class ErrorMessages {
         
         private static System.Resources.ResourceManager resourceMan;
         
         private static System.Globalization.CultureInfo resourceCulture;
         
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal ErrorMessages_en() {
+        internal ErrorMessages() {
         }
         
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.Equals(null, resourceMan)) {
-                    System.Resources.ResourceManager temp = new System.Resources.ResourceManager("Bit.Core.Resources.ErrorMessages_en", typeof(ErrorMessages_en).Assembly);
+                    System.Resources.ResourceManager temp = new System.Resources.ResourceManager("Bit.Core.Resources.ErrorMessages", typeof(ErrorMessages).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- src/Core/Resources/ErrorMessages.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Bit.Core.Resources;
-
-public class ErrorMessages
-{
-}
