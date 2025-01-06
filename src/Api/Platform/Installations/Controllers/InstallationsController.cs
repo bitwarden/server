cd7c4bf6ce080cfe05ab89c2e6e14721065c2728
Addison Beck <addison@bitwarden.com>
2025-01-06 12:10:53 -0500

    chore: move `Installation` and `Push` to platform's domain folders (#5085)
    * chore: set up a `CODEOWNERS` space for platform
    
    * chore: move sql objects for `Installation` to platform's domain
    
    * chore: move `Installation` and `PushRelay` code to platform's domain

---
+++ src/Api/Platform/Installations/Controllers/InstallationsController.cs
@@ -0,0 +1,42 @@
+﻿using Bit.Core.Exceptions;
+using Bit.Core.Platform.Installations;
+using Bit.Core.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bit.Api.Platform.Installations;
+
+[Route("installations")]
+[SelfHosted(NotSelfHostedOnly = true)]
+public class InstallationsController : Controller
+{
+    private readonly IInstallationRepository _installationRepository;
+
+    public InstallationsController(
+        IInstallationRepository installationRepository)
+    {
+        _installationRepository = installationRepository;
+    }
+
+    [HttpGet("{id}")]
+    [AllowAnonymous]
+    public async Task<InstallationResponseModel> Get(Guid id)
+    {
+        var installation = await _installationRepository.GetByIdAsync(id);
+        if (installation == null)
+        {
+            throw new NotFoundException();
+        }
+
+        return new InstallationResponseModel(installation, false);
+    }
+
+    [HttpPost("")]
+    [AllowAnonymous]
+    public async Task<InstallationResponseModel> Post([FromBody] InstallationRequestModel model)
+    {
+        var installation = model.ToInstallation();
+        await _installationRepository.CreateAsync(installation);
+        return new InstallationResponseModel(installation, true);
+    }
+}
---
+++ src/Infrastructure.Dapper/Platform/Installations/Repositories/InstallationRepository.cs
@@ -0,0 +1,26 @@
+﻿using Bit.Core.Platform.Installations;
+using Bit.Core.Settings;
+using Bit.Infrastructure.Dapper.Repositories;
+
+#nullable enable
+
+namespace Bit.Infrastructure.Dapper.Platform;
+
+/// <summary>
+/// The CRUD repository for communicating with `dbo.Installation`.
+/// </summary>
+/// <remarks>
+/// If referencing: you probably want the interface `IInstallationRepository`
+/// instead of directly calling this class.
+/// </remarks>
+/// <seealso cref="IInstallationRepository"/>
+public class InstallationRepository : Repository<Installation, Guid>, IInstallationRepository
+{
+    public InstallationRepository(GlobalSettings globalSettings)
+        : this(globalSettings.SqlServer.ConnectionString, globalSettings.SqlServer.ReadOnlyConnectionString)
+    { }
+
+    public InstallationRepository(string connectionString, string readOnlyConnectionString)
+        : base(connectionString, readOnlyConnectionString)
+    { }
+}
