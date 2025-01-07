371556a23396a7cb687701f7496fb9bf55e9fb07
Addison Beck <github@addisonbeck.com>
2025-01-07 13:09:43 -0500

    chore: expand tests of the new `UpdateInstallationCommand`

--- test/Core.Test/Platform/Installations/Commands/UpdateInstallationCommandTests.cs
+++ test/Core.Test/Platform/Installations/Commands/UpdateInstallationCommandTests.cs
@@ -11,6 +11,49 @@ public class UpdateInstallationCommandTests
 {
     [Theory]
     [BitAutoData]
+    public async Task UpdateLastActivityDateAsync_WithDefaultGuid_ThrowsException(SutProvider<UpdateInstallationCommand> sutProvider)
+    {
+        // Arrange
+        var defaultGuid = default(Guid);
+
+        // Act & Assert
+        var exception = await Assert.ThrowsAsync<Exception>(
+            () => sutProvider.Sut.UpdateLastActivityDateAsync(defaultGuid));
+
+        Assert.Contains("invalid installation id", exception.Message);
+
+        await sutProvider
+            .GetDependency<IInstallationRepository>()
+            .DidNotReceive()
+            .UpsertAsync(Arg.Any<Installation>());
+    }
+
+    [Theory]
+    [BitAutoData]
+    public async Task UpdateLastActivityDateAsync_WithNonExistentInstallation_ThrowsException(
+        Guid installationId,
+        SutProvider<UpdateInstallationCommand> sutProvider)
+    {
+        // Arrange
+        sutProvider
+            .GetDependency<IGetInstallationQuery>()
+            .GetByIdAsync(installationId)
+            .Returns((Installation)null);
+
+        // Act & Assert
+        var exception = await Assert.ThrowsAsync<Exception>(
+            () => sutProvider.Sut.UpdateLastActivityDateAsync(installationId));
+
+        Assert.Contains("no installation was found", exception.Message);
+
+        await sutProvider
+            .GetDependency<IInstallationRepository>()
+            .DidNotReceive()
+            .UpsertAsync(Arg.Any<Installation>());
+    }
+
+    [Theory]
+    [BitAutoData]
     public async Task UpdateLastActivityDateAsync_ShouldUpdateLastActivityDate(
         Installation installation
     )
