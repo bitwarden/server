2cadb7ef1e7dae87566910d913fb63322b34aac7
Todd Martin <tmartin@bitwarden.com>
2025-01-10 18:14:45 -0500

    More query optimizations to eliminate returning old requests for a device

--- src/Sql/Auth/dbo/Stored Procedures/Device_ReadActiveWithPendingAuthRequestsByUserId.sql
+++ src/Sql/Auth/dbo/Stored Procedures/Device_ReadActiveWithPendingAuthRequestsByUserId.sql
@@ -15,13 +15,14 @@ BEGIN
             Id,
             CreationDate,
             RequestDeviceIdentifier,
+            Approved,
             ROW_NUMBER() OVER (PARTITION BY RequestDeviceIdentifier ORDER BY CreationDate DESC) as rn
         FROM dbo.AuthRequestView
         WHERE Type IN (0, 1)  -- AuthenticateAndUnlock and Unlock types only
             AND CreationDate >= DATEADD(MINUTE, @ExpirationMinutes, GETUTCDATE()) -- Ensure the request hasn't expired
-            AND Approved IS NULL  -- Pending requests only
             AND UserId = @UserId --  Requests for this user only
-    ) AR ON D.Identifier = AR.RequestDeviceIdentifier AND AR.rn = 1  -- Get only the most recent request per device
+    ) AR -- This join will get the most recent request per device, regardless of approval status 
+    ON D.Identifier = AR.RequestDeviceIdentifier AND AR.rn = 1 AND AR.Approved = 0  -- Get only the most recent unapproved request per device
     WHERE
         D.UserId = @UserId -- Include only devices for this user
       AND D.Active = 1; -- Include only active devices
