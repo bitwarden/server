6e6d2f5aa3dc9176764a7040db0ccd9e36fc24a5
Todd Martin <tmartin@bitwarden.com>
2025-01-10 18:39:27 -0500

    Added negation of @ExpirationMinutes

--- util/Migrator/DbScripts/2025-01-10_00_ReadActiveWithPendingAuthRequestsByUserId.sql
+++ util/Migrator/DbScripts/2025-01-10_00_ReadActiveWithPendingAuthRequestsByUserId.sql
@@ -19,7 +19,7 @@ BEGIN
             ROW_NUMBER() OVER (PARTITION BY RequestDeviceIdentifier ORDER BY CreationDate DESC) as rn
         FROM dbo.AuthRequestView
         WHERE Type IN (0, 1)  -- AuthenticateAndUnlock and Unlock types only
-            AND CreationDate >= DATEADD(MINUTE, @ExpirationMinutes, GETUTCDATE()) -- Ensure the request hasn't expired
+            AND CreationDate >= DATEADD(MINUTE, -@ExpirationMinutes, GETUTCDATE()) -- Ensure the request hasn't expired
             AND UserId = @UserId --  Requests for this user only
     ) AR -- This join will get the most recent request per device, regardless of approval status 
     ON D.Identifier = AR.RequestDeviceIdentifier AND AR.rn = 1 AND AR.Approved IS NULL  -- Get only the most recent unapproved request per device
