a6b4c0d595d0065d23226f2900b01642658022ea
Rui Tome <rtome@bitwarden.com>
2025-01-16 14:52:36 +0000

    Create organization deletion command with logic extracted from OrganizationService

---
+++ src/Core/AdminConsole/OrganizationFeatures/Organizations/Interfaces/IOrganizationDeleteCommand.cs
@@ -0,0 +1,8 @@
+﻿using Bit.Core.AdminConsole.Entities;
+
+namespace Bit.Core.AdminConsole.OrganizationFeatures.Organizations.Interfaces;
+
+public interface IOrganizationDeleteCommand
+{
+    Task DeleteAsync(Organization organization);
+}
