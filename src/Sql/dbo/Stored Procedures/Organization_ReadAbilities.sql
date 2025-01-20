1fcb29d6b6370c1c5ef407cbe17ce09d5a5a5ae4
Jimmy Vo <huynhmaivo82@gmail.com>
2025-01-20 16:26:28 -0500

    [pm-17136] Fix missing UseRiskInsights

--- src/Sql/dbo/Stored Procedures/Organization_ReadAbilities.sql
+++ src/Sql/dbo/Stored Procedures/Organization_ReadAbilities.sql
@@ -24,6 +24,7 @@ BEGIN
         [LimitCollectionCreation],
         [LimitCollectionDeletion],
         [AllowAdminAccessToAllCollectionItems],
+        [UseRiskInsights],
         [LimitItemDeletion]
     FROM
         [dbo].[Organization]
