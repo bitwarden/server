c04decab445308dee02eb81b9f6fab6e0b0f11b5
Bernd Schoolmann <mail@quexten.com>
2025-01-20 13:34:52 +0100

    Deny key update only when replacing existing keys

--- src/Core/Auth/Models/Api/Request/Accounts/KeysRequestModel.cs
+++ src/Core/Auth/Models/Api/Request/Accounts/KeysRequestModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Bit.Core.Entities;
+using Bit.Core.Utilities;
 
 namespace Bit.Core.Auth.Models.Api.Request.Accounts;
 
@@ -19,7 +20,21 @@ public class KeysRequestModel
 
         if (!string.IsNullOrWhiteSpace(PublicKey) || !string.IsNullOrWhiteSpace(EncryptedPrivateKey))
         {
-            throw new InvalidOperationException("Updated public/private key(s) were included but the user already has keys.");
+            if (string.IsNullOrWhiteSpace(existingUser.PublicKey))
+            {
+                throw new InvalidOperationException("Must set both public and private key(s) at the same time, received only private key.");
+            }
+            else if (string.IsNullOrWhiteSpace(existingUser.PrivateKey))
+            {
+                throw new InvalidOperationException("Must set both public and private key(s) at the same time, received only public key.");
+            }
+            else
+            {
+                if (!(PublicKey == existingUser.PublicKey && CoreHelpers.FixedTimeEquals(EncryptedPrivateKey, existingUser.PrivateKey)))
+                {
+                    throw new InvalidOperationException("Cannot replace existing key(s) with new key(s).");
+                }
+            }
         }
 
         return existingUser;
