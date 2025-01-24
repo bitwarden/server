84ad415bd3a79406971e739c8a65c08a218ba138
Rui Tome <rtome@bitwarden.com>
2025-01-24 13:02:13 +0000

    Add xmldoc to IOrganizationEnableCommand

--- src/Core/AdminConsole/OrganizationFeatures/Organizations/Interfaces/IOrganizationEnableCommand.cs
+++ src/Core/AdminConsole/OrganizationFeatures/Organizations/Interfaces/IOrganizationEnableCommand.cs
@@ -2,6 +2,16 @@
 
 public interface IOrganizationEnableCommand
 {
+    /// <summary>
+    /// Enables an organization if it is currently disabled.
+    /// </summary>
+    /// <param name="organizationId">The unique identifier of the organization to enable.</param>
     Task EnableAsync(Guid organizationId);
+
+    /// <summary>
+    /// Enables an organization with a specified expiration date if it is currently disabled and has a gateway configured.
+    /// </summary>
+    /// <param name="organizationId">The unique identifier of the organization to enable.</param>
+    /// <param name="expirationDate">The optional expiration date when the organization's subscription will expire.</param>
     Task EnableAsync(Guid organizationId, DateTime? expirationDate);
 }
