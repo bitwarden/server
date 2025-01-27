9e718d733655f41514f0a026e7137f67fcedcdbf
Rui Tomé <108268980+r-tome@users.noreply.github.com>
2025-01-27 10:59:46 +0000

    [PM-15637] Add Email Notification Templates and Logic for Device Approval Requests (#5270)
    * Add device approval notification email templates
    
    * Add DeviceApprovalRequestedViewModel for device approval notifications
    
    * Add method to send device approval requested notification email
    
    * Send email notification to Organization Admins when adding a new admin approval auth request
    
    * Add tests for device approval notification email sending in AuthRequestServiceTests
    
    * fix(email-templates): Remove unnecessary triple braces from user name variable in device approval notification emails
    
    * Add feature flag for admin notifications on device approval requests
    
    * Add logging for skipped admin notifications on device approval requests

---
+++ src/Core/AdminConsole/Models/Mail/DeviceApprovalRequestedViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Bit.Core.Models.Mail;
+
+namespace Bit.Core.AdminConsole.Models.Mail;
+
+public class DeviceApprovalRequestedViewModel : BaseMailModel
+{
+    public Guid OrganizationId { get; set; }
+    public string UserNameRequestingAccess { get; set; }
+
+    public string Url => string.Format("{0}/organizations/{1}/settings/device-approvals",
+        WebVaultUrl,
+        OrganizationId);
+}
+
