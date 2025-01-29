6f5df04a2c95cdf85419c2db61f56ce5bf24610c
Thomas Rittson <trittson@bitwarden.com>
2025-01-29 11:43:14 +1000

    Wrap dict in separate class

--- src/Core/AdminConsole/OrganizationFeatures/Policies/Implementations/PolicyRequirementQuery.cs
+++ src/Core/AdminConsole/OrganizationFeatures/Policies/Implementations/PolicyRequirementQuery.cs
@@ -9,58 +9,53 @@ namespace Bit.Core.AdminConsole.OrganizationFeatures.Policies.Implementations;
 public class PolicyRequirementQuery : IPolicyRequirementQuery
 {
     private readonly IPolicyRepository _policyRepository;
-
-    /// <summary>
-    /// A dictionary associating a Policy Requirement's type with its factory function.
-    /// </summary>
-    private readonly IReadOnlyDictionary<Type, CreateRequirement<IPolicyRequirement>> _policyRequirements;
+    private readonly PolicyRequirementRegistry _policyRequirements = new();
 
     public PolicyRequirementQuery(IGlobalSettings globalSettings, IPolicyRepository policyRepository)
     {
         _policyRepository = policyRepository;
-        var policyRequirements = new Dictionary<Type, CreateRequirement<IPolicyRequirement>>();
 
         // Register Policy Requirement factory functions below
-        policyRequirements.AddRequirement(SendPolicyRequirement.Create);
-        policyRequirements.AddRequirement(up
+        _policyRequirements.Add(SendPolicyRequirement.Create);
+        _policyRequirements.Add(up
             => SsoPolicyRequirement.Create(up, globalSettings.Sso));
-
-        _policyRequirements = policyRequirements.AsReadOnly();
     }
 
     public async Task<T> GetAsync<T>(Guid userId) where T : IPolicyRequirement
-        => _policyRequirements.GetRequirement<T>()(await GetPolicyDetails(userId));
+        => _policyRequirements.Get<T>()(await GetPolicyDetails(userId));
 
     private Task<IEnumerable<OrganizationUserPolicyDetails>> GetPolicyDetails(Guid userId) =>
         _policyRepository.GetPolicyDetailsByUserId(userId);
-}
 
-/// <summary>
-/// Extension methods used to add and retrieve IPolicyRequirements in the dictionary by their specific type.
-/// </summary>
-internal static class PolicyRequirementDictionaryExtensions
-{
-    public static void AddRequirement<T>(
-        this IDictionary<Type, CreateRequirement<IPolicyRequirement>> registry,
-        CreateRequirement<T> factory) where T : IPolicyRequirement
+    /// <summary>
+    /// Helper class used to register and retrieve Policy Requirement factories by type.
+    /// </summary>
+    private class PolicyRequirementRegistry
     {
-        // Explicitly convert T to an IPolicyRequirement (C# doesn't do this automatically).
-        IPolicyRequirement Converted(IEnumerable<OrganizationUserPolicyDetails> up) => factory(up);
-        registry.Add(typeof(T), Converted);
-    }
+        /// <summary>
+        /// A dictionary associating a Policy Requirement's type with its factory function.
+        /// </summary>
+        private readonly Dictionary<Type, CreateRequirement<IPolicyRequirement>> _registry = new();
 
-    public static CreateRequirement<T> GetRequirement<T>(
-        this IReadOnlyDictionary<Type,
-        CreateRequirement<IPolicyRequirement>> registry) where T : IPolicyRequirement
-    {
-        if (!registry.TryGetValue(typeof(T), out var factory))
+        public void Add<T>(CreateRequirement<T> factory) where T : IPolicyRequirement
         {
-            throw new NotImplementedException("No Policy Requirement found for " + typeof(T));
+            // Explicitly convert T to an IPolicyRequirement (C# doesn't do this automatically).
+            IPolicyRequirement Converted(IEnumerable<OrganizationUserPolicyDetails> up) => factory(up);
+            _registry.Add(typeof(T), Converted);
         }
 
-        // Explicitly convert IPolicyRequirement back to T (C# doesn't do this automatically).
-        // The cast here relies on the Register method correctly associating the type and factory function.
-        T Converted(IEnumerable<OrganizationUserPolicyDetails> up) => (T)factory(up);
-        return Converted;
+        public CreateRequirement<T> Get<T>() where T : IPolicyRequirement
+        {
+            if (!_registry.TryGetValue(typeof(T), out var factory))
+            {
+                throw new NotImplementedException("No Policy Requirement found for " + typeof(T));
+            }
+
+            // Explicitly convert IPolicyRequirement back to T (C# doesn't do this automatically).
+            // The cast here relies on the Register method correctly associating the type and factory function.
+            T Converted(IEnumerable<OrganizationUserPolicyDetails> up) => (T)factory(up);
+            return Converted;
+        }
     }
 }
+
