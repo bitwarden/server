9dcb7891eb815e9f6144f9a65ab4f90138dd61f6
voommen-livefront <voommen@livefront.com>
2025-02-03 14:47:35 -0600

    PM-6939 added webhook to read out of Onyx

---
+++ src/Billing/Models/FreshdeskViewTicketModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Bit.Billing.Models;
+
+public class FreshdeskViewTicketModel
+{
+    public bool? Spam { get; set; }
+    public int? Priority { get; set; }
+    public int? Source { get; set; }
+    public int? Status { get; set; }
+    public string Subject { get; set; }
+    public string SupportEmail { get; set; }
+    public int Id { get; set; }
+    public string Description { get; set; }
+    public string DescriptionText { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public List<string> Tags { get; set; }
+}
---
+++ src/Billing/Models/OnyxAnswerWithCitationResponseModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Bit.Billing.Models;
+
+public class OnyxAnswerWithCitationResponseModel
+{
+    public string Answer { get; set; }
+    public string Rephrase { get; set; }
+    public List<Citation> Citations { get; set; }
+    public List<int> LlmSelectedDocIndices { get; set; }
+    public string ErrorMsg { get; set; }
+}
+
+public class Citation
+{
+    public int CitationNum { get; set; }
+    public string DocumentId { get; set; }
+}
