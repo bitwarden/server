d0d8047db451e789fadef2ee6f42589a9bce708b
jrmccannon <jmccannon@bitwarden.com>
2025-02-06 11:59:51 -0600

    Error messages

---
+++ src/Core/AdminConsole/OrganizationFeatures/OrganizationUsers/Validation/InviteUserValidationErrorMessages.cs
@@ -0,0 +1,13 @@
+﻿namespace Bit.Core.AdminConsole.OrganizationFeatures.OrganizationUsers.Validation;
+
+public static class InviteUserValidationErrorMessages
+{
+    //
+    public const string CannotAutoScaleOnSelfHostedError = "Cannot autoscale on self-hosted instance.";
+    public const string SeatLimitHasBeenReachedError = "Seat limit has been reached.";
+    public const string ProviderBillableSeatLimitError = "Seat limit has been reached. Please contact your provider to add more seats.";
+    public const string ProviderResellerSeatLimitError = "Seat limit has been reached. Contact your provider to purchase additional seats.";
+    public const string CancelledSubscriptionError = "Cannot autoscale with a canceled subscription.";
+    public const string NoPaymentMethodFoundError = "No payment method found.";
+    public const string NoSubscriptionFoundError = "No subscription found.";
+}
