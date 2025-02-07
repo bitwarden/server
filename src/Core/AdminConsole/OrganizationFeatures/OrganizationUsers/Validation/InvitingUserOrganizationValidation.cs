c93ac87daafa2b168a5b80fe2055ada33e7edfcb
jrmccannon <jmccannon@bitwarden.com>
2025-02-07 15:34:57 -0600

    part of removing payment validation

--- src/Core/AdminConsole/OrganizationFeatures/OrganizationUsers/Validation/InvitingUserOrganizationValidation.cs
+++ src/Core/AdminConsole/OrganizationFeatures/OrganizationUsers/Validation/InvitingUserOrganizationValidation.cs
@@ -1,19 +1,11 @@
 ﻿using Bit.Core.AdminConsole.Models.Business;
-using Bit.Core.Billing.Enums;
-using Bit.Core.Services;
 
 namespace Bit.Core.AdminConsole.OrganizationFeatures.OrganizationUsers.Validation;
 
 public static class InvitingUserOrganizationValidation
 {
-    public static async Task<ValidationResult<OrganizationDto>> Validate(OrganizationValidationDto organizationDto)
+    public static ValidationResult<OrganizationDto> Validate(OrganizationDto organization)
     {
-        var (organization, paymentService) = (organizationDto.Organization, organizationDto.PaymentService);
-
-        if (organization.Plan is { ProductTier: ProductTierType.Free })
-        {
-            return new Valid<OrganizationDto>(organization);
-        }
 
         if (string.IsNullOrWhiteSpace(organization.GatewayCustomerId))
         {
@@ -25,20 +17,6 @@ public static class InvitingUserOrganizationValidation
             return new Invalid<OrganizationDto>(InviteUserValidationErrorMessages.NoSubscriptionFoundError);
         }
 
-        var paymentSubscription = await paymentService.GetSubscriptionAsync(organization);
-
-        if (InviteUserPaymentValidation.Validate(PaymentSubscriptionDto.FromSubscriptionInfo(paymentSubscription)) is
-            Invalid<PaymentSubscriptionDto> invalidPaymentValidation)
-        {
-            return new Invalid<OrganizationDto>(invalidPaymentValidation.ErrorMessageString);
-        }
-
         return new Valid<OrganizationDto>(organization);
     }
-}
-
-public class OrganizationValidationDto
-{
-    public OrganizationDto Organization { get; init; }
-    public IPaymentService PaymentService { get; init; }
 }
