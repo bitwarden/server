679286f6aab650d1b8e20bbabca6c480b484e101
gbubemismith <gsmithwalter@gmail.com>
2025-02-07 15:58:55 -0500

    Formatted code

--- util/SqliteMigrations/Migrations/20250207204735_AddOptionalNotificationTaskId.cs
+++ util/SqliteMigrations/Migrations/20250207204735_AddOptionalNotificationTaskId.cs
@@ -1,49 +1,47 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Bit.SqliteMigrations.Migrations
+namespace Bit.SqliteMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class AddOptionalNotifificationTaskId : Migration
 {
     /// <inheritdoc />
-    public partial class AddOptionalNotifificationTaskId : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<Guid>(
-                name: "TaskId",
-                table: "Notification",
-                type: "TEXT",
-                nullable: true);
+        migrationBuilder.AddColumn<Guid>(
+            name: "TaskId",
+            table: "Notification",
+            type: "TEXT",
+            nullable: true);
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Notification_TaskId",
-                table: "Notification",
-                column: "TaskId");
+        migrationBuilder.CreateIndex(
+            name: "IX_Notification_TaskId",
+            table: "Notification",
+            column: "TaskId");
 
-            migrationBuilder.AddForeignKey(
-                name: "FK_Notification_SecurityTask_TaskId",
-                table: "Notification",
-                column: "TaskId",
-                principalTable: "SecurityTask",
-                principalColumn: "Id");
-        }
+        migrationBuilder.AddForeignKey(
+            name: "FK_Notification_SecurityTask_TaskId",
+            table: "Notification",
+            column: "TaskId",
+            principalTable: "SecurityTask",
+            principalColumn: "Id");
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropForeignKey(
-                name: "FK_Notification_SecurityTask_TaskId",
-                table: "Notification");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropForeignKey(
+            name: "FK_Notification_SecurityTask_TaskId",
+            table: "Notification");
 
-            migrationBuilder.DropIndex(
-                name: "IX_Notification_TaskId",
-                table: "Notification");
+        migrationBuilder.DropIndex(
+            name: "IX_Notification_TaskId",
+            table: "Notification");
 
-            migrationBuilder.DropColumn(
-                name: "TaskId",
-                table: "Notification");
-        }
+        migrationBuilder.DropColumn(
+            name: "TaskId",
+            table: "Notification");
     }
 }
