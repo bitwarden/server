02262476d6b13915b5c2313353d048702e4a84a5
Brant DeBow <125889545+brant-livefront@users.noreply.github.com>
2025-02-11 10:20:06 -0500

    [PM-17562] Add Azure Service Bus for Distributed Events (#5382)
    * [PM-17562] Add Azure Service Bus for Distributed Events
    
    * Fix failing test
    
    * Addressed issues mentioned in SonarQube
    
    * Respond to PR feedback
    
    * Respond to PR feedback - make webhook opt-in, remove message body from log

---
+++ src/Core/AdminConsole/Services/Implementations/AzureTableStorageEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using Bit.Core.Models.Data;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bit.Core.Services;
+
+public class AzureTableStorageEventHandler(
+    [FromKeyedServices("persistent")] IEventWriteService eventWriteService)
+    : IEventMessageHandler
+{
+    public Task HandleEventAsync(EventMessage eventMessage)
+    {
+        return eventWriteService.CreateManyAsync(EventTableEntity.IndexEvent(eventMessage));
+    }
+}
