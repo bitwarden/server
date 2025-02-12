ae9bb427a16ff2ff5d1ba3b62101ad7e55bec02e
Maciej Zieniuk <167752252+mzieniukbw@users.noreply.github.com>
2025-02-12 16:46:30 +0100

    [PM-10600] Push notification creation to affected clients (#4923)
    * PM-10600: Notification push notification
    
    * PM-10600: Sending to specific client types for relay push notifications
    
    * PM-10600: Sending to specific client types for other clients
    
    * PM-10600: Send push notification on notification creation
    
    * PM-10600: Explicit group names
    
    * PM-10600: Id typos
    
    * PM-10600: Revert global push notifications
    
    * PM-10600: Added DeviceType claim
    
    * PM-10600: Sent to organization typo
    
    * PM-10600: UT coverage
    
    * PM-10600: Small refactor, UTs coverage
    
    * PM-10600: UTs coverage
    
    * PM-10600: Startup fix
    
    * PM-10600: Test fix
    
    * PM-10600: Required attribute, organization group for push notification fix
    
    * PM-10600: UT coverage
    
    * PM-10600: Fix Mobile devices not registering to organization push notifications
    
    We only register devices for organization push notifications when the organization is being created. This does not work, since we have a use case (Notification Center) of delivering notifications to all users of organization. This fixes it, by adding the organization id tag when device registers for push notifications.
    
    * PM-10600: Unit Test coverage for NotificationHubPushRegistrationService
    
    Fixed IFeatureService substitute mocking for Android tests.
    Added user part of organization test with organizationId tags expectation.
    
    * PM-10600: Unit Tests fix to NotificationHubPushRegistrationService after merge conflict
    
    * PM-10600: Organization push notifications not sending to mobile device from self-hosted.
    
    Self-hosted instance uses relay to register the mobile device against Bitwarden Cloud Api. Only the self-hosted server knows client's organization membership, which means it needs to pass in the organization id's information to the relay. Similarly, for Bitwarden Cloud, the organizaton id will come directly from the server.
    
    * PM-10600: Fix self-hosted organization notification not being received by mobile device.
    
    When mobile device registers on self-hosted through the relay, every single id, like user id, device id and now organization id needs to be prefixed with the installation id. This have been missing in the PushController that handles this for organization id.
    
    * PM-10600: Broken NotificationsController integration test
    
    Device type is now part of JWT access token, so the notification center results in the integration test are now scoped to client type web and all.
    
    * PM-10600: Merge conflicts fix
    
    * merge conflict fix

--- src/Core/Platform/Push/Services/RelayPushNotificationService.cs
+++ src/Core/Platform/Push/Services/RelayPushNotificationService.cs
@@ -5,6 +5,7 @@ using Bit.Core.Enums;
 using Bit.Core.IdentityServer;
 using Bit.Core.Models;
 using Bit.Core.Models.Api;
+using Bit.Core.NotificationCenter.Entities;
 using Bit.Core.Repositories;
 using Bit.Core.Services;
 using Bit.Core.Settings;
@@ -138,11 +139,7 @@ public class RelayPushNotificationService : BaseIdentityClientService, IPushNoti
 
     private async Task PushUserAsync(Guid userId, PushType type, bool excludeCurrentContext = false)
     {
-        var message = new UserPushNotification
-        {
-            UserId = userId,
-            Date = DateTime.UtcNow
-        };
+        var message = new UserPushNotification { UserId = userId, Date = DateTime.UtcNow };
 
         await SendPayloadToUserAsync(userId, type, message, excludeCurrentContext);
     }
@@ -189,69 +186,32 @@ public class RelayPushNotificationService : BaseIdentityClientService, IPushNoti
 
     private async Task PushAuthRequestAsync(AuthRequest authRequest, PushType type)
     {
-        var message = new AuthRequestPushNotification
-        {
-            Id = authRequest.Id,
-            UserId = authRequest.UserId
-        };
+        var message = new AuthRequestPushNotification { Id = authRequest.Id, UserId = authRequest.UserId };
 
         await SendPayloadToUserAsync(authRequest.UserId, type, message, true);
     }
 
-    private async Task SendPayloadToUserAsync(Guid userId, PushType type, object payload, bool excludeCurrentContext)
+    public async Task PushSyncNotificationAsync(Notification notification)
     {
-        var request = new PushSendRequestModel
+        var message = new SyncNotificationPushNotification
         {
-            UserId = userId.ToString(),
-            Type = type,
-            Payload = payload
+            Id = notification.Id,
+            UserId = notification.UserId,
+            OrganizationId = notification.OrganizationId,
+            ClientType = notification.ClientType,
+            RevisionDate = notification.RevisionDate
         };
 
-        await AddCurrentContextAsync(request, excludeCurrentContext);
-        await SendAsync(HttpMethod.Post, "push/send", request);
-    }
-
-    private async Task SendPayloadToOrganizationAsync(Guid orgId, PushType type, object payload, bool excludeCurrentContext)
-    {
-        var request = new PushSendRequestModel
+        if (notification.UserId.HasValue)
         {
-            OrganizationId = orgId.ToString(),
-            Type = type,
-            Payload = payload
-        };
-
-        await AddCurrentContextAsync(request, excludeCurrentContext);
-        await SendAsync(HttpMethod.Post, "push/send", request);
-    }
-
-    private async Task AddCurrentContextAsync(PushSendRequestModel request, bool addIdentifier)
-    {
-        var currentContext = _httpContextAccessor?.HttpContext?.
-            RequestServices.GetService(typeof(ICurrentContext)) as ICurrentContext;
-        if (!string.IsNullOrWhiteSpace(currentContext?.DeviceIdentifier))
-        {
-            var device = await _deviceRepository.GetByIdentifierAsync(currentContext.DeviceIdentifier);
-            if (device != null)
-            {
-                request.DeviceId = device.Id.ToString();
-            }
-            if (addIdentifier)
-            {
-                request.Identifier = currentContext.DeviceIdentifier;
-            }
+            await SendPayloadToUserAsync(notification.UserId.Value, PushType.SyncNotification, message, true,
+                notification.ClientType);
         }
-    }
-
-    public Task SendPayloadToUserAsync(string userId, PushType type, object payload, string identifier,
-        string deviceId = null)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task SendPayloadToOrganizationAsync(string orgId, PushType type, object payload, string identifier,
-        string deviceId = null)
-    {
-        throw new NotImplementedException();
+        else if (notification.OrganizationId.HasValue)
+        {
+            await SendPayloadToOrganizationAsync(notification.OrganizationId.Value, PushType.SyncNotification, message,
+                true, notification.ClientType);
+        }
     }
 
     public async Task PushSyncOrganizationStatusAsync(Organization organization)
@@ -278,4 +238,65 @@ public class RelayPushNotificationService : BaseIdentityClientService, IPushNoti
             },
             false
         );
+
+    private async Task SendPayloadToUserAsync(Guid userId, PushType type, object payload, bool excludeCurrentContext,
+        ClientType? clientType = null)
+    {
+        var request = new PushSendRequestModel
+        {
+            UserId = userId.ToString(),
+            Type = type,
+            Payload = payload,
+            ClientType = clientType
+        };
+
+        await AddCurrentContextAsync(request, excludeCurrentContext);
+        await SendAsync(HttpMethod.Post, "push/send", request);
+    }
+
+    private async Task SendPayloadToOrganizationAsync(Guid orgId, PushType type, object payload,
+        bool excludeCurrentContext, ClientType? clientType = null)
+    {
+        var request = new PushSendRequestModel
+        {
+            OrganizationId = orgId.ToString(),
+            Type = type,
+            Payload = payload,
+            ClientType = clientType
+        };
+
+        await AddCurrentContextAsync(request, excludeCurrentContext);
+        await SendAsync(HttpMethod.Post, "push/send", request);
+    }
+
+    private async Task AddCurrentContextAsync(PushSendRequestModel request, bool addIdentifier)
+    {
+        var currentContext =
+            _httpContextAccessor?.HttpContext?.RequestServices.GetService(typeof(ICurrentContext)) as ICurrentContext;
+        if (!string.IsNullOrWhiteSpace(currentContext?.DeviceIdentifier))
+        {
+            var device = await _deviceRepository.GetByIdentifierAsync(currentContext.DeviceIdentifier);
+            if (device != null)
+            {
+                request.DeviceId = device.Id.ToString();
+            }
+
+            if (addIdentifier)
+            {
+                request.Identifier = currentContext.DeviceIdentifier;
+            }
+        }
+    }
+
+    public Task SendPayloadToUserAsync(string userId, PushType type, object payload, string identifier,
+        string deviceId = null, ClientType? clientType = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SendPayloadToOrganizationAsync(string orgId, PushType type, object payload, string identifier,
+        string deviceId = null, ClientType? clientType = null)
+    {
+        throw new NotImplementedException();
+    }
 }
