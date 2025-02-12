b489303d3e9407f6d36978766f31ad8d272382a1
Thomas Rittson <trittson@bitwarden.com>
2025-02-12 10:32:02 +1000

    Add SendPolicyRequirement

---
+++ src/Core/AdminConsole/OrganizationFeatures/Policies/PolicyRequirements/SendPolicyRequirement.cs
@@ -0,0 +1,32 @@
+﻿using Bit.Core.AdminConsole.Enums;
+using Bit.Core.AdminConsole.Models.Data.Organizations.Policies;
+using Bit.Core.Enums;
+
+namespace Bit.Core.AdminConsole.OrganizationFeatures.Policies.PolicyRequirements;
+
+public class SendPolicyRequirement : IPolicyRequirement
+{
+    public bool DisableSend { get; init; }
+    public bool DisableHideEmail { get; init; }
+
+    public static SendPolicyRequirement Create(IEnumerable<PolicyDetails> policyDetails)
+    {
+        var filteredPolicies = policyDetails
+            .ExemptRoles([OrganizationUserType.Owner, OrganizationUserType.Admin])
+            .ExemptStatus([OrganizationUserStatusType.Invited, OrganizationUserStatusType.Revoked])
+            .ExemptProviders()
+            .ToList();
+
+        return new SendPolicyRequirement
+        {
+            DisableSend = filteredPolicies
+                .GetPolicyType(PolicyType.DisableSend)
+                .Any(),
+
+            DisableHideEmail = filteredPolicies
+                .GetPolicyType(PolicyType.SendOptions)
+                .Select(up => up.GetDataModel<SendOptionsPolicyData>())
+                .Any(d => d.DisableHideEmail)
+        };
+    }
+}
