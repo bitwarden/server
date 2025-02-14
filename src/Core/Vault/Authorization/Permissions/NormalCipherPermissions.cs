696db0bd4721f4e36ac1e43680ffe1a2bd47fee9
Jimmy Vo <huynhmaivo82@gmail.com>
2025-02-14 10:40:46 -0500

    [PM-18086] Add CanRestore and CanDelete authorization methods.

---
+++ src/Core/Vault/Authorization/Permissions/NormalCipherPermissions.cs
@@ -0,0 +1,28 @@
+﻿
+using Bit.Core.Entities;
+using Bit.Core.Models.Data.Organizations;
+using Bit.Core.Vault.Models.Data;
+
+namespace Bit.Core.Vault.Authorization.Permissions;
+
+public class NormalCipherPermissions
+{
+    public static bool CanDelete(User user, CipherDetails cipherDetails, OrganizationAbility organizationAbility)
+    {
+        if (user.Id == cipherDetails.UserId)
+        {
+            return true;
+        }
+
+        if (organizationAbility.LimitItemDeletion)
+        {
+            return cipherDetails.Manage;
+        }
+        return cipherDetails.Manage || cipherDetails.Edit;
+    }
+
+    public static bool CanRestore(User user, CipherDetails cipherDetails, OrganizationAbility organizationAbility)
+    {
+        return CanDelete(user, cipherDetails, organizationAbility);
+    }
+}
