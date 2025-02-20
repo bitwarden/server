d53efcf83673a8f2b5b7a7c3c92db3259a5d2bd5
Cy Okeke <cokeke@bitwarden.com>
2025-02-20 10:18:18 +0100

    Rename the feature flag to lowercase

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -174,6 +174,7 @@ public static class FeatureFlagKeys
     public const string AndroidMutualTls = "mutual-tls";
     public const string RecoveryCodeLogin = "pm-17128-recovery-code-login";
     public const string PM3503_MobileAnonAddySelfHostAlias = "anon-addy-self-host-alias";
+    public const string PM13127_BreadcrumbEventLogs = "pm-13127-breadcrumb-event-logs";
 
     public static List<string> GetAllKeys()
     {
