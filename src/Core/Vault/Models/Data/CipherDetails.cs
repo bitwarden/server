ee3d02b8210f3acf0a9dad463a60726374f426f1
Jimmy Vo <huynhmaivo82@gmail.com>
2025-02-24 11:41:21 -0500

    [PM-18086] Undo files

--- src/Core/Vault/Models/Data/CipherDetails.cs
+++ src/Core/Vault/Models/Data/CipherDetails.cs
@@ -53,7 +53,6 @@ public class CipherDetailsWithCollections : CipherDetails
         FolderId = cipher.FolderId;
         Favorite = cipher.Favorite;
         Edit = cipher.Edit;
-        Manage = cipher.Manage;
         ViewPassword = cipher.ViewPassword;
         Manage = cipher.Manage;
 
