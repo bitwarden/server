085dbffed1cd46ad447e16d6928be6f6b7fd3cee
jrmccannon <jmccannon@bitwarden.com>
2025-03-05 08:21:30 -0600

    switched to initialization block

--- src/Core/AdminConsole/Models/Business/OrganizationDto.cs
+++ src/Core/AdminConsole/Models/Business/OrganizationDto.cs
@@ -4,28 +4,29 @@ using Bit.Core.Utilities;
 
 namespace Bit.Core.AdminConsole.Models.Business;
 
-public record OrganizationDto(
-    Guid OrganizationId,
-    bool UseCustomPermissions,
-    int? Seats,
-    int? MaxAutoScaleSeats,
-    int? SmSeats,
-    int? SmMaxAutoScaleSeats,
-    Plan Plan,
-    string GatewayCustomerId,
-    string GatewaySubscriptionId,
-    bool UseSecretsManager
-)
+public record OrganizationDto
 {
+    public Guid OrganizationId { get; init; }
+    public int? Seats { get; init; }
+    public int? MaxAutoScaleSeats { get; init; }
+    public int? SmSeats { get; init; }
+    public int? SmMaxAutoScaleSeats { get; init; }
+    public Plan Plan { get; init; }
+    public string GatewayCustomerId { get; init; }
+    public string GatewaySubscriptionId { get; init; }
+    public bool UseSecretsManager { get; init; }
+
     public static OrganizationDto FromOrganization(Organization organization) =>
-        new(organization.Id,
-            organization.UseCustomPermissions,
-            organization.Seats,
-            organization.MaxAutoscaleSeats,
-            organization.SmSeats,
-            organization.MaxAutoscaleSmSeats,
-            StaticStore.GetPlan(organization.PlanType),
-            organization.GatewayCustomerId,
-            organization.GatewaySubscriptionId,
-            organization.UseSecretsManager);
-};
+        new()
+        {
+            OrganizationId = organization.Id,
+            Seats = organization.Seats,
+            MaxAutoScaleSeats = organization.MaxAutoscaleSeats,
+            SmSeats = organization.SmSeats,
+            SmMaxAutoScaleSeats = organization.MaxAutoscaleSmSeats,
+            Plan = StaticStore.GetPlan(organization.PlanType),
+            GatewayCustomerId = organization.GatewayCustomerId,
+            GatewaySubscriptionId = organization.GatewaySubscriptionId,
+            UseSecretsManager = organization.UseSecretsManager
+        };
+}
