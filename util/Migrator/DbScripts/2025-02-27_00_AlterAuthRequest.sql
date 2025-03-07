3fb4716094124c39aabf6c2b9506cc79f2b799b1
Ike Kottlowski <ikottlowski@bitwarden.com>
2025-03-07 17:27:30 -0500

    fix : SQL formatting

--- util/Migrator/DbScripts/2025-02-27_00_AlterAuthRequest.sql
+++ util/Migrator/DbScripts/2025-02-27_00_AlterAuthRequest.sql
@@ -8,93 +8,93 @@ EXECUTE sp_refreshview 'dbo.AuthRequestView'
 GO
 
 CREATE OR ALTER PROCEDURE [dbo].[AuthRequest_Create]
-  @Id UNIQUEIDENTIFIER OUTPUT,
-  @UserId UNIQUEIDENTIFIER,
-  @OrganizationId UNIQUEIDENTIFIER = NULL,
-  @Type TINYINT,
-  @RequestDeviceIdentifier NVARCHAR(50),
-  @RequestDeviceType TINYINT,
-  @RequestIpAddress VARCHAR(50),
-  @RequestCountryName NVARCHAR(200),
-  @ResponseDeviceId UNIQUEIDENTIFIER,
-  @AccessCode VARCHAR(25),
-  @PublicKey VARCHAR(MAX),
-  @Key VARCHAR(MAX),
-  @MasterPasswordHash VARCHAR(MAX),
-  @Approved BIT,
-  @CreationDate DATETIME2(7),
-  @ResponseDate DATETIME2(7),
-  @AuthenticationDate DATETIME2(7)
+    @Id UNIQUEIDENTIFIER OUTPUT,
+    @UserId UNIQUEIDENTIFIER,
+    @OrganizationId UNIQUEIDENTIFIER = NULL,
+    @Type TINYINT,
+    @RequestDeviceIdentifier NVARCHAR(50),
+    @RequestDeviceType TINYINT,
+    @RequestIpAddress VARCHAR(50),
+    @RequestCountryName NVARCHAR(200),
+    @ResponseDeviceId UNIQUEIDENTIFIER,
+    @AccessCode VARCHAR(25),
+    @PublicKey VARCHAR(MAX),
+    @Key VARCHAR(MAX),
+    @MasterPasswordHash VARCHAR(MAX),
+    @Approved BIT,
+    @CreationDate DATETIME2(7),
+    @ResponseDate DATETIME2(7),
+    @AuthenticationDate DATETIME2(7)
 AS
 BEGIN
-  SET NOCOUNT ON
+    SET NOCOUNT ON
 
-  INSERT INTO [dbo].[AuthRequest]
-    (
-    [Id],
-    [UserId],
-    [OrganizationId],
-    [Type],
-    [RequestDeviceIdentifier],
-    [RequestDeviceType],
-    [RequestIpAddress],
-    [RequestCountryName],
-    [ResponseDeviceId],
-    [AccessCode],
-    [PublicKey],
-    [Key],
-    [MasterPasswordHash],
-    [Approved],
-    [CreationDate],
-    [ResponseDate],
-    [AuthenticationDate]
-    )
-  VALUES
-    (
-      @Id,
-      @UserId,
-      @OrganizationId,
-      @Type,
-      @RequestDeviceIdentifier,
-      @RequestDeviceType,
-      @RequestIpAddress,
-      @RequestCountryName,
-      @ResponseDeviceId,
-      @AccessCode,
-      @PublicKey,
-      @Key,
-      @MasterPasswordHash,
-      @Approved,
-      @CreationDate,
-      @ResponseDate,
-      @AuthenticationDate
+    INSERT INTO [dbo].[AuthRequest]
+        (
+        [Id],
+        [UserId],
+        [OrganizationId],
+        [Type],
+        [RequestDeviceIdentifier],
+        [RequestDeviceType],
+        [RequestIpAddress],
+        [RequestCountryName],
+        [ResponseDeviceId],
+        [AccessCode],
+        [PublicKey],
+        [Key],
+        [MasterPasswordHash],
+        [Approved],
+        [CreationDate],
+        [ResponseDate],
+        [AuthenticationDate]
+        )
+    VALUES
+        (
+            @Id,
+            @UserId,
+            @OrganizationId,
+            @Type,
+            @RequestDeviceIdentifier,
+            @RequestDeviceType,
+            @RequestIpAddress,
+            @RequestCountryName,
+            @ResponseDeviceId,
+            @AccessCode,
+            @PublicKey,
+            @Key,
+            @MasterPasswordHash,
+            @Approved,
+            @CreationDate,
+            @ResponseDate,
+            @AuthenticationDate
     )
 END
 GO
 
 CREATE OR ALTER PROCEDURE [dbo].[AuthRequest_Update]
-  @Id UNIQUEIDENTIFIER OUTPUT,
-  @UserId UNIQUEIDENTIFIER,
-  @OrganizationId UNIQUEIDENTIFIER = NULL,
-  @Type SMALLINT,
-  @RequestDeviceIdentifier NVARCHAR(50),
-  @RequestDeviceType SMALLINT,
-  @RequestIpAddress VARCHAR(50),
-  @RequestCountryName NVARCHAR(200),
-  @ResponseDeviceId UNIQUEIDENTIFIER,
-  @AccessCode VARCHAR(25),
-  @PublicKey VARCHAR(MAX),
-  @Key VARCHAR(MAX),
-  @MasterPasswordHash VARCHAR(MAX),
-  @Approved BIT,
-  @CreationDate DATETIME2 (7),
-  @ResponseDate DATETIME2 (7),
-  @AuthenticationDate DATETIME2 (7)
+    @Id UNIQUEIDENTIFIER OUTPUT,
+    @UserId UNIQUEIDENTIFIER,
+    @OrganizationId UNIQUEIDENTIFIER = NULL,
+    @Type SMALLINT,
+    @RequestDeviceIdentifier NVARCHAR(50),
+    @RequestDeviceType SMALLINT,
+    @RequestIpAddress VARCHAR(50),
+    @RequestCountryName NVARCHAR(200),
+    @ResponseDeviceId UNIQUEIDENTIFIER,
+    @AccessCode VARCHAR(25),
+    @PublicKey VARCHAR(MAX),
+    @Key VARCHAR(MAX),
+    @MasterPasswordHash VARCHAR(MAX),
+    @Approved BIT,
+    @CreationDate DATETIME2 (7),
+    @ResponseDate DATETIME2 (7),
+    @AuthenticationDate DATETIME2 (7)
 AS
 BEGIN
-  SET NOCOUNT ON
+    SET NOCOUNT ON
 
-  UPDATE
+    UPDATE
     [dbo].[AuthRequest]
 SET
     [UserId] = @UserId,
@@ -119,10 +119,10 @@ END
 GO
 
 CREATE OR ALTER PROCEDURE AuthRequest_UpdateMany
-  @jsonData NVARCHAR(MAX)
+    @jsonData NVARCHAR(MAX)
 AS
 BEGIN
-  UPDATE AR
+    UPDATE AR
     SET
         [Id] = ARI.[Id],
         [UserId] = ARI.[UserId],
@@ -142,9 +142,9 @@ BEGIN
         [AuthenticationDate] = ARI.[AuthenticationDate],
         [OrganizationId] = ARI.[OrganizationId]
     FROM
-    [dbo].[AuthRequest] AR
-    INNER JOIN
-    OPENJSON(@jsonData)
+        [dbo].[AuthRequest] AR
+        INNER JOIN
+        OPENJSON(@jsonData)
         WITH (
             Id UNIQUEIDENTIFIER '$.Id',
             UserId UNIQUEIDENTIFIER '$.UserId',
