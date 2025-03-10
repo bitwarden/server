bf2089a65fcb6239ae0568b0e18563323ef52c67
Matt Gibson <mgibson@bitwarden.com>
2025-03-10 08:11:55 -0700

    SDK decryption feature flag

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -172,6 +172,7 @@ public static class FeatureFlagKeys
     public const string WebPush = "web-push";
     public const string AndroidImportLoginsFlow = "import-logins-flow";
     public const string PM12276Breadcrumbing = "pm-12276-breadcrumbing-for-business-features";
+    public const string UseSdkForDecrypt = "use-sdk-for-decryption";
 
     public static List<string> GetAllKeys()
     {
