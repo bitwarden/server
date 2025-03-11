536101ee47280c0286d7c14a07cf86ba61b89219
gbubemismith <gsmithwalter@gmail.com>
2025-03-11 11:35:57 -0400

    Fixed linting

--- util/MySqlMigrations/Migrations/20250311152208_AddArchivedDateToCipher.cs
+++ util/MySqlMigrations/Migrations/20250311152208_AddArchivedDateToCipher.cs
@@ -1,29 +1,27 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Bit.MySqlMigrations.Migrations
+namespace Bit.MySqlMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class AddArchivedDateToCipher : Migration
 {
     /// <inheritdoc />
-    public partial class AddArchivedDateToCipher : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<DateTime>(
-                name: "ArchivedDate",
-                table: "Cipher",
-                type: "datetime(6)",
-                nullable: true);
-        }
+        migrationBuilder.AddColumn<DateTime>(
+            name: "ArchivedDate",
+            table: "Cipher",
+            type: "datetime(6)",
+            nullable: true);
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "ArchivedDate",
-                table: "Cipher");
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "ArchivedDate",
+            table: "Cipher");
     }
 }
