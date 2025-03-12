377dad6852f82ed752feef86c86382ce1b5336ee
Bernd Schoolmann <mail@quexten.com>
2025-03-12 13:40:04 +0100

    Add basic opaque usage

---
+++ src/Api/Auth/Models/Request/Opaque/RegisterStartRequest.cs
@@ -0,0 +1,9 @@
+﻿using Bitwarden.OPAQUE;
+
+namespace Bit.Api.Auth.Models.Request.Opaque;
+
+public class RegisterStartRequest
+{
+    public String ClientRegistrationStartResult { get; set; }
+    public CipherConfiguration CipherConfiguration { get; set; }
+}
---
+++ src/Api/Auth/Models/Response/Opaque/RegisterStartResponse.cs
@@ -0,0 +1,17 @@
+﻿using Bit.Core.Models.Api;
+
+namespace Bit.Api.Auth.Models.Response.Opaque;
+
+public class RegisterStartResponse : ResponseModel
+{
+    public RegisterStartResponse(Guid sessionId, string serverRegistrationStartResult, string obj = "register-start-response")
+        : base(obj)
+    {
+        ServerRegistrationStartResult = serverRegistrationStartResult;
+        SessionId = sessionId;
+    }
+
+    public String ServerRegistrationStartResult { get; set; }
+    public Guid SessionId { get; set; }
+}
+
