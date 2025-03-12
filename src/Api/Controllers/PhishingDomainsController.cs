011d86417d9cfa829facfd240e17feb438928f96
Cy Okeke <cokeke@bitwarden.com>
2025-03-12 10:21:32 +0100

    Add the phishing domain controller

---
+++ src/Api/Controllers/PhishingDomainsController.cs
@@ -0,0 +1,17 @@
+﻿using Bit.Core.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bit.Api.Controllers;
+
+[Route("phishing-domains")]
+[Authorize("Application")]
+public class PhishingDomainsController(IPhishingDomainRepository phishingDomainRepository) : Controller
+{
+    [HttpGet]
+    public async Task<ActionResult<ICollection<string>>> GetPhishingDomainsAsync()
+    {
+        var domains = await phishingDomainRepository.GetActivePhishingDomainsAsync();
+        return Ok(domains);
+    }
+}
