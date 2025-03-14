56167aae4a4401fd1d2de3e27d4b0a528da79a8d
Jonas Hendrickx <jhendrickx@bitwarden.com>
2025-03-14 16:16:52 +0100

    WIP

--- util/Migrator/DbScripts/2025-03-14_00_AddUseAdminInitiatedSponsorship.sql
+++ util/Migrator/DbScripts/2025-03-14_00_AddUseAdminInitiatedSponsorship.sql
@@ -471,6 +471,22 @@ BEGIN
 END
 GO;
 
+DROP TYPE IF EXISTS [dbo].[OrganizationSponsorshipType];
+
+CREATE TYPE [dbo].[OrganizationSponsorshipType] AS TABLE(
+    [Id] UNIQUEIDENTIFIER,
+    [SponsoringOrganizationId] UNIQUEIDENTIFIER,
+    [SponsoringOrganizationUserID] UNIQUEIDENTIFIER,
+    [SponsoredOrganizationId] UNIQUEIDENTIFIER,
+    [FriendlyName] NVARCHAR(256),
+    [OfferedToEmail] VARCHAR(256),
+    [PlanSponsorshipType] TINYINT,
+    [LastSyncDate] DATETIME2(7),
+    [ValidUntil] DATETIME2(7),
+    [ToDelete] BIT,
+    [IsAdminInitiated] BIT
+);
+
 CREATE OR ALTER PROCEDURE [dbo].[OrganizationSponsorship_UpdateMany]
     @OrganizationSponsorshipsInput [dbo].[OrganizationSponsorshipType] READONLY
 AS
