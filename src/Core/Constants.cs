27606e2d334f03d59571c5eb6b4c123e40f9071a
Patrick Honkonen <1883101+SaintPatrck@users.noreply.github.com>
2025-03-14 13:22:22 -0400

    [PM-3553] Feature flag: Mobile SimpleLogin self host alias generation (#5392)

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -176,6 +176,7 @@ public static class FeatureFlagKeys
     public const string AndroidImportLoginsFlow = "import-logins-flow";
     public const string PM12276Breadcrumbing = "pm-12276-breadcrumbing-for-business-features";
     public const string PM18794_ProviderPaymentMethod = "pm-18794-provider-payment-method";
+    public const string PM3553_MobileSimpleLoginSelfHostAlias = "simple-login-self-host-alias";
 
     public static List<string> GetAllKeys()
     {
