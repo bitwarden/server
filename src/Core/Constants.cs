7daf6cfad48260cb33f5f6b32f8f580a266d6f71
Alex Morask <144709477+amorask-bitwarden@users.noreply.github.com>
2025-03-14 11:33:24 -0400

    [PM-18794] Allow provider payment method (#5500)
    * Add PaymentSource to ProviderSubscriptionResponse
    
    * Add UpdatePaymentMethod to ProviderBillingController
    
    * Add GetTaxInformation to ProviderBillingController
    
    * Add VerifyBankAccount to ProviderBillingController
    
    * Add feature flag

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -175,6 +175,7 @@ public static class FeatureFlagKeys
     public const string WebPush = "web-push";
     public const string AndroidImportLoginsFlow = "import-logins-flow";
     public const string PM12276Breadcrumbing = "pm-12276-breadcrumbing-for-business-features";
+    public const string PM18794_ProviderPaymentMethod = "pm-18794-provider-payment-method";
 
     public static List<string> GetAllKeys()
     {
