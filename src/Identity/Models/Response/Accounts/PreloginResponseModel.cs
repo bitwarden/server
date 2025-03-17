4968ea0adb807bc800b8217469e45ed3cddee247
Bernd Schoolmann <mail@quexten.com>
2025-03-17 16:34:04 +0100

    Add prelogin response

--- src/Identity/Models/Response/Accounts/PreloginResponseModel.cs
+++ src/Identity/Models/Response/Accounts/PreloginResponseModel.cs
@@ -1,20 +1,23 @@
-﻿using Bit.Core.Enums;
+﻿using Bit.Core.Auth.Models.Api.Request.Opaque;
+using Bit.Core.Enums;
 using Bit.Core.Models.Data;
 
 namespace Bit.Identity.Models.Response.Accounts;
 
 public class PreloginResponseModel
 {
-    public PreloginResponseModel(UserKdfInformation kdfInformation)
+    public PreloginResponseModel(UserKdfInformation kdfInformation, CipherConfiguration opaqueConfiguration)
     {
         Kdf = kdfInformation.Kdf;
         KdfIterations = kdfInformation.KdfIterations;
         KdfMemory = kdfInformation.KdfMemory;
         KdfParallelism = kdfInformation.KdfParallelism;
+        OpaqueConfiguration = opaqueConfiguration;
     }
 
     public KdfType Kdf { get; set; }
     public int KdfIterations { get; set; }
     public int? KdfMemory { get; set; }
     public int? KdfParallelism { get; set; }
+    public CipherConfiguration OpaqueConfiguration { get; set; }
 }
