0aefe03f430d8578a1f67c55889a24a677c9d0bf
Bernd Schoolmann <mail@quexten.com>
2025-03-19 11:32:23 +0100

    Add ef impl

--- src/Infrastructure.EntityFramework/Auth/Repositories/OpaqueKeyExchangeCredentialRepository.cs
+++ src/Infrastructure.EntityFramework/Auth/Repositories/OpaqueKeyExchangeCredentialRepository.cs
@@ -3,19 +3,25 @@ using Bit.Core.Auth.Entities;
 using Bit.Core.Auth.Models.Data;
 using Bit.Core.Auth.Repositories;
 using Bit.Core.KeyManagement.UserKey;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Bit.Infrastructure.EntityFramework.Repositories;
 
 public class OpaqueKeyExchangeCredentialRepository : Repository<OpaqueKeyExchangeCredential, OpaqueKeyExchangeCredential, Guid>, IOpaqueKeyExchangeCredentialRepository
 {
-    public OpaqueKeyExchangeCredentialRepository(IServiceScopeFactory serviceScopeFactory, IMapper mapper) : base(serviceScopeFactory, mapper, (DatabaseContext context) => null)
+    public OpaqueKeyExchangeCredentialRepository(IServiceScopeFactory serviceScopeFactory, IMapper mapper) : base(serviceScopeFactory, mapper, (DatabaseContext context) => context.OpaqueKeyExchangeCredentials)
     {
     }
 
-    public Task<OpaqueKeyExchangeCredential> GetByUserIdAsync(Guid userId)
+    public async Task<OpaqueKeyExchangeCredential> GetByUserIdAsync(Guid userId)
     {
-        return null;
+        using (var scope = ServiceScopeFactory.CreateScope())
+        {
+            var dbContext = GetDatabaseContext(scope);
+            var opaqueConfig = await GetDbSet(dbContext).SingleOrDefaultAsync(sc => sc.UserId == userId);
+            return Mapper.Map<OpaqueKeyExchangeCredential>(opaqueConfig);
+        }
     }
     public UpdateEncryptedDataForKeyRotation UpdateKeysForRotationAsync(Guid userId, IEnumerable<OpaqueKeyExchangeRotateKeyData> credentials)
     {
