5184658e17f8b4211b07953bfea466a4669f5b97
Patrick Pimentel <ppimentel@bitwarden.com>
2025-03-20 10:51:08 -0400

    comment(innovation-archive): Controller Work - Added comments.

--- src/Core/Vault/Commands/Interfaces/IArchiveCiphersCommand.cs
+++ src/Core/Vault/Commands/Interfaces/IArchiveCiphersCommand.cs
@@ -4,5 +4,11 @@ namespace Bit.Core.Vault.Commands.Interfaces;
 
 public interface IArchiveCiphersCommand
 {
+    /// <summary>
+    /// Archives a cipher. This fills in the ArchivedDate property on a Cipher.
+    /// </summary>
+    /// <param name="cipherIds">Cipher ID to archive.</param>
+    /// <param name="archivingUserId">User ID to check against the Ciphers that are trying to be archived.</param>
+    /// <returns></returns>
     public Task<ICollection<CipherOrganizationDetails>> ArchiveManyAsync(IEnumerable<Guid> cipherIds, Guid archivingUserId);
 }
