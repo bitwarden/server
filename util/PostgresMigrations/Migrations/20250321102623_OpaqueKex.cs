83c155746a650311d381850f52caf9b6aa854f4a
Bernd Schoolmann <mail@quexten.com>
2025-03-21 23:49:42 +0100

    EF migrations (#5537)
    Co-authored-by: Ike Kottlowski <ikottlowski@bitwarden.com>

---
+++ util/PostgresMigrations/Migrations/20250321102623_OpaqueKex.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.PostgresMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class OpaqueKex : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateTable(
+            name: "OpaqueKeyExchangeCredential",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "uuid", nullable: false),
+                UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                CipherConfiguration = table.Column<string>(type: "text", nullable: true),
+                CredentialBlob = table.Column<string>(type: "text", nullable: true),
+                EncryptedPublicKey = table.Column<string>(type: "text", nullable: true),
+                EncryptedPrivateKey = table.Column<string>(type: "text", nullable: true),
+                EncryptedUserKey = table.Column<string>(type: "text", nullable: true),
+                CreationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_OpaqueKeyExchangeCredential", x => x.Id);
+            });
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "OpaqueKeyExchangeCredential");
+    }
+}
