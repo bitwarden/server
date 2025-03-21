64e2b85e8c700efae6ef781f3de1f5b5272bc99e
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2025-03-21 16:51:02 -0400

    Shim DevContainer and local

--- dev/add-dc-profile.sh
+++ dev/add-dc-profile.sh
@@ -3,9 +3,15 @@ if [[ -z "${CODESPACES}" ]]; then
   if [[ -z "${HOSTNAME}" ]]; then
     # Add docker compose profile using local strategy
     echo "Locally"
+    # No need to patch it into another compose project when ran locally
+    docker compose --profile "$1" up -d
   else
     # Add docker compose profile to dev container
+    # Is this definitely always the same thing as codespaces?
     echo "In DevContainer"
+    source_dir="$( dirname "${BASH_SOURCE[0]}")"
+    echo $source_dir
+    docker compose -p bitwarden_common --profile "$1" -f "${source_dir}/docker-compose.yml" up -d
   fi
 else
   # Add docker compose profile to codespace
