2931e73c299b904b766508ed27e78abbc42cbb6f
Patrick Pimentel <ppimentel@bitwarden.com>
2025-03-24 10:08:33 -0400

    test(innovation-archive): Controller Work - Got test logic working.

--- test/Core.Test/Vault/Commands/ArchiveCiphersCommandTest.cs
+++ test/Core.Test/Vault/Commands/ArchiveCiphersCommandTest.cs
@@ -19,28 +19,35 @@ namespace Bit.Core.Test.Vault.Commands;
 public class ArchiveCiphersCommandTest
 {
     [Theory]
-    [BitAutoData(true, false, 1, 1, 1)]
-    [BitAutoData(false, false, 1, 0, 1)]
-    [BitAutoData(false, true, 1, 0, 1)]
-    [BitAutoData(true, true, 1, 0, 1)]
+    [BitAutoData(true, false, 1, 1, 1, 1)]
+    [BitAutoData(false, false, 1, 0, 0, 1)]
+    [BitAutoData(false, true, 1, 0, 0, 1)]
+    [BitAutoData(true, true, 1, 0, 0, 1)]
     public async Task ArchiveAsync_Works(
         bool isEditable, bool hasOrganizationId,
-        int cipherRepoCalls, int eventServiceCalls, int pushNotificationsCalls,
+        int cipherRepoCalls, int resultCountFromQuery, int eventServiceCalls, int pushNotificationsCalls,
         SutProvider<ArchiveCiphersCommand> sutProvider, CipherDetails cipher, User user)
     {
         // Arrange
         cipher.Edit = isEditable;
         cipher.OrganizationId = hasOrganizationId ? Guid.NewGuid() : null;
 
+        var cipherList = new List<CipherDetails> { cipher };
+
         sutProvider.GetDependency<ICipherRepository>()
-            .GetManyByUserIdAsync(user.Id).Returns(new List<CipherDetails> { cipher });
+            .GetManyByUserIdAsync(user.Id).Returns(cipherList);
 
         // Act
         await sutProvider.Sut.ArchiveManyAsync([cipher.Id], user.Id);
 
         // Assert
-        await sutProvider.GetDependency<ICipherRepository>().Received(cipherRepoCalls).ArchiveAsync(Arg.Any<IEnumerable<Guid>>(), user.Id);
-        await sutProvider.GetDependency<IEventService>().Received(eventServiceCalls).LogCipherEventsAsync(Arg.Any<IEnumerable<Tuple<Cipher, EventType, DateTime?>>>());
-        await sutProvider.GetDependency<IPushNotificationService>().Received(pushNotificationsCalls).PushSyncCiphersAsync(user.Id);
+        await sutProvider.GetDependency<ICipherRepository>().Received(cipherRepoCalls).ArchiveAsync(
+            Arg.Is<IEnumerable<Guid>>(ids => ids.Count() == resultCountFromQuery
+                                             && ids.Count() >= 1 ? true : ids.All(id => cipherList.Contains(cipher))),
+            user.Id);
+        await sutProvider.GetDependency<IEventService>().Received(eventServiceCalls)
+            .LogCipherEventsAsync(Arg.Any<IEnumerable<Tuple<Cipher, EventType, DateTime?>>>());
+        await sutProvider.GetDependency<IPushNotificationService>().Received(pushNotificationsCalls)
+            .PushSyncCiphersAsync(user.Id);
     }
 }
