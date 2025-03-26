f25b308f57f5452b20ebfedaf04640d35fb2694e
Brant DeBow <brant@livefront.com>
2025-03-26 15:26:02 -0400

    Initial buildout of basic repositories

---
+++ src/Core/AdminConsole/Repositories/IOrganizationIntegrationConfigurationRepository.cs
@@ -0,0 +1,22 @@
+﻿using Bit.Core.AdminConsole.Entities;
+using Bit.Core.Enums;
+
+namespace Bit.Core.Repositories;
+
+public interface IOrganizationIntegrationConfigurationRepository : IRepository<OrganizationIntegrationConfiguration, Guid>
+{
+    Task<List<OrganizationIntegrationConfiguration>> GetConfigurationsAsync(
+        Guid organizationId,
+        IntegrationType integrationType,
+        EventType eventType);
+
+    // Task<List<IntegrationConfiguration<T>>> GetConfigurationsAsync<T>(
+    //     Guid organizationId,
+    //     IntegrationType integrationType,
+    //     EventType eventType);
+    //
+    // Task CreateOrganizationIntegrationAsync<T>(
+    //     Guid organizationId,
+    //     IntegrationType integrationType,
+    //     T configuration);
+}
