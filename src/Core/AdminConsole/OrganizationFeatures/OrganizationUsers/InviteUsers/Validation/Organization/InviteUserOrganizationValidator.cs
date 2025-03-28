f8b172ef32c5d8aa908d09f319e5b7ab7333580d
jrmccannon <jmccannon@bitwarden.com>
2025-03-28 10:57:20 -0500

    Fixing compiler complaints.

--- src/Core/AdminConsole/OrganizationFeatures/OrganizationUsers/InviteUsers/Validation/Organization/InviteUserOrganizationValidator.cs
+++ src/Core/AdminConsole/OrganizationFeatures/OrganizationUsers/InviteUsers/Validation/Organization/InviteUserOrganizationValidator.cs
@@ -7,23 +7,26 @@ public interface IInviteUserOrganizationValidator : IValidator<InviteOrganizatio
 
 public class InviteUserOrganizationValidator : IInviteUserOrganizationValidator
 {
-    public async Task<ValidationResult<InviteOrganization>> ValidateAsync(InviteOrganization inviteOrganization)
+    public Task<ValidationResult<InviteOrganization>> ValidateAsync(InviteOrganization inviteOrganization)
     {
         if (inviteOrganization.Seats is null)
         {
-            return new Valid<InviteOrganization>(inviteOrganization);
+            return Task.FromResult<ValidationResult<InviteOrganization>>(
+                new Valid<InviteOrganization>(inviteOrganization));
         }
 
         if (string.IsNullOrWhiteSpace(inviteOrganization.GatewayCustomerId))
         {
-            return new Invalid<InviteOrganization>(new OrganizationNoPaymentMethodFoundError(inviteOrganization));
+            return Task.FromResult<ValidationResult<InviteOrganization>>(
+                new Invalid<InviteOrganization>(new OrganizationNoPaymentMethodFoundError(inviteOrganization)));
         }
 
         if (string.IsNullOrWhiteSpace(inviteOrganization.GatewaySubscriptionId))
         {
-            return new Invalid<InviteOrganization>(new OrganizationNoSubscriptionFoundError(inviteOrganization));
+            return Task.FromResult<ValidationResult<InviteOrganization>>(
+                new Invalid<InviteOrganization>(new OrganizationNoSubscriptionFoundError(inviteOrganization)));
         }
 
-        return new Valid<InviteOrganization>(inviteOrganization);
+        return Task.FromResult<ValidationResult<InviteOrganization>>(new Valid<InviteOrganization>(inviteOrganization));
     }
 }
