79dff53ada167fe7e76975befa0185b89530c545
Brant DeBow <brant@livefront.com>
2025-03-28 10:13:23 -0400

    [PM-17562] Add Dapper Repositories For Ogranization Integrations and Configurations

---
+++ src/Core/AdminConsole/Repositories/IOrganizationIntegrationConfigurationRepository.cs
@@ -0,0 +1,12 @@
+﻿using Bit.Core.AdminConsole.Entities;
+using Bit.Core.Enums;
+
+namespace Bit.Core.Repositories;
+
+public interface IOrganizationIntegrationConfigurationRepository : IRepository<OrganizationIntegrationConfiguration, Guid>
+{
+    Task<List<OrganizationIntegrationConfiguration>> GetConfigurationsAsync(
+        Guid organizationId,
+        IntegrationType integrationType,
+        EventType eventType);
+}
