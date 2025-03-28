1e72072b3a69a33428d9c568edc3b31ee5ea912c
jrmccannon <jmccannon@bitwarden.com>
2025-03-28 10:00:02 -0500

    Fixing compiler complaint.

--- src/Core/AdminConsole/OrganizationFeatures/OrganizationUsers/InviteUsers/Validation/GlobalSettings/EnvironmentValidator.cs
+++ src/Core/AdminConsole/OrganizationFeatures/OrganizationUsers/InviteUsers/Validation/GlobalSettings/EnvironmentValidator.cs
@@ -6,8 +6,9 @@ public interface IEnvironmentValidator : IValidator<EnvironmentRequest>;
 
 public class EnvironmentValidator : IEnvironmentValidator
 {
-    public async Task<ValidationResult<EnvironmentRequest>> ValidateAsync(EnvironmentRequest value) =>
-        value.IsSelfHosted && value.PasswordManagerSubscriptionUpdate.SeatsRequiredToAdd > 0 ?
-           new Invalid<EnvironmentRequest>(new CannotAutoScaleOnSelfHostError(value)) :
-            new Valid<EnvironmentRequest>(value);
+    public Task<ValidationResult<EnvironmentRequest>> ValidateAsync(EnvironmentRequest value) =>
+        Task.FromResult<ValidationResult<EnvironmentRequest>>(
+            value.IsSelfHosted && value.PasswordManagerSubscriptionUpdate.SeatsRequiredToAdd > 0 ?
+                new Invalid<EnvironmentRequest>(new CannotAutoScaleOnSelfHostError(value)) :
+                new Valid<EnvironmentRequest>(value));
 }
