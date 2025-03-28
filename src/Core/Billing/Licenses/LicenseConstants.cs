1733416593e48a635cb4d75074919088cdd6a293
Cy Okeke <cokeke@bitwarden.com>
2025-03-28 10:26:20 +0100

    initial commit

--- src/Core/Billing/Licenses/LicenseConstants.cs
+++ src/Core/Billing/Licenses/LicenseConstants.cs
@@ -40,6 +40,7 @@ public static class OrganizationLicenseConstants
     public const string Refresh = nameof(Refresh);
     public const string ExpirationWithoutGracePeriod = nameof(ExpirationWithoutGracePeriod);
     public const string Trial = nameof(Trial);
+    public const string UseAdminSponsoredFamilies = nameof(UseAdminSponsoredFamilies);
 }
 
 public static class UserLicenseConstants
