e8d608a1c0bb387cdb81523a1860382572ff12b8
Brant DeBow <brant@livefront.com>
2025-03-28 14:13:01 -0400

    [PM-17562] Add Dapper Repositories For Ogranization Integrations and Configurations

---
+++ src/Core/AdminConsole/Models/Data/Organizations/OrganizationIntegrationConfigurationDetails.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json.Nodes;
+using Bit.Core.Enums;
+
+#nullable enable
+
+namespace Bit.Core.Models.Data.Organizations;
+
+public class OrganizationIntegrationConfigurationDetails
+{
+    public Guid Id { get; set; }
+    public Guid OrganizationIntegrationId { get; set; }
+    public IntegrationType IntegrationType { get; set; }
+    public EventType EventType { get; set; }
+    public string? Configuration { get; set; }
+    public string? IntegrationConfiguration { get; set; }
+    public string? Template { get; set; }
+
+    public JsonObject MergedConfiguration
+    {
+        get
+        {
+            var configJson = JsonNode.Parse(Configuration ?? string.Empty) as JsonObject ?? new JsonObject();
+            var integrationJson = JsonNode.Parse(IntegrationConfiguration ?? string.Empty) as JsonObject ?? new JsonObject();
+
+            foreach (var kvp in configJson)
+            {
+                integrationJson[kvp.Key] = kvp.Value;
+            }
+
+            return integrationJson;
+        }
+    }
+}
