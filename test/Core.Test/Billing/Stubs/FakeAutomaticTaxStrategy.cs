aed58506e7dbe0a61721c9e7b89c1055b2353d99
Jonas Hendrickx <jhendrickx@bitwarden.com>
2025-04-02 12:18:15 +0200

    Fix stub

--- test/Core.Test/Billing/Stubs/FakeAutomaticTaxStrategy.cs
+++ test/Core.Test/Billing/Stubs/FakeAutomaticTaxStrategy.cs
@@ -3,27 +3,33 @@ using Stripe;
 
 namespace Bit.Core.Test.Billing.Stubs;
 
-/// <param name="IsAutomaticTaxEnabled">
+/// <param name="isAutomaticTaxEnabled">
 /// Whether the subscription options will have automatic tax enabled or not.
 /// </param>
 public class FakeAutomaticTaxStrategy(
-    bool IsAutomaticTaxEnabled) : IAutomaticTaxStrategy
+    bool isAutomaticTaxEnabled) : IAutomaticTaxStrategy
 {
     public SubscriptionUpdateOptions? GetUpdateOptions(Subscription subscription)
     {
         return new SubscriptionUpdateOptions
         {
-            AutomaticTax = new SubscriptionAutomaticTaxOptions { Enabled = IsAutomaticTaxEnabled }
+            AutomaticTax = new SubscriptionAutomaticTaxOptions { Enabled = isAutomaticTaxEnabled }
         };
     }
 
     public void SetCreateOptions(SubscriptionCreateOptions options, Customer customer)
     {
-        options.AutomaticTax = new SubscriptionAutomaticTaxOptions { Enabled = IsAutomaticTaxEnabled };
+        options.AutomaticTax = new SubscriptionAutomaticTaxOptions { Enabled = isAutomaticTaxEnabled };
     }
 
     public void SetUpdateOptions(SubscriptionUpdateOptions options, Subscription subscription)
     {
-        options.AutomaticTax = new SubscriptionAutomaticTaxOptions { Enabled = IsAutomaticTaxEnabled };
+        options.AutomaticTax = new SubscriptionAutomaticTaxOptions { Enabled = isAutomaticTaxEnabled };
+    }
+
+    public void SetInvoiceCreatePreviewOptions(InvoiceCreatePreviewOptions options)
+    {
+        options.AutomaticTax = new InvoiceAutomaticTaxOptions { Enabled = isAutomaticTaxEnabled };
+
     }
 }
