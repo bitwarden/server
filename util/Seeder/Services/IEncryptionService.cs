0abf4da126f6bb6141de2908d78cc83b64d5ec1d
Hinton <hinton@users.noreply.github.com>
2025-04-02 19:27:06 +0900

    Extract logic into seeder classes

--- util/Seeder/Services/IEncryptionService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Bit.Seeder.Services;
-
-public interface IEncryptionService
-{
-    string HashPassword(string password);
-    byte[] DeriveKey(string password, string salt);
-    string EncryptString(string plaintext, byte[] key);
-}
--- util/Seeder/Services/ISeederService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Bit.Seeder.Services;
-
-public interface ISeederService
-{
-    Task GenerateSeedsAsync(int userCount, int ciphersPerUser, string seedName);
-    Task LoadSeedsAsync(string seedName, string? timestamp = null);
-    Task GenerateAndLoadSeedsAsync(int userCount, int ciphersPerUser, string seedName);
-    Task ExtractSeedsAsync(string seedName);
-}
