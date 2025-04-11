e3d5305604ef2a82953399372139b3e71e0c0153
El Yosemite <yurifullstack@gmail.com>
2025-04-11 08:14:03 -0300

    Move InstallationSettings, LaunchDarklySettings, MailSettings, NotificationHubPoolSettings, NotificationHubSettings and NotificationsSettings classes from GlobalSettings.cs to files in the same namespace

---
+++ src/Core/Settings/NotificationHubPoolSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Bit.Core.Settings;
+
+public class NotificationHubPoolSettings
+{
+    /// <summary>
+    /// List of Notification Hub settings to use for sending push notifications.
+    ///
+    /// Note that hubs on the same namespace share active device limits, so multiple namespaces should be used to increase capacity.
+    /// </summary>
+    public List<NotificationHubSettings> NotificationHubs { get; set; } = new();
+}
