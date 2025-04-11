31ad9c42e336cce5aa2396b730c94552020e329a
El Yosemite <yurifullstack@gmail.com>
2025-04-11 08:18:42 -0300

    Move PasswordlessAuthSettings SentrySettings, ServiceBusSettings, SqlSettings, SsoSettings, StripeSettings, SyslogSettings, WebPushSettings and YubicoSettings classes from GlobalSettings.cs to file in the same namespace

---
+++ src/Core/Settings/StripeSettings.cs
@@ -0,0 +1,6 @@
+﻿namespace Bit.Core.Settings;
+public class StripeSettings
+{
+    public string ApiKey { get; set; }
+    public int MaxNetworkRetries { get; set; } = 2;
+}
---
+++ src/Core/Settings/YubicoSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Bit.Core.Settings;
+
+public class YubicoSettings
+{
+    public string ClientId { get; set; }
+    public string Key { get; set; }
+    public string[] ValidationUrls { get; set; }
+}
+
