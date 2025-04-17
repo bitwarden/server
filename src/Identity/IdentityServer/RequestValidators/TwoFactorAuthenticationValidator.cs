f2b8a920e6e671b97f455ff2afc7448083e40125
Ike Kottlowski <ikottlowski@bitwarden.com>
2025-04-17 14:42:41 -0400

    fix : split out the interface from the TwoFactorAuthenticationValidator into separate file.

--- src/Identity/IdentityServer/RequestValidators/TwoFactorAuthenticationValidator.cs
+++ src/Identity/IdentityServer/RequestValidators/TwoFactorAuthenticationValidator.cs
@@ -16,42 +16,10 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Bit.Identity.IdentityServer.RequestValidators;
 
-public interface ITwoFactorAuthenticationValidator
-{
-    /// <summary>
-    /// Check if the user is required to use two-factor authentication to login. This is based on the user's
-    /// enabled two-factor providers, the user's organizations enabled two-factor providers, and the grant type.
-    /// Client credentials and webauthn grant types do not require two-factor authentication.
-    /// </summary>
-    /// <param name="user">the active user for the request</param>
-    /// <param name="request">the request that contains the grant types</param>
-    /// <returns>boolean</returns>
-    Task<Tuple<bool, Organization>> RequiresTwoFactorAsync(User user, ValidatedTokenRequest request);
-    /// <summary>
-    /// Builds the two-factor authentication result for the user based on the available two-factor providers
-    /// from either their user account or Organization.
-    /// </summary>
-    /// <param name="user">user trying to login</param>
-    /// <param name="organization">organization associated with the user; Can be null</param>
-    /// <returns>Dictionary with the TwoFactorProviderType as the Key and the Provider Metadata as the Value</returns>
-    Task<Dictionary<string, object>> BuildTwoFactorResultAsync(User user, Organization organization);
-    /// <summary>
-    /// Uses the built in userManager methods to verify the two-factor token for the user. If the organization uses
-    /// organization duo, it will use the organization duo token provider to verify the token.
-    /// </summary>
-    /// <param name="user">the active User</param>
-    /// <param name="organization">organization of user; can be null</param>
-    /// <param name="twoFactorProviderType">Two Factor Provider to use to verify the token</param>
-    /// <param name="token">secret passed from the user and consumed by the two-factor provider's verify method</param>
-    /// <returns>boolean</returns>
-    Task<bool> VerifyTwoFactorAsync(User user, Organization organization, TwoFactorProviderType twoFactorProviderType, string token);
-}
-
 public class TwoFactorAuthenticationValidator(
     IUserService userService,
     UserManager<User> userManager,
     IOrganizationDuoUniversalTokenProvider organizationDuoWebTokenProvider,
-    IFeatureService featureService,
     IApplicationCacheService applicationCacheService,
     IOrganizationUserRepository organizationUserRepository,
     IOrganizationRepository organizationRepository,
@@ -61,7 +29,6 @@ public class TwoFactorAuthenticationValidator(
     private readonly IUserService _userService = userService;
     private readonly UserManager<User> _userManager = userManager;
     private readonly IOrganizationDuoUniversalTokenProvider _organizationDuoUniversalTokenProvider = organizationDuoWebTokenProvider;
-    private readonly IFeatureService _featureService = featureService;
     private readonly IApplicationCacheService _applicationCacheService = applicationCacheService;
     private readonly IOrganizationUserRepository _organizationUserRepository = organizationUserRepository;
     private readonly IOrganizationRepository _organizationRepository = organizationRepository;
