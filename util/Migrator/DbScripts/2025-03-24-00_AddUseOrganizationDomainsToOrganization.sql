424e49036bd9986b3a2e0db359360351ef57ed16
Brandon <btreston@bitwarden.com>
2025-04-22 16:04:16 -0400

    add update statement to mssql migration script

--- util/Migrator/DbScripts/2025-03-24-00_AddUseOrganizationDomainsToOrganization.sql
+++ util/Migrator/DbScripts/2025-03-24-00_AddUseOrganizationDomainsToOrganization.sql
@@ -3,6 +3,16 @@
 ALTER TABLE [dbo].[Organization] ADD [UseOrganizationDomains] bit NOT NULL CONSTRAINT [DF_Organization_UseOrganizationDomains] default (0)
 GO
 
+/* update the new column to have the value used in UseSso to preserve existing orgs ability */
+
+UPDATE
+    [dbo].[Organization]
+SET
+    [UseOrganizationDomains] = [UseSso]
+WHERE
+    [UseOrganizationDomains] IS NULL
+GO
+
 /* add column to Organization_Create*/
 
 CREATE OR ALTER PROCEDURE [dbo].[Organization_Create]
