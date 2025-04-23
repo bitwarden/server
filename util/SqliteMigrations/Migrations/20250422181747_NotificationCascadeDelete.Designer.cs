680970962862ad94908b84c07d71cb0a8266df92
Nick Krantz <125900171+nick-livefront@users.noreply.github.com>
2025-04-23 13:16:29 -0500

    [PM-20064] Add cascade deletion for cipher with tasks (#5690)
    * add cascade deletion for cipher tasks
    
    * add migrations for cascade delete on ciphers and security tasks
    
    * remove trailing comma
    
    * add SQL migration for PasswordHealthReportApplication
    
    - Allow cascade delete when an organization is deleted

--- util/SqliteMigrations/Migrations/20250422181747_NotificationCascadeDelete.Designer.cs
+++ util/SqliteMigrations/Migrations/20250422181747_NotificationCascadeDelete.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Bit.Infrastructure.EntityFramework.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bit.SqliteMigrations.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20250422181747_NotificationCascadeDelete")]
+    partial class NotificationCascadeDelete
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.8");
@@ -2842,8 +2845,7 @@ namespace Bit.SqliteMigrations.Migrations
                 {
                     b.HasOne("Bit.Infrastructure.EntityFramework.Vault.Models.Cipher", "Cipher")
                         .WithMany()
-                        .HasForeignKey("CipherId")
-                        .OnDelete(DeleteBehavior.Cascade);
+                        .HasForeignKey("CipherId");
 
                     b.HasOne("Bit.Infrastructure.EntityFramework.AdminConsole.Models.Organization", "Organization")
                         .WithMany()
