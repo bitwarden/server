b30edc541660bb0b1af797cb9724e9697f14bc3e
tangowithfoxtrot <5676771+tangowithfoxtrot@users.noreply.github.com>
2025-05-02 08:39:43 -0700

    fix: compatibility with existing run.sh script

--- util/Setup/entrypoint.sh
+++ util/Setup/entrypoint.sh
@@ -19,27 +19,29 @@ then
     LGID=65534
 fi
 
-# Create user and group
+if [ "$(id -u)" = "0" ]
+then
+    # Create user and group
 
-groupadd -o -g $LGID $GROUPNAME >/dev/null 2>&1 ||
-groupmod -o -g $LGID $GROUPNAME >/dev/null 2>&1
-useradd -o -u $LUID -g $GROUPNAME -s /bin/false $USERNAME >/dev/null 2>&1 ||
-usermod -o -u $LUID -g $GROUPNAME -s /bin/false $USERNAME >/dev/null 2>&1
-mkhomedir_helper $USERNAME
+    groupadd -o -g $LGID $GROUPNAME >/dev/null 2>&1 ||
+    groupmod -o -g $LGID $GROUPNAME >/dev/null 2>&1
+    useradd -o -u $LUID -g $GROUPNAME -s /bin/false $USERNAME >/dev/null 2>&1 ||
+    usermod -o -u $LUID -g $GROUPNAME -s /bin/false $USERNAME >/dev/null 2>&1
+    mkhomedir_helper $USERNAME
 
-# The rest...
+    # The rest...
 
-chown -R $USERNAME:$GROUPNAME /app
-mkdir -p /bitwarden/env
-mkdir -p /bitwarden/docker
-mkdir -p /bitwarden/ssl
-mkdir -p /bitwarden/letsencrypt
-mkdir -p /bitwarden/identity
-mkdir -p /bitwarden/nginx
-mkdir -p /bitwarden/ca-certificates
-chown -R $USERNAME:$GROUPNAME /bitwarden
+    chown -R $USERNAME:$GROUPNAME /app
+    mkdir -p /bitwarden/env
+    mkdir -p /bitwarden/docker
+    mkdir -p /bitwarden/ssl
+    mkdir -p /bitwarden/letsencrypt
+    mkdir -p /bitwarden/identity
+    mkdir -p /bitwarden/nginx
+    mkdir -p /bitwarden/ca-certificates
+    chown -R $USERNAME:$GROUPNAME /bitwarden
+else
+    gosu_cmd=""
+fi
 
-cp /bitwarden/ca-certificates/*.crt /usr/local/share/ca-certificates/ >/dev/null 2>&1 \
-    && update-ca-certificates
-
-exec gosu $USERNAME:$GROUPNAME "$@"
+exec $gosu_cmd "$@"
