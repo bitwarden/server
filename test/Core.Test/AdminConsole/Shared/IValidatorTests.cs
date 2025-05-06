fee53d2d1845b96e41e8a6adcceb392b82c78c8a
Thomas Rittson <trittson@bitwarden.com>
2025-05-06 12:56:04 +1000

    Simplify and align CommandResult and ValidationResult

--- test/Core.Test/AdminConsole/Shared/IValidatorTests.cs
+++ test/Core.Test/AdminConsole/Shared/IValidatorTests.cs
@@ -22,13 +22,11 @@ public class IValidatorTests
         {
             if (string.IsNullOrWhiteSpace(value.Name))
             {
-                return Task.FromResult<ValidationResult<TestClass>>(new Invalid<TestClass>
-                {
-                    Errors = [new InvalidRequestError<TestClass>(value)]
-                });
+                return Task.FromResult<ValidationResult<TestClass>>(
+                    new Invalid<TestClass>(new InvalidRequestError<TestClass>(value)));
             }
 
-            return Task.FromResult<ValidationResult<TestClass>>(new Valid<TestClass> { Value = value });
+            return Task.FromResult<ValidationResult<TestClass>>(new Valid<TestClass>(value));
         }
     }
 
@@ -41,7 +39,7 @@ public class IValidatorTests
 
         Assert.IsType<Invalid<TestClass>>(result);
         var invalidResult = result as Invalid<TestClass>;
-        Assert.Equal(InvalidRequestError<TestClass>.Code, invalidResult.Errors.First().Message);
+        Assert.Equal(InvalidRequestError<TestClass>.Code, invalidResult!.Error.Message);
     }
 
     [Fact]
