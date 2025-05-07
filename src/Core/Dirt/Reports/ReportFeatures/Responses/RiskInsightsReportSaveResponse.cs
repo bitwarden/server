743a5e5d9fd3c25505af1acf449a767be0811058
voommen-livefront <voommen@livefront.com>
2025-05-07 08:33:54 -0500

    PM-20577 placeholder in reports controller

---
+++ src/Core/Dirt/Reports/ReportFeatures/Responses/RiskInsightsReportSaveResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Bit.Core.Tools.ReportFeatures.Requests;
+
+public class RiskInsightsReportResponse
+{
+    public Guid Id { get; set; }
+    public Guid OrganizationId { get; set; }
+    public string ReportData { get; set; }
+    public int TotalMembers { get; set; }
+    public int TotalAtRiskMembers { get; set; }
+    public int TotalApplications { get; set; }
+    public int TotalAtRiskApplications { get; set; }
+    public int TotalCriticalApplications { get; set; }
+}
