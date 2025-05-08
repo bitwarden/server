fb28fb60a9d15839c4d4f41ea2f4e87c8939ed6c
Hinton <hinton@users.noreply.github.com>
2025-05-08 17:50:57 +0200

    Cleanup

--- test/Api.IntegrationTest/AdminConsole/Controllers/OrganizationUsersControllerPerformanceTests.cs
+++ test/Api.IntegrationTest/AdminConsole/Controllers/OrganizationUsersControllerPerformanceTests.cs
@@ -7,41 +7,33 @@ using Xunit.Abstractions;
 
 namespace Bit.Api.IntegrationTest.AdminConsole.Controllers;
 
-public class OrganizationUsersControllerPerformanceTest
+public class OrganizationUsersControllerPerformanceTest(ITestOutputHelper testOutputHelper)
 {
-    private readonly HttpClient _client;
-    private readonly ApiApplicationFactory _factory;
-    private readonly ITestOutputHelper _testOutputHelper;
-
-    public OrganizationUsersControllerPerformanceTest(ITestOutputHelper testOutputHelper)
-    {
-        _factory = new ApiApplicationFactory();
-        _testOutputHelper = testOutputHelper;
-        _client = _factory.CreateClient();
-    }
-
-    [Theory(Skip = "Performance test")]
+    [Theory]
     [InlineData(100)]
     [InlineData(60000)]
     public async Task GetAsync(int seats)
     {
-        var db = _factory.GetDatabaseContext();
+        await using var factory = new ApiApplicationFactory();
+        var client = factory.CreateClient();
+
+        var db = factory.GetDatabaseContext();
         var seeder = new OrganizationWithUsersRecipe(db);
 
         var orgId = seeder.Seed("Org", seats, "large.test");
 
-        var tokens = await _factory.LoginAsync("admin@large.test", "c55hlJ/cfdvTd4awTXUqow6X3cOQCfGwn11o3HblnPs=");
-        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokens.Token);
+        var tokens = await factory.LoginAsync("admin@large.test", "c55hlJ/cfdvTd4awTXUqow6X3cOQCfGwn11o3HblnPs=");
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokens.Token);
 
         var stopwatch = System.Diagnostics.Stopwatch.StartNew();
 
-        var response = await _client.GetAsync($"/organizations/{orgId}/users?includeCollections=true");
+        var response = await client.GetAsync($"/organizations/{orgId}/users?includeCollections=true");
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
         var result = await response.Content.ReadAsStringAsync();
         Assert.NotEmpty(result);
 
         stopwatch.Stop();
-        _testOutputHelper.WriteLine($"Seed: {seats}; Request duration: {stopwatch.ElapsedMilliseconds} ms");
+        testOutputHelper.WriteLine($"Seed: {seats}; Request duration: {stopwatch.ElapsedMilliseconds} ms");
     }
 }
