8e7fa98bcf183fad20016b44e9e892fee2caf221
Thomas Rittson <trittson@bitwarden.com>
2025-05-09 15:11:10 +1000

    Rewrite using latest utils, add Create test

--- test/Infrastructure.IntegrationTest/AdminConsole/Repositories/CollectionRepository/CollectionRepositoryCreateTests.cs
+++ test/Infrastructure.IntegrationTest/AdminConsole/Repositories/CollectionRepository/CollectionRepositoryCreateTests.cs
@@ -6,10 +6,10 @@ using Xunit;
 
 namespace Bit.Infrastructure.IntegrationTest.AdminConsole.Repositories.CollectionRepository;
 
-public class CollectionRepositoryReplaceTests
+public class CollectionRepositoryCreateTests
 {
     [DatabaseTheory, DatabaseData]
-    public async Task ReplaceAsync_Works(
+    public async Task CreateAsync_Works(
         IUserRepository userRepository,
         IOrganizationRepository organizationRepository,
         IOrganizationUserRepository organizationUserRepository,
@@ -25,12 +25,8 @@ public class CollectionRepositoryReplaceTests
         var user2 = await userRepository.CreateTestUserAsync();
         var orgUser2 = await organizationUserRepository.CreateTestOrganizationUserAsync(organization, user2);
 
-        var user3 = await userRepository.CreateTestUserAsync();
-        var orgUser3 = await organizationUserRepository.CreateTestOrganizationUserAsync(organization, user3);
-
         var group1 = await groupRepository.CreateTestGroupAsync(organization);
         var group2 = await groupRepository.CreateTestGroupAsync(organization);
-        var group3 = await groupRepository.CreateTestGroupAsync(organization);
 
         var collection = new Collection
         {
@@ -38,6 +34,7 @@ public class CollectionRepositoryReplaceTests
             OrganizationId = organization.Id,
         };
 
+        // Act
         await collectionRepository.CreateAsync(collection,
             [
                 new CollectionAccessSelection { Id = group1.Id, Manage = true, HidePasswords = true, ReadOnly = false, },
@@ -49,49 +46,27 @@ public class CollectionRepositoryReplaceTests
             ]
         );
 
-        // Act
-        collection.Name = "Updated Collection Name";
-
-        await collectionRepository.ReplaceAsync(collection,
-            [
-                // Delete group1
-                // Update group2:
-                new CollectionAccessSelection { Id = group2.Id, Manage = true, HidePasswords = true, ReadOnly = false, },
-                // Add group3:
-                new CollectionAccessSelection { Id = group3.Id, Manage = false, HidePasswords = false, ReadOnly = true, },
-            ],
-            [
-                // Delete orgUser1
-                // Update orgUser2:
-                new CollectionAccessSelection { Id = orgUser2.Id, Manage = false, HidePasswords = false, ReadOnly = true },
-                // Add orgUser3:
-                new CollectionAccessSelection { Id = orgUser3.Id, Manage = true, HidePasswords = false, ReadOnly = true },
-            ]
-        );
-
         // Assert
         var (actualCollection, actualAccess) = await collectionRepository.GetByIdWithAccessAsync(collection.Id);
 
         Assert.NotNull(actualCollection);
-        Assert.Equal("Updated Collection Name", actualCollection.Name);
+        Assert.Equal("Test Collection Name", actualCollection.Name);
 
         var groups = actualAccess.Groups.ToArray();
         Assert.Equal(2, groups.Length);
-        Assert.Single(groups, g => g.Id == group2.Id && g.Manage && g.HidePasswords && !g.ReadOnly);
-        Assert.Single(groups, g => g.Id == group3.Id && !g.Manage && !g.HidePasswords && g.ReadOnly);
+        Assert.Single(groups, g => g.Id == group1.Id && g.Manage && g.HidePasswords && !g.ReadOnly);
+        Assert.Single(groups, g => g.Id == group2.Id && !g.Manage && !g.HidePasswords && g.ReadOnly);
 
         var users = actualAccess.Users.ToArray();
-
         Assert.Equal(2, users.Length);
-        Assert.Single(users, u => u.Id == orgUser2.Id && !u.Manage && !u.HidePasswords && u.ReadOnly);
-        Assert.Single(users, u => u.Id == orgUser3.Id && u.Manage && !u.HidePasswords && u.ReadOnly);
+        Assert.Single(users, u => u.Id == orgUser1.Id && u.Manage && !u.HidePasswords && u.ReadOnly);
+        Assert.Single(users, u => u.Id == orgUser2.Id && !u.Manage && u.HidePasswords && !u.ReadOnly);
 
         // Clean up data
         await userRepository.DeleteAsync(user1);
         await userRepository.DeleteAsync(user2);
-        await userRepository.DeleteAsync(user3);
         await organizationRepository.DeleteAsync(organization);
-        await groupRepository.DeleteManyAsync([group1.Id, group2.Id, group3.Id]);
-        await organizationUserRepository.DeleteManyAsync([orgUser1.Id, orgUser2.Id, orgUser3.Id]);
+        await groupRepository.DeleteManyAsync([group1.Id, group2.Id]);
+        await organizationUserRepository.DeleteManyAsync([orgUser1.Id, orgUser2.Id]);
     }
 }
