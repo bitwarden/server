cf3e8aa3343484e2bbcda277807f5b847a18f39b
Brandon <btreston@bitwarden.com>
2025-05-14 10:58:40 -0400

    fix file names

--- util/Migrator/DbScripts/2025-05-13-01_AddUseOrganizationDomainsToOrganization.sql
+++
@@ -1,9 +0,0 @@
-/* update the new column to have the value used in UseSso to preserve existing orgs ability */
-
-UPDATE
-    [dbo].[Organization]
-SET
-    [UseOrganizationDomains] = [UseSso]
-WHERE
-    [UseSso] = 1
-GO
