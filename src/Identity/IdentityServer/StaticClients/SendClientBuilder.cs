ceb5d73305d7d24ea24237f9b935c7a0ae9b29f7
Jared Snider <jsnider@bitwarden.com>
2025-05-15 22:27:44 -0400

    PM-20532 - (1) Move ApiClient into new StaticClients folder (2) Create SendClientBuilder as don't need to use inheritance w/ client (3) Register new SendClient using builder in StaticClientStore (4) StaticClientStore - update name of clients list to not be ApiClients and instead just be Clients.

---
+++ src/Identity/IdentityServer/StaticClients/SendClientBuilder.cs
@@ -0,0 +1,31 @@
+﻿using Bit.Core.Enums;
+using Bit.Core.IdentityServer;
+using Bit.Core.Settings;
+using Bit.Identity.IdentityServer.RequestValidators;
+using Duende.IdentityServer.Models;
+
+namespace Bit.Identity.IdentityServer.StaticClients;
+public static class SendClientBuilder
+{
+    public static Client Build(GlobalSettings globalSettings)
+    {
+        return new Client()
+        {
+            ClientId = BitwardenClient.Send,
+            AllowedGrantTypes = new[] { SendAccessGrantValidator.GrantType },
+            AccessTokenLifetime = 60 * 5, // 5 minutes
+
+            // Do not allow refresh tokens to be issued.
+            AllowOfflineAccess = false,
+
+            // Send is a public anonymous client, so no secret is required (or really possible to use securely).
+            RequireClientSecret = false,
+
+            // Allow web vault to use this client.
+            AllowedCorsOrigins = new[] { globalSettings.BaseServiceUri.Vault },
+
+            // Setup API scopes that the client can request in the scope property of the token request.
+            AllowedScopes = new string[] { ApiScopes.Send },
+        };
+    }
+}
