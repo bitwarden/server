18e13fd4808841c128a66725ae43363e9fd6ef5c
Conner Turnbull <cturnbull@bitwarden.com>
2025-05-20 13:47:17 -0400

    Created LicenseIgnoreAttribute

---
+++ src/Core/Billing/Licenses/Attributes/LicenseIgnoreAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Bit.Core.Billing.Licenses.Attributes;
+
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+public class LicenseIgnoreAttribute(bool includeInHash = true) : Attribute
+{
+
+    /// <summary>
+    /// If true, the property will be included when computing the license hash, but ignored for other operations.
+    /// If false, the property will be completely ignored including for hash computation.
+    /// </summary>
+    public bool IncludeInHash { get; } = includeInHash;
+}
