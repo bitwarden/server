ffd5629766c57525ccafed6f33ebba37e1023a4c
✨ Audrey ✨ <audrey@audreyality.com>
2025-05-22 18:12:26 -0400

    introduce `SendAuthenticationQuery`

--- src/Sql/Tools/dbo/Stored Procedures/Send_Create.sql
+++ src/Sql/Tools/dbo/Stored Procedures/Send_Create.sql
@@ -6,6 +6,9 @@
     @Data VARCHAR(MAX),
     @Key VARCHAR(MAX),
     @Password NVARCHAR(300),
+--  FIXME: remove null default value once this argument has been
+--         in 2 server releases
+    @Emails NVARCHAR(1024) = NULL,
     @MaxAccessCount INT,
     @AccessCount INT,
     @CreationDate DATETIME2(7),
