30110212b2eea4837639f2d2f88c738fd626193c
Cy Okeke <cokeke@bitwarden.com>
2025-05-27 15:16:33 +0100

    resolve the stored procedure name

--- util/Migrator/DbScripts/2025-05-20_00_UpdateOrgUserReadOccupiedSeatCountForSponsorships.sql
+++ util/Migrator/DbScripts/2025-05-20_00_UpdateOrgUserReadOccupiedSeatCountForSponsorships.sql
@@ -1,10 +1,10 @@
-IF OBJECT_ID('[dbo].[OrganizationUser_ReadOccupiedSeatCountByOrganizationId]') IS NOT NULL
+IF OBJECT_ID('[dbo].[Organization_ReadOccupiedSeatCountByOrganizationId]') IS NOT NULL
 BEGIN
-    DROP PROCEDURE [dbo].[OrganizationUser_ReadOccupiedSeatCountByOrganizationId]
+    DROP PROCEDURE [dbo].[Organization_ReadOccupiedSeatCountByOrganizationId]
 END
 GO
 
-CREATE PROCEDURE [dbo].[OrganizationUser_ReadOccupiedSeatCountByOrganizationId]
+CREATE PROCEDURE [dbo].[Organization_ReadOccupiedSeatCountByOrganizationId]
     @OrganizationId UNIQUEIDENTIFIER
 AS
 BEGIN
@@ -39,34 +39,6 @@ BEGIN
                 -- ACCEPTED status: When SponsoredOrganizationId is not null and ValidUntil is null or in the future
                 (SponsoredOrganizationId IS NOT NULL AND (ValidUntil IS NULL OR ValidUntil > GETUTCDATE()))
             )
-        ) as Sponsored,
-        (
-            -- Count organization users
-            SELECT COUNT(1)
-            FROM [dbo].[OrganizationUserView]
-            WHERE OrganizationId = @OrganizationId
-            AND Status >= 0 --Invited
-        ) + 
-        (
-            -- Count admin-initiated sponsorships towards the seat count
-            SELECT COUNT(1)
-            FROM [dbo].[OrganizationSponsorship]
-            WHERE SponsoringOrganizationId = @OrganizationId
-            AND IsAdminInitiated = 1
-            AND (
-                -- Not marked for deletion - always count
-                (ToDelete = 0) 
-                OR
-                -- Marked for deletion but has a valid until date in the future (RevokeWhenExpired status)
-                (ToDelete = 1 AND ValidUntil IS NOT NULL AND ValidUntil > GETUTCDATE())
-            )
-            AND (
-                -- SENT status: When SponsoredOrganizationId is null
-                SponsoredOrganizationId IS NULL
-                OR
-                -- ACCEPTED status: When SponsoredOrganizationId is not null and ValidUntil is null or in the future
-                (SponsoredOrganizationId IS NOT NULL AND (ValidUntil IS NULL OR ValidUntil > GETUTCDATE()))
-            )
-        ) as Total
+        ) as Sponsored
 END
 GO
