022562eb6f3ce1bca226a0f7eed7d6882d6b05a7
Jared Snider <jsnider@bitwarden.com>
2025-05-29 12:24:37 -0400

    PM-20532 - Add TODO

--- src/Core/Auth/UserFeatures/PasswordValidation/PasswordValidationConstants.cs
+++ src/Core/Auth/UserFeatures/PasswordValidation/PasswordValidationConstants.cs
@@ -1,5 +1,6 @@
 ﻿namespace Bit.Core.Auth.PasswordValidation;
 
+// TODO: move this to KM
 public static class PasswordValidationConstants
 {
     public const int PasswordHasherKdfIterations = 100000;
