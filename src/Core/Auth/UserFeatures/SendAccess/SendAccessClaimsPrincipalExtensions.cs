0d3133233fdcb679da03e1c6002125322caa2dba
Jared Snider <jsnider@bitwarden.com>
2025-05-30 17:32:13 -0400

    PM-20532 - Add first draft of ClaimsPrincipal extension GetSendId for getting send id in send claim protected controller methods.

---
+++ src/Core/Auth/UserFeatures/SendAccess/SendAccessClaimsPrincipalExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Claims;
+using Bit.Core.Identity;
+
+namespace Bit.Core.Auth.UserFeatures.SendAccess;
+
+// TODO: test this with test file in Tests/Core/Auth/UserFeatures/SendAccess/SendAccessClaimsPrincipalExtensionsTests.cs
+public static class SendAccessClaimsPrincipalExtensions
+{
+    public static Guid GetSendId(this ClaimsPrincipal user)
+    {
+        ArgumentNullException.ThrowIfNull(user);
+
+        var sendIdClaim = user.FindFirst(Claims.SendId);
+        if (sendIdClaim == null) throw new InvalidOperationException("Send ID claim not found.");
+
+        if (!Guid.TryParse(sendIdClaim.Value, out var sendGuid))
+        {
+            throw new InvalidOperationException("Invalid Send ID claim value.");
+        }
+
+        return sendGuid;
+    }
+}
