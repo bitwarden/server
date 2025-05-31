d189f3334015c05ba2efe220af43b749693dfe96
Rui Tome <rtome@bitwarden.com>
2025-05-31 10:21:05 +0100

    Add stored procedure for optimized retrieval of organization user details
    - Created OrganizationUserUserDetails_ReadByOrganizationIdOptimized stored procedure to fetch user details, group associations, and collection associations in a single call.
    - Supports conditional retrieval based on parameters for improved performance and reduced database round trips.

---
+++ src/Sql/dbo/Stored Procedures/OrganizationUserUserDetails_ReadByOrganizationIdOptimized.sql
@@ -0,0 +1,30 @@
+﻿CREATE PROCEDURE [dbo].[OrganizationUserUserDetails_ReadByOrganizationIdOptimized]
+    @OrganizationId UNIQUEIDENTIFIER,
+    @IncludeGroups BIT,
+    @IncludeCollections BIT
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    -- First result set: User details
+    SELECT * FROM [dbo].[OrganizationUserUserDetailsView]
+    WHERE [OrganizationId] = @OrganizationId
+
+    -- Second result set: Group associations (if requested)
+    IF @IncludeGroups = 1
+    BEGIN
+        SELECT gu.*
+        FROM [dbo].[GroupUser] gu
+        INNER JOIN [dbo].[OrganizationUser] ou ON gu.OrganizationUserId = ou.Id
+        WHERE ou.OrganizationId = @OrganizationId
+    END
+
+    -- Third result set: Collection associations (if requested)  
+    IF @IncludeCollections = 1
+    BEGIN
+        SELECT cu.*
+        FROM [dbo].[CollectionUser] cu
+        INNER JOIN [dbo].[OrganizationUser] ou ON cu.OrganizationUserId = ou.Id
+        WHERE ou.OrganizationId = @OrganizationId
+    END
+END
