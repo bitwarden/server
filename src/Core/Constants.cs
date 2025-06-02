92f7a7794b5f6635e2972703d1c7cba3eeeb7c6b
Rui Tome <rtome@bitwarden.com>
2025-06-02 14:04:41 +0100

    Add new feature flag for Members Get Endpoint Optimization

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -111,6 +111,7 @@ public static class FeatureFlagKeys
     public const string EventBasedOrganizationIntegrations = "event-based-organization-integrations";
     public const string OptimizeNestedTraverseTypescript = "pm-21695-optimize-nested-traverse-typescript";
     public const string SeparateCustomRolePermissions = "pm-19917-separate-custom-role-permissions";
+    public const string MembersGetEndpointOptimization = "pm-21031-members-get-endpoint-optimization";
 
     /* Auth Team */
     public const string PM9112DeviceApprovalPersistence = "pm-9112-device-approval-persistence";
