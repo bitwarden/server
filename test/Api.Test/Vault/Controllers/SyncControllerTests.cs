477053f78b1e044592ce29b674293be579848fab
Bernd Schoolmann <mail@quexten.com>
2025-06-09 10:29:50 +0200

    Cleanup

--- test/Api.Test/Vault/Controllers/SyncControllerTests.cs
+++ test/Api.Test/Vault/Controllers/SyncControllerTests.cs
@@ -13,7 +13,6 @@ using Bit.Core.Auth.UserFeatures.TwoFactorAuth.Interfaces;
 using Bit.Core.Entities;
 using Bit.Core.Enums;
 using Bit.Core.Exceptions;
-using Bit.Core.KeyManagement.Models.Data;
 using Bit.Core.Models.Data;
 using Bit.Core.Models.Data.Organizations.OrganizationUsers;
 using Bit.Core.Repositories;
