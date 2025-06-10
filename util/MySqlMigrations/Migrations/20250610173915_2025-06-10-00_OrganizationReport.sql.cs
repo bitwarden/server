b354fea6b97fd29dc056aeba3301807fa02a69a9
Graham Walker <gwalker@bitwarden.com>
2025-06-10 12:40:27 -0500

    PM-20574 adding mysql migration

---
+++ util/MySqlMigrations/Migrations/20250610173915_2025-06-10-00_OrganizationReport.sql.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.MySqlMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class _2025061000_OrganizationReportsql : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "RiskInsightCriticalApplication");
+
+        migrationBuilder.DropTable(
+            name: "RiskInsightReport");
+
+        migrationBuilder.CreateTable(
+            name: "OrganizationApplication",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                OrganizationId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                Applications = table.Column<string>(type: "longtext", nullable: false)
+                    .Annotation("MySql:CharSet", "utf8mb4"),
+                CreationDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                RevisionDate = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_OrganizationApplication", x => x.Id);
+                table.ForeignKey(
+                    name: "FK_OrganizationApplication_Organization_OrganizationId",
+                    column: x => x.OrganizationId,
+                    principalTable: "Organization",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+            })
+            .Annotation("MySql:CharSet", "utf8mb4");
+
+        migrationBuilder.CreateTable(
+            name: "OrganizationReport",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                OrganizationId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                Date = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                ReportData = table.Column<string>(type: "longtext", nullable: false)
+                    .Annotation("MySql:CharSet", "utf8mb4"),
+                CreationDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                RevisionDate = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_OrganizationReport", x => x.Id);
+                table.ForeignKey(
+                    name: "FK_OrganizationReport_Organization_OrganizationId",
+                    column: x => x.OrganizationId,
+                    principalTable: "Organization",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+            })
+            .Annotation("MySql:CharSet", "utf8mb4");
+
+        migrationBuilder.CreateIndex(
+            name: "IX_OrganizationApplication_Id",
+            table: "OrganizationApplication",
+            column: "Id");
+
+        migrationBuilder.CreateIndex(
+            name: "IX_OrganizationApplication_OrganizationId",
+            table: "OrganizationApplication",
+            column: "OrganizationId");
+
+        migrationBuilder.CreateIndex(
+            name: "IX_OrganizationReport_Id",
+            table: "OrganizationReport",
+            column: "Id");
+
+        migrationBuilder.CreateIndex(
+            name: "IX_OrganizationReport_OrganizationId",
+            table: "OrganizationReport",
+            column: "OrganizationId");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "OrganizationApplication");
+
+        migrationBuilder.DropTable(
+            name: "OrganizationReport");
+
+        migrationBuilder.CreateTable(
+            name: "RiskInsightCriticalApplication",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                OrganizationId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                Applications = table.Column<string>(type: "longtext", nullable: false)
+                    .Annotation("MySql:CharSet", "utf8mb4"),
+                CreationDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                RevisionDate = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_RiskInsightCriticalApplication", x => x.Id);
+                table.ForeignKey(
+                    name: "FK_RiskInsightCriticalApplication_Organization_OrganizationId",
+                    column: x => x.OrganizationId,
+                    principalTable: "Organization",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+            })
+            .Annotation("MySql:CharSet", "utf8mb4");
+
+        migrationBuilder.CreateTable(
+            name: "RiskInsightReport",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                OrganizationId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                CreationDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                Date = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                ReportData = table.Column<string>(type: "longtext", nullable: false)
+                    .Annotation("MySql:CharSet", "utf8mb4"),
+                RevisionDate = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_RiskInsightReport", x => x.Id);
+                table.ForeignKey(
+                    name: "FK_RiskInsightReport_Organization_OrganizationId",
+                    column: x => x.OrganizationId,
+                    principalTable: "Organization",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+            })
+            .Annotation("MySql:CharSet", "utf8mb4");
+
+        migrationBuilder.CreateIndex(
+            name: "IX_RiskInsightCriticalApplication_Id",
+            table: "RiskInsightCriticalApplication",
+            column: "Id");
+
+        migrationBuilder.CreateIndex(
+            name: "IX_RiskInsightCriticalApplication_OrganizationId",
+            table: "RiskInsightCriticalApplication",
+            column: "OrganizationId");
+
+        migrationBuilder.CreateIndex(
+            name: "IX_RiskInsightReport_Id",
+            table: "RiskInsightReport",
+            column: "Id");
+
+        migrationBuilder.CreateIndex(
+            name: "IX_RiskInsightReport_OrganizationId",
+            table: "RiskInsightReport",
+            column: "OrganizationId");
+    }
+}
