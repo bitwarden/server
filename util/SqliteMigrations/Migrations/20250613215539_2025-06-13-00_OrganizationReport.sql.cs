7d2b12b715c2ee4a712d0bf5ae77c725bb73d7aa
Graham Walker <gwalker@bitwarden.com>
2025-06-13 16:59:20 -0500

    PM-20574 generating new migration scripts

---
+++ util/SqliteMigrations/Migrations/20250613215539_2025-06-13-00_OrganizationReport.sql.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.SqliteMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class _2025061300_OrganizationReportsql : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateTable(
+            name: "OrganizationApplication",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                OrganizationId = table.Column<Guid>(type: "TEXT", nullable: false),
+                Applications = table.Column<string>(type: "TEXT", nullable: false),
+                CreationDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                RevisionDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_OrganizationApplication", x => x.Id);
+                table.ForeignKey(
+                    name: "FK_OrganizationApplication_Organization_OrganizationId",
+                    column: x => x.OrganizationId,
+                    principalTable: "Organization",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+            });
+
+        migrationBuilder.CreateIndex(
+            name: "IX_OrganizationApplication_Id",
+            table: "OrganizationApplication",
+            column: "Id");
+
+        migrationBuilder.CreateIndex(
+            name: "IX_OrganizationApplication_OrganizationId",
+            table: "OrganizationApplication",
+            column: "OrganizationId");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "OrganizationApplication");
+    }
+}
