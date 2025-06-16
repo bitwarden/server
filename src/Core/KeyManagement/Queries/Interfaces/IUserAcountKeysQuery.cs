103e86f17c910751d5a5881d68ce025765529d7c
Bernd Schoolmann <mail@quexten.com>
2025-06-16 11:08:58 +0200

    Fix build

--- src/Core/KeyManagement/Queries/Interfaces/IUserAcountKeysQuery.cs
+++ src/Core/KeyManagement/Queries/Interfaces/IUserAcountKeysQuery.cs
@@ -1,7 +1,7 @@
 ﻿#nullable enable
 
 using Bit.Core.Entities;
-using Bit.Core.KeyManagement.Models.Data;
+using Bit.Core.KeyManagement.Models.Data.Models;
 
 namespace Bit.Api.KeyManagement.Queries.Interfaces;
 
