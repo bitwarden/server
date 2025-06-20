550bbfb3c5cd55993e9b3950ae8f7276dfa0b492
Brant DeBow <brant@livefront.com>
2025-06-20 16:20:23 -0400

    Reorder Filters to the bottom of OrganizationIntegrationConfiguration

--- src/Sql/dbo/Tables/OrganizationIntegrationConfiguration.sql
+++ src/Sql/dbo/Tables/OrganizationIntegrationConfiguration.sql
@@ -4,10 +4,10 @@ CREATE TABLE [dbo].[OrganizationIntegrationConfiguration]
     [OrganizationIntegrationId] UNIQUEIDENTIFIER NOT NULL,
     [EventType] SMALLINT NOT NULL,
     [Configuration] VARCHAR (MAX) NULL,
-    [Filters] VARCHAR (MAX) NULL,
     [Template] VARCHAR (MAX) NULL,
     [CreationDate] DATETIME2 (7) NOT NULL,
     [RevisionDate] DATETIME2 (7) NOT NULL,
+    [Filters] VARCHAR (MAX) NULL,
     CONSTRAINT [PK_OrganizationIntegrationConfiguration] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_OrganizationIntegrationConfiguration_OrganizationIntegration] FOREIGN KEY ([OrganizationIntegrationId]) REFERENCES [dbo].[OrganizationIntegration] ([Id]) ON DELETE CASCADE
 );
