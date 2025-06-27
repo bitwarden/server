ba0a19ed1832f220673ea26125ec38ed04ab1194
Nick Krantz <nick@livefront.com>
2025-06-27 10:13:48 -0500

    add check for foreign key

--- util/Migrator/DbScripts/2025-06-26_00_CascadeDeleteNotificationStatus.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE [dbo].[NotificationStatus]
-DROP CONSTRAINT [FK_NotificationStatus_Notification];
-
-ALTER TABLE [dbo].[NotificationStatus]
-ADD CONSTRAINT [FK_NotificationStatus_Notification]
-    FOREIGN KEY ([NotificationId]) REFERENCES [dbo].[Notification]([Id]) ON DELETE CASCADE;
