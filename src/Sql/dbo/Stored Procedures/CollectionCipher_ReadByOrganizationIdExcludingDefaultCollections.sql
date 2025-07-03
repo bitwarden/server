32e4bc0a94dec49f50a5a57e3f3ec5a42930899d
jaasen-livefront <jaasen@livefront.com>
2025-07-03 09:52:59 -0700

    remove joins from proc

--- src/Sql/dbo/Stored Procedures/CollectionCipher_ReadByOrganizationIdExcludingDefaultCollections.sql
+++ src/Sql/dbo/Stored Procedures/CollectionCipher_ReadByOrganizationIdExcludingDefaultCollections.sql
@@ -1,3 +1,4 @@
+ -- Stored procedure that filters out ciphers that ONLY belong to default collections
 CREATE PROCEDURE [dbo].[CipherOrganizationDetails_ReadByOrganizationIdExcludingDefaultCollections]
     @OrganizationId UNIQUEIDENTIFIER
 AS
@@ -5,30 +6,22 @@ BEGIN
     SET NOCOUNT ON;
 
     SELECT
-        C.[Id],
-        C.[UserId],
-        C.[OrganizationId],
-        C.[Type],
-        C.[Data],
-        C.[Attachments],
-        C.[CreationDate],
-        C.[RevisionDate],
-        C.[DeletedDate],
-        C.[Reprompt],
-        C.[Key],
-        CASE WHEN O.[UseTotp] = 1 THEN 1 ELSE 0 END AS [OrganizationUseTotp],
-        CC.[CollectionId]
-    FROM [dbo].[Cipher] C
-    INNER JOIN [dbo].[Organization] O
-        ON C.[OrganizationId] = O.[Id]
-    LEFT JOIN [dbo].[CollectionCipher] CC
-        ON CC.[CipherId] = C.[Id]
-    LEFT JOIN [dbo].[Collection] COL
-        ON CC.[CollectionId] = COL.[Id]
-    WHERE
-        C.[UserId]       IS NULL                              -- only org-owned ciphers
-        AND C.[OrganizationId] = @OrganizationId
-        AND (CC.[CollectionId] IS NULL                        -- ciphers with no collections
-             OR COL.[Type]       <> 1);                       -- or non-default collections
-END
+        Id,
+        UserId,
+        OrganizationId,
+        [Type],
+        [Data],
+        [Attachments],
+        CreationDate,
+        RevisionDate,
+        DeletedDate,
+        Reprompt,
+        [Key],
+        OrganizationUseTotp,
+        CollectionId
+    FROM dbo.OrganizationCipherDetailsWithCollectionsView
+    WHERE OrganizationId = @OrganizationId
+      AND (CollectionId IS NULL       -- no collections
+           OR CollectionType <> 1);  -- or at least one non-default
+END;
 GO
