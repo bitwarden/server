b4b294cfd84c87a272ace335e2670253687b1ea5
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2025-07-08 09:38:09 -0400

    Add `#nullable disable` to billing code

--- src/Billing/Controllers/PayPalController.cs
+++ src/Billing/Controllers/PayPalController.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿// FIXME: Update this file to be null safe and then delete the line below
+#nullable disable
+
+using System.Text;
 using Bit.Billing.Models;
 using Bit.Core.AdminConsole.Repositories;
 using Bit.Core.Billing.Services;
