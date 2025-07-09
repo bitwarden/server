fb502440722c0080d04f993a278e487cbb3fd6af
Ike Kottlowski <ikottlowski@bitwarden.com>
2025-07-09 10:10:47 -0400

    fix: change constructor to make DI more ergonomic.

---
+++ src/Core/Auth/Identity/TokenProviders/OtpTokenProvider/IOtpTokenProvider.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+
+namespace Core.Auth.Identity.TokenProviders;
+
+/// <summary>
+/// A generic interface for a one-time password (OTP) token provider.
+/// </summary>
+public interface IOtpTokenProvider
+{
+    /// <summary>
+    /// Generates a new one-time password (OTP) based on the configured parameters.
+    /// The generated OTP is stored in the distributed cache with a key based on the unique identifier and purpose. If the
+    /// key is already in use, it will overwrite and generate a new OTP with a refreshed TTL.
+    /// </summary>
+    /// <param name="purpose">Purpose of the OTP token, used to distinguish different types of tokens.</param>
+    /// <param name="uniqueIdentifier">Unique identifier to distinguish one request from another</param>
+    /// <returns>generated token</returns>
+    Task<string> GenerateTokenAsync(string purpose, string uniqueIdentifier);
+
+    /// <summary>
+    /// Validates the provided token against the stored value in the distributed cache.
+    /// </summary>
+    /// <param name="token">string value matched against the unique identifier in the cache if found</param>
+    /// <param name="purpose">Purpose of the OTP token, used to distinguish different types of tokens.</param>
+    /// <param name="uniqueIdentifier">Unique identifier to distinguish one request from another</param>
+    /// <returns>true if the token matches what is fetched from the cache, false if not.</returns>
+    Task<bool> ValidateTokenAsync(string token, string purpose, string uniqueIdentifier);
+
+    /// <summary>
+    /// Configures the token provider with the specified parameters.
+    /// This method allows customization of the token length and character set.
+    /// </summary>
+    /// <param name="length">The length of the generated token.</param>
+    /// <param name="alpha">Whether the token should contain alphabetic characters.</param>
+    /// <param name="numeric">Whether the token should contain numeric characters.</param>
+    void ConfigureToken(int length, bool alpha, bool numeric);
+
+    /// <summary>
+    /// Sets the cache entry options for the token provider.
+    /// This method allows customization of the cache entry options, such as expiration time.
+    /// </summary>
+    /// <param name="options">The cache entry options to set.</param>
+    void SetCacheEntryOptions(DistributedCacheEntryOptions options);
+}
---
+++ src/Core/Auth/Identity/TokenProviders/OtpTokenProvider/OtpTokenProvider.cs
@@ -0,0 +1,95 @@
+﻿using System.Text;
+using Bit.Core.Utilities;
+using Core.Auth.Identity.TokenProviders;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bit.Core.Auth.Identity.TokenProviders;
+
+public class OtpTokenProvider(
+    [FromKeyedServices("persistent")]
+    IDistributedCache distributedCache) : IOtpTokenProvider
+{
+    /// <summary>
+    /// This is where the OTP tokens are stored.
+    /// </summary>
+    private readonly IDistributedCache _distributedCache = distributedCache;
+
+    /// <summary>
+    /// Used to store and fetch the OTP tokens from the distributed cache.
+    /// The format is "{purpose}_{uniqueIdentifier}".
+    /// </summary>
+    private readonly string _cacheKeyFormat = "{0}_{1}";
+
+    /// <summary>
+    /// Sets the cache entry options for the token provider.
+    /// Default is 5 minutes expiration.
+    /// </summary>
+    public DistributedCacheEntryOptions _distributedCacheEntryOptions { get; protected set; } =
+        new DistributedCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5)
+        };
+
+    /// <summary>
+    /// Sets the length of the token to be generated. Default is 6 characters.
+    /// </summary>
+    public int TokenLength { get; protected set; } = 6;
+    /// <summary>
+    /// Sets whether the token should contain alphabetic characters. Default is false.
+    /// </summary>
+    public bool TokenAlpha { get; protected set; } = false;
+    /// <summary>
+    /// Sets whether the token should contain numeric characters. Default is true.
+    /// </summary>
+    public bool TokenNumeric { get; protected set; } = true;
+
+    public async Task<string> GenerateTokenAsync(string purpose, string uniqueIdentifier)
+    {
+        if (string.IsNullOrEmpty(purpose) || string.IsNullOrEmpty(uniqueIdentifier))
+        {
+            return null;
+        }
+
+        var cacheKey = string.Format(_cacheKeyFormat, purpose, uniqueIdentifier);
+        var token = CoreHelpers.SecureRandomString(TokenLength, TokenAlpha, true, false, TokenNumeric, false);
+        await _distributedCache.SetAsync(cacheKey, Encoding.UTF8.GetBytes(token), _distributedCacheEntryOptions);
+        return token;
+    }
+
+    public async Task<bool> ValidateTokenAsync(string token, string purpose, string uniqueIdentifier)
+    {
+        if (string.IsNullOrEmpty(token) || string.IsNullOrEmpty(purpose) || string.IsNullOrEmpty(uniqueIdentifier))
+        {
+            return false;
+        }
+
+        var cacheKey = string.Format(_cacheKeyFormat, purpose, uniqueIdentifier);
+        var cachedValue = await _distributedCache.GetAsync(cacheKey);
+        if (cachedValue == null)
+        {
+            return false;
+        }
+
+        var code = Encoding.UTF8.GetString(cachedValue);
+        var valid = string.Equals(token, code);
+        if (valid)
+        {
+            await _distributedCache.RemoveAsync(cacheKey);
+        }
+
+        return valid;
+    }
+
+    public void ConfigureToken(int length, bool alpha, bool numeric)
+    {
+        TokenLength = length;
+        TokenAlpha = alpha;
+        TokenNumeric = numeric;
+    }
+
+    public void SetCacheEntryOptions(DistributedCacheEntryOptions options)
+    {
+        _distributedCacheEntryOptions = options;
+    }
+}
