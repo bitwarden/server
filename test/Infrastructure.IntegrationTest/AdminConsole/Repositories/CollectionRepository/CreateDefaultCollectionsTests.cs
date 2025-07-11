a3ac6fd4b952de2f8136bf087f55eb2e9360a9c5
Jimmy Vo <huynhmaivo82@gmail.com>
2025-07-11 18:30:21 -0400

    [PM-22102] Add database integration tests

--- test/Infrastructure.IntegrationTest/AdminConsole/Repositories/CollectionRepository/CreateDefaultCollectionsTests.cs
+++ test/Infrastructure.IntegrationTest/AdminConsole/Repositories/CollectionRepository/CreateDefaultCollectionsTests.cs
@@ -1,102 +1,132 @@
-﻿using Bit.Core.Repositories;
+﻿using Bit.Core.AdminConsole.Entities;
+using Bit.Core.Billing.Enums;
+using Bit.Core.Entities;
+using Bit.Core.Enums;
+using Bit.Core.Repositories;
+using Xunit;
 
 
 namespace Bit.Infrastructure.IntegrationTest.AdminConsole.Repositories.CollectionRepository;
 
 public class CreateDefaultCollectionsTests
 {
+
     [DatabaseTheory, DatabaseData]
-    public async Task SQL_Test(
-        // IOrganizationRepository organizationRepository,
-        // IUserRepository userRepository,
-        // IOrganizationUserRepository organizationUserRepository,
+    public async Task CreateDefaultCollectionsAsync_ShouldCreateDefaultCollection_WhenUsersDoNotHaveDefaultCollection(
+        IOrganizationRepository organizationRepository,
+        IUserRepository userRepository,
+        IOrganizationUserRepository organizationUserRepository,
         ICollectionRepository collectionRepository)
     {
-        //
-        // var user = await userRepository.CreateAsync(new User
-        // {
-        //     Name = "Test User",
-        //     Email = $"test+{Guid.NewGuid()}@email.com",
-        //     ApiKey = "TEST",
-        //     SecurityStamp = "stamp",
-        // });
-        //
-        // var organization = await organizationRepository.CreateAsync(new Organization
-        // {
-        //     Name = "Test Org",
-        //     PlanType = PlanType.EnterpriseAnnually,
-        //     Plan = "Test Plan",
-        //     BillingEmail = "billing@email.com"
-        // });
-        //
-        // var orgUser = await organizationUserRepository.CreateAsync(new OrganizationUser
-        // {
-        //     OrganizationId = organization.Id,
-        //     UserId = user.Id,
-        //     Status = OrganizationUserStatusType.Confirmed,
-        // });
+        // Arrange
+        var organizationId = Guid.NewGuid();
 
+        var organization = await organizationRepository.CreateAsync(new Organization
+        {
+            Id = organizationId,
+            Name = "Test name",
+            PlanType = PlanType.EnterpriseAnnually,
+            Plan = "Test",
+            BillingEmail = "billing@example.com"
+        });
+
+        var resulOrganizationUsers = await Task.WhenAll(
+            CreateUserForOrgAsync(userRepository, organizationUserRepository, organization),
+            CreateUserForOrgAsync(userRepository, organizationUserRepository, organization)
+            );
+
+
+        var affectedOrgUserIds = resulOrganizationUsers.Select(organizationUser => organizationUser.Id);
+        var defaultCollectionName = $"default-name-{organizationId}";
 
         // Act
-        var organizationId = Guid.Parse("75c909de-2624-461d-acd9-b2ff00e1c9aa");
-
-        var useridEmailOrgUserId = Guid.Parse("676931B3-5479-403A-AFF1-B30D014F2A26");
-        var useridUseridOrgUserId = Guid.Parse("D7AAC6DE-6958-4DF6-B22A-B30D0154D878");
-        var defaultCollectionName = "default name";
-
-        var affectedOrgUserIds = new[] { useridEmailOrgUserId, useridUseridOrgUserId };
-
-
         await collectionRepository.CreateDefaultCollectionsAsync(organizationId, affectedOrgUserIds, defaultCollectionName);
 
-
+        // Assert
+        await AssertAllUsersHaveOneDefaultCollectionAsync(collectionRepository, resulOrganizationUsers, organizationId);
     }
 
+
     [DatabaseTheory, DatabaseData]
-    public async Task EF_Test(
-        // IOrganizationRepository organizationRepository,
-        // IUserRepository userRepository,
-        // IOrganizationUserRepository organizationUserRepository,
+    public async Task CreateDefaultCollectionsAsync_ShouldNotCreateDefaultCollection_WhenUsersAlreadyHaveOne(
+        IOrganizationRepository organizationRepository,
+        IUserRepository userRepository,
+        IOrganizationUserRepository organizationUserRepository,
         ICollectionRepository collectionRepository)
     {
-        //
-        // var user = await userRepository.CreateAsync(new User
-        // {
-        //     Name = "Test User",
-        //     Email = $"test+{Guid.NewGuid()}@email.com",
-        //     ApiKey = "TEST",
-        //     SecurityStamp = "stamp",
-        // });
-        //
-        // var organization = await organizationRepository.CreateAsync(new Organization
-        // {
-        //     Name = "Test Org",
-        //     PlanType = PlanType.EnterpriseAnnually,
-        //     Plan = "Test Plan",
-        //     BillingEmail = "billing@email.com"
-        // });
-        //
-        // var orgUser = await organizationUserRepository.CreateAsync(new OrganizationUser
-        // {
-        //     OrganizationId = organization.Id,
-        //     UserId = user.Id,
-        //     Status = OrganizationUserStatusType.Confirmed,
-        // });
+        // Arrange
+        var organizationId = Guid.NewGuid();
 
+        var organization = await organizationRepository.CreateAsync(new Organization
+        {
+            Id = organizationId,
+            Name = "Test name",
+            PlanType = PlanType.EnterpriseAnnually,
+            Plan = "Test",
+            BillingEmail = "billing@example.com"
+        });
+
+        var resulOrganizationUsers = await Task.WhenAll(
+            CreateUserForOrgAsync(userRepository, organizationUserRepository, organization),
+            CreateUserForOrgAsync(userRepository, organizationUserRepository, organization)
+        );
+
+        var affectedOrgUserIds = resulOrganizationUsers.Select(organizationUser => organizationUser.Id);
+        var defaultCollectionName = $"default-name-{organizationId}";
+
+
+        await CreateUsersWithExistingDefaultCollections(collectionRepository, organizationId, affectedOrgUserIds, defaultCollectionName, resulOrganizationUsers);
 
         // Act
-        var organizationId = Guid.Parse("7cbb0a89-f80d-4ce7-8eb5-b2ff00e1c9a8");
-
-        var user1OrgUserId = Guid.Parse("75c909de-2624-461d-acd9-b2ff00e1c9aa");
-        var user2OrgUserId = Guid.Parse("c76d2372-c449-4abf-9c89-b26801573811");
-        var defaultCollectionName = "default name";
-
-        var affectedOrgUserIds = new[] { user1OrgUserId, user2OrgUserId };
-
-
         await collectionRepository.CreateDefaultCollectionsAsync(organizationId, affectedOrgUserIds, defaultCollectionName);
 
+        // Assert
+        await AssertAllUsersHaveOneDefaultCollectionAsync(collectionRepository, resulOrganizationUsers, organizationId);
+    }
 
+    private static async Task CreateUsersWithExistingDefaultCollections(ICollectionRepository collectionRepository,
+        Guid organizationId, IEnumerable<Guid> affectedOrgUserIds, string defaultCollectionName,
+        OrganizationUser[] resulOrganizationUsers)
+    {
+        await collectionRepository.CreateDefaultCollectionsAsync(organizationId, affectedOrgUserIds, defaultCollectionName);
+
+        await AssertAllUsersHaveOneDefaultCollectionAsync(collectionRepository, resulOrganizationUsers, organizationId);
+    }
+
+    private static async Task AssertAllUsersHaveOneDefaultCollectionAsync(ICollectionRepository collectionRepository,
+        OrganizationUser[] resulOrganizationUsers, Guid organizationId)
+    {
+        foreach (var resulOrganizationUser in resulOrganizationUsers)
+        {
+            var collectionDetails = await collectionRepository.GetManyByUserIdAsync(resulOrganizationUser!.UserId.Value);
+            var defaultCollection = collectionDetails
+                .SingleOrDefault(collectionDetail =>
+                    collectionDetail.OrganizationId == organizationId
+                    && collectionDetail.Type == CollectionType.DefaultUserCollection);
+
+            Assert.NotNull(defaultCollection);
+        }
+    }
+
+    private static async Task<OrganizationUser> CreateUserForOrgAsync(IUserRepository userRepository,
+        IOrganizationUserRepository organizationUserRepository, Organization organization)
+    {
+        var user = await userRepository.CreateAsync(new User
+        {
+            Name = "Test User",
+            Email = $"test+{Guid.NewGuid()}@example.com",
+            ApiKey = "TEST",
+            SecurityStamp = "stamp",
+        });
+
+        var orgUser = await organizationUserRepository.CreateAsync(new OrganizationUser
+        {
+            OrganizationId = organization.Id,
+            UserId = user.Id,
+            Status = OrganizationUserStatusType.Confirmed,
+        });
+
+        return orgUser;
     }
 
     // Jimmy TODO: make sure to add the clean up data for the tests.
