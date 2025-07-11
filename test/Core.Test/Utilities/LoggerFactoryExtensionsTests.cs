4a80ed323a3d26b168158b3df0487fdc9fe553c1
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2025-07-11 17:05:54 -0400

    Do some private reflection

--- test/Core.Test/Utilities/LoggerFactoryExtensionsTests.cs
+++ test/Core.Test/Utilities/LoggerFactoryExtensionsTests.cs
@@ -1,9 +1,11 @@
+using System.Reflection;
 using Bit.Core.Utilities;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
+using Serilog.Core;
 using Serilog.Extensions.Logging;
 using Xunit;
 
@@ -29,7 +31,20 @@ public class LoggerFactoryExtensionsTests
 
         var provider = Assert.Single(providers);
         var serilog = Assert.IsAssignableFrom<SerilogLoggerProvider>(provider);
-        
+        var details = GetSerilogDetails(serilog);
+    }
+
+    private Logger GetSerilogDetails(SerilogLoggerProvider provider)
+    {
+        var fieldInfo = typeof(SerilogLoggerProvider).GetField("_logger", BindingFlags.Instance | BindingFlags.NonPublic)
+            ?? throw new Exception("We tried to private reflection to get the serilog logger but it's not there. It may have moved.");
+
+        if (fieldInfo.GetValue(provider) is not Serilog.Core.Logger logger)
+        {
+            throw new Exception("The private field _logger was there but it's either null or not assignable to Serilog.Core.Logger");
+        }
+
+        return logger;
     }
 
     private static IEnumerable<ILoggerProvider> GetProviders(Dictionary<string, string?> initialData, string environment = "Production")
