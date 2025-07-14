d914ab8a988b3aee0113f5afbb70ac36675b7d72
Alex Morask <144709477+amorask-bitwarden@users.noreply.github.com>
2025-07-14 12:39:49 -0500

    [PM-23687] Support free organizations on Payment Details page (#6084)
    * Resolve JSON serialization bug in OneOf converters and organize pricing models
    
    * Support free organizations for payment method and billing address flows
    
    * Run dotnet format

--- test/Core.Test/Billing/Payment/Commands/UpdateBillingAddressCommandTests.cs
+++ test/Core.Test/Billing/Payment/Commands/UpdateBillingAddressCommandTests.cs
@@ -3,6 +3,7 @@ using Bit.Core.Billing.Constants;
 using Bit.Core.Billing.Enums;
 using Bit.Core.Billing.Payment.Commands;
 using Bit.Core.Billing.Payment.Models;
+using Bit.Core.Billing.Services;
 using Bit.Core.Services;
 using Bit.Core.Test.Billing.Extensions;
 using Microsoft.Extensions.Logging;
@@ -16,14 +17,15 @@ using static StripeConstants;
 
 public class UpdateBillingAddressCommandTests
 {
-    private readonly IStripeAdapter _stripeAdapter;
+    private readonly ISubscriberService _subscriberService = Substitute.For<ISubscriberService>();
+    private readonly IStripeAdapter _stripeAdapter = Substitute.For<IStripeAdapter>();
     private readonly UpdateBillingAddressCommand _command;
 
     public UpdateBillingAddressCommandTests()
     {
-        _stripeAdapter = Substitute.For<IStripeAdapter>();
         _command = new UpdateBillingAddressCommand(
             Substitute.For<ILogger<UpdateBillingAddressCommand>>(),
+            _subscriberService,
             _stripeAdapter);
     }
 
@@ -81,6 +83,66 @@ public class UpdateBillingAddressCommandTests
         Assert.True(result.IsT0);
         var output = result.AsT0;
         Assert.Equivalent(input, output);
+
+        await _stripeAdapter.Received(1).SubscriptionUpdateAsync(organization.GatewaySubscriptionId,
+            Arg.Is<SubscriptionUpdateOptions>(options => options.AutomaticTax.Enabled == true));
+    }
+
+    [Fact]
+    public async Task Run_PersonalOrganization_NoCurrentCustomer_MakesCorrectInvocations_ReturnsBillingAddress()
+    {
+        var organization = new Organization
+        {
+            PlanType = PlanType.FamiliesAnnually,
+            GatewaySubscriptionId = "sub_123"
+        };
+
+        var input = new BillingAddress
+        {
+            Country = "US",
+            PostalCode = "12345",
+            Line1 = "123 Main St.",
+            Line2 = "Suite 100",
+            City = "New York",
+            State = "NY"
+        };
+
+        var customer = new Customer
+        {
+            Address = new Address
+            {
+                Country = "US",
+                PostalCode = "12345",
+                Line1 = "123 Main St.",
+                Line2 = "Suite 100",
+                City = "New York",
+                State = "NY"
+            },
+            Subscriptions = new StripeList<Subscription>
+            {
+                Data =
+                [
+                    new Subscription
+                    {
+                        Id = organization.GatewaySubscriptionId,
+                        AutomaticTax = new SubscriptionAutomaticTax { Enabled = false }
+                    }
+                ]
+            }
+        };
+
+        _stripeAdapter.CustomerUpdateAsync(organization.GatewayCustomerId, Arg.Is<CustomerUpdateOptions>(options =>
+            options.Address.Matches(input) &&
+            options.HasExpansions("subscriptions")
+        )).Returns(customer);
+
+        var result = await _command.Run(organization, input);
+
+        Assert.True(result.IsT0);
+        var output = result.AsT0;
+        Assert.Equivalent(input, output);
+
+        await _subscriberService.Received(1).CreateStripeCustomer(organization);
 
         await _stripeAdapter.Received(1).SubscriptionUpdateAsync(organization.GatewaySubscriptionId,
             Arg.Is<SubscriptionUpdateOptions>(options => options.AutomaticTax.Enabled == true));
