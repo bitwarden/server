c70e89519e983480e0b9b46ab8f3222b4f3fa506
Jimmy Vo <huynhmaivo82@gmail.com>
2025-07-15 10:28:38 -0400

    [PM-22108] WIP

--- test/Infrastructure.IntegrationTest/AdminConsole/Repositories/PolicyRepository/PolicyDetailsReadByOrganizationIdAsyncTests.cs
+++ test/Infrastructure.IntegrationTest/AdminConsole/Repositories/PolicyRepository/PolicyDetailsReadByOrganizationIdAsyncTests.cs
@@ -50,35 +50,6 @@ public class PolicyDetailsReadByOrganizationIdAsyncTests
 
     }
 
-    [DatabaseTheory, DatabaseData]
-    public async Task PolicyDetailsReadByOrganizationIdAsync_OnlyReturnInputPolicyType(
-        IUserRepository userRepository,
-        IOrganizationUserRepository organizationUserRepository,
-        IOrganizationRepository organizationRepository,
-        IPolicyRepository policyRepository)
-    {
-        // Arrange
-        var userA = await userRepository.CreateTestUserAsync();
-        var org1 = await CreateEnterpriseOrg(organizationRepository);
-
-        var orgUser1 = await organizationUserRepository.CreateTestOrganizationUserAsync(org1, userA);
-
-        const PolicyType inputPolicyType = PolicyType.SingleOrg;
-        await policyRepository.CreateAsync(new Policy { OrganizationId = org1.Id, Enabled = true, Type = inputPolicyType });
-
-        const PolicyType notInputPolicyType = PolicyType.RequireSso;
-        await policyRepository.CreateAsync(new Policy { OrganizationId = org1.Id, Enabled = true, Type = notInputPolicyType });
-
-        // Act
-        var results = (await policyRepository.PolicyDetailsReadByOrganizationIdAsync(org1.Id, inputPolicyType)).ToList();
-
-        // Assert
-        Assert.Contains(results, result => result.OrganizationUserId == orgUser1.Id
-                                      && result.OrganizationId == orgUser1.OrganizationId
-                                      && result.PolicyType == inputPolicyType);
-
-        Assert.DoesNotContain(results, result => result.PolicyType == notInputPolicyType);
-    }
 
 
     [DatabaseTheory, DatabaseData]
@@ -110,7 +81,7 @@ public class PolicyDetailsReadByOrganizationIdAsyncTests
     }
 
     [DatabaseTheory, DatabaseData]
-    public async Task JimmyDone_ShouldNotReturnUserOtherOrgsIfNoUserIsConnected(
+    public async Task ShouldNotReturnOtherOrgUsers_WhenUserIsNotConnected(
         IUserRepository userRepository,
         IOrganizationUserRepository organizationUserRepository,
         IOrganizationRepository organizationRepository,
@@ -137,91 +108,33 @@ public class PolicyDetailsReadByOrganizationIdAsyncTests
 
     }
 
-
-
     [DatabaseTheory, DatabaseData]
-    public async Task EmailToUserId_UseCase2_ReturnsOrg4Policy(
+    public async Task ShouldOnlyReturnInputPolicyType(
         IUserRepository userRepository,
         IOrganizationUserRepository organizationUserRepository,
         IOrganizationRepository organizationRepository,
         IPolicyRepository policyRepository)
     {
         // Arrange
-        var userC = await userRepository.CreateTestUserAsync();
-        var org1 = await CreateEnterpriseOrg(organizationRepository);
-        // invited OrgUser in Org1 by Email
-        var orgUser1 = new OrganizationUser
-        {
-            OrganizationId = org1.Id,
-            UserId = null,
-            Status = OrganizationUserStatusType.Invited,
-            Type = OrganizationUserType.Custom,
-            Email = userC.Email
-        };
-        await organizationUserRepository.CreateAsync(orgUser1);
-        await policyRepository.CreateAsync(new Policy { OrganizationId = org1.Id, Enabled = true, Type = PolicyType.SingleOrg });
+        var user = await userRepository.CreateTestUserAsync();
 
-        // Org4 via Email → UserId
-        var org4 = await CreateEnterpriseOrg(organizationRepository);
-        var orgUser4 = new OrganizationUser
-        {
-            OrganizationId = org4.Id,
-            UserId = userC.Id,
-            Status = OrganizationUserStatusType.Confirmed,
-            Type = OrganizationUserType.Custom,
-            Email = null
-        };
-        await organizationUserRepository.CreateAsync(orgUser4);
-        await policyRepository.CreateAsync(new Policy { OrganizationId = org4.Id, Enabled = true, Type = PolicyType.SingleOrg });
+        const PolicyType inputPolicyType = PolicyType.SingleOrg;
+        var orgUser = await ArrangeDirectlyConnectedOrgByUserIdAsync(organizationUserRepository, organizationRepository, policyRepository, user, inputPolicyType);
+
+        const PolicyType notInputPolicyType = PolicyType.RequireSso;
+        await policyRepository.CreateAsync(new Policy { OrganizationId = orgUser.OrganizationId, Enabled = true, Type = notInputPolicyType });
 
         // Act
-        var results = await policyRepository.PolicyDetailsReadByOrganizationIdAsync(org1.Id, PolicyType.SingleOrg);
+        var results = (await policyRepository.PolicyDetailsReadByOrganizationIdAsync(orgUser.OrganizationId, inputPolicyType)).ToList();
 
         // Assert
-        Assert.Contains(results, result => result.OrganizationUserId == orgUser4.Id && result.OrganizationId == org4.Id);
+        Assert.Contains(results, result => result.OrganizationUserId == orgUser.Id
+                                           && result.OrganizationId == orgUser.OrganizationId
+                                           && result.PolicyType == inputPolicyType);
+
+        Assert.DoesNotContain(results, result => result.PolicyType == notInputPolicyType);
     }
 
-    [DatabaseTheory, DatabaseData]
-    public async Task UserIdToEmail_UseCase3_ReturnsOrg3Policy(
-        IUserRepository userRepository,
-        IOrganizationUserRepository organizationUserRepository,
-        IOrganizationRepository organizationRepository,
-        IPolicyRepository policyRepository)
-    {
-        // Arrange
-        var userB = await userRepository.CreateTestUserAsync();
-        var org1 = await CreateEnterpriseOrg(organizationRepository);
-        // direct OrgUser in Org1
-        var orgUser1 = new OrganizationUser
-        {
-            OrganizationId = org1.Id,
-            UserId = userB.Id,
-            Status = OrganizationUserStatusType.Confirmed,
-            Type = OrganizationUserType.Custom,
-            Email = null
-        };
-        await organizationUserRepository.CreateAsync(orgUser1);
-        await policyRepository.CreateAsync(new Policy { OrganizationId = org1.Id, Enabled = true, Type = PolicyType.SingleOrg });
-
-        // Org3 via UserId → Email
-        var org3 = await CreateEnterpriseOrg(organizationRepository);
-        var orgUser3 = new OrganizationUser
-        {
-            OrganizationId = org3.Id,
-            UserId = null,
-            Status = OrganizationUserStatusType.Invited,
-            Type = OrganizationUserType.Custom,
-            Email = userB.Email
-        };
-        await organizationUserRepository.CreateAsync(orgUser3);
-        await policyRepository.CreateAsync(new Policy { OrganizationId = org3.Id, Enabled = true, Type = PolicyType.SingleOrg });
-
-        // Act
-        var results = await policyRepository.PolicyDetailsReadByOrganizationIdAsync(org1.Id, PolicyType.SingleOrg);
-
-        // Assert
-        Assert.Contains(results, result => result.OrganizationUserId == orgUser3.Id && result.OrganizationId == org3.Id);
-    }
 
     [DatabaseTheory, DatabaseData]
     public async Task WhenDirectlyConnectedUserHasUserId_ShouldReturnOtherConnectedOrganizationPolicies(
