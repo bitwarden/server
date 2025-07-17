87667c7c2961da38256b6aadb69d5f736039f097
Bernd Schoolmann <mail@quexten.com>
2025-07-17 13:59:28 +0200

    Cleanup

--- src/Core/KeyManagement/Models/Data/SecurityStateData.cs
+++ src/Core/KeyManagement/Models/Data/SecurityStateData.cs
@@ -7,5 +7,5 @@ public class SecurityStateData
     public required string SecurityState { get; set; }
     // The security version is included in the security state, but needs COSE parsing,
     // so this is a separate copy that can be used directly.
-    public int SecurityVersion { get; set; }
+    public required int SecurityVersion { get; set; }
 }
