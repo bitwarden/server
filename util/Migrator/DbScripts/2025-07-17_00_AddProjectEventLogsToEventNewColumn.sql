c1808d0561e1b4a6512cb283e5a57ab32adaa933
cd-bitwarden <106776772+cd-bitwarden@users.noreply.github.com>
2025-07-18 09:29:16 -0400

    Trying to get it to for sure add the column before we try and update sprocs

--- util/Migrator/DbScripts/2025-07-17_00_AddProjectEventLogsToEventNewColumn.sql
+++ util/Migrator/DbScripts/2025-07-17_00_AddProjectEventLogsToEventNewColumn.sql
@@ -1,7 +1,5 @@
--- Add ProjectId column if it doesn't exist
 IF COL_LENGTH('[dbo].[Event]', 'ProjectId') IS NULL
 BEGIN
-    ALTER TABLE [dbo].[Event]
-        ADD [ProjectId] UNIQUEIDENTIFIER NULL;
+    EXEC('ALTER TABLE [dbo].[Event] ADD [ProjectId] UNIQUEIDENTIFIER NULL');
 END
 GO
