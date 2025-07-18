b936370c6622fce6887e605676c2905683b0e081
cd-bitwarden <106776772+cd-bitwarden@users.noreply.github.com>
2025-07-18 10:16:02 -0400

    Adding code to refresh the view to include ProjectId I hope

--- util/Migrator/DbScripts/2025-07-17_00_AddProjectEventLogsToEventNewColumn.sql
+++ util/Migrator/DbScripts/2025-07-17_00_AddProjectEventLogsToEventNewColumn.sql
@@ -3,3 +3,14 @@ BEGIN
     EXEC('ALTER TABLE [dbo].[Event] ADD [ProjectId] UNIQUEIDENTIFIER NULL');
 END
 GO
+
+IF OBJECT_ID('[dbo].[EventView]', 'V') IS NOT NULL
+BEGIN
+    DROP VIEW [dbo].[EventView];
+END
+GO
+
+CREATE VIEW [dbo].[EventView]
+AS
+SELECT * FROM [dbo].[Event];
+GO
