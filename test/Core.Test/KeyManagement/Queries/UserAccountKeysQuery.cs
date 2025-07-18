6d1f324b05445a8fdc793fa4751b22f80ad8a294
Bernd Schoolmann <mail@quexten.com>
2025-07-18 13:33:55 +0200

    Fix fixture

--- test/Core.Test/KeyManagement/Queries/UserAccountKeysQuery.cs
+++ test/Core.Test/KeyManagement/Queries/UserAccountKeysQuery.cs
@@ -20,11 +20,11 @@ public class UserAccountKeysQueryTests
     }
 
     [Theory, BitAutoData]
-    public async Task V2User_Success(SutProvider<UserAccountKeysQuery> sutProvider, User user,
-        IUserSignatureKeyPairRepository signatureKeyPairRepository)
+    public async Task V2User_Success(SutProvider<UserAccountKeysQuery> sutProvider, User user)
     {
         user.SecurityState = "v2";
         user.SecurityVersion = 2;
+        var signatureKeyPairRepository = sutProvider.GetDependency<IUserSignatureKeyPairRepository>();
         signatureKeyPairRepository.GetByUserIdAsync(user.Id).Returns(new SignatureKeyPairData(Core.KeyManagement.Enums.SignatureAlgorithm.Ed25519, "wrappedSigningKey", "verifyingKey"));
         var result = await sutProvider.Sut.Run(user);
         Assert.Equal(user.GetPublicKeyEncryptionKeyPair(), result.PublicKeyEncryptionKeyPairData);
