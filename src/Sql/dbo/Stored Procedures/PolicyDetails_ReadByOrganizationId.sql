18b38621919e2e25afdcf30301bbccdaa5081dac
Jimmy Vo <huynhmaivo82@gmail.com>
2025-07-18 10:18:59 -0400

    [PM-22108] wip

--- src/Sql/dbo/Stored Procedures/PolicyDetails_ReadByOrganizationId.sql
+++ src/Sql/dbo/Stored Procedures/PolicyDetails_ReadByOrganizationId.sql
@@ -5,44 +5,47 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
+BEGIN
+    SET NOCOUNT ON;
+
     ;WITH givenorgusers
-              AS (SELECT OU.id AS OrganizationUserID,
-                         OU.userid,
-                         U.email
-                  FROM   dbo.organizationuserview OU
-                             INNER JOIN dbo.userview U
-                                        ON U.id = OU.userid
-                  WHERE  OU.organizationid = @OrganizationId
-                  UNION ALL
-                  SELECT OU.id AS OrganizationUserID,
-                         U.id  AS UserId,
-                         U.email
-                  FROM   dbo.organizationuserview OU
-                             INNER JOIN dbo.userview U
-                                        ON U.email = OU.email
-                  WHERE  OU.organizationid = @OrganizationId),
+                      AS (SELECT OU.id AS OrganizationUserID,
+                                 OU.userid,
+                                 U.email
+                          FROM   dbo.organizationuserview OU
+                                     INNER JOIN dbo.userview U
+                                                ON U.id = OU.userid
+                          WHERE  OU.organizationid = @OrganizationId
+                          UNION ALL
+                          SELECT OU.id AS OrganizationUserID,
+                                 U.id  AS UserId,
+                                 U.email
+                          FROM   dbo.organizationuserview OU
+                                     INNER JOIN dbo.userview U
+                                                ON U.email = OU.email
+                          WHERE  OU.organizationid = @OrganizationId),
           allorgusers
-              AS (SELECT OU.id            AS OrganizationUserID,
-                         OU.userid,
-                         Ou.organizationid,
-                         AU.email,
-                         OU.[type]        AS OrganizationUserType,
-                         OU.[status]      AS OrganizationUserStatus,
-                         OU.[permissions] AS OrganizationUserPermissionsData
-                  FROM   dbo.organizationuserview OU
-                             INNER JOIN givenorgusers AU
-                                        ON AU.userid = OU.userid
-                  UNION ALL
-                  SELECT OU.id            AS OrganizationUserID,
-                         AU.userid,
-                         Ou.organizationid,
-                         AU.email,
-                         OU.[type]        AS OrganizationUserType,
-                         OU.[status]      AS OrganizationUserStatus,
-                         OU.[permissions] AS OrganizationUserPermissionsData
-                  FROM   dbo.organizationuserview OU
-                             INNER JOIN givenorgusers AU
-                                        ON AU.email = OU.email)
+                      AS (SELECT OU.id            AS OrganizationUserID,
+                                 OU.userid,
+                                 Ou.organizationid,
+                                 AU.email,
+                                 OU.[type]        AS OrganizationUserType,
+                                 OU.[status]      AS OrganizationUserStatus,
+                                 OU.[permissions] AS OrganizationUserPermissionsData
+                          FROM   dbo.organizationuserview OU
+                                     INNER JOIN givenorgusers AU
+                                                ON AU.userid = OU.userid
+                          UNION ALL
+                          SELECT OU.id            AS OrganizationUserID,
+                                 AU.userid,
+                                 Ou.organizationid,
+                                 AU.email,
+                                 OU.[type]        AS OrganizationUserType,
+                                 OU.[status]      AS OrganizationUserStatus,
+                                 OU.[permissions] AS OrganizationUserPermissionsData
+                          FROM   dbo.organizationuserview OU
+                                     INNER JOIN givenorgusers AU
+                                                ON AU.email = OU.email)
      SELECT OU.organizationuserid,
             P.[organizationid],
             P.[type] AS PolicyType,
@@ -50,22 +53,22 @@ BEGIN
             OU.organizationusertype,
             OU.organizationuserstatus,
             OU.organizationuserpermissionsdata
-             ,CASE
-                  WHEN EXISTS (SELECT 1
-                               FROM [dbo].[ProviderUserView] PU
+                     ,CASE
+                          WHEN EXISTS (SELECT 1
+                                       FROM [dbo].[ProviderUserView] PU
                                         INNER JOIN [dbo].[ProviderOrganizationView] PO
                                                    ON PO.[ProviderId] = PU.[ProviderId]
                                WHERE PU.[UserId] = OU.[UserId]
                                  AND PO.[OrganizationId] = P.[OrganizationId]) THEN 1
-                  ELSE 0 END   AS IsProvider
+                          ELSE 0 END   AS IsProvider
      FROM   [dbo].[policyview] P
-                INNER JOIN [dbo].[organizationview] O
-                           ON P.[organizationid] = O.[id]
-                INNER JOIN allorgusers OU
-                           ON OU.organizationid = O.[id]
+                 INNER JOIN [dbo].[organizationview] O
+     ON P.[organizationid] = O.[id]
+                 INNER JOIN allorgusers OU
+                 ON OU.organizationid = O.[id]
      WHERE  P.enabled = 1
        AND O.enabled = 1
        AND O.usepolicies = 1
        AND p.type = @PolicyType;
-    END
+END
 GO
