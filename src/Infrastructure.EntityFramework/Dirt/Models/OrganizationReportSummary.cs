37095f1d7f3900fa590e9793529b6f3e6570b8cc
Graham Walker <gwalker@bitwarden.com>
2025-07-22 12:26:14 -0500

    PM-23754 initial commit

---
+++ src/Infrastructure.EntityFramework/Dirt/Models/OrganizationReportSummary.cs
@@ -0,0 +1,20 @@
+﻿// FIXME: Update this file to be null safe and then delete the line below
+#nullable disable
+
+using AutoMapper;
+
+namespace Bit.Infrastructure.EntityFramework.Dirt.Models;
+
+public class OrganizationReportSummary : Core.Dirt.Entities.OrganizationReportSummary
+{
+    public virtual OrganizationReport OrganizationReport { get; set; }
+}
+
+public class OrganizationReportSummaryProfile : Profile
+{
+    public OrganizationReportSummaryProfile()
+    {
+        CreateMap<Core.Dirt.Entities.OrganizationReportSummary, OrganizationReportSummary>()
+            .ReverseMap();
+    }
+}
