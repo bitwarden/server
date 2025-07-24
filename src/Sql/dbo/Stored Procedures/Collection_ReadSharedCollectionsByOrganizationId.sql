bded5ce647c785a054c2427d42cf324baeb419de
Rui Tome <rtome@bitwarden.com>
2025-07-24 18:50:33 +0100

    Add stored procedure Collection_ReadSharedCollectionsByOrganizationId to retrieve collections by organization ID, excluding default user collections.

---
+++ src/Sql/dbo/Stored Procedures/Collection_ReadSharedCollectionsByOrganizationId.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[Collection_ReadSharedCollectionsByOrganizationId]
+    @OrganizationId UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    SELECT
+        *
+    FROM
+        [dbo].[CollectionView]
+    WHERE
+        [OrganizationId] = @OrganizationId AND
+        [Type] != 1 -- Exclude DefaultUserCollection
+END
--- util/Migrator/DbScripts/2025-07-24_00_ReadSharedCollections.sql
+++ util/Migrator/DbScripts/2025-07-24_00_ReadSharedCollections.sql
@@ -12,3 +12,19 @@ BEGIN
         [OrganizationId] = @OrganizationId
 END
 GO
+
+CREATE OR ALTER PROCEDURE [dbo].[Collection_ReadSharedCollectionsByOrganizationId]
+    @OrganizationId UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    SELECT
+        *
+    FROM
+        [dbo].[CollectionView]
+    WHERE
+        [OrganizationId] = @OrganizationId AND
+        [Type] != 1 -- Exclude DefaultUserCollection
+END
+GO
