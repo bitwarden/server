c0841aca18cc6dae48b725bce107f0fed04cee33
Brandon <btreston@bitwarden.com>
2025-07-31 11:32:02 -0400

    clean up

--- test/Core.Test/OrganizationFeatures/OrganizationCollections/UpdateCollectionCommandTests.cs
+++ test/Core.Test/OrganizationFeatures/OrganizationCollections/UpdateCollectionCommandTests.cs
@@ -177,10 +177,13 @@ public class UpdateCollectionCommandTests
             .Returns(organization);
 
         var ex = await Assert.ThrowsAsync<BadRequestException>(() => sutProvider.Sut.UpdateAsync(collection));
-        Assert.Contains("You cannot edit a collection with the type as DefaultUserCollection", ex.Message);
+        Assert.Contains("You cannot edit a collection with the type as DefaultUserCollection.", ex.Message);
         await sutProvider.GetDependency<ICollectionRepository>()
             .DidNotReceiveWithAnyArgs()
             .ReplaceAsync(default);
+        await sutProvider.GetDependency<ICollectionRepository>()
+            .DidNotReceiveWithAnyArgs()
+            .ReplaceAsync(default, default, default);
         await sutProvider.GetDependency<IEventService>()
             .DidNotReceiveWithAnyArgs()
             .LogCollectionEventAsync(default, default);
