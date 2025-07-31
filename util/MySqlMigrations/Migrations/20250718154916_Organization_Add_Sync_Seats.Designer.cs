86ce3a86e9ec3d79c9b57a5424a18e9b2a96356b
Jared McCannon <jmccannon@bitwarden.com>
2025-07-31 07:54:51 -0500

    [PM-20452] - Offloading Stripe Update (#6034)
    * Adding job to update stripe subscriptions and increment seat count  when inviting a user.
    
    * Updating name
    
    * Added ef migrations
    
    * Fixing script
    
    * Fixing procedures. Added repo tests.
    
    * Fixed set stored procedure. Fixed parameter name.
    
    * Added tests for database calls and updated stored procedures
    
    * Fixed build for sql file.
    
    * fixing sproc
    
    * File is nullsafe
    
    * Adding view to select from instead of table.
    
    * Updating UpdateSubscriptionStatus to use a CTE and do all the updates in 1 statement.
    
    * Setting revision date when incrementing seat count
    
    * Added feature flag check for the background job.
    
    * Fixing nullable property.
    
    * Removing new table and just adding the column to org. Updating to query and command. Updated tests.
    
    * Adding migration script rename
    
    * Add SyncSeats to Org.sql def
    
    * Adding contraint name
    
    * Removing old table files.
    
    * Added tests
    
    * Upped the frequency to be at the top of every 3rd hour.
    
    * Updating error message.
    
    * Removing extension method
    
    * Changed to GuidIdArray
    
    * Added xml doc and switched class to record

--- util/MySqlMigrations/Migrations/20250718154916_Organization_Add_Sync_Seats.Designer.cs
+++ util/MySqlMigrations/Migrations/20250718154916_Organization_Add_Sync_Seats.Designer.cs
@@ -4,6 +4,7 @@ using Bit.Infrastructure.EntityFramework.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bit.MySqlMigrations.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20250718154916_Organization_Add_Sync_Seats")]
+    partial class Organization_Add_Sync_Seats
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -316,7 +319,7 @@ namespace Bit.MySqlMigrations.Migrations
                     b.Property<DateTime>("CreationDate")
                         .HasColumnType("datetime(6)");
 
-                    b.Property<int?>("EventType")
+                    b.Property<int>("EventType")
                         .HasColumnType("int");
 
                     b.Property<string>("Filters")
