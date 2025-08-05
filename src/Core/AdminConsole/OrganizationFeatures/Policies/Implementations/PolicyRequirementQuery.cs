c2f0c208770f6cf9525c939af50d7f45a7da039c
Jimmy Vo <huynhmaivo82@gmail.com>
2025-08-05 11:14:36 -0400

    [PM-24278] undo

--- src/Core/AdminConsole/OrganizationFeatures/Policies/Implementations/PolicyRequirementQuery.cs
+++ src/Core/AdminConsole/OrganizationFeatures/Policies/Implementations/PolicyRequirementQuery.cs
@@ -22,6 +22,7 @@ public class PolicyRequirementQuery(
 
         var policyDetails = await GetPolicyDetails(userId);
         var filteredPolicies = policyDetails
+            .Where(p => p.PolicyType == factory.PolicyType)
             .Where(factory.Enforce);
         var requirement = factory.Create(filteredPolicies);
         return requirement;
