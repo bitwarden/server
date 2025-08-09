d9c5b0d6c4caca8fa979e8247a090c93cdfd472e
Thomas Rittson <trittson@bitwarden.com>
2025-08-09 11:45:52 +1000

    Add tests

--- src/Core/AdminConsole/OrganizationFeatures/AccountRecovery/IAdminRecoverAccountCommand.cs
+++ src/Core/AdminConsole/OrganizationFeatures/AccountRecovery/IAdminRecoverAccountCommand.cs
@@ -5,6 +5,6 @@ namespace Bit.Core.AdminConsole.OrganizationFeatures.AccountRecovery;
 
 public interface IAdminRecoverAccountCommand
 {
-    Task<IdentityResult> AdminResetPasswordAsync(OrganizationUserType callingUserType, Guid orgId, Guid id,
+    Task<IdentityResult> AdminResetPasswordAsync(OrganizationUserType callingUserType, Guid orgId, Guid organizationUserId,
         string newMasterPassword, string key);
 }
