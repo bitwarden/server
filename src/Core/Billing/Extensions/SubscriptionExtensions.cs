ec6da98883af736a7934af6b07124b031c28f690
Alex Morask <amorask@bitwarden.com>
2025-08-13 14:37:08 -0500

    Added SubscriptionExtensions with GetCurrentPeriodEnd

---
+++ src/Core/Billing/Extensions/SubscriptionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Stripe;
+
+namespace Bit.Core.Billing.Extensions;
+
+public static class SubscriptionExtensions
+{
+    /*
+     * For the time being, this is the simplest migration approach from v45 to v48 as
+     * we do not support multi-cadence subscriptions. Each subscription item should be on the
+     * same billing cycle. If this changes, we'll need a significantly more robust approach.
+     *
+     * Because we can't guarantee a subscription will have items, this has to be nullable.
+     */
+    public static DateTime? GetCurrentPeriodEnd(this Subscription subscription) =>
+        subscription.Items.FirstOrDefault()?.CurrentPeriodEnd;
+}
