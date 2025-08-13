e260b83bff5d94b8802c42f79197bb54aa07fa3d
Graham Walker <gwalker@bitwarden.com>
2025-08-13 15:46:33 -0500

    pm-23754 add new sql scripts

---
+++ util/Migrator/DbScripts/2025-08-13_01_AddOrganizationReportStoredProcedures.sql
@@ -0,0 +1,138 @@
+CREATE OR ALTER PROCEDURE [dbo].[OrganizationReport_GetLatestByOrganizationId]
+    @OrganizationId UNIQUEIDENTIFIER,
+AS
+BEGIN
+    SET NOCOUNT ON
+
+SELECT
+    [Id],
+    [OrganizationId],
+    [ReportData],
+    [CreationDate],
+    [ContentEncryptionKey],
+    [SummaryData],
+    [ApplicationData],
+    [RevisionDate]
+FROM [dbo].[OrganizationReport]
+WHERE [OrganizationId] = @OrganizationId
+ORDER BY [RevisionDate] DESC
+END
+
+CREATE OR ALTER PROCEDURE [dbo].[OrganizationReport_GetSummariesByDateRange]
+    @OrganizationId UNIQUEIDENTIFIER,
+    @Id UNIQUEIDENTIFIER,
+    @StartDate DATETIME2(7),
+    @EndDate DATETIME2(7)
+AS
+BEGIN
+    SET NOCOUNT ON
+
+SELECT
+    [Id],
+    [OrganizationId],
+    [SummaryData]
+FROM [dbo].[OrganizationReport]
+WHERE [OrganizationId] = @OrganizationId
+  AND [Id] = @Id
+  AND [RevisionDate] >= @StartDate
+  AND [RevisionDate] <= @EndDate
+ORDER BY [RevisionDate] DESC
+END
+
+CREATE PROCEDURE [dbo].[OrganizationReport_GetSummaryDataById]
+    @OrganizationId UNIQUEIDENTIFIER,
+    @Id UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON
+
+SELECT
+    [Id],
+    [OrganizationId], [
+    SummaryData]
+FROM [dbo].[OrganizationReport]
+WHERE [OrganizationId] = @OrganizationId AND [Id] = @Id
+END
+
+CREATE OR ALTER PROCEDURE [dbo].[OrganizationReport_UpdateSummaryData]
+    @Id UNIQUEIDENTIFIER,
+    @OrganizationId UNIQUEIDENTIFIER,
+    @SummaryData NVARCHAR(MAX)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+UPDATE [dbo].[OrganizationReport]
+SET
+    [SummaryData] = @SummaryData,
+    [RevisionDate] = GETUTCDATE()
+WHERE [Id] = @Id
+  AND [OrganizationId] = @OrganizationId;
+END
+
+CREATE OR ALTER PROCEDURE [dbo].[OrganizationReport_GetReportDataById]
+    @OrganizationId UNIQUEIDENTIFIER,
+    @Id UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON
+
+SELECT
+    [Id],
+    [OrganizationId],
+    [ReportData]
+FROM [dbo].[OrganizationReport]
+WHERE [OrganizationId] = @OrganizationId
+  AND [Id] = @Id
+END
+
+CREATE OR ALTER PROCEDURE [dbo].[OrganizationReport_UpdateReportData]
+    @Id UNIQUEIDENTIFIER,
+    @OrganizationId UNIQUEIDENTIFIER,
+    @ReportData NVARCHAR(MAX)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+UPDATE [dbo].[OrganizationReport]
+SET
+    [ReportData] = @ReportData,
+    [RevisionDate] = GETUTCDATE()
+WHERE [Id] = @Id
+  AND [OrganizationId] = @OrganizationId;
+END
+
+CREATE OR ALTER PROCEDURE [dbo].[OrganizationReport_GetApplicationDataById]
+    @OrganizationId UNIQUEIDENTIFIER,
+    @Id UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON
+
+SELECT
+    [Id],
+    [OrganizationId],
+    [ApplicationData]
+FROM [dbo].[OrganizationReport]
+WHERE [OrganizationId] = @OrganizationId
+  AND [Id] = @Id
+END
+
+CREATE OR ALTER PROCEDURE [dbo].[OrganizationReport_UpdateApplicationData]
+    @Id UNIQUEIDENTIFIER,
+    @OrganizationId UNIQUEIDENTIFIER,
+    @ApplicationData NVARCHAR(MAX)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+UPDATE [dbo].[OrganizationReport]
+SET
+    [ApplicationData] = @ApplicationData,
+    [RevisionDate] = GETUTCDATE()
+WHERE [Id] = @Id
+  AND [OrganizationId] = @OrganizationId;
+END
+
+
+
