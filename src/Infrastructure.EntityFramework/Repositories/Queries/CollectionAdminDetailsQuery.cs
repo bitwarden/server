c189e4aaf55fda7dbcf7ce75b41bb1e02b470cdb
Brandon Treston <btreston@bitwarden.com>
2025-08-19 14:12:34 -0400

    [PM-22104] Migrate default collection when org user is removed (#6135)
    * migrate default collection to a shared collection when users are removed
    
    * remove redundant logic
    
    * fix test
    
    * fix tests
    
    * fix test
    
    * clean up
    
    * add migrations
    
    * run dotnet format
    
    * clean up, refactor duplicate logic to sproc, wip integration test
    
    * fix sql
    
    * add migration for new sproc
    
    * integration test wip
    
    * integration test wip
    
    * integration test wip
    
    * integration test wip
    
    * fix integration test LINQ expression
    
    * fix using wrong Id
    
    * wip integration test for DeleteManyAsync
    
    * fix LINQ
    
    * only set DefaultUserEmail when it is null in sproc
    
    * check for null
    
    * spelling, separate create and update request models
    
    * fix test
    
    * fix child class
    
    * refactor sproc
    
    * clean up
    
    * more cleanup
    
    * fix tests
    
    * fix user email
    
    * remove unneccesary test
    
    * add DefaultUserCollectionEmail to EF query
    
    * fix test
    
    * fix EF logic to match sprocs
    
    * clean up logic
    
    * cleanup

--- src/Infrastructure.EntityFramework/Repositories/Queries/CollectionAdminDetailsQuery.cs
+++ src/Infrastructure.EntityFramework/Repositories/Queries/CollectionAdminDetailsQuery.cs
@@ -81,6 +81,7 @@ public class CollectionAdminDetailsQuery : IQuery<CollectionAdminDetails>
             ExternalId = x.c.ExternalId,
             CreationDate = x.c.CreationDate,
             RevisionDate = x.c.RevisionDate,
+            DefaultUserCollectionEmail = x.c.DefaultUserCollectionEmail,
             ReadOnly = (bool?)x.cu.ReadOnly ?? (bool?)x.cg.ReadOnly ?? false,
             HidePasswords = (bool?)x.cu.HidePasswords ?? (bool?)x.cg.HidePasswords ?? false,
             Manage = (bool?)x.cu.Manage ?? (bool?)x.cg.Manage ?? false,
