eeaef93c039b5cd17fbff7ef7f935b0005980998
Rui Tome <rtome@bitwarden.com>
2025-08-19 16:43:00 +0100

    Add CollectionsRecipe to manage collection creation and user relationships in organizations

---
+++ util/Seeder/Recipes/CollectionsRecipe.cs
@@ -0,0 +1,67 @@
+﻿using Bit.Core.Enums;
+using Bit.Infrastructure.EntityFramework.Repositories;
+using LinqToDB.EntityFrameworkCore;
+
+namespace Bit.Seeder.Recipes;
+
+public class CollectionsRecipe(DatabaseContext db)
+{
+    /// <summary>
+    /// Adds collections to an organization and creates relationships between users and collections.
+    /// </summary>
+    /// <param name="organizationId">The ID of the organization to add collections to.</param>
+    /// <param name="collections">The number of collections to add.</param>
+    /// <param name="organizationUserIds">The IDs of the users to create relationships with.</param>
+    /// <param name="maxUsersWithRelationships">The maximum number of users to create relationships with.</param>
+    public void AddToOrganization(Guid organizationId, int collections, List<Guid> organizationUserIds, int maxUsersWithRelationships = 1000)
+    {
+        var collectionList = new List<Core.Entities.Collection>();
+        for (var i = 0; i < collections; i++)
+        {
+            collectionList.Add(new Core.Entities.Collection
+            {
+                Id = Guid.NewGuid(),
+                OrganizationId = organizationId,
+                Name = $"Collection {i + 1}",
+                Type = CollectionType.SharedCollection,
+                CreationDate = DateTime.UtcNow,
+                RevisionDate = DateTime.UtcNow
+            });
+        }
+
+        if (collectionList.Any())
+        {
+            db.BulkCopy(collectionList);
+
+            if (organizationUserIds.Any())
+            {
+                var maxRelationships = Math.Min(organizationUserIds.Count, maxUsersWithRelationships);
+                var collectionUsers = new List<Core.Entities.CollectionUser>();
+
+                for (var i = 0; i < maxRelationships; i++)
+                {
+                    var orgUserId = organizationUserIds[i];
+
+                    var userCollectionCount = (i % 3) + 1; // 1-3 collections per user
+                    for (var j = 0; j < userCollectionCount; j++)
+                    {
+                        var collectionIndex = (i + j) % collectionList.Count;
+                        collectionUsers.Add(new Core.Entities.CollectionUser
+                        {
+                            CollectionId = collectionList[collectionIndex].Id,
+                            OrganizationUserId = orgUserId,
+                            ReadOnly = j > 0,
+                            HidePasswords = false,
+                            Manage = j == 0
+                        });
+                    }
+                }
+
+                if (collectionUsers.Any())
+                {
+                    db.BulkCopy(collectionUsers);
+                }
+            }
+        }
+    }
+}
