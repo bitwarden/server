a3ad14d6c8524962603ff4daa060d4a0a5f0a749
cyprain-okeke <108260115+cyprain-okeke@users.noreply.github.com>
2025-08-21 10:18:11 +0100

    Replace the old webhook secret key with new one (#6223)

--- src/Billing/Controllers/StripeController.cs
+++ src/Billing/Controllers/StripeController.cs
@@ -120,9 +120,7 @@ public class StripeController : Controller
 
         return deliveryContainer.ApiVersion switch
         {
-            "2024-06-20" => HandleVersionWith(_billingSettings.StripeWebhookSecret20240620),
-            "2023-10-16" => HandleVersionWith(_billingSettings.StripeWebhookSecret20231016),
-            "2022-08-01" => HandleVersionWith(_billingSettings.StripeWebhookSecret),
+            "2025-07-30.basil" => HandleVersionWith(_billingSettings.StripeWebhookSecret20250730Basil),
             _ => HandleDefault(deliveryContainer.ApiVersion)
         };
 
