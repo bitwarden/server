d883aee261323ea36a70b0ba4eccae7bb6194b9e
Ben Brooks <bbrooks@bitwarden.com>
2025-09-02 07:55:21 -0700

    feat(policies): add URI Match Defaults organizational policy
    Signed-off-by: Ben Brooks <bbrooks@bitwarden.com>

---
+++ src/Core/AdminConsole/Models/Data/Organizations/Policies/UriMatchDefaultsPolicyData.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Bit.Core.Enums;
+
+namespace Bit.Core.AdminConsole.Models.Data.Organizations.Policies;
+
+public class UriMatchDefaultsPolicyData : IPolicyDataModel
+{
+    /// <summary>
+    /// The default URI match type for new cipher logins.
+    /// </summary>
+    [Display(Name = "DefaultUriMatchType")]
+    public UriMatchType DefaultUriMatchType { get; set; } = UriMatchType.Domain; // Default to Base Domain
+
+    /// <summary>
+    /// Whether users can override the default URI match type for individual URIs
+    /// </summary>
+    [Display(Name = "AllowUserOverride")]
+    public bool AllowUserOverride { get; set; } = true;
+}
