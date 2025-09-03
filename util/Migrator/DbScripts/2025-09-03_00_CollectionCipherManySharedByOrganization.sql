9077ee3e6d36aa8820ec6e34f29c5149a6b27aed
Rui Tome <rtome@bitwarden.com>
2025-09-03 12:58:21 +0100

    Add SQL migration script

---
+++ util/Migrator/DbScripts/2025-09-03_00_CollectionCipherManySharedByOrganization.sql
@@ -0,0 +1,16 @@
+CREATE OR ALTER PROCEDURE [dbo].[CollectionCipher_ReadSharedByOrganizationId]
+    @OrganizationId UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    SELECT
+        CC.*
+    FROM
+        [dbo].[CollectionCipher] CC
+    INNER JOIN
+        [dbo].[Collection] C ON C.[Id] = CC.[CollectionId]
+    WHERE
+        C.[OrganizationId] = @OrganizationId
+        AND C.[Type] = 0 -- SharedCollections only
+END
