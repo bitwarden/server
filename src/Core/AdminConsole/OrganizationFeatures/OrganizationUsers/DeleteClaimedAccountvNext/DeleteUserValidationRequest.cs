6966ad4caa3cf14c2bc5ea93c4ec9fc12cf1a307
Thomas Rittson <trittson@bitwarden.com>
2025-09-03 10:49:23 +1000

    Remove nullable directive

--- src/Core/AdminConsole/OrganizationFeatures/OrganizationUsers/DeleteClaimedAccountvNext/DeleteUserValidationRequest.cs
+++ src/Core/AdminConsole/OrganizationFeatures/OrganizationUsers/DeleteClaimedAccountvNext/DeleteUserValidationRequest.cs
@@ -1,7 +1,6 @@
 ﻿using Bit.Core.Entities;
 
 namespace Bit.Core.AdminConsole.OrganizationFeatures.OrganizationUsers.DeleteClaimedAccountvNext;
-#nullable enable
 
 public class DeleteUserValidationRequest
 {
