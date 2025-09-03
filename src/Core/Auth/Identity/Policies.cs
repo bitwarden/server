049a9921aad80bf999be44c805abaa4f18b49d77
Ike Kottlowski <ikottlowski@bitwarden.com>
2025-09-03 17:35:03 -0400

    feat: add policy

---
+++ src/Core/Auth/Identity/Policies.cs
@@ -0,0 +1,13 @@
+﻿namespace Bit.Core.Auth.Identity;
+
+public static class Policies
+{
+    /// <summary>
+    /// Class holding the policies that support the Tools team
+    /// </summary>
+    public static class Tools
+    {
+        public const string Send = "Send";  // [Authorize(Policy = Policies.Send)]
+    }
+    // TODO: migrate other existing policies to use this class
+}
