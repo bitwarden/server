1bc41ad2eb2fa097eb5ddec99ca84eac1f13f0da
voommen-livefront <voommen@livefront.com>
2025-09-11 09:39:51 -0500

    PM-23761 use the auto-reply endpoint in freskdesk to add a reply to a note

---
+++ src/Billing/Models/FreshdeskReplyRequestModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Bit.Billing.Models;
+
+public class FreshdeskReplyRequestModel
+{
+    [JsonPropertyName("body")]
+    public required string Body { get; set; }
+}
