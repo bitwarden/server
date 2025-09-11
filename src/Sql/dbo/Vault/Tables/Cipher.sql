8c49c6596c4d4deb65dd01d22282331b88f5f0e6
jng <jng@bitwarden.com>
2025-09-11 15:04:30 -0400

    update sysutcdatetime in migration

--- src/Sql/dbo/Vault/Tables/Cipher.sql
+++ src/Sql/dbo/Vault/Tables/Cipher.sql
@@ -13,10 +13,7 @@ CREATE TABLE [dbo].[Cipher] (
     [DeletedDate]    DATETIME2 (7)    NULL,
     [Reprompt]       TINYINT          NULL,
     [Key]            VARCHAR(MAX)     NULL,
-    [ArchivedDate] DATETIME2
-(
-    7
-) NULL,
+    [ArchivedDate]   DATETIME2 (7)    NULL,
     CONSTRAINT [PK_Cipher] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_Cipher_Organization] FOREIGN KEY ([OrganizationId]) REFERENCES [dbo].[Organization] ([Id]),
     CONSTRAINT [FK_Cipher_User] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([Id])
