5770146e01b80b50dde0fb0d36a309adc602f872
cd-bitwarden <106776772+cd-bitwarden@users.noreply.github.com>
2025-09-25 12:59:09 -0400

    Fix

--- util/Migrator/DbScripts/2025-08-26_01_AddGrantedMachineAccountEventLogsToEventSprocs.sql
+++ util/Migrator/DbScripts/2025-08-26_01_AddGrantedMachineAccountEventLogsToEventSprocs.sql
@@ -157,33 +157,15 @@ BEGIN
 END
 GO
 
-IF EXISTS(SELECT 1 FROM sys.indexes WHERE name = 'IX_Event_DateOrganizationIdUserId')
-BEGIN
-    -- Check if neither ServiceAccountId nor GrantedServiceAccountId are included columns
-    IF NOT EXISTS (
-        SELECT 1
-        FROM 
-            sys.indexes i
-        INNER JOIN
-            sys.index_columns ic ON i.object_id = ic.object_id AND i.index_id = ic.index_id
-        INNER JOIN
-            sys.columns c ON ic.object_id = c.object_id AND ic.column_id = c.column_id
-        WHERE 
-            i.object_id = OBJECT_ID('[dbo].[Event]')
-            AND i.name = 'IX_Event_DateOrganizationIdUserId'
-            AND c.name IN ('ServiceAccountId', 'GrantedServiceAccountId')
-            AND ic.is_included_column = 1
-    )
-    BEGIN        
-        CREATE NONCLUSTERED INDEX [IX_Event_DateOrganizationIdUserId] 
-        ON [dbo].[Event]
-        (   [Date] DESC,
-            [OrganizationId] ASC,
-            [ActingUserId] ASC,
-            [CipherId] ASC
-        )
-        INCLUDE ([ServiceAccountId], [GrantedServiceAccountId])
-        WITH (DROP_EXISTING = ON)
-    END    
-END
+DROP INDEX IF EXISTS [IX_Event_DateOrganizationIdUserId] ON [dbo].[Event];
+GO
+
+CREATE NONCLUSTERED INDEX [IX_Event_DateOrganizationIdUserId] 
+ON [dbo].[Event]
+(   [Date] DESC,
+    [OrganizationId] ASC,
+    [ActingUserId] ASC,
+    [CipherId] ASC
+)
+INCLUDE ([ServiceAccountId], [GrantedServiceAccountId])
 GO
