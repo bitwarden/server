b77a43d00d3d3328f449a906dc8829c4d91ff3e9
Maciej Zieniuk <mzieniuk@bitwarden.com>
2025-09-29 20:40:09 +0100

    default low kdf - just for testing

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -74,7 +74,7 @@ public static class Constants
 
 public static class AuthConstants
 {
-    public static readonly RangeConstant PBKDF2_ITERATIONS = new(600_000, 2_000_000, 600_000);
+    public static readonly RangeConstant PBKDF2_ITERATIONS = new(5_000, 2_000_000, 5_000);
 
     public static readonly RangeConstant ARGON2_ITERATIONS = new(2, 10, 3);
     public static readonly RangeConstant ARGON2_MEMORY = new(15, 1024, 64);
