f66f599a24ebe9daafaef2649c64d1ba332dc514
Bernd Schoolmann <mail@quexten.com>
2025-09-30 12:54:38 +0200

    Add feature flag

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -187,6 +187,7 @@ public static class FeatureFlagKeys
     public const string ForceUpdateKDFSettings = "pm-18021-force-update-kdf-settings";
     public const string UnlockWithMasterPasswordUnlockData = "pm-23246-unlock-with-master-password-unlock-data";
     public const string WindowsBiometricsV2 = "pm-25373-windows-biometrics-v2";
+    public const string LinuxBiometricsV2 = "pm-26340-linux-biometrics-v2";
 
     /* Mobile Team */
     public const string NativeCarouselFlow = "native-carousel-flow";
