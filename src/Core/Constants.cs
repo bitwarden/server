b6a910f230d3b5891ec5bea021f3b63b2ec97475
Kyle Denney <4227399+kdenney@users.noreply.github.com>
2025-10-07 15:56:51 -0500

    [PM-23713] new feature flag for premium badge interaction

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -185,6 +185,7 @@ public static class FeatureFlagKeys
     public const string PM23385_UseNewPremiumFlow = "pm-23385-use-new-premium-flow";
     public const string PM24996ImplementUpgradeFromFreeDialog = "pm-24996-implement-upgrade-from-free-dialog";
     public const string PM24032_NewNavigationPremiumUpgradeButton = "pm-24032-new-navigation-premium-upgrade-button";
+    public const string PM23713_PremiumBadgeOpensNewPremiumUpgradeDialog = "pm-23713-premium-badge-opens-new-premium-upgrade-dialog";
 
     /* Key Management Team */
     public const string ReturnErrorOnExistingKeypair = "return-error-on-existing-keypair";
