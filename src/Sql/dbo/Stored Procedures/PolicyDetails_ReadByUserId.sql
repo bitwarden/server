e1c7f03150d2ddcdd3d488523c78059d00ecf4d3
Rui Tome <rtome@bitwarden.com>
2025-10-07 13:38:42 +0100

    Revert changes to PolicyDetails_ReadByUserId stored procedure

--- src/Sql/dbo/Stored Procedures/PolicyDetails_ReadByUserId.sql
+++ src/Sql/dbo/Stored Procedures/PolicyDetails_ReadByUserId.sql
@@ -3,56 +3,41 @@ CREATE PROCEDURE [dbo].[PolicyDetails_ReadByUserId]
 AS
 BEGIN
     SET NOCOUNT ON
+SELECT
+    OU.[Id] AS OrganizationUserId,
+    P.[OrganizationId],
+    P.[Type] AS PolicyType,
+    P.[Data] AS PolicyData,
+    OU.[Type] AS OrganizationUserType,
+    OU.[Status] AS OrganizationUserStatus,
+    OU.[Permissions] AS OrganizationUserPermissionsData,
+    CASE WHEN EXISTS (
+            SELECT 1
+            FROM [dbo].[ProviderUserView] PU
+            INNER JOIN [dbo].[ProviderOrganizationView] PO ON PO.[ProviderId] = PU.[ProviderId]
+            WHERE PU.[UserId] = OU.[UserId] AND PO.[OrganizationId] = P.[OrganizationId]
+        ) THEN 1 ELSE 0 END AS IsProvider
+FROM [dbo].[PolicyView] P
+INNER JOIN [dbo].[OrganizationUserView] OU
+    ON P.[OrganizationId] = OU.[OrganizationId]
+INNER JOIN [dbo].[OrganizationView] O
+    ON P.[OrganizationId] = O.[Id]
+WHERE
+    P.Enabled = 1
+    AND O.Enabled = 1
+    AND O.UsePolicies = 1
+    AND (
+            -- OrgUsers who have accepted their invite and are linked to a UserId
+            -- (Note: this excludes "invited but revoked" users who don't have an OU.UserId yet,
+            -- but those users will go through policy enforcement later as part of accepting their invite after being restored.
+            -- This is an intentionally unhandled edge case for now.)
+            (OU.[Status] != 0 AND OU.[UserId] = @UserId)
 
-    DECLARE @UserEmail NVARCHAR(320)
-    SELECT @UserEmail = Email
-    FROM
-        [dbo].[UserView]
-    WHERE
-        Id = @UserId
-
-    ;WITH OrgUsers AS
-    (
-        SELECT
-            OU.[Id],
-            OU.[OrganizationId],
-            OU.[Type],
-            OU.[Status],
-            OU.[Permissions]
-        FROM
-            [dbo].[OrganizationUserView] OU
-        WHERE
-            (OU.[Status] <> 0 AND OU.[UserId] = @UserId)
-            OR (OU.[Status] = 0 AND OU.[Email] = @UserEmail AND @UserEmail IS NOT NULL)
-    ),
-    Providers AS
-    (
-        SELECT
-            OrganizationId
-        FROM
-            [dbo].[UserProviderAccessView]
-        WHERE
-            UserId = @UserId
-    )
-    SELECT
-        OU.[Id] AS [OrganizationUserId],
-        P.[OrganizationId],
-        P.[Type] AS [PolicyType],
-        P.[Data] AS [PolicyData],
-        OU.[Type] AS [OrganizationUserType],
-        OU.[Status] AS [OrganizationUserStatus],
-        OU.[Permissions] AS [OrganizationUserPermissionsData],
-        CASE WHEN PR.[OrganizationId] IS NULL THEN 0 ELSE 1 END AS [IsProvider]
-    FROM
-        [dbo].[PolicyView] P
-    INNER JOIN
-        [dbo].[OrganizationView] O ON P.[OrganizationId] = O.[Id]
-    INNER JOIN
-        OrgUsers OU ON P.[OrganizationId] = OU.[OrganizationId]
-    LEFT JOIN
-        Providers PR ON PR.[OrganizationId] = OU.[OrganizationId]
-    WHERE
-        P.[Enabled] = 1
-        AND O.[Enabled] = 1
-        AND O.[UsePolicies] = 1
+            -- 'Invited' OrgUsers are not linked to a UserId yet, so we have to look up their email
+            OR EXISTS (
+                SELECT 1
+                FROM [dbo].[UserView] U
+                WHERE U.[Id] = @UserId AND OU.[Email] = U.[Email] AND OU.[Status] = 0
+            )
+        )
 END
--- util/Migrator/DbScripts/2025-10-02_00_RefactorPolicyDetailsQueries.sql
+++ util/Migrator/DbScripts/2025-10-02_00_RefactorPolicyDetailsQueries.sql
@@ -9,66 +9,6 @@ INNER JOIN
     [dbo].[ProviderOrganizationView] PO ON PO.[ProviderId] = PU.[ProviderId]
 GO
 
-CREATE OR ALTER PROCEDURE [dbo].[PolicyDetails_ReadByUserId]
-    @UserId UNIQUEIDENTIFIER
-AS
-BEGIN
-    SET NOCOUNT ON
-
-    DECLARE @UserEmail NVARCHAR(320)
-    SELECT @UserEmail = Email
-    FROM
-        [dbo].[UserView]
-    WHERE
-        Id = @UserId
-
-    ;WITH OrgUsers AS
-    (
-        SELECT
-            OU.[Id],
-            OU.[OrganizationId],
-            OU.[Type],
-            OU.[Status],
-            OU.[Permissions]
-        FROM
-            [dbo].[OrganizationUserView] OU
-        WHERE
-            (OU.[Status] <> 0 AND OU.[UserId] = @UserId)
-            OR (OU.[Status] = 0 AND OU.[Email] = @UserEmail AND @UserEmail IS NOT NULL)
-    ),
-    Providers AS
-    (
-        SELECT
-            OrganizationId
-        FROM
-            [dbo].[UserProviderAccessView]
-        WHERE
-            UserId = @UserId
-    )
-    SELECT
-        OU.[Id] AS [OrganizationUserId],
-        P.[OrganizationId],
-        P.[Type] AS [PolicyType],
-        P.[Data] AS [PolicyData],
-        OU.[Type] AS [OrganizationUserType],
-        OU.[Status] AS [OrganizationUserStatus],
-        OU.[Permissions] AS [OrganizationUserPermissionsData],
-        CASE WHEN PR.[OrganizationId] IS NULL THEN 0 ELSE 1 END AS [IsProvider]
-    FROM
-        [dbo].[PolicyView] P
-    INNER JOIN
-        [dbo].[OrganizationView] O ON P.[OrganizationId] = O.[Id]
-    INNER JOIN
-        OrgUsers OU ON P.[OrganizationId] = OU.[OrganizationId]
-    LEFT JOIN
-        Providers PR ON PR.[OrganizationId] = OU.[OrganizationId]
-    WHERE
-        P.[Enabled] = 1
-        AND O.[Enabled] = 1
-        AND O.[UsePolicies] = 1
-END
-GO
-
 CREATE OR ALTER PROCEDURE [dbo].[OrganizationUser_ReadByUserIdWithPolicyDetails]
     @UserId UNIQUEIDENTIFIER,
     @PolicyType TINYINT
