0441e640c83e6817ef277190b4ff864d3818f2b8
Ike Kottlowski <ikottlowski@bitwarden.com>
2025-10-09 18:28:45 -0400

    feat: clean up optional inputs for hero component

--- src/Core/MailTemplates/Mjml/components/mj-bw-hero.js
+++ src/Core/MailTemplates/Mjml/components/mj-bw-hero.js
@@ -19,26 +19,9 @@ class MjBwHero extends BodyComponent {
   static defaultAttributes = {};
 
   render() {
+    let buttonElement = "";
     if (this.getAttribute("button-text") && this.getAttribute("button-url")) {
-      return this.renderMJML(`
-      <mj-section
-        full-width="full-width"
-        background-color="#175ddc"
-        border-radius="4px 4px 0px 0px"
-      >
-        <mj-column width="70%">
-          <mj-image
-            align="left"
-            src="https://bitwarden.com/images/logo-horizontal-white.png"
-            width="150px"
-            height="30px"
-          ></mj-image>
-          <mj-text color="#fff" padding-top="0" padding-bottom="0">
-            <h1 style="font-weight: normal; font-size: 24px; line-height: 32px">
-              ${this.getAttribute("title")}
-            </h1>
-          </mj-text>
-          <mj-button
+      buttonElement = `<mj-button
             href="${this.getAttribute("button-url")}"
             background-color="#fff"
             color="#1A41AC"
@@ -47,22 +30,18 @@ class MjBwHero extends BodyComponent {
           >
               ${this.getAttribute("button-text")}
             </mj-button
-          >
-        </mj-column>
-        <mj-column width="30%" vertical-align="bottom">
-          <mj-image
-            src="${this.getAttribute("img-src")}"
-            alt=""
-            width="140px"
-            height="140px"
-            padding="0px"
-            css-class="hide-small-img"
-          />
-        </mj-column>
-      </mj-section>
-    `);
-    } else {
-      return this.renderMJML(`
+          >`;
+    }
+    let subTitleElement = "";
+    if (this.getAttribute("sub-title")) {
+      subTitleElement = `<mj-text color="#fff" padding-top="0" padding-bottom="0">
+            <h2 style="font-weight: normal; font-size: 16px; line-height: 0px">
+              ${this.getAttribute("sub-title")}
+            </h2>
+          </mj-text>`;
+    }
+    return this.renderMJML(
+      `
       <mj-section
         full-width="full-width"
         background-color="#175ddc"
@@ -79,21 +58,26 @@ class MjBwHero extends BodyComponent {
             <h1 style="font-weight: normal; font-size: 24px; line-height: 32px">
               ${this.getAttribute("title")}
             </h1>
-          </mj-text>
+            ` +
+        subTitleElement +
+        `
+          </mj-text>` +
+        buttonElement +
+        `
         </mj-column>
         <mj-column width="30%" vertical-align="bottom">
           <mj-image
             src="${this.getAttribute("img-src")}"
             alt=""
-            width="140px"
-            height="140px"
+            width="155px"
+            height="155px"
             padding="0px"
             css-class="hide-small-img"
           />
         </mj-column>
       </mj-section>
-    `);
-    }
+    `,
+    );
   }
 }
 
