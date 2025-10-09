4e73a53ef89a43f2ea9d1332b675e45a0406e0da
Ike Kottlowski <ikottlowski@bitwarden.com>
2025-10-09 18:35:01 -0400

    fix: wrapping on small screens

--- src/Core/MailTemplates/Mjml/components/mj-bw-icon-row.js
+++ src/Core/MailTemplates/Mjml/components/mj-bw-icon-row.js
@@ -10,35 +10,38 @@ class MjBwIconRow extends BodyComponent {
     "icon-src": "string",
     "icon-alt": "string",
     "url-text": "string",
-    "url": "string",
-    "text": "string",
+    url: "string",
+    text: "string",
   };
 
   static defaultAttributes = {};
 
   render() {
-      return this.renderMJML(`
+    return this.renderMJML(`
       <mj-section background-color="#fff" padding="10px 20px">
-        <mj-column width="15%" vertical-align="middle">
-          <mj-image
-            src="${this.getAttribute("icon-src")}"
-            alt="${this.getAttribute("icon-alt")}"
-            width="50px"
-            padding="0"
-            border-radius="8px"
-          />
-        </mj-column>
-        <mj-column width="85%" vertical-align="middle">
-          <mj-text padding-left="0">
-            <a href="${this.getAttribute("url")}" class="link">
-              ${this.getAttribute("url-text")}
-              <span style="text-decoration: none">&#x2197;</span>
-            </a>
-          </mj-text>
-          <mj-text font-size="14px" padding-top="0" padding-left="0">
-            ${this.getAttribute("text")}
-          </mj-text>
-        </mj-column>
+        <mj-group>
+          <mj-column width="15%" vertical-align="middle">
+            <mj-image
+              src="${this.getAttribute("icon-src")}"
+              alt="${this.getAttribute("icon-alt")}"
+              width="50px"
+              padding="0"
+              padding-right="10px"
+              border-radius="8px"
+            />
+          </mj-column>
+          <mj-column width="85%" vertical-align="middle">
+            <mj-text padding-left="0">
+              <a href="${this.getAttribute("url")}" class="link">
+                ${this.getAttribute("url-text")}
+                <span style="text-decoration: none">&#x2197;</span>
+              </a>
+            </mj-text>
+            <mj-text font-size="14px" padding-top="0" padding-left="0">
+              ${this.getAttribute("text")}
+            </mj-text>
+          </mj-column>
+        </mj-group>
       </mj-section>
     `);
   }
