c14552c9caca6ffdf329e51f75d706f5bcba5d1d
Thomas Rittson <trittson@bitwarden.com>
2025-10-09 21:11:43 +1000

    undo unrelated change

--- src/Core/AdminConsole/OrganizationFeatures/Policies/Implementations/PolicyRequirementQuery.cs
+++ src/Core/AdminConsole/OrganizationFeatures/Policies/Implementations/PolicyRequirementQuery.cs
@@ -1,11 +1,9 @@
 ﻿#nullable enable
 
-using Bit.Core.AdminConsole.Entities;
 using Bit.Core.AdminConsole.Enums;
 using Bit.Core.AdminConsole.Models.Data.Organizations.Policies;
 using Bit.Core.AdminConsole.OrganizationFeatures.Policies.PolicyRequirements;
 using Bit.Core.AdminConsole.Repositories;
-using Bit.Core.Enums;
 
 namespace Bit.Core.AdminConsole.OrganizationFeatures.Policies.Implementations;
 
@@ -55,56 +53,4 @@ public class PolicyRequirementQuery(
 
     private async Task<IEnumerable<OrganizationPolicyDetails>> GetOrganizationPolicyDetails(Guid organizationId, PolicyType policyType)
         => await policyRepository.GetPolicyDetailsByOrganizationIdAsync(organizationId, policyType);
-}
-
-interface IEnforcedPolicy<T>
-
-/// <summary>
-/// Represents a combination of multiple organization policies that apply to a user.
-/// </summary>
-interface IUserEnforcedPolicy<T>
-{
-    public Guid UserId { get; set; }
-    public PolicyType PolicyType { get; set; }
-    public bool Enforced { get; set; }
-    public T Data { get; set; }
-}
-
-/// <summary>
-/// Represents a combination of multiple organization policies that apply to a user.
-/// </summary>
-interface UserEnforcedPolicy<T>
-{
-    public Guid UserId { get; set; }
-    public PolicyType PolicyType { get; set; }
-    public bool Enforced { get; set; }
-    public T Data { get; set; }
-}
-
-interface IEnforcedPolicyStrategy<T>
-{
-    /// <summary>
-    /// The policy type this is for
-    /// </summary>
-    public PolicyType PolicyType { set; }
-
-    /// <summary>
-    /// Whether providers are exempt
-    /// </summary>
-    public bool ExemptProviders { set; }
-
-    /// <summary>
-    /// Roles that should be exempt, if any
-    /// </summary>
-    public OrganizationUserType[] ExemptRoles { set; }
-
-    /// <summary>
-    /// OrganizationUser statuses in which the policy is enforced
-    /// </summary>
-    public OrganizationUserStatusType[] EnforcedStatuses { set; }
-
-    /// <summary>
-    /// A function that combines multiple policies
-    /// </summary>
-    public Func<Policy, UserEnforcedPolicy<T>> Reducer { get; }
 }
