f42cd8a971f1676b9885ca4e090ec16aec475b5c
cd-bitwarden <106776772+cd-bitwarden@users.noreply.github.com>
2025-10-10 17:29:21 -0400

    api repository and controller additions for SecretVersion table, as well as tests

---
+++ src/Core/SecretsManager/Repositories/ISecretVersionRepository.cs
@@ -0,0 +1,13 @@
+﻿#nullable enable
+
+using Bit.Core.SecretsManager.Entities;
+
+namespace Bit.Core.SecretsManager.Repositories;
+
+public interface ISecretVersionRepository
+{
+    Task<SecretVersion?> GetByIdAsync(Guid id);
+    Task<IEnumerable<SecretVersion>> GetManyBySecretIdAsync(Guid secretId);
+    Task<SecretVersion> CreateAsync(SecretVersion secretVersion);
+    Task DeleteManyByIdAsync(IEnumerable<Guid> ids);
+}
---
+++ src/Core/SecretsManager/Repositories/Noop/NoopSecretVersionRepository.cs
@@ -0,0 +1,28 @@
+﻿#nullable enable
+
+using Bit.Core.SecretsManager.Entities;
+
+namespace Bit.Core.SecretsManager.Repositories.Noop;
+
+public class NoopSecretVersionRepository : ISecretVersionRepository
+{
+    public Task<SecretVersion?> GetByIdAsync(Guid id)
+    {
+        return Task.FromResult(null as SecretVersion);
+    }
+
+    public Task<IEnumerable<SecretVersion>> GetManyBySecretIdAsync(Guid secretId)
+    {
+        return Task.FromResult(Enumerable.Empty<SecretVersion>());
+    }
+
+    public Task<SecretVersion> CreateAsync(SecretVersion secretVersion)
+    {
+        return Task.FromResult(secretVersion);
+    }
+
+    public Task DeleteManyByIdAsync(IEnumerable<Guid> ids)
+    {
+        return Task.CompletedTask;
+    }
+}
