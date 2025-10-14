36480dc4b4f30459f91ed177acf74861176f95e4
Nick Krantz <nick@livefront.com>
2025-10-13 22:05:09 -0500

    throw error if a user imports archived ciphers into an organization

--- src/Api/Tools/Controllers/ImportCiphersController.cs
+++ src/Api/Tools/Controllers/ImportCiphersController.cs
@@ -74,9 +74,14 @@ public class ImportCiphersController : Controller
             throw new BadRequestException("You cannot import this much data at once.");
         }
 
+        var archivedCiphers = model.Ciphers.Where(c => c.ArchivedDate.HasValue).ToList();
+        if (archivedCiphers.Any())
+        {
+            throw new BadRequestException("You cannot import archived items into an organization.");
+        }
+
         var orgId = new Guid(organizationId);
         var collections = model.Collections.Select(c => c.ToCollection(orgId)).ToList();
-
 
         //An User is allowed to import if CanCreate Collections or has AccessToImportExport
         var authorized = await CheckOrgImportPermission(collections, orgId);
@@ -156,7 +161,8 @@ public class ImportCiphersController : Controller
         if (existingCollections.Any() && (await _authorizationService.AuthorizeAsync(User, existingCollections, BulkCollectionOperations.ImportCiphers)).Succeeded)
         {
             return true;
-        };
+        }
+        ;
 
         return false;
     }
