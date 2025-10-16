c0eb412dcbe7f108e3643ccdf5d93e224f093f44
Alex Morask <amorask@bitwarden.com>
2025-10-16 08:42:36 -0500

    Undo temporary logging

--- src/Core/Billing/Payment/Clients/BitPayClient.cs
+++ src/Core/Billing/Payment/Clients/BitPayClient.cs
@@ -1,7 +1,6 @@
 ﻿using Bit.Core.Settings;
 using BitPayLight;
 using BitPayLight.Models.Invoice;
-using Microsoft.Extensions.Logging;
 
 namespace Bit.Core.Billing.Payment.Clients;
 
@@ -11,46 +10,15 @@ public interface IBitPayClient
     Task<Invoice> CreateInvoice(Invoice invoice);
 }
 
-public class BitPayClient : IBitPayClient
+public class BitPayClient(
+    GlobalSettings globalSettings) : IBitPayClient
 {
-    private readonly BitPay _bitPay;
-    private readonly ILogger<BitPayClient> _logger;
-
-    public BitPayClient(
-        GlobalSettings globalSettings,
-        ILogger<BitPayClient> logger)
-    {
-        _bitPay = new BitPay(
-            globalSettings.BitPay.Token, globalSettings.BitPay.Production ? Env.Prod : Env.Test);
-        _logger = logger;
-
-        TemporarilyLogBitPayConfiguration(globalSettings);
-    }
+    private readonly BitPay _bitPay = new(
+        globalSettings.BitPay.Token, globalSettings.BitPay.Production ? Env.Prod : Env.Test);
 
     public Task<Invoice> GetInvoice(string invoiceId)
         => _bitPay.GetInvoice(invoiceId);
 
     public Task<Invoice> CreateInvoice(Invoice invoice)
         => _bitPay.CreateInvoice(invoice);
-
-    private void TemporarilyLogBitPayConfiguration(GlobalSettings globalSettings)
-    {
-        switch (globalSettings.BitPay)
-        {
-            case null:
-                _logger.LogError("BitPay configuration is null");
-                break;
-            case { Token: null or "" }:
-                _logger.LogError("BitPay token is null or empty");
-                break;
-            case { Token.Length: > 5 }:
-                _logger.LogInformation("BitPay token: {Token}", globalSettings.BitPay.Token[..5]);
-                break;
-            default:
-                _logger.LogInformation("BitPay token: {Token}", globalSettings.BitPay.Token);
-                break;
-        }
-
-        _logger.LogInformation("BitPay production: {Production}", globalSettings.BitPay?.Production);
-    }
 }
