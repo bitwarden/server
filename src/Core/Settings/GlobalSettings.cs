3c816b2b950d53aa447aa797bf6c4402b5b4af76
Brant DeBow <brant@livefront.com>
2025-10-17 10:54:15 -0400

    Azure Service Bus Local Batching

--- src/Core/Settings/GlobalSettings.cs
+++ src/Core/Settings/GlobalSettings.cs
@@ -320,6 +320,8 @@ public class GlobalSettings : IGlobalSettings
 
             public virtual int DefaultMaxConcurrentCalls { get; set; } = 1;
             public virtual int DefaultPrefetchCount { get; set; } = 0;
+            public virtual int SenderBatchSize { get; set; } = 100;
+            public virtual int SenderFlushInterval { get; set; } = 50;
 
             public virtual string EventRepositorySubscriptionName { get; set; } = "events-write-subscription";
             public virtual string SlackEventSubscriptionName { get; set; } = "events-slack-subscription";
