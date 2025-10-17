f27766b53ea9dcd78d8e5482a0805d6c0f53e5a9
Rui Tome <rtome@bitwarden.com>
2025-10-17 12:03:16 +0100

    Refactor PolicyRequestModel, SavePolicyRequest, and PolicyUpdateRequestModel to utilize PolicyDataValidator for data validation and serialization, removing redundant methods and improving code clarity.

--- src/Api/AdminConsole/Models/Request/PolicyRequestModel.cs
+++ src/Api/AdminConsole/Models/Request/PolicyRequestModel.cs
@@ -1,12 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Text.Json;
 using Bit.Core.AdminConsole.Enums;
 using Bit.Core.AdminConsole.Models.Data;
-using Bit.Core.AdminConsole.Models.Data.Organizations.Policies;
 using Bit.Core.AdminConsole.OrganizationFeatures.Policies.Models;
+using Bit.Core.AdminConsole.Utilities;
 using Bit.Core.Context;
-using Bit.Core.Exceptions;
-using Bit.Core.Utilities;
 
 namespace Bit.Api.AdminConsole.Models.Request;
 
@@ -20,7 +17,7 @@ public class PolicyRequestModel
 
     public async Task<PolicyUpdate> ToPolicyUpdateAsync(Guid organizationId, ICurrentContext currentContext)
     {
-        var serializedData = ValidateAndSerializePolicyData();
+        var serializedData = PolicyDataValidator.ValidateAndSerialize(Data, Type!.Value);
         var performedBy = new StandardUser(currentContext.UserId!.Value, await currentContext.OrganizationOwner(organizationId));
 
         return new()
@@ -31,37 +28,5 @@ public class PolicyRequestModel
             Enabled = Enabled.GetValueOrDefault(),
             PerformedBy = performedBy
         };
-    }
-
-    private string? ValidateAndSerializePolicyData()
-    {
-        if (Data == null)
-        {
-            return null;
-        }
-
-        try
-        {
-            var json = JsonSerializer.Serialize(Data);
-
-            switch (Type!.Value)
-            {
-                case PolicyType.MasterPassword:
-                    CoreHelpers.LoadClassFromJsonData<MasterPasswordPolicyData>(json);
-                    break;
-                case PolicyType.SendOptions:
-                    CoreHelpers.LoadClassFromJsonData<SendOptionsPolicyData>(json);
-                    break;
-                case PolicyType.ResetPassword:
-                    CoreHelpers.LoadClassFromJsonData<ResetPasswordDataModel>(json);
-                    break;
-            }
-
-            return json;
-        }
-        catch
-        {
-            throw new BadRequestException($"Invalid data for {Type!.Value} policy.");
-        }
     }
 }
--- src/Api/AdminConsole/Models/Request/SavePolicyRequest.cs
+++ src/Api/AdminConsole/Models/Request/SavePolicyRequest.cs
@@ -1,10 +1,8 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Text.Json;
-using Bit.Core.AdminConsole.Enums;
 using Bit.Core.AdminConsole.Models.Data;
 using Bit.Core.AdminConsole.OrganizationFeatures.Policies.Models;
+using Bit.Core.AdminConsole.Utilities;
 using Bit.Core.Context;
-using Bit.Core.Utilities;
 
 namespace Bit.Api.AdminConsole.Models.Request;
 
@@ -18,33 +16,9 @@ public class SavePolicyRequest
     public async Task<SavePolicyModel> ToSavePolicyModelAsync(Guid organizationId, ICurrentContext currentContext)
     {
         var policyUpdate = await Policy.ToPolicyUpdateAsync(organizationId, currentContext);
-        var metadata = ValidateAndDeserializeMetadata();
+        var metadata = PolicyDataValidator.ValidateAndDeserializeMetadata(Metadata, Policy.Type!.Value);
         var performedBy = new StandardUser(currentContext.UserId!.Value, await currentContext.OrganizationOwner(organizationId));
 
         return new SavePolicyModel(policyUpdate, performedBy, metadata);
-    }
-
-    private IPolicyMetadataModel ValidateAndDeserializeMetadata()
-    {
-        if (Metadata == null)
-        {
-            return new EmptyMetadataModel();
-        }
-
-        try
-        {
-            var json = JsonSerializer.Serialize(Metadata);
-
-            return Policy.Type!.Value switch
-            {
-                PolicyType.OrganizationDataOwnership =>
-                    CoreHelpers.LoadClassFromJsonData<OrganizationModelOwnershipPolicyModel>(json),
-                _ => new EmptyMetadataModel()
-            };
-        }
-        catch
-        {
-            return new EmptyMetadataModel();
-        }
     }
 }
