962b9b2369e101ee0efe1d49dbac4d151446b939
enmande <3836813+enmande@users.noreply.github.com>
2025-10-22 10:25:50 -0400

    feat(base-request-validator) [PM-21153]: Add validation state model for composable validation scenarios.

---
+++ src/Identity/Models/BaseRequestValidationState.cs
@@ -0,0 +1,19 @@
+﻿namespace Bit.Identity.Models;
+
+public class BaseRequestValidationState
+{
+    /// <summary>
+    /// Validation methods, by name, which have passed validation in the current context.
+    /// </summary>
+    public List<string> CompletedValidators { get; set; } = [];
+    /// <summary>
+    /// Whether the user has requested a Remember Me token for their current device.
+    /// </summary>
+    public bool RememberMeRequested { get; set; } = false;
+    /// <summary>
+    /// Whether the user has requested recovery of their 2FA methods using their one-time
+    /// recovery code.
+    /// </summary>
+    /// <see cref="Bit.Core.Auth.Enums.TwoFactorProviderType"/>
+    public bool TwoFactorRecoveryRequested { get; set; } = false;
+}
