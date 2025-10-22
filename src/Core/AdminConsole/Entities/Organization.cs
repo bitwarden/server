de5b797139de9010792d30282a09e9c2d0f34be8
cd-bitwarden <106776772+cd-bitwarden@users.noreply.github.com>
2025-10-22 17:21:24 -0400

    fixing missing comment

--- src/Core/AdminConsole/Entities/Organization.cs
+++ src/Core/AdminConsole/Entities/Organization.cs
@@ -129,6 +129,7 @@ public class Organization : ITableObject<Guid>, IStorableSubscriber, IRevisable
     /// </summary>
     public bool SyncSeats { get; set; }
 
+    /// <summary>
     /// If set to true,  user accounts created within the organization are automatically confirmed without requiring additional verification steps.
     /// </summary>
     public bool UseAutomaticUserConfirmation { get; set; }
