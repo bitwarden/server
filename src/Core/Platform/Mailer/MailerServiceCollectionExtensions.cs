055ac5053a6cbe9f05122ddbcfddb83c7b2a4977
Hinton <hinton@users.noreply.github.com>
2025-10-27 10:09:33 +0100

    Fix charset

--- src/Core/Platform/Mailer/MailerServiceCollectionExtensions.cs
+++ src/Core/Platform/Mailer/MailerServiceCollectionExtensions.cs
@@ -1,4 +1,4 @@
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Bit.Core.Platform.Mailer;
