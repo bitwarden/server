bb3cc6273ea8bdbbeee273cce7829c6d1d41cb43
Todd Martin <tmartin@bitwarden.com>
2025-11-01 18:18:13 -0400

    Add test API endpoint

---
+++ src/Api/Platform/Cookies/CookieController.cs
@@ -0,0 +1,22 @@
+using Bit.Core.Utilities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Platform.Cookies;
+
+[Route("cookies")]
+[SelfHosted(SelfHostedOnly = true)]
+public class CookieController : ControllerBase
+{
+    [HttpGet("test")]
+    public IActionResult GetTestCookie()
+    {
+        var cookieValue = Request.Cookies["AWSELBAuthSessionCookie"];
+
+        if (string.IsNullOrEmpty(cookieValue))
+        {
+            return NotFound(new { message = "Cookie not found in request" });
+        }
+
+        return Ok(new { cookieValue });
+    }
+}
