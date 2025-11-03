ce596b23cfd14497b702c13a27c5cbef06ff3601
jrmccannon <jmccannon@bitwarden.com>
2025-11-03 08:45:35 -0600

    Updated XML comment

--- src/Core/AdminConsole/OrganizationFeatures/OrganizationUsers/Interfaces/IAutomaticallyConfirmOrganizationUserCommand.cs
+++ src/Core/AdminConsole/OrganizationFeatures/OrganizationUsers/Interfaces/IAutomaticallyConfirmOrganizationUserCommand.cs
@@ -6,6 +6,13 @@ namespace Bit.Core.AdminConsole.OrganizationFeatures.OrganizationUsers.Interface
 /// <summary>
 /// Command to automatically confirm an organization user.
 /// </summary>
+/// <remarks>
+/// The auto-confirm feature enables eligible client apps to confirm OrganizationUsers
+/// automatically via push notifications, eliminating the need for manual administrator
+/// intervention. Client apps receive a push notification, perform the required key exchange,
+/// and submit an auto-confirm request to the server. This command processes those
+/// client-initiated requests and should only be used in that specific context.
+/// </remarks>
 public interface IAutomaticallyConfirmOrganizationUserCommand
 {
     /// <summary>
@@ -13,8 +20,8 @@ public interface IAutomaticallyConfirmOrganizationUserCommand
     /// </summary>
     /// <param name="request">The request containing necessary information to confirm the organization user.</param>
     /// <returns>
-    /// The result of the command. If there was an error, result will contain a typed error describing the problem that
-    /// occurred.
+    /// The result of the command. If there was an error, the result will contain a typed error describing the problem
+    /// that occurred.
     /// </returns>
     Task<CommandResult> AutomaticallyConfirmOrganizationUserAsync(AutomaticallyConfirmOrganizationUserRequest request);
 }
