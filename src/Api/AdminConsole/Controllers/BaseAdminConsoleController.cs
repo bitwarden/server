084849a746fbf351595a3eb7b99600c426b1c549
jrmccannon <jmccannon@bitwarden.com>
2025-11-12 10:27:27 -0600

    Remove unused method.

--- src/Api/AdminConsole/Controllers/BaseAdminConsoleController.cs
+++ src/Api/AdminConsole/Controllers/BaseAdminConsoleController.cs
@@ -7,23 +7,6 @@ namespace Bit.Api.AdminConsole.Controllers;
 
 public abstract class BaseAdminConsoleController : Controller
 {
-    protected static IResult Handle<T>(CommandResult<T> commandResult, Func<T, IActionResult> resultSelector) =>
-        commandResult.Match<IResult>(
-            error => error switch
-            {
-                BadRequestError badRequest => TypedResults.BadRequest(new ErrorResponseModel(badRequest.Message)),
-                NotFoundError notFound => TypedResults.NotFound(new ErrorResponseModel(notFound.Message)),
-                InternalError internalError => TypedResults.Json(
-                    new ErrorResponseModel(internalError.Message),
-                    statusCode: StatusCodes.Status500InternalServerError),
-                _ => TypedResults.Json(
-                    new ErrorResponseModel(error.Message),
-                    statusCode: StatusCodes.Status500InternalServerError
-                )
-            },
-            success => Results.Ok(resultSelector(success))
-        );
-
     protected static IResult Handle(CommandResult commandResult) =>
         commandResult.Match<IResult>(
             error => error switch
