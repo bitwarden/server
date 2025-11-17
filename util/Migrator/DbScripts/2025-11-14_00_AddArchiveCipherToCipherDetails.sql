806ccb195fb2b9bf4e76c29bb12ead568ab98b26
jaasen-livefront <jaasen@livefront.com>
2025-11-17 15:55:29 -0800

    fix migration

--- util/Migrator/DbScripts/2025-11-14_00_AddArchiveCipherToCipherDetails.sql
+++ util/Migrator/DbScripts/2025-11-14_00_AddArchiveCipherToCipherDetails.sql
@@ -309,3 +309,60 @@ FROM
 WHERE
     C.[UserId] = @UserId;
 GO
+
+IF OBJECT_ID('[dbo].[CipherDetails_ReadByIdUserId]') IS NOT NULL
+BEGIN
+    DROP PROCEDURE [dbo].[CipherDetails_ReadByIdUserId];
+END
+GO
+
+CREATE PROCEDURE [dbo].[CipherDetails_ReadByIdUserId]
+    @Id UNIQUEIDENTIFIER,
+    @UserId UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT
+        ucd.Id,
+        ucd.UserId,
+        ucd.OrganizationId,
+        ucd.Type,
+        ucd.Data,
+        ucd.Attachments,
+        ucd.CreationDate,
+        ucd.RevisionDate,
+        ucd.Favorite,
+        ucd.FolderId,
+        ucd.DeletedDate,
+        ucd.Reprompt,
+        ucd.[Key],
+        ucd.OrganizationUseTotp,
+        MAX(ca.ArchivedDate) AS ArchivedDate,
+        MAX(ucd.Edit) AS Edit,
+        MAX(ucd.ViewPassword) AS ViewPassword,
+        MAX(ucd.Manage) AS Manage
+    FROM
+        [dbo].[UserCipherDetails](@UserId) AS ucd
+        LEFT JOIN [dbo].[CipherArchive] AS ca
+            ON ca.CipherId = ucd.Id
+           AND ca.UserId = @UserId
+    WHERE
+        ucd.Id = @Id
+    GROUP BY
+        ucd.Id,
+        ucd.UserId,
+        ucd.OrganizationId,
+        ucd.Type,
+        ucd.Data,
+        ucd.Attachments,
+        ucd.CreationDate,
+        ucd.RevisionDate,
+        ucd.Favorite,
+        ucd.FolderId,
+        ucd.DeletedDate,
+        ucd.Reprompt,
+        ucd.[Key],
+        ucd.OrganizationUseTotp;
+END
+GO
