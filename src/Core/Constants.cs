d81c61637e75e098e46688f966e8f25d464020e4
Nik Gilmore <ngilmore@bitwarden.com>
2025-11-19 15:31:34 -0800

    [PM-24314] Remove feature flag pm-9111-extension-persist-add-edit-form (#6561)

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -242,7 +242,6 @@ public static class FeatureFlagKeys
 
     /* Vault Team */
     public const string PM8851_BrowserOnboardingNudge = "pm-8851-browser-onboarding-nudge";
-    public const string PM9111ExtensionPersistAddEditForm = "pm-9111-extension-persist-add-edit-form";
     public const string CipherKeyEncryption = "cipher-key-encryption";
     public const string PM19941MigrateCipherDomainToSdk = "pm-19941-migrate-cipher-domain-to-sdk";
     public const string EndUserNotifications = "pm-10609-end-user-notifications";
