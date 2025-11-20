0bae14884e8a3d5e4751d571ec196feb0a6e2d9d
Ike <137194738+ike-kottlowski@users.noreply.github.com>
2025-11-20 14:25:46 -0500

    Update MjmlWelcomeEmailTemplates constant value
    update feature flag to be correct value

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -163,7 +163,7 @@ public static class FeatureFlagKeys
         "pm-23174-manage-account-recovery-permission-drives-the-need-to-set-master-password";
     public const string RecoveryCodeSupportForSsoRequiredUsers = "pm-21153-recovery-code-support-for-sso-required";
     public const string MJMLBasedEmailTemplates = "mjml-based-email-templates";
-    public const string MjmlWelcomeEmailTemplates = "mjml-welcome-email-templates";
+    public const string MjmlWelcomeEmailTemplates = "pm-21741-mjml-welcome-email";
     public const string MarketingInitiatedPremiumFlow = "pm-26140-marketing-initiated-premium-flow";
 
     /* Autofill Team */
