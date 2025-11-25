fe77d2df20d4c6f49db28c80506833acc94be1cd
Vincent Salucci <vincesalucci21@gmail.com>
2025-11-25 16:32:40 -0600

    chore: remove ff definition, refs PM-26427

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -142,7 +142,6 @@ public static class FeatureFlagKeys
     public const string PM23845_VNextApplicationCache = "pm-24957-refactor-memory-application-cache";
     public const string AccountRecoveryCommand = "pm-25581-prevent-provider-account-recovery";
     public const string BlockClaimedDomainAccountCreation = "pm-28297-block-uninvited-claimed-domain-registration";
-    public const string PolicyValidatorsRefactor = "pm-26423-refactor-policy-side-effects";
     public const string IncreaseBulkReinviteLimitForCloud = "pm-28251-increase-bulk-reinvite-limit-for-cloud";
 
     /* Architecture */
