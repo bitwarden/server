54a9914e5c98ace2f25147c8c8b12e7ee6488980
Bernd Schoolmann <mail@quexten.com>
2025-12-03 12:58:14 +0100

    Expand comment

--- src/Core/KeyManagement/Commands/Interfaces/ISetAccountKeysForUserCommand.cs
+++ src/Core/KeyManagement/Commands/Interfaces/ISetAccountKeysForUserCommand.cs
@@ -3,7 +3,7 @@
 namespace Bit.Core.KeyManagement.Commands.Interfaces;
 
 /// <summary>
-/// Command to set account keys for a new user that does not have keys yet.
+/// Command to set account keys for a new user that does not have keys yet. This supports both V1 user and V2 user initialization.
 /// This is intended for the TDE and Key-connector account registration flows.
 /// </summary>
 public interface ISetAccountKeysForUserCommand
