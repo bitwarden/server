42b26be3b4f2803845eff6697f13d6f03151226d
jaasen-livefront <jaasen@livefront.com>
2025-12-03 12:46:45 -0800

    fix migration file

--- util/Migrator/DbScripts/2025-12-02_AddCipherArchives.sql
+++ util/Migrator/DbScripts/2025-12-02_AddCipherArchives.sql
@@ -335,47 +335,6 @@ BEGIN
 END
 GO
 
-IF OBJECT_ID('[dbo].[Cipher_CreateWithCollections]') IS NOT NULL
-BEGIN
-    DROP PROCEDURE [dbo].[Cipher_CreateWithCollections];
-END
-GO
-
-CREATE PROCEDURE [dbo].[Cipher_CreateWithCollections]
-    @Id UNIQUEIDENTIFIER,
-    @UserId UNIQUEIDENTIFIER,
-    @OrganizationId UNIQUEIDENTIFIER,
-    @Type TINYINT,
-    @Data NVARCHAR(MAX),
-    @Favorites NVARCHAR(MAX),
-    @Folders NVARCHAR(MAX),
-    @Attachments NVARCHAR(MAX),
-    @CreationDate DATETIME2(7),
-    @RevisionDate DATETIME2(7),
-    @DeletedDate DATETIME2(7),
-    @Reprompt TINYINT,
-    @Key VARCHAR(MAX) = NULL,
-    @Archives NVARCHAR(MAX),
-    @ArchivedDate DATETIME2(7) = NULL,
-    @CollectionIds [dbo].[GuidIdArray] READONLY
-AS
-BEGIN
-    SET NOCOUNT ON
-
-    EXEC [dbo].[Cipher_Create] @Id, @UserId, @OrganizationId, @Type, @Data, @Favorites, @Folders,
-        @Attachments, @CreationDate, @RevisionDate, @DeletedDate, @Reprompt, @Key, @ArchivedDate, @Archives
-
-    DECLARE @UpdateCollectionsSuccess INT
-    EXEC @UpdateCollectionsSuccess = [dbo].[Cipher_UpdateCollections] @Id, @UserId, @OrganizationId, @CollectionIds
-
-    -- Bump the account revision date AFTER collections are assigned.
-    IF @UpdateCollectionsSuccess = 0
-    BEGIN
-        EXEC [dbo].[User_BumpAccountRevisionDateByCipherId] @Id, @OrganizationId
-    END
-END
-GO
-
 IF OBJECT_ID('[dbo].[Cipher_Update]') IS NOT NULL
 BEGIN
     DROP PROCEDURE [dbo].[Cipher_Update];
