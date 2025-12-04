357ecd2f4d24ee066366fcccf5f7d1f47604228d
Jimmy Vo <huynhmaivo82@gmail.com>
2025-12-04 11:29:11 -0500

    [PM-21742] wip

--- src/Core/MailTemplates/Mjml/components/mj-bw-icon-row.js
+++ src/Core/MailTemplates/Mjml/components/mj-bw-icon-row.js
@@ -20,14 +20,14 @@ class MjBwIconRow extends BodyComponent {
 
   componentHeadStyle = (breakpoint) => {
     return `
-      @media only screen and (max-width:${breakpoint}): {
-        ".mj-bw-icon-row-text": {
-          padding-left: "5px !important",
-          line-height: "20px",
-        },
-        ".mj-bw-icon-row": {
-          padding: "10px 15px",
-          width: "fit-content !important",
+      @media only screen and (max-width:${breakpoint}) {
+        .mj-bw-icon-row-text {
+          padding-left: 5px !important;
+          line-height: 20px;
+        }
+        .mj-bw-icon-row {
+          padding: 10px 15px;
+          width: fit-content !important;
         }
       }
     `;
@@ -36,30 +36,35 @@ class MjBwIconRow extends BodyComponent {
   render() {
     const headAnchorElement =
       this.getAttribute("head-url-text") && this.getAttribute("head-url")
-        ? `<a href="${this.getAttribute("head-url")}" class="link">
-                ${this.getAttribute("head-url-text")}
-                <span style="text-decoration: none">
-                  <img src="https://assets.bitwarden.com/email/v1/bwi-external-link-16px.png"
-                    alt="External Link Icon"
-                    width="16px"
-                    style="vertical-align: middle;"
-                  />
-                </span>
-              </a>`
+        ? `
+            <mj-text css-class="mj-bw-icon-row-text" padding="5px 10px 0px 10px">
+                <a href="${this.getAttribute("head-url")}" class="link">
+                    ${this.getAttribute("head-url-text")}
+                    <span style="text-decoration: none">
+                      <img src="https://assets.bitwarden.com/email/v1/bwi-external-link-16px.png"
+                        alt="External Link Icon"
+                        width="16px"
+                        style="vertical-align: middle;"
+                      />
+                    </span>
+                  </a>
+            </mj-text>`
         : "";
 
     const footAnchorElement =
       this.getAttribute("foot-url-text") && this.getAttribute("foot-url")
-        ? `<a href="${this.getAttribute("foot-url")}" class="link">
-                ${this.getAttribute("foot-url-text")}
-                <span style="text-decoration: none">
-                  <img src="https://assets.bitwarden.com/email/v1/bwi-external-link-16px.png"
-                    alt="External Link Icon"
-                    width="16px"
-                    style="vertical-align: middle;"
-                  />
-                </span>
-          </a>`
+        ? ` <mj-text css-class="mj-bw-icon-row-text" padding="5px 10px 0px 10px">
+                <a href="${this.getAttribute("foot-url")}" class="link">
+                    ${this.getAttribute("foot-url-text")}
+                    <span style="text-decoration: none">
+                      <img src="https://assets.bitwarden.com/email/v1/bwi-external-link-16px.png"
+                        alt="External Link Icon"
+                        width="16px"
+                        style="vertical-align: middle;"
+                      />
+                    </span>
+              </a>
+          </mj-text>`
         : "";
 
     return this.renderMJML(
@@ -76,19 +81,15 @@ class MjBwIconRow extends BodyComponent {
             />
           </mj-column>
           <mj-column width="85%" vertical-align="top">
-            <mj-text css-class="mj-bw-icon-row-text" padding="5px 10px 0px 10px">
-              ` +
-        headAnchorElement +
-        `
-              </mj-text>
+
+              `+ headAnchorElement +`
+
               <mj-text css-class="mj-bw-icon-row-text" padding="5px 10px 0px 10px">
                 ${this.getAttribute("text")}
               </mj-text>
-              <mj-text css-class="mj-bw-icon-row-text" padding="5px 10px 0px 10px">
-              ` +
-        footAnchorElement +
-        `
-              </mj-text>
+
+              `+ footAnchorElement +`
+
           </mj-column>
         </mj-group>
       </mj-section>
