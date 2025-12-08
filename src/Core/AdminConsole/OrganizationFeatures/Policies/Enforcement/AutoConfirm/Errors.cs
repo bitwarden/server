8878b8ef14747f4f98c11c5239aca0a8876599cc
Jared McCannon <jmccannon@bitwarden.com>
2025-12-08 11:23:58 -0600

    Unused class

--- src/Core/AdminConsole/OrganizationFeatures/Policies/Enforcement/AutoConfirm/Errors.cs
+++
@@ -1,5 +0,0 @@
-﻿using Bit.Core.AdminConsole.Utilities.v2;
-
-namespace Bit.Core.AdminConsole.OrganizationFeatures.Policies.Enforcement.AutoConfirm;
-
-public record AutoConfirmDoesNotAllowMembershipToOtherOrganizations() : BadRequestError("Automatic User Confirmation policy does not support membership to other organizations.");
