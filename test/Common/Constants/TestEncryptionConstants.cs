79256d73ee355229ed1e9ecec4885402877449d6
Patrick Pimentel <ppimentel@bitwarden.com>
2025-12-08 14:34:11 -0500

    fix(auth-validator): [PM-22975] Client Version Validator - Minor changes to test encryption constants.

--- test/Common/Constants/TestEncryptionConstants.cs
+++ test/Common/Constants/TestEncryptionConstants.cs
@@ -2,17 +2,18 @@
 
 public static class TestEncryptionConstants
 {
-    // Intended for use as a V1 encrypted string, accepted by validators
-    public const string AES256_CBC_HMAC_Encstring = "2.QmFzZTY0UGFydA==|QmFzZTY0UGFydA==|QmFzZTY0UGFydA==";
 
-    // Simple stubs for other encstring versions used by parsing tests
+    // Simple stubs for different encrypted string versions
     public const string AES256_CBC_B64_Encstring = "0.stub";
     public const string AES128_CBC_HMACSHA256_B64_Encstring = "1.stub";
     public const string AES256_CBC_HMAC_EmptySuffix = "2.";
+    // Intended for use as a V1 encrypted string, accepted by validators
+    public const string AES256_CBC_HMAC_Encstring = "2.QmFzZTY0UGFydA==|QmFzZTY0UGFydA==|QmFzZTY0UGFydA==";
     public const string RSA2048_OAEPSHA256_B64_Encstring = "3.stub";
     public const string RSA2048_OAEPSHA1_B64_Encstring = "4.stub";
     public const string RSA2048_OAEPSHA256_HMACSHA256_B64_Encstring = "5.stub";
     public const string RSA2048_OAEPSHA1_HMACSHA256_B64_Encstring = "6.stub";
+    public const string XCHACHA20POLY1305_B64_Encstring = "7.stub";
 
     // Public key test placeholder
     public const string PublicKey = "pk_test";
