03b043155b3f19967c0d0b7b19e2fd4ad3da039e
Rui Tome <rtome@bitwarden.com>
2025-12-12 10:38:10 +0000

    Refactor UserPremiumAccessView to use LEFT JOINs and GROUP BY for improved performance and clarity

--- src/Sql/dbo/Views/UserPremiumAccessView.sql
+++ src/Sql/dbo/Views/UserPremiumAccessView.sql
@@ -1,18 +1,21 @@
 CREATE VIEW [dbo].[UserPremiumAccessView]
 AS
-SELECT
+SELECT 
     U.[Id],
     U.[Premium] AS [PersonalPremium],
-    CAST(CASE 
-        WHEN EXISTS (
-            SELECT 1
-            FROM [dbo].[OrganizationUser] OU
-            INNER JOIN [dbo].[Organization] O ON O.[Id] = OU.[OrganizationId]
-            WHERE OU.[UserId] = U.[Id]
-                AND O.[UsersGetPremium] = 1
-                AND O.[Enabled] = 1
-        ) THEN 1 
-        ELSE 0 
-    END AS BIT) AS [OrganizationPremium]
-FROM
-    [dbo].[User] U;
+    CAST(
+        MAX(CASE 
+            WHEN O.[Id] IS NOT NULL THEN 1 
+            ELSE 0 
+        END) AS BIT
+    ) AS [OrganizationPremium]
+FROM 
+    [dbo].[User] U
+LEFT JOIN 
+    [dbo].[OrganizationUser] OU ON OU.[UserId] = U.[Id]
+LEFT JOIN 
+    [dbo].[Organization] O ON O.[Id] = OU.[OrganizationId]
+    AND O.[UsersGetPremium] = 1
+    AND O.[Enabled] = 1
+GROUP BY 
+    U.[Id], U.[Premium];
