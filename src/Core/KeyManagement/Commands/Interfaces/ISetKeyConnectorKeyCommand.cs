daee25da5fb22780f9fcc92159e50f5c507899be
Maciej Zieniuk <mzieniuk@bitwarden.com>
2025-12-12 15:34:11 +0000

    documentation

--- src/Core/KeyManagement/Commands/Interfaces/ISetKeyConnectorKeyCommand.cs
+++ src/Core/KeyManagement/Commands/Interfaces/ISetKeyConnectorKeyCommand.cs
@@ -3,6 +3,10 @@ using Bit.Core.KeyManagement.Models.Api.Request;
 
 namespace Bit.Core.KeyManagement.Commands.Interfaces;
 
+/// <summary>
+/// Creates the user key and account cryptographic state for a new user registering
+/// with Key Connector SSO configuration.
+/// </summary>
 public interface ISetKeyConnectorKeyCommand
 {
     Task SetKeyConnectorKeyForUserAsync(User user, SetKeyConnectorKeyRequestModel requestModel);
