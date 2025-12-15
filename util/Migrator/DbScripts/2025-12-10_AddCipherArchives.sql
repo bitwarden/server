3d99dae6df70f2a3e9dcde45cd0ea967b9ce8330
jaasen-livefront <jaasen@livefront.com>
2025-12-15 15:16:15 -0800

    wrap in brackets

--- util/Migrator/DbScripts/2025-12-10_AddCipherArchives.sql
+++ util/Migrator/DbScripts/2025-12-10_AddCipherArchives.sql
@@ -614,7 +614,7 @@ END
 GO
 
 -- Drop deprecated ArchivedDate column (EF already removed this for other providers)
-IF COL_LENGTH('dbo.Cipher', 'ArchivedDate') IS NOT NULL
+IF COL_LENGTH('[dbo].[Cipher]', 'ArchivedDate') IS NOT NULL
 BEGIN
     ALTER TABLE [dbo].[Cipher] DROP COLUMN [ArchivedDate];
 END;
