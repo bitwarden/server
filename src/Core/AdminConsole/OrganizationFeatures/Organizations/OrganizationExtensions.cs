78fe901d63a5f484131bb6e7935db2bba410f14f
Thomas Rittson <trittson@bitwarden.com>
2025-12-16 17:28:19 +1000

    Tweak comment

--- src/Core/AdminConsole/OrganizationFeatures/Organizations/OrganizationExtensions.cs
+++ src/Core/AdminConsole/OrganizationFeatures/Organizations/OrganizationExtensions.cs
@@ -6,8 +6,9 @@ public static class OrganizationExtensions
 {
     /// <summary>
     /// Updates the organization public and private keys if provided and not already set.
-    /// This is legacy code for old organizations that were not created with a public/private keypair. It is a soft
-    /// migration that will silently migrate organizations when they change their details or upgrade their plan.
+    /// This is legacy code for old organizations that were not created with a public/private keypair.
+    /// It is a soft migration that will silently migrate organizations when they perform certain actions,
+    /// e.g. change their details or upgrade their plan.
     /// </summary>
     public static void BackfillPublicPrivateKeys(this Organization organization, OrganizationKeyPair? keyPair)
     {
