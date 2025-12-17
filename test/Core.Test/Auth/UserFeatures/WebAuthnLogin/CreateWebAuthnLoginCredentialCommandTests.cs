8ebf8c59119656b61bd1025644ec091dd567f220
enmande <3836813+enmande@users.noreply.github.com>
2025-12-17 12:17:14 -0500

    Revert "test(webauthn) [PM-20109]: Update command tests to use global configs."
    This reverts commit ba9f0d5fb6cfc8ad1bb8812d150172df6a617a3f.

--- test/Core.Test/Auth/UserFeatures/WebAuthnLogin/CreateWebAuthnLoginCredentialCommandTests.cs
+++ test/Core.Test/Auth/UserFeatures/WebAuthnLogin/CreateWebAuthnLoginCredentialCommandTests.cs
@@ -3,7 +3,6 @@ using Bit.Core.Auth.Entities;
 using Bit.Core.Auth.Repositories;
 using Bit.Core.Auth.UserFeatures.WebAuthnLogin.Implementations;
 using Bit.Core.Entities;
-using Bit.Core.Services;
 using Bit.Test.Common.AutoFixture;
 using Bit.Test.Common.AutoFixture.Attributes;
 using Fido2NetLib;
@@ -11,7 +10,6 @@ using Fido2NetLib.Objects;
 using NSubstitute;
 using Xunit;
 using static Fido2NetLib.Fido2;
-using GlobalSettings = Bit.Core.Settings.GlobalSettings;
 
 namespace Bit.Core.Test.Auth.UserFeatures.WebAuthnLogin;
 
@@ -19,16 +17,10 @@ namespace Bit.Core.Test.Auth.UserFeatures.WebAuthnLogin;
 public class CreateWebAuthnLoginCredentialCommandTests
 {
     [Theory, BitAutoData]
-    internal async Task ExceedsExistingCredentialsLimit_ReturnsFalse(SutProvider<CreateWebAuthnLoginCredentialCommand> sutProvider, User user, CredentialCreateOptions options, AuthenticatorAttestationRawResponse response, Generator<WebAuthnCredential> credentialGenerator, bool userCanAccessPremium)
+    internal async Task ExceedsExistingCredentialsLimit_ReturnsFalse(SutProvider<CreateWebAuthnLoginCredentialCommand> sutProvider, User user, CredentialCreateOptions options, AuthenticatorAttestationRawResponse response, Generator<WebAuthnCredential> credentialGenerator)
     {
-        var webAuthNGlobalSettings = sutProvider.GetDependency<GlobalSettings>().WebAuthN = new GlobalSettings.WebAuthNSettings()
-        {
-            NonPremiumMaximumAllowedCredentials = 5,
-            PremiumMaximumAllowedCredentials = 10,
-        };
-        sutProvider.GetDependency<IUserService>().CanAccessPremium(user).Returns(userCanAccessPremium);
-        var maximumAllowedCredentialCount = userCanAccessPremium ? webAuthNGlobalSettings.PremiumMaximumAllowedCredentials : webAuthNGlobalSettings.NonPremiumMaximumAllowedCredentials;
-        var existingCredentials = credentialGenerator.Take(maximumAllowedCredentialCount).ToList();
+        // Arrange
+        var existingCredentials = credentialGenerator.Take(CreateWebAuthnLoginCredentialCommand.MaxCredentialsPerUser).ToList();
         sutProvider.GetDependency<IWebAuthnCredentialRepository>().GetManyByUserIdAsync(user.Id).Returns(existingCredentials);
 
         // Act
@@ -40,17 +32,10 @@ public class CreateWebAuthnLoginCredentialCommandTests
     }
 
     [Theory, BitAutoData]
-    internal async Task DoesNotExceedExistingCredentialsLimit_CreatesCredential(SutProvider<CreateWebAuthnLoginCredentialCommand> sutProvider, User user, CredentialCreateOptions options, AuthenticatorAttestationRawResponse response, Generator<WebAuthnCredential> credentialGenerator, bool userCanAccessPremium)
+    internal async Task DoesNotExceedExistingCredentialsLimit_CreatesCredential(SutProvider<CreateWebAuthnLoginCredentialCommand> sutProvider, User user, CredentialCreateOptions options, AuthenticatorAttestationRawResponse response, Generator<WebAuthnCredential> credentialGenerator)
     {
         // Arrange
-        var webAuthNGlobalSettings = sutProvider.GetDependency<GlobalSettings>().WebAuthN = new GlobalSettings.WebAuthNSettings()
-        {
-            NonPremiumMaximumAllowedCredentials = 5,
-            PremiumMaximumAllowedCredentials = 10,
-        };
-        sutProvider.GetDependency<IUserService>().CanAccessPremium(user).Returns(userCanAccessPremium);
-        var maximumAllowedCredentialCount = userCanAccessPremium ? webAuthNGlobalSettings.PremiumMaximumAllowedCredentials : webAuthNGlobalSettings.NonPremiumMaximumAllowedCredentials;
-        var existingCredentials = credentialGenerator.Take(maximumAllowedCredentialCount - 1).ToList();
+        var existingCredentials = credentialGenerator.Take(CreateWebAuthnLoginCredentialCommand.MaxCredentialsPerUser - 1).ToList();
         sutProvider.GetDependency<IWebAuthnCredentialRepository>().GetManyByUserIdAsync(user.Id).Returns(existingCredentials);
         sutProvider.GetDependency<IFido2>().MakeNewCredentialAsync(
             response, options, Arg.Any<IsCredentialIdUniqueToUserAsyncDelegate>(), Arg.Any<byte[]>(), Arg.Any<CancellationToken>()
