4f4d63d4cf056900592ca4b9783c61242bd90b18
John Harrington <84741727+harr1424@users.noreply.github.com>
2025-12-18 09:20:29 -0700

    dotnet format

--- util/PostgresMigrations/Migrations/20251218153210_SendAuthTypeAndEmailLength.cs
+++ util/PostgresMigrations/Migrations/20251218153210_SendAuthTypeAndEmailLength.cs
@@ -2,39 +2,38 @@
 
 #nullable disable
 
-namespace Bit.PostgresMigrations.Migrations
+namespace Bit.PostgresMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class SendAuthTypeAndEmailLength : Migration
 {
     /// <inheritdoc />
-    public partial class SendAuthTypeAndEmailLength : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<string>(
-                name: "Emails",
-                table: "Send",
-                type: "character varying(4000)",
-                maxLength: 4000,
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "character varying(1024)",
-                oldMaxLength: 1024,
-                oldNullable: true);
-        }
+        migrationBuilder.AlterColumn<string>(
+            name: "Emails",
+            table: "Send",
+            type: "character varying(4000)",
+            maxLength: 4000,
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "character varying(1024)",
+            oldMaxLength: 1024,
+            oldNullable: true);
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<string>(
-                name: "Emails",
-                table: "Send",
-                type: "character varying(1024)",
-                maxLength: 1024,
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "character varying(4000)",
-                oldMaxLength: 4000,
-                oldNullable: true);
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<string>(
+            name: "Emails",
+            table: "Send",
+            type: "character varying(1024)",
+            maxLength: 1024,
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "character varying(4000)",
+            oldMaxLength: 4000,
+            oldNullable: true);
     }
 }
