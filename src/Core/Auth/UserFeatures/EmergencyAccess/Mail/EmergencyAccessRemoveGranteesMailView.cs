b1fc9470b0c444af8d242471ac19d7492a6770aa
enmande <3836813+enmande@users.noreply.github.com>
2025-12-31 08:25:53 -0500

    feat(emergency-access) [PM-29584]: Add compiled EmergencyAccess templates.

---
+++ src/Core/Auth/UserFeatures/EmergencyAccess/Mail/EmergencyAccessRemoveGranteesMailView.cs
@@ -0,0 +1,14 @@
+﻿using Bit.Core.Platform.Mail.Mailer;
+
+namespace Bit.Core.Auth.UserFeatures.EmergencyAccess.Mail;
+
+public class EmergencyAccessRemoveGranteesMailView : BaseMailView
+{
+    public required IEnumerable<string> RemovedGranteeNames { get; set; }
+    public required string WebVaultUrl { get; set; }
+}
+
+public class EmergencyAccessRemoveGranteesMail : BaseMail<EmergencyAccessRemoveGranteesMailView>
+{
+    public override string Subject => "Emergency contacts removed";
+}
