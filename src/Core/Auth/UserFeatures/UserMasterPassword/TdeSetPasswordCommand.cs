2e92a53f1129679c80d889b886f55914a0234555
Maciej Zieniuk <167752252+mzieniukbw@users.noreply.github.com>
2026-01-09 09:17:45 +0100

    [PM-27281] Support v2 account encryption on JIT master password signups (#6777)
    * V2 prep, rename existing SSO JIT MP command to V1
    
    * set initial master password for account registraton V2
    
    * later removel docs
    
    * TDE MP onboarding split
    
    * revert separate TDE onboarding controller api
    
    * Server side hash of the user master password hash
    
    * use `ValidationResult` instead for validation errors
    
    * unit test coverage
    
    * integration test coverage
    
    * update sql migration script date
    
    * revert validate password change
    
    * better requests validation
    
    * explicit error message when org sso identifier invalid
    
    * more unit test coverage
    
    * renamed onboarding to set, hash naming clarifications
    
    * update db sql script, formatting
    
    * use raw json as request instead of request models for integration test
    
    * v1 integration test coverage
    
    * change of name

--- src/Core/Auth/UserFeatures/UserMasterPassword/TdeSetPasswordCommand.cs
+++ src/Core/Auth/UserFeatures/UserMasterPassword/TdeSetPasswordCommand.cs
@@ -3,48 +3,41 @@ using Bit.Core.Auth.UserFeatures.UserMasterPassword.Interfaces;
 using Bit.Core.Entities;
 using Bit.Core.Enums;
 using Bit.Core.Exceptions;
-using Bit.Core.OrganizationFeatures.OrganizationUsers.Interfaces;
 using Bit.Core.Repositories;
 using Bit.Core.Services;
 using Microsoft.AspNetCore.Identity;
 
 namespace Bit.Core.Auth.UserFeatures.UserMasterPassword;
 
-public class SetInitialMasterPasswordCommand : ISetInitialMasterPasswordCommand
+public class TdeSetPasswordCommand : ITdeSetPasswordCommand
 {
-    private readonly IUserService _userService;
     private readonly IUserRepository _userRepository;
-    private readonly IAcceptOrgUserCommand _acceptOrgUserCommand;
     private readonly IOrganizationUserRepository _organizationUserRepository;
     private readonly IOrganizationRepository _organizationRepository;
     private readonly IPasswordHasher<User> _passwordHasher;
     private readonly IEventService _eventService;
 
-    public SetInitialMasterPasswordCommand(IUserService userService, IUserRepository userRepository,
-        IAcceptOrgUserCommand acceptOrgUserCommand, IOrganizationUserRepository organizationUserRepository,
-        IOrganizationRepository organizationRepository, IPasswordHasher<User> passwordHasher,
-        IEventService eventService)
+    public TdeSetPasswordCommand(IUserRepository userRepository,
+        IOrganizationUserRepository organizationUserRepository, IOrganizationRepository organizationRepository,
+        IPasswordHasher<User> passwordHasher, IEventService eventService)
     {
-        _userService = userService;
         _userRepository = userRepository;
-        _acceptOrgUserCommand = acceptOrgUserCommand;
         _organizationUserRepository = organizationUserRepository;
         _organizationRepository = organizationRepository;
         _passwordHasher = passwordHasher;
         _eventService = eventService;
     }
 
-    public async Task SetInitialMasterPasswordAsync(User user,
-        SetInitialMasterPasswordDataModel masterPasswordDataModel)
+    public async Task SetMasterPasswordAsync(User user, SetInitialMasterPasswordDataModel masterPasswordDataModel)
     {
         if (user.Key != null)
         {
             throw new BadRequestException("User already has a master password set.");
         }
 
-        if (masterPasswordDataModel.AccountKeys == null)
+        if (user.PublicKey == null || user.PrivateKey == null)
         {
-            throw new BadRequestException("Account keys are required.");
+            throw new BadRequestException("TDE user account keys must be set before setting initial master password.");
         }
 
         // Prevent a de-synced salt value from creating an un-decryptable unlock method
@@ -70,11 +63,8 @@ public class SetInitialMasterPasswordCommand : ISetInitialMasterPasswordCommand
         var setMasterPasswordTask = _userRepository.SetMasterPassword(user.Id,
             masterPasswordDataModel.MasterPasswordUnlock, serverSideHashedMasterPasswordAuthenticationHash,
             masterPasswordDataModel.MasterPasswordHint);
-        await _userRepository.SetV2AccountCryptographicStateAsync(user.Id, masterPasswordDataModel.AccountKeys,
-            [setMasterPasswordTask]);
+        await _userRepository.UpdateUserDataAsync([setMasterPasswordTask]);
 
         await _eventService.LogUserEventAsync(user.Id, EventType.User_ChangedPassword);
-
-        await _acceptOrgUserCommand.AcceptOrgUserAsync(orgUser, user, _userService);
     }
 }
