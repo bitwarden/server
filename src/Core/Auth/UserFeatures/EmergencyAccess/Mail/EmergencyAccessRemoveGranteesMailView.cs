63e78287c52cf7a710f33244851932bea831ec95
enmande <3836813+enmande@users.noreply.github.com>
2026-01-09 13:50:58 -0500

    feat(emergency-access) [PM-29584]: Update link for help pages, not web vault.

--- src/Core/Auth/UserFeatures/EmergencyAccess/Mail/EmergencyAccessRemoveGranteesMailView.cs
+++ src/Core/Auth/UserFeatures/EmergencyAccess/Mail/EmergencyAccessRemoveGranteesMailView.cs
@@ -5,10 +5,10 @@ namespace Bit.Core.Auth.UserFeatures.EmergencyAccess.Mail;
 public class EmergencyAccessRemoveGranteesMailView : BaseMailView
 {
     public required IEnumerable<string> RemovedGranteeNames { get; set; }
-    public required string WebVaultUrl { get; set; }
+    public string EmergencyAccessHelpPageUrl => "https://bitwarden.com/help/emergency-access/";
 }
 
 public class EmergencyAccessRemoveGranteesMail : BaseMail<EmergencyAccessRemoveGranteesMailView>
 {
-    public override string Subject => "Emergency contacts removed";
+    public override string Subject { get; set; } = "Emergency contacts removed";
 }
