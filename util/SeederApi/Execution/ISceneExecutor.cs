f144828a8775af4f32ae62ecd0179b8fac893a87
Oscar Hinton <Hinton@users.noreply.github.com>
2026-01-13 18:10:01 +0100

    [PM-22263] [PM-29849] Initial PoC of seeder API (#6424)
    We want to reduce the amount of business critical test data in the company. One way of doing that is to generate test data on demand prior to client side testing.
    
    Clients will request a scene to be set up with a JSON body set of options, specific to a given scene. Successful seed requests will be responded to with a mangleMap which maps magic strings present in the request to the mangled, non-colliding versions inserted into the database. This way, the server is solely responsible for understanding uniqueness requirements in the database. scenes also are able to return custom data, depending on the scene. For example, user creation would benefit from a return value of the userId for further test setup on the client side.
    
    Clients will indicate they are running tests by including a unique header, x-play-id which specifies a unique testing context. The server uses this PlayId as the seed for any mangling that occurs. This allows the client to decide it will reuse a given PlayId if the test context builds on top of previously executed tests. When a given context is no longer needed, the API user will delete all test data associated with the PlayId by calling a delete endpoint.
    
    ---------
    
    Co-authored-by: Matt Gibson <mgibson@bitwarden.com>

---
+++ util/SeederApi/Execution/ISceneExecutor.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using Bit.SeederApi.Models.Response;
+
+namespace Bit.SeederApi.Execution;
+
+/// <summary>
+/// Executor for dynamically resolving and executing scenes by template name.
+/// This is an infrastructure component that orchestrates scene execution,
+/// not a domain-level command.
+/// </summary>
+public interface ISceneExecutor
+{
+    /// <summary>
+    /// Executes a scene with the given template name and arguments.
+    /// </summary>
+    /// <param name="templateName">The name of the scene template (e.g., "SingleUserScene")</param>
+    /// <param name="arguments">Optional JSON arguments to pass to the scene's Seed method</param>
+    /// <returns>A scene response model containing the result and mangle map</returns>
+    /// <exception cref="Services.SceneNotFoundException">Thrown when the scene template is not found</exception>
+    /// <exception cref="Services.SceneExecutionException">Thrown when there's an error executing the scene</exception>
+    Task<SceneResponseModel> ExecuteAsync(string templateName, JsonElement? arguments);
+}
