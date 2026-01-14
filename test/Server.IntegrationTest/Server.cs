fd67255482d897d59f0cf7ceb2c59dc705aa2835
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2026-01-13 20:34:11 -0500

    Add some integration tests for the Server project

---
+++ test/Server.IntegrationTest/Server.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Bit.Server.IntegrationTest;
+
+public class Server : WebApplicationFactory<Program>
+{
+    public string? ContentRoot { get; set; }
+    public string? WebRoot { get; set; }
+    public bool ServeUnknown { get; set; }
+    public bool? WebVault { get; set; }
+    public string? AppIdLocation { get; set; }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        base.ConfigureWebHost(builder);
+
+        builder.ConfigureLogging(logging =>
+        {
+            logging.SetMinimumLevel(LogLevel.Debug);
+        });
+
+        var config = new Dictionary<string, string?>
+        {
+            {"contentRoot", ContentRoot},
+            {"webRoot", WebRoot},
+            {"serveUnknown", ServeUnknown.ToString().ToLowerInvariant()},
+        };
+
+        if (WebVault.HasValue)
+        {
+            config["webVault"] = WebVault.Value.ToString().ToLowerInvariant();
+        }
+
+        if (!string.IsNullOrEmpty(AppIdLocation))
+        {
+            config["appIdLocation"] = AppIdLocation;
+        }
+
+        builder.UseConfiguration(new ConfigurationBuilder().AddInMemoryCollection(config).Build());
+    }
+}
