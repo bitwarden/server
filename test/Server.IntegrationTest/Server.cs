6c844ea581061bde830be0997eef201cd734a5fb
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2026-01-14 09:14:21 -0500

    Format

--- test/Server.IntegrationTest/Server.cs
+++ test/Server.IntegrationTest/Server.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
--- test/Server.IntegrationTest/ServerTests.cs
+++ test/Server.IntegrationTest/ServerTests.cs
@@ -1,4 +1,4 @@
-using System.Net;
+﻿using System.Net;
 using System.Runtime.CompilerServices;
 
 namespace Bit.Server.IntegrationTest;
