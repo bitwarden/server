be1e952e1b157795982dc0ec1eacd89f8272e76b
Jared McCannon <jmccannon@bitwarden.com>
2026-01-20 15:08:36 -0600

    Committing feature flag and request model.

---
+++ src/Api/AdminConsole/Models/Request/Organizations/OrganizationUserRestoreRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Bit.Api.AdminConsole.Models.Request.Organizations;
+
+public class OrganizationUserRestoreRequest
+{
+    public string DefaultUserCollectionName { get; set; } = string.Empty;
+}
