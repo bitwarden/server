bf9596805039481629c1113a00fda1cdc55ed58d
Jared McCannon <jmccannon@bitwarden.com>
2026-01-21 11:04:39 -0600

    fix for tests.

--- src/Api/AdminConsole/Models/Request/Organizations/OrganizationUserRestoreRequest.cs
+++ src/Api/AdminConsole/Models/Request/Organizations/OrganizationUserRestoreRequest.cs
@@ -2,5 +2,8 @@
 
 public class OrganizationUserRestoreRequest
 {
+    /// <summary>
+    /// This is the encrypted default collection name to be used for restored users if required
+    /// </summary>
     public string DefaultUserCollectionName { get; set; } = string.Empty;
 }
