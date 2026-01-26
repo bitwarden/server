fc2fb5d8ba731c31ded3034141521ab0d210dc94
Jacob Olness <jolness1@gmail.com>
2026-01-26 14:00:44 -0700

    Update src/Core/Auth/Identity/TokenProviders/WebAuthnTokenProvider.cs
    Co-authored-by: Dave <3836813+enmande@users.noreply.github.com>

--- src/Core/Auth/Identity/TokenProviders/WebAuthnTokenProvider.cs
+++ src/Core/Auth/Identity/TokenProviders/WebAuthnTokenProvider.cs
@@ -148,8 +148,7 @@ public class WebAuthnTokenProvider : IUserTwoFactorTokenProvider<User>
         }
 
         // Load all WebAuthn credentials stored in metadata. The number of allowed credentials
-        // is controlled by PremiumMaximumAllowedCredentials and NonPremiumMaximumAllowedCredentials
-        // in GlobalSettings.WebAuthn (10 for premium, 5 for non-premium).
+        // is controlled by credential registration.
         for (var i = 1; i <=  provider.MetaData.Count(k => k.Key.StartsWith("Key")); i++)
         {
             var keyName = $"Key{i}";
