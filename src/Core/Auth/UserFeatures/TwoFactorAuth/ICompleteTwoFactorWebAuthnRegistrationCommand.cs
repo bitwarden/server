05dda7aefd5df46d0991c14eb14526e21a4089d8
enmande <3836813+enmande@users.noreply.github.com>
2026-01-28 21:13:20 -0500

    refctor(2fa-webauthn) [PM-29890]: Re-spell docs.

--- src/Core/Auth/UserFeatures/TwoFactorAuth/ICompleteTwoFactorWebAuthnRegistrationCommand.cs
+++ src/Core/Auth/UserFeatures/TwoFactorAuth/ICompleteTwoFactorWebAuthnRegistrationCommand.cs
@@ -11,7 +11,7 @@ public interface ICompleteTwoFactorWebAuthnRegistrationCommand
     /// <param name="user">The current user.</param>
     /// <param name="id">ID for the Key credential to complete.</param>
     /// <param name="name">Name for the Key credential to complete.</param>
-    /// <param name="attestationResponse">Fido2 attestation response.</param>
+    /// <param name="attestationResponse">WebAuthn attestation response.</param>
     /// <returns>Whether persisting the credential was successful.</returns>
     Task<bool> CompleteTwoFactorWebAuthnRegistrationAsync(User user, int id, string name,
         AuthenticatorAttestationRawResponse attestationResponse);
