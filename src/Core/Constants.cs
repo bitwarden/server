f870165828e7fddb68daddfd904dd08696882fb9
Jackson Engstrom <jengstrom@bitwarden.com>
2026-02-03 12:52:13 -0800

    adds feature flag pm-30521-autofill-button-view-login-screen

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -253,6 +253,7 @@ public static class FeatureFlagKeys
     public const string BrowserPremiumSpotlight = "pm-23384-browser-premium-spotlight";
     public const string MigrateMyVaultToMyItems = "pm-20558-migrate-myvault-to-myitems";
     public const string PM27632_CipherCrudOperationsToSdk = "pm-27632-cipher-crud-operations-to-sdk";
+    public const string PM30521_AutofillButtonViewLoginScreen = "pm-30521-autofill-button-view-login-screen";
 
     /* Innovation Team */
     public const string ArchiveVaultItems = "pm-19148-innovation-archive";
