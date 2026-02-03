68ac7bf1231e04c22f4747fcb3689ea0b4112430
Rui Tome <rtome@bitwarden.com>
2026-02-03 15:41:04 +0000

    Add new feature flag for updating the join organization email templates

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -144,6 +144,7 @@ public static class FeatureFlagKeys
     public const string DefaultUserCollectionRestore = "pm-30883-my-items-restored-users";
     public const string PremiumAccessQuery = "pm-29495-refactor-premium-interface";
     public const string RefactorMembersComponent = "pm-29503-refactor-members-inheritance";
+    public const string UpdateJoinOrganizationEmailTemplate = "pm-28396-update-join-organization-email-template";
 
     /* Architecture */
     public const string DesktopMigrationMilestone1 = "desktop-ui-migration-milestone-1";
