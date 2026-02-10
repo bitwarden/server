0d2ed4b57b0a5c66a158c22abcef689be1743c48
Ike Kottlowski <ikottlowski@bitwarden.com>
2026-02-10 16:20:46 -0500

    feat: update finalization to use a constraint that enforces the existence of the masterpasswordsalt if there is a masterpassword.

--- util/Migrator/DbScripts_finalization/2026-02-03_00_AlterUserMasterPasswordSaltNotNull.sql
+++ util/Migrator/DbScripts_finalization/2026-02-03_00_AlterUserMasterPasswordSaltNotNull.sql
@@ -7,8 +7,10 @@
 -- Guard: refuse to finalize if any NULLs remain
 IF EXISTS (SELECT 1
 FROM [dbo].[User]
-WHERE [MasterPasswordSalt] IS NULL)
+WHERE
+    [MasterPassword] IS NOT NULL AND
+    [MasterPasswordSalt] IS NULL)
     THROW 50000, 'MasterPasswordSalt contains NULLs; cannot finalize.', 1;
 
-ALTER TABLE [dbo].[User] ALTER COLUMN [MasterPasswordSalt] NVARCHAR(256) NOT NULL;
+ALTER TABLE [dbo].[User] ADD CONSTRAINT [CK_User_MasterPasswordSalt_Required] CHECK ([MasterPassword] IS NULL OR [MasterPasswordSalt] IS NOT NULL);
 GO
