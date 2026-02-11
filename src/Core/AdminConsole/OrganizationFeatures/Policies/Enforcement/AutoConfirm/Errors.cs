0566de90d62fbf09046eca0465c479889827695d
Jared McCannon <jmccannon@bitwarden.com>
2026-02-11 09:59:18 -0600

    [PM-27145] - Block Auto Confirm Enable Admin Portal (#6981)
    * Extracted policy compliance checking for the organization out and added a check when attempting to enable auto user confirm via Admin Portal
    
    * Moved injection order. Fixed error message.

---
+++ src/Core/AdminConsole/OrganizationFeatures/Policies/Enforcement/AutoConfirm/Errors.cs
@@ -0,0 +1,7 @@
+﻿using Bit.Core.AdminConsole.Utilities.v2;
+
+namespace Bit.Core.AdminConsole.OrganizationFeatures.Policies.Enforcement.AutoConfirm;
+
+public record UserNotCompliantWithSingleOrganization() : BadRequestError("All organization users must be compliant with the Single organization policy before enabling the Automatically confirm invited users policy. Please remove users who are members of multiple organizations.");
+
+public record ProviderExistsInOrganization() : BadRequestError("The organization has users with the Provider user type. Please remove provider users before enabling the Automatically confirm invited users policy.");
