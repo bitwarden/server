9dacedbd442922a022752f16f0ec378743406afe
Cy Okeke <cokeke@bitwarden.com>
2026-02-12 13:10:02 +0100

    Fix the build lint error

--- src/Core/Billing/Services/Implementations/SubscriptionDiscountService.cs
+++ src/Core/Billing/Services/Implementations/SubscriptionDiscountService.cs
@@ -1,4 +1,4 @@
-#nullable enable
+﻿#nullable enable
 
 using Bit.Core.Billing.Enums;
 using Bit.Core.Billing.Subscriptions.Repositories;
--- test/Core.Test/Billing/Organizations/Commands/PreviewOrganizationTaxCommandTests.cs
+++ test/Core.Test/Billing/Organizations/Commands/PreviewOrganizationTaxCommandTests.cs
@@ -1,4 +1,4 @@
-using Bit.Core.AdminConsole.Entities;
+﻿using Bit.Core.AdminConsole.Entities;
 using Bit.Core.Billing.Enums;
 using Bit.Core.Billing.Organizations.Commands;
 using Bit.Core.Billing.Organizations.Models;
