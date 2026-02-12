015da668022a534bb060453845b0d00a42ba6685
Ike Kottlowski <ikottlowski@bitwarden.com>
2026-02-12 16:08:58 -0500

    feat: update User_UpdateMasterPassword stored procedure to include the MasterPasswordSalt

--- util/Migrator/DbScripts/2026-02-11_00_AlterUserAddMasterPasswordSalt.sql
+++ util/Migrator/DbScripts/2026-02-11_00_AlterUserAddMasterPasswordSalt.sql
@@ -337,3 +337,37 @@ BEGIN
         [Id] = @Id
 END
 GO
+
+CREATE OR ALTER PROCEDURE [dbo].[User_UpdateMasterPassword]
+    @Id UNIQUEIDENTIFIER,
+    @MasterPassword NVARCHAR(300),
+    @MasterPasswordHint NVARCHAR(50) = NULL,
+    @Key VARCHAR(MAX),
+    @Kdf TINYINT,
+    @KdfIterations INT,
+    @KdfMemory INT = NULL,
+    @KdfParallelism INT = NULL,
+    @RevisionDate DATETIME2(7),
+    @AccountRevisionDate DATETIME2(7),
+    @MasterPasswordSalt NVARCHAR(256) = NULL
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    UPDATE
+        [dbo].[User]
+    SET
+        [MasterPassword] = @MasterPassword,
+        [MasterPasswordHint] = @MasterPasswordHint,
+        [Key] = @Key,
+        [Kdf] = @Kdf,
+        [KdfIterations] = @KdfIterations,
+        [KdfMemory] = @KdfMemory,
+        [KdfParallelism] = @KdfParallelism,
+        [RevisionDate] = @RevisionDate,
+        [AccountRevisionDate] = @AccountRevisionDate,
+        [MasterPasswordSalt] = [User].[Email]
+    WHERE
+        [Id] = @Id
+END
+GO
