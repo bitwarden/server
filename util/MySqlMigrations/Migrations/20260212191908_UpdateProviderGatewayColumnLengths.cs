37bc1b324e7b130e9b1a7d866a5df3bceee14889
Alex Morask <amorask@bitwarden.com>
2026-02-13 11:01:46 -0600

    chore: run dotnet format

--- util/MySqlMigrations/Migrations/20260212191908_UpdateProviderGatewayColumnLengths.cs
+++ util/MySqlMigrations/Migrations/20260212191908_UpdateProviderGatewayColumnLengths.cs
@@ -2,65 +2,64 @@
 
 #nullable disable
 
-namespace Bit.MySqlMigrations.Migrations
+namespace Bit.MySqlMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class UpdateProviderGatewayColumnLengths : Migration
 {
     /// <inheritdoc />
-    public partial class UpdateProviderGatewayColumnLengths : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<string>(
-                name: "GatewaySubscriptionId",
-                table: "Provider",
-                type: "varchar(50)",
-                maxLength: 50,
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "varchar(255)",
-                oldNullable: true)
-                .Annotation("MySql:CharSet", "utf8mb4")
-                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        migrationBuilder.AlterColumn<string>(
+            name: "GatewaySubscriptionId",
+            table: "Provider",
+            type: "varchar(50)",
+            maxLength: 50,
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "varchar(255)",
+            oldNullable: true)
+            .Annotation("MySql:CharSet", "utf8mb4")
+            .OldAnnotation("MySql:CharSet", "utf8mb4");
 
-            migrationBuilder.AlterColumn<string>(
-                name: "GatewayCustomerId",
-                table: "Provider",
-                type: "varchar(50)",
-                maxLength: 50,
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "varchar(255)",
-                oldNullable: true)
-                .Annotation("MySql:CharSet", "utf8mb4")
-                .OldAnnotation("MySql:CharSet", "utf8mb4");
-        }
+        migrationBuilder.AlterColumn<string>(
+            name: "GatewayCustomerId",
+            table: "Provider",
+            type: "varchar(50)",
+            maxLength: 50,
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "varchar(255)",
+            oldNullable: true)
+            .Annotation("MySql:CharSet", "utf8mb4")
+            .OldAnnotation("MySql:CharSet", "utf8mb4");
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<string>(
-                name: "GatewaySubscriptionId",
-                table: "Provider",
-                type: "varchar(255)",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "varchar(50)",
-                oldMaxLength: 50,
-                oldNullable: true)
-                .Annotation("MySql:CharSet", "utf8mb4")
-                .OldAnnotation("MySql:CharSet", "utf8mb4");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<string>(
+            name: "GatewaySubscriptionId",
+            table: "Provider",
+            type: "varchar(255)",
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "varchar(50)",
+            oldMaxLength: 50,
+            oldNullable: true)
+            .Annotation("MySql:CharSet", "utf8mb4")
+            .OldAnnotation("MySql:CharSet", "utf8mb4");
 
-            migrationBuilder.AlterColumn<string>(
-                name: "GatewayCustomerId",
-                table: "Provider",
-                type: "varchar(255)",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "varchar(50)",
-                oldMaxLength: 50,
-                oldNullable: true)
-                .Annotation("MySql:CharSet", "utf8mb4")
-                .OldAnnotation("MySql:CharSet", "utf8mb4");
-        }
+        migrationBuilder.AlterColumn<string>(
+            name: "GatewayCustomerId",
+            table: "Provider",
+            type: "varchar(255)",
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "varchar(50)",
+            oldMaxLength: 50,
+            oldNullable: true)
+            .Annotation("MySql:CharSet", "utf8mb4")
+            .OldAnnotation("MySql:CharSet", "utf8mb4");
     }
 }
