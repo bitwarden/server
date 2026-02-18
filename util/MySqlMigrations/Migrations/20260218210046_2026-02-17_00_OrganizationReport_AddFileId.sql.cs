98f3201d710a97bcc0b7db6a63639e5fe25d8c50
Graham Walker <gwalker@bitwarden.com>
2026-02-18 15:06:54 -0600

    PM-31923 running migration

---
+++ util/MySqlMigrations/Migrations/20260218210046_2026-02-17_00_OrganizationReport_AddFileId.sql.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.MySqlMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class _20260217_00_OrganizationReport_AddFileIdsql : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<string>(
+            name: "FileId",
+            table: "OrganizationReport",
+            type: "longtext",
+            nullable: true)
+            .Annotation("MySql:CharSet", "utf8mb4");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "FileId",
+            table: "OrganizationReport");
+    }
+}
---
+++ util/SqliteMigrations/Migrations/20260218210035_2026-02-17_00_OrganizationReport_AddFileId.sql.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.SqliteMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class _20260217_00_OrganizationReport_AddFileIdsql : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<string>(
+            name: "FileId",
+            table: "OrganizationReport",
+            type: "TEXT",
+            nullable: true);
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "FileId",
+            table: "OrganizationReport");
+    }
+}
