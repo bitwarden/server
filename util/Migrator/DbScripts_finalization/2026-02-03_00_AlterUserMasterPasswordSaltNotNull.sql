d8b26467f99a56e4555ec40a950691a99e0802d9
Ike Kottlowski <ikottlowski@bitwarden.com>
2026-02-19 13:03:04 -0500

    feat: update finalization guard to match check constraint.

--- util/Migrator/DbScripts_finalization/2026-02-03_00_AlterUserMasterPasswordSaltNotNull.sql
+++ util/Migrator/DbScripts_finalization/2026-02-03_00_AlterUserMasterPasswordSaltNotNull.sql
@@ -8,9 +8,9 @@
 IF EXISTS (SELECT 1
 FROM [dbo].[User]
 WHERE
-    [MasterPassword] IS NOT NULL AND
-    [MasterPasswordSalt] IS NULL)
-    THROW 50000, 'MasterPasswordSalt contains NULLs for MasterPassword users; cannot finalize.', 1;
+    ([MasterPassword] IS NOT NULL AND [MasterPasswordSalt] IS NULL)
+    OR ([MasterPassword] IS NULL AND [MasterPasswordSalt] IS NOT NULL))
+    THROW 50000, 'MasterPassword and MasterPasswordSalt are out of sync; cannot finalize.', 1;
 
 ALTER TABLE [dbo].[User] ADD CONSTRAINT [CK_User_MasterPasswordAndSalt_Required] CHECK (([MasterPassword] IS NULL AND [MasterPasswordSalt] IS NULL) OR ([MasterPassword] IS NOT NULL AND [MasterPasswordSalt] IS NOT NULL));
 GO
