c1fbcefa7e6d157f1342e2dd55ba589d0bf60f21
Stephon Brown <sbrown@livefront.com>
2026-02-19 12:56:16 -0500

    feat(billing): add pm-29593-premium-to-organization-upgrade feature flag (#7041)

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -191,6 +191,7 @@ public static class FeatureFlagKeys
     public const string PM28265_ReconcileAdditionalStorageJobEnableLiveMode = "pm-28265-reconcile-additional-storage-job-enable-live-mode";
     public const string PM29594_UpdateIndividualSubscriptionPage = "pm-29594-update-individual-subscription-page";
     public const string PM29108_EnablePersonalDiscounts = "pm-29108-enable-personal-discounts";
+    public const string PM29593_PremiumToOrganizationUpgrade = "pm-29593-premium-to-organization-upgrade";
 
     /* Key Management Team */
     public const string PrivateKeyRegeneration = "pm-12241-private-key-regeneration";
