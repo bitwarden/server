418e6cbfedd45159487e4ac112d47766c6e2d041
Nick Krantz <125900171+nick-livefront@users.noreply.github.com>
2026-02-19 14:08:45 -0600

    add prompt age feature flag (#7035)

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -259,6 +259,7 @@ public static class FeatureFlagKeys
     public const string PM30521_AutofillButtonViewLoginScreen = "pm-30521-autofill-button-view-login-screen";
     public const string PM32180_PremiumUpsellAccountAge = "pm-32180-premium-upsell-account-age";
     public const string PM29438_WelcomeDialogWithExtensionPrompt = "pm-29438-welcome-dialog-with-extension-prompt";
+    public const string PM29438_DialogWithExtensionPromptAccountAge = "pm-29438-dialog-with-extension-prompt-account-age";
     public const string PM31039_ItemActionInExtension = "pm-31039-item-action-in-extension";
 
     /* Innovation Team */
