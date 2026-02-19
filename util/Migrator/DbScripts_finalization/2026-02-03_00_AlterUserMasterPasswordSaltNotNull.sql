34e14a3935b81d3cc7a73f3f3602e2630f25108f
Ike Kottlowski <ikottlowski@bitwarden.com>
2026-02-19 12:43:56 -0500

    feat: Ensure that both or neither MasterPassword and MasterPasswordSalt are null for finalization script.

--- util/Migrator/DbScripts_finalization/2026-02-03_00_AlterUserMasterPasswordSaltNotNull.sql
+++ util/Migrator/DbScripts_finalization/2026-02-03_00_AlterUserMasterPasswordSaltNotNull.sql
@@ -12,5 +12,5 @@ WHERE
     [MasterPasswordSalt] IS NULL)
     THROW 50000, 'MasterPasswordSalt contains NULLs for MasterPassword users; cannot finalize.', 1;
 
-ALTER TABLE [dbo].[User] ADD CONSTRAINT [CK_User_MasterPasswordSalt_Required] CHECK ([MasterPassword] IS NULL OR [MasterPasswordSalt] IS NOT NULL);
+ALTER TABLE [dbo].[User] ADD CONSTRAINT [CK_User_MasterPasswordAndSalt_Required] CHECK (([MasterPassword] IS NULL AND [MasterPasswordSalt] IS NULL) OR ([MasterPassword] IS NOT NULL AND [MasterPasswordSalt] IS NOT NULL));
 GO
