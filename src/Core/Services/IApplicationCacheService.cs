107ebb1d32794e56d26325c4080c8503f3e7a510
Jimmy Vo <huynhmaivo82@gmail.com>
2026-02-23 11:18:12 -0500

    [PM-31193] Add new interfaces to IApplicationCacheService

--- src/Core/Services/IApplicationCacheService.cs
+++ src/Core/Services/IApplicationCacheService.cs
@@ -8,10 +8,13 @@ namespace Bit.Core.Services;
 public interface IApplicationCacheService
 {
     Task<IDictionary<Guid, OrganizationAbility>> GetOrganizationAbilitiesAsync();
+    Task<IDictionary<Guid, OrganizationAbility>> GetOrganizationAbilitiesAsync(IEnumerable<Guid> orgIds);
 #nullable enable
     Task<OrganizationAbility?> GetOrganizationAbilityAsync(Guid orgId);
+    Task<ProviderAbility?> GetProviderAbilityAsync(Guid providerId);
 #nullable disable
     Task<IDictionary<Guid, ProviderAbility>> GetProviderAbilitiesAsync();
+    Task<IDictionary<Guid, ProviderAbility>> GetProviderAbilitiesAsync(IEnumerable<Guid> providerIds);
     Task UpsertOrganizationAbilityAsync(Organization organization);
     Task UpsertProviderAbilityAsync(Provider provider);
     Task DeleteOrganizationAbilityAsync(Guid organizationId);
