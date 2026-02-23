dbf30bd56b4b72bac9a80d2887add48d9f6d7fc5
Jared Snider <jsnider@bitwarden.com>
2026-02-23 11:52:06 -0500

    PM-32035 - Add new EmergencyAccessDetails_ReadByUserIds stored proc

---
+++ src/Sql/dbo/Auth/Stored Procedures/EmergencyAccessDetails_ReadByUserIds.sql
@@ -0,0 +1,15 @@
+CREATE PROCEDURE [dbo].[EmergencyAccessDetails_ReadByUserIds]
+    @UserIds [dbo].[GuidIdArray] READONLY
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    SELECT
+        *
+    FROM
+        [dbo].[EmergencyAccessDetailsView]
+    WHERE
+        [GrantorId] IN (SELECT [Id] FROM @UserIds)
+        OR [GranteeId] IN (SELECT [Id] FROM @UserIds)
+END
+GO
