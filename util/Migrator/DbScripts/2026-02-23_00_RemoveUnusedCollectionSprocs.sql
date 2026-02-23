a339f82a903647baaf5d18823f00934063c38d45
Sven <svernyi@bitwarden.com>
2026-02-23 15:06:02 -0600

    Remove old/unused sprocs

---
+++ util/Migrator/DbScripts/2026-02-23_00_RemoveUnusedCollectionSprocs.sql
@@ -0,0 +1,17 @@
+IF OBJECT_ID('[dbo].[CollectionUser_ReadByOrganizationUserIds]') IS NOT NULL
+BEGIN
+    DROP PROCEDURE [dbo].[CollectionUser_ReadByOrganizationUserIds];
+END
+GO
+
+IF OBJECT_ID('[dbo].[OrganizationUserUserDetails_ReadWithCollectionsById]') IS NOT NULL
+BEGIN
+    DROP PROCEDURE [dbo].[OrganizationUserUserDetails_ReadWithCollectionsById]
+END
+GO
+
+IF OBJECT_ID('[dbo].[Collection_ReadByOrganizationIdWithPermissions]') IS NOT NULL
+BEGIN
+    DROP PROCEDURE [dbo].[Collection_ReadByOrganizationIdWithPermissions]
+END
+GO
