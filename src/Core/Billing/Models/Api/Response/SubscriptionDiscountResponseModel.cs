bab3400b6730920517c2f5bec10b89c0b9c3cd0c
Stephon Brown <sbrown@livefront.com>
2026-02-23 20:11:34 -0500

    feat(billing): add subscription discount response model

---
+++ src/Core/Billing/Models/Api/Response/SubscriptionDiscountResponseModel.cs
@@ -0,0 +1,34 @@
+#nullable enable
+
+using Bit.Core.Billing.Subscriptions.Entities;
+
+namespace Bit.Core.Billing.Models.Api.Response;
+
+public class SubscriptionDiscountResponseModel
+{
+    public Guid Id { get; init; }
+    public string StripeCouponId { get; init; } = null!;
+    public IEnumerable<string>? StripeProductIds { get; init; }
+    public decimal? PercentOff { get; init; }
+    public long? AmountOff { get; init; }
+    public string? Currency { get; init; }
+    public string Duration { get; init; } = null!;
+    public int? DurationInMonths { get; init; }
+    public string? Name { get; init; }
+    public DateTime StartDate { get; init; }
+    public DateTime EndDate { get; init; }
+
+    public static SubscriptionDiscountResponseModel From(SubscriptionDiscount discount) => new()
+    {
+        StripeCouponId = discount.StripeCouponId,
+        StripeProductIds = discount.StripeProductIds,
+        PercentOff = discount.PercentOff,
+        AmountOff = discount.AmountOff,
+        Currency = discount.Currency,
+        Duration = discount.Duration,
+        DurationInMonths = discount.DurationInMonths,
+        Name = discount.Name,
+        StartDate = discount.StartDate,
+        EndDate = discount.EndDate
+    };
+}
