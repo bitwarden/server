387a6fb8fe928ad0b79cf855e9de6ee64bdd00e7
Mick Letofsky <mletofsky@bitwarden.com>
2026-02-26 19:22:30 +0100

    Craft the density modeling profiles, options, and set schema

---
+++ util/Seeder/Data/Enums/CollectionFanOutShape.cs
@@ -0,0 +1,6 @@
+﻿namespace Bit.Seeder.Data.Enums;
+
+/// <summary>
+/// Distribution shape for group-to-collection fan-out.
+/// </summary>
+public enum CollectionFanOutShape { Uniform, PowerLaw, HeavyTail }
