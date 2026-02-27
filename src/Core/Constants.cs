85655bfb764974f25686b0046b50dff4b516cb1c
gbubemismith <gsmithwalter@gmail.com>
2026-02-27 18:45:13 -0500

    Added comment

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -31,6 +31,8 @@ public static class Constants
 
     public const string Fido2KeyCipherMinimumVersion = "2023.10.0";
     public const string SSHKeyCipherMinimumVersion = "2024.12.0";
+
+    // TODO: Update with actual version once the feature is implemented
     public const string BankAccountCipherMinimumVersion = "2026.2.0";
     public const string DenyLegacyUserMinimumVersion = "2025.6.0";
 
