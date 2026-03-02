8e8495d167170b8be5add9c640fefff933b03bda
voommen-livefront <voommen@livefront.com>
2026-03-02 16:25:05 -0600

    PM-28531 update the file name - as per PR comments and formatting issues

---
+++ src/Sql/dbo/Dirt/Stored Procedures/OrganizationReport_ReadByOrganizationIdAndRevisionDate.sql
@@ -0,0 +1,20 @@
+CREATE PROCEDURE [dbo].[OrganizationReport_ReadByOrganizationIdAndRevisionDate]
+    @OrganizationId UNIQUEIDENTIFIER,
+    @StartDate DATETIME2(7),
+    @EndDate DATETIME2(7)
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    SELECT
+        [OrganizationId],
+        [ContentEncryptionKey],
+        [SummaryData],
+        [RevisionDate]
+    FROM [dbo].[OrganizationReportView]
+    WHERE [OrganizationId] = @OrganizationId
+        AND [RevisionDate] >= @StartDate
+        AND [RevisionDate] <= @EndDate
+    ORDER BY [RevisionDate] DESC
+END
+GO
