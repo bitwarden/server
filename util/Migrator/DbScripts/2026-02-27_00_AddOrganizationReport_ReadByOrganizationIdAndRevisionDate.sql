2a4f451cce9c6cf658f9e0222fdc003c564011ce
voommen-livefront <voommen@livefront.com>
2026-03-02 16:37:58 -0600

    PM-28531 updating formatting

--- util/Migrator/DbScripts/2026-02-27_00_AddOrganizationReport_ReadByOrganizationIdAndRevisionDate.sql
+++ util/Migrator/DbScripts/2026-02-27_00_AddOrganizationReport_ReadByOrganizationIdAndRevisionDate.sql
@@ -9,15 +9,15 @@ AS
 BEGIN
     SET NOCOUNT ON
 
-SELECT
-    [OrganizationId], 
-    [ContentEncryptionKey], 
-    [SummaryData], 
-    [RevisionDate]
-FROM [dbo].[OrganizationReportView]
-WHERE [OrganizationId] = @OrganizationId
-  AND [RevisionDate] >= @StartDate
-  AND [RevisionDate] <= @EndDate
-ORDER BY [RevisionDate] DESC
+    SELECT
+        [OrganizationId],
+        [ContentEncryptionKey],
+        [SummaryData],
+        [RevisionDate]
+    FROM [dbo].[OrganizationReportView]
+    WHERE [OrganizationId] = @OrganizationId
+        AND [RevisionDate] >= @StartDate
+        AND [RevisionDate] <= @EndDate
+    ORDER BY [RevisionDate] DESC
 END
 GO
