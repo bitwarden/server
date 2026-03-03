e56ad3a1b41ac96e305be49589451a474eac0af0
Stephon Brown <sbrown@livefront.com>
2026-03-03 11:14:22 -0500

    refactor(billing): remove StripeProductIds from subscription discount response model

--- src/Core/Billing/Models/Api/Response/SubscriptionDiscountResponseModel.cs
+++ src/Core/Billing/Models/Api/Response/SubscriptionDiscountResponseModel.cs
@@ -8,7 +8,6 @@ namespace Bit.Core.Billing.Models.Api.Response;
 public class SubscriptionDiscountResponseModel
 {
     public string StripeCouponId { get; init; } = null!;
-    public IEnumerable<string>? StripeProductIds { get; init; }
     public decimal? PercentOff { get; init; }
     public long? AmountOff { get; init; }
     public string? Currency { get; init; }
@@ -24,7 +23,6 @@ public class SubscriptionDiscountResponseModel
         IDictionary<DiscountTierType, bool> tierEligibility) => new()
         {
             StripeCouponId = discount.StripeCouponId,
-            StripeProductIds = discount.StripeProductIds,
             PercentOff = discount.PercentOff,
             AmountOff = discount.AmountOff,
             Currency = discount.Currency,
