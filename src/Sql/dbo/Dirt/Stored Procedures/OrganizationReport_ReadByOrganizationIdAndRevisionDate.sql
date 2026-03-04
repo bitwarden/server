1c5c1547b8a409f5835d538698f6a442738bb266
Vijay Oommen <voommen@livefront.com>
2026-03-04 15:50:57 -0600

    Update src/Sql/dbo/Dirt/Stored Procedures/OrganizationReport_ReadByOrganizationIdAndRevisionDate.sql
    Co-authored-by: mkincaid-bw <mkincaid@bitwarden.com>

--- src/Sql/dbo/Dirt/Stored Procedures/OrganizationReport_ReadByOrganizationIdAndRevisionDate.sql
+++ src/Sql/dbo/Dirt/Stored Procedures/OrganizationReport_ReadByOrganizationIdAndRevisionDate.sql
@@ -11,10 +11,13 @@ BEGIN
         [ContentEncryptionKey],
         [SummaryData],
         [RevisionDate]
-    FROM [dbo].[OrganizationReportView]
-    WHERE [OrganizationId] = @OrganizationId
+    FROM 
+        [dbo].[OrganizationReportView]
+    WHERE
+        [OrganizationId] = @OrganizationId
         AND [RevisionDate] >= @StartDate
         AND [RevisionDate] <= @EndDate
-    ORDER BY [RevisionDate] DESC
+    ORDER BY
+        [RevisionDate] DESC
 END
 GO
