19b7c0258c568323073e48a642d7e5a1b8181979
Matt Gibson <mgibson@bitwarden.com>
2026-03-04 15:06:04 -0800

    Existing device scene

---
+++ util/Seeder/Factories/DeviceSeeder.cs
@@ -0,0 +1,19 @@
+﻿using Bit.Core.Enums;
+
+namespace Bit.Seeder.Factories;
+
+internal static class DeviceSeeder
+{
+    internal static Core.Entities.Device Create(Guid userId, DeviceType deviceType, string deviceName, string identifier, string? pushToken)
+    {
+        return new Core.Entities.Device
+        {
+            Id = Core.Utilities.CoreHelpers.GenerateComb(),
+            UserId = userId,
+            Type = deviceType,
+            Name = deviceName,
+            Identifier = identifier,
+            PushToken = pushToken
+        };
+    }
+}
---
+++ util/Seeder/Scenes/UserDeviceScene.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Bit.Core.Enums;
+using Bit.Core.Repositories;
+using Bit.Seeder;
+using Bit.Seeder.Factories;
+using Bit.Seeder.Services;
+
+public class UserDeviceScene(IUserRepository userRepository, IDeviceRepository deviceRepository, IManglerService manglerService) : IScene<UserDeviceScene.Request, UserDeviceScene.Result>
+{
+    public class Request
+    {
+        [Required]
+        public required Guid UserId { get; set; }
+        [Required]
+        public required DeviceType Type { get; set; }
+        [Required]
+        public required string Name { get; set; }
+        [Required]
+        public required string Identifier { get; set; }
+        public string? PushToken { get; set; }
+    }
+
+    public class Result
+    {
+    }
+
+    public async Task<SceneResult<Result>> SeedAsync(Request request)
+    {
+        var user = await userRepository.GetByIdAsync(request.UserId);
+        if (user == null)
+        {
+            throw new Exception($"User with ID {request.UserId} not found.");
+        }
+
+        var device = DeviceSeeder.Create(request.UserId, request.Type, request.Name, request.Identifier, request.PushToken);
+        await deviceRepository.CreateAsync(device);
+
+        return new SceneResult<Result>(
+            result: new Result
+            {
+            },
+            mangleMap: manglerService.GetMangleMap());
+    }
+}
