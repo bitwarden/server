41107d8077e813006acffc23515b9426dffc1ceb
Ike Kottlowski <ikottlowski@bitwarden.com>
2026-03-06 20:42:13 -0500

    feat: add dapper migration scripts

---
+++ util/Migrator/DbScripts/2026-03-06_00_AlterReadKdfByEmail.sql
@@ -0,0 +1,18 @@
+CREATE OR ALTER PROCEDURE [dbo].[User_ReadKdfByEmail]
+    @Email NVARCHAR(256)
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    SELECT
+        [Kdf],
+        [KdfIterations],
+        [KdfMemory],
+        [KdfParallelism],
+        [MasterPasswordSalt] AS [Salt]
+    FROM
+        [dbo].[User]
+    WHERE
+        [Email] = @Email
+END
+GO
