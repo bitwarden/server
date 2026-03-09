b35976bc0f60e7a019b5ccc705ef246dcdc5019c
Thomas Rittson <31796059+eliykat@users.noreply.github.com>
2026-03-10 09:15:41 +1000

    Update UseMyItems to use dedicated plan feature (#7101)

--- src/Core/AdminConsole/OrganizationFeatures/Organizations/CloudOrganizationSignUpCommand.cs
+++ src/Core/AdminConsole/OrganizationFeatures/Organizations/CloudOrganizationSignUpCommand.cs
@@ -79,7 +79,7 @@ public class CloudOrganizationSignUpCommand(
             MaxCollections = plan.PasswordManager.MaxCollections,
             MaxStorageGb = (short)(plan.PasswordManager.BaseStorageGb + signup.AdditionalStorageGb),
             UsePolicies = plan.HasPolicies,
-            UseMyItems = plan.HasPolicies, // TODO: use the plan property when added (PM-32366)
+            UseMyItems = plan.HasMyItems,
             UseSso = plan.HasSso,
             UseGroups = plan.HasGroups,
             UseEvents = plan.HasEvents,
