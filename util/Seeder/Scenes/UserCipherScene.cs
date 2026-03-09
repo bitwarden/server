fccc71a3b39349985c998dfdbc8454b55c33fea8
Matt Gibson <mgibson@bitwarden.com>
2026-03-09 12:38:53 -0700

    User Cipher scene
    For now only supports one login cipher

---
+++ util/Seeder/Scenes/UserCipherScene.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Bit.Core.Repositories;
+using Bit.Core.Vault.Repositories;
+using Bit.Seeder.Factories;
+using Bit.Seeder.Services;
+
+namespace Bit.Seeder.Scenes;
+
+public class UserCipherScene(IUserRepository userRepository, ICipherRepository cipherRepository, IManglerService manglerService) : IScene<UserCipherScene.Request, UserCipherScene.Result>
+{
+    public class Request
+    {
+        [Required]
+        public required Guid UserId { get; set; }
+        [Required]
+        public required string UserKeyB64 { get; set; }
+        [Required]
+        public required string Name { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Uri { get; set; }
+        public string? Notes { get; set; }
+        public IEnumerable<(string name, string value, int type)>? Fields { get; set; }
+    }
+
+    public class Result
+    {
+        public required Guid CipherId { get; set; }
+    }
+
+    public async Task<SceneResult<Result>> SeedAsync(Request request)
+    {
+        var user = await userRepository.GetByIdAsync(request.UserId);
+        if (user == null)
+        {
+            throw new Exception($"User with ID {request.UserId} not found.");
+        }
+
+        var cipher = LoginCipherSeeder.Create(request.UserKeyB64, request.Name, userId: request.UserId, username: request.Username, password: request.Password, uri: request.Uri, notes: request.Notes, fields: request.Fields);
+
+        await cipherRepository.CreateAsync(cipher);
+
+        return new SceneResult<Result>(
+            result: new Result
+            {
+                CipherId = cipher.Id
+            },
+            mangleMap: manglerService.GetMangleMap());
+    }
+}
