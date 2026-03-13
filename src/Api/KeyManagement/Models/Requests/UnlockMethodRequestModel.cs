aa8ed9e51b689bbafb922679d070a4c6aa95a75c
Thomas Avery <tavery@bitwarden.com>
2026-03-13 17:18:53 -0500

    Add new user key rotation endpoint with MP support

---
+++ src/Api/KeyManagement/Models/Requests/UnlockMethodRequestModel.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Bit.Api.KeyManagement.Enums;
+using Bit.Core.Exceptions;
+using Bit.Core.KeyManagement.Models.Api.Request;
+using Bit.Core.Utilities;
+
+namespace Bit.Api.KeyManagement.Models.Requests;
+
+public class UnlockMethodRequestModel : IValidatableObject
+{
+    // Master password user
+    public MasterPasswordUnlockDataRequestModel? MasterPasswordUnlockData { get; init; }
+
+    // Key Connector user.
+    [EncryptedString]
+    public string? KeyConnectorKeyWrappedUserKey { get; init; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (KeyConnectorKeyWrappedUserKey != null && MasterPasswordUnlockData != null)
+        {
+            yield return new ValidationResult("Invalid request user can't have KeyConnectorKeyWrappedUserKey and MasterPasswordUnlockData");
+        }
+    }
+
+    public UnlockMethod GetUnlockMethod()
+    {
+        if (KeyConnectorKeyWrappedUserKey == null && MasterPasswordUnlockData == null)
+        {
+            return UnlockMethod.Tde;
+        }
+
+        if (KeyConnectorKeyWrappedUserKey != null && MasterPasswordUnlockData == null)
+        {
+            return UnlockMethod.KeyConnector;
+        }
+
+        if (MasterPasswordUnlockData != null && KeyConnectorKeyWrappedUserKey == null)
+        {
+            return UnlockMethod.MasterPassword;
+        }
+
+        throw new BadRequestException("Unknown UnlockMethod");
+    }
+}
---
+++ src/Api/KeyManagement/Models/Requests/WrappedAccountCryptographicStateRequestModel.cs
@@ -0,0 +1,21 @@
+﻿using Bit.Core.KeyManagement.Models.Api.Request;
+using Bit.Core.KeyManagement.Models.Data;
+
+namespace Bit.Api.KeyManagement.Models.Requests;
+
+public class WrappedAccountCryptographicStateRequestModel
+{
+    public required PublicKeyEncryptionKeyPairRequestModel PublicKeyEncryptionKeyPair { get; set; }
+    public required SignatureKeyPairRequestModel SignatureKeyPair { get; set; }
+    public required SecurityStateModel SecurityState { get; set; }
+
+    public UserAccountKeysData ToAccountKeysData()
+    {
+        return new UserAccountKeysData
+        {
+            PublicKeyEncryptionKeyPairData = PublicKeyEncryptionKeyPair.ToPublicKeyEncryptionKeyPairData(),
+            SignatureKeyPairData = SignatureKeyPair.ToSignatureKeyPairData(),
+            SecurityStateData = SecurityState.ToSecurityState()
+        };
+    }
+}
