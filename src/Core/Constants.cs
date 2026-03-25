96244839b2934203839c3344fe597c7726a78064
Kyle Denney <4227399+kdenney@users.noreply.github.com>
2026-03-25 14:59:20 -0500

    feature flag for autofill triage

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -190,6 +190,7 @@ public static class FeatureFlagKeys
     public const string WindowsDesktopAutotypeGA = "windows-desktop-autotype-ga";
     public const string FillAssistTargetingRules = "fill-assist-targeting-rules";
     public const string NotificationUndeterminedCipherScenarioLogic = "undetermined-cipher-scenario-logic";
+    public const string EnableAutofillTriage = "enable-autofill-triage";
 
     /* Billing Team */
     public const string TrialPayment = "PM-8163-trial-payment";
