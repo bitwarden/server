7863a78180c94f5607624c84e73ce37977526b9a
Maciej Zieniuk <mzieniuk@bitwarden.com>
2026-03-25 15:10:34 +0000

    send folder feature flag

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -259,6 +259,7 @@ public static class FeatureFlagKeys
     public const string SendUIRefresh = "pm-28175-send-ui-refresh";
     public const string SendEmailOTP = "pm-19051-send-email-verification";
     public const string SendControls = "pm-31885-send-controls";
+    public const string SendFolder = "innovation-sprint-2026-send-folder";
 
     /* Vault Team */
     public const string CipherKeyEncryption = "cipher-key-encryption";
