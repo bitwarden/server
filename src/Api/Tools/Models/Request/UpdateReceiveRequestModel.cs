9e76eab9060efbd74c6623d7cc17c3cc9b3af7cc
Thomas Avery <43214426+Thomas-Avery@users.noreply.github.com>
2026-03-31 16:47:54 -0500

    Add update request model (#7367)

---
+++ src/Api/Tools/Models/Request/UpdateReceiveRequestModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Bit.Core.Tools.ReceiveFeatures.Models;
+using Bit.Core.Utilities;
+
+
+namespace Bit.Api.Tools.Models.Request;
+
+public class UpdateReceiveRequestModel
+{
+    /// <summary>
+    /// Label for the Receive.
+    /// </summary>
+    [Required]
+    [EncryptedString]
+    [EncryptedStringLength(1000)]
+    public required string Name { get; set; }
+
+    /// <summary>
+    /// The date this Receive becomes unavailable to potential uploaders.
+    /// </summary>
+    public DateTime ExpirationDate { get; set; }
+
+    public ReceiveUpdateData ToUpdateData(Guid id)
+    {
+        return new ReceiveUpdateData { Id = id, Name = Name, ExpirationDate = ExpirationDate, };
+    }
+}
