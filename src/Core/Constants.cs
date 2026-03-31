a658758d76557005184d7968bded3f1980724707
Jared Snider <116684653+JaredSnider-Bitwarden@users.noreply.github.com>
2026-03-31 11:09:32 -0400

    Auth/PM-34400 - Add desktop devices feature flag (#7361)

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -181,6 +181,7 @@ public static class FeatureFlagKeys
     public const string ChangeEmailNewAuthenticationApis = "pm-30811-change-email-new-authentication-apis";
     public const string PM31088_MasterPasswordServiceEmitSalt = "pm-31088-master-password-service-emit-salt";
     public const string PM32413_MultiClientPasswordManagement = "pm-32413-multi-client-password-management";
+    public const string PM34210_DesktopAddDevices = "pm-34210-desktop-add-devices";
 
     /* Autofill Team */
     public const string SSHAgentV2 = "ssh-agent-v2";
