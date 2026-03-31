6b2f02ab9c72e750764cb4cba890cc9749ed6651
Nick Krantz <125900171+nick-livefront@users.noreply.github.com>
2026-03-31 10:32:14 -0500

    add feature flag for new item types (#7358)

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -280,6 +280,7 @@ public static class FeatureFlagKeys
     public const string PM31039_ItemActionInExtension = "pm-31039-item-action-in-extension";
     public const string PM29437_WelcomeDialogNoExtPrompt = "pm-29437-welcome-dialog-no-ext-prompt";
     public const string PM31948_OrgUserNotificationBanner = "pm-31948-org-user-notification-banner";
+    public const string PM32009_NewItemTypes = "pm-32009-new-item-types";
 
     /* Innovation Team */
     public const string ArchiveVaultItems = "pm-19148-innovation-archive";
