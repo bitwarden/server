f237d1bbd64091ade69eed7c025eacc731091bea
Thomas Avery <tavery@bitwarden.com>
2026-04-01 14:48:51 -0500

    Add receive owner email to share response

--- src/Api/Tools/Models/Response/SharedReceiveResponseModel.cs
+++ src/Api/Tools/Models/Response/SharedReceiveResponseModel.cs
@@ -5,10 +5,11 @@ namespace Bit.Api.Tools.Models.Response;
 
 public class SharedReceiveResponseModel : ResponseModel
 {
-    public SharedReceiveResponseModel(Receive receive) : base("receiveShared")
+    public SharedReceiveResponseModel(Receive receive, string email) : base("receiveShared")
     {
         Name = receive.Name;
         ScekWrappedPublicKey = receive.ScekWrappedPublicKey;
+        OwnerEmail = email;
     }
 
     /// <summary>Label for the Receive. Encrypted.</summary>
@@ -16,4 +17,9 @@ public class SharedReceiveResponseModel : ResponseModel
 
     /// <summary>Public key (SCEK-wrapped) used by uploaders to encrypt file content.</summary>
     public string ScekWrappedPublicKey { get; set; }
+
+    /// <summary>
+    /// The Receive owner's email
+    /// </summary>
+    public string OwnerEmail { get; set; }
 }
