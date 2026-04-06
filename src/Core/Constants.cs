bd6cc1f4c85cbbd74384d5d1ad7da70e7b1b7534
Jared McCannon <jmccannon@bitwarden.com>
2026-04-06 14:44:48 -0500

    Adding feature flag

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -164,6 +164,7 @@ public static class FeatureFlagKeys
     public const string RefactorOrgAcceptInit = "pm-33082-refactor-org-accept-init";
     public const string AdminResetTwoFactor = "pm-15489-reset-two-factor-account-recovery";
     public const string PublicMembersInviteRefactor = "pm-33398-refactor-members-invite-org-users-command";
+    public const string OrgAbilityExtendedCache = "pm-32104-org-ability-extended-cache";
 
     /* Architecture */
     public const string DesktopMigrationMilestone1 = "desktop-ui-migration-milestone-1";
