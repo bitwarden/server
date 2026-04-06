c4f9740614384dff36933f77fc21c903b705ad86
JaredScar <thewolfbadger@gmail.com>
2026-04-06 14:42:02 -0400

    refactor(UsersController): Remove unused feature service and simplify revoke user logic
    * Removed references to IFeatureService and IRevokeOrganizationUserCommand from UsersController.
    * Simplified the logic for revoking users by directly using IRevokeOrganizationUserCommandV2.
    * Updated unit tests to reflect the removal of feature flag checks for SCIM revoke functionality.

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -159,7 +159,6 @@ public static class FeatureFlagKeys
     public const string PolicyRequirements = "pm-14439-policy-requirements";
     public const string ScimInviteUserOptimization = "pm-16811-optimize-invite-user-flow-to-fail-fast";
     public const string AutomaticConfirmUsers = "pm-19934-auto-confirm-organization-users";
-    public const string ScimRevokeV2 = "pm-32394-scim-revoke-put-v2";
     public const string BulkReinviteUI = "pm-28416-bulk-reinvite-ux-improvements";
     public const string RefactorOrgAcceptInit = "pm-33082-refactor-org-accept-init";
     public const string AdminResetTwoFactor = "pm-15489-reset-two-factor-account-recovery";
