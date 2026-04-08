0f0cfcd70fda27400b7c41a71b3532d954780bdd
Rui Tome <rtome@bitwarden.com>
2026-04-08 15:36:07 +0100

    Add CollectionGroup and CollectionUser operation authorization requirements

---
+++ src/Api/Vault/AuthorizationHandlers/Collections/CollectionGroupOperations.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+namespace Bit.Api.Vault.AuthorizationHandlers.Collections;
+
+public class CollectionGroupOperationRequirement : OperationAuthorizationRequirement { }
+
+public static class CollectionGroupOperations
+{
+    public static readonly CollectionGroupOperationRequirement Create = new() { Name = nameof(Create) };
+    public static readonly CollectionGroupOperationRequirement Update = new() { Name = nameof(Update) };
+    public static readonly CollectionGroupOperationRequirement Delete = new() { Name = nameof(Delete) };
+}
