810c9844da770ded5c04646ce3e8ab3f38b20c4b
Leslie Tilton <23057410+Banrion@users.noreply.github.com>
2026-04-13 10:36:40 -0500

    Add pm-31936-access-intelligence-new-architecture flag for client side use (#7459)

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -300,6 +300,7 @@ public static class FeatureFlagKeys
     public const string EventManagementForHuntress = "event-management-for-huntress";
     public const string Milestone11AppPageImprovements = "pm-30538-dirt-milestone-11-app-page-improvements";
     public const string AccessIntelligenceTrendChart = "pm-26961-access-intelligence-trend-chart";
+    public const string AccessIntelligenceNewArchitecture = "pm-31936-access-intelligence-new-architecture";
 
     /* UIF Team */
     public const string RouterFocusManagement = "router-focus-management";
