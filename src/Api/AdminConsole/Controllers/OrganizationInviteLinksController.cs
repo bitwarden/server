b20c1f3b12354275588a9bd8dc40b5688474ebd0
Rui Tome <rtome@bitwarden.com>
2026-04-15 17:05:00 +0100

    Add OrganizationInviteLinksController to manage invite link creation for organizations

---
+++ src/Api/AdminConsole/Controllers/OrganizationInviteLinksController.cs
@@ -0,0 +1,32 @@
+﻿using Bit.Api.AdminConsole.Authorization;
+using Bit.Api.AdminConsole.Authorization.Requirements;
+using Bit.Api.AdminConsole.Models.Request.Organizations;
+using Bit.Api.AdminConsole.Models.Response.Organizations;
+using Bit.Core;
+using Bit.Core.AdminConsole.OrganizationFeatures.InviteLinks.Interfaces;
+using Bit.Core.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bit.Api.AdminConsole.Controllers;
+
+[Route("organizations/{orgId}/invite-link")]
+[Authorize("Application")]
+[RequireFeature(FeatureFlagKeys.GenerateInviteLink)]
+public class OrganizationInviteLinksController(
+    ICreateOrganizationInviteLinkCommand createOrganizationInviteLinkCommand)
+    : BaseAdminConsoleController
+{
+    [HttpPost("")]
+    [Authorize<ManageUsersRequirement>]
+    public async Task<IResult> Create(Guid orgId, [FromBody] CreateOrganizationInviteLinkRequestModel model)
+    {
+        var result = await createOrganizationInviteLinkCommand.CreateAsync(
+            model.ToCommandRequest(orgId));
+
+        return Handle(result, link =>
+            TypedResults.Created(
+                $"organizations/{orgId}/invite-link",
+                new OrganizationInviteLinkResponseModel(link)));
+    }
+}
