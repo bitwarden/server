d9aef5c58f2fefbfa9045e52ba9ff477f66eab19
Jared McCannon <jmccannon@bitwarden.com>
2026-04-15 09:46:16 -0500

    [PM-32068] - Org Ability Extended Cache (#7443)
    * Added extended cache implementation for org ability.
    
    * fixed up events
    
    * Moved utilities to service class. Moved tests to integration tests - with specific endpoints tested. moved registration to ext method

---
+++ src/Core/AdminConsole/AbilitiesCache/IOrganizationAbilityCacheService.cs
@@ -0,0 +1,11 @@
+﻿using Bit.Core.AdminConsole.Entities;
+using Bit.Core.Models.Data.Organizations;
+
+namespace Bit.Core.AdminConsole.AbilitiesCache;
+
+public interface IOrganizationAbilityCacheService
+{
+    Task<OrganizationAbility?> GetOrganizationAbilityAsync(Guid orgId);
+    Task UpsertOrganizationAbilityAsync(Organization organization);
+    Task DeleteOrganizationAbilityAsync(Guid organizationId);
+}
