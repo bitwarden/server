474905d4e7982f57ecd704d1d6c799106c0ab403
Rui Tome <rtome@bitwarden.com>
2026-04-16 14:05:24 +0100

    Add encryption validation attributes to CreateOrganizationInviteLinkRequestModel and implement unit tests for model validation

--- src/Api/AdminConsole/Models/Request/Organizations/CreateOrganizationInviteLinkRequestModel.cs
+++ src/Api/AdminConsole/Models/Request/Organizations/CreateOrganizationInviteLinkRequestModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Bit.Core.AdminConsole.OrganizationFeatures.InviteLinks;
+using Bit.Core.Utilities;
 
 namespace Bit.Api.AdminConsole.Models.Request.Organizations;
 
@@ -15,11 +16,13 @@ public class CreateOrganizationInviteLinkRequestModel
     /// The invite key encrypted with the organization key.
     /// </summary>
     [Required]
+    [EncryptedString]
     public required string EncryptedInviteKey { get; set; }
 
     /// <summary>
     /// The organization key encrypted for the invite link. Currently unused; will be populated in a future stage.
     /// </summary>
+    [EncryptedString]
     public string? EncryptedOrgKey { get; set; }
 
     public CreateOrganizationInviteLinkRequest ToCommandRequest(Guid organizationId) => new()
