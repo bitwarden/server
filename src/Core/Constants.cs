474a22c7eeb4329b41d4dc6359421c74f7210787
Alex Morask <amorask@bitwarden.com>
2026-04-27 08:49:40 -0500

    chore(billing): remove unused FF

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -205,9 +205,6 @@ public static class FeatureFlagKeys
     /* Billing Team */
     public const string PM24032_NewNavigationPremiumUpgradeButton = "pm-24032-new-navigation-premium-upgrade-button";
     public const string PM23713_PremiumBadgeOpensNewPremiumUpgradeDialog = "pm-23713-premium-badge-opens-new-premium-upgrade-dialog";
-    public const string PM26793_FetchPremiumPriceFromPricingService = "pm-26793-fetch-premium-price-from-pricing-service";
-
-
     public const string PM29594_UpdateIndividualSubscriptionPage = "pm-29594-update-individual-subscription-page";
     public const string PM29108_EnablePersonalDiscounts = "pm-29108-enable-personal-discounts";
     public const string PM29593_PremiumToOrganizationUpgrade = "pm-29593-premium-to-organization-upgrade";
