88e3d0f438ca1c4eaad12b8c7cf11a50e4f9ca02
Rui Tome <rtome@bitwarden.com>
2026-04-30 17:06:01 +0100

    Add BulkCollectionGroupAuthorizationHandler for flows without a single target group

---
+++ test/Api.Test/Vault/AuthorizationHandlers/BulkCollectionGroupAuthorizationHandlerTests.cs
@@ -0,0 +1,228 @@
+﻿using System.Security.Claims;
+using Bit.Api.Vault.AuthorizationHandlers.Collections;
+using Bit.Core;
+using Bit.Core.Context;
+using Bit.Core.Entities;
+using Bit.Core.Enums;
+using Bit.Core.Models.Data;
+using Bit.Core.Models.Data.Organizations;
+using Bit.Core.Repositories;
+using Bit.Core.Services;
+using Bit.Core.Test.Vault.AutoFixture;
+using Bit.Test.Common.AutoFixture;
+using Bit.Test.Common.AutoFixture.Attributes;
+using Microsoft.AspNetCore.Authorization;
+using NSubstitute;
+using Xunit;
+
+namespace Bit.Api.Test.Vault.AuthorizationHandlers;
+
+[SutProviderCustomize]
+public class BulkCollectionGroupAuthorizationHandlerTests
+{
+    [Theory, BitAutoData, CollectionCustomization]
+    public async Task HandleRequirementAsync_WithFeatureFlagDisabled_DoesNotSucceed(
+        SutProvider<BulkCollectionGroupAuthorizationHandler> sutProvider,
+        ICollection<Collection> collections,
+        CurrentContextOrganization organization,
+        Guid actingUserId)
+    {
+        sutProvider.GetDependency<IFeatureService>()
+            .IsEnabled(FeatureFlagKeys.CollectionUserCollectionGroupAuthorizationHandlers)
+            .Returns(false);
+
+        var context = new AuthorizationHandlerContext(
+            new[] { CollectionGroupOperations.Create },
+            new ClaimsPrincipal(),
+            collections.ToList());
+
+        sutProvider.GetDependency<ICurrentContext>().UserId.Returns(actingUserId);
+        sutProvider.GetDependency<ICurrentContext>().GetOrganization(organization.Id).Returns(organization);
+
+        await sutProvider.Sut.HandleAsync(context);
+
+        Assert.False(context.HasSucceeded);
+    }
+
+    [Theory, BitAutoData, CollectionCustomization]
+    public async Task HandleRequirementAsync_WithNoUserId_DoesNotSucceed(
+        SutProvider<BulkCollectionGroupAuthorizationHandler> sutProvider,
+        ICollection<Collection> collections)
+    {
+        ArrangeFeatureFlag(sutProvider);
+
+        var context = new AuthorizationHandlerContext(
+            new[] { CollectionGroupOperations.Create },
+            new ClaimsPrincipal(),
+            collections.ToList());
+
+        sutProvider.GetDependency<ICurrentContext>().UserId.Returns((Guid?)null);
+
+        await sutProvider.Sut.HandleAsync(context);
+
+        Assert.False(context.HasSucceeded);
+    }
+
+    [Theory, CollectionCustomization]
+    [BitAutoData(OrganizationUserType.Admin)]
+    [BitAutoData(OrganizationUserType.Owner)]
+    public async Task CanCreate_WithAdminOrOwnerAndAllowAdminAccess_Succeeds(
+        OrganizationUserType userType,
+        Guid actingUserId,
+        SutProvider<BulkCollectionGroupAuthorizationHandler> sutProvider,
+        ICollection<Collection> collections,
+        CurrentContextOrganization organization)
+    {
+        organization.Type = userType;
+        organization.Permissions = new Permissions();
+
+        ArrangeFeatureFlag(sutProvider);
+        ArrangeOrganizationAbility(sutProvider, organization, true);
+
+        var context = new AuthorizationHandlerContext(
+            new[] { CollectionGroupOperations.Create },
+            new ClaimsPrincipal(),
+            collections.ToList());
+
+        sutProvider.GetDependency<ICurrentContext>().UserId.Returns(actingUserId);
+        sutProvider.GetDependency<ICurrentContext>().GetOrganization(organization.Id).Returns(organization);
+
+        await sutProvider.Sut.HandleAsync(context);
+
+        Assert.True(context.HasSucceeded);
+    }
+
+    [Theory, BitAutoData, CollectionCustomization]
+    public async Task CanCreate_WithCustomManageGroupsAndAllowAdminAccess_Succeeds(
+        SutProvider<BulkCollectionGroupAuthorizationHandler> sutProvider,
+        ICollection<Collection> collections,
+        CurrentContextOrganization organization,
+        Guid actingUserId)
+    {
+        organization.Type = OrganizationUserType.Custom;
+        organization.Permissions = new Permissions { ManageGroups = true };
+
+        ArrangeFeatureFlag(sutProvider);
+        ArrangeOrganizationAbility(sutProvider, organization, true);
+
+        var context = new AuthorizationHandlerContext(
+            new[] { CollectionGroupOperations.Create },
+            new ClaimsPrincipal(),
+            collections.ToList());
+
+        sutProvider.GetDependency<ICurrentContext>().UserId.Returns(actingUserId);
+        sutProvider.GetDependency<ICurrentContext>().GetOrganization(organization.Id).Returns(organization);
+
+        await sutProvider.Sut.HandleAsync(context);
+
+        Assert.True(context.HasSucceeded);
+    }
+
+    [Theory, BitAutoData, CollectionCustomization]
+    public async Task CanCreate_WithUserManagePermissionOnAllCollections_Succeeds(
+        SutProvider<BulkCollectionGroupAuthorizationHandler> sutProvider,
+        ICollection<CollectionDetails> collections,
+        CurrentContextOrganization organization,
+        Guid actingUserId)
+    {
+        organization.Type = OrganizationUserType.User;
+        organization.Permissions = new Permissions();
+
+        foreach (var c in collections)
+        {
+            c.Manage = true;
+        }
+
+        ArrangeFeatureFlag(sutProvider);
+        ArrangeOrganizationAbility(sutProvider, organization, false);
+
+        var context = new AuthorizationHandlerContext(
+            new[] { CollectionGroupOperations.Create },
+            new ClaimsPrincipal(),
+            collections.Cast<Collection>().ToList());
+
+        sutProvider.GetDependency<ICurrentContext>().UserId.Returns(actingUserId);
+        sutProvider.GetDependency<ICurrentContext>().GetOrganization(organization.Id).Returns(organization);
+        sutProvider.GetDependency<ICollectionRepository>().GetManyByUserIdAsync(actingUserId).Returns(collections);
+
+        await sutProvider.Sut.HandleAsync(context);
+
+        Assert.True(context.HasSucceeded);
+    }
+
+    [Theory, BitAutoData, CollectionCustomization]
+    public async Task CanCreate_WithUserWithoutManagePermission_DoesNotSucceed(
+        SutProvider<BulkCollectionGroupAuthorizationHandler> sutProvider,
+        ICollection<Collection> collections,
+        CurrentContextOrganization organization,
+        Guid actingUserId)
+    {
+        organization.Type = OrganizationUserType.User;
+        organization.Permissions = new Permissions();
+
+        ArrangeFeatureFlag(sutProvider);
+        ArrangeOrganizationAbility(sutProvider, organization, false);
+
+        var context = new AuthorizationHandlerContext(
+            new[] { CollectionGroupOperations.Create },
+            new ClaimsPrincipal(),
+            collections.ToList());
+
+        sutProvider.GetDependency<ICurrentContext>().UserId.Returns(actingUserId);
+        sutProvider.GetDependency<ICurrentContext>().GetOrganization(organization.Id).Returns(organization);
+        sutProvider.GetDependency<ICollectionRepository>()
+            .GetManyByUserIdAsync(actingUserId)
+            .Returns(new List<CollectionDetails>());
+        sutProvider.GetDependency<ICurrentContext>().ProviderUserForOrgAsync(Arg.Any<Guid>())
+            .Returns(false);
+
+        await sutProvider.Sut.HandleAsync(context);
+
+        Assert.False(context.HasSucceeded);
+    }
+
+    [Theory, BitAutoData, CollectionCustomization]
+    public async Task CanCreate_WithProviderUser_Succeeds(
+        SutProvider<BulkCollectionGroupAuthorizationHandler> sutProvider,
+        ICollection<Collection> collections,
+        Guid actingUserId)
+    {
+        ArrangeFeatureFlag(sutProvider);
+
+        var context = new AuthorizationHandlerContext(
+            new[] { CollectionGroupOperations.Create },
+            new ClaimsPrincipal(),
+            collections.ToList());
+
+        sutProvider.GetDependency<ICurrentContext>().UserId.Returns(actingUserId);
+        sutProvider.GetDependency<ICurrentContext>().GetOrganization(Arg.Any<Guid>())
+            .Returns((CurrentContextOrganization)null);
+        sutProvider.GetDependency<ICurrentContext>().ProviderUserForOrgAsync(Arg.Any<Guid>())
+            .Returns(true);
+
+        await sutProvider.Sut.HandleAsync(context);
+
+        Assert.True(context.HasSucceeded);
+    }
+
+    private static void ArrangeFeatureFlag(SutProvider<BulkCollectionGroupAuthorizationHandler> sutProvider)
+    {
+        sutProvider.GetDependency<IFeatureService>()
+            .IsEnabled(FeatureFlagKeys.CollectionUserCollectionGroupAuthorizationHandlers)
+            .Returns(true);
+    }
+
+    private static void ArrangeOrganizationAbility(
+        SutProvider<BulkCollectionGroupAuthorizationHandler> sutProvider,
+        CurrentContextOrganization organization,
+        bool allowAdminAccessToAllCollectionItems)
+    {
+        sutProvider.GetDependency<IApplicationCacheService>()
+            .GetOrganizationAbilityAsync(organization.Id)
+            .Returns(new OrganizationAbility
+            {
+                Id = organization.Id,
+                AllowAdminAccessToAllCollectionItems = allowAdminAccessToAllCollectionItems
+            });
+    }
+}
