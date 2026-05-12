480be678e0ac4784094d3b59e79b86933b843741
Graham Walker <gwalker@bitwarden.com>
2026-05-12 09:51:15 -0500

    PM-33527 fixing the model issue

---
+++ src/Core/Dirt/Entities/OrganizationEventCleanup.cs
@@ -0,0 +1,18 @@
+﻿using Bit.Core.Entities;
+using Bit.Core.Utilities;
+
+namespace Bit.Core.Dirt.Entities;
+
+public class OrganizationEventCleanup : ITableObject<Guid>
+{
+    public Guid Id { get; set; }
+    public Guid OrganizationId { get; set; }
+    public DateTime QueuedAt { get; set; }
+    public DateTime? StartedAt { get; set; }
+    public DateTime? LastProgressAt { get; set; }
+    public DateTime? CompletedAt { get; set; }
+    public long EventsDeletedCount { get; set; }
+    public int Attempts { get; set; }
+    public string? LastError { get; set; }
+    public void SetNewId() => Id = CoreHelpers.GenerateComb();
+}
---
+++ src/Core/Dirt/Repositories/IOrganizationEventCleanupRepository.cs
@@ -0,0 +1,13 @@
+﻿using Bit.Core.Dirt.Entities;
+
+namespace Bit.Core.Dirt.Repositories;
+
+public interface IOrganizationEventCleanupRepository
+{
+    Task CreateAsync(OrganizationEventCleanup cleanup);
+    Task<OrganizationEventCleanup?> ReadNextPendingAsync();
+    Task MarkStartedAsync(Guid id);
+    Task IncrementProgressAsync(Guid id, long delta);
+    Task RecordErrorAsync(Guid id, string message);
+    Task MarkCompletedAsync(Guid id);
+}
