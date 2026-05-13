a07ff015cb9a7c4c91708ba0e6794317fc354064
Thomas Rittson <trittson@bitwarden.com>
2026-05-14 09:58:28 +1000

    Add comment

--- test/Infrastructure.IntegrationTest/Services/IMigrationTesterService.cs
+++ test/Infrastructure.IntegrationTest/Services/IMigrationTesterService.cs
@@ -1,5 +1,12 @@
 ﻿namespace Bit.Infrastructure.IntegrationTest.Services;
 
+// WARNING: this has been disabled because it works by re-running migrations out of order.
+// For MSSQL, this causes problems because the schema may have changed since the data migration was merged,
+// so the data migration may fail.
+// For EF, this causes problems because it reverts all subsequent migrations in order to re-run the data migration,
+// which means the data migration should work but unrelated tests will now fail because the database is not up-to-date.
+// This needs to be redesigned before use.
+
 /// <summary>
 /// Defines the contract for applying a specific database migration across different database providers.
 /// Implementations of this interface are responsible for migration execution logic,
@@ -9,7 +16,7 @@
 /// Each implementation should receive the migration name as a parameter in the constructor
 /// to specify which migration is to be applied.
 /// </remarks>
-[Obsolete("DO NOT USE as this interferes with subsequent database migrations by other teams.")]
+[Obsolete("DO NOT USE as this interferes with subsequent database migrations.")]
 public interface IMigrationTesterService
 {
     /// <summary>
