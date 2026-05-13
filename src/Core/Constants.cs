be82dee8abcf15f14bc1ae6f6509a818c2364853
jaasen-livefront <jaasen@livefront.com>
2026-05-13 11:59:22 -0700

    add feature flag

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -295,6 +295,7 @@ public static class FeatureFlagKeys
     public const string PM34500_StrictCipherDecryption = "pm-34500-strict-cipher-decryption";
     public const string PM28091_AddCopyAndQuickLaunchActions = "pm-28091-add-copy-and-quick-launch-actions";
     public const string PM28192_CipherAttachmentOps = "pm-28192-cipher-attachment-ops-to-sdk";
+    public const string PM32016_RemoveAtRiskCallouts = "pm-32016-remove-at-risk-callouts";
 
     /* Secrets Manager Team */
     public const string SecretsVersioning = "sm-1587-secrets-versioning";
