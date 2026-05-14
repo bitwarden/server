138806f34e0b06a102d43bf7cd5de8d4ff8ef9ac
Stephon Brown <sbrown@livefront.com>
2026-05-14 11:35:55 -0400

    refactor(core): update discount models and extensions to use Source.Coupon

--- src/Core/Billing/Extensions/DiscountExtensions.cs
+++ src/Core/Billing/Extensions/DiscountExtensions.cs
@@ -5,11 +5,11 @@ namespace Bit.Core.Billing.Extensions;
 public static class DiscountExtensions
 {
     public static bool AppliesTo(this Discount discount, SubscriptionItem subscriptionItem)
-        => discount.Coupon.AppliesTo.Products.Contains(subscriptionItem.Price.Product.Id);
+        => discount.Source.Coupon.AppliesTo.Products.Contains(subscriptionItem.Price.Product.Id);
 
     public static bool AppliesTo(this Coupon coupon, SubscriptionItem subscriptionItem)
         => coupon.AppliesTo?.Products?.Contains(subscriptionItem.Price.Product.Id) ?? false;
 
     public static bool IsValid(this Discount? discount)
-        => discount?.Coupon?.Valid ?? false;
+        => discount?.Source?.Coupon?.Valid ?? false;
 }
