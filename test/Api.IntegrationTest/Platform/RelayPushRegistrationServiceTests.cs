b4c3a5835c16576d466864bae14171c7069e163b
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2026-05-18 08:56:28 -0400

    Assert more in mobile test and use actual mobile device type

--- test/Api.IntegrationTest/Platform/RelayPushRegistrationServiceTests.cs
+++ test/Api.IntegrationTest/Platform/RelayPushRegistrationServiceTests.cs
@@ -13,29 +13,31 @@ namespace Bit.Api.IntegrationTest.Platform;
 
 public class RelayPushRegistrationServiceTests
 {
+    private readonly ApiApplicationFactory _cloudApi;
+    private readonly Guid _fakeInstallationId;
     private readonly FakeLogCollector _logCollector;
     private readonly RelayPushRegistrationService _sut;
 
     public RelayPushRegistrationServiceTests()
     {
-        var cloudApi = new ApiApplicationFactory();
-        cloudApi.SubstituteService<IPushRegistrationService>(service => { });
+        _cloudApi = new ApiApplicationFactory();
+        _cloudApi.SubstituteService<IPushRegistrationService>(service => { });
 
-        var fakeInstallationId = Guid.NewGuid();
+        _fakeInstallationId = Guid.NewGuid();
 
-        cloudApi.Identity.SubstituteService<IInstallationRepository>(service =>
+        _cloudApi.Identity.SubstituteService<IInstallationRepository>(service =>
         {
-            service.GetByIdAsync(fakeInstallationId)
+            service.GetByIdAsync(_fakeInstallationId)
                 .Returns(new Installation
                 {
-                    Id = fakeInstallationId,
+                    Id = _fakeInstallationId,
                     Key = "test_key",
                     Enabled = true,
                 });
         });
 
-        var cloudApiHttpClient = cloudApi.CreateClient();
-        var cloudIdentityHttpClient = cloudApi.Identity.CreateClient();
+        var cloudApiHttpClient = _cloudApi.CreateClient();
+        var cloudIdentityHttpClient = _cloudApi.Identity.CreateClient();
 
         var httpClientFactory = Substitute.For<IHttpClientFactory>();
 
@@ -50,7 +52,7 @@ public class RelayPushRegistrationServiceTests
             PushRelayBaseUri = "http://api.localhost"
         };
         globalSettings.Installation.IdentityUri = "http://identity.localhost";
-        globalSettings.Installation.Id = fakeInstallationId;
+        globalSettings.Installation.Id = _fakeInstallationId;
         globalSettings.Installation.Key = "test_key";
 
         var logger = new FakeLogger<RelayPushRegistrationService>();
@@ -85,18 +87,39 @@ public class RelayPushRegistrationServiceTests
     [Fact]
     public async Task MobileData_ShouldNotLogIssues()
     {
+        var deviceId = Guid.NewGuid();
+        var userId = Guid.NewGuid();
+        var identifier = Guid.NewGuid();
+        var organizationId = Guid.NewGuid();
+        var installationId = Guid.NewGuid();
+
         await _sut.CreateOrUpdateRegistrationAsync(
             new PushRegistrationData("PushToken"),
-            deviceId: Guid.NewGuid().ToString(),
-            userId: Guid.NewGuid().ToString(),
-            identifier: Guid.NewGuid().ToString(),
-            DeviceType.ChromeExtension,
-            organizationIds: [Guid.NewGuid().ToString()],
-            installationId: Guid.NewGuid()
+            deviceId.ToString(),
+            userId.ToString(),
+            identifier.ToString(),
+            DeviceType.iOS,
+            [organizationId.ToString()],
+            installationId
         );
 
         var logs = _logCollector.GetSnapshot();
 
         Assert.DoesNotContain(logs, l => l.Level >= LogLevel.Warning);
+
+        // Mobile should also actually successfully make it to the cloud push registration service
+        // with all of its data prefixed with the self host installation id.
+        var mockPushRegistrationService = _cloudApi.GetService<IPushRegistrationService>();
+        await mockPushRegistrationService
+            .Received(1)
+            .CreateOrUpdateRegistrationAsync(
+                new PushRegistrationData("PushToken"),
+                deviceId: $"{_fakeInstallationId}_{deviceId}",
+                userId: $"{_fakeInstallationId}_{userId}",
+                identifier: $"{_fakeInstallationId}_{identifier}",
+                type: DeviceType.iOS,
+                Arg.Is<IEnumerable<string>>(v => v.Single() == $"{_fakeInstallationId}_{organizationId}"),
+                installationId
+            );
     }
 }
