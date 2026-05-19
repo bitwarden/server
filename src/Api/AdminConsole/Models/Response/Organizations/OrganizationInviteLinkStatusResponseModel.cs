3d537b1c364325c5ffedb197d3c3af8a82fae4fe
Rui Tome <rtome@bitwarden.com>
2026-05-19 14:20:53 +0100

    Refactor OrganizationInviteLinkStatusResponseModel and OrganizationInviteLinkStatus to remove OrganizationId property
    - Removed OrganizationId property from both OrganizationInviteLinkStatusResponseModel and OrganizationInviteLinkStatus records to streamline the data model.
    - Updated constructors accordingly to reflect the changes in the response models.

--- src/Api/AdminConsole/Models/Response/Organizations/OrganizationInviteLinkStatusResponseModel.cs
+++ src/Api/AdminConsole/Models/Response/Organizations/OrganizationInviteLinkStatusResponseModel.cs
@@ -4,18 +4,15 @@ namespace Bit.Api.AdminConsole.Models.Response.Organizations;
 
 public class OrganizationInviteLinkStatusResponseModel : ResponseModel
 {
-    public Guid OrganizationId { get; set; }
     public string OrganizationName { get; set; } = null!;
     public bool SeatsAvailable { get; set; }
     public OrganizationInviteLinkSsoResponseModel? Sso { get; set; }
 
     public OrganizationInviteLinkStatusResponseModel(
-        Guid organizationId,
         string organizationName,
         bool seatsAvailable,
         OrganizationInviteLinkSsoResponseModel? sso) : base("inviteLinkStatus")
     {
-        OrganizationId = organizationId;
         OrganizationName = organizationName;
         SeatsAvailable = seatsAvailable;
         Sso = sso;
