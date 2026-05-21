f9bcbc9bf35c6ddd9c5b7695334a9ebad7302087
Alex <adewitt@bitwarden.com>
2026-05-21 12:52:47 -0400

    chore(ef): add UseRiskInsights data migration for self-host
    Adds equivalent UPDATE migration for MySQL, Postgres, and SQLite
    providers used by self-hosted instances. Mirrors the MSSQL script.
    
    [PM-37470]

---
+++ util/PostgresMigrations/Migrations/20260521164709_UseRiskInsightsDataMigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.PostgresMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class UseRiskInsightsDataMigration : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(@"
+                UPDATE
+                    ""Organization""
+                SET
+                    ""UseRiskInsights"" = true
+                WHERE
+                    ""PlanType"" IN (4, 5, 10, 11, 14, 15, 19, 20);
+            ");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(@"
+                UPDATE
+                    ""Organization""
+                SET
+                    ""UseRiskInsights"" = false
+                WHERE
+                    ""PlanType"" IN (4, 5, 10, 11, 14, 15, 19, 20);
+            ");
+    }
+}
---
+++ util/SqliteMigrations/Migrations/20260521164638_UseRiskInsightsDataMigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.SqliteMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class UseRiskInsightsDataMigration : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(@"
+                UPDATE
+                    Organization
+                SET
+                    UseRiskInsights = 1
+                WHERE
+                    PlanType IN (4, 5, 10, 11, 14, 15, 19, 20);
+            ");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(@"
+                UPDATE
+                    Organization
+                SET
+                    UseRiskInsights = 0
+                WHERE
+                    PlanType IN (4, 5, 10, 11, 14, 15, 19, 20);
+            ");
+    }
+}
