9aa3da8c65c8998d7bb3e88b0eab28f8d0bc73ef
Sven <svernyi@bitwarden.com>
2026-05-25 15:14:33 -0500

    Proposed mgiration for backfilling users to have revocation reasons

---
+++ util/PostgresMigrations/Migrations/20260525194940_PopulateOrganizationUserRevocationReason.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.PostgresMigrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulateOrganizationUserRevocationReason : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE ""OrganizationUser""
+                SET ""RevocationReason"" = 0
+                WHERE ""Status"" = -1 AND ""RevocationReason"" IS NULL;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
