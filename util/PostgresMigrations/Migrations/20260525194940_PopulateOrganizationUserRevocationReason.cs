a21d9ff117eaa90712cc01c7e4792822b7ab5e66
Sven <svernyi@bitwarden.com>
2026-05-25 15:28:11 -0500

    fix lint issues

--- util/PostgresMigrations/Migrations/20260525194940_PopulateOrganizationUserRevocationReason.cs
+++ util/PostgresMigrations/Migrations/20260525194940_PopulateOrganizationUserRevocationReason.cs
@@ -2,24 +2,23 @@
 
 #nullable disable
 
-namespace Bit.PostgresMigrations.Migrations
+namespace Bit.PostgresMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class PopulateOrganizationUserRevocationReason : Migration
 {
     /// <inheritdoc />
-    public partial class PopulateOrganizationUserRevocationReason : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.Sql(@"
+        migrationBuilder.Sql(@"
                 UPDATE ""OrganizationUser""
                 SET ""RevocationReason"" = 0
                 WHERE ""Status"" = -1 AND ""RevocationReason"" IS NULL;
             ");
-        }
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
     }
 }
