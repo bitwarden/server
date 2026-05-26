1af7d66727704c0388c6c2a8c35eddd82755acc6
Jared McCannon <jmccannon@bitwarden.com>
2026-05-26 10:27:57 -0500

    added new validorg and organizationuser record for inject valid organization and org user.

---
+++ src/Api/AdminConsole/Attributes/InjectValidOrganizationAndUserAttribute.cs
@@ -0,0 +1,75 @@
+﻿using Bit.Api.AdminConsole.Authorization;
+using Bit.Api.AdminConsole.Models.Request;
+using Bit.Core.Exceptions;
+using Bit.Core.Repositories;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+
+namespace Bit.Api.AdminConsole.Attributes;
+
+[AttributeUsage(AttributeTargets.Parameter)]
+public sealed class InjectValidOrganizationAndUserAttribute(
+    string organizationIdRouteParameter = "orgId",
+    string organizationUserIdRouteParameter = "id") : Attribute
+{
+    public string OrganizationIdRouteParameter { get; } = organizationIdRouteParameter;
+    public string OrganizationUserIdRouteParameter { get; } = organizationUserIdRouteParameter;
+}
+
+public class ValidOrganizationAndUserModelBinderProvider : IModelBinderProvider
+{
+    public IModelBinder? GetBinder(ModelBinderProviderContext context)
+    {
+        if (context.Metadata is not DefaultModelMetadata metadata)
+        {
+            return null;
+        }
+
+        var attr = metadata.Attributes.ParameterAttributes
+            ?.OfType<InjectValidOrganizationAndUserAttribute>()
+            .FirstOrDefault();
+
+        return attr is null
+            ? null
+            : new ValidOrganizationAndUserModelBinder(
+                attr.OrganizationIdRouteParameter,
+                attr.OrganizationUserIdRouteParameter);
+    }
+}
+
+public class ValidOrganizationAndUserModelBinder(
+    string organizationIdRouteParameter,
+    string organizationUserIdRouteParameter) : IModelBinder
+{
+    public async Task BindModelAsync(ModelBindingContext bindingContext)
+    {
+        var orgId = bindingContext.HttpContext.GetOrganizationId(organizationIdRouteParameter);
+        var orgUserId = bindingContext.HttpContext.GetOrganizationUserId(organizationUserIdRouteParameter);
+
+        if (orgId is null || orgUserId is null)
+        {
+            throw new NotFoundException();
+        }
+
+        var organizationRepo = bindingContext.HttpContext.RequestServices.GetRequiredService<IOrganizationRepository>();
+        var organizationUserRepo = bindingContext.HttpContext.RequestServices.GetRequiredService<IOrganizationUserRepository>();
+
+        var organization = await organizationRepo.GetByIdAsync(orgId.Value);
+        if (organization is null)
+        {
+            throw new NotFoundException();
+        }
+
+        var organizationUser = await organizationUserRepo.GetByIdAsync(orgUserId.Value);
+        if (organizationUser is null || organization.Id != organizationUser.OrganizationId)
+        {
+            throw new NotFoundException();
+        }
+
+        bindingContext.Result = ModelBindingResult.Success(new ValidOrganizationAndUser
+        {
+            Organization = organization,
+            OrganizationUser = organizationUser
+        });
+    }
+}
---
+++ src/Api/AdminConsole/Models/Request/ValidOrganizationAndUser.cs
@@ -0,0 +1,10 @@
+﻿using Bit.Core.AdminConsole.Entities;
+using Bit.Core.Entities;
+
+namespace Bit.Api.AdminConsole.Models.Request;
+
+public record ValidOrganizationAndUser
+{
+    public required Organization Organization { get; init; }
+    public required OrganizationUser OrganizationUser { get; init; }
+}
