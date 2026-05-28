4e764cdc13688af9e234062edeed7b5201acad90
Jake Fink <jfink@bitwarden.com>
2026-05-28 15:59:40 -0400

    [PM-27298] Remove consolidated-session-timeout-component feature flag (#7718)

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -201,7 +201,6 @@ public static class FeatureFlagKeys
     public const string UnlockWithMasterPasswordUnlockData = "pm-23246-unlock-with-master-password-unlock-data";
     public const string LinuxBiometricsV2 = "pm-26340-linux-biometrics-v2";
     public const string NoLogoutOnKdfChange = "pm-23995-no-logout-on-kdf-change";
-    public const string ConsolidatedSessionTimeoutComponent = "pm-26056-consolidated-session-timeout-component";
     public const string V2RegistrationTDEJIT = "pm-27279-v2-registration-tde-jit";
     public const string EnableAccountEncryptionV2KeyConnectorRegistration = "enable-account-encryption-v2-key-connector-registration";
     public const string SdkKeyRotation = "pm-30144-sdk-key-rotation";
