51ed6f8c3be27c2c737809fc61523d4db1a16060
Bradley Nagawiecki <107435978+bnagawiecki@users.noreply.github.com>
2026-05-29 09:05:51 -0400

    fix file encoding

--- util/Seeder/Queries/EmergencyAccessInviteQuery.cs
+++ util/Seeder/Queries/EmergencyAccessInviteQuery.cs
@@ -1,4 +1,4 @@
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.Net;
 using Bit.Core.Auth.Models.Business.Tokenables;
