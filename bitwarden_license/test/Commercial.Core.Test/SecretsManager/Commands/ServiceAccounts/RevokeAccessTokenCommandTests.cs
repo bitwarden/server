b210c236d1bb67faca3777449e34384cc2f95070
maxkpower <mpower@bitwarden.com>
2026-05-29 15:43:43 +0200

    [SM-1952] Add access token lifecycle events

--- bitwarden_license/test/Commercial.Core.Test/SecretsManager/Commands/ServiceAccounts/RevokeAccessTokenCommandTests.cs
+++ bitwarden_license/test/Commercial.Core.Test/SecretsManager/Commands/ServiceAccounts/RevokeAccessTokenCommandTests.cs
@@ -39,9 +39,36 @@ public class RevokeAccessTokenCommandTests
             .GetManyByServiceAccountIdAsync(serviceAccount.Id)
             .Returns(new List<ApiKey> { apiKey1, apiKey2 });
 
-        await sutProvider.Sut.RevokeAsync(serviceAccount, new List<Guid> { apiKey1.Id });
+        var result = await sutProvider.Sut.RevokeAsync(serviceAccount, new List<Guid> { apiKey1.Id });
 
         await sutProvider.GetDependency<IApiKeyRepository>().Received(1)
             .DeleteManyAsync(Arg.Is<IEnumerable<ApiKey>>(arg => arg.SequenceEqual(new List<ApiKey> { apiKey1 })));
+        Assert.Equal(new List<ApiKey> { apiKey1 }, result);
+    }
+
+    [Theory]
+    [BitAutoData]
+    public async Task RevokeAsync_NoMatchingTokens_DeletesNothingAndReturnsEmpty(ServiceAccount serviceAccount, SutProvider<RevokeAccessTokensCommand> sutProvider)
+    {
+        var apiKey = new ApiKey
+        {
+            Id = Guid.NewGuid(),
+            ServiceAccountId = serviceAccount.Id,
+            Name = "Test Name",
+            Scope = "Test Scope",
+            EncryptedPayload = "Test EncryptedPayload",
+            Key = "Test Key",
+        };
+
+        sutProvider.GetDependency<IApiKeyRepository>()
+            .GetManyByServiceAccountIdAsync(serviceAccount.Id)
+            .Returns(new List<ApiKey> { apiKey });
+
+        // Ids that do not belong to this service account's tokens
+        var result = await sutProvider.Sut.RevokeAsync(serviceAccount, new List<Guid> { Guid.NewGuid() });
+
+        await sutProvider.GetDependency<IApiKeyRepository>().Received(1)
+            .DeleteManyAsync(Arg.Is<IEnumerable<ApiKey>>(arg => !arg.Any()));
+        Assert.Empty(result);
     }
 }
