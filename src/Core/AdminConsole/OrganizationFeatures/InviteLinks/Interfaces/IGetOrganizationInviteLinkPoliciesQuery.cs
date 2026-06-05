82041b30249990be5766a660b5e0b6f1e5669e3d
Rui Tome <rtome@bitwarden.com>
2026-06-05 11:07:08 +0100

    [PM-38163] Add GetOrganizationInviteLinkPoliciesQuery and interface for fetching organization policies by invite link code

---
+++ src/Core/AdminConsole/OrganizationFeatures/InviteLinks/Interfaces/IGetOrganizationInviteLinkPoliciesQuery.cs
@@ -0,0 +1,17 @@
+﻿using Bit.Core.AdminConsole.Entities;
+using Bit.Core.AdminConsole.Utilities.v2.Results;
+
+namespace Bit.Core.AdminConsole.OrganizationFeatures.InviteLinks.Interfaces;
+
+public interface IGetOrganizationInviteLinkPoliciesQuery
+{
+    /// <summary>
+    /// Gets the enabled policies for an organization by invite link code, for display to an anonymous user.
+    /// </summary>
+    /// <param name="code">The public invite link code (from the URL).</param>
+    /// <returns>
+    /// The enabled <see cref="Policy"/> records for the organization if the link is valid, or an error
+    /// if the link is not found, the organization is disabled, or the feature / policies are unavailable.
+    /// </returns>
+    Task<CommandResult<ICollection<Policy>>> GetPoliciesAsync(Guid code);
+}
