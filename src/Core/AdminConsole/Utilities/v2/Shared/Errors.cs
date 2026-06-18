066cdd60240b5c3cd1d13331eb9e6529aea4f295
Jared McCannon <jmccannon@bitwarden.com>
2026-06-18 14:05:46 -0500

    Removing unused file

--- src/Core/AdminConsole/Utilities/v2/Shared/Errors.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Bit.Core.AdminConsole.Utilities.v2.Shared;
-
-public record OrganizationNotFound() : NotFoundError("Organization not found");
-
-public record OrganizationUserNotFound() : BadRequestError("Organization user not found.");
