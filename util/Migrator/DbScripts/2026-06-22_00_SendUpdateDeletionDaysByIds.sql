a17c622251b523ddf537c0cd62adb8b1110db668
Mike Amirault <mamirault@bitwarden.com>
2026-06-23 11:48:16 -0400

    Remove old renamed SQL procedure, update migration script with new prcedure def

--- util/Migrator/DbScripts/2026-06-22_00_SendUpdateDeletionDaysByIds.sql
+++ util/Migrator/DbScripts/2026-06-22_00_SendUpdateDeletionDaysByIds.sql
@@ -5,6 +5,8 @@ AS
 BEGIN
     SET NOCOUNT ON
 
+    DECLARE @UserIds [dbo].[GuidIdArray]
+
     -- Set field
     UPDATE
         [dbo].[Send]
@@ -15,13 +17,14 @@ BEGIN
         [Id] IN (SELECT * FROM @Ids)
 
     -- Bump account revision dates
-    EXEC [dbo].[User_BumpManyAccountRevisionDates]
-    (
-        SELECT DISTINCT
-            UserId
-        FROM
-            [dbo].[Send]
-        WHERE
-            [Id] IN (SELECT * FROM @Ids)
-    )
+    INSERT INTO @UserIds
+    SELECT DISTINCT
+        UserId
+    FROM
+        [dbo].[Send]
+    WHERE
+        [Id] IN (SELECT * FROM @Ids)
+        AND [UserId] IS NOT NULL
+
+    EXEC [dbo].[User_BumpManyAccountRevisionDates] @UserIds
 END
