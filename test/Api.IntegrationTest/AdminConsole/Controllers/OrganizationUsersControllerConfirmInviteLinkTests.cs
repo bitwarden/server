9d29fcded4662726ab7d242cfcab23c7c0cfe7a4
Jimmy Vo <huynhmaivo82@gmail.com>
2026-07-07 15:33:02 -0400

    [PM-38796] wip

--- test/Api.IntegrationTest/AdminConsole/Controllers/OrganizationUsersControllerConfirmInviteLinkTests.cs
+++ test/Api.IntegrationTest/AdminConsole/Controllers/OrganizationUsersControllerConfirmInviteLinkTests.cs
@@ -77,7 +77,7 @@ public class OrganizationUsersControllerConfirmInviteLinkTests : IClassFixture<A
 
         // Act
         var response = await joinerClient.PostAsJsonAsync(
-            "/organizations/users/invite-link/link-confirm", BuildConfirmRequest(code));
+            "/organizations/users/invite-link/confirm", BuildConfirmRequest(code));
 
         // Assert
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
@@ -101,7 +101,7 @@ public class OrganizationUsersControllerConfirmInviteLinkTests : IClassFixture<A
 
         // Act
         var response = await joinerClient.PostAsJsonAsync(
-            "/organizations/users/invite-link/link-confirm", BuildConfirmRequest(Guid.NewGuid()));
+            "/organizations/users/invite-link/confirm", BuildConfirmRequest(Guid.NewGuid()));
 
         // Assert
         Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
@@ -115,13 +115,13 @@ public class OrganizationUsersControllerConfirmInviteLinkTests : IClassFixture<A
         var (joinerClient, _) = await CreateJoinerClientAsync();
 
         var firstResponse = await joinerClient.PostAsJsonAsync(
-            "/organizations/users/invite-link/link-confirm", BuildConfirmRequest(code));
+            "/organizations/users/invite-link/confirm", BuildConfirmRequest(code));
         Assert.Equal(HttpStatusCode.OK, firstResponse.StatusCode);
 
         // Act
         // Confirming again finds the already-confirmed membership and fails with a 400 validation problem.
         var secondResponse = await joinerClient.PostAsJsonAsync(
-            "/organizations/users/invite-link/link-confirm", BuildConfirmRequest(code));
+            "/organizations/users/invite-link/confirm", BuildConfirmRequest(code));
 
         // Assert
         Assert.Equal(HttpStatusCode.BadRequest, secondResponse.StatusCode);
