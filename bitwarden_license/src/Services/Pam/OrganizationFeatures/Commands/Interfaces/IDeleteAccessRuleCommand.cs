fb71907a7a4dde672d2a2a987b0eb061b70bb626
Hinton <hinton@users.noreply.github.com>
2026-07-14 13:44:47 +0200

    Implement AccessRule endpoints handler, commands, validator, and API models

---
+++ bitwarden_license/src/Services/Pam/OrganizationFeatures/Commands/Interfaces/IDeleteAccessRuleCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace Bit.Services.Pam.OrganizationFeatures.Commands.Interfaces;
+
+public interface IDeleteAccessRuleCommand
+{
+    Task DeleteAsync(Guid organizationId, Guid id, Guid? deletedBy);
+}
--- bitwarden_license/src/Services/Pam/Utilities/ServiceCollectionExtensions.cs
+++ bitwarden_license/src/Services/Pam/Utilities/ServiceCollectionExtensions.cs
@@ -1,6 +1,10 @@
 ﻿using Bit.HttpExtensions;
 using Bit.Services.Pam.Api.Endpoints;
 using Bit.Services.Pam.Api.Endpoints.Handlers;
+using Bit.Services.Pam.OrganizationFeatures.Commands;
+using Bit.Services.Pam.OrganizationFeatures.Commands.Interfaces;
+using Bit.Services.Pam.Services;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Bit.Services.Pam.Utilities;
 
@@ -13,6 +17,13 @@ public static class ServiceCollectionExtensions
         services.AddScoped<AccessRequestEndpointsHandler>();
         services.AddScoped<AccessRuleEndpointsHandler>();
         services.AddScoped<CipherLeaseEndpointsHandler>();
+
+        // AccessRule write path.
+        services.TryAddSingleton(TimeProvider.System);
+        services.AddSingleton<IAccessRuleValidator, AccessRuleValidator>();
+        services.AddScoped<ICreateAccessRuleCommand, CreateAccessRuleCommand>();
+        services.AddScoped<IUpdateAccessRuleCommand, UpdateAccessRuleCommand>();
+        services.AddScoped<IDeleteAccessRuleCommand, DeleteAccessRuleCommand>();
 
         services.AddPamOpenApiEndpointDataSource();
 
