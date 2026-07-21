0d2aaff4bf920256503c3a0f39ade4c669ac9973
Stephon Brown <sbrown@livefront.com>
2026-07-21 17:35:02 -0400

    fix: run dotnet format

--- util/PostgresMigrations/Migrations/20260721211850_AddRenewalNotificationSentDateToOrganizationPlanMigrationCohortAssignment.cs
+++ util/PostgresMigrations/Migrations/20260721211850_AddRenewalNotificationSentDateToOrganizationPlanMigrationCohortAssignment.cs
@@ -1,32 +1,30 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Bit.PostgresMigrations.Migrations
+namespace Bit.PostgresMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class AddRenewalNotificationSentDateToOrganizationPlanMigrationCohortAssignment : Migration
 {
     /// <inheritdoc />
-    public partial class AddRenewalNotificationSentDateToOrganizationPlanMigrationCohortAssignment : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<DateTime>(
-                name: "RenewalNotificationSentDate",
-                table: "OrganizationPlanMigrationCohortAssignment",
-                type: "timestamp with time zone",
-                nullable: true);
+        migrationBuilder.AddColumn<DateTime>(
+            name: "RenewalNotificationSentDate",
+            table: "OrganizationPlanMigrationCohortAssignment",
+            type: "timestamp with time zone",
+            nullable: true);
 
-            migrationBuilder.Sql(
-                "UPDATE \"OrganizationPlanMigrationCohortAssignment\" SET \"RenewalNotificationSentDate\" = \"ScheduledDate\" WHERE \"ScheduledDate\" IS NOT NULL");
-        }
+        migrationBuilder.Sql(
+            "UPDATE \"OrganizationPlanMigrationCohortAssignment\" SET \"RenewalNotificationSentDate\" = \"ScheduledDate\" WHERE \"ScheduledDate\" IS NOT NULL");
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "RenewalNotificationSentDate",
-                table: "OrganizationPlanMigrationCohortAssignment");
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "RenewalNotificationSentDate",
+            table: "OrganizationPlanMigrationCohortAssignment");
     }
 }
