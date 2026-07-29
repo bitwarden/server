d8d78f82988069b5318b68e12c864f09d4b87e0d
Brandon <btreston@bitwarden.com>
2026-07-29 16:34:10 -0400

    update auto confirm errors

--- src/Core/AdminConsole/OrganizationFeatures/OrganizationUsers/AutoConfirmUser/Errors.cs
+++ src/Core/AdminConsole/OrganizationFeatures/OrganizationUsers/AutoConfirmUser/Errors.cs
@@ -11,6 +11,6 @@ public record UserDoesNotHaveTwoFactorEnabled() : BadRequestError("User does not
 public record UserCannotBelongToAnotherOrganization(string Email) : BadRequestError($"Cannot confirm {Email} until they leave all other organization vaults.");
 public record OtherOrganizationDoesNotAllowOtherMembership(string Email) : BadRequestError($"Cannot confirm {Email} because they are a member of another organization which forbids it.");
 public record AutomaticallyConfirmUsersPolicyIsNotEnabled() : BadRequestError("Cannot confirm this member because the Automatically Confirm Users policy is not enabled.");
-public record ProviderUsersCannotJoin() : BadRequestError("An organization the user is a part of has enabled Automatic User Confirmation policy, and it does not support provider users joining.");
-public record UserCannotJoinProvider() : BadRequestError("An organization the user is a part of has enabled Automatic User Confirmation policy, and it does not support the user joining a provider.");
+public record ProviderUsersCannotJoin() : BadRequestError("This member belongs to an organization vault that has automatically confirm invited users enabled. Members with the Provider user type can't join additional organization vaults.");
+public record UserCannotJoinProvider() : BadRequestError("This member belongs to an organization vault that has automatically confirm invited users enabled. Members with the Provider user type can't join additional organization vaults.");
 public record CurrentOrganizationUserIsNotPresentInRequest() : BadRequestError("The current organization user does not exist in the request.");
