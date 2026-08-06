cded07940ee20bd1dba6b1691c3c22dadfb40485
Ben Brooks <56796209+bensbits91@users.noreply.github.com>
2026-08-06 07:10:08 -0700

    [PM-41311] Register Fill Assist policy (#8123)
    * [PM-41311] Register Fill Assist policy
    * [PM-41311] Enforce required rulesUrl and HTTPS on Fill Assist policy
    * [PM-41311] Fix disabling Fill Assist policy, case-insensitive HTTPS check
    * [PM-41311] Move enable/disable validation to IPolicyValidationEvent
    * [PM-41311] Trim integration tests to Success path only
    * [PM-41311] Move Fill Assist URL validation into event handler
    
    ---------
    
    Signed-off-by: Ben Brooks <bbrooks@bitwarden.com>

---
+++ src/Core/AdminConsole/Models/Data/Organizations/Policies/FillAssistPolicyData.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Bit.Core.AdminConsole.Models.Data.Organizations.Policies;
+
+public class FillAssistPolicyData : IPolicyDataModel
+{
+    [Required]
+    [Url]
+    [RegularExpression("(?i)^https://.*", ErrorMessage = "RulesUrl must use HTTPS.")]
+    [JsonPropertyName("rulesUrl")]
+    public string? RulesUrl { get; set; }
+}
