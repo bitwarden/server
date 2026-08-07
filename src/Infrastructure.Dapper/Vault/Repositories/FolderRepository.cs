55067abc5b6c4fc0ee45b8bdd663b9c13ec249b4
gbubemismith <gsmithwalter@gmail.com>
2026-08-07 13:10:57 -0400

    Wrap Folder_DeleteByIds in a transaction at the repository level

--- src/Infrastructure.Dapper/Vault/Repositories/FolderRepository.cs
+++ src/Infrastructure.Dapper/Vault/Repositories/FolderRepository.cs
@@ -51,10 +51,24 @@ public class FolderRepository : Repository<Folder, Guid>, IFolderRepository
     public async Task DeleteManyAsync(IEnumerable<Guid> folderIds, Guid userId)
     {
         await using var connection = new SqlConnection(ConnectionString);
-        await connection.ExecuteAsync(
-            $"[{Schema}].[Folder_DeleteByIds]",
-            new { Ids = folderIds.ToGuidIdArrayTVP(), UserId = userId },
-            commandType: CommandType.StoredProcedure);
+        await connection.OpenAsync();
+
+        await using var transaction = await connection.BeginTransactionAsync();
+        try
+        {
+            await connection.ExecuteAsync(
+                $"[{Schema}].[Folder_DeleteByIds]",
+                new { Ids = folderIds.ToGuidIdArrayTVP(), UserId = userId },
+                commandType: CommandType.StoredProcedure,
+                transaction: transaction);
+
+            await transaction.CommitAsync();
+        }
+        catch
+        {
+            await transaction.RollbackAsync();
+            throw;
+        }
     }
 
     /// <inheritdoc />
