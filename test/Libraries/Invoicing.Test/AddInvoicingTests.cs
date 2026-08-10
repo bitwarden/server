2a79a95fa7c98aee6988519ab72b0506754459fb
Kyle Denney <4227399+kdenney@users.noreply.github.com>
2026-08-10 14:46:23 -0500

    feat(billing): add the invoice preview service and register the projection

---
+++ test/Libraries/Invoicing.Test/AddInvoicingTests.cs
@@ -0,0 +1,24 @@
+﻿using Bit.Core.Billing.Services;
+using Bit.Invoicing.InvoicePreviews;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+using Xunit;
+
+namespace Bit.Invoicing.Test;
+
+public class AddInvoicingTests
+{
+    [Fact]
+    public void AddInvoicing_RegistersTheServiceClientAndBuilder()
+    {
+        var services = new ServiceCollection();
+        services.AddSingleton(Substitute.For<IStripeAdapter>());
+        services.AddLogging();
+
+        services.AddInvoicing();
+        var provider = services.BuildServiceProvider();
+
+        Assert.NotNull(provider.GetService<IInvoicePreviewService>());
+        Assert.NotNull(provider.GetService<InvoicePreviewBuilder>());
+    }
+}
