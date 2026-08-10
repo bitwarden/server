d73e19fae160f422165ec4cc9b8333584a5874b9
Leslie Tilton <23057410+Banrion@users.noreply.github.com>
2026-08-10 13:49:33 -0500

    style: apply file-scoped namespaces to generated EF migrations

--- util/PostgresMigrations/Migrations/20260806182701_AddEventOrganizationIdIndex.cs
+++ util/PostgresMigrations/Migrations/20260806182701_AddEventOrganizationIdIndex.cs
@@ -2,26 +2,25 @@
 
 #nullable disable
 
-namespace Bit.PostgresMigrations.Migrations
+namespace Bit.PostgresMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class AddEventOrganizationIdIndex : Migration
 {
     /// <inheritdoc />
-    public partial class AddEventOrganizationIdIndex : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateIndex(
-                name: "IX_Event_OrganizationId",
-                table: "Event",
-                column: "OrganizationId");
-        }
+        migrationBuilder.CreateIndex(
+            name: "IX_Event_OrganizationId",
+            table: "Event",
+            column: "OrganizationId");
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropIndex(
-                name: "IX_Event_OrganizationId",
-                table: "Event");
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropIndex(
+            name: "IX_Event_OrganizationId",
+            table: "Event");
     }
 }
