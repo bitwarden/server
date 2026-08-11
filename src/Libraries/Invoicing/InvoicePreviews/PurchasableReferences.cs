665cb0d4a7f980031e099c803beb2854ec5e0e0b
Kyle Denney <4227399+kdenney@users.noreply.github.com>
2026-08-11 18:02:12 -0500

    feat(billing): add central purchasable-reference product table

---
+++ src/Libraries/Invoicing/InvoicePreviews/PurchasableReferences.cs
@@ -0,0 +1,19 @@
+﻿using Bit.Core.Billing.Constants;
+using Bit.Core.Billing.Enums;
+
+namespace Bit.Invoicing.InvoicePreviews;
+
+internal static class PurchasableReferences
+{
+    private static readonly IReadOnlyDictionary<string, ProductType> ProductsByReference = new Dictionary<string, ProductType>
+    {
+        [StripeConstants.PurchasableReferences.PasswordManagerSeat] = ProductType.PasswordManager,
+        [StripeConstants.PurchasableReferences.PasswordManagerStorage] = ProductType.PasswordManager,
+        [StripeConstants.PurchasableReferences.SecretsManagerSeat] = ProductType.SecretsManager,
+        [StripeConstants.PurchasableReferences.SecretsManagerServiceAccount] = ProductType.SecretsManager,
+    };
+
+    internal static bool IsKnown(string reference) => ProductsByReference.ContainsKey(reference);
+
+    internal static ProductType ProductOf(string reference) => ProductsByReference[reference];
+}
