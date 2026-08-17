8c0039242877475edca04c73775d23df54f5cf56
Anders Åberg <github@andersaberg.com>
2026-08-17 21:12:57 +0200

    Add PAM approver-inbox and access-request push notifications

---
+++ bitwarden_license/src/Services/Pam/Services/RequesterNotifier.cs
@@ -0,0 +1,31 @@
+﻿using Bit.Core.Enums;
+using Bit.Core.Models;
+using Bit.Core.Platform.Push;
+
+namespace Bit.Services.Pam.Services;
+
+public class RequesterNotifier : IRequesterNotifier
+{
+    private readonly IPushNotificationService _pushNotificationService;
+
+    public RequesterNotifier(IPushNotificationService pushNotificationService)
+    {
+        _pushNotificationService = pushNotificationService;
+    }
+
+    public Task NotifyRequesterAsync(Guid requesterId)
+    {
+        return _pushNotificationService.PushAsync(new PushNotification<UserPushNotification>
+        {
+            Type = PushType.RefreshAccessRequest,
+            Target = NotificationTarget.User,
+            TargetId = requesterId,
+            Payload = new UserPushNotification
+            {
+                UserId = requesterId,
+                Date = DateTime.UtcNow,
+            },
+            ExcludeCurrentContext = false,
+        });
+    }
+}
