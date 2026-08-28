b8664186cefb8774d87615198d7399e4ed1773c8
JaredScar <thewolfbadger@gmail.com>
2026-08-28 16:39:31 -0400

    Update server-side validation messages for seat count and such

--- src/Core/AdminConsole/OrganizationFeatures/OrganizationUsers/InviteUsers/Validation/Provider/Errors.cs
+++ src/Core/AdminConsole/OrganizationFeatures/OrganizationUsers/InviteUsers/Validation/Provider/Errors.cs
@@ -2,12 +2,18 @@
 
 namespace Bit.Core.AdminConsole.OrganizationFeatures.OrganizationUsers.InviteUsers.Validation.Provider;
 
-public record ProviderBillableSeatLimitError(InviteOrganizationProvider InvalidRequest) : Error<InviteOrganizationProvider>(Code, InvalidRequest)
+public record ProviderBillableSeatLimitError(InviteOrganizationProvider InvalidRequest) : Error<InviteOrganizationProvider>(GetErrorMessage(InvalidRequest), InvalidRequest)
 {
-    public const string Code = "Seat limit has been reached. Please contact your provider to add more seats.";
+    private static string GetErrorMessage(InviteOrganizationProvider invalidRequest) =>
+        string.Format(Code, invalidRequest.Seats);
+
+    public const string Code = "Seat limit of {0} has been reached. Contact your provider to purchase additional seats.";
 }
 
-public record ProviderResellerSeatLimitError(InviteOrganizationProvider InvalidRequest) : Error<InviteOrganizationProvider>(Code, InvalidRequest)
+public record ProviderResellerSeatLimitError(InviteOrganizationProvider InvalidRequest) : Error<InviteOrganizationProvider>(GetErrorMessage(InvalidRequest), InvalidRequest)
 {
-    public const string Code = "Seat limit has been reached. Contact your provider to purchase additional seats.";
+    private static string GetErrorMessage(InviteOrganizationProvider invalidRequest) =>
+        string.Format(Code, invalidRequest.Seats);
+
+    public const string Code = "Seat limit of {0} has been reached. Contact your provider to purchase additional seats.";
 }
