674b2af48d7f5e8c0283703eba7b338ba75614ee
maxkpower <m.power@bitwarden.com>
2026-09-03 22:59:40 +0200

    Index Device by user and last activity date

---
+++ util/PostgresMigrations/Migrations/20260903163355_AddDeviceUserIdLastActivityDateIndex.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.PostgresMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class AddDeviceUserIdLastActivityDateIndex : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateIndex(
+            name: "IX_Device_UserId_LastActivityDate_Type",
+            table: "Device",
+            columns: new[] { "UserId", "LastActivityDate", "Type" });
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropIndex(
+            name: "IX_Device_UserId_LastActivityDate_Type",
+            table: "Device");
+    }
+}
