20a67ee4bdd6a5f45c76821b533b187e910d3428
Jared McCannon <jmccannon@bitwarden.com>
2026-09-10 08:45:48 -0500

    [PM-43035] - Set ActingUser Type for providers (#8313)
    * Adding test for bug and how we set standaruser orgusertype that affect orguservalidation.
    
    * Added query for retrieving acting user with new provider user type.
    
    * Updated to encapsulate provideruser better. updated tests and validation service since provider users shouldn't get there.
    
    * fixing exception type
    
    * Cleaned up provider user use case for editing a user. depcreated old property for provider or owner and added explicit is provider bool.
    
    * added obsolete
    
    * Few touch ups.
    
    (cherry picked from commit c3f2d65295f430eaff30034ab6a8714beba7bb60)

--- src/Core/AdminConsole/Models/Data/SystemUser.cs
+++ src/Core/AdminConsole/Models/Data/SystemUser.cs
@@ -5,7 +5,7 @@ namespace Bit.Core.AdminConsole.Models.Data;
 public class SystemUser(EventSystemUser systemUser) : IActingUser
 {
     public Guid? UserId => throw new Exception($"{nameof(SystemUserType)} does not have a {nameof(UserId)}.");
-
+    [Obsolete("This property is obsolete.")]
     public bool IsOrganizationOwnerOrProvider => false;
     public EventSystemUser? SystemUserType { get; } = systemUser;
 }
