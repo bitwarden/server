238b36358bfa206f4737152261cc060e347401a9
maxkpower <post@maximilianpower.com>
2026-09-17 00:10:30 +0200

    SM-2093: Prefix machine account access tokens with bw_ behind feature flag

--- bitwarden_license/src/Commercial.Core/SecretsManager/Commands/AccessTokens/CreateAccessTokenCommand.cs
+++ bitwarden_license/src/Commercial.Core/SecretsManager/Commands/AccessTokens/CreateAccessTokenCommand.cs
@@ -1,22 +1,27 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
+using Bit.Core;
 using Bit.Core.Exceptions;
 using Bit.Core.SecretsManager.Commands.AccessTokens.Interfaces;
 using Bit.Core.SecretsManager.Entities;
 using Bit.Core.SecretsManager.Models.Data;
 using Bit.Core.SecretsManager.Repositories;
 using Bit.Core.Utilities;
+using Bitwarden.Server.Sdk.Features;
 
 namespace Bit.Commercial.Core.SecretsManager.Commands.AccessTokens;
 
 public class CreateAccessTokenCommand : ICreateAccessTokenCommand
 {
-    private const int _clientSecretMaxLength = 30;
+    private const int _clientSecretRandomLength = 30;
+    private const string _clientSecretPrefix = "bw_";
     private readonly IApiKeyRepository _apiKeyRepository;
+    private readonly IFeatureService _featureService;
 
-    public CreateAccessTokenCommand(IApiKeyRepository apiKeyRepository)
+    public CreateAccessTokenCommand(IApiKeyRepository apiKeyRepository, IFeatureService featureService)
     {
         _apiKeyRepository = apiKeyRepository;
+        _featureService = featureService;
     }
 
     public async Task<ApiKeyClientSecretDetails> CreateAsync(ApiKey apiKey)
@@ -26,7 +31,11 @@ public class CreateAccessTokenCommand : ICreateAccessTokenCommand
             throw new BadRequestException();
         }
 
-        var clientSecret = CoreHelpers.SecureRandomString(_clientSecretMaxLength);
+        var clientSecret = CoreHelpers.SecureRandomString(_clientSecretRandomLength);
+        if (_featureService.IsEnabled(FeatureFlagKeys.Sm2093MachineAccountTokenPrefix))
+        {
+            clientSecret = _clientSecretPrefix + clientSecret;
+        }
         apiKey.ClientSecretHash = GetHash(clientSecret);
         var result = await _apiKeyRepository.CreateAsync(apiKey);
         return new ApiKeyClientSecretDetails { ApiKey = result, ClientSecret = clientSecret };
