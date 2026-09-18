1c7aac7713999a2365fb1b084636c5bb6e54ab0a
Anders Åberg <github@andersaberg.com>
2026-09-18 12:28:47 +0200

    Convert the audit access connector rename migrations to file-scoped namespaces

--- util/PostgresMigrations/Migrations/20260917231912_RenameAuditAccessConnectorColumns.cs
+++ util/PostgresMigrations/Migrations/20260917231912_RenameAuditAccessConnectorColumns.cs
@@ -2,37 +2,36 @@
 
 #nullable disable
 
-namespace Bit.PostgresMigrations.Migrations
+namespace Bit.PostgresMigrations.Migrations;
+
+/// <inheritdoc />
+public partial class RenameAuditAccessConnectorColumns : Migration
 {
     /// <inheritdoc />
-    public partial class RenameAuditAccessConnectorColumns : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.RenameColumn(
-                name: "DaemonName",
-                table: "AccessAuditEvent",
-                newName: "AccessConnectorName");
+        migrationBuilder.RenameColumn(
+            name: "DaemonName",
+            table: "AccessAuditEvent",
+            newName: "AccessConnectorName");
 
-            migrationBuilder.RenameColumn(
-                name: "DaemonId",
-                table: "AccessAuditEvent",
-                newName: "AccessConnectorId");
-        }
+        migrationBuilder.RenameColumn(
+            name: "DaemonId",
+            table: "AccessAuditEvent",
+            newName: "AccessConnectorId");
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.RenameColumn(
-                name: "AccessConnectorName",
-                table: "AccessAuditEvent",
-                newName: "DaemonName");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.RenameColumn(
+            name: "AccessConnectorName",
+            table: "AccessAuditEvent",
+            newName: "DaemonName");
 
-            migrationBuilder.RenameColumn(
-                name: "AccessConnectorId",
-                table: "AccessAuditEvent",
-                newName: "DaemonId");
-        }
+        migrationBuilder.RenameColumn(
+            name: "AccessConnectorId",
+            table: "AccessAuditEvent",
+            newName: "DaemonId");
     }
 }
