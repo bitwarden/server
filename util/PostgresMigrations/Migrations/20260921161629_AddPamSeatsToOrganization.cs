fd23e35e1f4e0f3fee7eabd6a85d7cf5ee861a8d
Alex Morask <amorask@bitwarden.com>
2026-09-21 11:27:40 -0500

    feat: generate EF migrations for PamSeats/MaxAutoscalePamSeats

---
+++ util/PostgresMigrations/Migrations/20260921161629_AddPamSeatsToOrganization.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.PostgresMigrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPamSeatsToOrganization : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MaxAutoscalePamSeats",
+                table: "Organization",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PamSeats",
+                table: "Organization",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaxAutoscalePamSeats",
+                table: "Organization");
+
+            migrationBuilder.DropColumn(
+                name: "PamSeats",
+                table: "Organization");
+        }
+    }
+}
