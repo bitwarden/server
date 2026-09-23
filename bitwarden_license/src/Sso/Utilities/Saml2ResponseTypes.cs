b8b3d9329f205955b42abc872cd3718782d381f4
enmande <3836813+enmande@users.noreply.github.com>
2026-09-23 08:48:11 -0400

    refactor(saml2): Move to feature folder

--- bitwarden_license/src/Sso/Utilities/Saml2ResponseTypes.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Bit.Sso.Utilities;
-
-public static class Saml2ResponseTypes
-{
-    public const string SuccessStatus = "urn:oasis:names:tc:SAML:2.0:status:Success";
-    public const string ResponderStatus = "urn:oasis:names:tc:SAML:2.0:status:Responder";
-    public const string AuthnFailedStatus = "urn:oasis:names:tc:SAML:2.0:status:AuthnFailed";
-}
