8551fb499a509be8bd60e1157c13d38898d3403b
Vince Grassia <593223+vgrassia@users.noreply.github.com>
2026-09-25 10:34:27 -0400

    [BRE-1175] Fix Teams integration tenant (#8452)

--- src/Core/Settings/GlobalSettings.cs
+++ src/Core/Settings/GlobalSettings.cs
@@ -307,6 +307,7 @@ public class GlobalSettings : IGlobalSettings
         public virtual string ClientId { get; set; }
         public virtual string ClientSecret { get; set; }
         public virtual string Scopes { get; set; }
+        public virtual string TenantId { get; set; }
     }
 
     public class EventLoggingSettings
